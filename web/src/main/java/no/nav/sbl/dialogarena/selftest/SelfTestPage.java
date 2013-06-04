17e60ce440c4875c52fa7b0749dfef4f3a382e89
Jarle Svendsrud <Jarle.Svendsrud@nav.no>
2013-06-04 11:35:24 +0000

    La til logger på selftestpage
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/modiabrukerdialog/trunk@41476 6ec884bf-e345-0410-bd42-d282eb737ba0

--- web/src/main/java/no/nav/sbl/dialogarena/selftest/SelfTestPage.java
+++ web/src/main/java/no/nav/sbl/dialogarena/selftest/SelfTestPage.java
@@ -1,10 +1,15 @@
 package no.nav.sbl.dialogarena.selftest;
 
 import org.apache.wicket.markup.html.WebPage;
+import org.slf4j.Logger;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 public class SelfTestPage extends WebPage {
 
-    public SelfTestPage() {
+    private static final Logger logger = getLogger(SelfTestPage.class);
 
+    public SelfTestPage() {
+        logger.info("entered SelfTestPage!");
     }
 }
