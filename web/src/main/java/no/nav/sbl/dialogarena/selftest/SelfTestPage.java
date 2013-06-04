454a167cc07a86cfe9061cfaeea104ce3aa7d9ec
Jarle Svendsrud <Jarle.Svendsrud@nav.no>
2013-06-04 11:43:09 +0000

    La opp basics for selftestpage
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/modiabrukerdialog/trunk@41478 6ec884bf-e345-0410-bd42-d282eb737ba0

--- web/src/main/java/no/nav/sbl/dialogarena/selftest/SelfTestPage.java
+++ web/src/main/java/no/nav/sbl/dialogarena/selftest/SelfTestPage.java
@@ -1,7 +1,14 @@
 package no.nav.sbl.dialogarena.selftest;
 
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.PropertyListView;
 import org.slf4j.Logger;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -11,5 +18,51 @@ public class SelfTestPage extends WebPage {
 
     public SelfTestPage() {
         logger.info("entered SelfTestPage!");
+        List<ServiceStatus> statusList = new ArrayList<>();
+        statusList.add(new ServiceStatus("name", "status", 0));
+        add(new ServiceStatusListView("serviceStatusTable", statusList));
     }
+
+    private static class ServiceStatusListView extends PropertyListView<ServiceStatus> {
+
+        private static final long serialVersionUID = 1L;
+
+        public ServiceStatusListView(String id, List<ServiceStatus> statusList) {
+            super(id, statusList);
+        }
+
+        @Override
+        protected void populateItem(ListItem<ServiceStatus> listItem) {
+            listItem.add(new Label("name"), new Label("status"), new Label("durationMilis"));
+        }
+    }
+
+    private static class ServiceStatus implements Serializable {
+
+        private static final long serialVersionUID = 1L;
+
+        private final String name;
+        private final String status;
+        private final long durationMilis;
+
+        public ServiceStatus(String name, String status, long durationMilis) {
+            this.name = name;
+            this.status = status;
+            this.durationMilis = durationMilis;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getStatus() {
+            return status;
+        }
+
+        public long getDurationMilis() {
+            return durationMilis;
+        }
+
+    }
+
 }
