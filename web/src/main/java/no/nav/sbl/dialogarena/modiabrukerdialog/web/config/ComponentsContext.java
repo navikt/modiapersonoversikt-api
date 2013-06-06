06a8427f3990a183a3133f968208351958474544
Stian Landsnes <Stian.Landsnes@nav.no>
2013-06-06 09:00:54 +0000

    Manuell import av configurasjonsklasser
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/modiabrukerdialog/trunk@41650 6ec884bf-e345-0410-bd42-d282eb737ba0

---
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/ComponentsContext.java
@@ -0,0 +1,16 @@
+package no.nav.sbl.dialogarena.modiabrukerdialog.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import({
+    no.nav.dialogarena.modiabrukerdialog.example.config.ExampleProdContext.class,
+    no.nav.kjerneinfo.consumer.config.ConsumerConfig.class,
+    no.nav.kontrakter.consumer.config.ConsumerConfig.class,
+    no.nav.sykmeldingsperioder.config.SykmeldingsperioderPanelConfig.class,
+    no.nav.kjerneinfo.consumer.config.MockConsumerConfig.class
+})
+public class ComponentsContext {
+
+}
