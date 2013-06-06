e518e34146cb5739deb8c5993c72686dffc413be
Stian Landsnes <Stian.Landsnes@nav.no>
2013-06-06 10:08:59 +0000

    test component
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/modiabrukerdialog/trunk@41662 6ec884bf-e345-0410-bd42-d282eb737ba0

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/ComponentsContext.java
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/ComponentsContext.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import({
-    no.nav.dialogarena.modiabrukerdialog.example.config.ExampleProdContext.class,
+    no.nav.dialogarena.modiabrukerdialog.example.config.ExampleContext.class,
     no.nav.kjerneinfo.consumer.config.ConsumerConfig.class,
     no.nav.kontrakter.consumer.config.ConsumerConfig.class,
     no.nav.sykmeldingsperioder.config.SykmeldingsperioderPanelConfig.class,
