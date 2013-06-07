f00fa73c90f2805c752af4e0bae8ece8f1b1a319
Øyvind Norsveen <Oyvind.Nordsveen@nav.no>
2013-06-07 10:24:48 +0000

    Fjernet ubrukt dependency, og fikset 404 feil ved lasting av side
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/modiabrukerdialog/trunk@41765 6ec884bf-e345-0410-bd42-d282eb737ba0

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/ComponentsContext.java
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/ComponentsContext.java
@@ -5,11 +5,10 @@ import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import({
-    no.nav.dialogarena.modiabrukerdialog.example.config.ExampleContext.class,
-    no.nav.kjerneinfo.consumer.config.ConsumerConfig.class,
-    no.nav.kontrakter.consumer.config.ConsumerConfig.class,
-    no.nav.sykmeldingsperioder.config.SykmeldingsperioderPanelConfig.class,
-    no.nav.kjerneinfo.consumer.config.MockConsumerConfig.class
+        no.nav.dialogarena.modiabrukerdialog.example.config.ExampleContext.class,
+        no.nav.kjerneinfo.consumer.config.ConsumerConfig.class,
+        no.nav.sykmeldingsperioder.config.SykmeldingsperioderPanelConfig.class,
+        no.nav.kjerneinfo.consumer.config.MockConsumerConfig.class
 })
 public class ComponentsContext {
 
