ceb56bbac1c243d567353c39db5e2b78f33b0922
Jarle Søberg <Jarle.Soberg@nav.no>
2013-06-17 06:50:24 +0000

    MODLIME-67
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/modiabrukerdialog/branches/med_komponenter@42429 6ec884bf-e345-0410-bd42-d282eb737ba0

--- example-component/src/main/java/no/nav/dialogarena/modiabrukerdialog/example/config/ExampleContext.java
+++ example-component/src/main/java/no/nav/dialogarena/modiabrukerdialog/example/config/ExampleContext.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Profile("test")
+@Profile({"test", "default"})
 @Configuration
 public class ExampleContext {
 
