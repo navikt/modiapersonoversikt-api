5a606041fa5bbda72ab49882e2da1ad3b42404eb
Øyvind Norsveen <Oyvind.Nordsveen@nav.no>
2013-06-20 11:51:46 +0000

    Ryddet i spring context
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/modiabrukerdialog/trunk@42782 6ec884bf-e345-0410-bd42-d282eb737ba0

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/ComponentsContext.java
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/ComponentsContext.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Import;
 
         //        , no.nav.kjerneinfo.kontrakter.config.KontrakterPanelConfig.class
 
-        , no.nav.personsok.config.spring.SecurityPolicyConfig.class    // flytt inn i PersonsokConfig på ett vis
         , no.nav.personsok.config.spring.PersonsokConfig.class
 
         //		kjerneinfo/visittkort/hentPerson
