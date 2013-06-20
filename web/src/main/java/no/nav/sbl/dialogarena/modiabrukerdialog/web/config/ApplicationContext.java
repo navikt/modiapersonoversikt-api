7325fc2f64fe8cb0af87911ddb16f4ddd556b7b9
Stian Landsnes <Stian.Landsnes@nav.no>
2013-06-20 14:34:13 +0000

    Lagt til tjenesteressurser i app-config
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/modiabrukerdialog/trunk@42806 6ec884bf-e345-0410-bd42-d282eb737ba0

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/ApplicationContext.java
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/ApplicationContext.java
@@ -2,13 +2,14 @@ package no.nav.sbl.dialogarena.modiabrukerdialog.web.config;
 
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.WicketApplication;
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.pages.intern.LamellHandler;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
-@Import({TemporaryEnvironmentContext.class, ComponentsContext.class})
+@Import({ComponentsContext.class})
 public class ApplicationContext {
 
     @Bean
