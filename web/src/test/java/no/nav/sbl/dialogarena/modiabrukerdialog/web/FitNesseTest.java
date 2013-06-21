e930f999c8db35cb0617d8ffe93102d0929df3f9
Øyvind Norsveen <Oyvind.Nordsveen@nav.no>
2013-06-21 13:39:32 +0000

    Fikset bygg
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/modiabrukerdialog/trunk@42873 6ec884bf-e345-0410-bd42-d282eb737ba0

--- web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/FitNesseTest.java
+++ web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/FitNesseTest.java
@@ -1,14 +1,7 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.web;
 
-import no.nav.modig.test.fitnesse.junit.FitNesseJUnitRunner;
-import no.nav.modig.test.fitnesse.junit.FitNesseSuite;
-
-import org.junit.Ignore;
-import org.junit.runner.RunWith;
-
-@Ignore
-@RunWith(FitNesseJUnitRunner.class)
-@FitNesseSuite(suiteName = "AlleTester", tags = "godkjent")
+//@RunWith(FitNesseJUnitRunner.class)
+//@FitNesseSuite(suiteName = "AlleTester", tags = "godkjent")
 public class FitNesseTest {
 
 }
