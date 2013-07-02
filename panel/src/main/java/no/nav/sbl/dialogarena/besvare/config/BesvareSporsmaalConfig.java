aa15212f6d0f7df96f931aac55185c9273c7296e
Rune Flobakk <Rune.Flobakk@nav.no>
2013-07-02 13:06:09 +0000

    Initial commit
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/besvare-sporsmaal/trunk@43586 6ec884bf-e345-0410-bd42-d282eb737ba0

---
+++ panel/src/main/java/no/nav/sbl/dialogarena/besvare/config/BesvareSporsmaalConfig.java
@@ -0,0 +1,8 @@
+package no.nav.sbl.dialogarena.besvare.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BesvareSporsmaalConfig {
+
+}
---
+++ panel/src/main/java/no/nav/sbl/dialogarena/besvare/config/BesvareSporsmaalResources.java
@@ -0,0 +1,30 @@
+package no.nav.sbl.dialogarena.besvare.config;
+
+import no.nav.modig.pagelet.spi.ResourceReferences;
+import org.apache.wicket.request.resource.CssResourceReference;
+import org.apache.wicket.request.resource.JavaScriptResourceReference;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
+
+
+public class BesvareSporsmaalResources implements ResourceReferences {
+
+    @Override
+    public List<CssResourceReference> getCssResourceReferences() {
+        return emptyList();
+    }
+
+    @Override
+    public List<JavaScriptResourceReference> getJavaScriptResourceReferences() {
+        return emptyList();
+    }
+
+    @Override
+    public List<Class<?>> getSpringConfiguration() {
+        return asList(new Class<?>[] {BesvareSporsmaalConfig.class});
+    }
+
+}
