70d39f1ada8809c1139877bdbf32987663f0037f
Fredrik Valdmanis <Fredrik.Valdmanis@nav.no>
2013-07-03 11:43:13 +0000

    Forsøk på å integrere panel for å besvare spørsmål
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/modiabrukerdialog/trunk@43643 6ec884bf-e345-0410-bd42-d282eb737ba0

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/pages/panels/sidebar/SideBar.java
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/pages/panels/sidebar/SideBar.java
@@ -1,6 +1,7 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.web.pages.panels.sidebar;
 
 import no.nav.kjerneinfo.web.pages.kjerneinfo.panel.visittkort.VisittkortPanel;
+import no.nav.sbl.dialogarena.besvare.BesvareSporsmalPanel;
 import org.apache.wicket.markup.html.panel.Panel;
 
 public class SideBar extends Panel {
@@ -8,6 +9,7 @@ public class SideBar extends Panel {
     public SideBar(String id, String fnr) {
         super(id);
         add(new VisittkortPanel("visittkortPanel", fnr));
+        add(new BesvareSporsmalPanel("besvarePanel"));
     }
 
 }
