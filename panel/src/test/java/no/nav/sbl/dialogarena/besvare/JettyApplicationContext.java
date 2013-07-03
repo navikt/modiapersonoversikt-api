ba6a99fe2daea94fccd38dee2f807fbe916a5e3c
Rune Flobakk <Rune.Flobakk@nav.no>
2013-07-03 13:26:57 +0000

    Oppsett av url til henvendelse i test.
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/besvare-sporsmaal/trunk@43659 6ec884bf-e345-0410-bd42-d282eb737ba0

--- panel/src/test/java/no/nav/sbl/dialogarena/besvare/JettyApplicationContext.java
+++ panel/src/test/java/no/nav/sbl/dialogarena/besvare/JettyApplicationContext.java
@@ -3,13 +3,21 @@ package no.nav.sbl.dialogarena.besvare;
 import no.nav.sbl.dialogarena.besvare.config.BesvareSporsmalConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Import(BesvareSporsmalConfig.class)
+@PropertySource("classpath:webservices.properties")
 public class JettyApplicationContext {
 
     @Bean
     public BesvareSporsmalApplication application() {
         return new BesvareSporsmalApplication();
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
     }
 
 }
