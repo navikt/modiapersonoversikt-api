8fba9e2a5950222d8efc46e35d9bc8626f753e3c
Rune Flobakk <Rune.Flobakk@nav.no>
2013-07-05 13:57:54 +0000

    Lagt inn, men deaktivert lasting av komponenters Spring-contexter gjennom SPI. Se ApplicationContext for mer info.
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/modiabrukerdialog/trunk@43869 6ec884bf-e345-0410-bd42-d282eb737ba0

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/ApplicationContext.java
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/ApplicationContext.java
@@ -2,15 +2,19 @@ package no.nav.sbl.dialogarena.modiabrukerdialog.web.config;
 
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.WicketApplication;
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.pages.intern.LamellHandler;
-
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.PriorityOrdered;
 
 @Configuration
-@Import({ComponentsContext.class})
-public class ApplicationContext {
+@Import(ComponentsContext.class)
+public class ApplicationContext implements BeanDefinitionRegistryPostProcessor, PriorityOrdered {
 
     @Bean
     public WicketApplication modiaApplication() {
@@ -23,8 +27,35 @@ public class ApplicationContext {
     }
 
     @Bean
-    public LamellHandler lamellHandler(){
+    public LamellHandler lamellHandler() {
         return new LamellHandler();
     }
+
+
+
+
+    @Override
+    public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) {
+        // Fungerer ikke for øyeblikket p.g.a. at komponenter tydeligvis angir Spring-config-klasser
+        // som ikke fungerer i sine respektive ResourceReferences-implementasjoner. F.eks. dras det inn både
+        // *Config og *TestConfig klasser, virker rart.
+        // Vi angir configklasser for øyeblikket eksplisitt med @Import(ComponentsContext.class) i toppen av denne klassen,
+        // men tanken er at dette skal gjøres med linjen under. Ta dette med de enkelte leverandørene av
+        // komponenter, og få de til å angi config-klasser som fungerer.
+        //
+        // Mulig videre utvikling av SPI kan være å angi config for 2 ulike modi: integrasjon og mock/stub.
+
+        // MergeSpringConfigFromComponents.merge(registry);
+    }
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+
 
 }
