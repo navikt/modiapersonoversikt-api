20a2eb6f1a977901d4c79136f6e1ff00bc2ea506
Joar Aurdal <joar.aurdal@nav.no>
2013-07-08 11:53:53 +0000

    MODLIME-110 tenkesnurredings ved avansert søk
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/modiabrukerdialog/trunk@43909 6ec884bf-e345-0410-bd42-d282eb737ba0

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/pages/intern/snurr.js
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/pages/intern/snurr.js
@@ -1,10 +1,18 @@
 $(function() {
     $('#utvidetPersonsokForm').navAjaxLoader({
         event       : 'submit',
-        placeElement: $('#personsokResult'),
+        placeElement: '#personsokResult',
         placement   : 'before',
         loadImage   : 'img/ajaxloader/svart/loader_svart_96.gif',
         css         : 'margin-bottom: 20px;'
+    });
+
+    $('.search-intern input[name="foedselsnummerInput"]').navAjaxLoader({
+        event       : 'change',
+        placeElement: '.lamell',
+        placement   : 'before',
+        loadImage   : 'img/ajaxloader/hvit/loader_hvit_96.gif',
+        css         : 'margin: 30px 0 0 30px;'
     });
 
 });
