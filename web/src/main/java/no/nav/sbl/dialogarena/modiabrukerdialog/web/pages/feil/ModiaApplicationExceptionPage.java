5e86e0f9ee54ac17bb7a2a865b450002f06cbbd2
Andreas Baakind <Andreas.Baakind@nav.no>
2013-07-11 12:41:02 +0000

    Ryddet opp i Modia-exception-pages. Rettet checkstyle- og PMD-warnings
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/modiabrukerdialog/branches/leveranse1@44192 6ec884bf-e345-0410-bd42-d282eb737ba0

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/pages/feil/ModiaApplicationExceptionPage.java
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/pages/feil/ModiaApplicationExceptionPage.java
@@ -1,13 +1,8 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.web.pages.feil;
 
 import no.nav.modig.wicket.errorhandling.listeners.ModigExceptionListener;
-import no.nav.modig.wicket.errorhandling.panels.DefaultErrorPanel;
 import no.nav.modig.wicket.errorhandling.panels.ErrorPanelVM;
-import no.nav.sbl.dialogarena.modiabrukerdialog.web.pages.hentperson.HentPersonPage;
 import org.apache.wicket.Session;
-import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.ajax.markup.html.AjaxLink;
-import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.pages.AbstractErrorPage;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
@@ -21,22 +16,31 @@ public class ModiaApplicationExceptionPage extends AbstractErrorPage {
 
     private static final String DEFAULT_ERROR_MESSAGE = "En feil har oppstått ";
 
-    private final ModiaDefaultErrorPanel errorpanel;
-
     public ModiaApplicationExceptionPage() {
 
         final String exceptionUniqueId = Session.get().getMetaData(ModigExceptionListener.EXCEPTION_UID_KEY);
         final String internasjonaliseringsnokkel = Session.get().getMetaData(ModigExceptionListener.EXCEPTION_MESSAGE);
-        IModel<String> exceptionModel = internasjonaliseringsnokkel == null ? new Model<>("") : new StringResourceModel(internasjonaliseringsnokkel, null, DEFAULT_ERROR_MESSAGE);
+        IModel<String> exceptionModel = createExceptionModel(internasjonaliseringsnokkel);
+        StringResourceModel errorCode = createErrorCodeResourceModel();
 
-        StringResourceModel errorCode = new StringResourceModel("errorpanel.errorcode", this, null, HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-
-        ErrorPanelVM errorPanelVM = new ErrorPanelVM(ErrorPanelVM.Icon.UTROPSTEGN, exceptionModel.getObject(), "",
-                asList(errorCode.getString(), exceptionUniqueId));
-
-        errorpanel = new ModiaDefaultErrorPanel("errorpanel", new Model<>(errorPanelVM));
         add(
-                errorpanel
+                createDefaultErrorPanel(createErrorPanelVM(exceptionModel, errorCode.toString(), exceptionUniqueId))
         );
+    }
+
+    private ModiaDefaultErrorPanel createDefaultErrorPanel(ErrorPanelVM errorPanelVM) {
+        return new ModiaDefaultErrorPanel("errorpanel", new Model<>(errorPanelVM));
+    }
+
+    private ErrorPanelVM createErrorPanelVM(IModel<String> exceptionModel, String errorCode, String exceptionUniqueId) {
+        return new ErrorPanelVM(ErrorPanelVM.Icon.UTROPSTEGN, exceptionModel.getObject(), "", asList(errorCode, exceptionUniqueId));
+    }
+
+    private StringResourceModel createErrorCodeResourceModel() {
+        return new StringResourceModel("errorpanel.errorcode", this, null, HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+    }
+
+    private IModel<String> createExceptionModel(String internasjonaliseringsnokkel) {
+        return internasjonaliseringsnokkel == null ? new Model<>("") : new StringResourceModel(internasjonaliseringsnokkel, null, DEFAULT_ERROR_MESSAGE);
     }
 }
--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/pages/feil/ModiaSystemExceptionPage.java
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/pages/feil/ModiaSystemExceptionPage.java
@@ -1,11 +1,8 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.web.pages.feil;
 
 import no.nav.modig.wicket.errorhandling.listeners.ModigExceptionListener;
-import no.nav.modig.wicket.errorhandling.panels.DefaultErrorPanel;
 import no.nav.modig.wicket.errorhandling.panels.ErrorPanelVM;
-import no.nav.sbl.dialogarena.modiabrukerdialog.web.pages.hentperson.HentPersonPage;
 import org.apache.wicket.Session;
-import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.pages.AbstractErrorPage;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.StringResourceModel;
@@ -17,20 +14,13 @@ import static java.util.Arrays.asList;
 
 public class ModiaSystemExceptionPage extends AbstractErrorPage {
 
-    private final ModiaDefaultErrorPanel errorpanel;
 
     public ModiaSystemExceptionPage() {
-
         final String exceptionUniqueId = Session.get().getMetaData(ModigExceptionListener.EXCEPTION_UID_KEY);
-
-        StringResourceModel errorCode = new StringResourceModel("system.error.code.message", this, null, HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-        ErrorPanelVM errorPanelVM = new ErrorPanelVM(ErrorPanelVM.Icon.UTROPSTEGN,
-                asList(errorCode.getString(), exceptionUniqueId));
-
-        errorpanel = new ModiaDefaultErrorPanel("errorpanel", new Model<>(errorPanelVM));
+        StringResourceModel errorCode = getErrorCode();
 
         add(
-                errorpanel
+                createDefaultErrorPanel(createErrorPanelVM(errorCode.toString(), exceptionUniqueId))
         );
     }
 
@@ -38,4 +28,17 @@ public class ModiaSystemExceptionPage extends AbstractErrorPage {
     protected void setHeaders(final WebResponse response) {
         response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
     }
+
+    private ModiaDefaultErrorPanel createDefaultErrorPanel(ErrorPanelVM errorPanelVM) {
+        return new ModiaDefaultErrorPanel("errorpanel", new Model<>(errorPanelVM));
+    }
+
+    private StringResourceModel getErrorCode() {
+        return new StringResourceModel("system.error.code.message", this, null, HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+    }
+
+    private ErrorPanelVM createErrorPanelVM(String errorCode, String exceptionUniqueId) {
+        return new ErrorPanelVM(ErrorPanelVM.Icon.UTROPSTEGN, asList(errorCode.toString(), exceptionUniqueId));
+    }
+
 }
