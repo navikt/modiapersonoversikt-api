9c13bccef3e380ab09e6c35cd1cc3457ddeabc95
Joar Aurdal <joar.aurdal@nav.no>
2013-08-01 10:57:23 +0000

    MODLIME-117 Byttet til feilsider i rammeverket og slettet de i modiabrukerdialog.
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/modiabrukerdialog/branches/leveranse1@70737 6ec884bf-e345-0410-bd42-d282eb737ba0

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/WicketApplication.java
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/WicketApplication.java
@@ -15,9 +15,9 @@ import no.nav.modig.security.tilgangskontroll.policy.pep.EnforcementPoint;
 import no.nav.modig.security.tilgangskontroll.wicket.BehaviorPolicyAuthorizationStrategy;
 import no.nav.modig.wicket.component.datepicker.DatePicker;
 import no.nav.modig.wicket.configuration.ApplicationSettingsConfig;
+import no.nav.modig.wicket.errorhandling.pages.ApplicationExceptionPage;
+import no.nav.modig.wicket.errorhandling.pages.SystemExceptionPage;
 import no.nav.modig.wicket.events.NamedEventDispatcher;
-import no.nav.sbl.dialogarena.modiabrukerdialog.web.pages.feil.ModiaApplicationExceptionPage;
-import no.nav.sbl.dialogarena.modiabrukerdialog.web.pages.feil.ModiaSystemExceptionPage;
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.pages.hentperson.HentPersonPage;
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.pages.intern.Intern;
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.selftest.SelfTestPage;
@@ -99,8 +99,8 @@ public class WicketApplication extends WebApplication {
 		getSecuritySettings().setAuthorizationStrategy(compoundAuthorizationStrategy);
 
         new ApplicationSettingsConfig()
-                .withApplicationExceptionPage(ModiaApplicationExceptionPage.class)
-                .withInternFeilSide(ModiaSystemExceptionPage.class)
+                .withApplicationExceptionPage(ApplicationExceptionPage.class)
+                .withInternFeilSide(SystemExceptionPage.class)
                 .configure(this);
 
         getExceptionSettings().setUnexpectedExceptionDisplay(IExceptionSettings.SHOW_INTERNAL_ERROR_PAGE);
