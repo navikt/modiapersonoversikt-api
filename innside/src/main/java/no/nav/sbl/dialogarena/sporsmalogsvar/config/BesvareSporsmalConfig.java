5c7f0e81b5140a9aae54a8e8c80a9f8a0712a30b
Thomas Fossum <Thomas.Fossum@nav.no>
2013-08-02 13:16:10 +0000

    La til manglende bean for henvendelsesPortType i test-scope, slik at springconfig i ModiaBrukerdialog ikke brekker
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/sporsmal-og-svar/trunk@70804 6ec884bf-e345-0410-bd42-d282eb737ba0

--- innside/src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/config/BesvareSporsmalConfig.java
+++ innside/src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/config/BesvareSporsmalConfig.java
@@ -1,6 +1,7 @@
 package no.nav.sbl.dialogarena.sporsmalogsvar.config;
 
 import no.nav.sbl.dialogarena.sporsmalogsvar.consumer.MeldingService;
+import no.nav.sbl.dialogarena.sporsmalogsvar.mock.HenvendelsePortTypeMock;
 import no.nav.sbl.dialogarena.sporsmalogsvar.mock.SporsmalOgSvarPortTypeMock;
 import no.nav.tjeneste.domene.brukerdialog.sporsmalogsvar.v1.SporsmalOgSvarPortType;
 import org.springframework.context.annotation.Bean;
@@ -36,6 +37,12 @@ public class BesvareSporsmalConfig {
         public SporsmalOgSvarPortType sporsmalOgSvarPortType() {
             return new SporsmalOgSvarPortTypeMock();
         }
+
+        @Bean
+        public HenvendelsePortTypeMock henvendelsePortType() {
+            return new HenvendelsePortTypeMock();
+        }
+
         @Bean
         public MeldingService meldingService() {
             return new MeldingService();
---
+++ innside/src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/mock/HenvendelsePortTypeMock.java
@@ -0,0 +1,18 @@
+package no.nav.sbl.dialogarena.sporsmalogsvar.mock;
+
+import no.nav.tjeneste.domene.brukerdialog.henvendelsefelles.v1.HenvendelsePortType;
+import no.nav.tjeneste.domene.brukerdialog.henvendelsefelles.v1.informasjon.WSHenvendelse;
+
+import javax.jws.WebParam;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Mock av webservice henvendelse, returnerer tom liste med Henvendelser.
+ */
+public class HenvendelsePortTypeMock implements HenvendelsePortType {
+    @Override
+    public List<WSHenvendelse> hentHenvendelseListe(@WebParam(name = "fodselsnummer", targetNamespace = "") String s) {
+        return new ArrayList<>();
+    }
+}
