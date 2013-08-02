b6dc43085a402bbb199e881eb2a6a5ca7ed3676a
Johan Anders Långberg <Johan.Anders.Langberg@nav.no>
2013-08-02 08:34:25 +0000

    Rullet tilbake til versjon 70770
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/modiabrukerdialog/branches/leveranse1@70782 6ec884bf-e345-0410-bd42-d282eb737ba0

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/BasePage.java
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/BasePage.java
@@ -5,12 +5,10 @@ import org.apache.wicket.markup.html.TransparentWebMarkupContainer;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.request.resource.JavaScriptResourceReference;
-import org.apache.wicket.request.resource.PackageResourceReference;
 
 public class BasePage extends WebPage {
 
     public static final JavaScriptResourceReference JS_RESOURCE = new JavaScriptResourceReference(Intern.class, "lokal.js");
-    public static final PackageResourceReference LESS_RESOURCE = new PackageResourceReference(Intern.class, "intern.less");
 
     private final WebMarkupContainer body;
 
