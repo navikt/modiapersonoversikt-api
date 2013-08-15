5a678f910e1332e47e995eb77d4ca12c45b0d53c
Øystein Strand <Oystein.Strand@nav.no>
2013-08-15 08:30:13 +0000

    Broken spenegod
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/sporsmal-og-svar/trunk@71583 6ec884bf-e345-0410-bd42-d282eb737ba0

--- innside/src/test/java/no/nav/sbl/dialogarena/sporsmalogsvar/JettyInnside.java
+++ innside/src/test/java/no/nav/sbl/dialogarena/sporsmalogsvar/JettyInnside.java
@@ -1,10 +1,11 @@
 package no.nav.sbl.dialogarena.sporsmalogsvar;
 
+import no.nav.modig.core.context.JettySubjectHandler;
 import no.nav.modig.core.context.SubjectHandler;
-import no.nav.modig.core.context.ThreadLocalSubjectHandler;
 import no.nav.modig.testcertificates.TestCertificates;
 import no.nav.sbl.dialogarena.common.jetty.Jetty;
 import no.nav.sbl.dialogarena.test.SystemProperties;
+import org.eclipse.jetty.jaas.JAASLoginService;
 
 import java.io.File;
 
@@ -19,10 +20,19 @@ public final class JettyInnside {
 
     public static void main(String ... args) {
         SystemProperties.setFrom("jetty.properties");
-        System.setProperty(SubjectHandler.SUBJECTHANDLER_KEY, ThreadLocalSubjectHandler.class.getName());
+//        System.setProperty(SubjectHandler.SUBJECTHANDLER_KEY, ThreadLocalSubjectHandler.class.getName());
+        System.setProperty(SubjectHandler.SUBJECTHANDLER_KEY, JettySubjectHandler.class.getName());
+        System.setProperty("java.security.auth.login.config", "src/test/resources/login.conf");
+        // For Eclipse:
+        // System.setProperty("java.security.auth.login.config", "<web-module-name>/src/test/resources/login.conf");
         TestCertificates.setupKeyAndTrustStore();
 
-        Jetty jetty = usingWar(new File(TEST_RESOURCES, "webapp")).port(8383).at("innside").buildJetty();
+        JAASLoginService jaasLoginService = new JAASLoginService("Simple Login Realm");
+        jaasLoginService.setLoginModuleName("simplelogin");
+        jaasLoginService.setRoleClassNames(new String[]{"Tullerolle"});
+
+        Jetty jetty = usingWar(new File(TEST_RESOURCES, "webapp")).withLoginService(jaasLoginService).port(8383).at("innside").buildJetty();
         jetty.startAnd(first(waitFor(gotKeypress())).then(jetty.stop));
+
     }
 }
