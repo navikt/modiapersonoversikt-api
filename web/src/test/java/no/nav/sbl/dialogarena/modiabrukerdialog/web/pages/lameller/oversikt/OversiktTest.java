432fc48725ced31c119217a40910241a010a64e8
Gunnar Gauslaa Bergem <Gunnar.Gauslaa.Bergem@nav.no>
2013-08-16 12:19:00 +0000

    unit test for Oversikt
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/modiabrukerdialog/branches/leveranse1@71867 6ec884bf-e345-0410-bd42-d282eb737ba0

---
+++ web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/pages/lameller/oversikt/OversiktTest.java
@@ -0,0 +1,44 @@
+package no.nav.sbl.dialogarena.modiabrukerdialog.web.pages.lameller.oversikt;
+
+import no.nav.modig.modia.widget.LenkeWidget;
+import no.nav.modig.wicket.test.FluentWicketTester;
+import no.nav.sbl.dialogarena.modiabrukerdialog.web.TestSecurityBaseClass;
+import no.nav.sbl.dialogarena.modiabrukerdialog.web.config.ApplicationContext;
+import no.nav.sbl.dialogarena.modiabrukerdialog.web.config.WicketTesterConfig;
+import no.nav.sykmeldingsperioder.widget.SykepengerWidget;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.inject.Inject;
+
+import static no.nav.modig.common.MDCOperations.MDC_CALL_ID;
+import static no.nav.modig.common.MDCOperations.generateCallId;
+import static no.nav.modig.common.MDCOperations.putToMDC;
+import static no.nav.modig.wicket.test.matcher.ComponentMatchers.ofType;
+import static no.nav.modig.wicket.test.matcher.ComponentMatchers.withId;
+
+@ActiveProfiles("test")
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {ApplicationContext.class, WicketTesterConfig.class})
+public class OversiktTest extends TestSecurityBaseClass {
+
+    @Inject
+    private FluentWicketTester<?> fluentWicketTester;
+
+    @Before
+    public void setupMDC() {
+        putToMDC(MDC_CALL_ID, generateCallId());
+    }
+
+    @Test
+    public void skalOppretteOversikt() {
+        fluentWicketTester.goToPageWith(new Oversikt("id", "fnr"))
+                .should().containComponent(withId("lenker").and(ofType(LenkeWidget.class)))
+                .should().containComponent(withId("sykepenger").and(ofType(SykepengerWidget.class)));
+    }
+
+}
