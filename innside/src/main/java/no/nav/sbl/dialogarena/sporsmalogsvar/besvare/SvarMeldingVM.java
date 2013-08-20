7773ae297869d7f686b96d81c520475380d6a3b2
Hans Olaf Borch <Hans.Olav.Borch@nav.no>
2013-08-20 06:40:10 +0000

    Manglet en serializable for å få innside til å kjøre
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/sporsmal-og-svar/trunk@71960 6ec884bf-e345-0410-bd42-d282eb737ba0

--- innside/src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/besvare/SvarMeldingVM.java
+++ innside/src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/besvare/SvarMeldingVM.java
@@ -1,8 +1,10 @@
 package no.nav.sbl.dialogarena.sporsmalogsvar.besvare;
 
+import java.io.Serializable;
+
 import no.nav.sbl.dialogarena.sporsmalogsvar.consumer.Melding;
 
-public class SvarMeldingVM {
+public class SvarMeldingVM implements Serializable {
 
 	public String behandlingsId, saksid, fritekst, overskrift;
     public boolean sensitiv;
