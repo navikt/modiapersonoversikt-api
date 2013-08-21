15287dc1184c0f3f89ac4471fee38883b7e352c7
Fredrik Valdmanis <Fredrik.Valdmanis@nav.no>
2013-08-21 08:41:28 +0000

    Checkstyle, unused import
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/sporsmal-og-svar/trunk@72106 6ec884bf-e345-0410-bd42-d282eb737ba0

--- utside/src/test/java/no/nav/sbl/dialogarena/sporsmalogsvar/servlet/Brukerindeks.java
+++ utside/src/test/java/no/nav/sbl/dialogarena/sporsmalogsvar/servlet/Brukerindeks.java
@@ -2,7 +2,6 @@ package no.nav.sbl.dialogarena.sporsmalogsvar.servlet;
 
 import org.apache.commons.io.IOUtils;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
