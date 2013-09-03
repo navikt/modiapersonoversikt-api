bc8350484a645a94bda222f4136a41f493a29b55
Frithjof Nerdrum <Frithjof.Nerdrum@nav.no>
2013-09-03 12:50:24 +0000

    Lagt til oppgaveBehandlingEndpoint i configen til modia
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/sporsmal-og-svar/trunk@73118 6ec884bf-e345-0410-bd42-d282eb737ba0

--- besvare/src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/config/BesvareHenvendelseConfig.java
+++ besvare/src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/config/BesvareHenvendelseConfig.java
@@ -2,20 +2,6 @@ package no.nav.sbl.dialogarena.sporsmalogsvar.config;
 
 import no.nav.sbl.dialogarena.sporsmalogsvar.mock.BesvareHenvendelsePortTypeMock;
 import no.nav.tjeneste.domene.brukerdialog.besvare.v1.BesvareHenvendelsePortType;
-import no.nav.tjeneste.virksomhet.sakogbehandling.v1.SakOgBehandlingPortType;
-import no.nav.tjeneste.virksomhet.sakogbehandling.v1.informasjon.Behandlingskjedetyper;
-import no.nav.tjeneste.virksomhet.sakogbehandling.v1.informasjon.Behandlingsstegtyper;
-import no.nav.tjeneste.virksomhet.sakogbehandling.v1.informasjon.Behandlingstid;
-import no.nav.tjeneste.virksomhet.sakogbehandling.v1.informasjon.Behandlingstidtyper;
-import no.nav.tjeneste.virksomhet.sakogbehandling.v1.informasjon.Temaer;
-import no.nav.tjeneste.virksomhet.sakogbehandling.v1.informasjon.finnsakogbehandlingskjedeliste.Behandlingskjede;
-import no.nav.tjeneste.virksomhet.sakogbehandling.v1.informasjon.finnsakogbehandlingskjedeliste.Sak;
-import no.nav.tjeneste.virksomhet.sakogbehandling.v1.meldinger.FinnSakOgBehandlingskjedeListeRequest;
-import no.nav.tjeneste.virksomhet.sakogbehandling.v1.meldinger.FinnSakOgBehandlingskjedeListeResponse;
-import no.nav.tjeneste.virksomhet.sakogbehandling.v1.meldinger.HentBehandlingRequest;
-import no.nav.tjeneste.virksomhet.sakogbehandling.v1.meldinger.HentBehandlingResponse;
-import no.nav.tjeneste.virksomhet.sakogbehandling.v1.meldinger.HentBehandlingskjedensBehandlingerRequest;
-import no.nav.tjeneste.virksomhet.sakogbehandling.v1.meldinger.HentBehandlingskjedensBehandlingerResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -24,43 +10,6 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 @Import({BesvareHenvendelseConfig.Default.class, BesvareHenvendelseConfig.DefaultWithoutCNCheck.class, BesvareHenvendelseConfig.Test.class})
 public class BesvareHenvendelseConfig {
-
-    @Bean
-    public SakOgBehandlingPortType sakOgBehandlingPortType() {
-        return new SakOgBehandlingPortType() {
-
-            @Override
-            public void ping() {
-            }
-
-            @Override
-            public HentBehandlingskjedensBehandlingerResponse hentBehandlingskjedensBehandlinger(HentBehandlingskjedensBehandlingerRequest request) {
-                return null;
-            }
-
-            @Override
-            public HentBehandlingResponse hentBehandling(HentBehandlingRequest request) {
-                return null;
-            }
-
-            @Override
-            public FinnSakOgBehandlingskjedeListeResponse finnSakOgBehandlingskjedeListe(FinnSakOgBehandlingskjedeListeRequest request) {
-                Behandlingskjede kjede = new Behandlingskjede()
-                        .withNormertBehandlingstid(new Behandlingstid().withType(new Behandlingstidtyper()))
-                        .withBehandlingskjedetype(new Behandlingskjedetyper())
-                        .withBehandlingskjedeId("id")
-//		            .withStartNAVtid(createXmlGregorianDate(1, 1, 2013))
-//		            .withKjedensNAVfrist(createXmlGregorianDate(11, 11, 2022))
-                        .withSisteBehandlingREF("sisteBehandlingref")
-                        .withSisteBehandlingsstegREF("sisteBehandlingsstegref")
-                        .withSisteBehandlingsstegtype(new Behandlingsstegtyper().withValue("value"));
-                return new FinnSakOgBehandlingskjedeListeResponse().
-                        withSak(new Sak().withTema(new Temaer().withKodeverksRef("Alderspensjon")).withBehandlingskjede(kjede),
-                                new Sak().withTema(new Temaer().withKodeverksRef("Foreldrepenger")).withBehandlingskjede(kjede));
-            }
-
-        };
-    }
 
     @Profile({"default", "brukerhenvendelserDefault"})
     @Configuration
--- besvare/src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/config/BesvareHenvendelseTjenester.java
+++ besvare/src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/config/BesvareHenvendelseTjenester.java
@@ -1,20 +1,28 @@
 package no.nav.sbl.dialogarena.sporsmalogsvar.config;
 
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import javax.inject.Inject;
+import no.nav.modig.modia.ping.PingResult;
 import no.nav.modig.modia.ping.Pingable;
 import no.nav.modig.security.ws.AbstractSAMLOutInterceptor;
 import no.nav.modig.security.ws.SystemSAMLOutInterceptor;
 import no.nav.modig.security.ws.UserSAMLOutInterceptor;
 import no.nav.tjeneste.domene.brukerdialog.besvare.v1.BesvareHenvendelsePortType;
+import no.nav.tjeneste.domene.brukerdialog.oppgavebehandling.v1.OppgavebehandlingPortType;
 import org.apache.cxf.endpoint.Client;
 import org.apache.cxf.frontend.ClientProxy;
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
 import org.apache.cxf.transport.http.HTTPConduit;
 import org.apache.cxf.ws.security.SecurityConstants;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import static no.nav.modig.modia.ping.PingResult.ServiceResult.SERVICE_FAIL;
+import static no.nav.modig.modia.ping.PingResult.ServiceResult.SERVICE_OK;
 
 @Configuration
 public class BesvareHenvendelseTjenester {
@@ -22,11 +30,19 @@ public class BesvareHenvendelseTjenester {
     @Inject
     private JaxWsFeatures jaxwsFeatures;
 
-    protected String besvareHenvendelseEndpoint = "http://localhost:8686/besvarehenvendelse/services/domene.Brukerdialog/BesvareHenvendelseService_v1?wsdl";
+    @Value("${besvarehenvendelseendpoint.url}")
+    protected String besvareHenvendelseEndpoint;
+
+    protected String oppgavebehandlingEndpoint = "https://d26jbsl00251.test.local:8443/besvarehenvendelse/services/domene.Brukerdialog/GsakOppgavebehandling_v1";
 
     @Bean
-    public Pingable spmOgSvarPing() {
+    public Pingable besvareHenvendelsePing() {
         return new BesvareHenvendelsePingImpl(besvareSystemUser());
+    }
+
+    @Bean
+    public Pingable oppgavebehandlingPing() {
+        return new OppgavebehandlingPingImpl();
     }
 
     @Bean
@@ -39,6 +55,11 @@ public class BesvareHenvendelseTjenester {
         return opprettBesvareHenvendelsePortType(new SystemSAMLOutInterceptor());
     }
 
+    @Bean
+    public OppgavebehandlingPortType oppgavebehandlingSystemUser() {
+        return opprettOppgavebehandlingPortType(new SystemSAMLOutInterceptor());
+    }
+
     private BesvareHenvendelsePortType opprettBesvareHenvendelsePortType(AbstractSAMLOutInterceptor samlOutInterceptor) {
         JaxWsProxyFactoryBean jaxwsClient = commonJaxWsConfig(samlOutInterceptor);
         jaxwsClient.setServiceClass(BesvareHenvendelsePortType.class);
@@ -46,7 +67,15 @@ public class BesvareHenvendelseTjenester {
         jaxwsClient.setWsdlURL("classpath:v1/BesvareHenvendelse.wsdl");
         BesvareHenvendelsePortType besvareHenvendelsePortType = jaxwsClient.create(BesvareHenvendelsePortType.class);
         return konfigurerMedHttps(besvareHenvendelsePortType);
+    }
 
+    private OppgavebehandlingPortType opprettOppgavebehandlingPortType(AbstractSAMLOutInterceptor samlOutInterceptor) {
+        JaxWsProxyFactoryBean jaxwsClient = commonJaxWsConfig(samlOutInterceptor);
+        jaxwsClient.setServiceClass(OppgavebehandlingPortType.class);
+        jaxwsClient.setAddress(oppgavebehandlingEndpoint);
+//        jaxwsClient.setWsdlURL("classpath:Oppgavebehandling.wsdl");
+        OppgavebehandlingPortType oppgavebehandlingPortType = jaxwsClient.create(OppgavebehandlingPortType.class);
+        return konfigurerMedHttps(oppgavebehandlingPortType);
     }
 
     private <T> T konfigurerMedHttps(T portType) {
@@ -67,4 +96,32 @@ public class BesvareHenvendelseTjenester {
         return factoryBean;
     }
 
+    private class OppgavebehandlingPingImpl implements Pingable {
+        @Override
+        public List<PingResult> ping() {
+            List<PingResult> pingResultList = new LinkedList<>();
+            pingResultList.add(performPing());
+            return pingResultList;
+        }
+
+        private PingResult performPing() {
+            boolean pingResponseSuccessful;
+            long timeElapsed;
+            long start = System.currentTimeMillis();
+            try {
+                oppgavebehandlingSystemUser().ping();
+                timeElapsed = System.currentTimeMillis() - start;
+//                logger.info("BesvareHenvendelse_v1.ping(): SUCCESS");
+                pingResponseSuccessful = true;
+            } catch (Exception e) {
+                timeElapsed = System.currentTimeMillis() - start;
+//                logger.info("BesvareHenvendelse_v1.ping(): ERROR" + e.getMessage());
+                pingResponseSuccessful = false;
+            }
+
+            PingResult.ServiceResult serviceResult = pingResponseSuccessful ? SERVICE_OK : SERVICE_FAIL;
+
+            return new PingResult("GsakOppgavebehandling_v1", serviceResult, timeElapsed);
+        }
+    }
 }
