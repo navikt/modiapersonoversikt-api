2927322a4124c105bc194997be341ff8023166dd
Frithjof Nerdrum <Frithjof.Nerdrum@nav.no>
2013-09-05 09:54:51 +0000

    Fikset configen for oppgavebehandling-endpoint. + Gir BesvareSporsmalPanel oppgaveid fra page parameters.
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/modiabrukerdialog/trunk@73270 6ec884bf-e345-0410-bd42-d282eb737ba0

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/HentOppgaveConfig.java
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/HentOppgaveConfig.java
@@ -1,11 +1,16 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.web.config;
 
+import java.util.HashMap;
+import java.util.Map;
+import no.nav.modig.security.ws.SystemSAMLOutInterceptor;
 import no.nav.tjeneste.domene.brukerdialog.oppgavebehandling.v1.OppgavebehandlingPortType;
 import org.apache.cxf.configuration.jsse.TLSClientParameters;
 import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.feature.LoggingFeature;
 import org.apache.cxf.frontend.ClientProxy;
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
 import org.apache.cxf.transport.http.HTTPConduit;
+import org.apache.cxf.ws.security.SecurityConstants;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,15 +18,26 @@ import org.springframework.context.annotation.Configuration;
 public class HentOppgaveConfig {
     protected String oppgavebehandlingEndpoint = "https://localhost:30103/besvarehenvendelse/services/domene.Brukerdialog/GsakOppgavebehandling_v1";
 
+
+
     @Bean
     public OppgavebehandlingPortType oppgavebehandlingPortType() {
-        JaxWsProxyFactoryBean jaxwsClient = new JaxWsProxyFactoryBean();
-        jaxwsClient.setServiceClass(OppgavebehandlingPortType.class);
-        jaxwsClient.setAddress(oppgavebehandlingEndpoint);
-        OppgavebehandlingPortType oppgavebehandlingPortType = jaxwsClient.create(OppgavebehandlingPortType.class);
+        JaxWsProxyFactoryBean factoryBean = new JaxWsProxyFactoryBean();
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("schema-validation-enabled", true);
+        properties.put(SecurityConstants.MUSTUNDERSTAND, false);
+        factoryBean.setProperties(properties);
+        factoryBean.getFeatures().add(new LoggingFeature());
+        factoryBean.getOutInterceptors().add(new SystemSAMLOutInterceptor());
+        factoryBean.setServiceClass(OppgavebehandlingPortType.class);
+        factoryBean.setAddress(oppgavebehandlingEndpoint);
+        OppgavebehandlingPortType oppgavebehandlingPortType = factoryBean.create(OppgavebehandlingPortType.class);
         Client client = ClientProxy.getClient(oppgavebehandlingPortType);
-        HTTPConduit httpConduit = (HTTPConduit) client.getConduit();
-        httpConduit.setTlsClientParameters(new TLSClientParameters());
+        HTTPConduit conduit = (HTTPConduit) client.getConduit();
+        TLSClientParameters params = new TLSClientParameters();
+        params.setDisableCNCheck(true);
+        conduit.setTlsClientParameters(params);
+
         return oppgavebehandlingPortType;
     }
 }
