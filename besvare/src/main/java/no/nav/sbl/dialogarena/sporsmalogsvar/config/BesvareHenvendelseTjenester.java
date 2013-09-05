49d8f70c7898e50f711eac135426e20462959720
Frithjof Nerdrum <Frithjof.Nerdrum@nav.no>
2013-09-05 07:50:12 +0000

    Flyttet config for oppgavebehandling fra s&s til modia
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/sporsmal-og-svar/trunk@73247 6ec884bf-e345-0410-bd42-d282eb737ba0

--- besvare/src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/config/BesvareHenvendelseTjenester.java
+++ besvare/src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/config/BesvareHenvendelseTjenester.java
@@ -1,17 +1,13 @@
 package no.nav.sbl.dialogarena.sporsmalogsvar.config;
 
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 import javax.inject.Inject;
-import no.nav.modig.modia.ping.PingResult;
 import no.nav.modig.modia.ping.Pingable;
 import no.nav.modig.security.ws.AbstractSAMLOutInterceptor;
 import no.nav.modig.security.ws.SystemSAMLOutInterceptor;
 import no.nav.modig.security.ws.UserSAMLOutInterceptor;
 import no.nav.tjeneste.domene.brukerdialog.besvare.v1.BesvareHenvendelsePortType;
-import no.nav.tjeneste.domene.brukerdialog.oppgavebehandling.v1.OppgavebehandlingPortType;
 import org.apache.cxf.endpoint.Client;
 import org.apache.cxf.frontend.ClientProxy;
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
@@ -20,9 +16,6 @@ import org.apache.cxf.ws.security.SecurityConstants;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import static no.nav.modig.modia.ping.PingResult.ServiceResult.SERVICE_FAIL;
-import static no.nav.modig.modia.ping.PingResult.ServiceResult.SERVICE_OK;
 
 @Configuration
 public class BesvareHenvendelseTjenester {
@@ -33,16 +26,9 @@ public class BesvareHenvendelseTjenester {
     @Value("${besvarehenvendelseendpoint.url}")
     protected String besvareHenvendelseEndpoint;
 
-    protected String oppgavebehandlingEndpoint = "https://localhost:30103/besvarehenvendelse/services/domene.Brukerdialog/GsakOppgavebehandling_v1";
-
     @Bean
     public Pingable besvareHenvendelsePing() {
         return new BesvareHenvendelsePingImpl(besvareSystemUser());
-    }
-
-    @Bean
-    public Pingable oppgavebehandlingPing() {
-        return new OppgavebehandlingPingImpl();
     }
 
     @Bean
@@ -55,11 +41,6 @@ public class BesvareHenvendelseTjenester {
         return opprettBesvareHenvendelsePortType(new SystemSAMLOutInterceptor());
     }
 
-    @Bean
-    public OppgavebehandlingPortType oppgavebehandlingSystemUser() {
-        return opprettOppgavebehandlingPortType(new SystemSAMLOutInterceptor());
-    }
-
     private BesvareHenvendelsePortType opprettBesvareHenvendelsePortType(AbstractSAMLOutInterceptor samlOutInterceptor) {
         JaxWsProxyFactoryBean jaxwsClient = commonJaxWsConfig(samlOutInterceptor);
         jaxwsClient.setServiceClass(BesvareHenvendelsePortType.class);
@@ -67,15 +48,6 @@ public class BesvareHenvendelseTjenester {
         jaxwsClient.setWsdlURL("classpath:v1/BesvareHenvendelse.wsdl");
         BesvareHenvendelsePortType besvareHenvendelsePortType = jaxwsClient.create(BesvareHenvendelsePortType.class);
         return konfigurerMedHttps(besvareHenvendelsePortType);
-    }
-
-    private OppgavebehandlingPortType opprettOppgavebehandlingPortType(AbstractSAMLOutInterceptor samlOutInterceptor) {
-        JaxWsProxyFactoryBean jaxwsClient = commonJaxWsConfig(samlOutInterceptor);
-        jaxwsClient.setServiceClass(OppgavebehandlingPortType.class);
-        jaxwsClient.setAddress(oppgavebehandlingEndpoint);
-//        jaxwsClient.setWsdlURL("classpath:Oppgavebehandling.wsdl");
-        OppgavebehandlingPortType oppgavebehandlingPortType = jaxwsClient.create(OppgavebehandlingPortType.class);
-        return konfigurerMedHttps(oppgavebehandlingPortType);
     }
 
     private <T> T konfigurerMedHttps(T portType) {
@@ -96,32 +68,4 @@ public class BesvareHenvendelseTjenester {
         return factoryBean;
     }
 
-    private class OppgavebehandlingPingImpl implements Pingable {
-        @Override
-        public List<PingResult> ping() {
-            List<PingResult> pingResultList = new LinkedList<>();
-            pingResultList.add(performPing());
-            return pingResultList;
-        }
-
-        private PingResult performPing() {
-            boolean pingResponseSuccessful;
-            long timeElapsed;
-            long start = System.currentTimeMillis();
-            try {
-                oppgavebehandlingSystemUser().ping();
-                timeElapsed = System.currentTimeMillis() - start;
-//                logger.info("BesvareHenvendelse_v1.ping(): SUCCESS");
-                pingResponseSuccessful = true;
-            } catch (Exception e) {
-                timeElapsed = System.currentTimeMillis() - start;
-//                logger.info("BesvareHenvendelse_v1.ping(): ERROR" + e.getMessage());
-                pingResponseSuccessful = false;
-            }
-
-            PingResult.ServiceResult serviceResult = pingResponseSuccessful ? SERVICE_OK : SERVICE_FAIL;
-
-            return new PingResult("GsakOppgavebehandling_v1", serviceResult, timeElapsed);
-        }
-    }
 }
