c2ff2bd873ea9af2b2cb219f40c5c44841191486
Henrik Hellerøy <Henrik.Helleroy@nav.no>
2013-09-16 13:39:31 +0000

    MOD-1666: La til keyboard shortcuts for svarfelt. Endret oppførsel til svarfeltet så det skjuler knappen når feltet er tomt og ute av fokus.
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/sporsmal-og-svar/trunk@73941 6ec884bf-e345-0410-bd42-d282eb737ba0

--- besvare/src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/web/besvare/besvare.js
+++ besvare/src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/web/besvare/besvare.js
@@ -1,10 +1,28 @@
 $(document).ready(function() {
-    var attachToggleHoydeListener = function() {
+
+    // Shortcuts
+    $(document).on('keydown', function (e) {
+        console.log(e.keyCode);
+        // Fokuser tekstfeltet: alt + F1 (keyCode 112)
+        if (e.altKey && e.keyCode == 112) {
+            $('#tekstfelt').focus();
+        }
+        // Send svar: shift + enter (keyCode 13)
+        else if (e.shiftKey && e.keyCode == 13) {
+            $('.send-svar-knapp').click();
+        }
+        // Unfocus tekstfeltet: esc (keyCode 27)
+        else if (e.keyCode == 27) {
+            $('#tekstfelt').blur();
+        }
+    });
+
+    function attachToggleHoydeListener() {
         var $dialogInnholdTekst = $('.tidligere-dialog .dialog-innhold p');
         var minHoyde = parseInt($dialogInnholdTekst.css('line-height')) * 2;
 
         $dialogInnholdTekst.each(function() {
-            if(!$(this).data('height') && $(this).height() >= minHoyde) {
+            if (!$(this).data('height') && $(this).height() >= minHoyde) {
                 $(this).data('height', $(this).height());
             }
         });
@@ -20,42 +38,60 @@ $(document).ready(function() {
             var $tekstFelt = $(this).find('p');
             var animasjonsHastighet = 100;
 
-            if($tekstFelt.height() == minHoyde) {
+            if ($tekstFelt.height() == minHoyde) {
                 $tekstFelt.animate({height: $tekstFelt.data('height')}, animasjonsHastighet);
             } else {
                 $tekstFelt.animate({height: minHoyde}, animasjonsHastighet);
             }
             $(this).find('.utvide-tekst-pil').toggleClass('rotert');
         });
-    };
+    }
 
-    var attachJusterTekstfeltListener = function() {
+    function attachSvarFormListener() {
         var $tekstfelt = $('#tekstfelt');
+        var $sendSvarKnapp = $('.send-svar-knapp');
+
         var orginalHoyde = $tekstfelt.height();
+        var utvidetHoyde = $tekstfelt.height() * 8;
+        var padding = $tekstfelt.innerHeight() - $tekstfelt.height();
 
         // I tilfelle AJAX events fra andre deler av Modia
-        $tekstfelt.off('keyup focusout');
+        $tekstfelt.off('keyup focusin focusout');
 
         $tekstfelt.on('keyup', function() {
-            this.style.height = 'auto';
-            $(this).height(this.scrollHeight);
+            if (this.scrollHeight - padding > utvidetHoyde) {
+                this.style.height = 'auto';
+                $(this).height(this.scrollHeight);
+
+                if (this.scrollHeight <= utvidetHoyde) {
+                    $(this).height(utvidetHoyde);
+                }
+            }
+        });
+
+        $tekstfelt.on('focusin', function () {
+            if ($(this).height() < utvidetHoyde) {
+                $(this).height(utvidetHoyde);
+            }
+            $sendSvarKnapp.show();
         });
 
         $tekstfelt.on('focusout', function() {
-            if(this.value.length == 0) {
+            if (this.value.length == 0) {
                 $(this).height(orginalHoyde);
+                $sendSvarKnapp.hide();
             }
         });
-    };
+    }
 
-    var attachAjaxCompleteListener = function() {
+    function attachAjaxCompleteListener() {
         $(document).on('ajaxComplete', function() {
-            attachJusterTekstfeltListener();
+            attachSvarFormListener();
             attachToggleHoydeListener();
         });
-    };
+    }
 
     attachAjaxCompleteListener();
-    attachJusterTekstfeltListener();
+    attachSvarFormListener();
     attachToggleHoydeListener();
 });
