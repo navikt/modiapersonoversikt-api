70d84b3f30584fbe31aa87856d419125adffd5b4
Øyvind Norsveen <Oyvind.Nordsveen@nav.no>
2013-09-25 07:03:50 +0000

    Flere tester
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/modiabrukerdialog/branches/leveranse1@76646 6ec884bf-e345-0410-bd42-d282eb737ba0

--- web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/pages/hentperson/HentPersonPageTest.java
+++ web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/pages/hentperson/HentPersonPageTest.java
@@ -2,6 +2,7 @@ package no.nav.sbl.dialogarena.modiabrukerdialog.web.pages.hentperson;
 
 import no.nav.kjerneinfo.hent.panels.HentPersonPanel;
 import no.nav.modig.wicket.test.FluentWicketTester;
+import no.nav.modig.wicket.test.internal.Parameters;
 import no.nav.personsok.PersonsokPanel;
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.TestSecurityBaseClass;
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.config.ApplicationContext;
@@ -27,6 +28,13 @@ public class HentPersonPageTest extends TestSecurityBaseClass {
     public void shouldRenderHentPersonPage() {
         fluentWicketTester.goTo(HentPersonPage.class).should().containComponent(withId("searchPanel").and(ofType(HentPersonPanel.class)))
                 .should().containComponent(withId("personsokPanel").and(ofType(PersonsokPanel.class)));
+    }
+
+    @Test
+    public void shouldRenderHentPersonPageWithErrorMessage() {
+        Parameters param = new Parameters();
+        param.pageParameters.set("error", "errorMessage");
+        fluentWicketTester.goTo(HentPersonPage.class, param).should().containPatterns("errorMessage");
     }
 
 }
