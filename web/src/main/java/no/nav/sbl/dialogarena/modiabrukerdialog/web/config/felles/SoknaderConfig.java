94115a7ee11d5a293cc51106671497400fbf8726
Oyvind Nordsveen <oyvind.nordsveen@nav.no>
2013-10-01 11:56:21 +0200

    Unused imports

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/felles/SoknaderConfig.java
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/felles/SoknaderConfig.java
@@ -3,8 +3,6 @@ package no.nav.sbl.dialogarena.modiabrukerdialog.web.config.felles;
 import no.nav.sbl.dialogarena.soknader.service.SoknaderWidgetMockService;
 import no.nav.sbl.dialogarena.soknader.service.SoknaderWidgetService;
 import no.nav.sbl.dialogarena.soknader.service.SoknaderWidgetServiceDefault;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
