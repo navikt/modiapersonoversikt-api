d126f465874216485653fea0dc19ee23a219fdc8
Jarle Svendsrud <jarle.svendsrud@nav.no>
2013-10-01 14:00:44 +0200

    Satte opp logging

--- panel/src/test/java/no/nav/sbl/dialogarena/soknader/panel/JettyApplicationContext.java
+++ panel/src/test/java/no/nav/sbl/dialogarena/soknader/panel/JettyApplicationContext.java
@@ -2,6 +2,7 @@ package no.nav.sbl.dialogarena.soknader.panel;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 public class JettyApplicationContext {
@@ -9,6 +10,11 @@ public class JettyApplicationContext {
     @Bean
     public SoeknaderTestApplication application() {
         return new SoeknaderTestApplication();
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
     }
 
 }
