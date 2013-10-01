e5a59cd9259686e1002193e40939e7a367937c96
Jarle Svendsrud <jarle.svendsrud@nav.no>
2013-10-01 11:49:27 +0200

    Begynt på oppsett for Jetty

---
+++ panel/src/test/java/no/nav/sbl/dialogarena/soknader/panel/JettyApplicationContext.java
@@ -0,0 +1,8 @@
+package no.nav.sbl.dialogarena.soknader.panel;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JettyApplicationContext {
+
+}
---
+++ panel/src/test/java/no/nav/sbl/dialogarena/soknader/panel/StartJettySoknader.java
@@ -0,0 +1,16 @@
+package no.nav.sbl.dialogarena.soknader.panel;
+
+import no.nav.sbl.dialogarena.common.jetty.Jetty;
+
+import java.io.File;
+
+import static no.nav.sbl.dialogarena.test.path.FilesAndDirs.TEST_RESOURCES;
+
+public final class StartJettySoknader {
+
+    public static void main(String ... args) {
+
+        Jetty.usingWar(new File(TEST_RESOURCES, "webapp")).at("soknader").port(8383)
+                .buildJetty().start();
+    }
+}
