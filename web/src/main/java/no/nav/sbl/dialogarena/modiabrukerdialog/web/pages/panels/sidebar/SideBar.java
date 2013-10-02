7b31c3661015cee6158ffe8adaf04ff329d44b73
Rune Flobakk <rune.flobakk@nav.no>
2013-10-02 10:23:06 +0200

    Sidebar styrer synlighet basert på event.
    Ikke-komplett implementasjon. Fyres ikke av eventer enda. InternTest
    feiler.

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/pages/panels/sidebar/SideBar.java
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/pages/panels/sidebar/SideBar.java
@@ -1,6 +1,8 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.web.pages.panels.sidebar;
 
 import no.nav.kjerneinfo.web.pages.kjerneinfo.panel.visittkort.VisittkortPanel;
+import no.nav.modig.wicket.events.annotations.RunOnEvents;
+import no.nav.sbl.dialogarena.modiabrukerdialog.web.Modus;
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.pages.panels.oppgave.HentOppgavePanel;
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.pages.panels.oppgavevalg.OppgavevalgPanel;
 import no.nav.sbl.dialogarena.sporsmalogsvar.web.besvare.BesvareSporsmalPanel;
@@ -8,14 +10,27 @@ import org.apache.wicket.markup.html.panel.Panel;
 
 public class SideBar extends Panel {
 
+    private final BesvareSporsmalPanel besvaresporsmalPanel;
+    private final OppgavevalgPanel oppgavevalg;
+
     public SideBar(String id, String fnr, String oppgaveIdFromRequest) {
         super(id);
-        OppgavevalgPanel oppgavevalg = new OppgavevalgPanel("oppgavevalg", oppgaveIdFromRequest);
-        oppgavevalg.setVisible(oppgaveIdFromRequest != null);
+        oppgavevalg = new OppgavevalgPanel("oppgavevalg", oppgaveIdFromRequest);
         VisittkortPanel visittkortPanel = new VisittkortPanel("visittkortPanel", fnr);
-        BesvareSporsmalPanel besvarePanel = new BesvareSporsmalPanel("besvarePanel", oppgaveIdFromRequest, fnr);
-        besvarePanel.setVisible(null != oppgaveIdFromRequest);
-        add(visittkortPanel, besvarePanel, new HentOppgavePanel("hent-oppgave"), oppgavevalg);
+        besvaresporsmalPanel = new BesvareSporsmalPanel("besvarePanel", oppgaveIdFromRequest, fnr);
+        add(visittkortPanel, besvaresporsmalPanel, new HentOppgavePanel("hent-oppgave"), oppgavevalg);
+        initVisibility();
+    }
+
+    @RunOnEvents(Modus.BESVARE)
+    public void besvarmodus() {
+        besvaresporsmalPanel.setVisibilityAllowed(true);
+        oppgavevalg.setVisibilityAllowed(true);
+    }
+
+    public void initVisibility() {
+        besvaresporsmalPanel.setVisibilityAllowed(false);
+        oppgavevalg.setVisibilityAllowed(false);
     }
 
 }
