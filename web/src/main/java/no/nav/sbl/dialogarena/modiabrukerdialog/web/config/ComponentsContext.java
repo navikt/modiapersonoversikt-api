eee781a11c38df87074580d8076c615dc4c5a21b
Henrik Hellerøy <henrik.helleroy@nav.no>
2013-10-02 17:58:03 +0200

    La til meldingsstatus propertystrings og fjernet dependency på service fra sporsmal-og-svar.

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/ComponentsContext.java
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/ComponentsContext.java
@@ -1,7 +1,6 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.web.config;
 
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.config.felles.SoknaderConfig;
-import no.nav.sbl.dialogarena.sporsmalogsvar.henvendelser.config.MeldingerInnboksConfig;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -18,7 +17,6 @@ import org.springframework.context.annotation.Import;
         no.nav.sbl.dialogarena.sporsmalogsvar.config.BesvareServiceConfig.class,
         OppgavebehandlingConfig.class,
         SoknaderConfig.class,
-        MeldingerInnboksConfig.class
 })
 public class ComponentsContext {
 
