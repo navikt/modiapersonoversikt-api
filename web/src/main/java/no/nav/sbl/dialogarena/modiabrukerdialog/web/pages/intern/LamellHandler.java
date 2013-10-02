f69f058f43683970df37e8772374eeacf73ae02f
Oyvind Nordsveen <oyvind.nordsveen@nav.no>
2013-10-02 11:52:23 +0200

    Fjernet soknader-panel

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/pages/intern/LamellHandler.java
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/pages/intern/LamellHandler.java
@@ -10,7 +10,6 @@ import no.nav.modig.modia.lamell.LerretFactory;
 import no.nav.modig.modia.lamell.TokenLamellPanel;
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.pages.lameller.GenericLerret;
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.pages.lameller.oversikt.Oversikt;
-import no.nav.sbl.dialogarena.soknader.panel.SoknaderPanel;
 import no.nav.sbl.dialogarena.sporsmalogsvar.innboks.BrukerhenvendelserPanel;
 import no.nav.sykmeldingsperioder.SykmeldingsperiodePanel;
 import no.nav.sykmeldingsperioder.foreldrepenger.ForeldrepengerPanel;
@@ -35,7 +34,6 @@ public class LamellHandler implements Serializable {
     public static final String LAMELL_OVERSIKT = "oversikt";
     public static final String LAMELL_BRUKERPROFIL = "brukerprofil";
     public static final String LAMELL_BRUKERHENVENDELSER = "brukerhenvendelser";
-    public static final String LAMELL_SOKNADER = "soknader";
     public static final String PANEL = "panel";
     private TokenLamellPanel lamellPanel;
     private List<Lerret> lerretList = new ArrayList<>();
@@ -104,8 +102,7 @@ public class LamellHandler implements Serializable {
                 createOversiktLamell(),
                 createKontrakterLamell(),
                 createBrukerprofilLamell(),
-                createBrukerhenvendelserLamell(),
-                createSoknaderLamell()
+                createBrukerhenvendelserLamell()
         );
     }
 
@@ -141,15 +138,6 @@ public class LamellHandler implements Serializable {
             @Override
             public Lerret createLerret(String id) {
                 return addLerretToListAndReturn(new BrukerhenvendelserPanel(id, fnrFromRequest, null)); //TODO: Missing MeldingService
-            }
-        });
-    }
-
-    private LamellFactory createSoknaderLamell() {
-        return newLamellFactory(LAMELL_SOKNADER, "S", new LerretFactory() {
-            @Override
-            public Lerret createLerret(String id) {
-                return addLerretToListAndReturn(new SoknaderPanel(id));
             }
         });
     }
