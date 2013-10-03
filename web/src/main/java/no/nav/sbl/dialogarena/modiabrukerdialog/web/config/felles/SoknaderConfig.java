ae832edf33fb7fa0b1a2f4fb38227c907700a732
Oyvind Nordsveen <oyvind.nordsveen@nav.no>
2013-10-03 13:14:20 +0200

    Checkstyle unused imports

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/felles/SoknaderConfig.java
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/felles/SoknaderConfig.java
@@ -1,10 +1,7 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.web.config.felles;
 
-import no.nav.modig.security.ws.SystemSAMLOutInterceptor;
-import no.nav.modig.security.ws.UserSAMLOutInterceptor;
 import no.nav.sbl.dialogarena.soknader.service.SoknaderService;
 import no.nav.sbl.dialogarena.soknader.service.SoknaderServiceImpl;
-import no.nav.tjeneste.domene.brukerdialog.henvendelsefelles.v1.HenvendelsePortType;
 import no.nav.tjeneste.virksomhet.sakogbehandling.v1.HentBehandlingHentBehandlingBehandlingIkkeFunnet;
 import no.nav.tjeneste.virksomhet.sakogbehandling.v1.HentBehandlingskjedensBehandlingerHentBehandlingskjedensBehandlingerBehandlingskjedeIkkeFunnet;
 import no.nav.tjeneste.virksomhet.sakogbehandling.v1.SakOgBehandlingPortType;
@@ -14,19 +11,11 @@ import no.nav.tjeneste.virksomhet.sakogbehandling.v1.meldinger.HentBehandlingReq
 import no.nav.tjeneste.virksomhet.sakogbehandling.v1.meldinger.HentBehandlingResponse;
 import no.nav.tjeneste.virksomhet.sakogbehandling.v1.meldinger.HentBehandlingskjedensBehandlingerRequest;
 import no.nav.tjeneste.virksomhet.sakogbehandling.v1.meldinger.HentBehandlingskjedensBehandlingerResponse;
-import org.apache.cxf.feature.LoggingFeature;
-import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
-import org.apache.cxf.ws.addressing.WSAddressingFeature;
-import org.apache.cxf.ws.security.SecurityConstants;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import javax.jws.WebParam;
-import java.util.HashMap;
-import java.util.Map;
-
-import static no.nav.sbl.dialogarena.modiabrukerdialog.web.config.felles.Utils.konfigurerMedHttps;
 
 @Configuration
 public class SoknaderConfig {
