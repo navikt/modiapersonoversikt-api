4415aa92b26576f59441dbe4b5367146ea2c8f6a
Rune Flobakk <rune.flobakk@nav.no>
2013-10-03 11:04:13 +0200

    Modus-klasse final med private constructor.

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/Modus.java
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/Modus.java
@@ -1,7 +1,9 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.web;
 
-public class Modus {
+public final class Modus {
     public static final String BESVARE = "modia.global.modus.besvare";
     public static final String KVITTERING = "modia.global.modus.kvittering";
     public static final String NULLSTILT = "modia.global.modus.nullstilt";
+
+    private Modus() {}
 }
