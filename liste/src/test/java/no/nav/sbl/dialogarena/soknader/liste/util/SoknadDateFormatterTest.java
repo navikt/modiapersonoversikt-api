8da4b80dbe5123551cf504c69c89dbda12054c39
Oyvind Nordsveen <oyvind.nordsveen@nav.no>
2013-10-04 13:11:53 +0200

    Dateformatter test

---
+++ liste/src/test/java/no/nav/sbl/dialogarena/soknader/liste/util/SoknadDateFormatterTest.java
@@ -0,0 +1,18 @@
+package no.nav.sbl.dialogarena.soknader.liste.util;
+
+import org.joda.time.DateTime;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SoknadDateFormatterTest {
+
+    @Test
+    public void testPrintShortDate() throws Exception {
+        DateTime dt = new DateTime(1980,1,2,12,0);
+        assertThat(SoknadDateFormatter.printShortDate(dt), is(equalTo("02.01.1980" )));
+
+    }
+}
