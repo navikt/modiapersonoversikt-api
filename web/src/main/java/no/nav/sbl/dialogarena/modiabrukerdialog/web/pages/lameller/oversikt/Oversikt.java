a126c63bb306ff0a7d8a5d0194405fa971c82283
Oyvind Nordsveen <oyvind.nordsveen@nav.no>
2013-10-04 10:16:14 +0200

    Byttet til soknad-liste

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/pages/lameller/oversikt/Oversikt.java
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/pages/lameller/oversikt/Oversikt.java
@@ -2,21 +2,28 @@ package no.nav.sbl.dialogarena.modiabrukerdialog.web.pages.lameller.oversikt;
 
 import no.nav.modig.modia.lamell.Lerret;
 import no.nav.modig.modia.widget.LenkeWidget;
+import no.nav.sbl.dialogarena.soknader.liste.SoknadListe;
+import no.nav.sbl.dialogarena.soknader.service.SoknaderService;
 import no.nav.sbl.dialogarena.soknader.widget.SoknaderWidget;
 import no.nav.sykmeldingsperioder.widget.SykepengerWidget;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.util.ListModel;
 
+import javax.inject.Inject;
+
 import static java.util.Arrays.asList;
 
 public class Oversikt extends Lerret {
+
+    @Inject
+    private SoknaderService soknaderService;
 
     public Oversikt(String id, String fnr) {
         super(id);
         add(
                 new LenkeWidget("lenker", "E", new ListModel<>(asList("kontrakter"))),
                 new SykepengerWidget("sykepenger", "Y", new Model<>(fnr)),
-                new SoknaderWidget("soknader", "S", new Model<>(fnr))
+                new SoknadListe("soknader",  new ListModel<>(soknaderService.getSoknader("")))
         );
     }
 
