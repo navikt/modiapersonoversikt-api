3b9990ec86b514f740e62cb1b131a19cc9b5634a
Rune Flobakk <rune.flobakk@nav.no>
2013-10-07 15:54:54 +0200

    Erstatter commons-logging med jcl-over-slf4j i avhengigheter.

--- besvare/src/test/java/no/nav/sbl/dialogarena/sporsmalogsvar/JettyBesvare.java
+++ besvare/src/test/java/no/nav/sbl/dialogarena/sporsmalogsvar/JettyBesvare.java
@@ -1,12 +1,16 @@
 package no.nav.sbl.dialogarena.sporsmalogsvar;
 
-import java.io.File;
 import no.nav.modig.core.context.SubjectHandler;
 import no.nav.modig.core.context.ThreadLocalSubjectHandler;
 import no.nav.modig.testcertificates.TestCertificates;
 import no.nav.sbl.dialogarena.common.jetty.Jetty;
 import no.nav.sbl.dialogarena.test.SystemProperties;
 
+import java.io.File;
+
+import static no.nav.modig.lang.collections.FactoryUtils.gotKeypress;
+import static no.nav.modig.lang.collections.RunnableUtils.first;
+import static no.nav.modig.lang.collections.RunnableUtils.waitFor;
 import static no.nav.sbl.dialogarena.test.path.FilesAndDirs.TEST_RESOURCES;
 
 public final class JettyBesvare {
@@ -17,7 +21,7 @@ public final class JettyBesvare {
 
         TestCertificates.setupKeyAndTrustStore();
 
-        Jetty.usingWar(new File(TEST_RESOURCES, "webapp")).at("besvare").port(8383)
-                .buildJetty().start();
+        Jetty jetty = Jetty.usingWar(new File(TEST_RESOURCES, "webapp")).at("besvare").port(8383).buildJetty();
+        jetty.startAnd(first(waitFor(gotKeypress())).then(jetty.stop));
     }
 }
