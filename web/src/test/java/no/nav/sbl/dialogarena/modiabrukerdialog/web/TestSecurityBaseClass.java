aab33fde0a006c933604059e81c00005bebb7193
Jarle Svendsrud <jarle.svendsrud@nav.no>
2013-10-07 10:37:01 +0200

    Forbedret app-context test

--- web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/TestSecurityBaseClass.java
+++ web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/TestSecurityBaseClass.java
@@ -4,10 +4,8 @@ import no.nav.modig.testcertificates.TestCertificates;
 import no.nav.sbl.dialogarena.test.SystemProperties;
 import org.junit.BeforeClass;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ActiveProfiles;
 
 @DirtiesContext
-@ActiveProfiles({"test"})
 public class TestSecurityBaseClass {
 
     @BeforeClass
