41dee5e4b93fdc89fa96d5cdfea07f570951779c
Oyvind Nordsveen <oyvind.nordsveen@nav.no>
2013-10-09 11:18:05 +0200

    Css for soknads status

--- liste/src/main/java/no/nav/sbl/dialogarena/soknader/liste/SoknadItem.java
+++ liste/src/main/java/no/nav/sbl/dialogarena/soknader/liste/SoknadItem.java
@@ -1,10 +1,13 @@
 package no.nav.sbl.dialogarena.soknader.liste;
 
-import org.apache.wicket.behavior.Behavior;
 import no.nav.sbl.dialogarena.soknader.domain.Soknad;
+import org.apache.wicket.Component;
+import org.apache.wicket.behavior.AttributeAppender;
+import org.apache.wicket.behavior.Behavior;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 
 import static no.nav.modig.wicket.conditional.ConditionalUtils.visibleIf;
 import static no.nav.modig.wicket.model.ModelUtils.isEmptyString;
@@ -26,12 +29,48 @@ public class SoknadItem extends Panel {
                 new Label("behandlingStart", "Under behandling siden  " + behandlingStart).add(visibleIfStringIsNotEmpty(behandlingStart)),
                 new Label("behandlingsTid", "Normert behandlingstid " + soknad.getNormertBehandlingsTid()).add(visibleIfStringIsNotEmpty(soknad.getNormertBehandlingsTid())),
                 new Label("ferdigBehandlet", "Ferdig behandlet " + ferdigdato).add(visibleIfStringIsNotEmpty(ferdigdato)),
-                new Label("status", soknad.getSoknadStatus())
+                createStatusLabel(soknad),
+                createStatusIcon(soknad)
         );
     }
 
     private Behavior visibleIfStringIsNotEmpty(String innsendtDato) {
         return visibleIf(not(isEmptyString(of(innsendtDato))));
+    }
+
+    private Component createStatusLabel(Soknad soknad){
+        if(soknad.getSoknadStatus().equals(Soknad.SoknadStatus.GAMMEL_FERDIG)){
+            return new Label("status", "Gammel");
+        }
+        if(soknad.getSoknadStatus().equals(Soknad.SoknadStatus.MOTTATT)){
+            return new Label("status", "Mottat");
+        }
+        if(soknad.getSoknadStatus().equals(Soknad.SoknadStatus.NYLIG_FERDIG)){
+            return new Label("status", "Nylig ferdig");
+        }
+        if(soknad.getSoknadStatus().equals(Soknad.SoknadStatus.UNDER_BEHANDLING)){
+            return new Label("status", "Under behandling");
+        }
+        return new Label("status", "Ukjent status");
+
+
+    }
+
+    private Component createStatusIcon(Soknad soknad){
+        Label icon = new Label("status-icon", "");
+        if(soknad.getSoknadStatus().equals(Soknad.SoknadStatus.GAMMEL_FERDIG)){
+            icon.add(new AttributeAppender("class",new Model<>("gammel-ferdig"), " "));
+        }
+        if(soknad.getSoknadStatus().equals(Soknad.SoknadStatus.MOTTATT)){
+            icon.add(new AttributeAppender("class",new Model<>("mottat"), " "));
+        }
+        if(soknad.getSoknadStatus().equals(Soknad.SoknadStatus.NYLIG_FERDIG)){
+            icon.add(new AttributeAppender("class",new Model<>("nylig-ferdig"), " "));
+        }
+        if(soknad.getSoknadStatus().equals(Soknad.SoknadStatus.UNDER_BEHANDLING)){
+            icon.add(new AttributeAppender("class",new Model<>("under-behandling"), " "));
+        }
+        return icon;
     }
 
 }
