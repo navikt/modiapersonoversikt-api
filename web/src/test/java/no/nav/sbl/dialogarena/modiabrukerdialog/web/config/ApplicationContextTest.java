72420d4084657428d0ffbfa61213e875b44dfd02
Jarle Svendsrud <jarle.svendsrud@nav.no>
2013-10-10 10:42:43 +0200

    Endret AppConTest til å sjekke samme profiler som definert i web.xml

--- web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/ApplicationContextTest.java
+++ web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/ApplicationContextTest.java
@@ -3,9 +3,22 @@ package no.nav.sbl.dialogarena.modiabrukerdialog.web.config;
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.TestSecurityBaseClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@ActiveProfiles({
+        "kjerneinfoDefault",
+        "sykemeldingsperioderDefault",
+        "kontrakterDefault",
+        "personsokDefault",
+        "brukerprofilDefault",
+        "behandleBrukerprofilDefault",
+        "brukerhenvendelserDefault",
+        "oppgavebehandlingDefault",
+        "henvendelseinnsynDefault",
+        "soknaderTest"
+})
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = ApplicationContext.class)
 public class ApplicationContextTest extends TestSecurityBaseClass {
