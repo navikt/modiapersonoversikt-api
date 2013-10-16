9b04499a545d9d559d737a728767c9fa575450f9
Jarle Svendsrud <jarle.svendsrud@nav.no>
2013-10-16 08:32:14 +0200

    Fjernet dirtiescontext

--- web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/ApplicationContextTest.java
+++ web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/ApplicationContextTest.java
@@ -3,7 +3,6 @@ package no.nav.sbl.dialogarena.modiabrukerdialog.web.config;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -11,7 +10,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import static no.nav.modig.testcertificates.TestCertificates.setupKeyAndTrustStore;
 import static no.nav.sbl.dialogarena.test.SystemProperties.setFrom;
 
-@DirtiesContext
 @ActiveProfiles({
         "kjerneinfoDefault",
         "sykemeldingsperioderDefault",
