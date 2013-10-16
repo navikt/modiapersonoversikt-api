386481a651f3ff1c1c2205c02df388b67848ee0b
Oyvind Nordsveen <oyvind.nordsveen@nav.no>
2013-10-16 13:27:21 +0200

    Begynt på lamell

---
+++ lamell/src/main/java/no/nav/sbl/dialogarena/utbetaling/lamell/UtbetalingLamell.java
@@ -0,0 +1,9 @@
+package no.nav.sbl.dialogarena.utbetaling.lamell;
+
+import no.nav.modig.modia.lamell.Lerret;
+
+public class UtbetalingLamell  extends Lerret {
+    public UtbetalingLamell(String id) {
+        super(id);
+    }
+}
