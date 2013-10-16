12262739ecb751d73589c741d011818fafe1e5f6
Oyvind Nordsveen <oyvind.nordsveen@nav.no>
2013-10-16 14:17:40 +0200

    Flere felter i utbetalingslijne og litt html struktur

--- lamell/src/main/java/no/nav/sbl/dialogarena/utbetaling/lamell/UtbetalingPanel.java
+++ lamell/src/main/java/no/nav/sbl/dialogarena/utbetaling/lamell/UtbetalingPanel.java
@@ -1,5 +1,7 @@
 package no.nav.sbl.dialogarena.utbetaling.lamell;
 
+import no.nav.modig.lang.option.Optional;
+import no.nav.sbl.dialogarena.time.Datoformat;
 import no.nav.sbl.dialogarena.utbetaling.domain.Utbetaling;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
@@ -7,7 +9,18 @@ import org.apache.wicket.markup.html.panel.Panel;
 public class UtbetalingPanel extends Panel {
     public UtbetalingPanel(String id, Utbetaling utbetaling) {
         super(id);
+        String utbetalingsDato = Optional.optional(utbetaling.getUtbetalingsDato()).map(Datoformat.KORT).getOrElse("Ingen utbetalingsdato");
+        String startDato =Optional.optional(utbetaling.getStartDate()).map(Datoformat.KORT).getOrElse("");
+        String sluttDato =Optional.optional(utbetaling.getEndDate()).map(Datoformat.KORT).getOrElse("");
+
+        String periode = startDato + " - " + sluttDato;
+
         add(new Label("beskrivelse", utbetaling.getBeskrivelse()));
+        add(new Label("utbetalingdato", utbetalingsDato));
+        add(new Label("belop", utbetaling.getNettoBelop()));
+        add(new Label("periode", periode));
+        add(new Label("status", utbetaling.getStatuskode()));
+
 
     }
 }
