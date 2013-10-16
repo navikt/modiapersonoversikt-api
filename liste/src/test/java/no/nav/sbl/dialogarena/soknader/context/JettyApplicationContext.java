a230aafbee03822f40127093c2330cd712d7ad51
Oyvind Nordsveen <oyvind.nordsveen@nav.no>
2013-10-16 13:31:41 +0200

    Unused import

--- liste/src/test/java/no/nav/sbl/dialogarena/soknader/context/JettyApplicationContext.java
+++ liste/src/test/java/no/nav/sbl/dialogarena/soknader/context/JettyApplicationContext.java
@@ -1,7 +1,6 @@
 package no.nav.sbl.dialogarena.soknader.context;
 
 import no.nav.sbl.dialogarena.soknader.SoknaderTestApplication;
-import no.nav.sbl.dialogarena.soknader.context.mock.SoknaderMockContext;
 import no.nav.tjeneste.virksomhet.sakogbehandling.v1.SakOgBehandlingPortType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
