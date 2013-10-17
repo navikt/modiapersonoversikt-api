6a0df494b1301d669567067e1596e68f8a4a3023
Jarle Svendsrud <jarle.svendsrud@nav.no>
2013-10-17 14:49:13 +0200

    Byttet ut mer mockito med dummyimplementasjoner

--- web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/mock/SykepengerWidgetMockContext.java
+++ web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/mock/SykepengerWidgetMockContext.java
@@ -1,17 +1,23 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.web.config.mock;
 
+import no.nav.modig.modia.widget.panels.InfoPanelVM;
 import no.nav.sykmeldingsperioder.widget.SykepengerWidgetService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import static org.mockito.Mockito.mock;
+import java.util.List;
 
 @Configuration
 public class SykepengerWidgetMockContext {
 
     @Bean
     public SykepengerWidgetService sykepengerWidgetService() {
-        return mock(SykepengerWidgetService.class);
+        return new SykepengerWidgetService() {
+            @Override
+            public List<InfoPanelVM> getWidgetContent(String fnr) {
+                return null;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+        };
     }
 
 }
--- web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/mock/SykmeldingsperiodePanelMockContext.java
+++ web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/mock/SykmeldingsperiodePanelMockContext.java
@@ -1,23 +1,34 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.web.config.mock;
 
+import no.nav.modig.modia.ping.PingResult;
 import no.nav.sykmeldingsperioder.consumer.sykepenger.SykepengerServiceBi;
+import no.nav.sykmeldingsperioder.consumer.sykepenger.mapping.to.SykepengerRequest;
+import no.nav.sykmeldingsperioder.consumer.sykepenger.mapping.to.SykepengerResponse;
 import no.nav.sykmeldingsperioder.loader.SykmeldingsperiodeLoader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import static org.mockito.Mockito.mock;
 
 @Configuration
 public class SykmeldingsperiodePanelMockContext {
 
     @Bean
     public SykmeldingsperiodeLoader sykmeldingsperiodeLoader() {
-        return mock(SykmeldingsperiodeLoader.class);
+        return new SykmeldingsperiodeLoader();
     }
 
     @Bean
     public SykepengerServiceBi sykepengerServiceBi() {
-        return mock(SykepengerServiceBi.class);
+        return new SykepengerServiceBi() {
+            @Override
+            public SykepengerResponse hentSykmeldingsperioder(SykepengerRequest request) {
+                return null;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            @Override
+            public PingResult ping() {
+                return null;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+        };
     }
 
 }
