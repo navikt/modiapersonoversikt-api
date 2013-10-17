fcfbad746852e0649027ab5e5c3f793f1e18a9d2
Oyvind Nordsveen <oyvind.nordsveen@nav.no>
2013-10-17 11:07:36 +0200

    Visning av skjult del av panelet

--- lamell/src/main/java/no/nav/sbl/dialogarena/utbetaling/lamell/UtbetalingPanel.java
+++ lamell/src/main/java/no/nav/sbl/dialogarena/utbetaling/lamell/UtbetalingPanel.java
@@ -3,6 +3,8 @@ package no.nav.sbl.dialogarena.utbetaling.lamell;
 import no.nav.modig.lang.option.Optional;
 import no.nav.sbl.dialogarena.time.Datoformat;
 import no.nav.sbl.dialogarena.utbetaling.domain.Utbetaling;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
 
@@ -16,22 +18,35 @@ public class UtbetalingPanel extends Panel {
         String startDato =Optional.optional(utbetaling.getStartDate()).map(Datoformat.KORT).getOrElse("");
         String sluttDato =Optional.optional(utbetaling.getEndDate()).map(Datoformat.KORT).getOrElse("");
 
-        String periode = startDato + " - " + sluttDato;
+        final String periode = startDato + " - " + sluttDato;
 
 
         NumberFormat currencyInstance = NumberFormat.getNumberInstance(Locale.forLanguageTag("nb-no"));
         currencyInstance.setMinimumFractionDigits(2);
         String nettoBelop = currencyInstance.format( utbetaling.getNettoBelop()) + " " + utbetaling.getValuta();
-
+        final Label hidden = new Label("hidden", "SKJULT");
+        hidden.setVisible(false);
+        hidden.setOutputMarkupId(true);
+        hidden.setOutputMarkupPlaceholderTag(true);
 
 
         add(new Label("beskrivelse", utbetaling.getBeskrivelse()));
+        add(hidden);
         add(new Label("utbetalingdato", utbetalingsDato));
         add(new Label("belop", nettoBelop));
         add(new Label("periode", periode));
         add(new Label("status", utbetaling.getStatuskode()));
         add(new Label("kontonr", utbetaling.getKontoNr()));
-
+        add(new AjaxLink<Void>("expandbutton") {
+            @Override
+            public void onClick(AjaxRequestTarget target) {
+                System.out.println("Link klikket");
+                System.out.println("target = " + target);
+                hidden.setVisible(!hidden.isVisible());
+//                hidden.setOutputMarkupId(true);
+                target.add(hidden);
+            }
+        });
 
     }
 }
