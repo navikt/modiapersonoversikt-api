b03a7911e2f2ba18af64959f1f8cf0d39750f898
Oyvind Nordsveen <oyvind.nordsveen@nav.no>
2013-10-18 15:35:08 +0200

    Config for søknader

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/artifacts/ArtifactsConfig.java
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/artifacts/ArtifactsConfig.java
@@ -2,6 +2,7 @@ package no.nav.sbl.dialogarena.modiabrukerdialog.web.config.artifacts;
 
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.config.artifacts.kjerneinfo.KjerneinfoContext;
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.config.artifacts.personsok.PersonsokContext;
+import no.nav.sbl.dialogarena.soknader.liste.config.SoknaderConfig;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
@@ -12,7 +13,8 @@ import org.springframework.context.annotation.Import;
 @Configuration
 @Import(value = {
         PersonsokContext.class,
-        KjerneinfoContext.class
+        KjerneinfoContext.class,
+        SoknaderConfig.class
 })
 public class ArtifactsConfig {
 
