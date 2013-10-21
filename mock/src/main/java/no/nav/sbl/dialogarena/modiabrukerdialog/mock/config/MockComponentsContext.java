79cbe694cb8bcef336d2e6b576e4070745f7fdb5
Jarle Svendsrud <jarle.svendsrud@nav.no>
2013-10-21 14:40:47 +0200

    La toppnivåkontekst i webmodul for å slippe sirkulær mavenavhengighet. StartJettyNoIntegration får nå kjørt uten feil, basic springoppsett på plass

--- mock/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/mock/config/MockComponentsContext.java
+++ mock/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/mock/config/MockComponentsContext.java
@@ -3,5 +3,7 @@ package no.nav.sbl.dialogarena.modiabrukerdialog.mock.config;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class MockApplicationContext {
+public class MockComponentsContext {
+
+
 }
---
+++ web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/jetty/NoIntegrationContext.java
@@ -0,0 +1,34 @@
+package no.nav.sbl.dialogarena.modiabrukerdialog.web.config.jetty;
+
+import no.nav.modig.security.tilgangskontroll.policy.pep.EnforcementPoint;
+import no.nav.sbl.dialogarena.modiabrukerdialog.mock.config.MockComponentsContext;
+import no.nav.sbl.dialogarena.modiabrukerdialog.web.WicketApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import static org.mockito.Mockito.mock;
+
+@Configuration
+@Import({
+        MockComponentsContext.class,
+
+})
+public class NoIntegrationContext {
+
+    @Bean
+    public WicketApplication modiaApplication() {
+        return new WicketApplication();
+    }
+
+    @Bean(name = "pep")
+    public EnforcementPoint pep() {
+        return mock(EnforcementPoint.class);
+    }
+
+    @Bean(name = "kjerneinfoPep")
+    public EnforcementPoint kjerneinfoPep() {
+        return mock(EnforcementPoint.class);
+    }
+
+}
