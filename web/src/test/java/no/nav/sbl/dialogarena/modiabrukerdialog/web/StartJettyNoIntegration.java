9a06bd35d656ebff5494cfd49e357bee9e91df19
Jarle Svendsrud <jarle.svendsrud@nav.no>
2013-10-21 13:57:41 +0200

    La på subjecthandler for jetty uten integrasjon, trenger kun springoppsett nå

--- web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/StartJettyNoIntegration.java
+++ web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/StartJettyNoIntegration.java
@@ -1,25 +1,24 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.web;
 
-import no.nav.sbl.dialogarena.common.jetty.Jetty;
-
 import java.io.File;
 
 import static no.nav.modig.core.test.FilesAndDirs.TEST_RESOURCES;
 import static no.nav.modig.core.test.FilesAndDirs.WEBAPP_SOURCE;
-import static no.nav.modig.lang.collections.FactoryUtils.gotKeypress;
-import static no.nav.modig.lang.collections.RunnableUtils.first;
-import static no.nav.modig.lang.collections.RunnableUtils.waitFor;
 import static no.nav.sbl.dialogarena.common.jetty.Jetty.usingWar;
+import static no.nav.sbl.dialogarena.modiabrukerdialog.web.StartJetty.createLoginService;
+import static no.nav.sbl.dialogarena.test.SystemProperties.setFrom;
 
 public class StartJettyNoIntegration {
 
     public static void main(String ... args) {
-        Jetty jetty = usingWar(WEBAPP_SOURCE)
+        setFrom("jetty-mock-environment.properties");
+        usingWar(WEBAPP_SOURCE)
                 .at("modiabrukerdialog")
+                .withLoginService(createLoginService())
                 .port(8083)
                 .overrideWebXml(new File(TEST_RESOURCES, "mock-web.xml"))
-                .buildJetty();
-        jetty.startAnd(first(waitFor(gotKeypress())).then(jetty.stop));
+                .buildJetty()
+                .start();
     }
 
 }
