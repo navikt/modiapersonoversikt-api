34e75ea8f000455c92bdf854ab3693466ba9bb00
Jarle Svendsrud <jarle.svendsrud@nav.no>
2013-10-21 12:27:34 +0200

    Påbegynt mockmodul for startjetty

---
+++ mock/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/mock/StartJettyNoIntegration.java
@@ -0,0 +1,25 @@
+package no.nav.sbl.dialogarena.modiabrukerdialog.mock;
+
+import no.nav.sbl.dialogarena.common.jetty.Jetty;
+
+import java.io.File;
+
+import static no.nav.modig.core.test.FilesAndDirs.TEST_RESOURCES;
+import static no.nav.modig.core.test.FilesAndDirs.WEBAPP_SOURCE;
+import static no.nav.modig.lang.collections.FactoryUtils.gotKeypress;
+import static no.nav.modig.lang.collections.RunnableUtils.first;
+import static no.nav.modig.lang.collections.RunnableUtils.waitFor;
+import static no.nav.sbl.dialogarena.common.jetty.Jetty.usingWar;
+
+public class StartJettyNoIntegration {
+
+    public static void main(String ... args) {
+        Jetty jetty = usingWar(WEBAPP_SOURCE)
+                .at("modiabrukerdialog")
+                .port(8083)
+                .overrideWebXml(new File(TEST_RESOURCES, "mock-web.xml"))
+                .buildJetty();
+        jetty.startAnd(first(waitFor(gotKeypress())).then(jetty.stop));
+    }
+
+}
