c27205c25281c438a001b8ee274a8869b825c99c
Oyvind Nordsveen <oyvind.nordsveen@nav.no>
2013-10-21 12:46:25 +0200

    Satt opp soknader config import

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/artifacts/ArtifactsConfig.java
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/artifacts/ArtifactsConfig.java
@@ -2,7 +2,6 @@ package no.nav.sbl.dialogarena.modiabrukerdialog.web.config.artifacts;
 
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.config.artifacts.kjerneinfo.KjerneinfoContext;
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.config.artifacts.personsok.PersonsokContext;
-import no.nav.sbl.dialogarena.soknader.liste.config.SoknaderConfig;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
@@ -13,8 +12,7 @@ import org.springframework.context.annotation.Import;
 @Configuration
 @Import(value = {
         PersonsokContext.class,
-        KjerneinfoContext.class,
-        SoknaderConfig.class
+        KjerneinfoContext.class
 })
 public class ArtifactsConfig {
 
--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/services/ServicesConfig.java
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/services/ServicesConfig.java
@@ -1,11 +1,14 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.web.config.services;
 
+import no.nav.sbl.dialogarena.soknader.liste.config.SoknaderConfig;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 /**
  * MODIA ønsker å selv definere opp sine tjenester for å full kontroll over springoppsettet
  * til sine komponenter
  */
 @Configuration
+@Import({SoknaderConfig.class})
 public class ServicesConfig {
 }
