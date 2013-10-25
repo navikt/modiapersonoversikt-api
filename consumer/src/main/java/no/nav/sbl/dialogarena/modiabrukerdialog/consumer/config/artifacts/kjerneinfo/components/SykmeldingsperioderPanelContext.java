17e72f3535fd5e2ad8c9afddbfb2d2821424b4a2
Håkon Haga <hakon.haga@nav.no>
2013-10-25 13:58:14 +0200

    clean up

--- consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/artifacts/kjerneinfo/components/SykmeldingsperioderPanelContext.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/artifacts/kjerneinfo/components/SykmeldingsperioderPanelContext.java
@@ -1,12 +1,13 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.artifacts.kjerneinfo.components;
 
+import no.nav.sykmeldingsperioder.config.SykmeldingsperioderPanelConfig;
 import no.nav.sykmeldingsperioder.config.spring.ConsumerConfig;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import({
-        no.nav.sykmeldingsperioder.config.SykmeldingsperioderPanelConfig.class,
+        SykmeldingsperioderPanelConfig.class,
         ConsumerConfig.class
 })
 public class SykmeldingsperioderPanelContext {
