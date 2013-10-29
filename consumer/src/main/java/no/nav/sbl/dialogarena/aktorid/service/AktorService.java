554f53fdc0985feccaabbf566281dafc1e417a61
Jarle Svendsrud <jarle.svendsrud@nav.no>
2013-10-29 14:49:17 +0100

    QA Integrasjon med aktørid

--- consumer/src/main/java/no/nav/sbl/dialogarena/aktorid/service/AktorService.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/aktorid/service/AktorService.java
@@ -11,16 +11,18 @@ public class AktorService {
     @Inject
     private AktoerPortType ws;
 
-    public AktorService() {}
-
     public String getAktorId(String fodselsnummer) {
         try {
-            HentAktoerIdForIdentRequest request = new HentAktoerIdForIdentRequest();
-            request.setIdent(fodselsnummer);
-            return ws.hentAktoerIdForIdent(request).getAktoerId();
+            return ws.hentAktoerIdForIdent(createRequest(fodselsnummer)).getAktoerId();
         } catch (Exception e) {
             throw new SystemException("Feil ved henting av aktorId for fnr: " + fodselsnummer, e);
         }
+    }
+
+    private HentAktoerIdForIdentRequest createRequest(String fodselsnummer) {
+        HentAktoerIdForIdentRequest request = new HentAktoerIdForIdentRequest();
+        request.setIdent(fodselsnummer);
+        return request;
     }
 
 }
