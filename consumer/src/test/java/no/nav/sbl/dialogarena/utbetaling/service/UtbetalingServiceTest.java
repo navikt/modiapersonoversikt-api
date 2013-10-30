14071f5c6080ee18422aaa9345178f998207278e
Borge Bardo <borge.bardo@nav.no>
2013-10-30 12:43:37 +0100

    fjerner hamcrest-core - bruk hamcrest-library

--- consumer/src/test/java/no/nav/sbl/dialogarena/utbetaling/service/UtbetalingServiceTest.java
+++ consumer/src/test/java/no/nav/sbl/dialogarena/utbetaling/service/UtbetalingServiceTest.java
@@ -17,7 +17,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import static org.hamcrest.core.Is.is;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
