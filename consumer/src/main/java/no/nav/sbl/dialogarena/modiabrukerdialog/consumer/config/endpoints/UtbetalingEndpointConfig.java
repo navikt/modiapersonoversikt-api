2f6ffa61a3dfa78e5171ed96872c8a1aa5b1d113
Kari B. Schjønsby <Kari.Bergh.Schjonsby@nav.no>
2013-11-01 09:57:28 +0100

    Leser use mock variabel fra start.properties

--- consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/endpoints/UtbetalingEndpointConfig.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/endpoints/UtbetalingEndpointConfig.java
@@ -5,25 +5,29 @@ import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.endpoints.portty
 import no.nav.sbl.dialogarena.modiabrukerdialog.mock.config.endpoints.MockPingable;
 import no.nav.sbl.dialogarena.modiabrukerdialog.mock.config.endpoints.UtbetalingPortTypeMock;
 import no.nav.virksomhet.tjenester.utbetaling.v2.UtbetalingPortType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import static no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.endpoints.ConfigUtil.setUseMock;
+
 @Configuration
 public class UtbetalingEndpointConfig {
-
-    private boolean useMock;
+    private static final Logger LOG = LoggerFactory.getLogger(UtbetalingEndpointConfig.class);
     private final UtbetalingPortTypeImpl portType;
     private final UtbetalingPortTypeMock portTypeMock;
+    private boolean useMock;
 
     public UtbetalingEndpointConfig() {
-        useMock = true;
+        useMock = setUseMock("start.utbetaling.withintegration", LOG);
         portType = new UtbetalingPortTypeImpl();
         portTypeMock = new UtbetalingPortTypeMock();
     }
 
     @Bean
     public UtbetalingPortType utbetalingPortType() {
-        if(useMock) {
+        if (useMock) {
             return portTypeMock.utbetalingPortType();
         }
         return portType.utbetalingPortType();
