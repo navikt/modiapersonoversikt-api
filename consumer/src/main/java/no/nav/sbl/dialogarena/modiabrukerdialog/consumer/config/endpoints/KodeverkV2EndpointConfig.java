7ffb83c098a0dc8c8e578a2061f01dedf1e3ef8b
Kari B. Schjønsby <Kari.Bergh.Schjonsby@nav.no>
2013-11-01 10:35:52 +0100

    Henvendelsesmeldingerporttypeimpl

--- consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/endpoints/KodeverkV2EndpointConfig.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/endpoints/KodeverkV2EndpointConfig.java
@@ -1,51 +1,47 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.endpoints;
 
-import no.nav.modig.lang.option.Optional;
-import no.nav.sbl.dialogarena.common.integrasjon.features.TimeoutFeature;
-import no.nav.sbl.dialogarena.common.kodeverk.CachingKodeverkClient;
-import no.nav.sbl.dialogarena.common.kodeverk.DefaultKodeverkClient;
 import no.nav.sbl.dialogarena.common.kodeverk.KodeverkClient;
+import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.endpoints.porttypeimpl.KodeverkV2EndpointConfigImpl;
+import no.nav.sbl.dialogarena.modiabrukerdialog.mock.config.endpoints.KodeverkV2PortTypeMock;
 import no.nav.tjeneste.virksomhet.kodeverk.v2.KodeverkPortType;
-import org.apache.cxf.feature.LoggingFeature;
-import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
-import org.apache.cxf.ws.addressing.WSAddressingFeature;
-import org.apache.cxf.ws.security.SecurityConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.File;
 import java.net.URL;
-import java.util.HashMap;
 
-import static no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.endpoints.EndpointsConfig.MODIA_CONNECTION_TIMEOUT;
-import static no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.endpoints.EndpointsConfig.MODIA_RECEIVE_TIMEOUT;
+import static no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.endpoints.ConfigUtil.setUseMock;
 
 @Configuration
 public class KodeverkV2EndpointConfig {
+    private static final Logger LOG = LoggerFactory.getLogger(KodeverkV2EndpointConfig.class);
 
     @Value("${kodeverkendpoint.v2.url}")
     private URL kodeverkEndpoint;
 
+    private boolean useMock;
+    private KodeverkV2EndpointConfigImpl portType = new KodeverkV2EndpointConfigImpl(kodeverkEndpoint);
+    private KodeverkV2PortTypeMock portTypeMock = new KodeverkV2PortTypeMock();
+
+    public KodeverkV2EndpointConfig() {
+        useMock = setUseMock("start.kodeverk.withintegration", LOG);
+    }
+
     @Bean(name = "kodeverkPortTypeV2")
     public KodeverkPortType kodeverkPortType() {
-        JaxWsProxyFactoryBean proxyFactoryBean = new JaxWsProxyFactoryBean();
-        proxyFactoryBean.setServiceClass(KodeverkPortType.class);
-        proxyFactoryBean.setAddress(kodeverkEndpoint.toString());
-        proxyFactoryBean.setWsdlURL("classpath:kodeverk/no/nav/tjeneste/virksomhet/kodeverk/v2/Kodeverk.wsdl");
-
-        //setter mustunderstand i header slik at tjenester som ikke forstår sikkerhetsheader ikke skal avvise requester
-        proxyFactoryBean.setProperties(new HashMap<String, Object>());
-        proxyFactoryBean.getProperties().put(SecurityConstants.MUST_UNDERSTAND, false);
-        proxyFactoryBean.getFeatures().add(new WSAddressingFeature());
-        proxyFactoryBean.getFeatures().add(new LoggingFeature());
-        proxyFactoryBean.getFeatures().add(new TimeoutFeature().withConnectionTimeout(MODIA_CONNECTION_TIMEOUT).withReceiveTimeout(MODIA_RECEIVE_TIMEOUT));
-
-        return proxyFactoryBean.create(KodeverkPortType.class);
+        if (useMock) {
+            return portTypeMock.kodeverkPortType();
+        }
+        return portType.kodeverkPortType();
     }
 
     @Bean
     public KodeverkClient kodeverkClient() {
-        return new CachingKodeverkClient(new DefaultKodeverkClient(kodeverkPortType()), new Optional.None<File>());
+        if (useMock) {
+            return portTypeMock.kodeverkClient();
+        }
+        return portType.kodeverkClient();
     }
 }
