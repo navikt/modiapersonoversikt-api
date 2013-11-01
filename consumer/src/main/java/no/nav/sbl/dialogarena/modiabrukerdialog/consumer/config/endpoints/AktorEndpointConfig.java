0702afa605121291ffd1d36fc4961d57869c05e9
Kari B. Schjønsby <Kari.Bergh.Schjonsby@nav.no>
2013-11-01 15:01:00 +0100

    AktorEndpoint med InstanceSwitcher

--- consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/endpoints/AktorEndpointConfig.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/endpoints/AktorEndpointConfig.java
@@ -3,61 +3,33 @@ package no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.endpoints;
 import no.nav.modig.modia.ping.Pingable;
 import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.endpoints.porttypeimpl.AktorPortTypeImpl;
 import no.nav.sbl.dialogarena.modiabrukerdialog.mock.config.endpoints.AktorPortTypeMock;
-import no.nav.sbl.dialogarena.modiabrukerdialog.mock.config.endpoints.MockPingable;
 import no.nav.tjeneste.virksomhet.aktoer.v1.AktoerPortType;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.net.URL;
 
-import static no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.endpoints.util.ConfigUtil.isInMockMode;
+import static no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.endpoints.util.InstanceSwitcher.createSwitcher;
 
 @Configuration
 public class AktorEndpointConfig {
-    private static final Logger LOG = LoggerFactory.getLogger(AktorEndpointConfig.class);
+
+    String key = "start.aktor.withintegration";
     @Value("${aktor.url}")
     private URL aktorEndpoint;
-    private boolean useMock;
-    private AktorPortTypeImpl portType = new AktorPortTypeImpl(aktorEndpoint);
-    private AktorPortTypeMock portTypeMock = new AktorPortTypeMock();
-
-    public AktorEndpointConfig() {
-        useMock = isInMockMode("start.aktor.withintegration");
-    }
+    private AktoerPortType portType = new AktorPortTypeImpl(aktorEndpoint).aktorPortType();
+    private AktoerPortType portTypeMock = new AktorPortTypeMock().aktorPortType();
+    private Pingable pingable = new AktorPortTypeImpl(aktorEndpoint).aktorIdPing();
+    private Pingable pingableMock = new AktorPortTypeImpl(aktorEndpoint).aktorIdPing();
 
     @Bean
     public AktoerPortType aktorPortType() {
-        if (useMock) {
-            return portTypeMock.aktorPortType();
-        }
-        return portType.aktorPortType();
+        return createSwitcher(portType, portTypeMock, key, AktoerPortType.class);
     }
 
     @Bean
     public Pingable aktorIdPing() {
-        if (useMock) {
-            return new MockPingable("AktorEndpointConfig");
-        }
-        return portType.aktorIdPing();
+        return createSwitcher(pingable, pingableMock, key, Pingable.class);
     }
-
-//    private AktoerPortType getAktoerPortType(final AktoerPortType aktoerPortType) {
-//        return new AktoerPortType() {
-//
-//            @Override
-//            @Cacheable("aktoridCache")
-//            public HentAktoerIdForIdentResponse hentAktoerIdForIdent(@WebParam(name = "request", targetNamespace = "") HentAktoerIdForIdentRequest hentAktoerIdForIdentRequest) throws HentAktoerIdForIdentPersonIkkeFunnet {
-//                return aktoerPortType.hentAktoerIdForIdent(hentAktoerIdForIdentRequest);
-//            }
-//
-//            @Override
-//            public void ping() {
-//                aktoerPortType.ping();
-//            }
-//        };
-//    }
-
 }
