e9affd6082af6a42c78e85991cb8cf849ea061b7
Kari B. Schjønsby <Kari.Bergh.Schjonsby@nav.no>
2013-11-04 12:55:09 +0100

    Switcher på PersonKjerneinfoServiceBi

--- consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/artifacts/kjerneinfo/components/mockable/PersonKjerneinfoConsumerConfigResolver.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/artifacts/kjerneinfo/components/mockable/PersonKjerneinfoConsumerConfigResolver.java
@@ -1,11 +1,15 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.artifacts.kjerneinfo.components.mockable;
 
+import no.nav.brukerprofil.consumer.BrukerprofilServiceBi;
 import no.nav.kjerneinfo.consumer.config.KjerneinfoSecurityPolicyConfig;
 import no.nav.kjerneinfo.consumer.fim.mapping.KjerneinfoMapper;
 import no.nav.kjerneinfo.consumer.fim.person.PersonKjerneinfoServiceBi;
 import no.nav.kjerneinfo.consumer.fim.person.config.PersonKjerneinfoConsumerConfig;
+import no.nav.kjerneinfo.consumer.fim.person.mock.PersonKjerneinfoServiceMock;
 import no.nav.kjerneinfo.consumer.fim.person.support.DefaultPersonKjerneinfoService;
 import no.nav.modig.security.tilgangskontroll.policy.pep.EnforcementPoint;
+import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.artifacts.kjerneinfo.components.mockable.mockableimpl.PersonKjerneinfoConsumerConfigImpl;
+import no.nav.sbl.dialogarena.modiabrukerdialog.mock.config.artifacts.kjerneinfo.PersonKjerneinfoServiceBiMock;
 import no.nav.tjeneste.virksomhet.person.v1.PersonPortType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +18,8 @@ import org.springframework.context.annotation.Import;
 import javax.annotation.Resource;
 import javax.inject.Inject;
 import javax.inject.Named;
+
+import static no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.endpoints.util.InstanceSwitcher.createSwitcher;
 
 @Configuration
 @Import({
@@ -37,9 +43,13 @@ public class PersonKjerneinfoConsumerConfigResolver {
     @Resource(name = "personKjerneinfoPep")
     private EnforcementPoint kjerneinfoPep;
 
+    private PersonKjerneinfoServiceBi defaultBi = new PersonKjerneinfoConsumerConfigImpl(personPortType, selfTestPersonPortType, kjerneinfoMapperBean, kjerneinfoPep).personKjerneinfoServiceBi();
+    private PersonKjerneinfoServiceBi mockBi =  PersonKjerneinfoServiceBiMock.getPersonKjerneinfoServiceBiMock();
+    private String key = "start.kjerneinfo.withintegration";
+
     @Bean
     public PersonKjerneinfoServiceBi personKjerneinfoServiceBi() {
-        return new DefaultPersonKjerneinfoService(personPortType, selfTestPersonPortType, kjerneinfoMapperBean, kjerneinfoPep);
+        return createSwitcher(defaultBi, mockBi, key, PersonKjerneinfoServiceBi.class);
     }
 
 }
