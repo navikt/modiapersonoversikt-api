0ec7bdf111841c92afbb8784f9126753f01887fb
Rune Flobakk <rune.flobakk@nav.no>
2013-11-05 15:37:49 +0100

    Snakkeboble i indikator for antall meldinger.

--- innboks/src/test/java/no/nav/sbl/dialogarena/sporsmalogsvar/innboks/StartJettyInnboks.java
+++ innboks/src/test/java/no/nav/sbl/dialogarena/sporsmalogsvar/innboks/StartJettyInnboks.java
@@ -5,13 +5,17 @@ import no.nav.sbl.dialogarena.test.SystemProperties;
 
 import java.io.File;
 
+import static no.nav.modig.lang.collections.FactoryUtils.gotKeypress;
+import static no.nav.modig.lang.collections.RunnableUtils.first;
+import static no.nav.modig.lang.collections.RunnableUtils.waitFor;
 import static no.nav.sbl.dialogarena.test.path.FilesAndDirs.TEST_RESOURCES;
 
 public final class StartJettyInnboks {
 
     public static void main(String ... args) {
         SystemProperties.setFrom("jetty-innside.properties");
-        Jetty.usingWar(new File(TEST_RESOURCES, "webapp")).at("innboks").port(8384)
-                .buildJetty().start();
+        Jetty jetty = Jetty.usingWar(new File(TEST_RESOURCES, "webapp")).at("innboks").port(8384)
+                .buildJetty();
+        jetty.startAnd(first(waitFor(gotKeypress())).then(jetty.stop));
     }
 }
