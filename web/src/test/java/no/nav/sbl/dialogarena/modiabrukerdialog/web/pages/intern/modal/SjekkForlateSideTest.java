1b889165d66b7c3e3bbf4d783341da52d023b0df
Håkon Haga <hakon.haga@nav.no>
2013-11-07 10:28:19 +0100

    fikset test context og sette dirty flag etter hver metode

--- web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/pages/intern/modal/SjekkForlateSideTest.java
+++ web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/pages/intern/modal/SjekkForlateSideTest.java
@@ -1,10 +1,20 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.web.pages.intern.modal;
 
+import no.nav.sbl.dialogarena.aktorid.service.AktorService;
+import no.nav.sbl.dialogarena.modiabrukerdialog.mock.config.endpoints.AktorPortTypeMock;
+import no.nav.sbl.dialogarena.modiabrukerdialog.mock.config.endpoints.SakOgBehandlingPortTypeMock;
+import no.nav.sbl.dialogarena.modiabrukerdialog.mock.config.endpoints.UtbetalingPortTypeMock;
+import no.nav.sbl.dialogarena.modiabrukerdialog.web.config.mock.HentPersonPanelMockContext;
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.config.mock.KjerneinfoPepMockContext;
+import no.nav.sbl.dialogarena.modiabrukerdialog.web.config.mock.SykepengerWidgetMockContext;
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.pages.WicketPageTest;
+import no.nav.sbl.dialogarena.modiabrukerdialog.web.pages.intern.Intern;
+import no.nav.sbl.dialogarena.soknader.service.SoknaderService;
+import no.nav.sbl.dialogarena.utbetaling.config.UtbetalingConfig;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -14,9 +24,19 @@ import static no.nav.sbl.dialogarena.modiabrukerdialog.web.pages.intern.modal.Sj
 import static no.nav.sbl.dialogarena.modiabrukerdialog.web.pages.intern.modal.SjekkForlateSideAnswer.AnswerType.DISCARD;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.springframework.test.annotation.DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD;
 
+@DirtiesContext(classMode = AFTER_EACH_TEST_METHOD)
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {KjerneinfoPepMockContext.class})
+@ContextConfiguration(classes = {KjerneinfoPepMockContext.class,
+        HentPersonPanelMockContext.class,
+        SykepengerWidgetMockContext.class,
+        SakOgBehandlingPortTypeMock.class,
+        AktorPortTypeMock.class,
+        SoknaderService.class,
+        AktorService.class,
+        UtbetalingConfig.class,
+        UtbetalingPortTypeMock.class})
 public class SjekkForlateSideTest extends WicketPageTest {
 
     private SjekkForlateSideAnswer answer;
@@ -30,14 +50,14 @@ public class SjekkForlateSideTest extends WicketPageTest {
 
     @Test
     public void skalOppretteSjekkForlateSide() {
-        wicket.goToPageWith(sjekkForlateSide)
+        wicket.goTo(Intern.class).goToPageWith(sjekkForlateSide)
                 .should().containComponent(withId("closeDiscard").and(ofType(AjaxLink.class)))
                 .should().containComponent(withId("closeCancel").and(ofType(AjaxLink.class)));
     }
 
     @Test
     public void skalReturnereCancelAswer() {
-        wicket.goToPageWith(sjekkForlateSide)
+        wicket.goTo(Intern.class).goToPageWith(sjekkForlateSide)
                 .click().link(withId("closeCancel"));
         assertTrue(answer.is(CANCEL));
         assertFalse(answer.is(DISCARD));
@@ -45,7 +65,8 @@ public class SjekkForlateSideTest extends WicketPageTest {
 
     @Test
     public void skalReturnereDiscardAswer() {
-        wicket.goToPageWith(sjekkForlateSide)
+        wicket.goTo(Intern.class)
+                .goToPageWith(sjekkForlateSide)
                 .click().link(withId("closeDiscard"));
         assertTrue(answer.is(DISCARD));
         assertFalse(answer.is(CANCEL));
