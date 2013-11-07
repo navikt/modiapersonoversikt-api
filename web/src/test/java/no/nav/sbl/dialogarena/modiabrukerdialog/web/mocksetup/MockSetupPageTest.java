ed84135a4c6af66c9bb0f922c1f6ca721b0a7990
Håkon Haga <hakon.haga@nav.no>
2013-11-07 11:05:55 +0100

    la på flere tester og dirty flag

--- web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/mocksetup/MockSetupPageTest.java
+++ web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/mocksetup/MockSetupPageTest.java
@@ -1,20 +1,51 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.web.mocksetup;
 
+import no.nav.sbl.dialogarena.aktorid.service.AktorService;
+import no.nav.sbl.dialogarena.modiabrukerdialog.mock.config.endpoints.AktorPortTypeMock;
+import no.nav.sbl.dialogarena.modiabrukerdialog.mock.config.endpoints.SakOgBehandlingPortTypeMock;
+import no.nav.sbl.dialogarena.modiabrukerdialog.mock.config.endpoints.UtbetalingPortTypeMock;
+import no.nav.sbl.dialogarena.modiabrukerdialog.web.config.mock.HentPersonPanelMockContext;
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.config.mock.KjerneinfoPepMockContext;
+import no.nav.sbl.dialogarena.modiabrukerdialog.web.config.mock.SykepengerWidgetMockContext;
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.pages.WicketPageTest;
+import no.nav.sbl.dialogarena.modiabrukerdialog.web.pages.hentperson.HentPersonPage;
+import no.nav.sbl.dialogarena.modiabrukerdialog.web.pages.intern.Intern;
+import no.nav.sbl.dialogarena.soknader.service.SoknaderService;
+import no.nav.sbl.dialogarena.utbetaling.config.UtbetalingConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static no.nav.modig.wicket.test.matcher.ComponentMatchers.withId;
+import static org.springframework.test.annotation.DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD;
 
-@ContextConfiguration(classes = {KjerneinfoPepMockContext.class})
+@DirtiesContext(classMode = AFTER_EACH_TEST_METHOD)
+@ContextConfiguration(classes = {KjerneinfoPepMockContext.class,
+        HentPersonPanelMockContext.class,
+        SykepengerWidgetMockContext.class,
+        SakOgBehandlingPortTypeMock.class,
+        AktorPortTypeMock.class,
+        SoknaderService.class,
+        AktorService.class,
+        UtbetalingConfig.class,
+        UtbetalingPortTypeMock.class})
 @RunWith(SpringJUnit4ClassRunner.class)
 public class MockSetupPageTest extends WicketPageTest {
 
     @Test
     public void shouldRenderMockSetupPage() {
         wicket.goTo(MockSetupPage.class).should().containComponent(withId("velgMockForm"));
+    }
+
+    @Test
+    public void shouldGoToHentPersonPageWhenSubmitMockSetup() {
+        wicket.goTo(MockSetupPage.class).inForm("velgMockForm").submit().should().beOn(HentPersonPage.class);
+    }
+
+    @Test
+    public void shouldGoToInternWhenSubmitMockSetupWithTestPerson() {
+        wicket.goTo(MockSetupPage.class).inForm("velgMockForm").check("brukTestPerson", true).submit().should().beOn(Intern.class);
     }
 }
