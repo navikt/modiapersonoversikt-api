2fcbd88a5e843a87e4a3f89a05d6a2bcce53af58
Henrik Hellerøy <henrik.helleroy@nav.no>
2013-11-21 13:25:14 +0100

    Fjernet LocaleFromWicketSession fra web-test, til fordel for inlining.

--- web/src/test/java/no/nav/sbl/dialogarena/utbetaling/UtbetalingApplication.java
+++ web/src/test/java/no/nav/sbl/dialogarena/utbetaling/UtbetalingApplication.java
@@ -7,12 +7,15 @@ import no.nav.modig.wicket.configuration.ApplicationSettingsConfig;
 import no.nav.sbl.dialogarena.time.Datoformat;
 import no.nav.sbl.dialogarena.utbetaling.lamell.UtbetalingLamell;
 import no.nav.sbl.dialogarena.utbetaling.widget.UtbetalingWidget;
+import org.apache.commons.collections15.Factory;
 import org.apache.wicket.Page;
+import org.apache.wicket.Session;
 import org.apache.wicket.protocol.http.WebApplication;
 import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
 import org.springframework.context.ApplicationContext;
 
 import javax.inject.Inject;
+import java.util.Locale;
 
 public class UtbetalingApplication extends WebApplication {
 
@@ -33,7 +36,12 @@ public class UtbetalingApplication extends WebApplication {
         setupSpringInjector();
         getMarkupSettings().setStripWicketTags(true);
 
-        Datoformat.brukLocaleFra(LocaleFromWicketSession.INSTANCE);
+        Datoformat.brukLocaleFra(new Factory<Locale>() {
+            @Override
+            public Locale create() {
+                return Session.get().getLocale();
+            }
+        });
 
         new ApplicationSettingsConfig().withUtf8Properties(true).configure(this);
         new FrontendConfigurator()
