6e4e539259528ef182d9c50cf46b9af9b8e339c8
Frithjof Nerdrum <frithjof.nerdrum@nav.no>
2013-11-25 09:40:12 +0100

    MOD-4940: Mulighet til å ekspandere visning av utbetaling.

--- lamell/src/main/java/no/nav/sbl/dialogarena/utbetaling/lamell/UtbetalingPanel.java
+++ lamell/src/main/java/no/nav/sbl/dialogarena/utbetaling/lamell/UtbetalingPanel.java
@@ -1,12 +1,18 @@
 package no.nav.sbl.dialogarena.utbetaling.lamell;
 
+import java.text.NumberFormat;
+import java.util.List;
+import no.nav.sbl.dialogarena.utbetaling.domain.Bilag;
+import no.nav.sbl.dialogarena.utbetaling.domain.PosteringsDetalj;
 import no.nav.sbl.dialogarena.utbetaling.domain.Utbetaling;
+import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Panel;
-
-import java.text.NumberFormat;
 
 import static java.text.NumberFormat.getNumberInstance;
 import static java.util.Locale.forLanguageTag;
@@ -17,10 +23,15 @@ public class UtbetalingPanel extends Panel {
 
     public UtbetalingPanel(String id, Utbetaling utbetaling) {
         super(id);
-        final Label hidden = createHiddenLabel();
+        ListView<Bilag> bilagListView = createBilagListView(utbetaling.getBilag());
+        // En ListView kan ikke addes direkte via Ajax. Den må derfor legges i en bilagWrapper.
+        WebMarkupContainer bilagWrapper = new WebMarkupContainer("bilag-bilagWrapper");
+        bilagWrapper.add(bilagListView);
+        bilagWrapper.setOutputMarkupPlaceholderTag(true);
+        bilagWrapper.setVisibilityAllowed(false);
 
         add(
-                hidden,
+                bilagWrapper,
                 new Label("beskrivelse", utbetaling.getBeskrivelse()),
                 createUtbetalingDatoLabel(utbetaling),
                 createBelopLabel(utbetaling),
@@ -28,7 +39,7 @@ public class UtbetalingPanel extends Panel {
                 new Label("status", utbetaling.getStatuskode()),
                 new Label("kontonr", utbetaling.getKontoNr()),
                 new Label("mottaker", utbetaling.getMottaker().getNavn()),
-                createExpandButton(hidden)
+                createExpandButton(bilagWrapper)
         );
     }
 
@@ -36,18 +47,33 @@ public class UtbetalingPanel extends Panel {
         return new Label("utbetalingdato", optional(utbetaling.getUtbetalingsDato()).map(KORT).getOrElse("Ingen utbetalingsdato"));
     }
 
-    private Label createHiddenLabel() {
-        return (Label) new Label("hidden", "SKJULT")
-                .setVisible(false)
-                .setOutputMarkupId(true)
-                .setOutputMarkupPlaceholderTag(true);
+    private ListView<Bilag> createBilagListView(List<Bilag> bilagListe) {
+        return new ListView<Bilag>("bilag-liste", bilagListe) {
+            @Override
+            protected void populateItem(ListItem<Bilag> item) {
+                Bilag bilag = item.getModelObject();
+                item.add(new Label("melding", bilag.getMelding()));
+                item.add(createPosteringsDetaljListView(bilag.getPosteringsDetaljer()));
+            }
+        };
     }
 
-    private AjaxLink<Void> createExpandButton(final Label hidden) {
+    private ListView<PosteringsDetalj> createPosteringsDetaljListView(List<PosteringsDetalj> posteringsDetaljListe) {
+        return new ListView<PosteringsDetalj>("posteringsdetalj-liste", posteringsDetaljListe) {
+            @Override
+            protected void populateItem(ListItem<PosteringsDetalj> item) {
+                PosteringsDetalj posteringsDetalj = item.getModelObject();
+                item.add(new Label("hovedbeskrivelse", posteringsDetalj.getHovedBeskrivelse()));
+                item.add(new Label("kontonr", posteringsDetalj.getKontoNr()));
+            }
+        };
+    }
+
+    private AjaxLink<Void> createExpandButton(final Component hidden) {
         return new AjaxLink<Void>("expandbutton") {
             @Override
             public void onClick(AjaxRequestTarget target) {
-                hidden.setVisible(!hidden.isVisible());
+                hidden.setVisibilityAllowed(!hidden.isVisibleInHierarchy());
                 target.add(hidden);
             }
         };
