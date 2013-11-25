e51cff3c686867b26e807def13a68637af0bc960
Henrik Hellerøy <henrik.helleroy@nav.no>
2013-11-25 10:02:16 +0100

    Statiske felter for mottakerkoder i Filter.

--- lamell/src/main/java/no/nav/sbl/dialogarena/utbetaling/lamell/filter/Filter.java
+++ lamell/src/main/java/no/nav/sbl/dialogarena/utbetaling/lamell/filter/Filter.java
@@ -10,10 +10,13 @@ public class Filter implements Serializable {
 
     public static final String ENDRET = "filter.endret";
 
+    private static final String MOTTAKERKODE_ARBEIDSGIVER = "arbeidsgiver";
+    private static final String MOTTAKERKODE_BRUKER = "bruker";
+
     private IModel<LocalDate> startDato;
     private IModel<LocalDate> sluttDato;
-    private Boolean visBruker = true;
-    private Boolean visArbeidsgiver = true;
+    private Boolean visBruker;
+    private Boolean visArbeidsgiver;
 
     public Filter(LocalDate startDato, LocalDate sluttDato, Boolean brukerCheckbox, Boolean arbeidsgiverCheckbox) {
         this.visBruker = brukerCheckbox;
@@ -36,8 +39,8 @@ public class Filter implements Serializable {
     }
 
     public boolean filtrerPaaMottaker(String mottakerkode) {
-        boolean visArbeidsgiver = this.visArbeidsgiver && "arbeidsgiver".equalsIgnoreCase(mottakerkode);
-        boolean visBruker = this.visBruker && "bruker".equalsIgnoreCase(mottakerkode);
+        boolean visArbeidsgiver = this.visArbeidsgiver && MOTTAKERKODE_ARBEIDSGIVER.equalsIgnoreCase(mottakerkode);
+        boolean visBruker = this.visBruker && MOTTAKERKODE_BRUKER.equalsIgnoreCase(mottakerkode);
         return visArbeidsgiver || visBruker;
     }
 
