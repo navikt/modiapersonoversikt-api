0e81b4b1062050d38ed6a3fb935396341c8db692
Henrik Hellerøy <henrik.helleroy@nav.no>
2013-11-25 12:32:31 +0100

    Statisk felt for minimumsdato i FilterForm.

--- lamell/src/main/java/no/nav/sbl/dialogarena/utbetaling/lamell/filter/FilterForm.java
+++ lamell/src/main/java/no/nav/sbl/dialogarena/utbetaling/lamell/filter/FilterForm.java
@@ -8,7 +8,6 @@ import org.apache.wicket.ajax.form.AjaxFormSubmitBehavior;
 import org.apache.wicket.ajax.markup.html.form.AjaxCheckBox;
 import org.apache.wicket.event.Broadcast;
 import org.apache.wicket.markup.html.form.Form;
-import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.model.PropertyModel;
 import org.joda.time.LocalDate;
 
@@ -16,20 +15,19 @@ import static no.nav.modig.wicket.component.datepicker.DatePickerConfigurator.Da
 
 public class FilterForm extends Form<Void> {
 
-    private static final LocalDate MIN_DATO =  LocalDate.now().minusYears(150);
-    private static final LocalDate MAKS_DATO = LocalDate.now();
+    private static final int AAR_TILBAKE = 100;
 
-    public FilterForm(String id, Filter filter, final FeedbackPanel feedbackpanel) {
+    public FilterForm(String id, Filter filter) {
         super(id);
 
-        add(createMottakerButton("visBruker", filter, feedbackpanel));
-        add(createMottakerButton("visArbeidsgiver", filter, feedbackpanel));
+        add(createMottakerButton("visBruker", filter));
+        add(createMottakerButton("visArbeidsgiver", filter));
 
         add(createDateRangePicker(filter));
         add(createAjaxFormSubmitBehaviour());
     }
 
-    private AjaxCheckBox createMottakerButton(final String mottaker, Filter filter, final FeedbackPanel feedbackpanel) {
+    private AjaxCheckBox createMottakerButton(final String mottaker, Filter filter) {
         return new AjaxCheckBox(mottaker, new PropertyModel<Boolean>(filter, mottaker)) {
             @Override
             protected void onUpdate(AjaxRequestTarget target) {
@@ -38,10 +36,16 @@ public class FilterForm extends Form<Void> {
     }
 
     private DateRangePicker createDateRangePicker(Filter filter) {
-        DatePickerConfigurator datePickerConfigurator = datePickerConfigurator().withMaxDate("0d").build();
+        DatePickerConfigurator datePickerConfigurator = datePickerConfigurator()
+                .withMinDate("-" + AAR_TILBAKE + "y")
+                .withMaxDate("0d")
+                .withYearRange("-" + AAR_TILBAKE + "y:c")
+                .build();
         DateRangeModel dateRangeModel = new DateRangeModel(filter.getStartDato(), filter.getSluttDato());
+        LocalDate minDato = LocalDate.now().minusYears(AAR_TILBAKE);
+        LocalDate maksDato = LocalDate.now();
 
-        return new DateRangePicker("datoFilter", dateRangeModel, datePickerConfigurator, MIN_DATO, MAKS_DATO);
+        return new DateRangePicker("datoFilter", dateRangeModel, datePickerConfigurator, minDato, maksDato);
     }
 
     private AjaxFormSubmitBehavior createAjaxFormSubmitBehaviour() {
