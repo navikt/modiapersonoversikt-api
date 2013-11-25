dd919ab408c6f5b0e3eb6e5a65b1274fe917d1a1
Jarle Svendsrud <jarle.svendsrud@nav.no>
2013-11-25 12:26:07 +0100

    Fikset stuboppsett for jetty

--- web/src/test/java/no/nav/sbl/dialogarena/utbetaling/context/JettyApplicationContext.java
+++ web/src/test/java/no/nav/sbl/dialogarena/utbetaling/context/JettyApplicationContext.java
@@ -2,14 +2,14 @@ package no.nav.sbl.dialogarena.utbetaling.context;
 
 import no.nav.sbl.dialogarena.utbetaling.UtbetalingApplication;
 import no.nav.sbl.dialogarena.utbetaling.config.UtbetalingConfig;
-import no.nav.sbl.dialogarena.utbetaling.config.UtbetalingPortTypeStubConfig;
+import no.nav.sbl.dialogarena.utbetaling.config.UtbetalingStubConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
-@Import({UtbetalingConfig.class, UtbetalingPortTypeStubConfig.class})
+@Import({UtbetalingConfig.class, UtbetalingStubConfig.class})
 public class JettyApplicationContext {
 
     @Bean
