5b38af2a0f45209d62c55030c598ccd33aa02f8a
Jarle Svendsrud <jarle.svendsrud@nav.no>
2013-11-25 12:26:07 +0100

    Fikset stuboppsett for jetty

---
+++ consumer/src/test/java/no/nav/sbl/dialogarena/utbetaling/config/UtbetalingStubConfig.java
@@ -0,0 +1,30 @@
+package no.nav.sbl.dialogarena.utbetaling.config;
+
+import no.nav.virksomhet.tjenester.utbetaling.meldinger.v2.WSHentUtbetalingListeRequest;
+import no.nav.virksomhet.tjenester.utbetaling.meldinger.v2.WSHentUtbetalingListeResponse;
+import no.nav.virksomhet.tjenester.utbetaling.v2.HentUtbetalingListeBaksystemIkkeTilgjengelig;
+import no.nav.virksomhet.tjenester.utbetaling.v2.HentUtbetalingListeForMangeForekomster;
+import no.nav.virksomhet.tjenester.utbetaling.v2.HentUtbetalingListeMottakerIkkeFunnet;
+import no.nav.virksomhet.tjenester.utbetaling.v2.HentUtbetalingListeUgyldigDato;
+import no.nav.virksomhet.tjenester.utbetaling.v2.UtbetalingPortType;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.jws.WebParam;
+
+import static no.nav.sbl.dialogarena.utbetaling.domain.testdata.WSUtbetalingTestData.getWsUtbetalinger;
+
+@Configuration
+public class UtbetalingStubConfig {
+
+    @Bean
+    public no.nav.virksomhet.tjenester.utbetaling.v2.Utbetaling utbetaling() {
+        return new no.nav.virksomhet.tjenester.utbetaling.v2.Utbetaling() {
+            @Override
+            public WSHentUtbetalingListeResponse hentUtbetalingListe(@WebParam(name = "request", targetNamespace = "") WSHentUtbetalingListeRequest request) throws HentUtbetalingListeMottakerIkkeFunnet, HentUtbetalingListeForMangeForekomster, HentUtbetalingListeBaksystemIkkeTilgjengelig, HentUtbetalingListeUgyldigDato {
+                return new WSHentUtbetalingListeResponse().withUtbetalingListe(getWsUtbetalinger());
+            }
+        };
+    }
+
+}
