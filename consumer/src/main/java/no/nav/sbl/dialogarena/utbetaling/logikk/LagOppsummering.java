91f9d5a760d6669285356252733b4b2709713ec4
Kari B. Schjønsby <Kari.Bergh.Schjonsby@nav.no>
2013-11-27 16:17:56 +0100

    Flyttet finnUtbetalingerIPeriode til UtbetalingsDatakilde

--- consumer/src/main/java/no/nav/sbl/dialogarena/utbetaling/logikk/LagOppsummering.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/utbetaling/logikk/LagOppsummering.java
@@ -4,13 +4,14 @@ package no.nav.sbl.dialogarena.utbetaling.logikk;
 import no.nav.sbl.dialogarena.utbetaling.domain.Oppsummering;
 import no.nav.sbl.dialogarena.utbetaling.domain.Periode;
 
-import static no.nav.sbl.dialogarena.utbetaling.logikk.OppsummeringsKalkulator.finnUtbetalingerIPeriode;
 import static no.nav.sbl.dialogarena.utbetaling.logikk.OppsummeringsKalkulator.regnUtOppsummering;
-import static no.nav.sbl.dialogarena.utbetaling.service.UtbetalingsDatakilde.get;
+import static no.nav.sbl.dialogarena.utbetaling.service.UtbetalingsDatakilde.finnUtbetalingerIPeriode;
+import static no.nav.sbl.dialogarena.utbetaling.service.UtbetalingsDatakilde.getKilde;
+
 
 public class LagOppsummering {
 
     public Oppsummering lagOppsummering(Periode periode) {
-        return regnUtOppsummering(finnUtbetalingerIPeriode(get().getUtbetalinger(), periode));
+        return regnUtOppsummering(finnUtbetalingerIPeriode(getKilde().getUtbetalinger(), periode));
     }
 }
--- consumer/src/main/java/no/nav/sbl/dialogarena/utbetaling/logikk/OppsummeringsKalkulator.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/utbetaling/logikk/OppsummeringsKalkulator.java
@@ -2,10 +2,8 @@ package no.nav.sbl.dialogarena.utbetaling.logikk;
 
 
 import no.nav.sbl.dialogarena.utbetaling.domain.Oppsummering;
-import no.nav.sbl.dialogarena.utbetaling.domain.Periode;
 import no.nav.sbl.dialogarena.utbetaling.domain.Utbetaling;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class OppsummeringsKalkulator {
@@ -18,17 +16,5 @@ public class OppsummeringsKalkulator {
             oppsummering.trekk += utbetaling.getBruttoBelop() - utbetaling.getNettoBelop();
         }
         return oppsummering;
-    }
-
-
-    public static List<Utbetaling> finnUtbetalingerIPeriode(List<Utbetaling> utbetalinger, Periode periode) {
-        List<Utbetaling> resultat = new ArrayList<>();
-        for (Utbetaling utbetaling : utbetalinger) {
-            if(periode.containsDate(utbetaling.getUtbetalingsDato())) {
-                resultat.add(utbetaling);
-            }
-        }
-
-        return resultat;
     }
 }
