2f1395a408e9420b286bf914de3566ea82b7c030
Henrik Hellerøy <henrik.helleroy@nav.no>
2013-11-27 17:34:52 +0100

    Styling av FilterForm etter testing i Modia.

--- lamell/src/main/java/no/nav/sbl/dialogarena/utbetaling/lamell/filter/FilterForm.java
+++ lamell/src/main/java/no/nav/sbl/dialogarena/utbetaling/lamell/filter/FilterForm.java
@@ -64,7 +64,7 @@ public class FilterForm extends Form {
     private AjaxFormSubmitBehavior createDateRangePickerChangeBehaviour() {
         return new AjaxFormSubmitBehavior("onchange") {
             @Override
-            protected void onSubmit(AjaxRequestTarget target) {
+            protected void onEvent(AjaxRequestTarget target) {
                 sendFilterEndretEvent();
             }
         };
