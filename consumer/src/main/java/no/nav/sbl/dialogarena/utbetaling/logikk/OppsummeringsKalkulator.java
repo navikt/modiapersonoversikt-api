9db01818e1b908ba5cbaf634a2f5410accb1b83d
Kari B. Schjønsby <Kari.Bergh.Schjonsby@nav.no>
2013-11-29 12:16:56 +0100

    Flyttet valutaformatering til egen utilklasse

--- consumer/src/main/java/no/nav/sbl/dialogarena/utbetaling/logikk/OppsummeringsKalkulator.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/utbetaling/logikk/OppsummeringsKalkulator.java
@@ -11,6 +11,7 @@ public class OppsummeringsKalkulator {
     public static Oppsummering regnUtOppsummering(List<Utbetaling> utbetalinger) {
         Oppsummering oppsummering = new Oppsummering();
         for (Utbetaling utbetaling : utbetalinger) {
+            oppsummering.valuta = utbetaling.getValuta();
             oppsummering.utbetalt += utbetaling.getNettoBelop();
             oppsummering.brutto += utbetaling.getBruttoBelop();
             oppsummering.trekk += utbetaling.getTrekk() == 0.0 ?
