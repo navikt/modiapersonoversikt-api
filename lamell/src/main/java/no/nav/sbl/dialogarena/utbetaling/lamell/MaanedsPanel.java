31fa077cb19a822f30fa240d718e1007b9d0f5df
Kari B. Schjønsby <Kari.Bergh.Schjonsby@nav.no>
2013-12-02 15:45:48 +0100

    Fikset formatering av oppsummering-periodestring. Flyttet modell-opprettelsen til MaanedsPanel.

--- lamell/src/main/java/no/nav/sbl/dialogarena/utbetaling/lamell/MaanedsPanel.java
+++ lamell/src/main/java/no/nav/sbl/dialogarena/utbetaling/lamell/MaanedsPanel.java
@@ -5,16 +5,23 @@ import no.nav.sbl.dialogarena.utbetaling.lamell.filter.OppsummeringProperties;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
+import org.joda.time.LocalDate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static no.nav.modig.wicket.conditional.ConditionalUtils.visibleIf;
 
 
 public class MaanedsPanel extends Panel {
 
-    public MaanedsPanel(String id, IModel<OppsummeringProperties> oppsummeringsModel) {
-        super(id, oppsummeringsModel);
+    public MaanedsPanel(String maanedsPanel, List<Utbetaling> utbetalingListe) {
+        super(maanedsPanel);
+
+        CompoundPropertyModel<OppsummeringProperties> oppsummeringsModel = createOppsummeringPropertiesModel(utbetalingListe);
 
         OppsummeringPanel oppsummeringsPanel = new OppsummeringPanel("oppsummeringsPanel", oppsummeringsModel);
         oppsummeringsPanel.add(visibleIf(new Model<>(oppsummeringsModel.getObject().getUtbetalinger().size() > 1)));
@@ -23,6 +30,7 @@ public class MaanedsPanel extends Panel {
                 oppsummeringsPanel,
                 createUtbetalingListView(oppsummeringsModel)
         );
+
     }
 
     private ListView<Utbetaling> createUtbetalingListView(IModel<OppsummeringProperties> oppsummeringsModel) {
@@ -32,6 +40,15 @@ public class MaanedsPanel extends Panel {
                 item.add(new UtbetalingPanel("utbetaling", item.getModelObject()));
             }
         };
+    }
+
+    private CompoundPropertyModel<OppsummeringProperties> createOppsummeringPropertiesModel(List<Utbetaling> liste) {
+        if (liste.isEmpty()) {
+            return new CompoundPropertyModel<>(new OppsummeringProperties(new ArrayList<Utbetaling>(), LocalDate.now(), LocalDate.now()));
+        }
+        LocalDate startDato = liste.get(liste.size() - 1).getUtbetalingsDato().dayOfMonth().withMinimumValue().toLocalDate();
+        LocalDate sluttDato = liste.get(0).getUtbetalingsDato().dayOfMonth().withMaximumValue().toLocalDate();
+        return new CompoundPropertyModel<>(new OppsummeringProperties(liste, startDato, sluttDato));
     }
 
 }
--- lamell/src/main/java/no/nav/sbl/dialogarena/utbetaling/lamell/filter/OppsummeringProperties.java
+++ lamell/src/main/java/no/nav/sbl/dialogarena/utbetaling/lamell/filter/OppsummeringProperties.java
@@ -1,10 +1,13 @@
 package no.nav.sbl.dialogarena.utbetaling.lamell.filter;
 
 
+import no.nav.sbl.dialogarena.time.Datoformat;
 import no.nav.sbl.dialogarena.utbetaling.domain.Oppsummering;
 import no.nav.sbl.dialogarena.utbetaling.domain.Periode;
 import no.nav.sbl.dialogarena.utbetaling.domain.Utbetaling;
 import no.nav.sbl.dialogarena.utbetaling.logikk.OppsummeringsKalkulator;
+import org.apache.commons.collections15.Transformer;
+import org.joda.time.DateTime;
 import org.joda.time.LocalDate;
 
 import java.io.Serializable;
@@ -17,15 +20,21 @@ public class OppsummeringProperties implements Serializable {
 
     private LocalDate sluttDato;
     private LocalDate startDato;
-    private Periode oppsummertPeriode;
-
+    private Periode periode;
+    private String oppsummertPeriode;
     private Oppsummering oppsummering;
 
     public OppsummeringProperties(List<Utbetaling> utbetalinger, LocalDate startDato, LocalDate sluttDato) {
+        init(utbetalinger, startDato, sluttDato);
+    }
+
+
+
+    private void init(List<Utbetaling> utbetalinger, LocalDate startDato, LocalDate sluttDato) {
         this.utbetalinger = utbetalinger;
         this.sluttDato = sluttDato;
         this.startDato = startDato;
-        oppsummertPeriode = createPeriode(startDato, sluttDato);
+        periode = createPeriode(startDato, sluttDato);
         oppsummering = regnUtOppsummering();
     }
 
@@ -53,7 +62,7 @@ public class OppsummeringProperties implements Serializable {
         return startDato;
     }
 
-    public Periode getOppsummertPeriode() {
-        return oppsummertPeriode;
+    public String getOppsummertPeriode() {
+        return periode.getPeriodeString(Datoformat.KORT);
     }
 }
