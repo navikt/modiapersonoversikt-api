69e9cad76d5a58eed7c445f77edb22aa67fa3ae5
Henrik Hellerøy <henrik.helleroy@nav.no>
2013-12-03 15:24:18 +0100

    Omdøpte UtbetalingLamell til UtbetalingLerret.

--- web/src/test/java/no/nav/sbl/dialogarena/utbetaling/UtbetalingApplication.java
+++ web/src/test/java/no/nav/sbl/dialogarena/utbetaling/UtbetalingApplication.java
@@ -7,7 +7,7 @@ import no.nav.modig.modia.shortcuts.ShortcutListenerResourceReference;
 import no.nav.modig.modia.widget.Widget;
 import no.nav.modig.wicket.configuration.ApplicationSettingsConfig;
 import no.nav.sbl.dialogarena.time.Datoformat;
-import no.nav.sbl.dialogarena.utbetaling.lamell.UtbetalingLamell;
+import no.nav.sbl.dialogarena.utbetaling.lamell.UtbetalingLerret;
 import no.nav.sbl.dialogarena.utbetaling.widget.UtbetalingWidget;
 import org.apache.commons.collections15.Factory;
 import org.apache.wicket.Page;
@@ -56,7 +56,7 @@ public class UtbetalingApplication extends WebApplication {
                                 .attribute("content", "IE=edge,chrome=1")
                                 .done())
                 .withResourcePacking(this.usesDeploymentConfig())
-                .addLess(UtbetalingLamell.UTBETALING_LAMELL_LESS, UtbetalingWidget.UTBETALING_WIDGET_LESS)
+                .addLess(UtbetalingLerret.UTBETALING_LAMELL_LESS, UtbetalingWidget.UTBETALING_WIDGET_LESS)
                 .addScripts(Widget.JS_RESOURCE,
                         ShortcutListenerResourceReference.get())
                 .configure(this);
