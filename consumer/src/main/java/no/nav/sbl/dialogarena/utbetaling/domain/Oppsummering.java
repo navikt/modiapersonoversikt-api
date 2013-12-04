8390e5fad62cd1d6ea986984be56a0a7ea878c9c
Kari B. Schjønsby <Kari.Bergh.Schjonsby@nav.no>
2013-12-04 21:55:23 +0100

    La til oppsummerte ytelser i UI

--- consumer/src/main/java/no/nav/sbl/dialogarena/utbetaling/domain/Oppsummering.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/utbetaling/domain/Oppsummering.java
@@ -2,6 +2,8 @@ package no.nav.sbl.dialogarena.utbetaling.domain;
 
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import static no.nav.sbl.dialogarena.utbetaling.domain.util.ValutaUtil.getBelopString;
@@ -14,6 +16,7 @@ public class Oppsummering implements Serializable {
     public String valuta = "kr";
     public Map<String, Map<String, Double>> ytelserUtbetalt;
 
+    private List<HovedBeskrivelse> hovedYtelsesBeskrivelser;
 
     public String getUtbetalt() {
         return getBelopString(this.utbetalt, this.valuta);
@@ -25,5 +28,55 @@ public class Oppsummering implements Serializable {
 
     public String getBrutto() {
         return getBelopString(this.brutto, this.valuta);
+    }
+
+    public List<HovedBeskrivelse> getHovedYtelsesBeskrivelser() {
+        if(hovedYtelsesBeskrivelser == null) {
+            hovedYtelsesBeskrivelser = new ArrayList<>();
+            for (Map.Entry<String, Map<String, Double>> entry : ytelserUtbetalt.entrySet()) {
+                hovedYtelsesBeskrivelser.add(new Oppsummering.HovedBeskrivelse(entry, this.valuta));
+            }
+        }
+        return this.hovedYtelsesBeskrivelser;
+    }
+
+    public class HovedBeskrivelse implements Serializable {
+        private String hovedYtelsesBeskrivelse;
+        private List<UnderBeskrivelse> underYtelsesBeskrivelser;
+
+        HovedBeskrivelse(Map.Entry<String, Map<String, Double>> ytelseUtbetalt, String valuta) {
+            hovedYtelsesBeskrivelse = ytelseUtbetalt.getKey();
+            underYtelsesBeskrivelser = new ArrayList<>();
+            for (Map.Entry<String, Double> indreEntry : ytelseUtbetalt.getValue().entrySet()) {
+                underYtelsesBeskrivelser.add(new UnderBeskrivelse(indreEntry, valuta));
+            }
+        }
+
+        public String getHovedYtelsesBeskrivelse() {
+            return hovedYtelsesBeskrivelse;
+        }
+
+        public List<UnderBeskrivelse> getUnderYtelsesBeskrivelser() {
+            return underYtelsesBeskrivelser;
+        }
+    }
+
+    public class UnderBeskrivelse implements  Serializable {
+        private String underYtelsesBeskrivelse;
+        private Double ytelsesBelop;
+        private String valuta;
+
+        UnderBeskrivelse(Map.Entry<String, Double> indreEntry, String valuta) {
+            this.valuta = valuta;
+            underYtelsesBeskrivelse = indreEntry.getKey();
+            ytelsesBelop = indreEntry.getValue();
+        }
+        public String getYtelsesBelop() {
+            return getBelopString(ytelsesBelop, this.valuta);
+        }
+
+        public String getUnderYtelsesBeskrivelse() {
+            return underYtelsesBeskrivelse;
+        }
     }
 }
--- lamell/src/main/java/no/nav/sbl/dialogarena/utbetaling/lamell/oppsummering/OppsummeringPanel.java
+++ lamell/src/main/java/no/nav/sbl/dialogarena/utbetaling/lamell/oppsummering/OppsummeringPanel.java
@@ -1,6 +1,9 @@
 package no.nav.sbl.dialogarena.utbetaling.lamell.oppsummering;
 
+import no.nav.sbl.dialogarena.utbetaling.domain.Oppsummering;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
 
@@ -13,7 +16,29 @@ public class OppsummeringPanel extends Panel {
                 new Label("oppsummertPeriode"),
                 new Label("oppsummering.utbetalt"),
                 new Label("oppsummering.trekk"),
-                new Label("oppsummering.brutto")
+                new Label("oppsummering.brutto"),
+                createYtelsesOppsummering()
         );
     }
+
+    private ListView<Oppsummering.HovedBeskrivelse> createYtelsesOppsummering() {
+        ListView<Oppsummering.HovedBeskrivelse> listView = new ListView<Oppsummering.HovedBeskrivelse>("oppsummering.hovedYtelsesBeskrivelser") {
+            @Override
+            protected void populateItem(ListItem<Oppsummering.HovedBeskrivelse> item) {
+                ListView<Oppsummering.UnderBeskrivelse> underBeskrivelseListView = new ListView<Oppsummering.UnderBeskrivelse>("underYtelsesBeskrivelser", item.getModelObject().getUnderYtelsesBeskrivelser()) {
+                    @Override
+                    protected void populateItem(ListItem<Oppsummering.UnderBeskrivelse> item) {
+                        item.add(
+                                new Label("underYtelsesBeskrivelse", item.getModelObject().getUnderYtelsesBeskrivelse()),
+                                new Label("ytelsesBelop", item.getModelObject().getYtelsesBelop())
+                        );
+                    }
+                };
+                item.add(new Label("hovedYtelsesBeskrivelse", item.getModelObject().getHovedYtelsesBeskrivelse()));
+                item.add(underBeskrivelseListView);
+            }
+        };
+        return listView;
+    }
+
 }
