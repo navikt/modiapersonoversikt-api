ef3912c87543239070ab2160f9ad043153de1fe8
Borge Bardo <borge.bardo@nav.no>
2013-12-05 14:09:03 +0100

    PK-8459 tatt ut widgeten i egen side + justering slik at htmlen er mest mulig lik modiabrukerdialog

--- web/src/test/java/no/nav/sbl/dialogarena/utbetaling/UtbetalingTestPage.java
+++ web/src/test/java/no/nav/sbl/dialogarena/utbetaling/UtbetalingTestPage.java
@@ -1,14 +1,14 @@
 package no.nav.sbl.dialogarena.utbetaling;
 
 import no.nav.sbl.dialogarena.utbetaling.lamell.UtbetalingLerret;
-import no.nav.sbl.dialogarena.utbetaling.widget.UtbetalingWidget;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 
 public class UtbetalingTestPage extends WebPage {
 
     public UtbetalingTestPage() {
         String fnr = "12345612345";
-        add(new UtbetalingWidget("utbetalingWidget", "U", fnr));
+        add(new BookmarkablePageLink<WebPage>("widget-link", UtbetalingwidgetTestPage.class));
         add(new UtbetalingLerret("utbetaling", fnr));
     }
 
