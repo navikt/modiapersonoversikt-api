ccd0cd88a634d4ff292e71afb086ef0fb750da37
Jarle Svendsrud <jarle.svendsrud@nav.no>
2013-12-10 09:35:29 +0100

    [kaizen] Mer opprydding i testdata

--- mock/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/mock/config/artifacts/kjerneinfo/YtelseskontraktServiceBiMock.java
+++ mock/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/mock/config/artifacts/kjerneinfo/YtelseskontraktServiceBiMock.java
@@ -17,6 +17,7 @@ import java.util.Date;
 import java.util.List;
 
 import static java.lang.Math.round;
+import static java.math.BigInteger.valueOf;
 import static java.util.Arrays.asList;
 import static no.nav.kjerneinfo.common.mockutils.DateUtils.convertDateToXmlGregorianCalendar;
 import static no.nav.kjerneinfo.common.mockutils.DateUtils.getDate;
@@ -35,100 +36,91 @@ public class YtelseskontraktServiceBiMock {
     }
 
     private static YtelseskontraktResponse lagYtelsesMockRespons() {
-        FimHentYtelseskontraktListeResponse respons = new FimHentYtelseskontraktListeResponse();
-        respons.setBruker(new FimBruker().withRettighetsgruppe(new FimRettighetsgruppe().withRettighetsGruppe("test")));
-        respons.withYtelseskontraktListe(createYtelsesKontrakter());
+        FimHentYtelseskontraktListeResponse respons = new FimHentYtelseskontraktListeResponse()
+                .withBruker(new FimBruker().withRettighetsgruppe(new FimRettighetsgruppe().withRettighetsGruppe("test")))
+                .withYtelseskontraktListe(createYtelsesKontrakter());
         return new YtelseskontraktMapper().map(respons, YtelseskontraktResponse.class);
     }
 
     private static List<FimYtelseskontrakt> createYtelsesKontrakter() {
-        FimVedtak vedtak7 = new FimVedtak();
-        vedtak7.setPeriodetypeForYtelse("Lang periode");
-        vedtak7.setUttaksgrad(new BigInteger("100"));
-        vedtak7.setVedtakBruttoBeloep(new BigInteger("10000"));
-        vedtak7.setVedtakNettoBeloep(new BigInteger("5000"));
+        FimVedtak vedtak7 = new FimVedtak()
+                .withPeriodetypeForYtelse("Lang periode")
+                .withUttaksgrad(new BigInteger("100"))
+                .withVedtakBruttoBeloep(new BigInteger("10000"))
+                .withVedtakNettoBeloep(new BigInteger("5000"));
         FimVedtak vedtak = populateVedtak(vedtak7, "Innvilget", "Annuler sanksjon", getDate(2012, 12, 12), createPeriode(getDate(2012, 1, 1), getDate(2012, 3, 1)));
 
-        FimVedtak vedtak6 = new FimVedtak();
-        vedtak6.setPeriodetypeForYtelse("Kort periode");
-        vedtak6.setUttaksgrad(new BigInteger("75"));
-        vedtak6.setVedtakBruttoBeloep(new BigInteger("2000"));
-        vedtak6.setVedtakNettoBeloep(new BigInteger("1000"));
+        FimVedtak vedtak6 = new FimVedtak()
+                .withPeriodetypeForYtelse("Kort periode")
+                .withUttaksgrad(new BigInteger("75"))
+                .withVedtakBruttoBeloep(new BigInteger("2000"))
+                .withVedtakNettoBeloep(new BigInteger("1000"));
         FimVedtak vedtak2 = populateVedtak(vedtak6, "Avslått", "Forlenget ventetid", getDate(2012, 12, 12), createPeriode(getDate(2012, 2, 1), getDate(2012, 2, 15)));
 
-        FimVedtak vedtak5 = new FimVedtak();
-        vedtak5.setPeriodetypeForYtelse("Kort periode");
-        vedtak5.setUttaksgrad(new BigInteger("75"));
-        vedtak5.setVedtakBruttoBeloep(new BigInteger("2000"));
-        vedtak5.setVedtakNettoBeloep(new BigInteger("1000"));
+        FimVedtak vedtak5 = new FimVedtak()
+                .withPeriodetypeForYtelse("Kort periode")
+                .withUttaksgrad(new BigInteger("75"))
+                .withVedtakBruttoBeloep(new BigInteger("2000"))
+                .withVedtakNettoBeloep(new BigInteger("1000"));
         FimVedtak vedtak3 = populateVedtak(vedtak5, "Avslått", "Tidsbegrenset bortfall", getDate(2012, 12, 12), createPeriode(getDate(2012, 2, 15), getDate(2012, 2, 25)));
 
-        FimVedtak vedtak1 = new FimVedtak();
-        vedtak1.setPeriodetypeForYtelse("Laaang periode");
-        vedtak1.setUttaksgrad(new BigInteger("25"));
-        vedtak1.setVedtakBruttoBeloep(new BigInteger("3000"));
-        vedtak1.setVedtakNettoBeloep(new BigInteger("1500"));
+        FimVedtak vedtak1 = new FimVedtak()
+                .withPeriodetypeForYtelse("Laaang periode")
+                .withUttaksgrad(new BigInteger("25"))
+                .withVedtakBruttoBeloep(new BigInteger("3000"))
+                .withVedtakNettoBeloep(new BigInteger("1500"));
         FimVedtak vedtak4 = populateVedtak(vedtak1, "Avbrutt", "Stans", getDate(2012, 12, 12), createPeriode(getDate(2012, 2, 20), getDate(2012, 2, 22)));
 
         return asList(
-                createDagpengekontrakt(getDate(2012, 12, 10), getDate(2012, 12, 14), getDate(2013, 2, 10), "Aktiv", "Dagpenger", new BigInteger("34"),
-                        vedtak,
-                        vedtak2,
-                        vedtak3,
-                        vedtak4),
+                createDagpengekontrakt(getDate(2012, 12, 10), getDate(2012, 12, 14), getDate(2013, 2, 10), "Aktiv", "Dagpenger", new BigInteger("34"), vedtak, vedtak2, vedtak3, vedtak4),
                 createYtelsesKontrakt(getDate(2012, 12, 14), getDate(2012, 12, 19), getDate(2013, 1, 10), "Lukket", "Arbeidsavklaringspenger", "Avslått", "Endring"),
                 createYtelsesKontrakt(getDate(2012, 12, 18), getDate(2012, 12, 31), getDate(2013, 5, 11), "Inaktiv", "Individstønad", "Avbrutt", "Kontroll"));
     }
 
     private static FimVedtak populateVedtak(FimVedtak vedtak, String status, String type, Date beslutningsDato, FimPeriode periode) {
-        vedtak.setStatus(status);
-        vedtak.setVedtakstype(type);
-        vedtak.setBeslutningsdato(convertDateToXmlGregorianCalendar(beslutningsDato));
-        vedtak.setVedtaksperiode(periode);
-        return vedtak;
+        return vedtak
+                .withStatus(status)
+                .withVedtakstype(type)
+                .withBeslutningsdato(convertDateToXmlGregorianCalendar(beslutningsDato))
+                .withVedtaksperiode(periode);
     }
 
     private static FimPeriode createPeriode(Date fom, Date tom) {
-        FimPeriode periode = new FimPeriode();
-        periode.setFom(convertDateToXmlGregorianCalendar(fom));
-        periode.setTom(convertDateToXmlGregorianCalendar(tom));
-        return periode;
+        return new FimPeriode()
+                .withFom(convertDateToXmlGregorianCalendar(fom))
+                .withTom(convertDateToXmlGregorianCalendar(tom));
     }
 
     private static FimYtelseskontrakt createYtelsesKontrakt(Date datoKravMottat, Date fom, Date tom, String status, String ytelsestype, String vedtaksstatus, String vedtakstype) {
-        FimYtelseskontrakt kontrakt = createYtelsesKontrakt(datoKravMottat, fom, tom, status, ytelsestype);
-        kontrakt.withIhtVedtak(createVedtakWithRandomDates(vedtaksstatus, vedtakstype));
-        return kontrakt;
+        return createYtelsesKontrakt(datoKravMottat, fom, tom, status, ytelsestype)
+                .withIhtVedtak(createVedtakWithRandomDates(vedtaksstatus, vedtakstype));
     }
 
     private static FimYtelseskontrakt createYtelsesKontrakt(Date datoKravMottat, Date fom, Date tom, String status, String ytelsestype) {
-        FimYtelseskontrakt kontrakt = new FimYtelseskontrakt();
-        populateYtelsesKontrakt(kontrakt, datoKravMottat, fom, tom, status, ytelsestype);
-        return kontrakt;
+        return populateYtelsesKontrakt(new FimYtelseskontrakt(), datoKravMottat, fom, tom, status, ytelsestype);
     }
 
     private static FimVedtak createVedtakWithRandomDates(String status, String type) {
-        FimVedtak vedtak = new FimVedtak();
         Date[] datePair = getRandomDatePair();
-        return populateVedtak(vedtak, status, type, getRandomDate(), createPeriode(datePair[0], datePair[1]));
+        return populateVedtak(new FimVedtak(), status, type, getRandomDate(), createPeriode(datePair[0], datePair[1]));
     }
 
     private static FimDagpengekontrakt createDagpengekontrakt(Date datoKravMottat, Date fom, Date tom, String status, String ytelsestype, BigInteger antallDagerIgjen, FimVedtak... vedtak) {
         FimDagpengekontrakt kontrakt = new FimDagpengekontrakt();
         populateYtelsesKontrakt(kontrakt, datoKravMottat, fom, tom, status, ytelsestype);
-        kontrakt.setAntallDagerIgjen(antallDagerIgjen);
-        kontrakt.setAntallUkerIgjen(BigInteger.valueOf(round((antallDagerIgjen.floatValue() / 7))));
-        kontrakt.withIhtVedtak(vedtak);
-        return kontrakt;
+        return kontrakt
+                .withAntallDagerIgjen(antallDagerIgjen)
+                .withAntallUkerIgjen(valueOf(round((antallDagerIgjen.floatValue() / 7))))
+                .withIhtVedtak(vedtak);
     }
 
     private static FimYtelseskontrakt populateYtelsesKontrakt(FimYtelseskontrakt kontrakt, Date datoKravMottat, Date fom, Date tom, String status, String ytelsestype) {
-        kontrakt.setDatoKravMottatt(convertDateToXmlGregorianCalendar(datoKravMottat));
-        kontrakt.setFomGyldighetsperiode(convertDateToXmlGregorianCalendar(fom));
-        kontrakt.setTomGyldighetsperiode(convertDateToXmlGregorianCalendar(tom));
-        kontrakt.setStatus(status);
-        kontrakt.setYtelsestype(ytelsestype);
-        return kontrakt;
+        return kontrakt
+                .withDatoKravMottatt(convertDateToXmlGregorianCalendar(datoKravMottat))
+                .withFomGyldighetsperiode(convertDateToXmlGregorianCalendar(fom))
+                .withTomGyldighetsperiode(convertDateToXmlGregorianCalendar(tom))
+                .withStatus(status)
+                .withYtelsestype(ytelsestype);
     }
 
 }
