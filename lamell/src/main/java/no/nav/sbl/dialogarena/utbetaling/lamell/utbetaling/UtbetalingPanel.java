18e2f364bd5640d9767a339cdf19dca4298423fe
Jarle Svendsrud <jarle.svendsrud@nav.no>
2013-12-10 15:14:14 +0100

    Separerte ut detaljvisning i egen klasse

--- lamell/src/main/java/no/nav/sbl/dialogarena/utbetaling/lamell/utbetaling/UtbetalingPanel.java
+++ lamell/src/main/java/no/nav/sbl/dialogarena/utbetaling/lamell/utbetaling/UtbetalingPanel.java
@@ -1,27 +1,22 @@
 package no.nav.sbl.dialogarena.utbetaling.lamell.utbetaling;
 
-import no.nav.sbl.dialogarena.utbetaling.domain.Bilag;
-import no.nav.sbl.dialogarena.utbetaling.domain.PosteringsDetalj;
 import no.nav.sbl.dialogarena.utbetaling.domain.Utbetaling;
+import no.nav.sbl.dialogarena.utbetaling.lamell.utbetaling.detaljvisning.DetaljPanel;
 import org.apache.wicket.ajax.AjaxEventBehavior;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.list.ListItem;
-import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.CompoundPropertyModel;
-
-import java.util.List;
 
 public class UtbetalingPanel extends Panel {
 
     public UtbetalingPanel(String id, Utbetaling utbetaling) {
         super(id, new CompoundPropertyModel<>(utbetaling));
-        WebMarkupContainer bilagWrapper = createBilagWrapper(utbetaling.getBilag());
+        DetaljPanel detaljPanel = createDetaljPanel(utbetaling);
 
         add(
-                bilagWrapper,
+                detaljPanel,
                 new Label("statusBeskrivelse"),
                 new Label("kortUtbetalingsDato"),
                 new Label("beskrivelse"),
@@ -30,39 +25,13 @@ public class UtbetalingPanel extends Panel {
                 new Label("trekk"),
                 new Label("belopMedValuta")
         );
-        add(createClickBehavior(bilagWrapper));
+        add(createClickBehavior(detaljPanel));
     }
 
-    private WebMarkupContainer createBilagWrapper(List<Bilag> bilagsliste) {
-        return (WebMarkupContainer) new WebMarkupContainer("bilagWrapper")
-                .add(createBilagListView(bilagsliste))
+    private DetaljPanel createDetaljPanel(Utbetaling utbetaling) {
+        return (DetaljPanel) new DetaljPanel("bilagWrapper", utbetaling)
                 .setOutputMarkupPlaceholderTag(true)
                 .setVisibilityAllowed(false);
-    }
-
-    private ListView<Bilag> createBilagListView(List<Bilag> bilagsliste) {
-        return new ListView<Bilag>("bilag-liste", bilagsliste) {
-            @Override
-            protected void populateItem(ListItem<Bilag> item) {
-                Bilag bilag = item.getModelObject();
-                item.add(new Label("melding", bilag.getMelding()));
-                item.add(createPosteringsDetaljListView(bilag.getPosteringsDetaljer()));
-            }
-        };
-    }
-
-    private ListView<PosteringsDetalj> createPosteringsDetaljListView(List<PosteringsDetalj> posteringsDetaljListe) {
-        return new ListView<PosteringsDetalj>("posteringsdetalj-liste", posteringsDetaljListe) {
-            @Override
-            protected void populateItem(ListItem<PosteringsDetalj> item) {
-                PosteringsDetalj posteringsDetalj = item.getModelObject();
-                item.add(new Label("hovedbeskrivelse", posteringsDetalj.getHovedBeskrivelse()));
-                item.add(new Label("underbeskrivelse", posteringsDetalj.getUnderBeskrivelse()));
-                item.add(new Label("kontonr", posteringsDetalj.getKontoNr()));
-                item.add(new Label("sats", posteringsDetalj.getSats()));
-                item.add(new Label("antall", posteringsDetalj.getAntall()));
-            }
-        };
     }
 
     private AjaxEventBehavior createClickBehavior(final WebMarkupContainer container) {
