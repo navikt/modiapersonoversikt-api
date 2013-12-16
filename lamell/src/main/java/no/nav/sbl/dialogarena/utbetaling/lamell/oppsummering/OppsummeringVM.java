e1289e930fa70b5c2d5ff8cca9e29ac09ebf32ca
Frithjof Nerdrum <frithjof.nerdrum@nav.no>
2013-12-16 10:36:05 +0100

    Bruker modig-lang for å hente og sortere hovedytelser i oppsummeringVM.

--- lamell/src/main/java/no/nav/sbl/dialogarena/utbetaling/lamell/oppsummering/OppsummeringVM.java
+++ lamell/src/main/java/no/nav/sbl/dialogarena/utbetaling/lamell/oppsummering/OppsummeringVM.java
@@ -1,31 +1,29 @@
 package no.nav.sbl.dialogarena.utbetaling.lamell.oppsummering;
 
 
+import java.io.Serializable;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 import no.nav.sbl.dialogarena.utbetaling.domain.Utbetaling;
 import org.apache.commons.collections15.Transformer;
 import org.joda.time.LocalDate;
 
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-
-import static java.util.Collections.sort;
+import static no.nav.modig.lang.collections.ComparatorUtils.compareWith;
 import static no.nav.modig.lang.collections.IterUtils.on;
 import static no.nav.modig.lang.collections.ReduceUtils.sumDouble;
 import static no.nav.sbl.dialogarena.time.Datoformat.KORT;
-import static no.nav.sbl.dialogarena.utbetaling.lamell.oppsummering.HovedYtelseVM.HovedYtelseComparator.NAVN;
 import static no.nav.sbl.dialogarena.utbetaling.domain.util.UtbetalingListeUtils.summerBelopForUnderytelser;
 import static no.nav.sbl.dialogarena.utbetaling.domain.util.ValutaUtil.getBelopString;
+import static no.nav.sbl.dialogarena.utbetaling.lamell.oppsummering.HovedYtelseVM.NAVN;
 
 
 public class OppsummeringVM implements Serializable {
 
-    private List<Utbetaling> utbetalinger;
-    private LocalDate sluttDato;
-    private LocalDate startDato;
-    private List<HovedYtelseVM> hovedytelser;
+    public List<Utbetaling> utbetalinger;
+    public LocalDate sluttDato;
+    public LocalDate startDato;
+    public List<HovedYtelseVM> hovedytelser;
     public String utbetalt, trekk, brutto;
 
     public OppsummeringVM(List<Utbetaling> utbetalinger, LocalDate startDato, LocalDate sluttDato) {
@@ -35,25 +33,15 @@ public class OppsummeringVM implements Serializable {
         this.utbetalt = getBelopString(on(utbetalinger).map(NETTO).reduce(sumDouble));
         this.trekk = getBelopString(on(utbetalinger).map(BEREGNET_TREKK).reduce(sumDouble));
         this.brutto = getBelopString(on(utbetalinger).map(BRUTTO).reduce(sumDouble));
-        this.hovedytelser = new ArrayList<>();
-        Map<String,Map<String,Double>> ytelserUtbetalt = summerBelopForUnderytelser(utbetalinger);
-        for (String hovedytelse : ytelserUtbetalt.keySet()) {
-            hovedytelser.add(new HovedYtelseVM(hovedytelse, ytelserUtbetalt.get(hovedytelse)));
+        this.hovedytelser = on(summerBelopForUnderytelser(utbetalinger)).map(TIL_HOVEDYTELSE).collect(compareWith(NAVN));
+    }
+
+    private static final Transformer<Map.Entry<String, Map<String,Double>>, HovedYtelseVM> TIL_HOVEDYTELSE = new Transformer<Map.Entry<String, Map<String, Double>>, HovedYtelseVM>() {
+        @Override
+        public HovedYtelseVM transform(Map.Entry<String, Map<String, Double>> entry) {
+            return new HovedYtelseVM(entry.getKey(), entry.getValue());
         }
-        sort(hovedytelser, NAVN);
-    }
-
-    public List<Utbetaling> getUtbetalinger() {
-        return utbetalinger;
-    }
-
-    public LocalDate getSluttDato() {
-        return sluttDato;
-    }
-
-    public LocalDate getStartDato() {
-        return startDato;
-    }
+    };
 
     public String getOppsummertPeriode() {
         if (startDato.getMonthOfYear() == sluttDato.getMonthOfYear()) {
