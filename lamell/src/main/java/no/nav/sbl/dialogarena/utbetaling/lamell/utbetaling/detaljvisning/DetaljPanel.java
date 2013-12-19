39934fcac489ab77544f7606db604f6bd741a286
Kari B. Schjønsby <Kari.Bergh.Schjonsby@nav.no>
2013-12-19 15:59:38 +0100

    Fjernet Bilag osv fra DetaljPanel

--- lamell/src/main/java/no/nav/sbl/dialogarena/utbetaling/lamell/utbetaling/detaljvisning/DetaljPanel.java
+++ lamell/src/main/java/no/nav/sbl/dialogarena/utbetaling/lamell/utbetaling/detaljvisning/DetaljPanel.java
@@ -4,14 +4,17 @@ import no.nav.sbl.dialogarena.utbetaling.lamell.utbetaling.UtbetalingVM;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
 
+import static no.nav.modig.lang.collections.IterUtils.on;
+
 public class DetaljPanel extends Panel {
 
     public DetaljPanel(String id, UtbetalingVM utbetalingVM) {
         super(id);
         add(
-                new Label("mottatt", utbetalingVM.getMottakerId()),
+                new Label("mottakerid", utbetalingVM.getMottakerId()),
                 new Label("konto", utbetalingVM.getKontonr()),
                 new Label("ytelsesinfo", utbetalingVM.getBeskrivelse()),
+                new Label("underytelser", utbetalingVM.getUnderytelser().toString()),
                 new Label("periode", utbetalingVM.getPeriodeMedKortDato())
         );
     }
