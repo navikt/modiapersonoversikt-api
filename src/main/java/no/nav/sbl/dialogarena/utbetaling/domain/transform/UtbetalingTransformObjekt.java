1ca022f1380a0779e6be02b26e7b769bd9758c9b
Borge Bardo <borge.bardo@nav.no>
2013-12-20 16:42:29 +0100

    glemte en liten checkstyle

--- src/main/java/no/nav/sbl/dialogarena/utbetaling/domain/transform/UtbetalingTransformObjekt.java
+++ src/main/java/no/nav/sbl/dialogarena/utbetaling/domain/transform/UtbetalingTransformObjekt.java
@@ -49,8 +49,12 @@ final class UtbetalingTransformObjekt {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof UtbetalingTransformObjekt)) return false;
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof UtbetalingTransformObjekt)) {
+            return false;
+        }
 
         UtbetalingTransformObjekt that = (UtbetalingTransformObjekt) o;
 
