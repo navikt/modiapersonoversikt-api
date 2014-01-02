cadc0e46d42fd1c22700e0e47833b928d9b66afd
Kari B. Schjønsby <Kari.Bergh.Schjonsby@nav.no>
2014-01-02 16:21:17 +0100

    Fjernet unødvendige 'else'

--- src/main/java/no/nav/sbl/dialogarena/utbetaling/widget/UtbetalingVMComparator.java
+++ src/main/java/no/nav/sbl/dialogarena/utbetaling/widget/UtbetalingVMComparator.java
@@ -11,12 +11,13 @@ public class UtbetalingVMComparator implements Comparator<UtbetalingVM> {
     public int compare(UtbetalingVM utbetalingVM1, UtbetalingVM utbetalingVM2) {
         if (utbetalingVM1.getUtbetalingsDato() == null && utbetalingVM2.getUtbetalingsDato() == null) {
             return 0;
-        } else if (utbetalingVM1.getUtbetalingsDato() == null) {
-            return -1;
-        } else if (utbetalingVM2.getUtbetalingsDato() == null) {
-            return 1;
-        } else {
-            return utbetalingVM2.getUtbetalingsDato().compareTo(utbetalingVM1.getUtbetalingsDato());
         }
+        if (utbetalingVM1.getUtbetalingsDato() == null) {
+            return -1;
+        }
+        if (utbetalingVM2.getUtbetalingsDato() == null) {
+            return 1;
+        }
+        return utbetalingVM2.getUtbetalingsDato().compareTo(utbetalingVM1.getUtbetalingsDato());
     }
 }
