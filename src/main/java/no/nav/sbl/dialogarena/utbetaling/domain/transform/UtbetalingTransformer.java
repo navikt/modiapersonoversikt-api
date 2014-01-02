4d6cf358c319424bc19cbafdf366e47b0e10a2ca
Kari B. Schjønsby <Kari.Bergh.Schjonsby@nav.no>
2014-01-02 16:36:26 +0100

    Fjernet ubrukt import

--- src/main/java/no/nav/sbl/dialogarena/utbetaling/domain/transform/UtbetalingTransformer.java
+++ src/main/java/no/nav/sbl/dialogarena/utbetaling/domain/transform/UtbetalingTransformer.java
@@ -13,7 +13,6 @@ import org.joda.time.LocalDate;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
