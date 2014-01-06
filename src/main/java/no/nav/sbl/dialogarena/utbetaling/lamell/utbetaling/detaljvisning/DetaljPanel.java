e5b669366c03da11b1a9f1d2faec5dc059964016
Jarle Svendsrud <jarle.svendsrud@nav.no>
2014-01-06 08:56:56 +0100

    WiP detaljpanel

--- src/main/java/no/nav/sbl/dialogarena/utbetaling/lamell/utbetaling/detaljvisning/DetaljPanel.java
+++ src/main/java/no/nav/sbl/dialogarena/utbetaling/lamell/utbetaling/detaljvisning/DetaljPanel.java
@@ -1,8 +1,14 @@
 package no.nav.sbl.dialogarena.utbetaling.lamell.utbetaling.detaljvisning;
 
+import no.nav.sbl.dialogarena.utbetaling.domain.Underytelse;
 import no.nav.sbl.dialogarena.utbetaling.lamell.utbetaling.UtbetalingVM;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Panel;
+
+import java.util.List;
+
 
 public class DetaljPanel extends Panel {
 
@@ -14,7 +20,19 @@ public class DetaljPanel extends Panel {
                 new Label("mottakerid", utbetalingVM.getMottakerId()),
                 new Label("konto", utbetalingVM.getKontonr()),
                 new Label("ytelsesinfo", utbetalingVM.getBeskrivelse()),
+                createYtelsesrader(utbetalingVM),
                 new Label("bilagsmelding", "TODO fiks bilagsmelding")
         );
     }
+
+    private ListView createYtelsesrader(UtbetalingVM utbetalingVM) {
+        List<Underytelse> underytelser = utbetalingVM.getUnderytelser(); //TODO lag wicketmodell, smell inn i listview
+        return new ListView("ytelsesrader") {
+            @Override
+            protected void populateItem(ListItem item) {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+        };
+    }
+
 }
