7c30b05ba4fe97332f56473b98af7ef7b34fed43
Frithjof Nerdrum <frithjof.nerdrum@nav.no>
2014-01-06 16:25:27 +0100

    PMD

--- src/main/java/no/nav/sbl/dialogarena/utbetaling/lamell/utbetaling/detaljvisning/DetaljPanel.java
+++ src/main/java/no/nav/sbl/dialogarena/utbetaling/lamell/utbetaling/detaljvisning/DetaljPanel.java
@@ -1,10 +1,7 @@
 package no.nav.sbl.dialogarena.utbetaling.lamell.utbetaling.detaljvisning;
 
-import no.nav.sbl.dialogarena.utbetaling.domain.Underytelse;
 import no.nav.sbl.dialogarena.utbetaling.lamell.utbetaling.UtbetalingVM;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.list.ListItem;
-import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Panel;
 
 public class DetaljPanel extends Panel {
@@ -22,21 +19,21 @@ public class DetaljPanel extends Panel {
         );
     }
 
-    private ListView createYtelsesrader(final UtbetalingVM utbetalingVM) {
+//    private ListView createYtelsesrader(final UtbetalingVM utbetalingVM) {
 //        List<Underytelse> underytelser = utbetalingVM.getUnderytelser(); //TODO lag wicketmodell, smell inn i listview
-        return new ListView<Underytelse>("ytelsesrader") {
-            @Override
-            protected void populateItem(ListItem<Underytelse> item) {
-                item.add(
-                        new Label("ytelse", ""), //??
-                        new Label("antall", item.getModelObject().getAntall()),
-                        new Label("sats", item.getModelObject().getSats()),
-                        new Label("brutto", ""), //??
-                        new Label("trekk", ""), //??
-                        new Label("tilutbetaling", "") //??
-                );
-            }
-        };
-    }
+//        return new ListView<Underytelse>("ytelsesrader") {
+//            @Override
+//            protected void populateItem(ListItem<Underytelse> item) {
+//                item.add(
+//                        new Label("ytelse", ""), //??
+//                        new Label("antall", item.getModelObject().getAntall()),
+//                        new Label("sats", item.getModelObject().getSats()),
+//                        new Label("brutto", ""), //??
+//                        new Label("trekk", ""), //??
+//                        new Label("tilutbetaling", "") //??
+//                );
+//            }
+//        };
+//    }
 
 }
