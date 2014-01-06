21170f4f9e67fa55ea0d0f627c69ef67410eee01
Jarle Svendsrud <jarle.svendsrud@nav.no>
2014-01-06 14:41:54 +0100

    Stabilt mtp. detaljpanel. En del WiP som må tas med Kari

--- src/main/java/no/nav/sbl/dialogarena/utbetaling/lamell/utbetaling/detaljvisning/DetaljPanel.java
+++ src/main/java/no/nav/sbl/dialogarena/utbetaling/lamell/utbetaling/detaljvisning/DetaljPanel.java
@@ -9,7 +9,6 @@ import org.apache.wicket.markup.html.panel.Panel;
 
 import java.util.List;
 
-
 public class DetaljPanel extends Panel {
 
     public DetaljPanel(String id, UtbetalingVM utbetalingVM) {
@@ -20,17 +19,24 @@ public class DetaljPanel extends Panel {
                 new Label("mottakernavn", utbetalingVM.getMottakerNavn()),
                 new Label("konto", utbetalingVM.getKontonr()),
                 new Label("ytelsesinfo", utbetalingVM.getBeskrivelse()),
-                createYtelsesrader(utbetalingVM),
+//                createYtelsesrader(utbetalingVM),
                 new Label("bilagsmelding", utbetalingVM.getMelding())
         );
     }
 
-    private ListView createYtelsesrader(UtbetalingVM utbetalingVM) {
+    private ListView createYtelsesrader(final UtbetalingVM utbetalingVM) {
         List<Underytelse> underytelser = utbetalingVM.getUnderytelser(); //TODO lag wicketmodell, smell inn i listview
-        return new ListView("ytelsesrader") {
+        return new ListView<Underytelse>("ytelsesrader") {
             @Override
-            protected void populateItem(ListItem item) {
-                //To change body of implemented methods use File | Settings | File Templates.
+            protected void populateItem(ListItem<Underytelse> item) {
+                item.add(
+                        new Label("ytelse", ""), //??
+                        new Label("antall", item.getModelObject().getAntall()),
+                        new Label("sats", item.getModelObject().getSats()),
+                        new Label("brutto", ""), //??
+                        new Label("trekk", ""), //??
+                        new Label("tilutbetaling", "") //??
+                );
             }
         };
     }
