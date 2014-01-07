4d6c230f4db02de739b4212b379782e80592a678
Jarle Svendsrud <jarle.svendsrud@nav.no>
2014-01-07 12:12:41 +0100

    Initielt oppsett for detaljvisning med liste av underytelser

--- src/main/java/no/nav/sbl/dialogarena/utbetaling/lamell/utbetaling/detaljvisning/DetaljPanel.java
+++ src/main/java/no/nav/sbl/dialogarena/utbetaling/lamell/utbetaling/detaljvisning/DetaljPanel.java
@@ -1,7 +1,10 @@
 package no.nav.sbl.dialogarena.utbetaling.lamell.utbetaling.detaljvisning;
 
+import no.nav.sbl.dialogarena.utbetaling.domain.Underytelse;
 import no.nav.sbl.dialogarena.utbetaling.lamell.utbetaling.UtbetalingVM;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Panel;
 
 public class DetaljPanel extends Panel {
@@ -14,26 +17,18 @@ public class DetaljPanel extends Panel {
                 new Label("mottakernavn", utbetalingVM.getMottakerNavn()),
                 new Label("konto", utbetalingVM.getKontonr()),
                 new Label("ytelsesinfo", utbetalingVM.getBeskrivelse()),
-//                createYtelsesrader(utbetalingVM),
+                createYtelsesrader(utbetalingVM),
                 new Label("bilagsmelding", utbetalingVM.getMelding())
         );
     }
 
-//    private ListView createYtelsesrader(final UtbetalingVM utbetalingVM) {
-//        List<Underytelse> underytelser = utbetalingVM.getUnderytelser(); //TODO lag wicketmodell, smell inn i listview
-//        return new ListView<Underytelse>("ytelsesrader") {
-//            @Override
-//            protected void populateItem(ListItem<Underytelse> item) {
-//                item.add(
-//                        new Label("ytelse", ""), //??
-//                        new Label("antall", item.getModelObject().getAntall()),
-//                        new Label("sats", item.getModelObject().getSats()),
-//                        new Label("brutto", ""), //??
-//                        new Label("trekk", ""), //??
-//                        new Label("tilutbetaling", "") //??
-//                );
-//            }
-//        };
-//    }
+    private ListView createYtelsesrader(final UtbetalingVM utbetalingVM) {
+        return new ListView<Underytelse>("underytelser", utbetalingVM.getUnderytelser()) {
+            @Override
+            protected void populateItem(ListItem<Underytelse> item) {
+                item.add(new UnderytelsePanel("underytelse", item.getModelObject()));
+            }
+        };
+    }
 
 }
