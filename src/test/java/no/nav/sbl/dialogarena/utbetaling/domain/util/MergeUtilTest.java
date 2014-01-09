d1d88ea5ea4bc9c8a3373aec1b0c829a67fef37f
Kari B. Schjønsby <Kari.Bergh.Schjonsby@nav.no>
2014-01-09 13:02:14 +0100

    checkstyle i testkoden

--- src/test/java/no/nav/sbl/dialogarena/utbetaling/domain/util/MergeUtilTest.java
+++ src/test/java/no/nav/sbl/dialogarena/utbetaling/domain/util/MergeUtilTest.java
@@ -53,20 +53,46 @@ public class MergeUtilTest {
         Farge gronn2 = new Farge(100, "Grønn");
         Farge gronn3 = new Farge(115, "Grønn");
 
-        List<Farge> blandede_farger = merge(new ArrayList<Mergeable<Farge>>(asList(rod, gronn1, gul, gronn1, gronn2, gronn3)), Farge.MERGEABLE_NAVN, Farge.MERGEABLE_NAVN);
+        List<Farge> blandedeFarger = merge(new ArrayList<Mergeable<Farge>>(asList(rod, gronn1, gul, gronn1, gronn2, gronn3)), Farge.MERGEABLE_NAVN, Farge.MERGEABLE_NAVN);
 
         List<String> navn = asList("Grønn", "Gul", "Rød");
-        List<Integer> hues = asList(   108,    60,     1);
-        assertThat(blandede_farger.size(), is(3));
-        assertThat(blandede_farger.get(0).navn, is(navn.get(0)));
-        assertThat(blandede_farger.get(0).hue, is(hues.get(0)));
-        assertThat(blandede_farger.get(1).navn, is(navn.get(1)));
-        assertThat(blandede_farger.get(1).hue, is(hues.get(1)));
-        assertThat(blandede_farger.get(2).navn, is(navn.get(2)));
-        assertThat(blandede_farger.get(2).hue, is(hues.get(2)));
+        List<Integer> hues = asList(108, 60, 1);
+        assertThat(blandedeFarger.size(), is(3));
+        assertThat(blandedeFarger.get(0).navn, is(navn.get(0)));
+        assertThat(blandedeFarger.get(0).hue, is(hues.get(0)));
+        assertThat(blandedeFarger.get(1).navn, is(navn.get(1)));
+        assertThat(blandedeFarger.get(1).hue, is(hues.get(1)));
+        assertThat(blandedeFarger.get(2).navn, is(navn.get(2)));
+        assertThat(blandedeFarger.get(2).hue, is(hues.get(2)));
     }
 
     private static final class Farge implements Mergeable<Farge> {
+        static final Comparator<Mergeable<Farge>> MERGEABLE_NAVN = new Comparator<Mergeable<Farge>>() {
+            @Override
+            public int compare(Mergeable<Farge> o1, Mergeable<Farge> o2) {
+                Farge obj1 = (Farge) o1;
+                Farge obj2 = (Farge) o2;
+                return obj1.navn.compareToIgnoreCase(obj2.navn);
+            }
+        };
+        static final Transformer<Mergeable, Farge> FARGE_TRANSFORMER = new Transformer<Mergeable, Farge>() {
+            @Override
+            public Farge transform(Mergeable mergeable) {
+                return (Farge) mergeable;
+            }
+        };
+        static final Transformer<Farge, Integer> HUE = new Transformer<Farge, Integer>() {
+            @Override
+            public Integer transform(Farge farge) {
+                return farge.hue;
+            }
+        };
+        static final Transformer<Farge, String> NAVN = new Transformer<Farge, String>() {
+            @Override
+            public String transform(Farge farge) {
+                return farge.navn;
+            }
+        };
         String navn;
         int hue;
 
@@ -78,44 +104,16 @@ public class MergeUtilTest {
         @Override
         public Farge doMerge(List<Mergeable> skalMerges) {
             List<Farge> farger = on(skalMerges).map(FARGE_TRANSFORMER).collectIn(new ArrayList<Farge>());
-            if(farger.isEmpty()) { return null; }
+            if (farger.isEmpty()) {
+                return null;
+            }
 
             double sum = on(farger).map(HUE).reduce(sumInt);
-            Double average = sum/farger.size();
-            Set<String> navn = on(farger).map(NAVN).collectIn(new HashSet<String>());
-            String fargeNavn = join(navn, ", ");
+            Double average = sum / farger.size();
+            Set<String> navneSet = on(farger).map(NAVN).collectIn(new HashSet<String>());
+            String fargeNavn = join(navneSet, ", ");
             return new Farge(Double.valueOf(Math.floor(average)).intValue(), fargeNavn);
         }
-
-        static final Comparator<Mergeable<Farge>> MERGEABLE_NAVN = new Comparator<Mergeable<Farge>>() {
-            @Override
-            public int compare(Mergeable<Farge> o1, Mergeable<Farge> o2) {
-                Farge obj1 = (Farge) o1;
-                Farge obj2 = (Farge) o2;
-                return obj1.navn.compareToIgnoreCase(obj2.navn);
-            }
-        };
-
-        static final Transformer<Mergeable, Farge> FARGE_TRANSFORMER = new Transformer<Mergeable, Farge>() {
-            @Override
-            public Farge transform(Mergeable mergeable) {
-                return (Farge) mergeable;
-            }
-        };
-
-        static final Transformer<Farge, Integer> HUE = new Transformer<Farge, Integer>() {
-            @Override
-            public Integer transform(Farge farge) {
-                return farge.hue;
-            }
-        };
-
-        static final Transformer<Farge, String> NAVN = new Transformer<Farge, String>() {
-            @Override
-            public String transform(Farge farge) {
-                return farge.navn;
-            }
-        };
 
 
     }
