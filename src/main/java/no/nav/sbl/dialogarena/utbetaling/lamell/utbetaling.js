6ba0a2d3ae029f2bfd2ee824e6dbf6f94e6cc768
Andreas Baakind <andreas.baakind@nav.no>
2014-01-09 10:17:42 +0100

    Bakgrunnsfarge på ekspandert detaljvisning

--- src/main/java/no/nav/sbl/dialogarena/utbetaling/lamell/utbetaling.js
+++ src/main/java/no/nav/sbl/dialogarena/utbetaling/lamell/utbetaling.js
@@ -2,8 +2,7 @@ var utbetalingslinjeSelector = '.utbetaling-ramme .utbetalingslinje';
 
 $(document).on('click', utbetalingslinjeSelector, function () {
     $(this).children('.detaljpanel').animate({height: 'toggle'}, 300);
-    $(this).find('.ekspander-pil').toggleClass('ekspandert');
-    $(this).toggleClass('ekspandert-bakgrunn');
+    $(this).toggleClass('ekspandert');
 });
 
 $(document).on('click', utbetalingslinjeSelector + ' .skriv-ut', function(event) {
@@ -18,7 +17,7 @@ var Utbetalinger = (function () {
     var haandterDetaljPanelVisning = function (detaljPanelID) {
         var $detaljPanel = $("#" + detaljPanelID);
         $detaljPanel.animate({height: 'toggle'}, 900);
-        $detaljPanel.siblings('.main').find('.ekspander-pil').toggleClass('ekspandert');
+        $detaljPanel.parent().toggleClass('ekspandert');
         $('html,body').animate({scrollTop: $($detaljPanel).parent().offset().top - 76}, 'slow');
         $detaljPanel.focus();
     };
