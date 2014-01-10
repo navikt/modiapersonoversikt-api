5f7d01982cdb59f1e39b4b86b43e274159fe2476
Kari B. Schjønsby <Kari.Bergh.Schjonsby@nav.no>
2014-01-10 09:51:20 +0100

    La til wickettester

---
+++ src/test/java/no/nav/sbl/dialogarena/utbetaling/lamell/filter/FilterFormPanelTest.java
@@ -0,0 +1,34 @@
+package no.nav.sbl.dialogarena.utbetaling.lamell.filter;
+
+import no.nav.sbl.dialogarena.utbetaling.domain.Utbetaling;
+import no.nav.sbl.dialogarena.utbetaling.wickettest.AbstractWicketTest;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import static java.util.Arrays.asList;
+import static no.nav.sbl.dialogarena.utbetaling.domain.Utbetaling.BRUKER;
+
+
+public class FilterFormPanelTest extends AbstractWicketTest{
+
+    @Override
+    protected void setup() {
+        wicketTester.tester.getSession().replaceSession();
+    }
+
+    @Test
+    public void testFilterPanel() throws Exception {
+
+        Map<String, Boolean> mottakere = new HashMap<>();
+        mottakere.put(BRUKER, true);
+        Set<String> hovedYtelser = new HashSet<>(asList("Mat"));
+        FilterParametere filterParametre = new FilterParametere(Utbetaling.defaultStartDato(), Utbetaling.defaultSluttDato(), mottakere, hovedYtelser);
+        FilterFormPanel filterFormPanel = new FilterFormPanel("filterFormPanel", filterParametre);
+        wicketTester.goToPageWith(filterFormPanel);
+//        wicketTester.printComponentsTree();
+    }
+}
