239f45869d19cf9d3fb30f25b4e21f8190a6bf4a
Borge Bardo <borge.bardo@nav.no>
2014-01-14 11:17:19 +0100

    [kaizen] implementerer IAjaxIndicatorAware på formet istedet for alle knappene + lagt til snurrepipp for 'prøv-igjen' lenke på feilmeldingside

--- src/main/java/no/nav/sbl/dialogarena/utbetaling/lamell/unntak/FeilmeldingPanel.java
+++ src/main/java/no/nav/sbl/dialogarena/utbetaling/lamell/unntak/FeilmeldingPanel.java
@@ -2,6 +2,7 @@ package no.nav.sbl.dialogarena.utbetaling.lamell.unntak;
 
 import no.nav.modig.wicket.events.components.AjaxEventLink;
 import no.nav.sbl.dialogarena.utbetaling.lamell.filter.FilterParametere;
+import org.apache.wicket.ajax.IAjaxIndicatorAware;
 
 /**
  * Panel for feilmelding som inneholder en prøv-igjen lenke
@@ -10,6 +11,17 @@ public class FeilmeldingPanel extends UtbetalingerMessagePanel {
 
     public FeilmeldingPanel(String id, String messageKey, String cssClass) {
         super(id, messageKey, cssClass);
-        add(new AjaxEventLink<>("prov-igjen", FilterParametere.FILTER_ENDRET));
+        add(new IndicatingAjaxEventLink("prov-igjen", FilterParametere.FILTER_ENDRET));
+    }
+
+    private class IndicatingAjaxEventLink extends AjaxEventLink implements IAjaxIndicatorAware {
+        public IndicatingAjaxEventLink(String id, String eventName) {
+            super(id, eventName);
+        }
+
+        @Override
+        public String getAjaxIndicatorMarkupId() {
+            return "ajax-indikator";
+        }
     }
 }
