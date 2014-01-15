b7034f42dbcee0a6c58ded3df0e85b415c95f179
Andreas Baakind <andreas.baakind@nav.no>
2014-01-15 14:47:39 +0100

    [gui] Flytt feilmelding over totalt utbetalt pa hoyre side

--- src/test/java/no/nav/sbl/dialogarena/utbetaling/lamell/filter/FilterFormPanelTest.java
+++ src/test/java/no/nav/sbl/dialogarena/utbetaling/lamell/filter/FilterFormPanelTest.java
@@ -2,6 +2,7 @@ package no.nav.sbl.dialogarena.utbetaling.lamell.filter;
 
 import no.nav.sbl.dialogarena.utbetaling.domain.Utbetaling;
 import no.nav.sbl.dialogarena.utbetaling.wickettest.AbstractWicketTest;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.junit.Test;
 
 import java.util.HashMap;
@@ -27,7 +28,7 @@ public class FilterFormPanelTest extends AbstractWicketTest{
         mottakere.put(BRUKER, true);
         Set<String> hovedYtelser = new HashSet<>(asList("Mat"));
         FilterParametere filterParametre = new FilterParametere(Utbetaling.defaultStartDato(), Utbetaling.defaultSluttDato(), mottakere, hovedYtelser);
-        FilterFormPanel filterFormPanel = new FilterFormPanel("filterFormPanel", filterParametre);
+        FilterFormPanel filterFormPanel = new FilterFormPanel("filterFormPanel", filterParametre, new FeedbackPanel("test"));
         wicketTester.goToPageWith(filterFormPanel);
 //        wicketTester.printComponentsTree();
     }
