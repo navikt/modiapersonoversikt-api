b221a6af693da2d7f95b846643867f1287404783
Henrik Hellerøy <henrik.helleroy@nav.no>
2014-01-23 15:47:55 +0100

    Egen pakke for HentUtbetalingerPanel og feilmelding i widget.

--- src/main/java/no/nav/sbl/dialogarena/utbetaling/widget/hentutbetalinger/HentUtbetalingerFeilmeldingPanel.java
+++ src/main/java/no/nav/sbl/dialogarena/utbetaling/widget/hentutbetalinger/HentUtbetalingerFeilmeldingPanel.java
@@ -1,4 +1,6 @@
-package no.nav.sbl.dialogarena.utbetaling.widget;
+package no.nav.sbl.dialogarena.utbetaling.widget.hentutbetalinger;
+
+import no.nav.sbl.dialogarena.utbetaling.widget.UtbetalingWidget;
 
 public class HentUtbetalingerFeilmeldingPanel extends HentUtbetalingerPanel {
     public HentUtbetalingerFeilmeldingPanel(final UtbetalingWidget widget) {
--- src/main/java/no/nav/sbl/dialogarena/utbetaling/widget/hentutbetalinger/HentUtbetalingerPanel.java
+++ src/main/java/no/nav/sbl/dialogarena/utbetaling/widget/hentutbetalinger/HentUtbetalingerPanel.java
@@ -1,7 +1,8 @@
-package no.nav.sbl.dialogarena.utbetaling.widget;
+package no.nav.sbl.dialogarena.utbetaling.widget.hentutbetalinger;
 
 import no.nav.modig.modia.widget.panels.GenericListingPanel;
 import no.nav.sbl.dialogarena.utbetaling.util.AjaxIndicator;
+import no.nav.sbl.dialogarena.utbetaling.widget.UtbetalingWidget;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 
 public class HentUtbetalingerPanel extends GenericListingPanel {
