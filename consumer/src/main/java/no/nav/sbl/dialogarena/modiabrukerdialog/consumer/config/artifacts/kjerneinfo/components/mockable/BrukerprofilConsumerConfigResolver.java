0ced974903927e37c6878f439bc12e471d9a7078
Jarle Svendsrud <jarle.svendsrud@nav.no>
2014-01-28 15:16:09 +0100

    Fikset riktig anonym indreinstansering for service

--- consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/artifacts/kjerneinfo/components/mockable/BrukerprofilConsumerConfigResolver.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/artifacts/kjerneinfo/components/mockable/BrukerprofilConsumerConfigResolver.java
@@ -2,8 +2,14 @@ package no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.artifacts.kjern
 
 import no.nav.brukerprofil.config.spring.brukerprofil.BrukerprofilConsumerConfig;
 import no.nav.brukerprofil.consumer.BrukerprofilServiceBi;
+import no.nav.brukerprofil.consumer.messages.BrukerprofilRequest;
+import no.nav.brukerprofil.consumer.messages.BrukerprofilResponse;
+import no.nav.brukerprofil.consumer.support.mapping.BrukerprofilMapper;
+import no.nav.modig.modia.ping.PingResult;
 import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.artifacts.kjerneinfo.components.mockable.mockableimpl.BrukerprofilConsumerConfigImpl;
 import no.nav.tjeneste.virksomhet.brukerprofil.v1.BrukerprofilPortType;
+import no.nav.tjeneste.virksomhet.brukerprofil.v1.HentKontaktinformasjonOgPreferanserPersonIkkeFunnet;
+import no.nav.tjeneste.virksomhet.brukerprofil.v1.HentKontaktinformasjonOgPreferanserSikkerhetsbegrensning;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -11,7 +17,8 @@ import org.springframework.context.annotation.Import;
 import javax.inject.Inject;
 
 import static no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.artifacts.kjerneinfo.components.mockable.MockableContext.KJERNEINFO_KEY;
-import static no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.endpoints.util.InstanceSwitcher.createSwitcher;
+import static no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.endpoints.util.MockUtil.mockErSlaattPaaForKey;
+import static no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.endpoints.util.MockUtil.mockSetupErTillatt;
 import static no.nav.sbl.dialogarena.modiabrukerdialog.mock.config.artifacts.kjerneinfo.BrukerprofilServiceBiMock.getBrukerprofilServiceBiMock;
 
 @Configuration
@@ -25,9 +32,34 @@ public class BrukerprofilConsumerConfigResolver {
 
     @Bean
     public BrukerprofilServiceBi brukerprofilServiceBi() {
-        BrukerprofilServiceBi defaultBi = new BrukerprofilConsumerConfigImpl(brukerprofilPortType, selfTestBrukerprofilPortType).brukerprofilServiceBi();
-        BrukerprofilServiceBi alternateBi = getBrukerprofilServiceBiMock();
-        return createSwitcher(defaultBi, alternateBi, KJERNEINFO_KEY, BrukerprofilServiceBi.class);
-    }
+        final BrukerprofilServiceBi defaultBi = new BrukerprofilConsumerConfigImpl(brukerprofilPortType, selfTestBrukerprofilPortType).brukerprofilServiceBi();
+        final BrukerprofilServiceBi alternateBi = getBrukerprofilServiceBiMock();
+        return new BrukerprofilServiceBi() {
+            @Override
+            public BrukerprofilResponse hentKontaktinformasjonOgPreferanser(BrukerprofilRequest request) throws HentKontaktinformasjonOgPreferanserPersonIkkeFunnet, HentKontaktinformasjonOgPreferanserSikkerhetsbegrensning {
+                if (mockSetupErTillatt() && mockErSlaattPaaForKey(KJERNEINFO_KEY)) {
+                    return alternateBi.hentKontaktinformasjonOgPreferanser(request);
+                }
+                return defaultBi.hentKontaktinformasjonOgPreferanser(request);
+            }
 
+            @Override
+            public void setMapper(BrukerprofilMapper mapper) {
+                if (mockSetupErTillatt() && mockErSlaattPaaForKey(KJERNEINFO_KEY)) {
+                    alternateBi.setMapper(mapper);
+                } else {
+                    defaultBi.setMapper(mapper);
+                }
+            }
+
+            @Override
+            public PingResult ping() {
+                if (mockSetupErTillatt() && mockErSlaattPaaForKey(KJERNEINFO_KEY)) {
+                    return alternateBi.ping();
+                }
+                return defaultBi.ping();
+            }
+        };
+
+    }
 }
