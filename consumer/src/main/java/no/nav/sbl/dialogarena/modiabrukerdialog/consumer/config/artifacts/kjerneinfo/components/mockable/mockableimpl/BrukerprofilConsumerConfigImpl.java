71ed80c7bc8116674c876643311f013490835d5d
Jarle Svendsrud <jarle.svendsrud@nav.no>
2014-01-28 12:59:45 +0100

    Gjorde om brukerprofilservice til å bruke cachede (forhåpentligvis), anonyme indre klasser

--- consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/artifacts/kjerneinfo/components/mockable/mockableimpl/BrukerprofilConsumerConfigImpl.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/artifacts/kjerneinfo/components/mockable/mockableimpl/BrukerprofilConsumerConfigImpl.java
@@ -1,9 +1,15 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.artifacts.kjerneinfo.components.mockable.mockableimpl;
 
 import no.nav.brukerprofil.consumer.BrukerprofilServiceBi;
+import no.nav.brukerprofil.consumer.messages.BrukerprofilRequest;
+import no.nav.brukerprofil.consumer.messages.BrukerprofilResponse;
 import no.nav.brukerprofil.consumer.support.DefaultBrukerprofilService;
 import no.nav.brukerprofil.consumer.support.mapping.BrukerprofilMapper;
+import no.nav.modig.modia.ping.PingResult;
 import no.nav.tjeneste.virksomhet.brukerprofil.v1.BrukerprofilPortType;
+import no.nav.tjeneste.virksomhet.brukerprofil.v1.HentKontaktinformasjonOgPreferanserPersonIkkeFunnet;
+import no.nav.tjeneste.virksomhet.brukerprofil.v1.HentKontaktinformasjonOgPreferanserSikkerhetsbegrensning;
+import org.springframework.cache.annotation.Cacheable;
 
 public class BrukerprofilConsumerConfigImpl {
 
@@ -16,7 +22,26 @@ public class BrukerprofilConsumerConfigImpl {
     }
 
     public BrukerprofilServiceBi brukerprofilServiceBi() {
-        return new DefaultBrukerprofilService(brukerprofilPortType, selfTestBrukerprofilPortType, new BrukerprofilMapper());
+        final DefaultBrukerprofilService defaultBrukerprofilService = new DefaultBrukerprofilService(brukerprofilPortType, selfTestBrukerprofilPortType, new BrukerprofilMapper());
+
+        return new BrukerprofilServiceBi() {
+
+            @Cacheable("endpointCache")
+            @Override
+            public BrukerprofilResponse hentKontaktinformasjonOgPreferanser(BrukerprofilRequest request) throws HentKontaktinformasjonOgPreferanserPersonIkkeFunnet, HentKontaktinformasjonOgPreferanserSikkerhetsbegrensning {
+                return defaultBrukerprofilService.hentKontaktinformasjonOgPreferanser(request);
+            }
+
+            @Override
+            public void setMapper(BrukerprofilMapper mapper) {
+                defaultBrukerprofilService.setMapper(mapper);
+            }
+
+            @Override
+            public PingResult ping() {
+                return defaultBrukerprofilService.ping();
+            }
+        };
     }
 
 }
