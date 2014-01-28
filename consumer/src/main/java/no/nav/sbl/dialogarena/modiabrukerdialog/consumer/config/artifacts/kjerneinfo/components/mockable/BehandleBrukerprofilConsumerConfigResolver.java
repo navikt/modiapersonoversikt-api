4adcc10a21c65ba134b382b813a36b508f12616e
Jarle Svendsrud <jarle.svendsrud@nav.no>
2014-01-28 10:31:29 +0100

    Forsøk på å fikse cachingproblematikk

--- consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/artifacts/kjerneinfo/components/mockable/BehandleBrukerprofilConsumerConfigResolver.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/artifacts/kjerneinfo/components/mockable/BehandleBrukerprofilConsumerConfigResolver.java
@@ -2,8 +2,13 @@ package no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.artifacts.kjern
 
 import no.nav.behandlebrukerprofil.config.spring.BehandleBrukerprofilConsumerConfig;
 import no.nav.behandlebrukerprofil.consumer.BehandleBrukerprofilServiceBi;
+import no.nav.behandlebrukerprofil.consumer.messages.BehandleBrukerprofilRequest;
+import no.nav.modig.modia.ping.PingResult;
 import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.artifacts.kjerneinfo.components.mockable.mockableimpl.BehandleBrukerProfilPortTypeImpl;
 import no.nav.tjeneste.virksomhet.behandlebrukerprofil.v1.BehandleBrukerprofilPortType;
+import no.nav.tjeneste.virksomhet.behandlebrukerprofil.v1.OppdaterKontaktinformasjonOgPreferanserPersonIkkeFunnet;
+import no.nav.tjeneste.virksomhet.behandlebrukerprofil.v1.OppdaterKontaktinformasjonOgPreferanserSikkerhetsbegrensning;
+import no.nav.tjeneste.virksomhet.behandlebrukerprofil.v1.OppdaterKontaktinformasjonOgPreferanserUgyldigInput;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -11,7 +16,8 @@ import org.springframework.context.annotation.Import;
 import javax.inject.Inject;
 
 import static no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.artifacts.kjerneinfo.components.mockable.MockableContext.KJERNEINFO_KEY;
-import static no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.endpoints.util.InstanceSwitcher.createSwitcher;
+import static no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.endpoints.util.MockUtil.mockErSlaattPaaForKey;
+import static no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.endpoints.util.MockUtil.mockSetupErTillatt;
 import static no.nav.sbl.dialogarena.modiabrukerdialog.mock.config.artifacts.kjerneinfo.BehandleBrukerprofilServiceBiMock.getBehandleBrukerprofilServiceBiMock;
 
 @Configuration
@@ -22,15 +28,34 @@ public class BehandleBrukerprofilConsumerConfigResolver {
 
     @Inject
     private BehandleBrukerprofilPortType behandleBrukerprofilPortType;
+
     @Inject
     private BehandleBrukerprofilPortType selfTestBehandleBrukerprofilPortType;
 
     @Bean
     public BehandleBrukerprofilServiceBi behandleBrukerprofilServiceBi() {
-        BehandleBrukerprofilServiceBi defaultBi = new BehandleBrukerProfilPortTypeImpl(behandleBrukerprofilPortType, selfTestBehandleBrukerprofilPortType).behandleBrukerprofilServiceBi();
-        BehandleBrukerprofilServiceBi alternateBi = getBehandleBrukerprofilServiceBiMock();
+        final BehandleBrukerprofilServiceBi defaultBi = new BehandleBrukerProfilPortTypeImpl(behandleBrukerprofilPortType, selfTestBehandleBrukerprofilPortType).behandleBrukerprofilServiceBi();
+        final BehandleBrukerprofilServiceBi alternateBi = getBehandleBrukerprofilServiceBiMock();
 
-        return createSwitcher(defaultBi, alternateBi, KJERNEINFO_KEY, BehandleBrukerprofilServiceBi.class);
+        return new BehandleBrukerprofilServiceBi() {
+            @Override
+            public void oppdaterKontaktinformasjonOgPreferanser(BehandleBrukerprofilRequest request) throws OppdaterKontaktinformasjonOgPreferanserSikkerhetsbegrensning, OppdaterKontaktinformasjonOgPreferanserPersonIkkeFunnet, OppdaterKontaktinformasjonOgPreferanserUgyldigInput {
+                if (mockSetupErTillatt() && mockErSlaattPaaForKey(KJERNEINFO_KEY)) {
+                    alternateBi.oppdaterKontaktinformasjonOgPreferanser(request);
+                } else {
+                    defaultBi.oppdaterKontaktinformasjonOgPreferanser(request);
+                }
+            }
+
+            @Override
+            public PingResult ping() {
+                if (mockSetupErTillatt() && mockErSlaattPaaForKey(KJERNEINFO_KEY)) {
+                    return alternateBi.ping();
+                }
+                return defaultBi.ping();
+            }
+        };
+//        return createSwitcher(defaultBi, alternateBi, KJERNEINFO_KEY, BehandleBrukerprofilServiceBi.class);
     }
 
 }
--- consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/endpoints/util/MockUtil.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/endpoints/util/MockUtil.java
@@ -5,17 +5,27 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import static com.google.common.collect.Sets.newHashSet;
+import static java.lang.System.getProperty;
 
 public class MockUtil {
 
     public static final String DEFAULT_MOCK_TILLATT = "nei";
 
     public static boolean mockSetupErTillatt(String url) {
-        if(url == null) { return false; }
+        if (url == null) { return false; }
         try {
             return newHashSet(new URL(url.toLowerCase()).getHost().split("\\.")).contains("ja");
         } catch (MalformedURLException e) {
             return false;
         }
     }
+
+    public static boolean mockSetupErTillatt() {
+        return mockSetupErTillatt(getProperty("tillatmocksetup.url"));
+    }
+
+    public static boolean mockErSlaattPaaForKey(String key) {
+        return getProperty(key, "no").equalsIgnoreCase("yes");
+    }
+
 }
