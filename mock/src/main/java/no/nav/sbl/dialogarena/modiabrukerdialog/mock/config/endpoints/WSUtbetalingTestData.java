7c2c352a1ee1564cadf30713906af0cd214a4549
Jarle Svendsrud <jarle.svendsrud@nav.no>
2014-02-10 08:53:06 +0100

    PMD/Czechstyle

--- mock/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/mock/config/endpoints/WSUtbetalingTestData.java
+++ mock/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/mock/config/endpoints/WSUtbetalingTestData.java
@@ -23,24 +23,13 @@ public class WSUtbetalingTestData {
     private static final String KONTO_NR = "12345678900";
     private static final String KONTO_NR2 = "1234 56 78900";
     private static final String NAVN = "Ola Nordmann";
-    private static final String UTBETALT = "UTBETALT";
     private static final String MOTTATT_KONTOFORER = "MOTTATT KONTOFØRER";
     private static final String STATUS_KODE = "0018";
-    private static final Double BELOP = 1000.0;
-    private static final Double SKATTE_PROSENT = -0.35;
-    private static final String GRUNNBELOP = "Grunnbeløp";
-    private static final String FORSKUDDSTREKK_SKATT = "Forskuddstrekk skatt";
-    private static final String FORSKUDDSTREKK = "Forskuddstrekk";
     private static final String SKATT = "Skattetrekk";
-    private static final String DAGPENGER = "Dagpenger";
     private static final String ANDRE = "Andre trekk";
     private static final String AAP = "AAP - grunnsats";
     private static final String KONTANT = "KONTANTSTØTTE";
     private static final String ORDINAER = "ORDINÆR STØNAD";
-    private static final String FORELDREPENGER = "Foreldrepenger";
-    private static final String VALUTA = "NOK";
-    private static final String PERSON_KONTONR = "321321321321";
-    private static final String ANNEN_MOTTAKER_KONTONR = "123123123123";
     private static String fnr;
 
     public static List<WSUtbetaling> getWsUtbetalinger(String fNr, DateTime startDato, DateTime sluttDato) {
@@ -162,13 +151,6 @@ public class WSUtbetalingTestData {
                 .withMottakerId(fnr)
                 .withMottakertypeKode("")
                 .withNavn(NAVN);
-    }
-
-    private static WSMottaker createAnnenMottaker() {
-        return new WSMottaker()
-                .withMottakerId("51321")
-                .withMottakertypeKode("")
-                .withNavn("RIMI");
     }
 
 }
