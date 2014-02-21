fc3c46b1e6a837d9d97a362e176fefcf8fbb5a9e
Henrik Hellerøy <henrik.helleroy@nav.no>
2014-02-21 10:24:06 +0100

    PKFEIL-233: Javascript for å fikse høyden på sidebars når de er høyere enn viewport.

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/pages/personpage/lokal.js
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/pages/personpage/lokal.js
@@ -1,51 +1,55 @@
 jQuery(document).ready(function ($) {
-	'use strict';
+    'use strict';
 
-	createTabHandler("modiabrukerdialog");
+    createTabHandler("modiabrukerdialog");
 
+    resizeSidebar();
 
-	// Lagt til midlertidig for å skjule overskrift ved kun en lamell
-	$('.sidebar > *').on('click', function (e) {
-		e.stopPropagation();
-	});
+    function resizeSidebar(){
+        var $venstre = $('.sidebar-venstre');
+        var $hoyre = $('.sidebar-hoyre');
+        var height = $venstre.outerHeight() > $hoyre.outerHeight() ? $venstre.outerHeight() : $hoyre.outerHeight();
+        $venstre.outerHeight(height);
+        $hoyre.outerHeight(height);
+    }
 
-	if ($('.main-content .lamell').length === 1) {
-		$('.main-content .lamell').first().find('.lamellhode').hide();
-	}
+    if ($('.main-content .lamell').length === 1) {
+        $('.main-content .lamell').first().find('.lamellhode').hide();
+    }
 
-	$('#toggle-personsok').on('click', checkIfToggleAvansertSok);
-	Modig.shortcutListener.on({key: 'A'}, checkIfToggleAvansertSok);
+    $('#toggle-personsok').on('click', checkIfToggleAvansertSok);
+    Modig.shortcutListener.on({key: 'A'}, checkIfToggleAvansertSok);
 
-	Modig.shortcutListener.on({alt: true, keyCode: 114}, focusSearchField); // F3
-	Modig.shortcutListener.on({alt: true, keyCode: 116}, closeResetPerson); // F5
-	Modig.shortcutListener.on({alt: true, keyCode: 117}, focusLamellHead);  // F6
-	Modig.shortcutListener.on({alt: true, keyCode: 118}, closeLamellHead);  // F7
+    Modig.shortcutListener.on({alt: true, keyCode: 114}, focusSearchField); // F3
+    Modig.shortcutListener.on({alt: true, keyCode: 116}, closeResetPerson); // F5
+    Modig.shortcutListener.on({alt: true, keyCode: 117}, focusLamellHead);  // F6
+    Modig.shortcutListener.on({alt: true, keyCode: 118}, closeLamellHead);  // F7
 
-	$('body').on('click', '.lamell .lamellhode > a', function () {
-		if ($('.main > .personsok').is(':visible')) {
-			toggleAvansertSok();
-		}
-	});
+    $('body').on('click', '.lamell .lamellhode > a', function () {
+        if ($('.main > .personsok').is(':visible')) {
+            toggleAvansertSok();
+        }
+    });
 
-	Modig.shortcutListener.on({alt: false, shift: true, keyCode: 13}, gjennomfoerAvansertSok);
+    Modig.shortcutListener.on({alt: false, shift: true, keyCode: 13}, gjennomfoerAvansertSok);
 
-	// IE9 fiks til søk-knapp i personsøk pga bugg med :hover på anet en A-element
-	$('.search-intern input[type=button]').hover(function () {
-		$(this).addClass('hover');
-	}, function () {
-		$(this).removeClass('hover');
-	});
+    // IE9 fiks til søk-knapp i personsøk pga bugg med :hover på anet en A-element
+    $('.search-intern input[type=button]').hover(function () {
+        $(this).addClass('hover');
+    }, function () {
+        $(this).removeClass('hover');
+    });
 
-	/** Registrer ajax loadere **/
+    /** Registrer ajax loadere **/
 
-	Modig.ajaxLoader.register({
-		urlPattern: /utvidetPersonsokForm-hiddenSumbitLink/,
-		placeElement: '#personsokResult',
-		imageUrl: '/modiabrukerdialog/img/ajaxloader/svart/loader_svart_96.gif',
-		css: 'margin-bottom: 20px;'
-	});
+    Modig.ajaxLoader.register({
+        urlPattern: /utvidetPersonsokForm-hiddenSumbitLink/,
+        placeElement: '#personsokResult',
+        imageUrl: '/modiabrukerdialog/img/ajaxloader/svart/loader_svart_96.gif',
+        css: 'margin-bottom: 20px;'
+    });
 
-	var submitFodselsnummer = $('#submitFodselsnummer');
+    var submitFodselsnummer = $('#submitFodselsnummer');
     if (submitFodselsnummer.length > 0) {
         var scaling = 0.8;
         var height = submitFodselsnummer.innerHeight();
@@ -66,7 +70,7 @@ jQuery(document).ready(function ($) {
 });
 
 function focusSearchField() {
-	$('#foedselsnummerInput').focus()
+    $('#foedselsnummerInput').focus()
 }
 
 function focusLamellHead() {
@@ -78,76 +82,76 @@ function closeLamellHead() {
 }
 
 function closeResetPerson() {
-	$('.nullstill-button').click();
+    $('.nullstill-button').click();
 }
 
 function movePersonsok() {
-	var navbar = $('.navbar');
-	var logo = $('.modia-logo');
-	var nullstill = $('INPUT[name=nullstillSok]');
+    var navbar = $('.navbar');
+    var logo = $('.modia-logo');
+    var nullstill = $('INPUT[name=nullstillSok]');
     var error = $('.feedbackPanelERROR');
     error.remove();
 
-	if (navbar[0].style.marginTop == '1%') {
-		toggleAvansertSok();
-		navbar.animate({marginTop: '8%'}, 300, 'linear');
-		navbar.css('marginBottom', '0');
-		logo.css('display', 'block');
-		// Nullstiller søket
-		nullstill.click();
-	} else {
-		if ($('.main').hasClass('hentperson')) {
-			navbar.animate({marginTop: '1%'}, 400, 'linear', toggleAvansertSok);
-			navbar.css('marginBottom', '1.1%');
-		} else {
-			if ($('#personsokPanel').is(':visible')) {
-				toggleAvansertSok();
-			} else {
-				$('#personsok').slideDown(400);
+    if (navbar[0].style.marginTop == '1%') {
+        toggleAvansertSok();
+        navbar.animate({marginTop: '8%'}, 300, 'linear');
+        navbar.css('marginBottom', '0');
+        logo.css('display', 'block');
+        // Nullstiller søket
+        nullstill.click();
+    } else {
+        if ($('.main').hasClass('hentperson')) {
+            navbar.animate({marginTop: '1%'}, 400, 'linear', toggleAvansertSok);
+            navbar.css('marginBottom', '1.1%');
+        } else {
+            if ($('#personsokPanel').is(':visible')) {
+                toggleAvansertSok();
+            } else {
+                $('#personsok').slideDown(400);
 
-			}
-		}
-		logo.css('display', 'none');
-	}
+            }
+        }
+        logo.css('display', 'none');
+    }
 }
 
 function checkIfToggleAvansertSok() {
-	if (!$('#toggle-personsok').hasClass('personsok-movable')) {
-		toggleAvansertSok();
-	} else {
-		movePersonsok();
-	}
+    if (!$('#toggle-personsok').hasClass('personsok-movable')) {
+        toggleAvansertSok();
+    } else {
+        movePersonsok();
+    }
 }
 
 function toggleAvansertSok() {
-	var personsokElement = $('#personsok'), toppmeny = $('.main > .menu-intern'),
-			fornavnInput = $('#utvidetPersonsokForm').find('input[name="fornavn"]'), sokKnapp = $('#toggle-personsok'),
-			dataApne = sokKnapp.attr('data-apne'),
-			dataLukke = sokKnapp.attr('data-lukke');
-	if (personsokElement.is(':visible')) {
-		personsokElement.hide();
-		toppmeny.removeClass('active');
-		sokKnapp.removeClass('active');
-		sokKnapp.attr('title', dataApne);
-		sokKnapp.attr('aria-label', dataApne);
-	} else {
-		personsokElement.show();
-		toppmeny.addClass('active');
-		sokKnapp.addClass('active');
-		sokKnapp.attr('title', dataLukke);
-		sokKnapp.attr('aria-label', dataLukke);
+    var personsokElement = $('#personsok'), toppmeny = $('.main > .menu-intern'),
+        fornavnInput = $('#utvidetPersonsokForm').find('input[name="fornavn"]'), sokKnapp = $('#toggle-personsok'),
+        dataApne = sokKnapp.attr('data-apne'),
+        dataLukke = sokKnapp.attr('data-lukke');
+    if (personsokElement.is(':visible')) {
+        personsokElement.hide();
+        toppmeny.removeClass('active');
+        sokKnapp.removeClass('active');
+        sokKnapp.attr('title', dataApne);
+        sokKnapp.attr('aria-label', dataApne);
+    } else {
+        personsokElement.show();
+        toppmeny.addClass('active');
+        sokKnapp.addClass('active');
+        sokKnapp.attr('title', dataLukke);
+        sokKnapp.attr('aria-label', dataLukke);
 
-		if (fornavnInput.length != 0) {
-			fornavnInput.focus();
-		}
-	}
+        if (fornavnInput.length != 0) {
+            fornavnInput.focus();
+        }
+    }
 }
 
 function gjennomfoerAvansertSok() {
-	var personsokElement = $('.main > .personsok');
-	if (personsokElement.is(":visible")) {
-		$('#utvidetPersonsokForm:visible').submit();
-	}
+    var personsokElement = $('.main > .personsok');
+    if (personsokElement.is(":visible")) {
+        $('#utvidetPersonsokForm:visible').submit();
+    }
 }
 
 function setSessionTimeoutBox() {
@@ -172,130 +176,130 @@ function createTimeoutBox() {
 }
 
 function createTabHandler(application) {
-	var localStorageId = application + ".activeTab";
-	var sessionStorageId = application + ".tabGuid";
-	var isReloadingId = application + ".reloading";
-	var greyBackground = '<div class="wicket-mask-dark" style="z-index: 20000; background-image: none; position: absolute; top: 0px; left: 0px;"></div>';
-	var modalDialog = '<div class="wicket-modal" id="_wicket_window_0" tabindex="-1" role="dialog" style="position: absolute; width: 600px; left: 660px; top: 180.5px; visibility: visible;"><form><div class="w_content_1" ><div class="w_caption" id="_wicket_window_1"><a class="w_close" role="button" id="closeModalButton" style="z-index:1" href="#"></a></div><div id="_wicket_window_2" class="w_content_container" style="overflow: auto; height: 300px;"><div id="content5c"><section class="bekreft-dialog"><h1 class="robust-ikon-hjelp-strek">Du har allerede et vindu med ModiaBrukerDialog åpent. Hvis du fortsetter i dette vinduet så vil du miste ulagret arbeide i det andre vinduet. Ønsker du å fortsette med dette vinduet?</h1><ul><li><submit class="knapp-stor" id="confirmCloseTab" >Avbryt, jeg vil ikke miste ulagret arbeide</submit></li><li><a id="confirmActivateTab" href="#">Fortsett med dette vinduet</a></li></ul></section></div></div></div></form></div>';
+    var localStorageId = application + ".activeTab";
+    var sessionStorageId = application + ".tabGuid";
+    var isReloadingId = application + ".reloading";
+    var greyBackground = '<div class="wicket-mask-dark" style="z-index: 20000; background-image: none; position: absolute; top: 0px; left: 0px;"></div>';
+    var modalDialog = '<div class="wicket-modal" id="_wicket_window_0" tabindex="-1" role="dialog" style="position: absolute; width: 600px; left: 660px; top: 180.5px; visibility: visible;"><form><div class="w_content_1" ><div class="w_caption" id="_wicket_window_1"><a class="w_close" role="button" id="closeModalButton" style="z-index:1" href="#"></a></div><div id="_wicket_window_2" class="w_content_container" style="overflow: auto; height: 300px;"><div id="content5c"><section class="bekreft-dialog"><h1 class="robust-ikon-hjelp-strek">Du har allerede et vindu med ModiaBrukerDialog åpent. Hvis du fortsetter i dette vinduet så vil du miste ulagret arbeide i det andre vinduet. Ønsker du å fortsette med dette vinduet?</h1><ul><li><submit class="knapp-stor" id="confirmCloseTab" >Avbryt, jeg vil ikke miste ulagret arbeide</submit></li><li><a id="confirmActivateTab" href="#">Fortsett med dette vinduet</a></li></ul></section></div></div></div></form></div>';
 
-	/**
-	 * Generates a GUID string, according to RFC4122 standards.
-	 * @returns {String} The generated guid
-	 * @example eff5d22c-74fa-7139-d4e8-6879736b04fa
-	 * @author Slavik Meltser (slavik@meltser.info)
-	 * @link http://slavik.meltser.info/?p=142
-	 */
-	var createGuid = function () {
-		function _p8(s) {
-			var p = (Math.random().toString(16) + "000000000").substr(2, 8);
-			return s ? "-" + p.substr(0, 4) + "-" + p.substr(4, 4) : p;
-		}
+    /**
+     * Generates a GUID string, according to RFC4122 standards.
+     * @returns {String} The generated guid
+     * @example eff5d22c-74fa-7139-d4e8-6879736b04fa
+     * @author Slavik Meltser (slavik@meltser.info)
+     * @link http://slavik.meltser.info/?p=142
+     */
+    var createGuid = function () {
+        function _p8(s) {
+            var p = (Math.random().toString(16) + "000000000").substr(2, 8);
+            return s ? "-" + p.substr(0, 4) + "-" + p.substr(4, 4) : p;
+        }
 
-		return _p8(false) + _p8(true) + _p8(true) + _p8(false);
-	};
+        return _p8(false) + _p8(true) + _p8(true) + _p8(false);
+    };
 
 
-	var getTabGuid = function () {
-		var tabGuid = window.sessionStorage.getItem(sessionStorageId);
-		if (!tabGuid) {
-			tabGuid = createGuid();
-			window.sessionStorage.setItem(sessionStorageId, tabGuid);
-		}
-		return tabGuid;
-	};
+    var getTabGuid = function () {
+        var tabGuid = window.sessionStorage.getItem(sessionStorageId);
+        if (!tabGuid) {
+            tabGuid = createGuid();
+            window.sessionStorage.setItem(sessionStorageId, tabGuid);
+        }
+        return tabGuid;
+    };
 
-	var getCurrentActiveTab = function () {
-		return window.localStorage.getItem(localStorageId);
-	};
+    var getCurrentActiveTab = function () {
+        return window.localStorage.getItem(localStorageId);
+    };
 
-	var setActiveTab = function (tabGuid) {
-		window.localStorage.setItem(localStorageId, tabGuid);
-	};
+    var setActiveTab = function (tabGuid) {
+        window.localStorage.setItem(localStorageId, tabGuid);
+    };
 
-	var activateTab = function () {
-		removeModalDialog();
-		setActiveTab(getTabGuid());
-		setIsReloadingFlag();
-		window.location.reload();
-	};
+    var activateTab = function () {
+        removeModalDialog();
+        setActiveTab(getTabGuid());
+        setIsReloadingFlag();
+        window.location.reload();
+    };
 
-	var closeTab = function () {
-		//funker i IE og Chrome
-		//funker ikke i Firefox
-		window.open('', '_self', '');
-		window.close();
-	};
+    var closeTab = function () {
+        //funker i IE og Chrome
+        //funker ikke i Firefox
+        window.open('', '_self', '');
+        window.close();
+    };
 
-	var isReloading = function () {
-		var isReloading = window.sessionStorage.getItem(isReloadingId);
-		return isReloading === "true";
-	};
+    var isReloading = function () {
+        var isReloading = window.sessionStorage.getItem(isReloadingId);
+        return isReloading === "true";
+    };
 
-	var setIsReloadingFlag = function () {
-		window.sessionStorage.setItem(isReloadingId, "true");
-	};
+    var setIsReloadingFlag = function () {
+        window.sessionStorage.setItem(isReloadingId, "true");
+    };
 
-	var clearIsReloadingFlag = function () {
-		window.sessionStorage.removeItem(isReloadingId);
-	};
+    var clearIsReloadingFlag = function () {
+        window.sessionStorage.removeItem(isReloadingId);
+    };
 
-	var isModalDialogVisible = function () {
-		var backGround = $('.wicket-mask-dark');
-		return backGround.length != 0;
+    var isModalDialogVisible = function () {
+        var backGround = $('.wicket-mask-dark');
+        return backGround.length != 0;
 
-	};
+    };
 
-	var createModalDialog = function () {
-		$("body").append(modalDialog).append(greyBackground);
-		$("#confirmActivateTab").click(activateTab);
-		$('#confirmCloseTab').click(closeTab);
-		$('#closeModalButton').click(closeTab);
-	};
+    var createModalDialog = function () {
+        $("body").append(modalDialog).append(greyBackground);
+        $("#confirmActivateTab").click(activateTab);
+        $('#confirmCloseTab').click(closeTab);
+        $('#closeModalButton').click(closeTab);
+    };
 
-	var removeModalDialog = function () {
-		$('.wicket-mask-dark').remove();
-		$('wicket-modal').remove();
-	};
+    var removeModalDialog = function () {
+        $('.wicket-mask-dark').remove();
+        $('wicket-modal').remove();
+    };
 
-	var storageEventListener = function (e) {
-		var tabGuid;
-		if (e.key === localStorageId) { // bryr oss bare om localStorage events
-			if (!e.newValue) { // hvis ny verdi er null eller tom, ignorer event
-				return;
-			}
-			tabGuid = getTabGuid();
-			if (e.newValue !== tabGuid && !isModalDialogVisible() && !isReloading()) {  // dette er en forskjellig verdi og vi ikke viser dialog fra før, og vi ikke er i en refresh, vis dialog
-				createModalDialog();
-			}
-		}
-	};
+    var storageEventListener = function (e) {
+        var tabGuid;
+        if (e.key === localStorageId) { // bryr oss bare om localStorage events
+            if (!e.newValue) { // hvis ny verdi er null eller tom, ignorer event
+                return;
+            }
+            tabGuid = getTabGuid();
+            if (e.newValue !== tabGuid && !isModalDialogVisible() && !isReloading()) {  // dette er en forskjellig verdi og vi ikke viser dialog fra før, og vi ikke er i en refresh, vis dialog
+                createModalDialog();
+            }
+        }
+    };
 
-	var unloadListener = function () {
-		var tabGuid = getTabGuid();
-		var currentActiveTab = getCurrentActiveTab();
-		if (tabGuid == currentActiveTab) { //hvis det er aktiv tab som lukkes, slette fra localStorage
-			window.localStorage.removeItem(localStorageId);
-		}
-	};
+    var unloadListener = function () {
+        var tabGuid = getTabGuid();
+        var currentActiveTab = getCurrentActiveTab();
+        if (tabGuid == currentActiveTab) { //hvis det er aktiv tab som lukkes, slette fra localStorage
+            window.localStorage.removeItem(localStorageId);
+        }
+    };
 
-	var init = function () {
-		var tabGuid = getTabGuid();
-		var currentActiveTab = getCurrentActiveTab();
-		if (isReloading()) {
-			setActiveTab(tabGuid);
-		} else {
-			if (!currentActiveTab) { //ingen aktiv tab fra før, setter denne som aktiv
-				setActiveTab(tabGuid);
-				currentActiveTab = tabGuid;
-			}
-			if (currentActiveTab !== tabGuid) {//Aktiv tab er en annen en denne. Spør bruker om vi skal bruke denne tab
-				createModalDialog();
-			}
-		}
-		window.addEventListener("storage", storageEventListener);
-		window.addEventListener("unload", unloadListener);
-		clearIsReloadingFlag();
-	};
+    var init = function () {
+        var tabGuid = getTabGuid();
+        var currentActiveTab = getCurrentActiveTab();
+        if (isReloading()) {
+            setActiveTab(tabGuid);
+        } else {
+            if (!currentActiveTab) { //ingen aktiv tab fra før, setter denne som aktiv
+                setActiveTab(tabGuid);
+                currentActiveTab = tabGuid;
+            }
+            if (currentActiveTab !== tabGuid) {//Aktiv tab er en annen en denne. Spør bruker om vi skal bruke denne tab
+                createModalDialog();
+            }
+        }
+        window.addEventListener("storage", storageEventListener);
+        window.addEventListener("unload", unloadListener);
+        clearIsReloadingFlag();
+    };
 
-	init();
+    init();
 }
