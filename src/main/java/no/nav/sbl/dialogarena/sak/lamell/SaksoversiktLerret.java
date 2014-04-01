735ed11663e06f95c9c663ba9f7c380be88819e1
Andreas Baakind <andreas.baakind@nav.no>
2014-04-01 09:51:56 +0200

    GUI og tekst-endringer for SaksoversiktWidget

--- src/main/java/no/nav/sbl/dialogarena/sak/lamell/SaksoversiktLerret.java
+++ src/main/java/no/nav/sbl/dialogarena/sak/lamell/SaksoversiktLerret.java
@@ -2,11 +2,14 @@ package no.nav.sbl.dialogarena.sak.lamell;
 
 import no.nav.modig.modia.lamell.Lerret;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.request.resource.PackageResourceReference;
 
 public class SaksoversiktLerret extends Lerret {
+
+    public static final PackageResourceReference SAKSOVERSIKT_LESS = new PackageResourceReference(SaksoversiktLerret.class, "saksoversikt.less");
+
     public SaksoversiktLerret(String id, String fnr) {
         super(id);
-
 
         add(new Label("saksoversikt.fnr", fnr));
     }
