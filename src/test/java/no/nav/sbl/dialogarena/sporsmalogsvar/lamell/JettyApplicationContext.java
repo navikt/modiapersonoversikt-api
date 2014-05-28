58e02401d5f2d761a2c467820b2c437f63c528d0
Henrik Hellerøy <henrik.helleroy@nav.no>
2014-05-28 16:04:00 +0200

    Fjernet henvendelse-meldinger-tjenestespec. Byttet ut TestContext med mock av ny PortType.

--- src/test/java/no/nav/sbl/dialogarena/sporsmalogsvar/lamell/JettyApplicationContext.java
+++ src/test/java/no/nav/sbl/dialogarena/sporsmalogsvar/lamell/JettyApplicationContext.java
@@ -1,10 +1,13 @@
 package no.nav.sbl.dialogarena.sporsmalogsvar.lamell;
 
+import no.nav.sbl.dialogarena.sporsmalogsvar.context.SporsmalOgSvarContext;
 import no.nav.sbl.dialogarena.sporsmalogsvar.lamell.config.TestContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
-@Import(TestContext.class)
+@Configuration
+@Import({TestContext.class, SporsmalOgSvarContext.class})
 public class JettyApplicationContext {
 
     @Bean
--- src/test/java/no/nav/sbl/dialogarena/sporsmalogsvar/lamell/config/TestContext.java
+++ src/test/java/no/nav/sbl/dialogarena/sporsmalogsvar/lamell/config/TestContext.java
@@ -1,41 +1,36 @@
 package no.nav.sbl.dialogarena.sporsmalogsvar.lamell.config;
 
-import no.nav.tjeneste.domene.brukerdialog.besvare.v1.BesvareHenvendelsePortType;
-import no.nav.tjeneste.domene.brukerdialog.besvare.v1.informasjon.WSSak;
-import no.nav.tjeneste.domene.brukerdialog.besvare.v1.informasjon.WSSporsmalOgSvar;
-import no.nav.tjeneste.domene.brukerdialog.besvare.v1.informasjon.WSSvar;
-import no.nav.tjeneste.domene.brukerdialog.besvare.v1.meldinger.HentSakerRequest;
-import no.nav.tjeneste.domene.brukerdialog.besvare.v1.meldinger.HentSakerResponse;
-import no.nav.tjeneste.domene.brukerdialog.henvendelsemeldinger.v1.HenvendelseMeldingerPortType;
-import no.nav.tjeneste.domene.brukerdialog.henvendelsemeldinger.v1.informasjon.WSMelding;
-import no.nav.tjeneste.domene.brukerdialog.henvendelsemeldinger.v1.informasjon.WSMeldingstype;
-import no.nav.tjeneste.domene.brukerdialog.henvendelsemeldinger.v1.meldinger.HentMeldingListe;
-import no.nav.tjeneste.domene.brukerdialog.henvendelsemeldinger.v1.meldinger.HentMeldingListeResponse;
-import org.apache.commons.collections15.Closure;
-import org.apache.commons.collections15.Transformer;
+import no.nav.melding.domene.brukerdialog.behandlingsinformasjon.v2.XMLBehandlingsinformasjonV2;
+import no.nav.melding.domene.brukerdialog.behandlingsinformasjon.v2.XMLHenvendelseType;
+import no.nav.melding.domene.brukerdialog.behandlingsinformasjon.v2.XMLMetadata;
+import no.nav.melding.domene.brukerdialog.behandlingsinformasjon.v2.XMLMetadataListe;
+import no.nav.melding.domene.brukerdialog.behandlingsinformasjon.v2.XMLReferat;
+import no.nav.melding.domene.brukerdialog.behandlingsinformasjon.v2.XMLSporsmal;
+import no.nav.melding.domene.brukerdialog.behandlingsinformasjon.v2.XMLSvar;
+import no.nav.tjeneste.domene.brukerdialog.henvendelse.informasjon.v2.HenvendelseInformasjonV2PortType;
+import no.nav.tjeneste.domene.brukerdialog.henvendelse.informasjon.v2.meldinger.WSHentHenvendelseListeRequest;
+import no.nav.tjeneste.domene.brukerdialog.henvendelse.informasjon.v2.meldinger.WSHentHenvendelseListeResponse;
+import no.nav.tjeneste.domene.brukerdialog.henvendelse.informasjon.v2.meldinger.WSHentHenvendelseRequest;
+import no.nav.tjeneste.domene.brukerdialog.henvendelse.informasjon.v2.meldinger.WSHentHenvendelseResponse;
 import org.joda.time.DateTime;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.context.annotation.Configuration;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
 import static java.util.Arrays.asList;
-import static no.nav.modig.lang.collections.IterUtils.on;
-import static no.nav.modig.lang.collections.PredicateUtils.containedIn;
-import static no.nav.modig.lang.collections.PredicateUtils.where;
-import static no.nav.tjeneste.domene.brukerdialog.henvendelsemeldinger.v1.informasjon.WSMeldingstype.INNGAENDE;
-import static no.nav.tjeneste.domene.brukerdialog.henvendelsemeldinger.v1.informasjon.WSMeldingstype.UTGAENDE;
-import static org.apache.commons.lang3.StringUtils.abbreviate;
+import static no.nav.melding.domene.brukerdialog.behandlingsinformasjon.v2.XMLHenvendelseType.REFERAT;
+import static no.nav.melding.domene.brukerdialog.behandlingsinformasjon.v2.XMLHenvendelseType.SPORSMAL;
+import static no.nav.melding.domene.brukerdialog.behandlingsinformasjon.v2.XMLHenvendelseType.SVAR;
+import static org.joda.time.DateTime.now;
 
-
+@Configuration
 public class TestContext {
 
-    private static final Logger LOG = LoggerFactory.getLogger(TestContext.class);
-    private static final Integer TRAAD_ID = 1;
+    private static Random idGenerator = new Random();
+    private static int behandlingsid;
+
     private static final String LANG_TEKST = "Lorem ipsum dolor sit amet, " +
             "consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad " +
             "minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure " +
@@ -46,125 +41,67 @@ public class TestContext {
             " dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit" +
             " litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes" +
             " in futurum.";
-    private static final List<WSMelding> MELDINGER = asList(
-            createWSMelding(INNGAENDE, "" + TRAAD_ID, "ARBEIDSSOKER_ARBEIDSAVKLARING_SYKEMELDT", DateTime.now().minusWeeks(2)),
-            createWSMelding(INNGAENDE, "" + (TRAAD_ID + 1), "INTERNASJONALT", DateTime.now().minusWeeks(1)),
-            createWSMelding(UTGAENDE, "" + (TRAAD_ID + 1), "INTERNASJONALT", DateTime.now().minusDays(5), DateTime.now().minusDays(4)),
-            createWSMelding(INNGAENDE, "" + (TRAAD_ID + 2), "HJELPEMIDLER", DateTime.now().minusDays(3)),
-            createWSMelding(UTGAENDE, "" + (TRAAD_ID + 2), "HJELPEMIDLER", DateTime.now().minusHours(5), null),
-            createWSMelding(INNGAENDE, "" + (TRAAD_ID + 3), "HJELPEMIDLER", DateTime.now().minusHours(10)),
-            createWSMelding(INNGAENDE, "" + (TRAAD_ID + 4), "HJELPEMIDLER", DateTime.now().minusMonths(4), DateTime.now().minusMonths(4),
-                    DateTime.now().minusMonths(2), "UFO", "1234568"),
-            createWSMelding(INNGAENDE, "" + (TRAAD_ID + 4), "HJELPEMIDLER", DateTime.now().minusMonths(4).plusDays(1), DateTime.now().minusMonths(4).plusDays(2),
-                    DateTime.now().minusMonths(2), "UFO", "1234568"));
 
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
-        return new PropertySourcesPlaceholderConfigurer();
+    private static final String KORT_TEKST = "Lorem ipsum dolor sit amet, " +
+            "consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad " +
+            "minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure " +
+            "dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto";
+
+    public static final List<XMLBehandlingsinformasjonV2> HENVENDELSER = asList(
+            createXmlBehandlingsinformasjonV2(behandlingsid = idGenerator.nextInt(), SPORSMAL, now().minusWeeks(2), createXMLSporsmal("ARBEIDSSOKER_ARBEIDSAVKLARING_SYKEMELDT", LANG_TEKST)),
+            createXmlBehandlingsinformasjonV2(behandlingsid = idGenerator.nextInt(), SPORSMAL, now().minusWeeks(1), createXMLSporsmal("INTERNASJONALT", LANG_TEKST)),
+            createXmlBehandlingsinformasjonV2(behandlingsid, SVAR, now().minusDays(5), createXMLSvar("INTERNASJONALT", String.valueOf(behandlingsid), now().minusDays(4), KORT_TEKST)),
+            createXmlBehandlingsinformasjonV2(behandlingsid = idGenerator.nextInt(), SPORSMAL, now().minusDays(3), createXMLSporsmal("HJELPEMIDLER", LANG_TEKST)),
+            createXmlBehandlingsinformasjonV2(behandlingsid, SVAR, now().minusHours(5), createXMLSvar("HJELPEMIDLER", String.valueOf(behandlingsid), null, KORT_TEKST)),
+            createXmlBehandlingsinformasjonV2(behandlingsid = idGenerator.nextInt(), SPORSMAL, now().minusMonths(4), createXMLSporsmal("SOSIALE_TJENESTER", LANG_TEKST)),
+            createXmlBehandlingsinformasjonV2(behandlingsid, SVAR, now().minusMonths(4).plusDays(1), createXMLSvar("SOSIALE_TJENESTER", String.valueOf(behandlingsid), now().minusMonths(4).plusDays(3), LANG_TEKST)),
+            createXmlBehandlingsinformasjonV2(behandlingsid, SVAR, now().minusDays(7), createXMLSvar("SOSIALE_TJENESTER", String.valueOf(behandlingsid), null, KORT_TEKST)),
+            createXmlBehandlingsinformasjonV2(behandlingsid = idGenerator.nextInt(), REFERAT, now(), createXMLReferat("HJELPEMIDLER", "TELEFON", null, LANG_TEKST)),
+            createXmlBehandlingsinformasjonV2(behandlingsid = idGenerator.nextInt(), SPORSMAL, now().minusDays(1), createXMLSporsmal("ARBEIDSSOKER_ARBEIDSAVKLARING_SYKEMELDT", LANG_TEKST))
+    );
+
+    private static XMLBehandlingsinformasjonV2 createXmlBehandlingsinformasjonV2(int behandlingsId, XMLHenvendelseType type, DateTime opprettet, XMLMetadata metadata) {
+        return new XMLBehandlingsinformasjonV2()
+                .withHenvendelseType(type.name())
+                .withOpprettetDato(opprettet)
+                .withBehandlingsId(String.valueOf(behandlingsId))
+                .withMetadataListe(
+                        new XMLMetadataListe().withMetadata(metadata));
+    }
+
+    private static XMLSporsmal createXMLSporsmal(String tema, String tekst) {
+        return new XMLSporsmal().withTemagruppe(tema).withFritekst(tekst);
+    }
+
+    private static XMLSvar createXMLSvar(String tema, String sporsmalsId, DateTime lestDato, String fritekst) {
+        return new XMLSvar().withTemagruppe(tema).withSporsmalsId(sporsmalsId).withLestDato(lestDato).withFritekst(fritekst);
+    }
+
+    private static XMLReferat createXMLReferat(String tema, String kanal, DateTime lestDato, String tekst) {
+        return new XMLReferat().withTemagruppe(tema).withKanal(kanal).withLestDato(lestDato).withFritekst(tekst);
     }
 
     @Bean
-    public BesvareHenvendelsePortType besvareHenvendelsePortType() {
-        return new BesvareHenvendelsePortType() {
-
-            @Override
-            public boolean ping() {
-                return true;
-            }
-
-            @Override
-            public void journalforMeldinger(final List<no.nav.tjeneste.domene.brukerdialog.besvare.v1.informasjon.WSMelding> meldinger) {
-                Transformer<no.nav.tjeneste.domene.brukerdialog.besvare.v1.informasjon.WSMelding, String> behandlingsId = new Transformer<no.nav.tjeneste.domene.brukerdialog.besvare.v1.informasjon.WSMelding, String>() {
-                    @Override
-                    public String transform(no.nav.tjeneste.domene.brukerdialog.besvare.v1.informasjon.WSMelding wsMelding) {
-                        return wsMelding.getBehandlingsId();
-                    }
-                };
-                if (!meldinger.isEmpty()) {
-                        on(MELDINGER).filter(where(BEHANDLINGSID, containedIn(on(meldinger).map(behandlingsId).collect()))).forEach(new Closure<WSMelding>() {
-                            @Override
-                            public void execute(WSMelding melding) {
-                                melding.setJournalfortDato(DateTime.now());
-                                melding.setJournalfortTema(meldinger.get(0).getArkivtema());
-                                melding.setJournalfortSaksId(meldinger.get(0).getSaksId());
-                            }
-                        });
-
-                    }
-                }
-
-            @Override
-            public WSSporsmalOgSvar hentSporsmalOgSvar(String oppgaveId) {
-                return new WSSporsmalOgSvar();
-            }
-
-            @Override
-            public HentSakerResponse hentSaker(HentSakerRequest parameters) {
-                return new HentSakerResponse().withSaker(Arrays.asList(
-                        new WSSak().withGenerell(true).withOpprettetDato(DateTime.now()).withSakId("123").withStatuskode("Foobar").withTemakode("UFO"),
-                        new WSSak().withGenerell(false).withOpprettetDato(DateTime.now().minusDays(1)).withSakId("12").withStatuskode("Something").withTemakode("BIL"),
-                        new WSSak().withGenerell(true).withOpprettetDato(DateTime.now().minusDays(3)).withSakId("13").withStatuskode("Ingen Status").withTemakode("BIL"),
-                        new WSSak().withGenerell(false).withOpprettetDato(DateTime.now().minusDays(3)).withSakId("1234").withStatuskode("Ingen Status").withTemakode("BAR")));
-            }
-
-            @Override
-            public void besvarSporsmal(WSSvar svar) {
-                LOG.info("BesvareHenvendelsePortType besvarer spørsmål (svar behandlingId {} \"{}\")", svar.getBehandlingsId(), abbreviate(svar.getFritekst(), 30));
-            }
-
-            public final Transformer<WSMelding, String> BEHANDLINGSID = new Transformer<WSMelding, String>() {
-                @Override
-                public String transform(WSMelding wsMelding) {
-                    return wsMelding.getBehandlingsId();
-                }
-            };
-        };
+    public HenvendelseInformasjonV2PortType henvendelseInformasjonV2PortType() {
+        return createHenvendelseInformasjonV2PortTypeMock();
     }
 
-    @Bean
-    public HenvendelseMeldingerPortType henvendelseMeldingerPortType() {
-
-        return new HenvendelseMeldingerPortType() {
-
+    public static HenvendelseInformasjonV2PortType createHenvendelseInformasjonV2PortTypeMock() {
+        return new HenvendelseInformasjonV2PortType() {
             @Override
-            public HentMeldingListeResponse hentMeldingListe(HentMeldingListe hentMeldingListeRequest) {
-                return new HentMeldingListeResponse().withMelding(MELDINGER);
-            }
-
-            @Override
-            public void merkMeldingSomLest(String behandlingsId) {
+            public WSHentHenvendelseListeResponse hentHenvendelseListe(WSHentHenvendelseListeRequest parameters) {
+                return new WSHentHenvendelseListeResponse().withAny(HENVENDELSER);
             }
 
             @Override
             public void ping() {
             }
 
+            @Override
+            public WSHentHenvendelseResponse hentHenvendelse(WSHentHenvendelseRequest parameters) {
+                return new WSHentHenvendelseResponse();
+            }
         };
-    }
-
-    private static WSMelding createWSMelding(WSMeldingstype type, String traad, String tema, DateTime opprettet) {
-        return createWSMelding(type, traad, tema, opprettet, opprettet);
-    }
-
-    private static WSMelding createWSMelding(WSMeldingstype type, String traad, String tema, DateTime opprettet, DateTime lestdato) {
-        return createWSMelding(type, traad, tema, opprettet, lestdato, null, null, null);
-    }
-
-    // CHECKSTYLE:OFF
-    private static WSMelding createWSMelding(WSMeldingstype type, String traad, String tema, DateTime opprettet, DateTime lestdato,
-                                             DateTime journalfortDato, String journalfortTema, String journalfortSaksid) {
-        Random random = new Random();
-        return new WSMelding()
-                .withBehandlingsId("" + random.nextInt())
-                .withMeldingsType(type)
-                .withOpprettetDato(opprettet)
-                .withTraad(traad)
-                .withTemastruktur(tema)
-                .withLestDato(lestdato)
-                .withTekst(LANG_TEKST)
-                .withJournalfortDato(journalfortDato)
-                .withJournalfortTema(journalfortTema)
-                .withJournalfortSaksId(journalfortSaksid);
     }
 
 }
