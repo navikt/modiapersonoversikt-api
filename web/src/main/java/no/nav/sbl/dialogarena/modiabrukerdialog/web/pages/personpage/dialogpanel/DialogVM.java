13ded7ea385ada4183c5cf0435524d6c89072880
Henrik Hellerøy <henrik.helleroy@nav.no>
2014-06-06 13:33:38 +0200

    Fjernet telefon som default kanalvalg

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/pages/personpage/dialogpanel/DialogVM.java
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/pages/personpage/dialogpanel/DialogVM.java
@@ -2,10 +2,8 @@ package no.nav.sbl.dialogarena.modiabrukerdialog.web.pages.personpage.dialogpane
 
 import no.nav.modig.wicket.component.enhancedtextarea.EnhancedTextAreaModel;
 
-import static no.nav.sbl.dialogarena.modiabrukerdialog.web.pages.personpage.dialogpanel.Kanal.TELEFON;
-
 public class DialogVM extends EnhancedTextAreaModel {
-    public Kanal kanal = TELEFON;
+    public Kanal kanal;
     public String tema;
 
     public String getFritekst() {
