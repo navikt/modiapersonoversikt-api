c6da6457eaf99450ccb2522f1d5fff2a33a173cc
Peter Hemmen <peter.hemmen@nav.no>
2014-06-11 16:45:59 +0200

    Import-fix etter merge

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/pages/personpage/dialogpanel/Dialogpanel.java
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/pages/personpage/dialogpanel/Dialogpanel.java
@@ -10,9 +10,7 @@ import no.nav.tjeneste.domene.brukerdialog.henvendelse.v2.meldinger.WSSendHenven
 import no.nav.tjeneste.domene.brukerdialog.henvendelse.v2.sendhenvendelse.SendHenvendelsePortType;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.form.AjaxSubmitLink;
-import org.apache.wicket.markup.head.IHeaderResponse;
-import org.apache.wicket.markup.head.JavaScriptHeaderItem;
-import org.apache.wicket.markup.head.OnDomReadyHeaderItem;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.form.ChoiceRenderer;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
@@ -23,7 +21,6 @@ import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.CompoundPropertyModel;
-import org.apache.wicket.request.resource.JavaScriptResourceReference;
 import org.apache.wicket.request.resource.PackageResourceReference;
 import org.apache.wicket.util.time.Duration;
 
