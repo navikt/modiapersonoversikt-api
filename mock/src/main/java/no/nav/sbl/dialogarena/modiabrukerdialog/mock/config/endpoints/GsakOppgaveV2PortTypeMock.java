3bd01f48cb24c0c96d2b122f8301d9f90a07c2f3
Henrik Hellerøy <henrik.helleroy@nav.no>
2014-06-18 12:45:19 +0200

    Mock av finnOppgaveListe.

--- mock/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/mock/config/endpoints/GsakOppgaveV2PortTypeMock.java
+++ mock/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/mock/config/endpoints/GsakOppgaveV2PortTypeMock.java
@@ -1,6 +1,9 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.mock.config.endpoints;
 
+import no.nav.virksomhet.gjennomforing.oppgave.v2.Bruker;
+import no.nav.virksomhet.gjennomforing.oppgave.v2.Fagomrade;
 import no.nav.virksomhet.gjennomforing.oppgave.v2.Oppgavetype;
+import no.nav.virksomhet.gjennomforing.oppgave.v2.Status;
 import no.nav.virksomhet.tjenester.oppgave.meldinger.v2.FinnFeilregistrertOppgaveListeRequest;
 import no.nav.virksomhet.tjenester.oppgave.meldinger.v2.FinnFeilregistrertOppgaveListeResponse;
 import no.nav.virksomhet.tjenester.oppgave.meldinger.v2.FinnFerdigstiltOppgaveListeRequest;
@@ -14,6 +17,12 @@ import no.nav.virksomhet.tjenester.oppgave.meldinger.v2.HentOppgaveResponse;
 import no.nav.virksomhet.tjenester.oppgave.v2.binding.HentOppgaveOppgaveIkkeFunnet;
 import no.nav.virksomhet.tjenester.oppgave.v2.binding.Oppgave;
 
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import java.util.GregorianCalendar;
+
+import static org.joda.time.DateTime.now;
+
 public class GsakOppgaveV2PortTypeMock {
 
     public static Oppgave createOppgavePortTypeMock() {
@@ -24,7 +33,7 @@ public class GsakOppgaveV2PortTypeMock {
                 no.nav.virksomhet.gjennomforing.oppgave.v2.Oppgave wsOppgave = new no.nav.virksomhet.gjennomforing.oppgave.v2.Oppgave();
                 wsOppgave.setOppgaveId("1");
                 Oppgavetype oppgavetype = new Oppgavetype();
-                oppgavetype.setKode("kode");
+                oppgavetype.setKode("oppgavetype");
                 wsOppgave.setOppgavetype(oppgavetype);
                 hentOppgaveResponse.setOppgave(wsOppgave);
                 return hentOppgaveResponse;
@@ -32,22 +41,46 @@ public class GsakOppgaveV2PortTypeMock {
 
             @Override
             public FinnOppgaveListeResponse finnOppgaveListe(FinnOppgaveListeRequest finnOppgaveListeRequest) {
-                return null;
+                FinnOppgaveListeResponse finnOppgaveListeResponse = new FinnOppgaveListeResponse();
+                no.nav.virksomhet.gjennomforing.oppgave.v2.Oppgave wsOppgave = new no.nav.virksomhet.gjennomforing.oppgave.v2.Oppgave();
+                wsOppgave.setOppgaveId("1");
+                Oppgavetype wsOppgavetype = new Oppgavetype();
+                wsOppgavetype.setKode("wsOppgavetype");
+                wsOppgave.setOppgavetype(wsOppgavetype);
+                Bruker wsBruker = new Bruker();
+                wsBruker.setBrukerId("11111111111");
+                wsOppgave.setGjelder(wsBruker);
+                Status wsStatus = new Status();
+                wsStatus.setKode("statuskode");
+                wsOppgave.setStatus(wsStatus);
+                Fagomrade wsFagomrade = new Fagomrade();
+                wsFagomrade.setKode("fagomrade");
+                wsOppgave.setFagomrade(wsFagomrade);
+                GregorianCalendar gregorianCalendar = new GregorianCalendar();
+                gregorianCalendar.setTime(now().toDate());
+                try {
+                    wsOppgave.setAktivFra(DatatypeFactory.newInstance().newXMLGregorianCalendar(gregorianCalendar));
+                } catch (DatatypeConfigurationException e) {
+                    e.printStackTrace();
+                }
+                wsOppgave.setVersjon(1);
+                finnOppgaveListeResponse.getOppgaveListe().add(wsOppgave);
+                return finnOppgaveListeResponse;
             }
 
             @Override
             public FinnFerdigstiltOppgaveListeResponse finnFerdigstiltOppgaveListe(FinnFerdigstiltOppgaveListeRequest finnFerdigstiltOppgaveListeRequest) {
-                return null;
+                return new FinnFerdigstiltOppgaveListeResponse();
             }
 
             @Override
             public FinnFeilregistrertOppgaveListeResponse finnFeilregistrertOppgaveListe(FinnFeilregistrertOppgaveListeRequest finnFeilregistrertOppgaveListeRequest) {
-                return null;
+                return new FinnFeilregistrertOppgaveListeResponse();
             }
 
             @Override
             public FinnMappeListeResponse finnMappeListe(FinnMappeListeRequest finnMappeListeRequest) {
-                return null;
+                return new FinnMappeListeResponse();
             }
         };
     }
