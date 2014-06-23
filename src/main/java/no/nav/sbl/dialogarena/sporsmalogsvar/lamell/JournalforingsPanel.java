343cde6aea2a3cfe2904e2d04bcee8d70cc5b8e0
S143006 <Guro.Seternes@nav.no>
2014-06-23 12:33:34 +0200

    Lagt til feilhåndtering i JournalføringsPanelet. Må velge én sak.

--- src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/lamell/JournalforingsPanel.java
+++ src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/lamell/JournalforingsPanel.java
@@ -5,12 +5,14 @@ import no.nav.sbl.dialogarena.sporsmalogsvar.consumer.Sak;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.ajax.markup.html.form.AjaxSubmitLink;
+import org.apache.wicket.feedback.ContainerFeedbackMessageFilter;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.Radio;
 import org.apache.wicket.markup.html.form.RadioGroup;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
@@ -35,6 +37,9 @@ public class JournalforingsPanel extends Panel {
 
         Form<Sak> form = new Form<>("plukkSakForm", valgtSak);
 
+        final FeedbackPanel feedbackPanel = new FeedbackPanel("feedback", new ContainerFeedbackMessageFilter(this));
+        feedbackPanel.setOutputMarkupPlaceholderTag(true);
+
         RadioGroup radioGroup = new RadioGroup<>("sakRadiogruppe", valgtSak);
         radioGroup.setRequired(true);
         radioGroup.add(new ListView<Sak>("saker", saker) {
@@ -55,11 +60,15 @@ public class JournalforingsPanel extends Panel {
                meldingService.journalforTraad(innboksVM.getObject().getValgtTraad(), sak);
                lukkJournalforingsPanel(target);
             }
+
             @Override
-            protected void onError(AjaxRequestTarget target, Form<?> form) {}
+            protected void onError(AjaxRequestTarget target, Form<?> form) {
+                target.add(feedbackPanel);
+            }
+
         };
 
-        form.add(radioGroup, journalforTraad);
+        form.add(feedbackPanel, radioGroup, journalforTraad);
 
         final AjaxLink<InnboksVM> avbryt = new AjaxLink<InnboksVM>("avbrytJournalforing")
         {
