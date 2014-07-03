c4ef22a9a40948877d84c04b1d29482b7dbabd18
Jarle Svendsrud <jarle.svendsrud@nav.no>
2014-07-03 14:40:26 +0200

    Satt opp rigg for testing, lagt inn basic test for Lerret

---
+++ src/test/java/no/nav/sbl/dialogarena/sak/lamell/SaksoversiktLerretTest.java
@@ -0,0 +1,29 @@
+package no.nav.sbl.dialogarena.sak.lamell;
+
+import no.nav.sbl.dialogarena.sak.AbstractWicketTest;
+import no.nav.sbl.dialogarena.sak.config.SaksoversiktService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SaksoversiktLerretTest extends AbstractWicketTest {
+
+    @Mock
+    private SaksoversiktService service;
+    private SaksoversiktLerret lerret;
+
+    @Override
+    protected void setup() {
+        applicationContext.putBean(service);
+
+        lerret = new SaksoversiktLerret("lerret", "");
+        wicketTester.goToPageWith(lerret);
+    }
+
+    @Test
+    public void skalÅpneLerretUtenFeil() {
+
+    }
+}
