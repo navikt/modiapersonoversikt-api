2b4dc0c808676efd913a5d4c0890c9b46e0ca669
Jarle Svendsrud <jarle.svendsrud@nav.no>
2014-07-04 12:56:38 +0200

    La til SpringJUnitClassRunner slik at AppConTest kjører ordentlig og ikke bare gir falsk trygghet

--- src/test/java/no/nav/sbl/dialogarena/sak/config/SaksoversiktServiceConfigTest.java
+++ src/test/java/no/nav/sbl/dialogarena/sak/config/SaksoversiktServiceConfigTest.java
@@ -1,8 +1,11 @@
 package no.nav.sbl.dialogarena.sak.config;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {
         ModiaStubConfig.class,
         SaksoversiktServiceConfig.class
