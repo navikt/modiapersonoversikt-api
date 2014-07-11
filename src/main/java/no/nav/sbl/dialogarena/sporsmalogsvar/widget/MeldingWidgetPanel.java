3a71a5c5e462477306b4e2374d88255c3d80a70e
Henrik Hellerøy <henrik.helleroy@nav.no>
2014-07-11 10:50:17 +0200

    Fikset MeldingWidgetPanel så den ikke kaller getString i konstruktøren. Ryddet også opp i klassen.

--- src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/widget/MeldingWidgetPanel.java
+++ src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/widget/MeldingWidgetPanel.java
@@ -1,6 +1,5 @@
 package no.nav.sbl.dialogarena.sporsmalogsvar.widget;
 
-import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.GenericPanel;
@@ -8,6 +7,7 @@ import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.StringResourceModel;
 
+import static no.nav.modig.wicket.shortcuts.Shortcuts.cssClass;
 import static no.nav.sbl.dialogarena.sporsmalogsvar.common.utils.VisningUtils.getStatusKlasse;
 
 public class MeldingWidgetPanel extends GenericPanel<MeldingVM> {
@@ -16,21 +16,12 @@ public class MeldingWidgetPanel extends GenericPanel<MeldingVM> {
         super(id, new CompoundPropertyModel<>(model));
         setOutputMarkupId(true);
 
-        MeldingVM meldingVM = model.getObject();
-
-        WebMarkupContainer statusContainer = new WebMarkupContainer("status-container");
-
-        WebMarkupContainer status = new WebMarkupContainer("statusIndikator");
-        status.add(new AttributeModifier("class", getStatusKlasse(meldingVM.melding.status)));
-
-        statusContainer.add(
-                status,
-                new Label("melding.status",  new StringResourceModel("widget.${melding.status}", getModel())));
-
         add(
                 new Label("opprettetDato"),
-                new Label("avsender", getString(meldingVM.avsender)),
+                new Label("avsender", new StringResourceModel("${avsender}", getModel())),
                 new Label("melding.temagruppe", new StringResourceModel("${melding.temagruppe}", getModel())),
-                statusContainer);
+                new WebMarkupContainer("statusIndikator")
+                        .add(cssClass(getStatusKlasse(getModelObject().melding.status))),
+                new Label("melding.status",  new StringResourceModel("widget.${melding.status}", getModel())));
     }
 }
