993460744adeb6abb3c741bd3b1589754b79a958
Henrik Hellerøy <henrik.helleroy@nav.no>
2014-07-15 16:18:25 +0200

    Påbegynt styling og funksjonalitet i NyOppgavePanel.

---
+++ src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/lamell/nyoppgave/NyOppgave.java
@@ -0,0 +1,8 @@
+package no.nav.sbl.dialogarena.sporsmalogsvar.lamell.nyoppgave;
+
+import java.io.Serializable;
+
+public class NyOppgave implements Serializable {
+
+    String beskrivelse;
+}
--- src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/lamell/nyoppgave/NyOppgavePanel.java
+++ src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/lamell/nyoppgave/NyOppgavePanel.java
@@ -1,10 +1,61 @@
 package no.nav.sbl.dialogarena.sporsmalogsvar.lamell.nyoppgave;
 
+import no.nav.modig.wicket.events.annotations.RunOnEvents;
+import no.nav.sbl.dialogarena.sporsmalogsvar.lamell.InnboksVM;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.ajax.markup.html.form.AjaxSubmitLink;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextArea;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.model.StringResourceModel;
+
+import static no.nav.sbl.dialogarena.sporsmalogsvar.lamell.Innboks.VALGT_MELDING_EVENT;
 
 public class NyOppgavePanel extends Panel {
-    public NyOppgavePanel(String id) {
+
+    private final CompoundPropertyModel<NyOppgave> nyOppgaveModel;
+
+    public NyOppgavePanel(String id, IModel<InnboksVM> innboksVM) {
         super(id);
         setOutputMarkupPlaceholderTag(true);
+
+        add(new Label("temagruppe", new StringResourceModel("${temagruppe}", this, new PropertyModel<>(innboksVM, "valgtTraad.eldsteMelding.melding"))));
+
+        nyOppgaveModel = new CompoundPropertyModel<>(new NyOppgave());
+        Form<NyOppgave> form = new Form<>("nyoppgaveform", nyOppgaveModel);
+        add(form);
+
+        form.add(new TextArea<String>("beskrivelse"));
+
+        form.add(new AjaxSubmitLink("opprettoppgave") {
+            @Override
+            protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+                lukkNyOppgavePanel(target);
+                nullstillSkjema();
+            }
+        });
+
+        add(new AjaxLink<Void>("avbrytlink") {
+            @Override
+            public void onClick(AjaxRequestTarget target) {
+                lukkNyOppgavePanel(target);
+            }
+        });
     }
+
+    private void nullstillSkjema() {
+        nyOppgaveModel.setObject(new NyOppgave());
+    }
+
+    @RunOnEvents(VALGT_MELDING_EVENT)
+    private void lukkNyOppgavePanel(AjaxRequestTarget target) {
+        this.setVisibilityAllowed(false);
+        target.add(this);
+    }
+
 }
