0f467de6189b2c7c37c032ff84bf99e1d0b8e23b
Guro Seternes <Guro.Seternes@nav.no>
2014-07-21 10:39:16 +0200

    PK-14132: Laget ny test til SaksbehandlerPanel.

--- web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/panels/saksbehandlerpanel/SaksbehandlerPanelTest.java
+++ web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/panels/saksbehandlerpanel/SaksbehandlerPanelTest.java
@@ -1,21 +1,39 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.web.panels.saksbehandlerpanel;
 
 import no.nav.modig.core.context.StaticSubjectHandler;
+import no.nav.modig.wicket.test.matcher.BehaviorMatchers;
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.config.mock.HentPersonPanelMockContext;
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.pages.WicketPageTest;
+import org.apache.wicket.ajax.AjaxEventBehavior;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import static no.nav.modig.wicket.test.matcher.ComponentMatchers.*;
+
 @ContextConfiguration(classes = {HentPersonPanelMockContext.class})
 @RunWith(SpringJUnit4ClassRunner.class)
 public class SaksbehandlerPanelTest extends WicketPageTest {
 
+    @Before
+    public void setUp(){
+        System.setProperty(StaticSubjectHandler.SUBJECTHANDLER_KEY, StaticSubjectHandler.class.getName());
+    }
+
     @Test
     public void skalStarteSaksbehandlerPanelUtenFeil() {
-        System.setProperty(StaticSubjectHandler.SUBJECTHANDLER_KEY, StaticSubjectHandler.class.getName());
         wicket.goToPageWith(new SaksbehandlerPanel("saksbehandlerPanel"));
     }
 
+    @Test
+    public void sjekkAtNavEnhetPanelVisesVedKlikkPaNavIdentKnapp() {
+        wicket
+                .goToPageWith(new SaksbehandlerPanel("saksbehandlerPanel"))
+                .should().containComponent(thatIsInvisible().and(ofType(WebMarkupContainer.class)).and(withId("valgContainer")))
+                .executeAjaxBehaviors(BehaviorMatchers.ofType(AjaxEventBehavior.class))
+                .should().containComponent(thatIsVisible().and(ofType(WebMarkupContainer.class)).and(withId("valgContainer")));
+    }
 }
