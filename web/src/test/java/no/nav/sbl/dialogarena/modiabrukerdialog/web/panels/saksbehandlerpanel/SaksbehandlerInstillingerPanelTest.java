b99e1407fc94a17b5e2ec25a432b7b02f4159f2a
Guro Seternes <Guro.Seternes@nav.no>
2014-07-23 09:22:09 +0200

    PK-14132: Lagt inn ny test.

--- web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/panels/saksbehandlerpanel/SaksbehandlerInstillingerPanelTest.java
+++ web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/panels/saksbehandlerpanel/SaksbehandlerInstillingerPanelTest.java
@@ -2,6 +2,8 @@ package no.nav.sbl.dialogarena.modiabrukerdialog.web.panels.saksbehandlerpanel;
 
 import no.nav.modig.core.context.StaticSubjectHandler;
 import no.nav.modig.wicket.test.matcher.BehaviorMatchers;
+import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.domain.AnsattEnhet;
+import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.services.AnsattService;
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.config.mock.KjerneinfoPepMockContext;
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.config.mock.SaksbehandlerInstillingerPanelMockContext;
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.pages.WicketPageTest;
@@ -13,16 +15,21 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static no.nav.modig.wicket.test.matcher.ComponentMatchers.ofType;
-import static no.nav.modig.wicket.test.matcher.ComponentMatchers.thatIsInvisible;
-import static no.nav.modig.wicket.test.matcher.ComponentMatchers.thatIsVisible;
-import static no.nav.modig.wicket.test.matcher.ComponentMatchers.withId;
+import javax.inject.Inject;
+import java.util.Arrays;
+import java.util.List;
+
+import static no.nav.modig.wicket.test.matcher.ComponentMatchers.*;
+import static org.mockito.Mockito.when;
 
 @ContextConfiguration(classes = {
         KjerneinfoPepMockContext.class,
         SaksbehandlerInstillingerPanelMockContext.class})
 @RunWith(SpringJUnit4ClassRunner.class)
 public class SaksbehandlerInstillingerPanelTest extends WicketPageTest {
+
+    @Inject
+    private AnsattService ansattService;
 
     @Before
     public void setUp(){
@@ -35,11 +42,24 @@ public class SaksbehandlerInstillingerPanelTest extends WicketPageTest {
     }
 
     @Test
-    public void sjekkAtNavEnhetPanelVisesVedKlikkPaNavIdentKnapp() {
+    public void sjekkAtSaksbehandlerPanelVisesVedKlikkPaNavIdentKnapp() {
         wicket
                 .goToPageWith(new SaksbehandlerInstillingerPanel("saksbehandlerPanel"))
                 .should().containComponent(thatIsInvisible().and(ofType(WebMarkupContainer.class)).and(withId("valgContainer")))
                 .executeAjaxBehaviors(BehaviorMatchers.ofType(AjaxEventBehavior.class))
                 .should().containComponent(thatIsVisible().and(ofType(WebMarkupContainer.class)).and(withId("valgContainer")));
+    }
+
+    @Test
+    public void sjekkAtSaksbehandlerPanelSkjulesVedKlikkPaVelgKnapp() {
+        List<AnsattEnhet> ansattEnheter = Arrays.asList(new AnsattEnhet("111", "Grunerløkka"), new AnsattEnhet("222", "Torshov"));
+        when(ansattService.hentEnhetsliste()).thenReturn(ansattEnheter);
+        wicket
+                .goToPageWith(new SaksbehandlerInstillingerPanel("saksbehandlerPanel"))
+                .executeAjaxBehaviors(BehaviorMatchers.ofType(AjaxEventBehavior.class))
+                .inForm(withId("enhetsform"))
+                .select("enhet", 1)
+                .submitWithAjaxButton(withId("velg"))
+                .should().containComponent(thatIsInvisible().and(ofType(WebMarkupContainer.class)).and(withId("valgContainer")));
     }
 }
