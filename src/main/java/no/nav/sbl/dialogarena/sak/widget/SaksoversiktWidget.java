d7832a5a6c6b596dc076ff5523c709bc514748cf
Holger Ludvigsen <holger.ludvigsen@nav.no>
2014-07-29 12:21:33 +0200

    PK-9457: La til feilmelding ved feil i tjenesten fra sak & behandling + La til egen tekst om bruker har 0 saker

--- src/main/java/no/nav/sbl/dialogarena/sak/widget/SaksoversiktWidget.java
+++ src/main/java/no/nav/sbl/dialogarena/sak/widget/SaksoversiktWidget.java
@@ -1,19 +1,30 @@
 package no.nav.sbl.dialogarena.sak.widget;
 
+import no.nav.modig.core.exception.ApplicationException;
+import no.nav.modig.core.exception.SystemException;
+import no.nav.modig.modia.model.FeedItemVM;
 import no.nav.modig.modia.widget.FeedWidget;
+import no.nav.modig.modia.widget.panels.ErrorListing;
+import no.nav.modig.modia.widget.panels.GenericListing;
 import no.nav.sbl.dialogarena.sak.service.SaksoversiktService;
 import no.nav.sbl.dialogarena.sak.viewdomain.widget.TemaVM;
 import org.apache.wicket.Component;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.LoadableDetachableModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import java.util.List;
+
+import static java.util.Arrays.asList;
 
 public class SaksoversiktWidget extends FeedWidget<TemaVM> {
 
     @Inject
     private SaksoversiktService saksoversiktService;
+
+    private static final Logger log = LoggerFactory.getLogger(SaksoversiktWidget.class);
 
     public SaksoversiktWidget(String id, String initial, String fnr) {
         super(id, initial, true);
@@ -21,11 +32,17 @@ public class SaksoversiktWidget extends FeedWidget<TemaVM> {
         setDefaultModel(lagLDMforTema(fnr));
     }
 
-    private LoadableDetachableModel<List<TemaVM>> lagLDMforTema(final String fnr) {
-        return new LoadableDetachableModel<List<TemaVM>>() {
+    private LoadableDetachableModel<List<? extends FeedItemVM>> lagLDMforTema(final String fnr) {
+        return new LoadableDetachableModel<List<? extends FeedItemVM>>() {
             @Override
-            protected List<TemaVM> load() {
-                return saksoversiktService.hentTemaer(fnr);
+            protected List<? extends FeedItemVM> load() {
+                try {
+                    List<? extends FeedItemVM> temaVMList = saksoversiktService.hentTemaer(fnr);
+                    return temaVMList.isEmpty() ? asList(new GenericListing(getString("ingen.saker"))) : temaVMList;
+                } catch (ApplicationException | SystemException e) {
+                    log.warn("Feilet ved henting av saksbehandlingsinformasjon for fnr {}", fnr, e);
+                    return asList(new ErrorListing(getString("saker.feilet")));
+                }
             }
         };
     }
