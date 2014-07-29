9419e3368f99aae6837d88e87d945c80d1712466
Guro Seternes <Guro.Seternes@nav.no>
2014-07-29 09:51:11 +0200

    PK-12035. Laget ValgtEnhetService og brukt denne til å legge på journalfoerendeEnhetREF i journalforingAvHenvendelse mot JOARK.

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/ApplicationContextBeans.java
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/ApplicationContextBeans.java
@@ -2,6 +2,7 @@ package no.nav.sbl.dialogarena.modiabrukerdialog.web.config;
 
 import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.CacheConfig;
 import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.ConsumerContext;
+import no.nav.sbl.dialogarena.modiabrukerdialog.web.service.SaksbehandlerInnstillingerService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -18,5 +19,8 @@ public class ApplicationContextBeans {
     public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
         return new PropertySourcesPlaceholderConfigurer();
     }
+
+    @Bean
+    public SaksbehandlerInnstillingerService saksbehandlerInnstillingerService() { return new SaksbehandlerInnstillingerService();}
 
 }
