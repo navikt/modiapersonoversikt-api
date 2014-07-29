87b2a66981e1a4e455edf9721d1b562b417e4203
Jarle Svendsrud <jarle.svendsrud@nav.no>
2014-07-29 16:22:06 +0200

    Lagt på felter for dato i panelet

--- src/main/java/no/nav/sbl/dialogarena/sak/lamell/BehandlingsPanel.java
+++ src/main/java/no/nav/sbl/dialogarena/sak/lamell/BehandlingsPanel.java
@@ -1,13 +1,35 @@
 package no.nav.sbl.dialogarena.sak.lamell;
 
 import no.nav.sbl.dialogarena.sak.viewdomain.lamell.GenerellBehandling;
+import org.apache.wicket.Component;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.Model;
+
+import java.util.Locale;
+
+import static java.lang.String.format;
+import static no.nav.modig.wicket.conditional.ConditionalUtils.visibleIf;
+import static no.nav.sbl.dialogarena.sak.viewdomain.lamell.GenerellBehandling.BehandlingsStatus.AVSLUTTET;
+import static org.apache.wicket.model.Model.of;
 
 public class BehandlingsPanel extends Panel {
 
     public BehandlingsPanel(String id, Model<GenerellBehandling> behandlingModel) {
         super(id, behandlingModel);
+
+        GenerellBehandling behandling = behandlingModel.getObject();
+        String opprettetDato = behandling.opprettetDato.toString("d. MMMM yyyy, HH:mm", new Locale("nb", "no"));
+        add(
+                new Label("opprettet-dato", format("Behandling opprettet: ", opprettetDato)),
+                lagAvsluttetDato(behandling)
+        );
+    }
+
+    private Component lagAvsluttetDato(GenerellBehandling behandling) {
+        return new Label("avsluttet-dato",
+                format("Behandling avsluttet: ", behandling.behandlingDato.toString("d. MMMM yyyy, HH:mm", new Locale("nb", "no"))))
+                .add(visibleIf(of(behandling.behandlingsStatus.equals(AVSLUTTET))));
     }
 
 }
--- src/test/java/no/nav/sbl/dialogarena/sak/lamell/BehandlingsPanelTest.java
+++ src/test/java/no/nav/sbl/dialogarena/sak/lamell/BehandlingsPanelTest.java
@@ -1,9 +1,15 @@
 package no.nav.sbl.dialogarena.sak.lamell;
 
 import no.nav.sbl.dialogarena.sak.AbstractWicketTest;
+import no.nav.sbl.dialogarena.sak.viewdomain.lamell.GenerellBehandling;
+import org.joda.time.DateTime;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static no.nav.sbl.dialogarena.sak.viewdomain.lamell.GenerellBehandling.BehandlingsStatus.OPPRETTET;
+import static org.apache.wicket.model.Model.of;
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class BehandlingsPanelTest extends AbstractWicketTest {
@@ -12,7 +18,12 @@ public class BehandlingsPanelTest extends AbstractWicketTest {
 
     @Override
     protected void setup() {
-        behandlingsPanel = new BehandlingsPanel("id", null);
+        DateTime dato = new DateTime();
+        behandlingsPanel = new BehandlingsPanel("id", of(new GenerellBehandling()
+                .withOpprettetDato(dato)
+                .withBehandlingsDato(dato)
+                .withBehandlingStatus(OPPRETTET)
+        ));
         wicketTester.goToPageWith(behandlingsPanel);
     }
 
