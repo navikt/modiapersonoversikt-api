9325084cbd9d08f251eafa45ac7a19b88219013c
Jarle Svendsrud <jarle.svendsrud@nav.no>
2014-08-05 13:34:07 +0200

    Fikset støtte for kodeverk i test slik at OversiktLerretTest kjører

--- web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/pages/lameller/oversikt/OversiktLerretTest.java
+++ web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/pages/lameller/oversikt/OversiktLerretTest.java
@@ -1,10 +1,11 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.web.pages.lameller.oversikt;
 
 import no.nav.modig.modia.widget.LenkeWidget;
+import no.nav.sbl.dialogarena.modiabrukerdialog.mock.config.endpoints.KodeverkV2PortTypeMock;
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.WicketPageTest;
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.config.mock.EndpointMockContext;
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.config.mock.SykepengerWidgetMockContext;
-import no.nav.sbl.dialogarena.sak.service.SaksoversiktService;
+import no.nav.sbl.dialogarena.sak.config.SaksoversiktServiceConfig;
 import no.nav.sbl.dialogarena.sporsmalogsvar.context.SporsmalOgSvarContext;
 import no.nav.sbl.dialogarena.utbetaling.lamell.context.UtbetalingLamellContext;
 import no.nav.sykmeldingsperioder.widget.SykepengerWidget;
@@ -22,12 +23,14 @@ import static no.nav.modig.wicket.test.matcher.ComponentMatchers.withId;
         SykepengerWidgetMockContext.class,
         UtbetalingLamellContext.class,
         SporsmalOgSvarContext.class,
-        SaksoversiktService.class,
+        SaksoversiktServiceConfig.class,
+        KodeverkV2PortTypeMock.class
 })
 public class OversiktLerretTest extends WicketPageTest {
 
     @Test
     public void skalOppretteOversikt() {
+        System.setProperty("kodeverkendpoint.v2.url", "something");
         wicket.goToPageWith(new OversiktLerret("oversiktId", "fnr"))
                 .should().containComponent(withId("lenker").and(ofType(LenkeWidget.class)))
                 .should().containComponent(withId("sykepenger").and(ofType(SykepengerWidget.class)));
