a11d54891779d31a0525642be184eea56f7e6eab
Jarle Svendsrud <jarle.svendsrud@nav.no>
2014-08-06 10:12:58 +0200

    Flyttet kodeverkconfig ut i egen klasse som blir importert av toppnivåconfig

--- src/main/java/no/nav/sbl/dialogarena/sak/config/SaksoversiktServiceConfig.java
+++ src/main/java/no/nav/sbl/dialogarena/sak/config/SaksoversiktServiceConfig.java
@@ -1,29 +1,20 @@
 package no.nav.sbl.dialogarena.sak.config;
 
-import no.nav.sbl.dialogarena.common.kodeverk.JsonKodeverk;
-import no.nav.sbl.dialogarena.common.kodeverk.Kodeverk;
-import no.nav.sbl.dialogarena.sak.service.BulletProofKodeverkService;
 import no.nav.sbl.dialogarena.sak.service.SaksoversiktService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 
 @Configuration
+@Import({
+        KodeverkConfig.class
+})
 public class SaksoversiktServiceConfig {
 
     @Bean
     public SaksoversiktService saksoversiktService() {
         return new SaksoversiktService();
-    }
-
-    @Bean
-    public BulletProofKodeverkService bulletProofKodeverkService() {
-        return new BulletProofKodeverkService();
-    }
-
-    @Bean
-    public Kodeverk kodeverk() {
-        return new JsonKodeverk(getClass().getResourceAsStream("/kodeverk.json"));
     }
 
 }
