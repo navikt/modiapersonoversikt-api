10a04d1496aacaadf62093cf0103407ad6904981
Jarle Svendsrud <jarle.svendsrud@nav.no>
2014-08-07 11:55:46 +0200

    Stubconfig som brukes i tester returnerer nå dummydata fra sakogbehandling i stedet for tom respons

--- src/test/java/no/nav/sbl/dialogarena/sak/config/ModiaStubConfig.java
+++ src/test/java/no/nav/sbl/dialogarena/sak/config/ModiaStubConfig.java
@@ -4,9 +4,14 @@ import no.nav.sbl.dialogarena.common.kodeverk.KodeverkClient;
 import no.nav.tjeneste.domene.brukerdialog.henvendelsesoknader.v1.HenvendelseSoknaderPortType;
 import no.nav.tjeneste.virksomhet.aktoer.v1.AktoerPortType;
 import no.nav.tjeneste.virksomhet.sakogbehandling.v1.SakOgBehandlingPortType;
+import no.nav.tjeneste.virksomhet.sakogbehandling.v1.meldinger.FinnSakOgBehandlingskjedeListeRequest;
+import no.nav.tjeneste.virksomhet.sakogbehandling.v1.meldinger.FinnSakOgBehandlingskjedeListeResponse;
 import org.springframework.context.annotation.Bean;
 
+import static no.nav.sbl.dialogarena.sak.mock.SakOgBehandlingMocks.createWSSak;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Denne konfigen emulerer MODIA sin og tilbyr
@@ -21,7 +26,10 @@ public class ModiaStubConfig {
 
     @Bean
     public SakOgBehandlingPortType sakOgBehandlingPortType() {
-        return mock(SakOgBehandlingPortType.class);
+        SakOgBehandlingPortType mock = mock(SakOgBehandlingPortType.class);
+        when(mock.finnSakOgBehandlingskjedeListe(any(FinnSakOgBehandlingskjedeListeRequest.class)))
+                .thenReturn(new FinnSakOgBehandlingskjedeListeResponse().withSak(createWSSak()));
+        return mock;
     }
 
     @Bean
