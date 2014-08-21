599d140ed1a457b50dee49f5908aadabf9042859
Kjell Arne Brødreskift <kjell.arne.brodreskift@nav.no>
2014-08-21 15:50:01 +0200

    Lagt til flexbox-styling for nye nettlesere. Fallback-css for ie9.

--- web/src/main/resources/no/nav/sbl/dialogarena/modiabrukerdialog/web/js/flexbox-ie9.js
+++ web/src/main/resources/no/nav/sbl/dialogarena/modiabrukerdialog/web/js/flexbox-ie9.js
@@ -1,22 +1,17 @@
 $(function() {
-	setHeight();
+	setFlexHeight();
 	$(window).on('resize', function() {
-		setHeight();
+		setFlexHeight();
 	});
 	$(document).on('resize', '.flexbox.row [class*=flex]', function() {
-		setHeight();
+		setFlexHeight();
 	});
 });
 
-function setHeight() {
+function setFlexHeight() {
 	var $flexbox = $('.flexbox.row');
 	var $childs  = $flexbox.find('[class*=flex]');
 	$childs.css('height', 'auto');
-	$flexbox.find('.flex1').css('width', '25%');
-	$flexbox.find('.flex2').css('width', '50%');
-	var height = $flexbox.height();
-	$childs.css({ 'float': 'left', 'height': height });
-	var $flik = $('.saksbehandlerpanelflik');
-	var left = ($(window).width() - $flik.width()) / 2;
-	$flik.css('marginLeft', left);
+	$flexbox.find('.flex1').css({ 'display': 'table-cell', 'width': '25%' });
+	$flexbox.find('.flex2').css({ 'display': 'table-cell', 'width': '50%', 'verticalAlign': 'top' });
 }
