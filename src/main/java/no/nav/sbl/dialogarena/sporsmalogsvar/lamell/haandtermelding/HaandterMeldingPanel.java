d4db2e71d5b24fba919d76329bee0f051a432b16
Frithjof Nerdrum <frithjof.nerdrum@nav.no>
2014-08-22 13:49:13 +0200

    Lagt til ikon for MerkePanel. Lagt til piler for alle animerte paneler.

--- src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/lamell/haandtermelding/HaandterMeldingPanel.java
+++ src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/lamell/haandtermelding/HaandterMeldingPanel.java
@@ -5,22 +5,27 @@ import no.nav.sbl.dialogarena.sporsmalogsvar.lamell.InnboksVM;
 import no.nav.sbl.dialogarena.sporsmalogsvar.lamell.haandtermelding.journalforing.JournalforingsPanel;
 import no.nav.sbl.dialogarena.sporsmalogsvar.lamell.haandtermelding.merke.MerkePanel;
 import no.nav.sbl.dialogarena.sporsmalogsvar.lamell.haandtermelding.nyoppgave.NyOppgavePanel;
+import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.event.Broadcast;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.PropertyModel;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.Arrays.asList;
 import static no.nav.modig.modia.events.InternalEvents.SVAR_PAA_MELDING;
 import static no.nav.modig.wicket.conditional.ConditionalUtils.enabledIf;
+import static no.nav.modig.wicket.conditional.ConditionalUtils.hasCssClassIf;
 import static no.nav.modig.wicket.model.ModelUtils.not;
 
 public class HaandterMeldingPanel extends Panel {
 
     private final List<AnimertPanel> meldingHaandteringsPaneler;
+    private List<WebMarkupContainer> piler = new ArrayList<>();
 
     public HaandterMeldingPanel(String id, final InnboksVM innboksVM) {
         super(id);
@@ -49,6 +54,11 @@ public class HaandterMeldingPanel extends Panel {
             }
         };
         journalforLink.add(enabledIf(not(new PropertyModel<Boolean>(innboksVM, "valgtTraad.nyesteMelding.nyesteMeldingISinJournalfortgruppe"))));
+        WebMarkupContainer journalforPil = new WebMarkupContainer("journalfor-pil");
+        journalforPil.setOutputMarkupId(true);
+        journalforPil.add(hasCssClassIf("opp", new PropertyModel<Boolean>(journalforingsPanel, "visibilityAllowed")));
+        journalforPil.add(hasCssClassIf("ned", not(new PropertyModel<Boolean>(journalforingsPanel, "visibilityAllowed"))));
+        piler.add(journalforPil);
 
         AjaxLink nyOppgaveLink = new AjaxLink("nyoppgave") {
             @Override
@@ -57,6 +67,11 @@ public class HaandterMeldingPanel extends Panel {
             }
         };
         nyOppgaveLink.add(enabledIf(new PropertyModel<Boolean>(innboksVM, "valgtTraad.erBehandlet()")));
+        WebMarkupContainer nyOppgavePil = new WebMarkupContainer("ny-oppgave-pil");
+        nyOppgavePil.setOutputMarkupId(true);
+        nyOppgavePil.add(hasCssClassIf("opp", new PropertyModel<Boolean>(nyOppgavePanel, "visibilityAllowed")));
+        nyOppgavePil.add(hasCssClassIf("ned", not(new PropertyModel<Boolean>(nyOppgavePanel, "visibilityAllowed"))));
+        piler.add(nyOppgavePil);
 
         AjaxLink merkeLink = new AjaxLink("merke") {
             @Override
@@ -64,8 +79,14 @@ public class HaandterMeldingPanel extends Panel {
                 togglePaneler(target, MerkePanel.class);
             }
         };
+        WebMarkupContainer merkePil = new WebMarkupContainer("merke-pil");
+        merkePil.setOutputMarkupId(true);
+        merkePil.add(hasCssClassIf("opp", new PropertyModel<Boolean>(merkePanel, "visibilityAllowed")));
+        merkePil.add(hasCssClassIf("ned", not(new PropertyModel<Boolean>(merkePanel, "visibilityAllowed"))));
+        piler.add(merkePil);
 
-        add(besvarLink, journalforLink, nyOppgaveLink, merkeLink, journalforingsPanel, nyOppgavePanel, merkePanel);
+        add(besvarLink, journalforLink, nyOppgaveLink, merkeLink, journalforPil, nyOppgavePil, merkePil,
+                journalforingsPanel, nyOppgavePanel, merkePanel);
     }
 
     private void togglePaneler(AjaxRequestTarget target, Class synligPanelType) {
@@ -76,5 +97,6 @@ public class HaandterMeldingPanel extends Panel {
                 panel.lukkPanel(target);
             }
         }
+        target.add(piler.toArray(new Component[piler.size()]));
     }
 }
