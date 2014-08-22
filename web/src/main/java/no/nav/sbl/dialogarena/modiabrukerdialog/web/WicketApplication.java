1b07dfceb7946b815a330e4ca623c31230fa5755
Kjell Arne Brødreskift <kjell.arne.brodreskift@nav.no>
2014-08-22 07:13:29 +0200

    Endret innrykk i java-filer til 4 ordmellomrom...

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/WicketApplication.java
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/WicketApplication.java
@@ -49,143 +49,143 @@ import static org.apache.wicket.util.time.Duration.ONE_SECOND;
 
 public class WicketApplication extends WebApplication {
 
-	@Inject
-	private ApplicationContext applicationContext;
+    @Inject
+    private ApplicationContext applicationContext;
 
-	@Resource(name = "kjerneinfoPep")
-	private EnforcementPoint kjerneinfoPep;
+    @Resource(name = "kjerneinfoPep")
+    private EnforcementPoint kjerneinfoPep;
 
-	public static WicketApplication get() {
-		return (WicketApplication) Application.get();
-	}
+    public static WicketApplication get() {
+        return (WicketApplication) Application.get();
+    }
 
-	public ApplicationContext getApplicationContext() {
-		return applicationContext;
-	}
+    public ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
 
-	@Override
-	public Class<? extends Page> getHomePage() {
-		return HentPersonPage.class;
-	}
+    @Override
+    public Class<? extends Page> getHomePage() {
+        return HentPersonPage.class;
+    }
 
-	@Override
-	protected void init() {
-		super.init();
-		Locale.setDefault(new Locale("nb", "no"));
-		if (usesDevelopmentConfig()) {
-			getResourceSettings().setResourcePollFrequency(ONE_SECOND);
-		}
+    @Override
+    protected void init() {
+        super.init();
+        Locale.setDefault(new Locale("nb", "no"));
+        if (usesDevelopmentConfig()) {
+            getResourceSettings().setResourcePollFrequency(ONE_SECOND);
+        }
 
-		configureFrontend();
+        configureFrontend();
 
-		// Innstillinger vi bør ha
-		getRequestCycleSettings().setResponseRequestEncoding("UTF-8");
+        // Innstillinger vi bør ha
+        getRequestCycleSettings().setResponseRequestEncoding("UTF-8");
 
-		setMarkupSettings();
+        setMarkupSettings();
 
-		CompoundAuthorizationStrategy compoundAuthorizationStrategy = new CompoundAuthorizationStrategy();
-		compoundAuthorizationStrategy.add(new BehaviorPolicyAuthorizationStrategy(kjerneinfoPep));
-		getSecuritySettings().setAuthorizationStrategy(compoundAuthorizationStrategy);
+        CompoundAuthorizationStrategy compoundAuthorizationStrategy = new CompoundAuthorizationStrategy();
+        compoundAuthorizationStrategy.add(new BehaviorPolicyAuthorizationStrategy(kjerneinfoPep));
+        getSecuritySettings().setAuthorizationStrategy(compoundAuthorizationStrategy);
 
-		new ApplicationSettingsConfig().withUtf8Properties(true).configure(this);
+        new ApplicationSettingsConfig().withUtf8Properties(true).configure(this);
 
-		Application.get().getRequestLoggerSettings().setRequestLoggerEnabled(true);
-
-
-		new ModiaApplicationConfigurator()
-				.withExceptionHandler(true)
-				.configure(this);
-
-		mountPages();
-
-		setSpringComponentInjector();
-
-		Datoformat.brukLocaleFra(LocaleFromWicketSession.INSTANCE);
-	}
-
-	private void setMarkupSettings() {
-		IMarkupSettings markupSettings = getMarkupSettings();
-		markupSettings.setStripWicketTags(true);
-		markupSettings.setStripComments(true);
-		markupSettings.setCompressWhitespace(true);
-		markupSettings.setDefaultMarkupEncoding("UTF-8");
-	}
-
-	private void configureFrontend() {
-		new FrontendConfigurator()
-				.withModules(MODIA)
-				.addMetas(
-						MetaTag.XUA_IE_EDGE,
-						MetaTag.CHARSET_UTF8,
-						MetaTag.VIEWPORT_SCALE_1
-				)
-				.addConditionalCss(
-						PersonPage.INTERN_IE,
-						SaksoversiktLerret.SAKSOVERSIKT_IE_CSS,
-						BasePage.MODIA_FLEXBOX_IE_CSS
-				)
-				.addConditionalJavascript(
-						PersonPage.RESPOND_JS,
-						SaksoversiktLerret.SAKSOVERSIKT_IE_JS
-				)
-				.addLess(
-						BasePage.MODIA_COMMON_LESS,
-						BasePage.MODIA_KOMPONENTER_LESS,
-						BasePage.MODIA_RAMME_LESS,
-						BasePage.MODIA_FLEXBOX_LESS,
-						BasePage.PERSONINFO_LESS,
-						UtbetalingLerret.UTBETALING_LESS,
-						SaksoversiktLerret.SAKSOVERSIKT_LESS,
-						BasePage.MELDINGERWIDGET,
-						BasePage.MELDINGERLAMELL,
-						PersonPage.SVAR_OG_REFERATPANEL_LESS
-				)
-				.addCss(
-						BasePage.PERSONSOKRESULT,
-						BasePage.PERSONSOKSEARCH
-				)
-				.addScripts(SPIResources.getScripts())
-				.addScripts(
-						BasePage.JS_RESOURCE,
-						ShortcutListenerResourceReference.get(),
-						KeyNavigationResourceReference.get(),
-						Widget.JS_RESOURCE,
-						LamellPanel.JS_RESOURCE,
-						DatePicker.DATEPICKER_JS,
-						DateRangePicker.JS_REFERENCE,
-						JqueryTokenValueChangeBehavior.JS_REFERENCE,
-						ModigModalWindow.JS,
-						EkspanderingsListe.JS_RESOURCE,
-						Liste.JS_RESOURCE,
-						DatePicker.JQUERY_PLACEHOLDER,
-						ModalErrorPanel.JS_RESOURCE,
-						UtbetalingLerret.UTBETALING_LAMELL_JS,
-						PersonPage.SELECTMENU_JS,
-						SaksoversiktLerret.SAKSOVERSIKT_JS
-				)
-				.withResourcePacking(this.usesDeploymentConfig())
-				.configure(this);
-	}
-
-	private void mountPages() {
-		mountPage("/person/${fnr}", PersonPage.class);
-		mountPage("internal/selftest", SelfTestPage.class);
-		mountResource("internal/selftest.json", new JsonResourceReference(SelfTestPage.class));
-		if (mockSetupErTillatt()) {
-			mountPage("internal/mocksetup", MockSetupPage.class);
-		}
-	}
+        Application.get().getRequestLoggerSettings().setRequestLoggerEnabled(true);
 
 
-	@Override
-	public Session newSession(Request request, Response response) {
-		Session session = super.newSession(request, response);
-		session.setAttribute(ModiaConstants.HENT_PERSON_BEGRUNNET, false);
-		return session;
-	}
+        new ModiaApplicationConfigurator()
+                .withExceptionHandler(true)
+                .configure(this);
 
-	protected void setSpringComponentInjector() {
-		getComponentInstantiationListeners().add(new SpringComponentInjector(this, applicationContext));
-	}
+        mountPages();
+
+        setSpringComponentInjector();
+
+        Datoformat.brukLocaleFra(LocaleFromWicketSession.INSTANCE);
+    }
+
+    private void setMarkupSettings() {
+        IMarkupSettings markupSettings = getMarkupSettings();
+        markupSettings.setStripWicketTags(true);
+        markupSettings.setStripComments(true);
+        markupSettings.setCompressWhitespace(true);
+        markupSettings.setDefaultMarkupEncoding("UTF-8");
+    }
+
+    private void configureFrontend() {
+        new FrontendConfigurator()
+                .withModules(MODIA)
+                .addMetas(
+                        MetaTag.XUA_IE_EDGE,
+                        MetaTag.CHARSET_UTF8,
+                        MetaTag.VIEWPORT_SCALE_1
+                )
+                .addConditionalCss(
+                        PersonPage.INTERN_IE,
+                        SaksoversiktLerret.SAKSOVERSIKT_IE_CSS,
+                        BasePage.MODIA_FLEXBOX_IE_CSS
+                )
+                .addConditionalJavascript(
+                        PersonPage.RESPOND_JS,
+                        SaksoversiktLerret.SAKSOVERSIKT_IE_JS
+                )
+                .addLess(
+                        BasePage.MODIA_COMMON_LESS,
+                        BasePage.MODIA_KOMPONENTER_LESS,
+                        BasePage.MODIA_RAMME_LESS,
+                        BasePage.MODIA_FLEXBOX_LESS,
+                        BasePage.PERSONINFO_LESS,
+                        UtbetalingLerret.UTBETALING_LESS,
+                        SaksoversiktLerret.SAKSOVERSIKT_LESS,
+                        BasePage.MELDINGERWIDGET,
+                        BasePage.MELDINGERLAMELL,
+                        PersonPage.SVAR_OG_REFERATPANEL_LESS
+                )
+                .addCss(
+                        BasePage.PERSONSOKRESULT,
+                        BasePage.PERSONSOKSEARCH
+                )
+                .addScripts(SPIResources.getScripts())
+                .addScripts(
+                        BasePage.JS_RESOURCE,
+                        ShortcutListenerResourceReference.get(),
+                        KeyNavigationResourceReference.get(),
+                        Widget.JS_RESOURCE,
+                        LamellPanel.JS_RESOURCE,
+                        DatePicker.DATEPICKER_JS,
+                        DateRangePicker.JS_REFERENCE,
+                        JqueryTokenValueChangeBehavior.JS_REFERENCE,
+                        ModigModalWindow.JS,
+                        EkspanderingsListe.JS_RESOURCE,
+                        Liste.JS_RESOURCE,
+                        DatePicker.JQUERY_PLACEHOLDER,
+                        ModalErrorPanel.JS_RESOURCE,
+                        UtbetalingLerret.UTBETALING_LAMELL_JS,
+                        PersonPage.SELECTMENU_JS,
+                        SaksoversiktLerret.SAKSOVERSIKT_JS
+                )
+                .withResourcePacking(this.usesDeploymentConfig())
+                .configure(this);
+    }
+
+    private void mountPages() {
+        mountPage("/person/${fnr}", PersonPage.class);
+        mountPage("internal/selftest", SelfTestPage.class);
+        mountResource("internal/selftest.json", new JsonResourceReference(SelfTestPage.class));
+        if (mockSetupErTillatt()) {
+            mountPage("internal/mocksetup", MockSetupPage.class);
+        }
+    }
+
+
+    @Override
+    public Session newSession(Request request, Response response) {
+        Session session = super.newSession(request, response);
+        session.setAttribute(ModiaConstants.HENT_PERSON_BEGRUNNET, false);
+        return session;
+    }
+
+    protected void setSpringComponentInjector() {
+        getComponentInstantiationListeners().add(new SpringComponentInjector(this, applicationContext));
+    }
 
 }
