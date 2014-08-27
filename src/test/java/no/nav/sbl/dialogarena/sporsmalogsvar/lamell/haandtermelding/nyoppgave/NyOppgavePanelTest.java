534e26daa1735d87c6576c9b829776c51a873ca7
Nicklas Utgaard <nicklas.utgaard@nav.no>
2014-08-27 10:25:03 +0200

    PK-12429: Refaktorering av tester.

--- src/test/java/no/nav/sbl/dialogarena/sporsmalogsvar/lamell/haandtermelding/nyoppgave/NyOppgavePanelTest.java
+++ src/test/java/no/nav/sbl/dialogarena/sporsmalogsvar/lamell/haandtermelding/nyoppgave/NyOppgavePanelTest.java
@@ -2,9 +2,7 @@ package no.nav.sbl.dialogarena.sporsmalogsvar.lamell.haandtermelding.nyoppgave;
 
 import no.nav.sbl.dialogarena.sporsmalogsvar.config.WicketPageTest;
 import no.nav.sbl.dialogarena.sporsmalogsvar.config.mock.ServiceTestContext;
-import no.nav.sbl.dialogarena.sporsmalogsvar.consumer.GsakService;
 import no.nav.sbl.dialogarena.sporsmalogsvar.domain.Melding;
-import no.nav.sbl.dialogarena.sporsmalogsvar.domain.NyOppgave;
 import no.nav.sbl.dialogarena.sporsmalogsvar.lamell.InnboksVM;
 import no.nav.sbl.dialogarena.sporsmalogsvar.lamell.MeldingVM;
 import no.nav.sbl.dialogarena.sporsmalogsvar.lamell.TraadVM;
@@ -12,43 +10,25 @@ import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
-
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.ContextConfiguration;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Captor;
-import org.mockito.ArgumentCaptor;
-import org.mockito.MockitoAnnotations;
-
-import javax.inject.Inject;
-import java.util.List;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static no.nav.modig.wicket.test.matcher.CombinableMatcher.both;
-import static no.nav.modig.wicket.test.matcher.ComponentMatchers.ofType;
-import static no.nav.modig.wicket.test.matcher.ComponentMatchers.thatIsVisible;
-import static no.nav.modig.wicket.test.matcher.ComponentMatchers.withId;
-
+import static no.nav.modig.wicket.test.matcher.ComponentMatchers.*;
 import static no.nav.sbl.dialogarena.sporsmalogsvar.domain.Meldingstype.SPORSMAL;
 import static no.nav.sbl.dialogarena.sporsmalogsvar.lamell.haandtermelding.journalforing.TestUtils.createMelding;
 import static org.hamcrest.core.Is.is;
 import static org.joda.time.DateTime.now;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ContextConfiguration(classes = {ServiceTestContext.class})
 @RunWith(SpringJUnit4ClassRunner.class)
 public class NyOppgavePanelTest extends WicketPageTest {
-
-    @Captor
-    private ArgumentCaptor<NyOppgave> nyOppgaveArgumentCaptor;
-
-    @Inject
-    private GsakService gsakService;
 
     private Melding melding;
     private InnboksVM innboksVM = mock(InnboksVM.class);
@@ -74,21 +54,6 @@ public class NyOppgavePanelTest extends WicketPageTest {
     }
 
     @Test
-    public void skalGiFeilmeldingHvisManSenderInnSkjemaUtenAAFylleInnAlleFeltene() {
-        TestNyOppgavePanel nyOppgavePanel = new TestNyOppgavePanel("panel", innboksVM);
-        wicket.goToPageWith(nyOppgavePanel)
-                .inForm("panel:nyoppgave-form:nyoppgaveform")
-                .submit();
-
-        List<String> errorMessages = wicket.get().errorMessages();
-        assertTrue(errorMessages.contains(nyOppgavePanel.get("nyoppgave-form:nyoppgaveform").getString("nyoppgaveform.tema.Required")));
-        assertTrue(errorMessages.contains(nyOppgavePanel.get("nyoppgave-form:nyoppgaveform").getString("enhet.Required")));
-        assertTrue(errorMessages.contains(nyOppgavePanel.get("nyoppgave-form:nyoppgaveform").getString("type.Required")));
-        assertTrue(errorMessages.contains(nyOppgavePanel.get("nyoppgave-form:nyoppgaveform").getString("prioritet.Required")));
-        assertTrue(errorMessages.contains(nyOppgavePanel.get("nyoppgave-form:nyoppgaveform").getString("beskrivelse.Required")));
-    }
-
-    @Test
     public void skalLukkePaneletIdetManAvbryter() {
         TestNyOppgavePanel nyOppgavePanel = new TestNyOppgavePanel("panel", innboksVM);
         wicket.goToPageWith(nyOppgavePanel)
@@ -97,25 +62,4 @@ public class NyOppgavePanelTest extends WicketPageTest {
 
         assertThat(nyOppgavePanel.isVisibilityAllowed(), is(false));
     }
-
-    @Test
-    public void skalSendeNyOppgaveObjektetTilGsakTjenestenForAaOppretteNy() {
-        String beskrivelse = "Dette er en beskrivelse";
-        TestNyOppgavePanel nyOppgavePanel = new TestNyOppgavePanel("panel", innboksVM);
-        MockitoAnnotations.initMocks(this);
-        wicket.goToPageWith(nyOppgavePanel)
-                .inForm("panel:nyoppgave-form:nyoppgaveform")
-                    .select("tema", 0)
-                    .select("enhet", 0)
-                    .select("type", 0)
-                    .select("prioritet", 0)
-                    .write("beskrivelse", beskrivelse)
-                    .submitWithAjaxButton(withId("opprettoppgave"));
-
-        verify(gsakService).opprettGsakOppgave(nyOppgaveArgumentCaptor.capture());
-        NyOppgave nyOppgave = nyOppgaveArgumentCaptor.getValue();
-        assertThat(nyOppgave.beskrivelse, is(beskrivelse));
-        assertThat(nyOppgave.henvendelseId, is(melding.id));
-    }
-
 }
---
+++ src/test/java/no/nav/sbl/dialogarena/sporsmalogsvar/lamell/haandtermelding/nyoppgavewrapper/NyOppgaveWrapperTest.java
@@ -0,0 +1,82 @@
+package no.nav.sbl.dialogarena.sporsmalogsvar.lamell.haandtermelding.nyoppgavewrapper;
+
+import no.nav.sbl.dialogarena.sporsmalogsvar.config.WicketPageTest;
+import no.nav.sbl.dialogarena.sporsmalogsvar.config.mock.ServiceTestContext;
+import no.nav.sbl.dialogarena.sporsmalogsvar.consumer.GsakService;
+import no.nav.sbl.dialogarena.sporsmalogsvar.domain.Melding;
+import no.nav.sbl.dialogarena.sporsmalogsvar.domain.NyOppgave;
+import no.nav.sbl.dialogarena.sporsmalogsvar.lamell.InnboksVM;
+import no.nav.sbl.dialogarena.sporsmalogsvar.lamell.MeldingVM;
+import no.nav.sbl.dialogarena.sporsmalogsvar.lamell.TraadVM;
+import no.nav.sbl.dialogarena.sporsmalogsvar.lamell.haandtermelding.nyoppgaveformwrapper.NyOppgaveFormWrapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.inject.Inject;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static no.nav.modig.wicket.test.matcher.ComponentMatchers.withId;
+import static no.nav.sbl.dialogarena.sporsmalogsvar.domain.Meldingstype.SPORSMAL;
+import static no.nav.sbl.dialogarena.sporsmalogsvar.lamell.haandtermelding.journalforing.TestUtils.createMelding;
+import static org.hamcrest.core.Is.is;
+import static org.joda.time.DateTime.now;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
+
+@ContextConfiguration(classes = {ServiceTestContext.class})
+@RunWith(SpringJUnit4ClassRunner.class)
+public class NyOppgaveWrapperTest extends WicketPageTest {
+
+    @Captor
+    private ArgumentCaptor<NyOppgave> nyOppgaveArgumentCaptor;
+
+    @Inject
+    private GsakService gsakService;
+
+    @Test
+    public void skalGiFeilmeldingHvisManSenderInnSkjemaUtenAAFylleInnAlleFeltene() {
+        NyOppgaveFormWrapper nyOppgaveWrapper = new NyOppgaveFormWrapper("panel", mock(InnboksVM.class));
+        wicket.goToPageWith(nyOppgaveWrapper)
+                .inForm("panel:nyoppgaveform")
+                .submit();
+
+        List<String> errorMessages = wicket.get().errorMessages();
+        assertTrue(errorMessages.contains(nyOppgaveWrapper.getString("nyoppgaveform.tema.Required")));
+        assertTrue(errorMessages.contains(nyOppgaveWrapper.getString("nyoppgaveform.enhet.Required")));
+        assertTrue(errorMessages.contains(nyOppgaveWrapper.getString("nyoppgaveform.type.Required")));
+        assertTrue(errorMessages.contains(nyOppgaveWrapper.getString("nyoppgaveform.prioritet.Required")));
+        assertTrue(errorMessages.contains(nyOppgaveWrapper.getString("nyoppgaveform.beskrivelse.Required")));
+    }
+
+    @Test
+    public void skalSendeNyOppgaveObjektetTilGsakTjenestenForAaOppretteNy() {
+        InnboksVM innboksVM = mock(InnboksVM.class);
+        Melding melding = createMelding("id", SPORSMAL, now(), "temagruppe", "id");
+        when(innboksVM.getValgtTraad()).thenReturn(new TraadVM(asList(new MeldingVM(melding, 1))));
+
+
+        String beskrivelse = "Dette er en beskrivelse";
+        NyOppgaveFormWrapper nyOppgaveFormWrapper = new NyOppgaveFormWrapper("panel", innboksVM);
+        MockitoAnnotations.initMocks(this);
+        wicket.goToPageWith(nyOppgaveFormWrapper)
+                .inForm("panel:nyoppgaveform")
+                .select("tema", 0)
+                .select("enhet", 0)
+                .select("type", 0)
+                .select("prioritet", 0)
+                .write("beskrivelse", beskrivelse)
+                .submitWithAjaxButton(withId("opprettoppgave"));
+
+        verify(gsakService).opprettGsakOppgave(nyOppgaveArgumentCaptor.capture());
+        NyOppgave nyOppgave = nyOppgaveArgumentCaptor.getValue();
+        assertThat(nyOppgave.beskrivelse, is(beskrivelse));
+        assertThat(nyOppgave.henvendelseId, is(melding.id));
+    }
+}
