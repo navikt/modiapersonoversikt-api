0a85e121126081952ac2418fc6e7fe0e8efa38dc
Nicklas Utgaard <nicklas.utgaard@nav.no>
2014-08-27 14:36:25 +0200

    PK-12429: Styring av synlighet av Merk, Journalfør panel, og refaktorering av HaandterMeldingPanel

--- src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/lamell/haandtermelding/HaandterMeldingPanel.java
+++ src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/lamell/haandtermelding/HaandterMeldingPanel.java
@@ -8,10 +8,12 @@ import no.nav.sbl.dialogarena.sporsmalogsvar.lamell.haandtermelding.nyoppgave.Ny
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.PropertyModel;
 
 import static no.nav.modig.modia.events.InternalEvents.SVAR_PAA_MELDING;
 import static no.nav.modig.wicket.conditional.ConditionalUtils.enabledIf;
+import static no.nav.modig.wicket.model.ModelUtils.both;
 import static no.nav.modig.wicket.model.ModelUtils.not;
 import static org.apache.wicket.event.Broadcast.EXACT;
 
@@ -21,7 +23,13 @@ public class HaandterMeldingPanel extends Panel {
     static final String PANEL_LUKKET = "sos.haandtermelding.panelLukket";
 
     public HaandterMeldingPanel(String id, final InnboksVM innboksVM) {
-        super(id);
+        super(id, new PropertyModel(innboksVM, "valgtTraad"));
+
+        IModel<Boolean> erKontorsperret = new PropertyModel<>(getDefaultModel(), "erKontorsperret()");
+        IModel<Boolean> nyesteMeldingErJournalfort = new PropertyModel<>(getDefaultModel(), "nyesteMelding.journalfort");
+        IModel<Boolean> eldsteMeldingErJournalfort = new PropertyModel<>(getDefaultModel(), "eldsteMelding.journalfort");
+        IModel<Boolean> erBehandlet = new PropertyModel<>(getDefaultModel(), "erBehandlet()");
+        IModel<Boolean> bleInitiertAvBruker = new PropertyModel<>(getDefaultModel(), "bleInitiertAvBruker()");
 
         add(
                 new AjaxLink<InnboksVM>("besvar") {
@@ -29,20 +37,20 @@ public class HaandterMeldingPanel extends Panel {
                     public void onClick(AjaxRequestTarget target) {
                         send(getPage(), EXACT, new NamedEventPayload(SVAR_PAA_MELDING, innboksVM.getValgtTraad().getEldsteMelding().melding.id));
                     }
-                }.add(enabledIf(new PropertyModel<Boolean>(innboksVM, "valgtTraad.bleInitiertAvBruker()")))
+                }.add(enabledIf(bleInitiertAvBruker))
         );
 
         JournalforingsPanel journalforingsPanel = new JournalforingsPanel("journalfor-panel", innboksVM);
         add(journalforingsPanel);
-        add(new MeldingValgPanel("journalforingValg", not(new PropertyModel<Boolean>(innboksVM, "valgtTraad.nyesteMelding.nyesteMeldingISinJournalfortgruppe")), journalforingsPanel));
+        add(new MeldingValgPanel("journalforingValg", both(not(erKontorsperret)).and(not(nyesteMeldingErJournalfort)), journalforingsPanel));
 
         NyOppgavePanel nyOppgavePanel = new NyOppgavePanel("nyoppgave-panel", innboksVM);
         add(nyOppgavePanel);
-        add(new MeldingValgPanel("nyoppgaveValg", new PropertyModel<Boolean>(innboksVM, "valgtTraad.erBehandlet()"), nyOppgavePanel));
+        add(new MeldingValgPanel("nyoppgaveValg", erBehandlet, nyOppgavePanel));
 
         MerkePanel merkePanel = new MerkePanel("merke-panel", innboksVM);
         add(merkePanel);
-        add(new MeldingValgPanel("merkeValg", not(new PropertyModel<Boolean>(innboksVM, "valgtTraad.eldsteMelding.journalfort")), merkePanel));
+        add(new MeldingValgPanel("merkeValg", both(not(eldsteMeldingErJournalfort)).and(not(erKontorsperret)).and(erBehandlet), merkePanel));
     }
 
 }
