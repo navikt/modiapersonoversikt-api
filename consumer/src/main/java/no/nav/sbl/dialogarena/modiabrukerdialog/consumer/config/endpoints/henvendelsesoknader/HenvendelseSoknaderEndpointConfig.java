5887cff6a5ec4c2b3ba612552d13d8f287a93ca3
Jarle Svendsrud <jarle.svendsrud@nav.no>
2014-08-28 13:24:47 +0200

    La til henvendelsessøknader som endepunkt

--- consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/endpoints/henvendelsesoknader/HenvendelseSoknaderEndpointConfig.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/endpoints/henvendelsesoknader/HenvendelseSoknaderEndpointConfig.java
@@ -1,5 +1,6 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.endpoints.henvendelsesoknader;
 
+import no.nav.sbl.dialogarena.common.cxf.CXFClient;
 import no.nav.sbl.dialogarena.modiabrukerdialog.mock.config.endpoints.HenvendelseSoknaderPortTypeMock;
 import no.nav.tjeneste.domene.brukerdialog.henvendelsesoknader.v1.HenvendelseSoknaderPortType;
 import no.nav.tjeneste.domene.brukerdialog.henvendelsesoknader.v1.informasjon.WSSoknad;
@@ -19,7 +20,7 @@ public class HenvendelseSoknaderEndpointConfig {
     @Bean
     public HenvendelseSoknaderPortType henvendelseSoknaderPortType() {
         final HenvendelseSoknaderPortType mock = new HenvendelseSoknaderPortTypeMock().getHenvendelseSoknaderPortTypeMock();
-        final HenvendelseSoknaderPortType prod = null;
+        final HenvendelseSoknaderPortType prod = createHenvendelsePortType();
         return new HenvendelseSoknaderPortType() {
             @Override
             public List<WSSoknad> hentSoknadListe(@WebParam(name = "fodselsnummer", targetNamespace = "") String fodselsnummer) {
@@ -38,6 +39,13 @@ public class HenvendelseSoknaderEndpointConfig {
                 }
             }
         };
+    }
+
+    private HenvendelseSoknaderPortType createHenvendelsePortType() {
+        return new CXFClient<>(HenvendelseSoknaderPortType.class)
+                .address(System.getProperty("henvendelse.soknader.url"))
+                .wsdl("classpath:no/nav/tjeneste/domene/brukerdialog/henvendelsesoknader/v1/Soknader.wsdl")
+                .build();
     }
 
 }
