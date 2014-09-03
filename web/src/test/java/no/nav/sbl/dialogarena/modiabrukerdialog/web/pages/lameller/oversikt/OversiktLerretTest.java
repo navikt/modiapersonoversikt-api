0c0fc4cf43728cd04bb8a69705d1bf8156cc3d39
Peter Hemmen <peter.hemmen@nav.no>
2014-09-03 10:22:41 +0200

    Legger til en nødvendig when() på saksbehandlerInnstillingerService

--- web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/pages/lameller/oversikt/OversiktLerretTest.java
+++ web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/pages/lameller/oversikt/OversiktLerretTest.java
@@ -1,6 +1,7 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.web.pages.lameller.oversikt;
 
 import no.nav.modig.modia.widget.LenkeWidget;
+import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.services.SaksbehandlerInnstillingerService;
 import no.nav.sbl.dialogarena.modiabrukerdialog.mock.config.endpoints.KodeverkV2PortTypeMock;
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.WicketPageTest;
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.config.mock.EndpointMockContext;
@@ -10,14 +11,21 @@ import no.nav.sbl.dialogarena.sak.config.SaksoversiktServiceConfig;
 import no.nav.sbl.dialogarena.sporsmalogsvar.context.SporsmalOgSvarContext;
 import no.nav.sbl.dialogarena.utbetaling.lamell.context.UtbetalingLamellContext;
 import no.nav.sykmeldingsperioder.widget.SykepengerWidget;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.inject.Inject;
+
 import static no.nav.modig.wicket.test.matcher.ComponentMatchers.ofType;
 import static no.nav.modig.wicket.test.matcher.ComponentMatchers.withId;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.annotation.DirtiesContext.ClassMode.AFTER_CLASS;
 
+@DirtiesContext(classMode = AFTER_CLASS)
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {
         EndpointMockContext.class,
@@ -29,6 +37,14 @@ import static no.nav.modig.wicket.test.matcher.ComponentMatchers.withId;
         PersonPageMockContext.class
 })
 public class OversiktLerretTest extends WicketPageTest {
+
+    @Inject
+    private SaksbehandlerInnstillingerService saksbehandlerInnstillingerService;
+
+    @Before
+    public void setUp() {
+        when(saksbehandlerInnstillingerService.getSaksbehandlerValgtEnhet()).thenReturn("enhet");
+    }
 
     @Test
     public void skalOppretteOversikt() {
