ce88cbf622b3a73d246153a3c0fe03ee74856191
g131744 <marcus.gustafson@nav.no>
2014-09-12 16:22:10 +0200

    PKSTEGG-519: Viser sikkerhetstiltak i skjermebildet, dels når person-detaljer vises og dels ved geografisk tilgangskontroll feil.

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/pages/hentperson/HentPersonPage.java
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/pages/hentperson/HentPersonPage.java
@@ -1,6 +1,7 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.web.pages.hentperson;
 
 import no.nav.kjerneinfo.hent.panels.HentPersonPanel;
+import no.nav.kjerneinfo.web.pages.kjerneinfo.panel.sikkerhetstiltak.SikkerhetstiltakPersonPanel;
 import no.nav.modig.wicket.events.NamedEventPayload;
 import no.nav.modig.wicket.events.annotations.RunOnEvents;
 import no.nav.personsok.PersonsokPanel;
@@ -12,8 +13,10 @@ import no.nav.sbl.dialogarena.modiabrukerdialog.web.panels.saksbehandlerpanel.Sa
 import org.apache.wicket.RestartResponseException;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.util.string.StringValue;
+import org.apache.ws.security.util.StringUtil;
 
 import static no.nav.modig.modia.constants.ModiaConstants.HENT_PERSON_BEGRUNNET;
 import static no.nav.modig.modia.events.InternalEvents.FNR_CHANGED;
@@ -24,6 +27,7 @@ import static no.nav.modig.modia.events.InternalEvents.HENTPERSON_CLEAR_MESSAGES
 import static no.nav.modig.modia.events.InternalEvents.HENTPERSON_FODSELSNUMMER_IKKE_TILGANG;
 import static no.nav.modig.modia.events.InternalEvents.PERSONSOK_FNR_CLICKED;
 import static no.nav.modig.modia.events.InternalEvents.PERSONSOK_SEARCH_PERFORMED;
+import static no.nav.modig.modia.events.InternalEvents.GOTO_HENT_PERSONPAGE;
 import static org.apache.wicket.event.Broadcast.BREADTH;
 import static org.apache.wicket.event.Broadcast.DEPTH;
 import static org.apache.wicket.markup.head.OnLoadHeaderItem.forScript;
@@ -40,6 +44,12 @@ public class HentPersonPage extends BasePage {
                 new PlukkOppgavePanel("plukkOppgave"),
                 new PersonsokPanel("personsokPanel").setVisible(true)
         );
+		StringValue sikkerhetstiltakBeskrivelse = pageParameters.get("sikkerhetstiltak");
+		if (!sikkerhetstiltakBeskrivelse.isEmpty()) {
+			add(new SikkerhetstiltakPersonPanel("sikkerhetstiltak", sikkerhetstiltakBeskrivelse.toString()));
+		} else {
+			add(new SikkerhetstiltakPersonPanel("sikkerhetstiltak", new String()));
+		}
     }
 
     private void setupErrorText(PageParameters pageParameters, HentPersonPanel hentPersonPanel) {
@@ -62,6 +72,16 @@ public class HentPersonPage extends BasePage {
     public void refreshKjerneinfo(AjaxRequestTarget target, String query) {
         throw new RestartResponseException(PersonPage.class, new PageParameters().set("fnr", query));
     }
+
+	@RunOnEvents(GOTO_HENT_PERSONPAGE)
+	public void refreshKjerneinfoSikkerhetsInfo(AjaxRequestTarget target, String query) {
+		String[] queryL = StringUtil.split(query,';');
+		if (queryL.length == 2) {
+			throw new RestartResponseException(HentPersonPage.class, new PageParameters().set("error", queryL[0]).set("sikkerhetstiltak", queryL[1]));
+		} else {
+			throw new RestartResponseException(HentPersonPage.class, new PageParameters().set("error", query));
+		}
+	}
 
     @RunOnEvents(FODSELSNUMMER_FUNNET_MED_BEGRUNNElSE)
     public void refreshKjerneinfoMedBegrunnelse(AjaxRequestTarget target, String query) {
