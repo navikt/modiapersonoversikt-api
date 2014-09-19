da49e2ff2a35d56df94c2e7e77d55b06f83e7518
Henrik Hellerøy <henrik.helleroy@nav.no>
2014-09-19 15:50:13 +0200

    PK-15300: QA på ArenaService.

--- src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/consumer/ArenaService.java
+++ src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/consumer/ArenaService.java
@@ -10,7 +10,10 @@ import org.apache.commons.collections15.Transformer;
 import org.joda.time.DateTime;
 
 import javax.inject.Inject;
-import java.util.List;
+
+import static no.nav.modig.lang.collections.IterUtils.on;
+import static no.nav.modig.lang.collections.PredicateUtils.equalToIgnoreCase;
+import static no.nav.modig.lang.collections.PredicateUtils.where;
 
 public class ArenaService {
 
@@ -24,23 +27,19 @@ public class ArenaService {
     public Optional<Sak> hentOppfolgingssak(String fodselsnummer) {
         WSHentSakListeResponse response = arbeidOgAktivitet.hentSakListe(
                 new WSHentSakListeRequest().withBruker(new WSBruker().withBrukertypeKode(BRUKERKODE_PERSON).withBruker(fodselsnummer)));
-        List<no.nav.virksomhet.gjennomforing.sak.arbeidogaktivitet.v1.Sak> arenaSakListe = response.getSakListe();
-        Optional<no.nav.virksomhet.gjennomforing.sak.arbeidogaktivitet.v1.Sak> oppfolgingssak = filtrerUtOppfolgingssak(arenaSakListe);
-        return oppfolgingssak.isSome() ? Optional.optional(TIL_SAK.transform(oppfolgingssak.get())) : Optional.<Sak>none();
+
+        return on(response.getSakListe())
+                .filter(where(FAGOMRADE_KODE, equalToIgnoreCase(OPPFOLGINGSSAK_TEMA_IDENTIFIKATOR)))
+                .head()
+                .map(TIL_SAK);
     }
 
-    private Optional<no.nav.virksomhet.gjennomforing.sak.arbeidogaktivitet.v1.Sak> filtrerUtOppfolgingssak(List<no.nav.virksomhet.gjennomforing.sak.arbeidogaktivitet.v1.Sak> arenaSakListe) {
-        for (no.nav.virksomhet.gjennomforing.sak.arbeidogaktivitet.v1.Sak sak : arenaSakListe) {
-            if (sakErOppfolgingssak(sak)) {
-                return Optional.optional(sak);
-            }
+    private static final Transformer<no.nav.virksomhet.gjennomforing.sak.arbeidogaktivitet.v1.Sak, String> FAGOMRADE_KODE = new Transformer<no.nav.virksomhet.gjennomforing.sak.arbeidogaktivitet.v1.Sak, String>() {
+        @Override
+        public String transform(no.nav.virksomhet.gjennomforing.sak.arbeidogaktivitet.v1.Sak sak) {
+            return sak.getFagomradeKode().getKode();
         }
-        return Optional.none();
-    }
-
-    private boolean sakErOppfolgingssak(no.nav.virksomhet.gjennomforing.sak.arbeidogaktivitet.v1.Sak sak) {
-        return OPPFOLGINGSSAK_TEMA_IDENTIFIKATOR.equalsIgnoreCase(sak.getFagomradeKode().getKode());
-    }
+    };
 
     private static final Transformer<no.nav.virksomhet.gjennomforing.sak.arbeidogaktivitet.v1.Sak, Sak> TIL_SAK =
             new Transformer<no.nav.virksomhet.gjennomforing.sak.arbeidogaktivitet.v1.Sak, Sak>() {
