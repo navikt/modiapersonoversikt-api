0eb2c83f8a2059fc2b857ad2a1259d664f06f2bf
Nicklas Utgaard <nicklas.utgaard@nav.no>
2014-09-19 16:41:24 +0200

    Cache: Fungerende AOP cache setup

--- consumer/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/endpoint/henvendelsesoknader/HenvendelseSoknaderCacheTest.java
+++ consumer/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/endpoint/henvendelsesoknader/HenvendelseSoknaderCacheTest.java
@@ -1,32 +1,48 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.endpoint.henvendelsesoknader;
 
-import no.nav.modig.cache.CacheConfig;
+import net.sf.ehcache.Ehcache;
+import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.endpoint.util.SubjectHandlerTestConfig;
+import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.util.cache.CacheConfiguration;
 import no.nav.tjeneste.domene.brukerdialog.henvendelsesoknader.v1.HenvendelseSoknaderPortType;
-import org.junit.After;
+import no.nav.tjeneste.domene.brukerdialog.henvendelsesoknader.v1.informasjon.WSSoknad;
+import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.cache.ehcache.EhCacheCache;
 import org.springframework.cache.ehcache.EhCacheCacheManager;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.inject.Inject;
 
+import static java.util.Arrays.asList;
 import static no.nav.modig.testcertificates.TestCertificates.setupKeyAndTrustStore;
 import static no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.endpoint.henvendelsesoknader.HenvendelseSoknaderEndpointConfig.HENVENDELSESOKNADER_KEY;
 import static no.nav.sbl.dialogarena.modiabrukerdialog.consumer.util.MockUtil.TILLATMOCKSETUP_PROPERTY;
-import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.util.MatcherAssertionErrors.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {
-        CacheConfig.class,
+        CacheConfiguration.class,
+        SubjectHandlerTestConfig.class,
         HenvendelseSoknaderEndpointConfig.class
-        })
+})
 public class HenvendelseSoknaderCacheTest {
 
+    public static final String ENDPOINT_CACHE = "endpointCache";
+
+
+    private static EhCacheCacheManager cm;
+
     @Inject
-    private EhCacheCacheManager cm;
+    public void setEhCacheCacheManager(EhCacheCacheManager eccm) {
+        cm = eccm;
+    }
 
     @Inject
     private HenvendelseSoknaderPortType henvendelse;
@@ -39,19 +55,38 @@ public class HenvendelseSoknaderCacheTest {
         setupKeyAndTrustStore();
     }
 
-    @Test
-    public void cacheManager_harEntryForEndpointCache_etterKallTilHenvendelse() {
-        String cacheKey = "string";
-        henvendelse.hentSoknadListe(cacheKey);
-
-        Object fromCache = cm.getCache("endpointCache").get(cacheKey).get();
-
-        assertThat(fromCache, notNullValue());
-    }
-
-    @After
-    public void shutdown() {
+    @AfterClass
+    public static void after() {
         cm.getCacheManager().shutdown();
     }
 
+    @Before
+    public void teardown() {
+        getCache().removeAll();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void cache_virker() {
+        String fodselsnummer = "string";
+        when(henvendelse.hentSoknadListe(anyString())).thenReturn(
+                asList(new WSSoknad().withBehandlingsId("1")),
+                asList(new WSSoknad().withBehandlingsId("2")));
+
+        String behandlingsId = henvendelse.hentSoknadListe(fodselsnummer).get(0).getBehandlingsId();
+        String behandlingsId1 = henvendelse.hentSoknadListe(fodselsnummer).get(0).getBehandlingsId();
+
+        assertThat(behandlingsId, is(behandlingsId1));
+    }
+
+    @Test
+    public void cacheManager_harIkkeEntryForEndpointCache_etterKallTilPing() throws NoSuchMethodException {
+        henvendelse.ping();
+
+        assertThat(getCache().getKeys().size(), is(0));
+    }
+
+    private Ehcache getCache() {
+        return ((EhCacheCache) cm.getCache(ENDPOINT_CACHE)).getNativeCache();
+    }
 }
