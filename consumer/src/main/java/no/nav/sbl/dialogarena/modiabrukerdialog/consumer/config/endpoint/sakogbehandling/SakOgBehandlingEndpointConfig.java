43f7f2b2d4eff8ab52a6b9bd17669f94546935af
Nicklas Utgaard <nicklas.utgaard@nav.no>
2014-09-22 14:26:50 +0200

    Cache: Oppdatert SakOgBehandling til nye cacheoppsettet.

--- consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/endpoint/sakogbehandling/SakOgBehandlingEndpointConfig.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/endpoint/sakogbehandling/SakOgBehandlingEndpointConfig.java
@@ -6,30 +6,20 @@ import no.nav.modig.security.ws.AbstractSAMLOutInterceptor;
 import no.nav.modig.security.ws.SystemSAMLOutInterceptor;
 import no.nav.modig.security.ws.UserSAMLOutInterceptor;
 import no.nav.sbl.dialogarena.modiabrukerdialog.mock.config.endpoints.SakOgBehandlingPortTypeMock;
-import no.nav.tjeneste.virksomhet.sakogbehandling.v1.HentBehandlingBehandlingIkkeFunnet;
-import no.nav.tjeneste.virksomhet.sakogbehandling.v1.HentBehandlingskjedensBehandlingerBehandlingskjedeIkkeFunnet;
 import no.nav.tjeneste.virksomhet.sakogbehandling.v1.SakOgBehandling_v1PortType;
-import no.nav.tjeneste.virksomhet.sakogbehandling.v1.meldinger.FinnSakOgBehandlingskjedeListeRequest;
-import no.nav.tjeneste.virksomhet.sakogbehandling.v1.meldinger.FinnSakOgBehandlingskjedeListeResponse;
-import no.nav.tjeneste.virksomhet.sakogbehandling.v1.meldinger.HentBehandlingRequest;
-import no.nav.tjeneste.virksomhet.sakogbehandling.v1.meldinger.HentBehandlingResponse;
-import no.nav.tjeneste.virksomhet.sakogbehandling.v1.meldinger.HentBehandlingskjedensBehandlingerRequest;
-import no.nav.tjeneste.virksomhet.sakogbehandling.v1.meldinger.HentBehandlingskjedensBehandlingerResponse;
 import org.apache.cxf.feature.LoggingFeature;
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
 import org.apache.cxf.ws.addressing.WSAddressingFeature;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.jws.WebParam;
 import java.util.List;
 
 import static java.lang.System.currentTimeMillis;
 import static java.util.Arrays.asList;
 import static no.nav.modig.modia.ping.PingResult.ServiceResult.SERVICE_FAIL;
 import static no.nav.modig.modia.ping.PingResult.ServiceResult.SERVICE_OK;
-import static no.nav.sbl.dialogarena.modiabrukerdialog.consumer.util.MockUtil.mockErTillattOgSlaattPaaForKey;
+import static no.nav.sbl.dialogarena.modiabrukerdialog.consumer.util.InstanceSwitcher.createSwitcher;
 
 @Configuration
 public class SakOgBehandlingEndpointConfig {
@@ -40,38 +30,7 @@ public class SakOgBehandlingEndpointConfig {
     public SakOgBehandling_v1PortType sakOgBehandlingPortType() {
         final SakOgBehandling_v1PortType prod = createSakogbehandlingPortType(new UserSAMLOutInterceptor());
         final SakOgBehandling_v1PortType mock = new SakOgBehandlingPortTypeMock().getSakOgBehandlingPortTypeMock();
-        return new SakOgBehandling_v1PortType() {
-
-            @Cacheable("endpointCache")
-            @Override
-            public FinnSakOgBehandlingskjedeListeResponse finnSakOgBehandlingskjedeListe(@WebParam(name = "request", targetNamespace = "") FinnSakOgBehandlingskjedeListeRequest request) {
-                if (mockErTillattOgSlaattPaaForKey(SAKOGBEHANDLING_KEY)) {
-                    return mock.finnSakOgBehandlingskjedeListe(request);
-                }
-                return prod.finnSakOgBehandlingskjedeListe(request);
-            }
-
-            @Override
-            public void ping() {
-                if (mockErTillattOgSlaattPaaForKey(SAKOGBEHANDLING_KEY)) {
-                    mock.ping();
-                } else {
-                    prod.ping();
-                }
-            }
-
-            @Override
-            public HentBehandlingskjedensBehandlingerResponse hentBehandlingskjedensBehandlinger
-                    (@WebParam(name = "request", targetNamespace = "") HentBehandlingskjedensBehandlingerRequest hentBehandlingskjedensBehandlingerRequest)
-                    throws HentBehandlingskjedensBehandlingerBehandlingskjedeIkkeFunnet {
-                throw new RuntimeException("skal ikke brukes");
-            }
-
-            @Override
-            public HentBehandlingResponse hentBehandling(@WebParam(name = "request", targetNamespace = "") HentBehandlingRequest hentBehandlingRequest) throws HentBehandlingBehandlingIkkeFunnet {
-                throw new RuntimeException("skal ikke brukes");
-            }
-        };
+        return createSwitcher(prod, mock, SAKOGBEHANDLING_KEY, SakOgBehandling_v1PortType.class);
     }
 
     @Bean
--- consumer/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/endpoint/sakogbehandling/SakOgBehandlingCacheTest.java
+++ consumer/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/endpoint/sakogbehandling/SakOgBehandlingCacheTest.java
@@ -1,14 +1,15 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.endpoint.sakogbehandling;
 
 
-import no.nav.modig.cache.CacheConfig;
+import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.endpoint.util.CacheTest;
 import no.nav.tjeneste.virksomhet.sakogbehandling.v1.SakOgBehandling_v1PortType;
+import no.nav.tjeneste.virksomhet.sakogbehandling.v1.informasjon.finnsakogbehandlingskjedeliste.WSSak;
 import no.nav.tjeneste.virksomhet.sakogbehandling.v1.meldinger.FinnSakOgBehandlingskjedeListeRequest;
-import org.junit.After;
+import no.nav.tjeneste.virksomhet.sakogbehandling.v1.meldinger.FinnSakOgBehandlingskjedeListeResponse;
+import org.joda.time.DateTime;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.cache.ehcache.EhCacheCacheManager;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -17,21 +18,25 @@ import javax.inject.Inject;
 import static no.nav.modig.testcertificates.TestCertificates.setupKeyAndTrustStore;
 import static no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.endpoint.sakogbehandling.SakOgBehandlingEndpointConfig.SAKOGBEHANDLING_KEY;
 import static no.nav.sbl.dialogarena.modiabrukerdialog.consumer.util.MockUtil.TILLATMOCKSETUP_PROPERTY;
-import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.util.MatcherAssertionErrors.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {
-        CacheConfig.class,
         SakOgBehandlingEndpointConfig.class
 })
-public class SakOgBehandlingCacheTest {
+public class SakOgBehandlingCacheTest extends CacheTest {
 
-    @Inject
-    private EhCacheCacheManager cm;
+    public static final String ENDPOINT_CACHE = "endpointCache";
 
     @Inject
     private SakOgBehandling_v1PortType sakOgBehandling;
+
+    public SakOgBehandlingCacheTest() {
+        super(ENDPOINT_CACHE);
+    }
 
     @BeforeClass
     public static void setup() {
@@ -43,17 +48,18 @@ public class SakOgBehandlingCacheTest {
 
     @Test
     public void cacheManager_harEntryForEndpointCache_etterKallTilHenvendelse() {
-        FinnSakOgBehandlingskjedeListeRequest cacheKey = new FinnSakOgBehandlingskjedeListeRequest().withAktoerREF("aktoer");
-        sakOgBehandling.finnSakOgBehandlingskjedeListe(cacheKey);
+        FinnSakOgBehandlingskjedeListeRequest request1 = new FinnSakOgBehandlingskjedeListeRequest().withAktoerREF("aktoer");
+        FinnSakOgBehandlingskjedeListeRequest request2 = new FinnSakOgBehandlingskjedeListeRequest().withAktoerREF("aktoer");
+        WSSak sak1 = new WSSak().withOpprettet(DateTime.now());
+        WSSak sak2 = new WSSak().withOpprettet(DateTime.now().plusDays(1));
+        when(sakOgBehandling.finnSakOgBehandlingskjedeListe(any(FinnSakOgBehandlingskjedeListeRequest.class))).thenReturn(
+                new FinnSakOgBehandlingskjedeListeResponse().withSak(sak1),
+                new FinnSakOgBehandlingskjedeListeResponse().withSak(sak2)
+        );
 
-        Object fromCache = cm.getCache("endpointCache").get(cacheKey).get();
+        FinnSakOgBehandlingskjedeListeResponse resp1 = sakOgBehandling.finnSakOgBehandlingskjedeListe(request1);
+        FinnSakOgBehandlingskjedeListeResponse resp2 = sakOgBehandling.finnSakOgBehandlingskjedeListe(request2);
 
-        assertThat(fromCache, notNullValue());
+        assertThat(resp1.getSak().get(0).getOpprettet(), is(resp2.getSak().get(0).getOpprettet()));
     }
-
-    @After
-    public void shutdown() {
-        cm.getCacheManager().shutdown();
-    }
-
 }
