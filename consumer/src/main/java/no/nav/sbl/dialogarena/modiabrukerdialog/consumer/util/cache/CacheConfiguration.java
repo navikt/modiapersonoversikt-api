dbee6ba3dc8ca5a6f652b722960dfc2965d11f1b
Ketil Velle <ketil.s.velle@nav.no>
2014-09-25 18:12:48 +0200

    KAIZEN fjerner caching for aa prover aa finne feil i saksliste

--- consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/util/cache/CacheConfiguration.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/util/cache/CacheConfiguration.java
@@ -3,10 +3,9 @@ package no.nav.sbl.dialogarena.modiabrukerdialog.consumer.util.cache;
 import no.nav.modig.cache.CacheConfig;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
 
 @Configuration
 @EnableCaching
-@ImportResource("classpath*:*cacheconfig.xml")
+//@ImportResource("classpath*:*cacheconfig.xml")
 public class CacheConfiguration extends CacheConfig {
 }
