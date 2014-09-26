cbdc9b4e2c59cdeff2d5f235a70ec8ecf6bc0721
Ketil Velle <ketil.s.velle@nav.no>
2014-09-26 10:09:57 +0200

    Kaizen revert

--- consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/endpoint/v1/gsak/hentsaker/GsakHentSakslisteEndpointConfig.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/endpoint/v1/gsak/hentsaker/GsakHentSakslisteEndpointConfig.java
@@ -53,7 +53,7 @@ public class GsakHentSakslisteEndpointConfig {
     private static SakBugfix createEndpoint() {
         return new CXFClient<>(SakBugfix.class)
                 .address(System.getProperty("gsak.saksliste.v1.url"))
-                //.wsdl("classpath:nav-tjeneste-sak_SakWSEXP.wsdl")
+                .wsdl("classpath:no/nav/virksomhet/tjenester/sak/sak.wsdl")
                 .withOutInterceptor(new SystemSAMLOutInterceptor())
                 .build();
     }
