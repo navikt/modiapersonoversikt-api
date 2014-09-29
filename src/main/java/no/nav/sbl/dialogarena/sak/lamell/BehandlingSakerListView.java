d4f9bdedef9b379eafedde3c66d37a3fa23c59c6
Steffen Hageland <steffen.hageland@nav.no>
2014-09-29 16:29:03 +0200

    [print][gui] Fikset slik at print-siden til saksoversikt ikke kutter etter 1 side. (flexbox + inline problemer)

--- src/main/java/no/nav/sbl/dialogarena/sak/lamell/BehandlingSakerListView.java
+++ src/main/java/no/nav/sbl/dialogarena/sak/lamell/BehandlingSakerListView.java
@@ -1,16 +1,23 @@
 package no.nav.sbl.dialogarena.sak.lamell;
 
+import no.nav.sbl.dialogarena.sak.service.BulletProofKodeverkService;
 import no.nav.sbl.dialogarena.sak.viewdomain.widget.TemaVM;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.PropertyListView;
 
+import javax.inject.Inject;
 import java.util.List;
 
 import static no.nav.modig.wicket.conditional.ConditionalUtils.hasCssClassIf;
+import static no.nav.sbl.dialogarena.sak.service.BulletProofKodeverkService.ARKIVTEMA;
 import static org.apache.wicket.model.Model.of;
 
 
 public class BehandlingSakerListView extends PropertyListView<TemaVM> {
+    @Inject
+    private BulletProofKodeverkService kodeverk;
+
     private SaksoversiktLerret lerret;
     private String fnr;
 
@@ -25,7 +32,8 @@ public class BehandlingSakerListView extends PropertyListView<TemaVM> {
         TemaVM sakstema = item.getModelObject();
         item.add(
                 new BehandlingerListView("behandlinger", lerret.getBehandlingerForTema(sakstema), fnr),
-                new SaksinformasjonPanel("saksinformasjonContainer", sakstema.temakode)
+                new SaksinformasjonPanel("saksinformasjonContainer", sakstema.temakode),
+                new Label("sakstema-print", kodeverk.getTemanavnForTemakode(sakstema.temakode, ARKIVTEMA))
         ).setOutputMarkupId(true).setMarkupId("behandling_" + sakstema.temakode);
 
         boolean sakstemaErValgt = sakstema.temakode.equals(lerret.getAktivtTema().getObject());
