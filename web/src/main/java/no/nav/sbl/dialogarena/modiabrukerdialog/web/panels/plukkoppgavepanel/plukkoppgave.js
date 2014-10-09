5d3a1c9eb2f5c99ff9b351aa8d23e914916a31a3
Nicklas Utgaard <nicklas.utgaard@nav.no>
2014-10-09 15:06:07 +0200

    UX12: PlukkOppgave: fjerner "dropdown" når man velger tema.

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/panels/plukkoppgavepanel/plukkoppgave.js
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/panels/plukkoppgavepanel/plukkoppgave.js
@@ -10,3 +10,8 @@ $(document).on('click', '.velg-temagruppe', function (e) {
     $('.temagruppe-liste').toggle();
     e.stopPropagation();
 });
+
+$(document).on('click', '.temagruppe-liste > label', function(e) {
+    $('.temagruppe-liste').fadeToggle(100);
+    e.stopPropagation();
+});
