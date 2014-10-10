56bec0ba6c136420896e6398c4b0949d1c0c0a32
Henrik Hellerøy <henrik.helleroy@nav.no>
2014-10-10 10:30:14 +0200

    Fjernet enter-listener fra meldinger.js

--- src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/lamell/meldinger.js
+++ src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/lamell/meldinger.js
@@ -1,7 +1,6 @@
 var Meldinger = (function () {
     var PIL_NED = 40;
     var PIL_OPP = 38;
-    var ENTER = 13;
     var TAB = 9;
 
     var addKeyNavigation = function () {
@@ -10,8 +9,6 @@ var Meldinger = (function () {
                 $(e.currentTarget).prev().click();
             } else if (e.keyCode === PIL_NED) {
                 $(e.currentTarget).next().click();
-            } else if (e.keyCode === ENTER) {
-                $(e.currentTarget).click();
             } else if (e.keyCode === TAB && e.shiftKey) {
                 $('.meldinger .lamellhode ~ .close').focus();
             } else if (e.keyCode === TAB) {
