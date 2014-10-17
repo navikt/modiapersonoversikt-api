5494499c683af293a92b17be7c808383375e0133
Nicklas Utgaard <nicklas.utgaard@nav.no>
2014-10-17 09:47:32 +0200

    PKFEIL-5766: La til tabbing støtte for PlukkOppgavePanel

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/panels/plukkoppgavepanel/plukkoppgave.js
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/panels/plukkoppgavepanel/plukkoppgave.js
@@ -14,3 +14,14 @@ $(document).on('click', '.velg-temagruppe', function (e) {
 $(document).on('click', '.plukk-knapp', function() {
     $('.velg-temagruppe').addClass('plukket');
 });
+
+$(document).on('keydown', '.plukk-knapp, .velg-temagruppe', function(e){
+    var $target = $(e.currentTarget);
+    if (e.keyCode == 13) {
+        $target.click();
+    }
+
+    if ($target.is('.velg-temagruppe')) {
+        $target.siblings('.temagruppe-liste').find('input[type=radio]').first().focus();
+    }
+});
