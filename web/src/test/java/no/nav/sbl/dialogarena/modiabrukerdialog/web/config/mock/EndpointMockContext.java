9e6f61786086a4ee7e774f20249371d27fcdaa8d
Frithjof Nerdrum <frithjof.nerdrum@nav.no>
2014-12-10 10:49:36 +0100

    Fikk ikke med alt i forrige commit

--- web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/mock/EndpointMockContext.java
+++ web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/mock/EndpointMockContext.java
@@ -1,19 +1,9 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.web.config.mock;
 
-import no.nav.sbl.dialogarena.modiabrukerdialog.mock.config.endpoints.AktoerPortTypeMock;
-import no.nav.sbl.dialogarena.modiabrukerdialog.mock.config.endpoints.BehandleHenvendelsePortTypeMock;
-import no.nav.sbl.dialogarena.modiabrukerdialog.mock.config.endpoints.BehandleJournalV2PortTypeMock;
-import no.nav.sbl.dialogarena.modiabrukerdialog.mock.config.endpoints.GosysNavAnsattPortTypeMock;
-import no.nav.sbl.dialogarena.modiabrukerdialog.mock.config.endpoints.GsakHentSakslistePortTypeMock;
-import no.nav.sbl.dialogarena.modiabrukerdialog.mock.config.endpoints.GsakOppgaveV3PortTypeMock;
-import no.nav.sbl.dialogarena.modiabrukerdialog.mock.config.endpoints.GsakOppgavebehandlingV3PortTypeMock;
-import no.nav.sbl.dialogarena.modiabrukerdialog.mock.config.endpoints.GsakRutingPortTypeMock;
-import no.nav.sbl.dialogarena.modiabrukerdialog.mock.config.endpoints.HenvendelsePortTypeMock;
-import no.nav.sbl.dialogarena.modiabrukerdialog.mock.config.endpoints.HenvendelseSoknaderPortTypeMock;
-import no.nav.sbl.dialogarena.modiabrukerdialog.mock.config.endpoints.KodeverkV2PortTypeMock;
-import no.nav.sbl.dialogarena.modiabrukerdialog.mock.config.endpoints.SakOgBehandlingPortTypeMock;
-import no.nav.sbl.dialogarena.modiabrukerdialog.mock.config.endpoints.SendUtHenvendelsePortTypeMock;
-import no.nav.sbl.dialogarena.modiabrukerdialog.mock.config.endpoints.UtbetalingPortTypeMock;
+import no.nav.brukerprofil.consumer.BrukerprofilServiceBi;
+import no.nav.sbl.dialogarena.modiabrukerdialog.mock.config.artifacts.kjerneinfo.BrukerprofilServiceBiMock;
+import no.nav.sbl.dialogarena.modiabrukerdialog.mock.config.endpoints.*;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
@@ -36,4 +26,8 @@ import org.springframework.context.annotation.Import;
 })
 public class EndpointMockContext {
 
+    @Bean
+    public BrukerprofilServiceBi brukerprofilServiceBi() {
+        return BrukerprofilServiceBiMock.getBrukerprofilServiceBiMock();
+    }
 }
