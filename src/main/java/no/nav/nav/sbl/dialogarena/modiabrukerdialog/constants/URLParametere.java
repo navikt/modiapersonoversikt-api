79912e806480e8b6df8dc853137794fa1116dc73
Henrik Hellerøy <henrik.helleroy@nav.no>
2015-01-13 13:53:07 +0100

    Felles URL-parametere for henvendelsesid og oppgaveid

---
+++ src/main/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/constants/URLParametere.java
@@ -0,0 +1,6 @@
+package no.nav.nav.sbl.dialogarena.modiabrukerdialog.constants;
+
+public class URLParametere {
+    public static final String OPPGAVEID = "oppgaveid";
+    public static final String HENVENDELSEID = "henvendelseid";
+}
