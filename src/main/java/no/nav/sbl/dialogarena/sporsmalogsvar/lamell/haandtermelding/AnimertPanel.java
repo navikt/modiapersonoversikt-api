624386be67af504eb8eea3177e554a1143932960
Nicklas Utgaard <nicklas.utgaard@nav.no>
2015-01-19 10:12:06 +0100

    Byttet til bruk av String.format for konstruksjon av javascript.

--- src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/lamell/haandtermelding/AnimertPanel.java
+++ src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/lamell/haandtermelding/AnimertPanel.java
@@ -4,6 +4,7 @@ import no.nav.modig.wicket.events.annotations.RunOnEvents;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.panel.Panel;
 
+import static java.lang.String.format;
 import static no.nav.sbl.dialogarena.sporsmalogsvar.lamell.Innboks.VALGT_MELDING_EVENT;
 import static no.nav.sbl.dialogarena.sporsmalogsvar.lamell.haandtermelding.HaandterMeldingPanel.PANEL_LUKKET;
 import static no.nav.sbl.dialogarena.sporsmalogsvar.lamell.haandtermelding.HaandterMeldingPanel.PANEL_TOGGLET;
@@ -19,14 +20,13 @@ public abstract class AnimertPanel extends Panel {
 
     public void togglePanel(AjaxRequestTarget target) {
         if (isVisibilityAllowed()) {
-            target.prependJavaScript("lukket|$('#" + this.getMarkupId() + "').slideUp(lukket)");
+            target.prependJavaScript(format("lukket|$('#%s').slideUp(lukket)", this.getMarkupId()));
             this.setVisibilityAllowed(false);
-            target.add(this);
         } else {
-            target.appendJavaScript("$('#" + this.getMarkupId() + "').slideDown()");
+            target.appendJavaScript(format("$('#%s').slideDown()", this.getMarkupId()));
             this.setVisibilityAllowed(true);
-            target.add(this);
         }
+        target.add(this);
     }
 
     @RunOnEvents(PANEL_TOGGLET)
@@ -37,13 +37,13 @@ public abstract class AnimertPanel extends Panel {
             lukkPanel(target);
         }
     }
-
+    
     @RunOnEvents(VALGT_MELDING_EVENT)
     public void lukkPanel(AjaxRequestTarget target) {
         if (isVisibilityAllowed()) {
             this.setVisibilityAllowed(false);
             send(getParent(), BREADTH, PANEL_LUKKET);
-            target.prependJavaScript("lukket|$('#" + this.getMarkupId() + "').slideUp(lukket)");
+            target.prependJavaScript(format("lukket|$('#%s').slideUp(lukket)", this.getMarkupId()));
             target.add(this);
         }
     }
