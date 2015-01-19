5494b4fd0e507cf7a6e895adf0539a18729c309d
Nicklas Utgaard <nicklas.utgaard@nav.no>
2015-01-19 15:45:41 +0100

    Flyttet kall mot Gsak-SAK endepunkt inn i SakerService.

--- src/main/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/service/SakerService.java
+++ src/main/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/service/SakerService.java
@@ -1,7 +1,11 @@
 package no.nav.nav.sbl.dialogarena.modiabrukerdialog.service;
 
+import no.nav.nav.sbl.dialogarena.modiabrukerdialog.domain.Sak;
 import no.nav.nav.sbl.dialogarena.modiabrukerdialog.domain.Saker;
+
+import java.util.List;
 
 public interface SakerService {
     public Saker hentSaker(String fnr);
+    public List<Sak> hentListeAvSaker(String fnr);
 }
