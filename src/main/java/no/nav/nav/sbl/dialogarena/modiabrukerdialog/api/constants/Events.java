2a6af5712f473b3837c40c85c27bcbf40e2ba3d2
Henrik Hellerøy <henrik.helleroy@nav.no>
2015-01-29 10:59:45 +0100

    PK-18982: Globale events mellom sporsmalogsvar og modia

---
+++ src/main/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/api/constants/Events.java
@@ -0,0 +1,11 @@
+package no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.constants;
+
+public class Events {
+
+    public static class SporsmalOgSvar {
+        private static final String PREFIX = "sporsmalogsvar.";
+
+        public static final String SVAR_AVBRUTT = PREFIX + "svar.avbrutt";
+        public static final String LEGG_TILBAKE_UTFORT = PREFIX + "leggtilbake.utfort";
+    }
+}
