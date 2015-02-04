40778025b0fbacc76c5b3e1fae475b3e382c355b
Andreas Baakind <andreas.baakind@nav.no>
2015-02-04 11:25:53 +0100

    Suppress variabel-navngivings-warning da denne ikke gir mening i dette tilfellet

--- src/main/java/no/nav/sbl/dialogarena/utbetaling/domain/Aktoer.java
+++ src/main/java/no/nav/sbl/dialogarena/utbetaling/domain/Aktoer.java
@@ -2,6 +2,7 @@ package no.nav.sbl.dialogarena.utbetaling.domain;
 
 import no.nav.sbl.dialogarena.common.records.Key;
 
+@SuppressWarnings("all")
 public interface Aktoer {
     Key<String> aktoerId = new Key<>("AKTOER_ID");
     Key<String> navn = new Key<>("NAVN");
--- src/main/java/no/nav/sbl/dialogarena/utbetaling/domain/Trekk.java
+++ src/main/java/no/nav/sbl/dialogarena/utbetaling/domain/Trekk.java
@@ -2,6 +2,7 @@ package no.nav.sbl.dialogarena.utbetaling.domain;
 
 import no.nav.sbl.dialogarena.common.records.Key;
 
+@SuppressWarnings("all")
 public interface Trekk {
     Key<String> trekksType = new Key<>("TREKK_TYPE");
     Key<Double> trekkBeloep = new Key<>("TREKK_BELOEP");
