158142d3914c50b31fb577d8dc5a7e9ede8a5a5b
Andreas Baakind <andreas.baakind@nav.no>
2015-02-04 10:52:10 +0100

    LamellContext test

--- src/test/java/no/nav/sbl/dialogarena/utbetaling/config/UtbetalingPortTypeStubConfig.java
+++ src/test/java/no/nav/sbl/dialogarena/utbetaling/config/UtbetalingPortTypeStubConfig.java
@@ -14,6 +14,7 @@ public class UtbetalingPortTypeStubConfig {
 
     private static final String FNR = "***REMOVED***";
 
+
     @Bean
     public UtbetalingV1 utbetalingPortType() {
         return new UtbetalingV1() {
