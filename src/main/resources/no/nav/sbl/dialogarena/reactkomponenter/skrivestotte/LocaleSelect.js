d5d42055f921ffebfb0c492ccc9e76b177d35733
Frithjof Nerdrum <frithjof.nerdrum@nav.no>
2015-02-12 13:15:42 +0100

    PK-9318: Viser språk istedenfor locale i språkvelger

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/skrivestotte/LocaleSelect.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/skrivestotte/LocaleSelect.js
@@ -1,5 +1,11 @@
 /** @jsx React.DOM */
 var React = require('react');
+
+var spraak = {
+    'nb_NO': 'Norsk (Bokmål)',
+    'nn_NO': 'Norsk (Nynorsk)',
+    'en_US': 'Engelsk'
+};
 var LocaleSelect = React.createClass({
     onChange: function (event) {
         this.props.setValgtLocale(event.nativeEvent.target.value);
@@ -8,7 +14,7 @@ var LocaleSelect = React.createClass({
         return Object.keys(this.props.valgtTekst.innhold).length > 1 ? (
             <select onChange={this.onChange} value={this.props.valgtLocale}>
             {Object.keys(this.props.valgtTekst.innhold).map(function (locale) {
-                return <option value={locale}>{locale}</option>;
+                return <option value={locale}>{spraak[locale]}</option>;
             }.bind(this))}
             </select>
         ) : null;
