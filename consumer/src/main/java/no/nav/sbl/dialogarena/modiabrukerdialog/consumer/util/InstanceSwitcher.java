1c32a8d0c14d2e8d5fdc5eea05bd4f78594479fe
Nicklas Utgaard <nicklas.utgaard@nav.no>
2015-02-13 15:32:04 +0100

    Mer sonarfiks

--- consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/util/InstanceSwitcher.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/util/InstanceSwitcher.java
@@ -35,6 +35,7 @@ public final class InstanceSwitcher implements InvocationHandler {
     }
 
     @Override
+    @SuppressWarnings("PMD")
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         method.setAccessible(true);
         try {
