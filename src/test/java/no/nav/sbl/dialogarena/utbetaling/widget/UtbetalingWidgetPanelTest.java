e871b81e43ed45c3921143b95bd7c52f854d5b23
Knut Nygaard <knut.nygaard@nav.no>
2015-02-18 12:31:21 +0100

    PK-20611: La til en test for funksjonaliteten

--- src/test/java/no/nav/sbl/dialogarena/utbetaling/widget/UtbetalingWidgetPanelTest.java
+++ src/test/java/no/nav/sbl/dialogarena/utbetaling/widget/UtbetalingWidgetPanelTest.java
@@ -1,8 +1,14 @@
 package no.nav.sbl.dialogarena.utbetaling.widget;
 
+import no.nav.sbl.dialogarena.common.records.Record;
+import no.nav.sbl.dialogarena.utbetaling.domain.Aktoer;
+import no.nav.sbl.dialogarena.utbetaling.domain.Hovedytelse;
+import no.nav.sbl.dialogarena.utbetaling.domain.Konto;
 import no.nav.sbl.dialogarena.utbetaling.wickettest.AbstractWicketTest;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.Model;
+import org.joda.time.DateTime;
+import org.joda.time.Interval;
 import org.junit.Test;
 
 import static no.nav.modig.wicket.test.matcher.ComponentMatchers.ofType;
@@ -20,4 +26,27 @@ public class UtbetalingWidgetPanelTest extends AbstractWicketTest{
     public void shouldContainCorrectAmountOfComponents() {
         wicketTester.should().containComponents(5, ofType(Label.class));
     }
+
+    @Test
+    public void overskriftViserDatoPaaKorrektFormat(){
+        HovedytelseVM hovedytelseVM = lagHovedYtelse();
+        UtbetalingWidgetPanel utbetalingWidgetPanel = new UtbetalingWidgetPanel("panel", new Model<>(hovedytelseVM));
+
+        wicketTester.goToPageWith(utbetalingWidgetPanel);
+        wicketTester.should().containPatterns("18. februar 2015");
+    }
+
+    private HovedytelseVM lagHovedYtelse(){
+        return new HovedytelseVM(
+                new Record<Hovedytelse>()
+                    .with(Hovedytelse.id, "123")
+                    .with(Hovedytelse.hovedytelsedato, new DateTime().withDate(2015, 2, 18))
+                    .with(Hovedytelse.ytelsesperiode, new Interval(
+                            new DateTime().withDate(2015,2,1),
+                            new DateTime().withDate(2015,3,1)))
+                    .with(Hovedytelse.utbetaltTilKonto, new Record<Konto>().with(Konto.kontonummer, "123"))
+                    .with(Hovedytelse.utbetaltTil, new Record<Aktoer>().with(Aktoer.aktoerId, "12345678910"))
+        );
+    }
+
 }
