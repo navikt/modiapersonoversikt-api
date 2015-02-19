ea18afaba3dfb281e63e1ab2d5a755972dfddd2c
Ingrid Hjulstad <ingrid.hjulstad@nav.no>
2015-02-19 12:02:13 +0100

    Feilfiks: Tester i KvitteringsPanelTest feilet ved lokal mvn clean install. Må ha SpringContexten for å fungere.

--- web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/pages/personpage/dialogpanel/KvitteringsPanelTest.java
+++ web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/pages/personpage/dialogpanel/KvitteringsPanelTest.java
@@ -1,11 +1,14 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.web.pages.personpage.dialogpanel;
 
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.WicketPageTest;
+import no.nav.sbl.dialogarena.modiabrukerdialog.web.config.mock.ConsumerServicesMockContext;
+import no.nav.sbl.dialogarena.modiabrukerdialog.web.config.mock.EndpointMockContext;
 import org.apache.wicket.ajax.AjaxRequestHandler;
 import org.apache.wicket.markup.html.form.Form;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static no.nav.modig.wicket.test.matcher.ComponentMatchers.ofType;
@@ -14,6 +17,9 @@ import static org.springframework.test.annotation.DirtiesContext.ClassMode.AFTER
 
 @DirtiesContext(classMode = AFTER_EACH_TEST_METHOD)
 @RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {
+        ConsumerServicesMockContext.class,
+        EndpointMockContext.class})
 public class KvitteringsPanelTest extends WicketPageTest {
 
     @Test
