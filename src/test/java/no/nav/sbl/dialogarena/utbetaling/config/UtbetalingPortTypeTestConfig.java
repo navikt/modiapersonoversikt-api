65f2ca4631d95a5dcb77b856e5d6707106f9ae2d
Andreas Baakind <andreas.baakind@nav.no>
2015-02-20 11:34:42 +0100

    StartJetty kan gå mot mocktjeneste i f.eks U

--- src/test/java/no/nav/sbl/dialogarena/utbetaling/config/UtbetalingPortTypeTestConfig.java
+++ src/test/java/no/nav/sbl/dialogarena/utbetaling/config/UtbetalingPortTypeTestConfig.java
@@ -4,19 +4,32 @@ import no.nav.tjeneste.virksomhet.utbetaling.v1.HentUtbetalingsinformasjonPeriod
 import no.nav.tjeneste.virksomhet.utbetaling.v1.UtbetalingV1;
 import no.nav.tjeneste.virksomhet.utbetaling.v1.meldinger.WSHentUtbetalingsinformasjonRequest;
 import no.nav.tjeneste.virksomhet.utbetaling.v1.meldinger.WSHentUtbetalingsinformasjonResponse;
+import org.apache.cxf.feature.LoggingFeature;
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+import org.apache.cxf.ws.addressing.WSAddressingFeature;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.xml.namespace.QName;
+
+import static java.lang.Boolean.valueOf;
+import static java.lang.System.getProperty;
 import static no.nav.sbl.dialogarena.utbetaling.domain.testdata.WSUtbetalingTestData.getWsUtbetalinger;
 
 @Configuration
-public class UtbetalingPortTypeStubConfig {
+public class UtbetalingPortTypeTestConfig {
 
     private static final String FNR = "12345612345";
 
-
     @Bean
     public UtbetalingV1 utbetalingPortType() {
+        if(valueOf(getProperty("utbetal.endpoint.mock", "true"))) {
+            return getUtbetalingV1Stub();
+        }
+        return createUtbetalingPortType();
+    }
+
+    protected UtbetalingV1 getUtbetalingV1Stub() {
         return new UtbetalingV1() {
             @Override
             public void ping() {
@@ -32,6 +45,18 @@ public class UtbetalingPortTypeStubConfig {
                 return new WSHentUtbetalingsinformasjonResponse().withUtbetalingListe(getWsUtbetalinger(ident, request.getPeriode().getFom(), request.getPeriode().getTom()));
             }
         };
+    }
+
+    private UtbetalingV1 createUtbetalingPortType() {
+        JaxWsProxyFactoryBean proxyFactoryBean = new JaxWsProxyFactoryBean();
+        proxyFactoryBean.setWsdlLocation("classpath:utbetaling/no/nav/tjeneste/virksomhet/utbetaling/v1/Binding.wsdl");
+        proxyFactoryBean.setAddress(getProperty("utbetalingendpoint.url"));
+        proxyFactoryBean.setServiceClass(UtbetalingV1.class);
+        proxyFactoryBean.setServiceName(new QName("http://nav.no/tjeneste/virksomhet/utbetaling/v1/Binding", "Utbetaling_v1"));
+        proxyFactoryBean.setEndpointName(new QName("http://nav.no/tjeneste/virksomhet/utbetaling/v1/Binding", "Utbetaling_v1Port"));
+        proxyFactoryBean.getFeatures().add(new WSAddressingFeature());
+        proxyFactoryBean.getFeatures().add(new LoggingFeature());
+        return proxyFactoryBean.create(UtbetalingV1.class);
     }
 
 }
