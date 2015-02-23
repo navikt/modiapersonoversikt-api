68068f1c515412428999ba04c5b4fee3943b28d2
Henrik Hellerøy <henrik.helleroy@nav.no>
2015-02-23 10:33:29 +0100

    Fjernet Karma dependency-feil

--- src/test/java/no/nav/sbl/dialogarena/reactkomponenter/JSTests.java
+++ src/test/java/no/nav/sbl/dialogarena/reactkomponenter/JSTests.java
@@ -1,9 +1,4 @@
 package no.nav.sbl.dialogarena.reactkomponenter;
 
-import de.is24.util.karmatestrunner.junit.KarmaTestSuiteRunner;
-import org.junit.runner.RunWith;
-
-@RunWith(KarmaTestSuiteRunner.class)
-@KarmaTestSuiteRunner.KarmaConfigPath("karma.conf.js")
 public class JSTests {
 }
