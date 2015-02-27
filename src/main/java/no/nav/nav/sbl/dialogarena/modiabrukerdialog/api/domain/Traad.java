6d1a6acbc49f552f461d42a24a611bed7d5e9ad4
Nicklas Utgaard <nicklas.utgaard@nav.no>
2015-02-27 13:53:58 +0100

    PK-12029: Flyttet VisningsUtils til api slik at det kan brukes i Traad domenet

--- src/main/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/api/domain/Traad.java
+++ src/main/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/api/domain/Traad.java
@@ -1,5 +1,6 @@
 package no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain;
 
+import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.utils.VisningUtils;
 import org.apache.commons.collections15.Transformer;
 import org.joda.time.DateTime;
 
@@ -12,7 +13,7 @@ import static no.nav.modig.lang.collections.ComparatorUtils.compareWith;
 import static no.nav.modig.lang.collections.IterUtils.on;
 
 public class Traad {
-    public final String traadId, temagruppe, journalfortTema;
+    public final String traadId, temagruppe, journalfortTema, statusKlasse, meldingStatus;
     public final List<Melding> meldinger;
     public final DateTime dato;
 
@@ -28,6 +29,8 @@ public class Traad {
         this.temagruppe = forsteMelding.temagruppe;
         this.journalfortTema = forsteMelding.journalfortTema;
         this.dato = sisteMelding.opprettetDato;
+        this.statusKlasse = VisningUtils.lagStatusIkonKlasse(meldinger.get(0));
+        this.meldingStatus = VisningUtils.lagMeldingStatusTekstKey(meldinger.get(0));
     }
 
     public static final Transformer<Traad, DateTime> DATO = new Transformer<Traad, DateTime>() {
---
+++ src/main/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/api/utils/VisningUtils.java
@@ -0,0 +1,34 @@
+package no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.utils;
+
+import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain.Melding;
+import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain.Meldingstype;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain.Meldingstype.*;
+import static no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain.Status.LEST_AV_BRUKER;
+
+public class VisningUtils {
+    public static final List<Meldingstype> FRA_BRUKER = asList(SPORSMAL_SKRIFTLIG, SVAR_SBL_INNGAAENDE);
+    public static final List<Meldingstype> FRA_NAV = asList(SVAR_SKRIFTLIG, SVAR_OPPMOTE, SVAR_TELEFON, SAMTALEREFERAT_OPPMOTE, SAMTALEREFERAT_TELEFON, SPORSMAL_MODIA_UTGAAENDE);
+    public static final List<Meldingstype> SPORSMAL = asList(SPORSMAL_SKRIFTLIG, SPORSMAL_MODIA_UTGAAENDE);
+
+    public static String lagStatusIkonKlasse(Melding melding) {
+        String meldingstype = melding.meldingstype.name().substring(0, melding.meldingstype.name().indexOf("_")).toLowerCase();
+        if (FRA_BRUKER.contains(melding.meldingstype)) {
+            return meldingstype;
+        } else {
+            return String.format("%s-%s", meldingstype, melding.status.name()).toLowerCase().replace("_", "-");
+        }
+    }
+
+    public static String lagMeldingStatusTekstKey(Melding melding) {
+        String key = String.format("melding.status.%s", melding.meldingstype.name());
+        if (FRA_NAV.contains(melding.meldingstype)) {
+            key += String.format(".%s", melding.status == LEST_AV_BRUKER ? "lest" : "ulest");
+        }
+        return key;
+    }
+
+}
