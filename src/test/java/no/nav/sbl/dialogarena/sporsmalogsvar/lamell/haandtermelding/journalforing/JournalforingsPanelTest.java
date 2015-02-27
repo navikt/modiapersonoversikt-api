0ca0275bd5d629b8eb994284806216644f927c39
Henrik Hellerøy <henrik.helleroy@nav.no>
2015-02-27 12:02:25 +0100

    PK-15300: Test for teknisk feil i JournalforingsPanel

---
+++ src/test/java/no/nav/sbl/dialogarena/sporsmalogsvar/lamell/haandtermelding/journalforing/JournalforingsPanelTest.java
@@ -0,0 +1,46 @@
+package no.nav.sbl.dialogarena.sporsmalogsvar.lamell.haandtermelding.journalforing;
+
+import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.service.SakerService;
+import no.nav.sbl.dialogarena.sporsmalogsvar.config.ServiceTestContext;
+import no.nav.sbl.dialogarena.sporsmalogsvar.config.WicketPageTest;
+import no.nav.sbl.dialogarena.sporsmalogsvar.consumer.HenvendelseBehandlingService;
+import no.nav.sbl.dialogarena.sporsmalogsvar.lamell.InnboksVM;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.inject.Inject;
+
+import static no.nav.modig.wicket.test.matcher.ComponentMatchers.*;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.springframework.test.annotation.DirtiesContext.ClassMode.AFTER_CLASS;
+
+@ContextConfiguration(classes = {ServiceTestContext.class})
+@DirtiesContext(classMode = AFTER_CLASS)
+@RunWith(SpringJUnit4ClassRunner.class)
+public class JournalforingsPanelTest extends WicketPageTest {
+
+    @Inject
+    private SakerService sakerService;
+    @Inject
+    private HenvendelseBehandlingService henvendelseBehandlingService;
+
+    @Test
+    public void viserTekniskFeilHvisSakerServiceTryner() throws Exception {
+        doThrow(Exception.class).when(sakerService).hentListeAvSaker(anyString());
+        doThrow(Exception.class).when(sakerService).hentSaker(anyString());
+
+        JournalforingsPanel journalforingsPanel = new JournalforingsPanel("id", new InnboksVM("", henvendelseBehandlingService));
+        journalforingsPanel.togglePanel(mock(AjaxRequestTarget.class));
+
+        wicket.goToPageWith(journalforingsPanel)
+                .should().containComponent(thatIsVisible().and(withId("tekniskFeilContainer")))
+                .should().containComponent(thatIsInvisible().and(withId("journalforingsPanelEnkeltSak")))
+                .should().containComponent(thatIsInvisible().and(withId("journalforingsPanelVelgSak")));
+    }
+}
