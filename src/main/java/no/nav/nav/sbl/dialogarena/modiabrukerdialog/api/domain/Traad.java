984fa34ac41c7231e7e33f8d9825eb9528598551
Nicklas Utgaard <nicklas.utgaard@nav.no>
2015-03-02 10:12:51 +0100

    PK-12029: StatusKlasse lagt til som felt for hver meldign

--- src/main/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/api/domain/Traad.java
+++ src/main/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/api/domain/Traad.java
@@ -10,7 +10,6 @@ import static java.util.Arrays.asList;
 import static java.util.Collections.reverseOrder;
 import static no.nav.modig.lang.collections.ComparatorUtils.compareWith;
 import static no.nav.modig.lang.collections.IterUtils.on;
-import static no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.utils.VisningUtils.lagStatusIkonKlasse;
 
 public class Traad {
     public final String traadId, temagruppe, journalfortTema, statusKlasse, statusTekst;
@@ -29,7 +28,7 @@ public class Traad {
         this.temagruppe = forsteMelding.temagruppeNavn;
         this.journalfortTema = forsteMelding.journalfortTemanavn;
         this.dato = sisteMelding.opprettetDato;
-        this.statusKlasse = lagStatusIkonKlasse(sisteMelding);
+        this.statusKlasse = sisteMelding.statusKlasse;
         this.statusTekst = sisteMelding.statusTekst;
     }
 
