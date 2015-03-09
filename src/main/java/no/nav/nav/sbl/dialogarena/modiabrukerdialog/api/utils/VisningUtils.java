11eb3c079e22534f3ff9d80a76efc629d6f9e51c
Nicklas Utgaard <nicklas.utgaard@nav.no>
2015-03-09 11:59:35 +0100

    PK-12029: omskriving av henvendelsesøket til å bruke store istedet for søklayout.

--- src/main/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/api/utils/VisningUtils.java
+++ src/main/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/api/utils/VisningUtils.java
@@ -2,6 +2,7 @@ package no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.utils;
 
 import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain.Melding;
 import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain.Meldingstype;
+import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain.Status;
 
 import java.util.List;
 
@@ -11,15 +12,9 @@ import static no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain.Meldingsty
 public class VisningUtils {
     public static final List<Meldingstype> FRA_BRUKER = asList(SPORSMAL_SKRIFTLIG, SVAR_SBL_INNGAAENDE);
     public static final List<Meldingstype> FRA_NAV = asList(SVAR_SKRIFTLIG, SVAR_OPPMOTE, SVAR_TELEFON, SAMTALEREFERAT_OPPMOTE, SAMTALEREFERAT_TELEFON, SPORSMAL_MODIA_UTGAAENDE);
-    public static final List<Meldingstype> SPORSMAL = asList(SPORSMAL_SKRIFTLIG, SPORSMAL_MODIA_UTGAAENDE);
 
     public static String lagStatusIkonKlasse(Melding melding) {
-        String meldingstype = melding.meldingstype.name().substring(0, melding.meldingstype.name().indexOf("_")).toLowerCase();
-        if (FRA_BRUKER.contains(melding.meldingstype)) {
-            return meldingstype;
-        } else {
-            return String.format("%s-%s", meldingstype, melding.status.name()).toLowerCase().replace("_", "-");
-        }
+        return String.format("statusIkon %s", melding.status == Status.IKKE_BESVART ? "ubesvart" : "besvart");
     }
 
     public static String lagMeldingStatusTekstKey(Melding melding) {
