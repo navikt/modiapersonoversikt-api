928b3bf1728f2941a87b229a8fdb41a0c1bdd8ce
Nicklas Utgaard <nicklas.utgaard@nav.no>
2015-03-10 14:40:22 +0100

    PK-12029: Tester for modal

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/modal/tests/test.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/modal/tests/test.js
@@ -27,12 +27,14 @@ describe('Modal', function () {
         var portal = modal.modal.getDOMNode();
 
         expect(portal.hasAttribute('tabindex')).to.be.true;
+        expect(portal.hasAttribute('class')).to.be.true;
         expect(portal.hasAttribute('aria-hidden')).to.be.true;
         expect(portal.hasAttribute('role')).to.be.true;
         expect(portal.hasAttribute('aria-labelledby')).to.be.true;
         expect(portal.hasAttribute('aria-describedby')).to.be.true;
 
         expect(portal.getAttribute('tabindex')).to.be.eql('-1');
+        expect(portal.getAttribute('class')).to.be.eql('hidden');
         expect(portal.getAttribute('aria-hidden')).to.be.eql('true');
         expect(portal.getAttribute('role')).to.be.eql('dialog');
 
@@ -44,6 +46,16 @@ describe('Modal', function () {
 
         expect(labelledBy).not.to.be.null;
         expect(describedBy).not.to.be.null;
+    });
+
+    it('Should repect the isOpen prop', function(){
+        var modal = TestUtils.renderIntoDocument(
+            <Modal isOpen={true}>
+                <span></span>
+            </Modal>
+        );
+        var portal = modal.modal.getDOMNode();
+        expect(portal.getAttribute('class')).not.to.be.eql('hidden');
     });
 
     it('Renderer content til portal.content div', function () {
--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/testConfig.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/testConfig.js
@@ -1,11 +1,11 @@
 window.ModiaJS = {};
 window.ModiaJS.Components = {};
-(function(){
+(function () {
     var Ap = Array.prototype;
     var slice = Ap.slice;
     var Fp = Function.prototype;
 
-    Fp.bind = function(context) {
+    Fp.bind = function (context) {
         var func = this;
         var args = slice.call(arguments, 1);
 
@@ -21,4 +21,41 @@ window.ModiaJS.Components = {};
         return bound;
     };
 })();
+
+//jQuery
+window.$ = require('jquery');
+
+//shim for $(':focusable') since it is part of jQuery UI and we dont need that.
+function visible(element) {
+    return $.expr.filters.visible(element) && !$(element).parents().addBack().filter(function () {
+            return $.css(this, 'visibility') === 'hidden';
+        }).length;
+}
+function focusable(element, isTabIndexNotNaN) {
+    var map, mapName, img, nodeName = element.nodeName.toLowerCase();
+
+    if ('area' === nodeName) {
+        map = element.parentNode;
+        mapName = map.name;
+        if (!element.href || !mapName || map.nodeName.toLocaleLowerCase() !== 'map') {
+            return false;
+        }
+        img = $('img[usemap=#' + mapName + ']')[0];
+        return !!img && visible(img);
+    }
+    return (/input|select|textarea|button|object/.test(nodeName) ?
+            !element.disabled :
+            'a' === nodeName ?
+            element.href || isTabIndexNotNaN :
+                isTabIndexNotNaN) &&
+        visible(element);
+}
+
+$.extend($.expr[':'], {
+    focusable: function(element) {
+        return focusable(element, !isNaN($.attr(element, 'tabindex')));
+    }
+});
+
+
 module.exports = {};
