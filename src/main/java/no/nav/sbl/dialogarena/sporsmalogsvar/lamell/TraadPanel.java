e22027f6889343a590089c1badab1c64c9af5342
Nicklas Utgaard <nicklas.utgaard@nav.no>
2015-03-13 09:15:33 +0100

    La til lest/ulest tekst i TraadPanel visning.

--- src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/lamell/TraadPanel.java
+++ src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/lamell/TraadPanel.java
@@ -9,6 +9,9 @@ import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.PropertyModel;
 
 import static java.util.Arrays.asList;
+import static no.nav.modig.wicket.conditional.ConditionalUtils.visibleIf;
+import static no.nav.modig.wicket.model.ModelUtils.isEqualTo;
+import static no.nav.modig.wicket.model.ModelUtils.not;
 import static no.nav.modig.wicket.shortcuts.Shortcuts.cssClass;
 import static no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain.Meldingstype.SPORSMAL_SKRIFTLIG;
 import static no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain.Meldingstype.SVAR_SBL_INNGAAENDE;
@@ -21,11 +24,15 @@ public class TraadPanel extends Panel {
             protected void populateItem(ListItem<MeldingVM> item) {
                 String meldingTypeKlasse = meldingKlasse(item.getModelObject());
                 item.add(cssClass(meldingTypeKlasse));
+                PropertyModel<String> lestStatusModel = new PropertyModel<>(item.getModel(), "melding.lestStatus");
 
                 item.add(new FeilsendtInfoPanel("feilsendtInfo", item.getModel()));
                 item.add(new AvsenderBilde("avsenderBilde", item.getModel()).add(cssClass(meldingTypeKlasse)));
-                item.add(new Label("temagruppe", new PropertyModel<String>(item.getModel(), "melding.temagruppeNavn")));
                 item.add(new Label("meldingstatus", new PropertyModel<String>(item.getModel(), "melding.statusTekst")));
+                item.add(new Label("lestStatus", lestStatusModel)
+                        .add(visibleIf(not(isEqualTo(lestStatusModel, "")))));
+
+                item.add(new Label("temagruppe", new PropertyModel<String>(item.getModel(), "melding.temagruppeNavn")));
                 item.add(new Label("avsenderTekst"));
                 item.add(new URLParsingMultiLineLabel("fritekst", new PropertyModel<String>(item.getModel(), "melding.fritekst")));
                 item.add(new Journalpost("journalpost", item.getModel()));
