011aa664b8b87e488d737beca5070dd2731dec05
Ingrid Hjulstad <ingrid.hjulstad@nav.no>
2015-03-24 15:28:16 +0100

    PK-12029: Interaksjonsfiks i Meldingssøket. I trådvisningen skal det synes hvor mange meldinger som treffer i søket av totalt antall i hver tråd.

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/henvendelseSok/ForhandsvisningKomponent.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/henvendelseSok/ForhandsvisningKomponent.js
@@ -2,19 +2,28 @@ var React = require('react');
 var Melding = require('./Melding');
 
 var ForhandsvisningKomponent = React.createClass({
-    render: function(){
-        if (!this.props.traad.hasOwnProperty('meldinger')){
+    render: function() {
+        if (!this.props.traad.hasOwnProperty('meldinger')) {
             return null;
         }
 
-        var meldinger = this.props.traad.meldinger.map(function(melding){
+        var meldinger = this.props.traad.meldinger;
+
+        var meldingElementer = meldinger.map(function(melding){
             return <Melding key={melding.id} melding={melding} />
         });
 
         return (
             <div>
                 <div className="traadPanel">
-                    {meldinger}
+                    <div className="traadinfo">
+                        {"Viser "}
+                        <span className="antall-meldinger">{meldinger.length}</span>
+                        {" av "}
+                        <span className="antall-meldinger">{meldinger[0].antallMeldingerITraad}</span>
+                        {" " + (meldinger[0].antallMeldingerITraad === "1" ? "melding" : "meldinger") + " i dialogen"}
+                    </div>
+                    <div>{meldingElementer}</div>
                 </div>
                 <div className="velgPanel">
                     <input type="submit" value="Velg tråd" className="knapp-hoved-liten" />
