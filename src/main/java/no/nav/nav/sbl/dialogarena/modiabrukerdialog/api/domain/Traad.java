bc190e66ca78eade4c7b77d7864f927a5019682f
Ingrid Hjulstad <ingrid.hjulstad@nav.no>
2015-03-25 16:06:54 +0100

    Refaktorering meldingersøk: Flyttet antallMeldingerIOpprinneligTraad til Traad, i stedet for Melding. Indekserer ikke dette feltet per melding, men henter det ut fra cachen per tråd.

--- src/main/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/api/domain/Traad.java
+++ src/main/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/api/domain/Traad.java
@@ -6,7 +6,6 @@ import org.joda.time.DateTime;
 import java.util.Comparator;
 import java.util.List;
 
-import static java.util.Arrays.asList;
 import static java.util.Collections.reverseOrder;
 import static no.nav.modig.lang.collections.ComparatorUtils.compareWith;
 import static no.nav.modig.lang.collections.IterUtils.on;
@@ -15,13 +14,11 @@ public class Traad {
     public final String traadId, temagruppe, journalfortTema, statusKlasse, statusTekst;
     public final List<Melding> meldinger;
     public final DateTime dato;
+    public final int antallMeldingerIOpprinneligTraad;
 
-    public Traad(String traadId, Melding... meldinger) {
-        this(traadId, asList(meldinger));
-    }
-
-    public Traad(String traadId, List<Melding> meldinger) {
+    public Traad(String traadId, int antallMeldingerIOpprinneligTraad, List<Melding> meldinger) {
         this.traadId = traadId;
+        this.antallMeldingerIOpprinneligTraad = antallMeldingerIOpprinneligTraad;
         this.meldinger = on(meldinger).collect(Melding.NYESTE_FORST);
         Melding forsteMelding = this.meldinger.get(this.meldinger.size() - 1);
         Melding sisteMelding = this.meldinger.get(0);
