53058e81d47094ed7c611128526763a49eb6bc4e
Andreas Baakind <andreas.baakind@nav.no>
2015-04-14 12:06:50 +0200

    Endret test som har endret grunnlag

--- src/test/java/no/nav/sbl/dialogarena/utbetaling/lamell/filter/FilterFormPanelTest.java
+++ src/test/java/no/nav/sbl/dialogarena/utbetaling/lamell/filter/FilterFormPanelTest.java
@@ -1,9 +1,6 @@
 package no.nav.sbl.dialogarena.utbetaling.lamell.filter;
 
 import no.nav.modig.wicket.component.daterangepicker.DateRangePicker;
-import no.nav.sbl.dialogarena.common.records.Record;
-import no.nav.sbl.dialogarena.utbetaling.domain.Hovedytelse;
-import no.nav.sbl.dialogarena.utbetaling.domain.Mottakertype;
 import no.nav.sbl.dialogarena.utbetaling.wickettest.AbstractWicketTest;
 import org.apache.wicket.ajax.markup.html.form.AjaxCheckBox;
 import org.apache.wicket.markup.html.form.Form;
@@ -15,9 +12,6 @@ import java.util.Set;
 import static java.util.Arrays.asList;
 import static no.nav.modig.wicket.test.matcher.ComponentMatchers.ofType;
 import static no.nav.modig.wicket.test.matcher.ComponentMatchers.withId;
-import static org.joda.time.DateTime.now;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 
 public class FilterFormPanelTest extends AbstractWicketTest{
@@ -49,28 +43,4 @@ public class FilterFormPanelTest extends AbstractWicketTest{
                 .should().inComponent(Form.class).containComponent(ofType(DateRangePicker.class));
     }
 
-    @Test
-    public void testYtelseFilterBryter() {
-        Record<Hovedytelse> ytelse = new Record<Hovedytelse>()
-                .with(Hovedytelse.id, ID)
-                .with(Hovedytelse.ytelse, DAGPENGER)
-                .with(Hovedytelse.mottakertype, Mottakertype.BRUKER)
-                .with(Hovedytelse.hovedytelsedato, now());
-
-        assertTrue(filterParametre.evaluate(ytelse));
-
-        utbetalingEvaluererTrueOmYtelsenBlirValgt(ytelse);
-        utbetalingEvaluererFalseOmYtelseIkkeErValgt(ytelse);
-        utbetalingEvaluererTrueOmYtelsenBlirValgt(ytelse);
-    }
-
-    private void utbetalingEvaluererTrueOmYtelsenBlirValgt(Record<Hovedytelse> ytelse) {
-        wicketTester.click().ajaxCheckbox(withId("visYtelse"));
-        assertTrue(filterParametre.evaluate(ytelse));
-    }
-
-    private void utbetalingEvaluererFalseOmYtelseIkkeErValgt(Record<Hovedytelse> ytelse) {
-        wicketTester.click().ajaxCheckbox(withId("visYtelse"));
-        assertFalse(filterParametre.evaluate(ytelse));
-    }
 }
