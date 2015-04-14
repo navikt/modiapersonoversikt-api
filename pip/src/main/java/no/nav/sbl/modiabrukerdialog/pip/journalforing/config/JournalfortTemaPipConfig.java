c6d057d9a69299e8559b140a69a0350a947d931d
Henrik Hellerøy <henrik.helleroy@nav.no>
2015-04-14 15:55:47 +0200

    AOP caching av tjenestekall i JournalfortTemaPip

--- pip/src/main/java/no/nav/sbl/modiabrukerdialog/pip/journalforing/config/JournalfortTemaPipConfig.java
+++ pip/src/main/java/no/nav/sbl/modiabrukerdialog/pip/journalforing/config/JournalfortTemaPipConfig.java
@@ -1,6 +1,7 @@
 package no.nav.sbl.modiabrukerdialog.pip.journalforing.config;
 
 import _0._0.nav_cons_sak_gosys_3.no.nav.inf.navansatt.GOSYSNAVansatt;
+import net.sf.ehcache.CacheManager;
 import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.service.AnsattService;
 import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.service.SaksbehandlerInnstillingerService;
 import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.endpoint.v1.norg.NAVAnsattEndpointConfig;
@@ -8,18 +9,30 @@ import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.service.AnsattServiceIm
 import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.service.DefaultSaksbehandlerInnstillingerService;
 import no.nav.sbl.modiabrukerdialog.pip.journalforing.support.DefaultJournalfortTemaAttributeLocatorDelegate;
 import no.nav.sbl.modiabrukerdialog.pip.journalforing.support.JournalfortTemaAttributeLocatorDelegate;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
 
 import javax.inject.Inject;
 
 @Configuration
-@Import(NAVAnsattEndpointConfig.class)
+@EnableCaching
+@ImportResource("classpath*:*cacheconfig.xml")
+@Import({NAVAnsattEndpointConfig.class})
 public class JournalfortTemaPipConfig {
 
     @Inject
     private GOSYSNAVansatt gosysNAVAnsatt;
+
+    @Bean
+    public EhCacheCacheManager cacheManager() {
+        EhCacheCacheManager cacheManager = new EhCacheCacheManager();
+        cacheManager.setCacheManager(CacheManager.create());
+        return cacheManager;
+    }
 
     @Bean
     public JournalfortTemaAttributeLocatorDelegate enhetAttributeLocatorDelegate() {
