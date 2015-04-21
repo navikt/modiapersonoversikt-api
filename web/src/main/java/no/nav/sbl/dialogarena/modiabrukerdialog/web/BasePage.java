41ca42f7c705fc8a9627a99cdddc5c301e51110f
Frithjof Nerdrum <frithjof.nerdrum@nav.no>
2015-04-21 10:06:04 +0200

    Ordnet less-filene i forskjellige mapper avhengig av om det er en del av rammen til Modia, eller bare tilhører en spesifikk komponent.

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/BasePage.java
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/BasePage.java
@@ -22,23 +22,22 @@ public class BasePage extends WebPage {
     public static final CssResourceReference PERSONINFO_LESS = new CssResourceReference(PersonPage.class, "personpage.less");
     public static final CssResourceReference PERSONSOKRESULT = new CssResourceReference(PersonsokResultPanel.class, "PersonsokResultPanel.css");
     public static final CssResourceReference PERSONSOKSEARCH = new CssResourceReference(PersonsokSearchPanel.class, "PersonsokSearchPanel.css");
-    public static final PackageResourceReference MODIA_COMMON_LESS = new PackageResourceReference(BasePage.class, "less/common.less");
-    public static final PackageResourceReference MODIA_WIDGET_LESS = new PackageResourceReference(BasePage.class, "less/widget.less");
-    public static final PackageResourceReference MODIA_LAMELL_LESS = new PackageResourceReference(BasePage.class, "less/lamell.less");
-    public static final PackageResourceReference MODIA_LAYOUT_LESS = new PackageResourceReference(BasePage.class, "less/layout.less");
-    public static final PackageResourceReference HEADER = new PackageResourceReference(BasePage.class, "less/header.less");
-    public static final PackageResourceReference RESPONSIVE = new PackageResourceReference(BasePage.class, "less/responsive.less");
+    public static final PackageResourceReference MODIA_COMMON_LESS = new PackageResourceReference(BasePage.class, "less/felles/common.less");
+    public static final PackageResourceReference MODIA_WIDGET_LESS = new PackageResourceReference(BasePage.class, "less/felles/widget.less");
+    public static final PackageResourceReference MODIA_LAMELL_LESS = new PackageResourceReference(BasePage.class, "less/felles/lamell.less");
+    public static final PackageResourceReference MODIA_LAYOUT_LESS = new PackageResourceReference(BasePage.class, "less/felles/layout.less");
+    public static final PackageResourceReference RESPONSIVE = new PackageResourceReference(BasePage.class, "less/felles/responsive.less");
 
-
+    public static final PackageResourceReference HEADER = new PackageResourceReference(BasePage.class, "less/komponenter/header.less");
     public static final PackageResourceReference MELDINGERWIDGET = new PackageResourceReference(MeldingerWidget.class, "meldingerwidget.less");
     public static final PackageResourceReference MELDINGERLAMELL = new PackageResourceReference(Innboks.class, "innboks.less");
-    public static final PackageResourceReference OPPFOLGING = new PackageResourceReference(BasePage.class, "less/oppfolging.less");
-    public static final PackageResourceReference OPPGAVEFORM = new PackageResourceReference(BasePage.class, "less/oppgaveform.less");
-    public static final PackageResourceReference PERSONSOK = new PackageResourceReference(BasePage.class, "less/personsok.less");
-    public static final PackageResourceReference SAKBEHANDLERINNSTILLINGER = new PackageResourceReference(BasePage.class, "less/sakbehandlerinnstillinger.less");
-    public static final PackageResourceReference LISTE = new PackageResourceReference(BasePage.class, "less/liste.less");
-    public static final PackageResourceReference JOURNALFORING = new PackageResourceReference(BasePage.class, "less/journalforing.less");
-    public static final PackageResourceReference BRUKERPROFIL = new PackageResourceReference(BasePage.class, "less/brukerprofil.less");
+    public static final PackageResourceReference OPPFOLGING = new PackageResourceReference(BasePage.class, "less/komponenter/oppfolging.less");
+    public static final PackageResourceReference OPPGAVEFORM = new PackageResourceReference(BasePage.class, "less/komponenter/oppgaveform.less");
+    public static final PackageResourceReference PERSONSOK = new PackageResourceReference(BasePage.class, "less/komponenter/personsok.less");
+    public static final PackageResourceReference SAKBEHANDLERINNSTILLINGER = new PackageResourceReference(BasePage.class, "less/komponenter/sakbehandlerinnstillinger.less");
+    public static final PackageResourceReference LISTE = new PackageResourceReference(BasePage.class, "less/komponenter/liste.less");
+    public static final PackageResourceReference JOURNALFORING = new PackageResourceReference(BasePage.class, "less/komponenter/journalforing.less");
+    public static final PackageResourceReference BRUKERPROFIL = new PackageResourceReference(BasePage.class, "less/komponenter/brukerprofil.less");
 
     private final WebMarkupContainer body;
 
