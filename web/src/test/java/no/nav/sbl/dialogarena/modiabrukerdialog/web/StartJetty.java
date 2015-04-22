053847ecff6f0a1b3c996b966ac610917d8acd40
Andreas Lund <andreas.lund@nav.no>
2015-04-22 12:30:52 +0200

    Oppgraderer Jetty

--- web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/StartJetty.java
+++ web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/StartJetty.java
@@ -4,19 +4,13 @@ import no.nav.modig.security.loginmodule.DummyRole;
 import no.nav.sbl.dialogarena.common.jetty.Jetty;
 import org.eclipse.jetty.jaas.JAASLoginService;
 
-import java.io.File;
-
 import static java.lang.Boolean.TRUE;
 import static java.lang.System.setProperty;
-import static no.nav.modig.core.test.FilesAndDirs.TEST_RESOURCES;
-import static no.nav.modig.core.test.FilesAndDirs.WEBAPP_SOURCE;
 import static no.nav.modig.lang.collections.FactoryUtils.gotKeypress;
 import static no.nav.modig.lang.collections.RunnableUtils.first;
 import static no.nav.modig.lang.collections.RunnableUtils.waitFor;
-import static no.nav.modig.testcertificates.TestCertificates.setupKeyAndTrustStore;
 import static no.nav.sbl.dialogarena.common.jetty.Jetty.usingWar;
 import static no.nav.sbl.dialogarena.modiabrukerdialog.consumer.util.TLSOppsettUtils.SKRU_AV_SERTIFIKATSJEKK_LOKALT;
-import static no.nav.sbl.dialogarena.test.SystemProperties.setFrom;
 
 
 /**
@@ -25,25 +19,17 @@ import static no.nav.sbl.dialogarena.test.SystemProperties.setFrom;
  * NB!
  * Sett start.properties for å styre integrasjon.
  */
-public class StartJetty {
+public final class StartJetty {
 
-    public static void main(String[] args) {
-        setupProperties();
-        runJetty();
-    }
-
-    private static void setupProperties() {
+    public static void main(String... args) throws Exception {
         setProperty("wicket.configuration", "development");
         setProperty(SKRU_AV_SERTIFIKATSJEKK_LOKALT, TRUE.toString());
-        setFrom("jetty-environment.properties");
-        setupKeyAndTrustStore();
-    }
 
-    private static void runJetty() {
-        Jetty jetty = usingWar(WEBAPP_SOURCE)
+        Jetty jetty = usingWar()
                 .at("modiabrukerdialog")
                 .port(8083)
-                .overrideWebXml(new File(TEST_RESOURCES, "override-web.xml"))
+                .loadProperties("/jetty-environment.properties")
+                .overrideWebXml()
                 .withLoginService(createLoginService())
                 .buildJetty();
         jetty.startAnd(first(waitFor(gotKeypress())).then(jetty.stop));
