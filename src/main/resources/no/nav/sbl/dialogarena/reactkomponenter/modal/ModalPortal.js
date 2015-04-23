19fd8c2d35128a46fb90ce1d848b0553aedd03cb
Henrik Hellerøy <henrik.helleroy@nav.no>
2015-04-23 11:55:32 +0200

    PKFEIL-10011: Modalene rydder opp etter seg i DOMen ved lukking

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/modal/ModalPortal.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/modal/ModalPortal.js
@@ -23,7 +23,6 @@ var ModalPortal = React.createClass({
     },
     componentDidUpdate: function () {
         if (this.props.isOpen) {
-            $(document.body).addClass('modal-open');
             $(document.body).children().not(this.getDOMNode().parentNode).attr('aria-hidden', true);
 
             if (!$.contains(this.refs.content.getDOMNode(), document.activeElement)) {
@@ -32,7 +31,6 @@ var ModalPortal = React.createClass({
         } else {
             $(document.body).children().not(this.getDOMNode().parentNode).removeAttr('aria-hidden');
             this.restoreFocus();
-            $(document.body).removeClass('modal-open');
         }
     },
     keyHandler: function (event) {
--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/modal/index.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/modal/index.js
@@ -32,8 +32,6 @@ var Modal = React.createClass({
             this.portalElement.className = "react-modal-container";
             document.body.appendChild(this.portalElement);
         }
-
-        this.renderPortal(this.props, this.state);
     },
     componentWillReceiveProps: function (props) {
         this.renderPortal(props, this.state)
@@ -41,14 +39,31 @@ var Modal = React.createClass({
     componentWillUnmount: function () {
         document.body.removeChild(this.portalElement);
     },
-    componentDidUpdate: function(){
+    componentDidUpdate: function () {
         this.renderPortal(this.props, this.state)
     },
     open: function () {
+        var elementsByClassName = document.getElementsByClassName('react-modal-container');
+        var match = false;
+        for(var i = 0; i < elementsByClassName.length; i++) {
+            if (elementsByClassName[i].innerHTML === this.portalElement.innerHTML) {
+                match = true;
+                break;
+            }
+        }
+
+        if (!match) {
+            document.body.appendChild(this.portalElement);
+        }
+
+        $(document.body).addClass('modal-open');
         this.setState({isOpen: true});
     },
     close: function () {
         this.setState({isOpen: false});
+        React.unmountComponentAtNode(this.portalElement);
+        document.body.removeChild(this.portalElement);
+        $(document.body).removeClass('modal-open');
     },
     renderPortal: function (props, state) {
         var modal = {
