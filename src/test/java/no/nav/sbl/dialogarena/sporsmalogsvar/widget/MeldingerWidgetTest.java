df6311bac294745bdb715db6b9029407dcf645d7
Frithjof Nerdrum <frithjof.nerdrum@nav.no>
2015-04-24 09:25:58 +0200

    Sonar likte ikke at vi ignorte tester. Så vi sletter dem istedenfor.

--- src/test/java/no/nav/sbl/dialogarena/sporsmalogsvar/widget/MeldingerWidgetTest.java
+++ src/test/java/no/nav/sbl/dialogarena/sporsmalogsvar/widget/MeldingerWidgetTest.java
@@ -1,11 +1,10 @@
 package no.nav.sbl.dialogarena.sporsmalogsvar.widget;
 
 import no.nav.modig.wicket.test.EventGenerator;
-import no.nav.sbl.dialogarena.sporsmalogsvar.config.WicketPageTest;
 import no.nav.sbl.dialogarena.sporsmalogsvar.config.ServiceTestContext;
+import no.nav.sbl.dialogarena.sporsmalogsvar.config.WicketPageTest;
 import no.nav.sbl.dialogarena.sporsmalogsvar.consumer.HenvendelseBehandlingService;
 import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
@@ -16,7 +15,6 @@ import javax.inject.Inject;
 import static java.util.Arrays.asList;
 import static no.nav.modig.modia.events.InternalEvents.MELDING_SENDT_TIL_BRUKER;
 import static no.nav.modig.wicket.test.matcher.ComponentMatchers.ofType;
-import static no.nav.modig.wicket.test.matcher.ComponentMatchers.withId;
 import static no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain.Meldingstype.SPORSMAL_SKRIFTLIG;
 import static no.nav.sbl.dialogarena.sporsmalogsvar.lamell.TestUtils.createMelding;
 import static org.joda.time.DateTime.now;
@@ -34,49 +32,6 @@ public class MeldingerWidgetTest extends WicketPageTest {
     public void konstrueresRiktig() {
         when(henvendelseBehandlingService.hentMeldinger(anyString())).thenReturn(asList(createMelding("id1", SPORSMAL_SKRIFTLIG, now(), "ARBD", "id1")));
         wicket.goToPageWith(new MeldingerWidget("meldinger", "M", "fnr"));
-    }
-
-    @Ignore
-    @Test
-    public void inneholderRiktigAntallMeldinger() {
-        when(henvendelseBehandlingService.hentMeldinger(anyString())).thenReturn(asList(
-                createMelding("id1", SPORSMAL_SKRIFTLIG, now(), "ARBD", "id1"),
-                createMelding("id2", SPORSMAL_SKRIFTLIG, now(), "ARBD", "id2"),
-                createMelding("id3", SPORSMAL_SKRIFTLIG, now(), "ARBD", "id3"),
-                createMelding("id4", SPORSMAL_SKRIFTLIG, now(), "ARBD", "id4")
-        ));
-        wicket.goToPageWith(new MeldingerWidget("meldinger", "M", "fnr"))
-                .should().containComponents(4, ofType(MeldingerWidgetPanel.class));
-    }
-
-    @Ignore
-    @Test
-    public void inneholdeMaksFemMeldinger() {
-        when(henvendelseBehandlingService.hentMeldinger(anyString())).thenReturn(asList(
-                createMelding("id1", SPORSMAL_SKRIFTLIG, now(), "ARBD", "id1"),
-                createMelding("id2", SPORSMAL_SKRIFTLIG, now(), "ARBD", "id2"),
-                createMelding("id3", SPORSMAL_SKRIFTLIG, now(), "ARBD", "id3"),
-                createMelding("id4", SPORSMAL_SKRIFTLIG, now(), "ARBD", "id4"),
-                createMelding("id5", SPORSMAL_SKRIFTLIG, now(), "ARBD", "id5")
-        ));
-        wicket.goToPageWith(new MeldingerWidget("meldinger", "M", "fnr"))
-                .should().containComponents(5, ofType(MeldingerWidgetPanel.class));
-    }
-
-    @Ignore
-    @Test
-    public void inneholdeOverflowElementHvisMerEnnFemMeldinger() {
-        when(henvendelseBehandlingService.hentMeldinger(anyString())).thenReturn(asList(
-                createMelding("id1", SPORSMAL_SKRIFTLIG, now(), "ARBD", "id1"),
-                createMelding("id2", SPORSMAL_SKRIFTLIG, now(), "ARBD", "id2"),
-                createMelding("id3", SPORSMAL_SKRIFTLIG, now(), "ARBD", "id3"),
-                createMelding("id4", SPORSMAL_SKRIFTLIG, now(), "ARBD", "id4"),
-                createMelding("id5", SPORSMAL_SKRIFTLIG, now(), "ARBD", "id5"),
-                createMelding("id6", SPORSMAL_SKRIFTLIG, now(), "ARBD", "id6")
-        ));
-        wicket.goToPageWith(new MeldingerWidget("meldinger", "M", "fnr"))
-                .should().containComponents(4, ofType(MeldingerWidgetPanel.class))
-                .should().containComponent(withId("overflowitem"));
     }
 
     @Test
