26fb2b42ccc9f8e8fd2980619a0433d3e76cda38
Frithjof Nerdrum <frithjof.nerdrum@nav.no>
2015-04-29 13:07:07 +0200

    UtbetalingWidget er AsyncWidget

--- src/main/java/no/nav/sbl/dialogarena/utbetaling/widget/UtbetalingWidget.java
+++ src/main/java/no/nav/sbl/dialogarena/utbetaling/widget/UtbetalingWidget.java
@@ -1,44 +1,32 @@
 package no.nav.sbl.dialogarena.utbetaling.widget;
 
-import no.nav.modig.core.exception.ApplicationException;
-import no.nav.modig.core.exception.SystemException;
-import no.nav.modig.modia.widget.FeedWidget;
-import no.nav.modig.modia.widget.panels.ErrorListing;
-import no.nav.modig.modia.widget.panels.GenericListing;
+import no.nav.modig.modia.widget.async.AsyncWidget;
 import no.nav.sbl.dialogarena.common.records.Record;
 import no.nav.sbl.dialogarena.utbetaling.domain.Hovedytelse;
 import no.nav.sbl.dialogarena.utbetaling.service.UtbetalingService;
 import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.StringResourceModel;
-import org.apache.wicket.model.util.ListModel;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import java.util.List;
 
-import static java.util.Arrays.asList;
 import static no.nav.modig.lang.collections.IterUtils.on;
-import static no.nav.sbl.dialogarena.utbetaling.domain.util.YtelseUtils.betweenNowAndMonthsBefore;
-import static no.nav.sbl.dialogarena.utbetaling.domain.util.YtelseUtils.defaultSluttDato;
-import static no.nav.sbl.dialogarena.utbetaling.domain.util.YtelseUtils.defaultStartDato;
+import static no.nav.sbl.dialogarena.utbetaling.domain.util.YtelseUtils.*;
 import static no.nav.sbl.dialogarena.utbetaling.widget.HovedytelseVM.TIL_HOVEDYTELSEVM;
 import static no.nav.sbl.dialogarena.utbetaling.widget.HovedytelseVM.UtbetalingVMComparator;
 
-public class UtbetalingWidget extends FeedWidget<HovedytelseVM> {
+public class UtbetalingWidget extends AsyncWidget<HovedytelseVM> {
 
-    private static final Logger LOG = LoggerFactory.getLogger(UtbetalingWidget.class);
     private static final int MAX_NUMBER_OF_UTBETALINGER = 5;
     public static final int NUMBER_OF_MONTHS_TO_SHOW = 3;
 
+    private final String fnr;
     @Inject
     private UtbetalingService utbetalingService;
 
     public UtbetalingWidget(String id, String initial, String fnr) {
-        super(id, initial, true, "widget.utbetalingWidget.flereUtbetalinger");
-        this.setDefaultModel(lagModell(fnr));
-
-        setMaxNumberOfFeedItems(MAX_NUMBER_OF_UTBETALINGER);
+        super(id, initial, MAX_NUMBER_OF_UTBETALINGER,
+                "utbetalinger.feilet", "widget.utbetalingWidget.flereUtbetalinger");
+        this.fnr = fnr;
     }
 
     protected static List<HovedytelseVM> transformUtbetalingToVM(List<Record<Hovedytelse>> utbetalinger) {
@@ -48,25 +36,10 @@ public class UtbetalingWidget extends FeedWidget<HovedytelseVM> {
                 .collect(new UtbetalingVMComparator());
     }
 
-    protected ListModel<?> lagModell(final String fnr) {
-        List<?> listContent;
-        try {
-            List<Record<Hovedytelse>> hovedytelser = utbetalingService.hentUtbetalinger(fnr, defaultStartDato(), defaultSluttDato());
-            if (hovedytelser.isEmpty()) {
-                listContent = asList(new GenericListing(new StringResourceModel("ingen.utbetalinger", UtbetalingWidget.this, null).getString()));
-            } else {
-                List<HovedytelseVM> hovedytelseVMList = transformUtbetalingToVM(hovedytelser);
-                if(hovedytelseVMList.isEmpty()) {
-                    listContent = asList(new GenericListing(new StringResourceModel("ingen.utbetalinger", UtbetalingWidget.this, null).getString()));
-                } else {
-                    listContent = hovedytelseVMList;
-                }
-            }
-        } catch (ApplicationException | SystemException e) {
-            LOG.warn("Feilet ved henting av utbetalingsinformasjon for fnr {}", fnr, e);
-            listContent = asList(new ErrorListing(new StringResourceModel("utbetalinger.feilet", UtbetalingWidget.this, null).getString()));
-        }
-        return new ListModel<>(listContent);
+    @Override
+    public List<HovedytelseVM> getFeedItems() throws Exception {
+        List<Record<Hovedytelse>> hovedytelser = utbetalingService.hentUtbetalinger(fnr, defaultStartDato(), defaultSluttDato());
+        return transformUtbetalingToVM(hovedytelser);
     }
 
     @Override
