aadbef57a624cc68b08ebd8792a713040b8f503f
Andreas Baakind <andreas.baakind@nav.no>
2015-04-30 08:47:59 +0200

    WCAG Bedre støtte for skjermleser

--- src/test/java/no/nav/sbl/dialogarena/utbetaling/StartJettyUtbetalinger.java
+++ src/test/java/no/nav/sbl/dialogarena/utbetaling/StartJettyUtbetalinger.java
@@ -26,7 +26,7 @@ public final class StartJettyUtbetalinger {
 
     private static void setupProperties() {
         setProperty("wicket.configuration", "development");
-        setProperty("utbetal.endpoint.mock", REAL_DATA);
+        setProperty("utbetal.endpoint.mock", MOCK_DATA);
         setProperty("server.arena.url", "arenaserver");
         setProperty("utbetalingendpoint.url", "https://wasapp-t4.adeo.no/nav-utbetaldata-ws/virksomhet/Utbetaling_v1");
     }
