ca186da98f2d1377c66115672f40d8cdfe8c2b95
Andreas Lund <andreas.lund@nav.no>
2015-05-05 13:59:18 +0200

    PK-18562: Legger til lenke og funksjonalitet for å vise en pdf og feilmelding dersom pdf'en ikke finnes/tjenesten returnerer feil.

--- src/main/java/no/nav/sbl/dialogarena/sak/lamell/KvitteringsPanel.java
+++ src/main/java/no/nav/sbl/dialogarena/sak/lamell/KvitteringsPanel.java
@@ -1,9 +1,15 @@
 package no.nav.sbl.dialogarena.sak.lamell;
 
+import no.nav.modig.wicket.component.modal.ModalAdvarselPanel;
+import no.nav.modig.wicket.component.modal.ModigModalWindow;
 import no.nav.sbl.dialogarena.sak.service.BulletProofKodeverkService;
 import no.nav.sbl.dialogarena.sak.service.BulletproofCmsService;
+import no.nav.sbl.dialogarena.sak.util.ResourceStreamAjaxBehaviour;
 import no.nav.sbl.dialogarena.sak.viewdomain.lamell.Dokument;
 import no.nav.sbl.dialogarena.sak.viewdomain.lamell.Kvittering;
+import org.apache.commons.io.IOUtils;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.head.OnLoadHeaderItem;
 import org.apache.wicket.markup.html.WebMarkupContainer;
@@ -12,10 +18,13 @@ import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.PropertyListView;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.util.resource.AbstractResourceStreamWriter;
+import org.apache.wicket.util.resource.IResourceStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
+import java.io.*;
 import java.util.List;
 
 import static java.lang.String.format;
@@ -33,6 +42,7 @@ public class KvitteringsPanel extends Panel {
     private BulletProofKodeverkService kodeverk;
 
     private Logger logger = LoggerFactory.getLogger(KvitteringsPanel.class);
+    private ModigModalWindow modalWindow;
 
     public KvitteringsPanel(String id, String tittel, Model<Kvittering> kvitteringsModel, String fnr) {
         super(id, kvitteringsModel);
@@ -64,7 +74,7 @@ public class KvitteringsPanel extends Panel {
         leggTilInnsendteVedlegg(kvittering);
         leggTilManglendeVedlegg(kvittering);
         leggTilBehandlingsTidInfo(kvittering);
-
+        leggTilVedleggFeiletPoput();
     }
 
     private void leggTilBehandlingsTidInfo(Kvittering kvittering) {
@@ -82,6 +92,12 @@ public class KvitteringsPanel extends Panel {
         }
 
         add(new Label("behandlingstid", format(behandlingstidTekst, behandlingstid)));
+    }
+
+    private void leggTilVedleggFeiletPoput() {
+        modalWindow = new ModigModalWindow("vedleggFeiletPopup");
+        modalWindow.setContent(new ModalAdvarselPanel(modalWindow, "Dokumentet er slettet", "Dette dokumentet kan ikke vises fordi det er slettet.", "Lukk"));
+        add(modalWindow);
     }
 
     private void leggTilInnsendteVedlegg(Kvittering kvittering) {
@@ -110,6 +126,7 @@ public class KvitteringsPanel extends Panel {
             @Override
             protected void populateItem(ListItem<Dokument> item) {
                 Dokument dokument = item.getModelObject();
+
                 String dokumentTittel = kodeverk.getSkjematittelForSkjemanummer(dokument.kodeverkRef);
                 if (kodeverk.isEgendefinert(dokument.kodeverkRef)) {
                     dokumentTittel += ": " + dokument.tilleggstittel;
@@ -118,6 +135,39 @@ public class KvitteringsPanel extends Panel {
                     item.add(new Label("innsendingsvalg", cms.hentTekst("kvittering.innsendingsvalg." + dokument.innsendingsvalg)));
                 }
                 item.add(new Label("dokument", dokumentTittel));
+
+                if (!visInnsendingsvalg) {
+                    AjaxLink<Void> hentVedleggLenke = new AjaxLink<Void>("hent-vedlegg") {
+                        @Override
+                        public void onClick(AjaxRequestTarget target) {
+                            final byte[] pdfSomBytes = getMockPdf();
+
+                            if (pdfSomBytes.length > 0) {
+                                ResourceStreamAjaxBehaviour resourceStreamAjaxBehavoiur = lagHentPdfAjaxBehaviour(pdfSomBytes);
+                                add(resourceStreamAjaxBehavoiur);
+                                resourceStreamAjaxBehavoiur.init(target);
+                            } else {
+                                modalWindow.show(target);
+                            }
+                        }
+                    };
+
+                    item.add(hentVedleggLenke);
+                }
+            }
+        };
+    }
+
+    private ResourceStreamAjaxBehaviour lagHentPdfAjaxBehaviour(final byte[] pdfSomBytes) {
+        return new ResourceStreamAjaxBehaviour() {
+            @Override
+            protected IResourceStream getResourceStream() {
+                return new AbstractResourceStreamWriter() {
+                    @Override
+                    public void write(OutputStream output) throws IOException {
+                        output.write(pdfSomBytes);
+                    }
+                };
             }
         };
     }
@@ -126,5 +176,14 @@ public class KvitteringsPanel extends Panel {
     public void renderHead(IHeaderResponse response) {
         super.renderHead(response);
         response.render(OnLoadHeaderItem.forScript("addKvitteringsPanelEvents()"));
+    }
+
+    //TODO midlertidig. Byttes ut med ordentlig mock-oppsett når tjenesten kommer mer på plass
+    private byte[] getMockPdf() {
+        try {
+            return IOUtils.toByteArray(getClass().getResourceAsStream("/mock/mock.pdf"));
+        } catch (IOException e) {
+            throw new RuntimeException("IOException ved henting av Mock PDFen", e);
+        }
     }
 }
