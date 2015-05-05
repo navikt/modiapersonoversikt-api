2180813d09910790d81ef4c159cb452f4de88530
Frithjof Nerdrum <frithjof.nerdrum@nav.no>
2015-05-05 12:20:12 +0200

    Operasjon for å hente alle ansatte for en gitt enhet i AnsattService

--- src/main/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/api/service/AnsattService.java
+++ src/main/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/api/service/AnsattService.java
@@ -1,10 +1,12 @@
 package no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.service;
 
+import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain.Ansatt;
 import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain.AnsattEnhet;
 
 import java.util.List;
 
 public interface AnsattService {
-    public List<AnsattEnhet> hentEnhetsliste();
-    public String hentAnsattNavn(String ident);
+    List<AnsattEnhet> hentEnhetsliste();
+    String hentAnsattNavn(String ident);
+    List<Ansatt> ansatteForEnhet(AnsattEnhet enhet);
 }
