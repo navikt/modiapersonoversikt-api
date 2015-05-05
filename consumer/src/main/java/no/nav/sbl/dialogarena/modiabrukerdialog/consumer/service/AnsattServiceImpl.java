b3f47a9540febdc41658e4c39b6176e8460dd791
Frithjof Nerdrum <frithjof.nerdrum@nav.no>
2015-05-05 12:20:10 +0200

    Operasjon for å hente alle ansatte for en gitt enhet i AnsattService

--- consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/service/AnsattServiceImpl.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/service/AnsattServiceImpl.java
@@ -3,6 +3,7 @@ package no.nav.sbl.dialogarena.modiabrukerdialog.consumer.service;
 import _0._0.nav_cons_sak_gosys_3.no.nav.asbo.navansatt.ASBOGOSYSNAVAnsatt;
 import _0._0.nav_cons_sak_gosys_3.no.nav.asbo.navorgenhet.ASBOGOSYSNavEnhet;
 import _0._0.nav_cons_sak_gosys_3.no.nav.inf.navansatt.*;
+import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain.Ansatt;
 import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain.AnsattEnhet;
 import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.service.AnsattService;
 import org.apache.commons.collections15.Transformer;
@@ -35,6 +36,23 @@ public class AnsattServiceImpl implements AnsattService {
             return ansattWS.hentNAVAnsatt(ansattRequest).getAnsattNavn();
         } catch (HentNAVAnsattFaultGOSYSNAVAnsattIkkeFunnetMsg | HentNAVAnsattFaultGOSYSGeneriskfMsg e) {
             throw new RuntimeException("Noe gikk galt ved henting av ansatt med ident " + ident, e);
+        }
+    }
+
+    @Override
+    public List<Ansatt> ansatteForEnhet(AnsattEnhet enhet) {
+        ASBOGOSYSNavEnhet request = new ASBOGOSYSNavEnhet();
+        request.setEnhetsId(enhet.enhetId);
+        request.setEnhetsNavn(enhet.enhetNavn);
+        try {
+            return on(ansattWS.hentNAVAnsattListe(request).getNAVAnsatte()).map(new Transformer<ASBOGOSYSNAVAnsatt, Ansatt>() {
+                @Override
+                public Ansatt transform(ASBOGOSYSNAVAnsatt ansatt) {
+                    return new Ansatt(ansatt.getFornavn(), ansatt.getEtternavn(), ansatt.getAnsattId());
+                }
+            }).collect();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 
