4960471056ae0416b84a3b6a9d512adcd9297a95
Henrik Hellerøy <henrik.helleroy@nav.no>
2015-05-05 16:42:46 +0200

    TimeoutFeature på NORG-endepunkt

--- consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/endpoint/v1/norg/NAVAnsattEndpointConfig.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/endpoint/v1/norg/NAVAnsattEndpointConfig.java
@@ -1,6 +1,7 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.endpoint.v1.norg;
 
 import _0._0.nav_cons_sak_gosys_3.no.nav.inf.navansatt.GOSYSNAVansatt;
+import no.nav.sbl.dialogarena.common.cxf.TimeoutFeature;
 import org.apache.cxf.configuration.jsse.TLSClientParameters;
 import org.apache.cxf.feature.LoggingFeature;
 import org.apache.cxf.frontend.ClientProxy;
@@ -34,6 +35,7 @@ public class NAVAnsattEndpointConfig {
         proxyFactoryBean.setEndpointName(new QName("http://nav-cons-sak-gosys-3.0.0/no/nav/inf/NAVansatt/Binding", "GOSYSNAVAnsattWSEXP_GOSYSNAVansattHttpPort"));
         proxyFactoryBean.setServiceClass(GOSYSNAVansatt.class);
         proxyFactoryBean.getFeatures().add(new LoggingFeature());
+        proxyFactoryBean.getFeatures().add(new TimeoutFeature());
         proxyFactoryBean.getOutInterceptors().add(new WSS4JOutInterceptor(getSecurityProps()));
 
         GOSYSNAVansatt gosysnaVansatt = proxyFactoryBean.create(GOSYSNAVansatt.class);
