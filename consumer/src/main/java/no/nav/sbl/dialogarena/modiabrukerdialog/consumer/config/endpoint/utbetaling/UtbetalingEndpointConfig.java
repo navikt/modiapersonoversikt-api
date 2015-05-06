131356f3aefad4ad700a784fdb787670475c1f90
Henrik Hellerøy <henrik.helleroy@nav.no>
2015-05-06 09:53:02 +0200

    Timeout på utbetalingstjenesten

--- consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/endpoint/utbetaling/UtbetalingEndpointConfig.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/endpoint/utbetaling/UtbetalingEndpointConfig.java
@@ -5,11 +5,9 @@ import no.nav.modig.modia.ping.Pingable;
 import no.nav.modig.security.ws.AbstractSAMLOutInterceptor;
 import no.nav.modig.security.ws.SystemSAMLOutInterceptor;
 import no.nav.modig.security.ws.UserSAMLOutInterceptor;
+import no.nav.sbl.dialogarena.common.cxf.CXFClient;
 import no.nav.sbl.dialogarena.modiabrukerdialog.mock.config.endpoints.UtbetalingPortTypeMock;
 import no.nav.tjeneste.virksomhet.utbetaling.v1.UtbetalingV1;
-import org.apache.cxf.feature.LoggingFeature;
-import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
-import org.apache.cxf.ws.addressing.WSAddressingFeature;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -52,15 +50,12 @@ public class UtbetalingEndpointConfig {
     }
 
     private UtbetalingV1 createUtbetalingPortType(AbstractSAMLOutInterceptor interceptor) {
-        JaxWsProxyFactoryBean proxyFactoryBean = new JaxWsProxyFactoryBean();
-        proxyFactoryBean.setWsdlLocation("classpath:utbetaling/no/nav/tjeneste/virksomhet/utbetaling/v1/Binding.wsdl");
-        proxyFactoryBean.setAddress(System.getProperty("utbetalingendpoint.url"));
-        proxyFactoryBean.setServiceClass(UtbetalingV1.class);
-        proxyFactoryBean.getOutInterceptors().add(interceptor);
-        proxyFactoryBean.setServiceName(new QName("http://nav.no/tjeneste/virksomhet/utbetaling/v1/Binding", "Utbetaling_v1"));
-        proxyFactoryBean.setEndpointName(new QName("http://nav.no/tjeneste/virksomhet/utbetaling/v1/Binding", "Utbetaling_v1Port"));
-        proxyFactoryBean.getFeatures().add(new WSAddressingFeature());
-        proxyFactoryBean.getFeatures().add(new LoggingFeature());
-        return proxyFactoryBean.create(UtbetalingV1.class);
+        CXFClient<UtbetalingV1> cxfClient = new CXFClient<>(UtbetalingV1.class)
+                .wsdl("classpath:utbetaling/no/nav/tjeneste/virksomhet/utbetaling/v1/Binding.wsdl")
+                .address(System.getProperty("utbetalingendpoint.url"))
+                .withOutInterceptor(interceptor);
+        cxfClient.factoryBean.setServiceName(new QName("http://nav.no/tjeneste/virksomhet/utbetaling/v1/Binding", "Utbetaling_v1"));
+        cxfClient.factoryBean.setEndpointName(new QName("http://nav.no/tjeneste/virksomhet/utbetaling/v1/Binding", "Utbetaling_v1Port"));
+        return cxfClient.build();
     }
 }
