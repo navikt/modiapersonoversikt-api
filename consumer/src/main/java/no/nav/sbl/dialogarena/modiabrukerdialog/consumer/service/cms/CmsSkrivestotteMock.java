43a886e1b4c686c669c9c5495f174e037a9a486f
Nicklas Utgaard <nicklas.utgaard@nav.no>
2015-05-12 10:12:32 +0200

    Mer mock data for skrivestotte, bumpet reactkomponenter

--- consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/service/cms/CmsSkrivestotteMock.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/service/cms/CmsSkrivestotteMock.java
@@ -1,5 +1,6 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.consumer.service.cms;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -8,10 +9,13 @@ import static java.util.Arrays.asList;
 public class CmsSkrivestotteMock implements CmsSkrivestotte {
 
     private static int key = 0;
+    private static final int RANDOM_TEKSTER = 20;
 
     @Override
     public List<SkrivestotteTekst> hentSkrivestotteTekster() {
-        return asList(
+        List<SkrivestotteTekst> tekster = new ArrayList<>();
+
+        tekster.addAll(asList(
                 skrivestotteTekst(
                         "Taushetsbelagt eller sensitiv informasjon",
                         "Takk for din henvendelse.\n" +
@@ -43,7 +47,13 @@ public class CmsSkrivestotteMock implements CmsSkrivestotte {
                                 "Dersom du velger å sende inn henvendelsen per post, anbefaler vi at du henter ut en førsteside til saken din på www.nav.no. Alle dokumenter sendes til den adressen som er oppgitt på førstesiden.\n" +
                                 "\n" +
                                 "Søknadsskjemaer, selvbetjeningsløsninger, informasjon og «Dine utbetalinger» finner du på vår internettside www.nav.no. Her vil du også finne besøksadresse til ditt NAV-kontor.\n",
-                        "generell", "sensitiv", "feilsendt"));
+                        "generell", "sensitiv", "feilsendt")));
+
+        for (int i = 0; i < RANDOM_TEKSTER; i++) {
+            tekster.add(skrivestotteTekst("Random tekst " + i, "Norsk tekst streng " + i, "random", "id"+i));
+        }
+
+        return tekster;
     }
 
     private static SkrivestotteTekst skrivestotteTekst(String tittel, String norsk, String... tags) {
