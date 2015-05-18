89368a1c234483f862800f67d8be4705e4c84f97
Nicklas Utgaard <nicklas.utgaard@nav.no>
2015-05-18 13:11:42 +0200

    Fixed spring config for journalfortTemaPipConfig

--- pip/src/main/java/no/nav/sbl/modiabrukerdialog/pip/journalforing/config/JournalfortTemaPipConfig.java
+++ pip/src/main/java/no/nav/sbl/modiabrukerdialog/pip/journalforing/config/JournalfortTemaPipConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 public class JournalfortTemaPipConfig {
 
     @Bean
-    public JournalfortTemaAttributeLocatorDelegate enhetAttributeLocatorDelegate() {
+    public JournalfortTemaAttributeLocatorDelegate journalfortTemaAttributeLocatorDelegate() {
         return new DefaultJournalfortTemaAttributeLocatorDelegate();
     }
 
