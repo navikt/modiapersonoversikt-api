d8c193917ddfea7836333b3a7de089bf7f09e0dd
Henrik Hellerøy <henrik.helleroy@nav.no>
2015-05-28 13:07:29 +0200

    Klasse for delte sessionparametere

---
+++ src/main/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/api/constants/SessionParametere.java
@@ -0,0 +1,9 @@
+package no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.constants;
+
+public class SessionParametere {
+    public static class SporsmalOgSvar {
+        private static final String PREFIX = "sporsmalogsvar.";
+
+        public static final String BESVARMODUS = PREFIX + "modus.besvarer";
+    }
+}
