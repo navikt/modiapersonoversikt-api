14864d178ba95d377c9d654943b688f0e5608ca5
Henrik Hellerøy <henrik.helleroy@nav.no>
2015-05-28 14:03:46 +0200

    Spørsmål og svar events flyttet fra modia-felleskomponenter

--- src/main/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/api/constants/Events.java
+++ src/main/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/api/constants/Events.java
@@ -8,6 +8,8 @@ public class Events {
         public static final String SVAR_AVBRUTT = PREFIX + "svar.avbrutt";
         public static final String LEGG_TILBAKE_UTFORT = PREFIX + "leggtilbake.utfort";
         public static final String MELDING_VALGT = PREFIX + "melding.valgt";
+        public static final String SVAR_PAA_MELDING = PREFIX + "svar.paa.melding";
+        public static final String MELDING_SENDT_TIL_BRUKER = PREFIX + "melding.sendt.til.bruker";
     }
 
     public static class Brukerprofil {
