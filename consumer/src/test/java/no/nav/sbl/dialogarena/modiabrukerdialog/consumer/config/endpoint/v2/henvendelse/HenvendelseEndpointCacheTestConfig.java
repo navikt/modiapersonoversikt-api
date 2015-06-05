648bb2e09172e909a1708bbc401b3f0c3fa72ca6
Frithjof Nerdrum <frithjof.nerdrum@nav.no>
2015-06-05 08:33:01 +0200

    Fikset test

---
+++ consumer/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/endpoint/v2/henvendelse/HenvendelseEndpointCacheTestConfig.java
@@ -0,0 +1,20 @@
+package no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.endpoint.v2.henvendelse;
+
+import no.nav.tjeneste.domene.brukerdialog.henvendelse.v2.henvendelse.HenvendelsePortType;
+import org.mockito.Mockito;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static no.nav.sbl.dialogarena.modiabrukerdialog.consumer.util.TimingMetricsProxy.createMetricsProxyWithInstanceSwitcher;
+import static org.mockito.Mockito.mock;
+
+@Configuration
+public class HenvendelseEndpointCacheTestConfig {
+
+    @Bean
+    public HenvendelsePortType henvendelsePortType() {
+        String key = "henvendelse-key";
+        System.setProperty(key, "true");
+        return createMetricsProxyWithInstanceSwitcher(null, mock(HenvendelsePortType.class), key, HenvendelsePortType.class);
+    }
+}
