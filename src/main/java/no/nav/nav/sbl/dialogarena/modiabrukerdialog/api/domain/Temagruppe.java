12ea29772021382de85cada37cde05e7f862db3a
Frithjof Nerdrum <frithjof.nerdrum@nav.no>
2015-06-05 08:52:59 +0200

    Endret til foreslåtte temagruppekoder for økonomisk sosialhjelp og andre sosiale tjenester

--- src/main/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/api/domain/Temagruppe.java
+++ src/main/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/api/domain/Temagruppe.java
@@ -12,10 +12,10 @@ public enum Temagruppe {
     ORT_HJE,
     OVRG,
     PENS,
-    OK_SOS,
-    ANDRE_SOS;
+    OKSOS,
+    ANSOS;
 
     public static final List<Temagruppe> INNGAAENDE = asList(ARBD, FMLI, HJLPM, BIL, ORT_HJE);
-    public static final List<Temagruppe> UTGAAENDE = asList(ARBD, FMLI, HJLPM, OK_SOS, ANDRE_SOS, OVRG);
+    public static final List<Temagruppe> UTGAAENDE = asList(ARBD, FMLI, HJLPM, OKSOS, ANSOS, OVRG);
 }
 
