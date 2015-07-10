91c1a99f07e3d1a580417be4620ccd23475b142d
Henrik Hellerøy <henrik.helleroy@nav.no>
2015-07-10 10:32:27 +0200

    Liste for kommunale tjenester

--- src/main/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/api/domain/Temagruppe.java
+++ src/main/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/api/domain/Temagruppe.java
@@ -19,5 +19,6 @@ public enum Temagruppe {
     public static final List<Temagruppe> SAMTALEREFERAT = asList(ARBD, FMLI, HJLPM, PENS, OKSOS, ANSOS, OVRG);
     public static final List<Temagruppe> LEGG_TILBAKE = asList(ARBD, FMLI, HJLPM, BIL, ORT_HJE, PENS, UFRT, OKSOS, ANSOS);
     public static final List<Temagruppe> PLUKKBARE = asList(ARBD, FMLI, HJLPM, BIL, ORT_HJE, PENS, UFRT);
+    public static final List<Temagruppe> KOMMUNALE_TJENESTER = asList(OKSOS, ANSOS);
 }
 
