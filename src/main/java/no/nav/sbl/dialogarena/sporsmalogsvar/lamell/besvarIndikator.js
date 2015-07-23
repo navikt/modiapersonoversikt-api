1fb6adfc71e9e11686be70abcd386456f3843039
Frithjof Nerdrum <frithjof.nerdrum@nav.no>
2015-07-23 11:42:30 +0200

    Viser spinner istedenfor bobler mens svar-panelet lastes

--- src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/lamell/besvarIndikator.js
+++ src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/lamell/besvarIndikator.js
@@ -4,26 +4,7 @@
 
         $lamell.on('click', '.besvarBoble > a', function () {
             var $link = $(this);
-            var $nyMeldingContainer = $link.closest('.nyMelding');
-
-            var panelId = $link.closest('.traadDetaljer').attr('id');
-            var $alleMeldingerListe = $lamell.find('.meldingsforhandsvisning');
-
-            //Legger til indikator for nyMeldingContainer
-            $nyMeldingContainer.addClass('besvares');
-
-            //Skjuler alle indikatorer i listen
-            $alleMeldingerListe
-                .find('[id^=besvarIndikator]')
-                .hide();
-
-            //Legger til indikator for valgt melding
-            $alleMeldingerListe
-                .filter('.valgt')
-                .find('[id^=besvarIndikator]')
-                .addClass('besvarIndikator')
-                .show()
-                .attr('aria-hidden', true);
+            $link.parent('.besvarBoble').replaceWith('<img class="snurrepipp" src="/modiabrukerdialog/img/ajaxloader/hvit_roed/loader_hvit_rod_32.gif"/>');
         });
     });
 })();
