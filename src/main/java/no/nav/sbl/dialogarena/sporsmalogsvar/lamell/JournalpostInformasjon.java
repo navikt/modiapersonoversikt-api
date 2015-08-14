c5e73448533def352fa24b989a8b98f63daf93e0
Nicklas Utgaard <nicklas.utgaard@nav.no>
2015-08-14 12:21:00 +0200

    PKFEIL-12726: Endret fra masse spans til en p-tag

--- src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/lamell/JournalpostInformasjon.java
+++ src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/lamell/JournalpostInformasjon.java
@@ -1,35 +1,36 @@
 package no.nav.sbl.dialogarena.sporsmalogsvar.lamell;
 
+import no.nav.modig.modia.model.StringFormatModel;
 import no.nav.sbl.dialogarena.sporsmalogsvar.lamell.haandtermelding.AnimertPanel;
-import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.model.IModel;
-
-import static no.nav.modig.wicket.conditional.ConditionalUtils.visibleIf;
+import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.model.StringResourceModel;
 
 public class JournalpostInformasjon extends AnimertPanel {
     public JournalpostInformasjon(String id, IModel<MeldingVM> model) {
         super(id);
         setDefaultModel(model);
         setOutputMarkupPlaceholderTag(true);
-        add(
-                new Label("melding.journalfortAv.navn"),
-                new Label("melding.journalfortAvNavIdent"),
-                new Label("journalfortDatoFormatert"),
-                new Label("melding.journalfortTemanavn"),
-                new WebMarkupContainer("saksIdContainer")
-                        .add(new Label("melding.journalfortSaksId"))
-                        .add(visibleIf(meldingHarSaksId(model)))
-        );
+
+        String format = "%s %s (%s) | %s | %s";
+        if (meldingHarSaksId(model)) {
+            format = "%s %s (%s) | %s | %s | %s %s";
+        }
+
+        add(new Label("tekst", new StringFormatModel(
+                format,
+                new StringResourceModel("journalpost.journalfort.av", this, null),
+                new PropertyModel<String>(model, "melding.journalfortAv.navn"),
+                new PropertyModel<String>(model, "melding.journalfortAvNavIdent"),
+                new PropertyModel<String>(model, "journalfortDatoFormatert"),
+                new PropertyModel<String>(model, "melding.journalfortTemanavn"),
+                new StringResourceModel("journalpost.jornalfort.saksid", this, null),
+                new PropertyModel<String>(model, "melding.journalfortSaksId")
+        )));
     }
 
-    private static AbstractReadOnlyModel<Boolean> meldingHarSaksId(final IModel<MeldingVM> model) {
-        return new AbstractReadOnlyModel<Boolean>() {
-            @Override
-            public Boolean getObject() {
-                return model.getObject().melding.journalfortSaksId != null;
-            }
-        };
+    private static boolean meldingHarSaksId(final IModel<MeldingVM> model) {
+        return model.getObject().melding.journalfortSaksId != null;
     }
 }
