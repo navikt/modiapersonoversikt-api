f3d22897f4f8b348ea851f2ca3c4e0622f498ae4
Joanna <joanna.eriksson@nav.no>
2015-09-01 13:18:47 +0200

    PK-21875: funktionalitet før radio buttons tillagt

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/journalforingspanel/index.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/journalforingspanel/index.js
@@ -2,20 +2,34 @@ import React from 'react';
 import TypeValg from './typevalg';
 import SakerListe from './sakerliste';
 import LukkKnapp from './lukkknapp';
+import { partition } from 'lodash';
 
 class JournalforingsPanel extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            valgtKategori: 'FAG'
+        };
+        this.endreKategori = this.endreKategori.bind(this);
+    }
+
+    endreKategori(kategori) {
+        this.setState({valgtKategori: kategori});
     }
 
     render() {
-        console.log('saker', this.props.saker);
+        const kategorier = partition(this.props.saker, sak => sak.sakstype === 'GEN');
+        const generelle = kategorier[0];
+        const fagsaker = kategorier[1];
+
+        const saker = this.state.valgtKategori === 'FAG' ? fagsaker : generelle;
+
         return (
             <div className="journalforings-panel">
                 <h2 className="header">Journalføring</h2>
-                <TypeValg></TypeValg>
+                <TypeValg valgtKategori={this.state.valgtKategori} endreKategori={this.endreKategori}/>
 
-                <SakerListe saker={this.props.saker}></SakerListe>
+                <SakerListe saker={saker}></SakerListe>
                 <LukkKnapp></LukkKnapp>
             </div>
         );
--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/journalforingspanel/typevalg.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/journalforingspanel/typevalg.js
@@ -3,15 +3,20 @@ import React from 'react';
 class TypeValg extends React.Component {
     constructor(props) {
         super(props);
+        this.endre = this.endre.bind(this)
+    }
+
+    endre(event) {
+        this.props.endreKategori(event.target.value);
     }
 
     render() {
         return (
-            <form className="type-valg">
-                <input name="typeVelger" type="radio" checked="checked" value="FAG"/>
-                <label className="label">Fagsaker</label>
-                <input name="typeVelger" type="radio" value="GEN"/>
-                <label className="label">Generelle saker</label>
+            <form className="type-valg" onChange={this.endre}>
+                <input name="typeVelger" id="t1" type="radio" checked={this.props.valgtKategori === 'FAG'} value="FAG" onChange={this.endre}/>
+                <label className="label" htmlFor="t1">Fagsaker</label>
+                <input name="typeVelger" id="t2" type="radio" checked={this.props.valgtKategori === 'GEN'} value="GEN" onChange={this.endre}/>
+                <label className="label" htmlFor="t2">Generelle saker</label>
             </form>
         );
     }
