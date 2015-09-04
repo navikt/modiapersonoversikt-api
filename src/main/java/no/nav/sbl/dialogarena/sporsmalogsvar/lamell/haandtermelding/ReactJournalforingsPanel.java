538a8e66514ca74f5c7073b93df201390bda5174
Frithjof Nerdrum <frithjof.nerdrum@nav.no>
2015-09-04 12:04:11 +0200

    PK-21875: Kan lukke journalføringspanelet

--- src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/lamell/haandtermelding/ReactJournalforingsPanel.java
+++ src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/lamell/haandtermelding/ReactJournalforingsPanel.java
@@ -1,27 +1,29 @@
 package no.nav.sbl.dialogarena.sporsmalogsvar.lamell.haandtermelding;
 
+import no.nav.sbl.dialogarena.reactkomponenter.utils.wicket.ReactComponentCallback;
 import no.nav.sbl.dialogarena.reactkomponenter.utils.wicket.ReactComponentPanel;
 import no.nav.sbl.dialogarena.sporsmalogsvar.lamell.InnboksVM;
+import org.apache.wicket.ajax.AjaxRequestTarget;
 
 import java.util.HashMap;
 
 public class ReactJournalforingsPanel extends AnimertPanel {
 
-    private final ReactComponentPanel reactComponentPanel;
-
     public ReactJournalforingsPanel(String id, final InnboksVM innboksVM) {
         super(id, true);
-        reactComponentPanel = new ReactComponentPanel("reactjournalforing", "JournalforingsPanel", new HashMap<String, Object>() {
+        ReactComponentPanel reactComponentPanel = new ReactComponentPanel("reactjournalforing", "JournalforingsPanel", new HashMap<String, Object>() {
             {
                 put("fnr", innboksVM.getFnr());
                 put("traadId", innboksVM.getValgtTraad().getEldsteMelding().melding.traadId);
             }
         });
+        reactComponentPanel.addCallback("lukkPanel", Void.class, new ReactComponentCallback<Void>() {
+            @Override
+            public void onCallback(AjaxRequestTarget target, Void data) {
+                lukkPanel(target);
+            }
+        });
         add(reactComponentPanel);
     }
 
-    @Override
-    protected void onOpen() {
-        super.onOpen();
-    }
 }
