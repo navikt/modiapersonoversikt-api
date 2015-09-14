aaa6052fa27c53d5a64fd2b29fc675e007addbf1
Nicklas Utgaard <nicklas.utgaard@nav.no>
2015-09-14 08:24:16 +0200

    PK-21875: Snurrepipp ved journalføring

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/journalforingspanel/journalforknapp.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/journalforingspanel/journalforknapp.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import Snurrepipp from '../utils/Snurrepipp.js';
 
 class JournalforKnapp extends React.Component {
     constructor(props) {
         super(props);
         this.journalfor = this.journalfor.bind(this);
         this.state = {
-            sender: false,
-            sendt: false
+            sender: false
         }
     }
 
     journalfor(event) {
         event.preventDefault();
+        this.setState({
+            sender: true
+        });
+
         $.ajax({
             type: 'POST',
             url: '/modiabrukerdialog/rest/journalforing/' + this.props.fnr + '/' + this.props.traadId,
@@ -19,7 +23,7 @@ class JournalforKnapp extends React.Component {
             data: JSON.stringify(this.props.sak)
         })
             .done(function (response, status, xhr) {
-                this.setState({sender: false, sendt: true});
+                this.setState({sender: false});
                 this.props.traadJournalfort();
             }.bind(this))
             .fail(function () {
@@ -29,11 +33,15 @@ class JournalforKnapp extends React.Component {
     }
 
     render() {
-        return (
-            <div>
-                <button className="journalfor-knapp knapp-advarsel-stor" onClick={this.journalfor}>Journalfør</button>
-            </div>
-        );
+        if (this.state.sender) {
+            return <Snurrepipp />;
+        } else {
+            return (
+                <div>
+                    <button className="journalfor-knapp knapp-advarsel-stor" onClick={this.journalfor}>Journalfør</button>
+                </div>
+            );
+        }
     }
 }
 
