8964ddfde90ea323c30855889dede62454b26672
Christoffer Karlsson <christoffer.karlsson@nav.no>
2015-09-16 10:00:41 +0200

    [PK-25076] Mockoppsett for opprett og ferdigstill henvendelse

--- mock/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/mock/config/endpoints/SendUtHenvendelsePortTypeMock.java
+++ mock/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/mock/config/endpoints/SendUtHenvendelsePortTypeMock.java
@@ -3,6 +3,8 @@ package no.nav.sbl.dialogarena.modiabrukerdialog.mock.config.endpoints;
 import no.nav.melding.domene.brukerdialog.behandlingsinformasjon.v1.XMLHenvendelse;
 import no.nav.melding.domene.brukerdialog.behandlingsinformasjon.v1.XMLMelding;
 import no.nav.tjeneste.domene.brukerdialog.henvendelse.v1.senduthenvendelse.SendUtHenvendelsePortType;
+import no.nav.tjeneste.domene.brukerdialog.henvendelse.v1.senduthenvendelse.meldinger.WSFerdigstillHenvendelseRequest;
+import no.nav.tjeneste.domene.brukerdialog.henvendelse.v1.senduthenvendelse.meldinger.WSFerdigstillHenvendelseResponse;
 import no.nav.tjeneste.domene.brukerdialog.henvendelse.v1.senduthenvendelse.meldinger.WSSendUtHenvendelseRequest;
 import no.nav.tjeneste.domene.brukerdialog.henvendelse.v1.senduthenvendelse.meldinger.WSSendUtHenvendelseResponse;
 import org.springframework.context.annotation.Bean;
@@ -36,7 +38,28 @@ public class SendUtHenvendelsePortTypeMock {
             }
 
             @Override
+            public void avbrytHenvendelse(String behandlingsId) {
+
+            }
+
+            @Override
+            public String opprettHenvendelse(String type, String fodselsnummer, String behandlingskjedeId) {
+                return UUID.randomUUID().toString();
+            }
+
+            @Override
             public void ping() {
+            }
+
+            @Override
+            public WSFerdigstillHenvendelseResponse ferdigstillHenvendelse(WSFerdigstillHenvendelseRequest parameters) {
+                XMLHenvendelse xmlHenvendelse = (XMLHenvendelse) parameters.getAny();
+                String behandlingsId = parameters.getBehandlingsId();
+                xmlHenvendelse.setBehandlingsId(behandlingsId);
+                XMLMelding xmlMelding = (XMLMelding) xmlHenvendelse.getMetadataListe().getMetadata().get(0);
+                xmlHenvendelse.setGjeldendeTemagruppe(xmlMelding.getTemagruppe());
+                HENVENDELSER.add(xmlHenvendelse);
+                return new WSFerdigstillHenvendelseResponse();
             }
         };
     }
