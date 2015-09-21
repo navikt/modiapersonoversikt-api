3ee26b782f79f13f90721d61bd12aa7ef54b9334
Nicklas Utgaard <nicklas.utgaard@nav.no>
2015-09-21 15:25:49 +0200

    PK-10629: Varsler oppsett av rest endepunkt og cxf-integrasjon

--- src/main/java/no/nav/sbl/dialogarena/varsel/service/VarselService.java
+++
@@ -1,6 +0,0 @@
-package no.nav.sbl.dialogarena.varsel.service;
-
-public interface VarselService {
-
-    void ping();
-}
--- src/test/java/no/nav/sbl/dialogarena/varsel/service/VarselServiceImplTest.java
+++
@@ -1,5 +0,0 @@
-package no.nav.sbl.dialogarena.varsel.service;
-
-public class VarselServiceImplTest {
-
-}
