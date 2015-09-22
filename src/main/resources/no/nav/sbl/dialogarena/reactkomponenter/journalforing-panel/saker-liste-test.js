0763c20a265306659b6c2c302495811a73abf15e
Joanna Eriksson <joanna.eriksson@nav.no>
2015-09-22 08:24:58 +0200

    PK-21875: la til test som sjeker att saker sorteres i bokstavsordning. Flyttat klassen saker før tema til egen fil

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/journalforing-panel/saker-liste-test.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/journalforing-panel/saker-liste-test.js
@@ -5,23 +5,54 @@ var TestUtils = React.addons.TestUtils;
 var sinon = require('sinon');
 require('sinon-chai');
 var SakerListe = require('./saker-liste');
+var SakerForTema = require('./saker-for-tema');
 
 describe('SakerListe', function () {
 
-    it('skal gruppere skaer på samme temaKode', function () {
-        var saker = [
-            {temaKode: 'DAG'},
-            {temaKode: 'DAG'},
-            {temaKode: 'AAP'},
-            {temaKode: 'DAG'},
-            {temaKode: 'BIL'}
+    it('skal gruppere saker på samme temaKode', function () {
+        const saker = [
+            lagSak('DAG'),
+            lagSak('DAG'),
+            lagSak('AAP'),
+            lagSak('DAG'),
+            lagSak('AAP'),
+            lagSak('BIL')
         ];
+        const sakerForTema = setup(saker);
 
-        var sakerListe = TestUtils.renderIntoDocument(<SakerListe saker={saker}/>);
-        var renderedDOM = React.findDOMNode(sakerListe);
-        var sakerTema = renderedDOM.querySelectorAll('.saker-tema');
+        expect(sakerForTema.length).to.equal(3);
 
-        expect(sakerTema.length).to.equal(3);
+        const filtrerPaaTema = (tema) => sakerForTema.filter(elem => elem.props.tema === tema)[0];
+
+        // DAG har tre saker
+        expect(filtrerPaaTema('DAG').props.saker.length).to.equal(3);
+
+        // AAP har to sak
+        expect(filtrerPaaTema('AAP').props.saker.length).to.equal(2);
+
+        // BIL har én sak
+        expect(filtrerPaaTema('BIL').props.saker.length).to.equal(1);
+
     });
 
+    it("sorterer på alfabetisk på temaNavn", function () {
+        const saker = [
+            lagSak('AAB'),
+            lagSak('AAA'),
+            lagSak('AAAC'),
+            lagSak('AAAA')
+        ];
+        const sakerForTema = setup(saker);
+
+        expect(sakerForTema.map(elem => elem.props.tema)).to.eql(['AAA', 'AAAA', 'AAAC', 'AAB']);
+    });
+
+    const lagSak = (tema) => {
+        return {temaKode: tema, temaNavn: tema}
+    };
+
+    const setup = (saker) => {
+        const sakerListe = TestUtils.renderIntoDocument(<SakerListe saker={saker}/>);
+        return TestUtils.scryRenderedComponentsWithType(sakerListe, SakerForTema);
+    }
 });
--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/journalforing-panel/saker-liste.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/journalforing-panel/saker-liste.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { chain, mapValues, contains, partition, flatten } from 'lodash';
 
+import SakerForTema from './saker-for-tema';
+
 class SakerListe extends React.Component {
     constructor(props) {
         super(props);
@@ -61,63 +63,5 @@ function grupperPaaTemakodeOgSorter(saker) {
         .value();
 }
 
-
-class SakerForTema extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            ekspandert: props.erEkspandert
-        };
-        this.toggleEkspandering = this.toggleEkspandering.bind(this);
-    }
-
-    componentWillReceiveProps(nextProps) {
-        this.setState({
-            ekspandert: nextProps.erEkspandert
-        })
-    }
-
-    toggleEkspandering(event) {
-        event.preventDefault();
-        this.setState({ekspandert: !this.state.ekspandert});
-    }
-
-    render() {
-        if (this.state.ekspandert) {
-            return (
-                <div className="saker-tema">
-                    <button onClick={this.toggleEkspandering} aria-expanded="true">
-                        <div className="tema-bar">
-                            <h3 className="tema-overskrift">{this.props.tema}</h3>
-
-                            <div className="ekspanderingspil opp"></div>
-                        </div>
-
-                        <div className="info-bar">
-                            <span className="text-cell">SAKSID</span>
-                            <span className="vekk"> | </span>
-                            <span className="text-cell text-align-right">OPPRETTET</span>
-                            <span className="vekk"> | </span>
-                            <span className="text-cell">FAGSYSTEM</span>
-                        </div>
-                    </button>
-                    <ul className="list-saker">
-                        {this.props.saker}
-                    </ul>
-                </div>
-            );
-        } else {
-            return (
-                <div className="saker-tema">
-                    <button className="tema-bar" onClick={this.toggleEkspandering} aria-expanded="false">
-                        <h3 className="tema-overskrift">{this.props.tema}</h3>
-
-                        <div className="ekspanderingspil ned"></div>
-                    </button>
-                </div>
-            );
-        }
-    }
-}
 
 export default SakerListe;
