97cbb954c63c1a22043f7c1eb14a3bb837b5f7b3
Nicklas Utgaard <nicklas.utgaard@nav.no>
2015-09-22 09:38:06 +0200

    PK-10629: Flyttet mesteparten av varsling spesifike klasser til modia-varsling

--- src/main/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/api/service/VarslerService.java
+++
@@ -1,9 +0,0 @@
-package no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.service;
-
-import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain.Varsel;
-
-import java.util.List;
-
-public interface VarslerService {
-    List<Varsel> hentAlleVarsler(String fnr);
-}
