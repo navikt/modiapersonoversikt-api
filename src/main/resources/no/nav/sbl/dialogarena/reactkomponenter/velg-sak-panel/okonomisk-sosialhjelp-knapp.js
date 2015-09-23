0e3f31e75f17b6fe8835c2537a6e8901da38de10
Frithjof Nerdrum <frithjof.nerdrum@nav.no>
2015-09-23 09:04:48 +0200

    PKFEIL-13515: Ny økonomisk-sosialhjelp-knapp

---
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/velg-sak-panel/okonomisk-sosialhjelp-knapp.js
@@ -0,0 +1,65 @@
+import React from 'react';
+
+class OkonomiskSosialhjelpKnapp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ekspandert: true
+        };
+        this.toggleEkspandering = this.toggleEkspandering.bind(this);
+        this.velgOkonomiskSosialhelp = this.velgOkonomiskSosialhelp.bind(this);
+    }
+
+    toggleEkspandering(event) {
+        event.preventDefault();
+        this.setState({ekspandert: !this.state.ekspandert});
+    }
+
+    velgOkonomiskSosialhelp(event) {
+        event.preventDefault();
+        const sak = {
+            saksId: null,
+            fagsystemSaksId: null,
+            temaKode: 'OKSOS',
+            temaNavn: 'Økonomisk sosialhjelp',
+            fagsystemKode: null,
+            fagsystemNavn: null,
+            sakstype: null,
+            finnesIGsak: null,
+            finnesIPsak: null
+        };
+        this.props.velgSak(sak);
+    }
+
+
+    render() {
+        const pilRetning = this.state.ekspandert ? 'opp' : 'ned';
+        let content = null;
+        if (this.state.ekspandert) {
+            content = (
+                <ul className="list-saker">
+                    <li className="text-row-list">
+                        <a href="javascript:void(0)" role="button" className="content-row-list"
+                           onClick={this.velgOkonomiskSosialhelp}>
+                            Økonomisk sosialhjelp - vil ikke bli journalført
+                        </a>
+                    </li>
+                </ul>
+            );
+        }
+        return (
+            <div className="saker-tema">
+                <button className="sosialhjelp-knapp" onClick={this.toggleEkspandering}>
+                    <div className="tema-bar">
+                        <h3 className="tema-overskrift">Økonomisk sosialhjelp</h3>
+
+                        <div className={'ekspanderingspil ' + pilRetning}></div>
+                    </div>
+                </button>
+                {content}
+            </div>
+        );
+    }
+}
+
+export default OkonomiskSosialhjelpKnapp;
