87d758eb4c265a7d4d170cfc39a819c36bda92e2
Nicklas Utgaard <nicklas.utgaard@nav.no>
2015-09-24 13:31:31 +0200

    PK-10629: la til rest-endepunkt for enonic integrasjon

--- src/main/java/no/nav/sbl/dialogarena/varsel/rest/VarslerController.java
+++ src/main/java/no/nav/sbl/dialogarena/varsel/rest/VarslerController.java
@@ -1,14 +1,17 @@
 package no.nav.sbl.dialogarena.varsel.rest;
 
+import no.nav.modig.content.CmsContentRetriever;
 import no.nav.sbl.dialogarena.varsel.domain.Varsel;
 import no.nav.sbl.dialogarena.varsel.service.VarslerService;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import java.util.List;
+import java.util.Map;
 
 
 @Path("/varsler/{fnr}")
@@ -18,9 +21,19 @@ public class VarslerController {
     @Inject
     VarslerService varslerService;
 
+    @Inject
+    @Named("varsling-cms-integrasjon")
+    private CmsContentRetriever contentRetriever;
+
     @GET
     @Path("/")
     public List<Varsel> hentAlleVarsler(@PathParam("fnr") String fnr) {
         return varslerService.hentAlleVarsler(fnr);
+    }
+
+    @GET
+    @Path("/resources")
+    public Map<String, String> hentAlleResources() {
+        return contentRetriever.hentAlleTekster();
     }
 }
