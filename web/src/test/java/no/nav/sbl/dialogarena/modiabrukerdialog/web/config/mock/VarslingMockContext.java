9668304419a6c5c1f6317d36cb039dc558574461
Nygård, Geir Furland <Geir.Furland.Nygard@nav.no>
2015-09-25 15:25:10 +0200

    Mocket CmsContentRetriever i VarslingMockContext for å fikse en test

--- web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/mock/VarslingMockContext.java
+++ web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/mock/VarslingMockContext.java
@@ -1,8 +1,11 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.web.config.mock;
 
+import no.nav.modig.content.CmsContentRetriever;
 import no.nav.sbl.dialogarena.varsel.service.VarslerService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.net.URISyntaxException;
 
 import static org.mockito.Mockito.mock;
 
@@ -12,5 +15,10 @@ public class VarslingMockContext {
     @Bean
     public VarslerService varslerService() {
         return mock(VarslerService.class);
+    }
+
+    @Bean(name = "varsling-cms-integrasjon")
+    public CmsContentRetriever varslingCmsContentRetriver() throws URISyntaxException {
+        return mock(CmsContentRetriever.class);
     }
 }
