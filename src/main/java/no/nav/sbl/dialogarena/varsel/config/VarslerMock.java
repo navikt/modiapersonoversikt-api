43522e7716952754b184d752d0849cf38a92ff18
Nicklas Utgaard <nicklas.utgaard@nav.no>
2015-09-25 13:18:13 +0200

    PK-10629: Tar ibruk tekster fra enonic.

--- src/main/java/no/nav/sbl/dialogarena/varsel/config/VarslerMock.java
+++ src/main/java/no/nav/sbl/dialogarena/varsel/config/VarslerMock.java
@@ -12,35 +12,45 @@ public class VarslerMock implements VarslerPorttype {
         DateTime now = DateTime.now();
 
         return new WSHentVarslerResponse()
-            .withVarselListe(new WSVarselListe().withVarsel(
+                .withVarselListe(new WSVarselListe().withVarsel(
 
-                new WSVarsel()
-                    .withVarseltype("SPORSMAL")
-                    .withMottattidspunkt(new XMLGregorianCalendarImpl(now.minusMinutes(60).toGregorianCalendar()))
-                    .withStatus("Ferdig")
-                    .withMeldingListe(new WSMeldingListe().withMelding(
-                        lagWsMelding("SMS", "En sms melding", "12345678", "", "OK", now.minusDays(1), "", ""),
-                        lagWsMelding("EPOST", "En EPOST melding", "test@testeres.com", "", "OK", now.minusDays(1), "epostemne", ""),
-                        lagWsMelding("NAV.NO", "En NAVNO melding", "", "", "OK", now.minusDays(1), "", "url")
-                    )),
-                new WSVarsel()
-                    .withVarseltype("SVAR")
-                    .withMottattidspunkt(new XMLGregorianCalendarImpl(now.minusDays(2).minusHours(1).toGregorianCalendar()))
-                    .withStatus("Feilet")
-                    .withMeldingListe(new WSMeldingListe().withMelding(
-                        lagWsMelding("SMS", "En feilet sms", "", "Ingen telefon nummer", "ERROR", now.minusDays(2), "", ""),
-                        lagWsMelding("EPOST", "En EPOST sms", "", "Ingen epost adr", "ERROR", now.minusDays(2), "epostemne", ""),
-                        lagWsMelding("NAV.NO", "OK", "", "", "OK", now.minusDays(2), "", "url")
-                    )),
-                new WSVarsel()
-                    .withVarseltype("MOTE")
-                    .withMottattidspunkt(new XMLGregorianCalendarImpl(now.minusDays(2).minusHours(1).toGregorianCalendar()))
-                    .withStatus("Ferdig")
-                    .withMeldingListe(new WSMeldingListe().withMelding(
-                        lagWsMelding("SMS", "En sms melding", "12345678", "", "OK", now.minusDays(1), "", ""),
-                        lagWsMelding("EPOST", "En EPOST melding", "test@testeres.com", "", "500", now.minusDays(1), "epostemne", ""),
-                        lagWsMelding("NAV.NO", "En NAVNO melding", "", "", "OK", now.minusDays(1), "", "url")
-                    ))));
+                        new WSVarsel()
+                                .withVarseltype("SPORSMAL")
+                                .withMottattidspunkt(new XMLGregorianCalendarImpl(now.minusMinutes(60).toGregorianCalendar()))
+                                .withStatus("Ferdig")
+                                .withMeldingListe(new WSMeldingListe().withMelding(
+                                        lagWsMelding("SMS", "En sms melding", "12345678", "", "OK", now.minusDays(1), "", ""),
+                                        lagWsMelding("EPOST", "En EPOST melding", "test@testeres.com", "", "OK", now.minusDays(1), "epostemne", ""),
+                                        lagWsMelding("NAV.NO", "En NAVNO melding", "", "", "OK", now.minusDays(1), "", "url")
+                                )),
+                        new WSVarsel()
+                                .withVarseltype("SVAR")
+                                .withMottattidspunkt(new XMLGregorianCalendarImpl(now.minusDays(2).minusHours(1).toGregorianCalendar()))
+                                .withStatus("Feilet")
+                                .withMeldingListe(new WSMeldingListe().withMelding(
+                                        lagWsMelding("SMS", "En feilet sms", "", "Ingen telefon nummer", "ERROR", now.minusDays(2), "", ""),
+                                        lagWsMelding("EPOST", "En EPOST sms", "", "Ingen epost adr", "ERROR", now.minusDays(2), "epostemne", ""),
+                                        lagWsMelding("NAV.NO", "OK", "", "", "OK", now.minusDays(2), "", "url")
+                                )),
+                        new WSVarsel()
+                                .withVarseltype("MOTE")
+                                .withMottattidspunkt(new XMLGregorianCalendarImpl(now.minusDays(2).minusHours(1).toGregorianCalendar()))
+                                .withStatus("Ferdig")
+                                .withMeldingListe(new WSMeldingListe().withMelding(
+                                        lagWsMelding("SMS", "En sms melding", "12345678", "", "OK", now.minusDays(1), "", ""),
+                                        lagWsMelding("EPOST", "En EPOST melding", "test@testeres.com", "", "500", now.minusDays(1), "epostemne", ""),
+                                        lagWsMelding("NAV.NO", "En NAVNO melding", "", "", "OK", now.minusDays(1), "", "url")
+                                )),
+                        new WSVarsel()
+                                .withVarseltype("Gruppeaktivitet")
+                                .withMottattidspunkt(new XMLGregorianCalendarImpl(now.minusDays(3).toGregorianCalendar()))
+                                .withStatus("Ferdig")
+                                .withMeldingListe(new WSMeldingListe().withMelding(
+                                        lagWsMelding("SMS", "Kom på gruppeaktivititet SMS", "12345678", "", "OK", now.minusDays(1), "", ""),
+                                        lagWsMelding("EPOST", "Kom på gruppeaktivititet EPOST", "test@testesen.com", "", "OK", now.minusDays(1), "Dette er en invitasjon", ""),
+                                        lagWsMelding("NAV.NO", "Kom på gruppeaktivititet NAVNO", "", "", "OK", now.minusDays(1), "", "http://www.nav.no/")
+                                ))
+                ));
     }
 
     @Override
@@ -53,13 +63,13 @@ public class VarslerMock implements VarslerPorttype {
             date = DateTime.now();
         }
         return new WSMelding()
-            .withKanal(kanal)
-            .withInnhold(innhold)
-            .withMottakerinformasjon(mottakerInformasjon)
-            .withFeilbeskrivelse(feilbeskrivelse)
-            .withUtsendingstidspunkt(new XMLGregorianCalendarImpl(date.toGregorianCalendar()))
-            .withStatuskode(statusKode)
-            .withEpostemne(epostemne)
-            .withUrl(url);
+                .withKanal(kanal)
+                .withInnhold(innhold)
+                .withMottakerinformasjon(mottakerInformasjon)
+                .withFeilbeskrivelse(feilbeskrivelse)
+                .withUtsendingstidspunkt(new XMLGregorianCalendarImpl(date.toGregorianCalendar()))
+                .withStatuskode(statusKode)
+                .withEpostemne(epostemne)
+                .withUrl(url);
     }
 }
