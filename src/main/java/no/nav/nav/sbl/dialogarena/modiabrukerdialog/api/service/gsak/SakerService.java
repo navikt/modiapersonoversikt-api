e5896d1373bd713985a06def028d216b61193181
Joanna Eriksson <joanna.eriksson@nav.no>
2015-09-25 14:24:55 +0200

    [PK-22050]  Skapet to REST-endepunkter før att hente data fra GSAK,PSAK etc. til journalforingspanelen.

--- src/main/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/api/service/gsak/SakerService.java
+++ src/main/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/api/service/gsak/SakerService.java
@@ -1,17 +1,15 @@
 package no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.service.gsak;
 
 import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain.gsak.Sak;
-import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain.gsak.Saker;
 import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.exceptions.JournalforingFeilet;
 
 import java.util.List;
 
 public interface SakerService {
-    Saker hentSaker(String fnr);
 
-    List<Sak> hentListeAvSaker(String fnr);
+    List<Sak> hentSammensatteSaker(String fnr);
 
-    List<Sak> hentRelevanteSaker(String fnr);
+    List<Sak> hentPensjonSaker(String fnr);
 
     void knyttBehandlingskjedeTilSak(String fnr, String behandlingskjede, Sak sak) throws JournalforingFeilet;
 
