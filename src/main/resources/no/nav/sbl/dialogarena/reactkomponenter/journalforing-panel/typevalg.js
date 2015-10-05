ef115d1472725e8b01160b8bc2cde5dddd168b97
Frithjof Nerdrum <frithjof.nerdrum@nav.no>
2015-10-05 15:08:55 +0200

    PKFEIL-13977: Prevent default på enter

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/journalforing-panel/typevalg.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/journalforing-panel/typevalg.js
@@ -5,8 +5,8 @@ class TypeValg extends React.Component {
     constructor(props) {
         super(props);
 
-        this.id1=generateId("typevalg");
-        this.id2=generateId("typevalg");
+        this.id1 = generateId("typevalg");
+        this.id2 = generateId("typevalg");
 
         this.endre = this.endre.bind(this)
     }
@@ -21,11 +21,26 @@ class TypeValg extends React.Component {
     }
 
     render() {
+        const preventDefault = (event) => event.preventDefault();
         return (
             <div className="type-valg">
-                <input name="typeVelger" id={this.id1} type="radio" checked={this.props.valgtKategori === 'FAG'} value="FAG" ref="typevalg.FAG" onChange={this.endre}/>
+                <input name="typeVelger"
+                       id={this.id1}
+                       type="radio"
+                       checked={this.props.valgtKategori === 'FAG'}
+                       value="FAG"
+                       ref="typevalg.FAG"
+                       onKeyPress={preventDefault}
+                       onChange={this.endre}/>
                 <label className="label" htmlFor={this.id1}>Fagsaker</label>
-                <input name="typeVelger" id={this.id2} type="radio" checked={this.props.valgtKategori === 'GEN'} value="GEN" ref="typevalg.GEN" onChange={this.endre}/>
+                <input name="typeVelger"
+                       id={this.id2}
+                       type="radio"
+                       checked={this.props.valgtKategori === 'GEN'}
+                       value="GEN"
+                       ref="typevalg.GEN"
+                       onKeyPress={preventDefault}
+                       onChange={this.endre}/>
                 <label className="label" htmlFor={this.id2}>Generelle saker</label>
             </div>
         );
