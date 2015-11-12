5266a082c518a56c5154b0399a108042d4f2fdda
Tine Kleivane <tine.kleivane@nav.no>
2015-11-12 10:50:56 +0100

    [PK-26800] Kjør prefetch mandag-fredag kl 7 og 19

--- consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/service/ScheduledAnsattListePrefetch.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/service/ScheduledAnsattListePrefetch.java
@@ -2,14 +2,12 @@ package no.nav.sbl.dialogarena.modiabrukerdialog.consumer.service;
 
 import _0._0.nav_cons_sak_gosys_3.no.nav.asbo.navorgenhet.ASBOGOSYSNavEnhet;
 import _0._0.nav_cons_sak_gosys_3.no.nav.inf.navansatt.GOSYSNAVansatt;
-import _0._0.nav_cons_sak_gosys_3.no.nav.inf.navansatt.HentNAVAnsattListeFaultGOSYSGeneriskMsg;
-import _0._0.nav_cons_sak_gosys_3.no.nav.inf.navansatt.HentNAVAnsattListeFaultGOSYSNAVEnhetIkkeFunnetMsg;
-import net.sf.ehcache.CacheManager;
 import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain.norg.AnsattEnhet;
 import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.service.norg.EnhetService;
 import org.apache.commons.collections15.Closure;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import javax.inject.Inject;
 import java.util.List;
@@ -17,6 +15,8 @@ import java.util.List;
 import static no.nav.modig.lang.collections.IterUtils.on;
 
 public class ScheduledAnsattListePrefetch {
+
+    public static final String KL_7_OG_KL_19_MANDAG_TIL_FERDAG = "0 0 7,19 * * MON-FRI";
 
     Logger logger = LoggerFactory.getLogger(ScheduledAnsattListePrefetch.class);
 
@@ -26,10 +26,10 @@ public class ScheduledAnsattListePrefetch {
     @Inject
     private GOSYSNAVansatt ansattWS;
 
-
-    public void prefetchAnsattListe(){
+    @Scheduled(cron = KL_7_OG_KL_19_MANDAG_TIL_FERDAG)
+    public void prefetchAnsattListe() {
+        logger.info("Starter prefetch av alle ansatte i alle enheter");
         List<AnsattEnhet> alleEnheter = enhetService.hentAlleEnheter();
-
         on(alleEnheter).forEach(new Closure<AnsattEnhet>() {
             @Override
             public void execute(AnsattEnhet ansattEnhet) {
@@ -43,5 +43,6 @@ public class ScheduledAnsattListePrefetch {
                 }
             }
         });
+        logger.info("Ferdig behandlet prefetch av ansatte i {} enheter", alleEnheter.size());
     }
 }
