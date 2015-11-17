33079d3e0c1e3d2ef73c4242499cbb096fe1cec4
Joanna Eriksson <joanna.eriksson@nav.no>
2015-11-17 12:38:09 +0100

    Lagt til tester i skrivstøtte

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/skrivestotte/skrivestotte-store-test.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/skrivestotte/skrivestotte-store-test.js
@@ -19,4 +19,54 @@ describe('SkrivestotteStore', function () {
         expect(store.state.valgtTekst).to.equal(tekster[0]);
     });
 
+    it('knagger kan legges til og fjernes', function () {
+        var store = new SkrivestotteStore(assign({}, initialState, {knagger: []}));
+        store.leggTilKnagg("knagg1");
+        store.leggTilKnagg("knagg2");
+        store.slettKnagg("knagg1");
+        store.leggTilKnagg("knagg3");
+        expect(store.state.knagger).to.eql(["knagg2", "knagg3"]);
+
+    });
+
+    it('pil opp ger førre tekst men er ikke cyklisk', function () {
+        const t1 = {key: "key1"};
+        const t2 = {key: "key2"};
+        const t3 = {key: "key3"};
+        const tekster = [t1, t2, t3];
+        var event = $.Event("keypress");
+        event.which = 38;
+        event.keyCode = 38;
+        var store = new SkrivestotteStore(assign({}, initialState, {tekster: tekster}));
+
+        store.onKeyDown([], event);
+
+        expect(store.state.valgtTekst).to.equal(t1);
+
+        store.state.valgtTekst = t3;
+        store.onKeyDown([], event);
+        expect(store.state.valgtTekst).to.equal(t2);
+
+    });
+
+    it('pil ned ger neste tekst men er ikke cyklisk', function () {
+        const t1 = {key: "key1"};
+        const t2 = {key: "key2"};
+        const t3 = {key: "key3"};
+        const tekster = [t1, t2, t3];
+        var event = $.Event("keypress");
+        event.which = 40;
+        event.keyCode = 40;
+        var store = new SkrivestotteStore(assign({}, initialState, {tekster: tekster}));
+        store.state.valgtTekst = t3;
+
+        store.onKeyDown([], event);
+
+        expect(store.state.valgtTekst).to.equal(t3);
+
+        store.state.valgtTekst = t2;
+        store.onKeyDown([], event);
+        expect(store.state.valgtTekst).to.equal(t3);
+
+    });
 });
