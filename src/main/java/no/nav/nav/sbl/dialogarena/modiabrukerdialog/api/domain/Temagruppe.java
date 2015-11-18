eb177cb2d89d6543bd5172986657215afda60c9e
Toan Hoang <toan.hoang@nav.no>
2015-11-18 13:31:10 +0100

    PKFEIL-15150: Legger OKSOS og ANSOS nederst i temagruppevelgeren når man skal registere et samtalereferat

--- src/main/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/api/domain/Temagruppe.java
+++ src/main/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/api/domain/Temagruppe.java
@@ -16,7 +16,7 @@ public enum Temagruppe {
     OKSOS,
     ANSOS;
 
-    public static final List<Temagruppe> SAMTALEREFERAT = asList(ARBD, FMLI, HJLPM, PENS, OKSOS, ANSOS, OVRG);
+    public static final List<Temagruppe> SAMTALEREFERAT = asList(ARBD, FMLI, HJLPM, PENS, OVRG, OKSOS, ANSOS);
     public static final List<Temagruppe> LEGG_TILBAKE = asList(ARBD, FMLI, HJLPM, BIL, ORT_HJE, PENS, UFRT, OKSOS, ANSOS);
     public static final List<Temagruppe> PLUKKBARE = asList(ARBD, FMLI, HJLPM, BIL, ORT_HJE, PENS, UFRT);
     public static final List<Temagruppe> KOMMUNALE_TJENESTER = asList(OKSOS, ANSOS);
