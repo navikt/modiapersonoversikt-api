024bacf460c50b31521462e542a1cb85e7c688d5
Nicklas Utgaard <nicklas.utgaard@nav.no>
2015-11-19 13:29:09 +0100

    [TEST] debounce test

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/utils/utils-module-test.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/utils/utils-module-test.js
@@ -1,9 +1,10 @@
-import { expect } from 'chai';
+var expect = require('chai').expect;
+var assert = require('chai').assert;
 import { debounce } from './utils-module';
 import { delayed } from './../test-utils';
 
 describe("utils test", function () {
-    it("debounce test", function () {
+    it("debounce test immediate", function (done) {
         let c = 0;
         const fn = debounce(function () {
             c = c + 1;
@@ -14,8 +15,22 @@ describe("utils test", function () {
         fn();
 
         expect(c).to.equal(1);
+        done();
+    });
 
+    it("debounce test", function (done) {
+        let c = 0;
+        const fn = debounce(function () {
+            c = c + 1;
+        }, 0);
+
+        fn();
+        fn();
+        fn();
+        expect(c).to.equal(0);
+        delayed(()=>{
+            expect(c).to.equal(1);
+            done();
+        }, 0);
     });
 });
-
-
