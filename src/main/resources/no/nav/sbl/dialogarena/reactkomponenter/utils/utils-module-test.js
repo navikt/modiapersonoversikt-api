42efe2e470d852f074572caab5f9bb59b38494f3
Nicklas Utgaard <nicklas.utgaard@nav.no>
2015-11-19 14:19:23 +0100

    [TEST] utils-module

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/utils/utils-module-test.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/utils/utils-module-test.js
@@ -1,7 +1,7 @@
 var expect = require('chai').expect;
 var assert = require('chai').assert;
-import { debounce } from './utils-module';
 import { delayed } from './../test-utils';
+import { debounce, generateId, leggTilLenkerTags, omit, kvpair } from './utils-module';
 
 describe("utils test", function () {
     it("debounce test immediate", function (done) {
@@ -33,4 +33,52 @@ describe("utils test", function () {
             done();
         }, 0);
     });
+
+    it("Genererer OK id", function(){
+        const suffix = "minid";
+
+        var exp = new RegExp("^" + suffix + "\\d{13}-\\d{1,17}$");
+        var result = exp.test(generateId(suffix));
+
+        assert.isTrue(result);
+    });
+
+    it("Finner lenker i fritekst", function(){
+        const fritekst = `
+            Her er vg.no (denne funker ikke) du kan også bruke www.vg.no eller om du virkelig vil https://www.vg.no
+        `;
+        const result = leggTilLenkerTags(fritekst);
+
+        assert.isTrue(result.indexOf('<a target="_blank" href="http://www.vg.no">') >= 0);
+        assert.isTrue(result.indexOf('<a target="_blank" href="https://www.vg.no">') >= 0);
+    });
+
+    it("omit kopierer object med unntatt av verdier", function(){
+        const myObj = {
+            a: 1,
+            b: 2,
+            c: 3
+        };
+
+        const filterVerdi = omit(myObj, 'a');
+        const filterListe = omit(myObj, ['a', 'b']);
+
+        assert.isTrue(Object.keys(filterVerdi).length === 2);
+        assert.isTrue(Object.keys(filterListe).length === 1);
+    });
+
+    it("kvpair mapper object til double-array", function(){
+        const myObj = {
+            a: 1,
+            b: 2,
+            c: 3
+        };
+
+        const kv = kvpair(myObj);
+
+        expect(kv).to.have.length(3);
+        expect(kv[0]).to.eql(['a', 1]);
+        expect(kv[1]).to.eql(['b', 2]);
+        expect(kv[2]).to.eql(['c', 3]);
+    })
 });
