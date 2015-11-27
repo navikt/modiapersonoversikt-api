0c4e3a102b89c16c50a54f14c502e816c6cf2479
Joanna Eriksson <joanna.eriksson@nav.no>
2015-11-27 08:16:15 +0100

    reactkomponent for feilmelding sesjonen har løpt ut

---
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/feilmeldingsmodaler/sesjonen-har-lopt-ut.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Modal from './../modal/modal-module';
+
+class SesjonenHarLoptUt extends React.Component {
+    render() {
+        return (
+            <Modal><h1>Sesjonen har løpt ut</h1></Modal>
+        );
+    }
+}
+
+export default SesjonenHarLoptUt;
