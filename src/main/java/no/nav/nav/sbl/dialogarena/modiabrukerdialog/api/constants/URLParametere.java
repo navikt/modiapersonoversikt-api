9211f96883080ea3076816c5a746b3f577f8d2a0
Nicklas Utgaard <nicklas.utgaard@nav.no>
2015-12-03 08:31:15 +0100

    Mulig løsning for forced begrunnelse når man kommer fra gosys etc

--- src/main/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/api/constants/URLParametere.java
+++ src/main/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/api/constants/URLParametere.java
@@ -1,7 +1,13 @@
 package no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.constants;
 
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
 public class URLParametere {
     public static final String OPPGAVEID = "oppgaveid";
     public static final String HENVENDELSEID = "henvendelseid";
     public static final String BESVARES = "besvares";
+
+    public static final List<String> URL_TIL_SESSION_PARAMETERE = asList(HENVENDELSEID, OPPGAVEID, BESVARES);
 }
