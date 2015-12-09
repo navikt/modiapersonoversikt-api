11f0683ee78fbff521fc79498e158d2a51482f7e
Nicklas Utgaard <nicklas.utgaard@nav.no>
2015-12-09 14:06:23 +0100

    Fjerner scrollbars

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/utils/scroll-portal.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/utils/scroll-portal.js
@@ -3,22 +3,22 @@ var omit = require('./../utils/utils-module').omit;
 
 var ScrollPortal = React.createClass({
     renderList: function () {
-        var wrapper = $(this.refs.wrapper.getDOMNode());
-        wrapper.mCustomScrollbar($.extend({}, Modig.scrollOptions, {scrollbarPosition: 'inside'}));
-        var container = wrapper.find('.mCSB_container:first').get(0);
-        React.render(<div className={this.props.innerClassName}>{this.props.children}</div>, container);
-        oppdaterScrollbarEtterRenderingErFerdig(wrapper);
+        //var wrapper = $(this.refs.wrapper.getDOMNode());
+        //wrapper.mCustomScrollbar($.extend({}, Modig.scrollOptions, {scrollbarPosition: 'inside'}));
+        //var container = wrapper.find('.mCSB_container:first').get(0);
+        //React.render(<div className={this.props.innerClassName}>{this.props.children}</div>, container);
+        //oppdaterScrollbarEtterRenderingErFerdig(wrapper);
     },
     componentDidMount: function () {
-        this.renderList();
+        //this.renderList();
     },
     componentDidUpdate: function () {
-        this.renderList();
+        //this.renderList();
     },
     render: function () {
         var props = omit(this.props, ['children', 'innerClassName']);
         return (
-            <div ref="wrapper" {...props}></div>
+            <div ref="wrapper" {...props}>{this.props.children}</div>
         );
     }
 });
