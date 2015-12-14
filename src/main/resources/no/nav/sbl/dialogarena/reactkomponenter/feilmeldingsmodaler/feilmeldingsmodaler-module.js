7cf550b7e5d1026eae96605c8b3063a4078070d5
Joanna Eriksson <joanna.eriksson@nav.no>
2015-12-14 10:23:07 +0100

    [PK-28049 PKULV-1075] Laget et modal vindue for oppgi begrunnelse

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/feilmeldingsmodaler/feilmeldingsmodaler-module.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/feilmeldingsmodaler/feilmeldingsmodaler-module.js
@@ -2,11 +2,13 @@ import FlereApneVinduer from './flere-apne-vinduer';
 import ForkasteEndringer from './forkaste-endringer';
 import SesjonenHarLoptUt from './sesjonen-har-lopt-ut';
 import TekniskFeil from './teknisk-feil';
+import OppgiBegrunnelse from './oppgi-begrunnelse';
 
 // Legg til feilmeldingsmodaler her
 export default {
     FlereApneVinduer: FlereApneVinduer,
     ForkasteEndringer: ForkasteEndringer,
     SesjonenHarLoptUt: SesjonenHarLoptUt,
-    TekniskFeil: TekniskFeil
+    TekniskFeil: TekniskFeil,
+    OppgiBegrunnelse: OppgiBegrunnelse
 };
