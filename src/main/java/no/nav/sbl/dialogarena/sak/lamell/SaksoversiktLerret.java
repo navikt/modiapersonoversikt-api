5fd618c3b5146558d31ca88e5c66ac20ac8860d4
Andreas Lund <andreas.lund@nav.no>
2016-02-08 17:09:52 +0100

    Går mot riktig versjon av reactkomponenter.

--- src/main/java/no/nav/sbl/dialogarena/sak/lamell/SaksoversiktLerret.java
+++ src/main/java/no/nav/sbl/dialogarena/sak/lamell/SaksoversiktLerret.java
@@ -1,12 +1,19 @@
 package no.nav.sbl.dialogarena.sak.lamell;
 
+import no.nav.modig.frontend.FrontendModule;
 import no.nav.modig.modia.lamell.Lerret;
+import no.nav.sbl.dialogarena.reactkomponenter.ResourceReference;
 import no.nav.sbl.dialogarena.reactkomponenter.utils.wicket.ReactComponentPanel;
+import org.apache.wicket.request.resource.PackageResourceReference;
 
 import java.util.HashMap;
 
 
 public class SaksoversiktLerret extends Lerret {
+    public static final FrontendModule RESOURCES = new FrontendModule.With()
+            .less(new PackageResourceReference(ResourceReference.class, "build/saksoversikt-module.less"))
+            .done();
+
     public SaksoversiktLerret(String id, final String fnr) {
         super(id);
 
