463c4286e3765d3620d77e0dc90ad0e633302236
Joanna Eriksson <joanna.eriksson@nav.no>
2016-02-10 12:59:07 +0100

    Har lagt til SaksoversiktController i RestConfig

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/rest/RestConfig.java
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/rest/RestConfig.java
@@ -1,12 +1,13 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.web.rest;
 
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.config.JacksonConfig;
+import no.nav.sbl.dialogarena.sak.rest.SaksoversiktController;
 import no.nav.sbl.dialogarena.varsel.rest.VarslerController;
 import org.glassfish.jersey.server.ResourceConfig;
 
 public class RestConfig extends ResourceConfig {
 
     public RestConfig() {
-        super(JacksonConfig.class, SkrivestotteController.class, MeldingerController.class, JournalforingController.class, VarslerController.class);
+        super(JacksonConfig.class, SkrivestotteController.class, MeldingerController.class, JournalforingController.class, VarslerController.class, SaksoversiktController.class);
     }
 }
