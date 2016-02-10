bf324ee040b4b10fb7f8c2c7c84b9ba12c20977f
Joanna Eriksson <joanna.eriksson@nav.no>
2016-02-10 13:02:14 +0100

    utviklet saksoversikt-store for å gjøre rest-kall

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/saksoversikt-lerret/saksoversikt-store.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/saksoversikt-lerret/saksoversikt-store.js
@@ -8,11 +8,34 @@ class SaksoversiktStore extends Store {
     constructor(fnr) {
         super();
         this._resourcesResolved = this._resourcesResolved.bind(this);
+        const behandlingerByTema = Ajax.get('/modiabrukerdialog/rest/saksoversikt/' + fnr);
+
+        this.state = {
+            behandlingerByTema: [],
+            promise: behandlingerByTema
+        };
         this.state.promise.done(this._resourcesResolved);
+    }
+
+    componentDidMount() {
+        this.store.addListener(this.updateState);
+    }
+
+    componentWillUnmount() {
+        this.store.removeListener(this.updateState);
+    }
+
+    updateState() {
+        this.setState(this.store.getState());
     }
 
     getResources() {
         return this.state.resources;
+    }
+
+    _resourcesResolved(behandlingerByTema) {
+        this.state.behandlingerByTema = behandlingerByTema;
+        this.fireUpdate();
     }
 }
 
