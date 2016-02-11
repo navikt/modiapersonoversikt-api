64551e48f95c3a560ed601ebc7ea31ea5fd4bdef
Joanna Eriksson <joanna.eriksson@nav.no>
2016-02-11 09:39:42 +0100

    [PK-28259 PKULV-1323] OnClick tilagt for radiobuttons

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/saksoversikt-lerret/tema-liste-komponent.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/saksoversikt-lerret/tema-liste-komponent.js
@@ -1,11 +1,15 @@
 import React from 'react/addons';
 
 class TemaListeKomponent extends React.Component {
+    _onClick() {
+        const DOMNode = React.findDOMNode(this);
+        DOMNode.querySelector('input').focus();
+    }
 
     render() {
         const tema = this.props.tema;
         return (
-            <div>
+            <div className="tema-liste-komponent" onClick={this._onClick.bind(this)}>
                 <input id={"radio-"+tema} name="tema" type="radio" value={tema}/>
                 <label htmlFor={"radio-"+tema}>
                     {tema}
