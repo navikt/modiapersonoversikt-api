3fd47885ef4c4099b64bfad3e5fc4d139a154f58
Andreas Lund <andreas.lund@nav.no>
2016-02-16 09:11:52 +0100

    PKULV-1346: Grunnleggende oppsett Sakwidget.

--- src/main/java/no/nav/sbl/dialogarena/sak/widget/SaksoversiktWidget.java
+++ src/main/java/no/nav/sbl/dialogarena/sak/widget/SaksoversiktWidget.java
@@ -1,33 +1,25 @@
 package no.nav.sbl.dialogarena.sak.widget;
 
-import no.nav.modig.modia.widget.async.AsyncWidget;
-import no.nav.sbl.dialogarena.sak.service.interfaces.SaksoversiktService;
-import no.nav.sbl.dialogarena.sak.viewdomain.widget.TemaVM;
-import org.apache.wicket.Component;
-import org.apache.wicket.model.IModel;
+import no.nav.modig.modia.lamell.Lerret;
+import no.nav.sbl.dialogarena.reactkomponenter.utils.wicket.ReactComponentPanel;
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.OnDomReadyHeaderItem;
 
-import javax.inject.Inject;
-import java.util.List;
+import java.util.HashMap;
 
-public class SaksoversiktWidget extends AsyncWidget<TemaVM> {
+public class SaksoversiktWidget extends Lerret {
 
-    @Inject
-    private SaksoversiktService saksoversiktService;
-
-    private final String fnr;
-
-    public SaksoversiktWidget(String id, String initial, String fnr) {
-        super(id, initial, new PropertyKeys().withErrorKey("saker.feilet").withOverflowKey("mange.saker").withEmptyKey("ingen.saker"));
-        this.fnr = fnr;
+    public SaksoversiktWidget(String id, final String fnr) {
+        super(id);
+        add(new ReactComponentPanel("saksoversiktWidget", "SaksoversiktWidget", new HashMap<String, Object>() {{
+            put("fnr", fnr);
+        }}));
     }
 
     @Override
-    public List<TemaVM> getFeedItems() {
-        return saksoversiktService.hentTemaer(fnr);
+    public void renderHead(IHeaderResponse response) {
+        response.render(OnDomReadyHeaderItem.forScript("new Modig.Modia.WidgetView('#" + getMarkupId() + "','" + "S" + "');"));
     }
 
-    @Override
-    public Component newFeedPanel(String id, IModel<TemaVM> model) {
-        return new SaksoversiktWidgetPanel(id, model).setOutputMarkupId(true);
-    }
+
 }
