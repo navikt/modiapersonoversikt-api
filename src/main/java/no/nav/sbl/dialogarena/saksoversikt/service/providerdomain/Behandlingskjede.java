4782902f613cde6d487d8fa5ffa88e6df9b60eb8
Joanna Eriksson <joanna.eriksson@nav.no>
2016-02-17 09:55:58 +0100

    Fikser av Critical sonar-violations

--- src/main/java/no/nav/sbl/dialogarena/saksoversikt/service/providerdomain/Behandlingskjede.java
+++ src/main/java/no/nav/sbl/dialogarena/saksoversikt/service/providerdomain/Behandlingskjede.java
@@ -3,8 +3,8 @@ package no.nav.sbl.dialogarena.saksoversikt.service.providerdomain;
 import org.joda.time.DateTime;
 
 public class Behandlingskjede {
-    public BehandlingsStatus status;
-    public DateTime sistOppdatert;
+    private BehandlingsStatus status;
+    private DateTime sistOppdatert;
 
     public Behandlingskjede withStatus(BehandlingsStatus status){
         this.status = status;
