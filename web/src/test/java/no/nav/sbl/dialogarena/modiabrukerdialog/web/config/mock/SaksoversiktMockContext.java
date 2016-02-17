bec5daff269100f400ce6dd15f7f8f328ec3afa6
Andreas Lund <andreas.lund@nav.no>
2016-02-17 13:03:56 +0100

    Mockcontext saksoversikt.

--- web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/mock/SaksoversiktMockContext.java
+++ web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/mock/SaksoversiktMockContext.java
@@ -1,40 +1,10 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.web.config.mock;
 
-import no.nav.sbl.dialogarena.sak.service.interfaces.SaksoversiktService;
-import no.nav.sbl.dialogarena.sak.transformers.FilterImpl;
-import no.nav.sbl.dialogarena.saksoversikt.service.service.DataFletter;
-import no.nav.sbl.dialogarena.saksoversikt.service.service.HenvendelseService;
-import no.nav.sbl.dialogarena.saksoversikt.service.service.SakOgBehandlingService;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import static org.mockito.Mockito.mock;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@Import(value = {SaksoversiktMockContext.class})
 public class SaksoversiktMockContext {
 
-    @Bean
-    public SaksoversiktService saksoversiktService() {
-        return mock(SaksoversiktService.class);
-    }
-
-    @Bean
-    public FilterImpl sakOgBehandlingFilter() {
-        return mock(FilterImpl.class);
-    }
-
-    @Bean
-    public SakOgBehandlingService sakOgBehandlingService() {
-        return mock(SakOgBehandlingService.class);
-    }
-
-    @Bean
-    public HenvendelseService henvendelseService() {
-        return mock(HenvendelseService.class);
-    }
-
-    @Bean
-    public DataFletter dataFletter() {
-        return mock(DataFletter.class);
-    }
 }
