1e3d7a43201d248e6ef3989d7fa53e7fc0504f10
Nicklas Utgaard <nicklas.utgaard@nav.no>
2016-02-22 13:15:55 +0100

    Småfixes, sluttet å bruke react/addons etter deprecation

--- src/main/java/no/nav/sbl/dialogarena/sak/widget/SaksoversiktWidget.java
+++ src/main/java/no/nav/sbl/dialogarena/sak/widget/SaksoversiktWidget.java
@@ -1,25 +1,17 @@
 package no.nav.sbl.dialogarena.sak.widget;
 
-import no.nav.modig.modia.lamell.Lerret;
+import no.nav.modig.modia.widget.Widget;
 import no.nav.sbl.dialogarena.reactkomponenter.utils.wicket.ReactComponentPanel;
-import org.apache.wicket.markup.head.IHeaderResponse;
-import org.apache.wicket.markup.head.OnDomReadyHeaderItem;
 
 import java.util.HashMap;
 
-public class SaksoversiktWidget extends Lerret {
+public class SaksoversiktWidget extends Widget {
 
     public SaksoversiktWidget(String id, final String fnr) {
-        super(id);
+        super(id, "S", null);
+
         add(new ReactComponentPanel("saksoversiktWidget", "SaksoversiktWidget", new HashMap<String, Object>() {{
             put("fnr", fnr);
         }}));
     }
-
-    @Override
-    public void renderHead(IHeaderResponse response) {
-        response.render(OnDomReadyHeaderItem.forScript("new Modig.Modia.WidgetView('#" + getMarkupId() + "','" + "S" + "');"));
-    }
-
-
 }
