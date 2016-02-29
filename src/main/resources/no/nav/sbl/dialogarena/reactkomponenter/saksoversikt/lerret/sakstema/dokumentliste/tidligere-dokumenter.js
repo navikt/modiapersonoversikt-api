a512262ed0eb20866ab4841e4af0e922afcaf2bb
Joanna Eriksson <joanna.eriksson@nav.no>
2016-02-29 09:04:40 +0100

    [PK-28262 PKULV-1347] Styling av tekst for tidligare dokument.

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/saksoversikt/lerret/sakstema/dokumentliste/tidligere-dokumenter.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/saksoversikt/lerret/sakstema/dokumentliste/tidligere-dokumenter.js
@@ -3,8 +3,10 @@ import { FormattedMessage } from 'react-intl';
 
 const TidligereDokumenter = () => (
     <section className="panel gamle-dokumenter">
-        <img src="/modiabrukerdialog/img/saksoversikt/dokument_stop.png" alt="Dokumentet kan ikke vises"/>
-        <h1 className="typo-element netto"><FormattedMessage id={'dokumentinfo.ikke.vise.tidligere.dokumenter.head'}/>
+        <img className="tidligaredokumentimage" src="/modiabrukerdialog/img/saksoversikt/dokument_stop.png"
+             alt="Dokumentet kan ikke vises"/>
+        <h1 className="tidligaredokumentheader"><FormattedMessage
+            id={'dokumentinfo.ikke.vise.tidligere.dokumenter.head'}/>
         </h1>
         <p className="typo-infotekst"><FormattedMessage id={'dokumentinfo.ikke.vise.tidligere.dokumenter'}/></p>
     </section>
