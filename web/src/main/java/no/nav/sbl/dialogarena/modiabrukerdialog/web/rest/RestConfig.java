caed9f4ee320a40cd341bdaa4c19a8a38b0c613b
Andreas Lund <andreas.lund@nav.no>
2016-03-01 13:10:54 +0100

    Setter opp dokumentvisningssiden og REST-tjenesten med feilvisning

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/rest/RestConfig.java
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/rest/RestConfig.java
@@ -1,6 +1,7 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.web.rest;
 
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.config.JacksonConfig;
+import no.nav.sbl.dialogarena.sak.rest.DokumentController;
 import no.nav.sbl.dialogarena.sak.rest.InformasjonController;
 import no.nav.sbl.dialogarena.sak.rest.SaksoversiktController;
 import no.nav.sbl.dialogarena.varsel.rest.VarslerController;
@@ -9,6 +10,6 @@ import org.glassfish.jersey.server.ResourceConfig;
 public class RestConfig extends ResourceConfig {
 
     public RestConfig() {
-        super(JacksonConfig.class, InformasjonController.class, SkrivestotteController.class, MeldingerController.class, JournalforingController.class, VarslerController.class, SaksoversiktController.class);
+        super(JacksonConfig.class, InformasjonController.class, SkrivestotteController.class, MeldingerController.class, JournalforingController.class, VarslerController.class, DokumentController.class, SaksoversiktController.class);
     }
 }
