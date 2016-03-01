f5949a4e426ed4348a49ee6c3bd09e25e9841235
Joanna Eriksson <joanna.eriksson@nav.no>
2016-03-01 12:18:31 +0100

    [PK-28262 PKULV-1347] Dato i dokumentliste

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/saksoversikt/lerret/sakstema/dokumentliste/dokument-info-elm.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/saksoversikt/lerret/sakstema/dokumentliste/dokument-info-elm.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import DokumentinfoVedlegg from './dokument-info-vedlegg';
 import DokumentAvsender from './dokument/dokument-avsender';
+import { FormattedDate } from 'react-intl';
+import { javaLocalDateTimeToJSDate } from './../../../utils/dato-utils';
 
 class DokumentInfoElm extends React.Component {
     render() {
@@ -8,12 +10,15 @@ class DokumentInfoElm extends React.Component {
         const temaHvisAlleTemaer = this.props.visTema === 'true' ? <p>{dokumentinfo.temakodeVisning}</p> : <noscript/>;
         return (
             <li className="dokumentlisteelement">
+                <p className="datodokumentliste"><FormattedDate value={javaLocalDateTimeToJSDate(dokumentinfo.dato)}
+                                                                day="2-digit" month="2-digit"
+                                                                year="numeric"/></p>
                 <DokumentAvsender className="avsendertext" retning={dokumentinfo.retning}
                                   avsender={dokumentinfo.avsender}
                                   mottaker={dokumentinfo.mottaker}
                                   brukerNavn={this.props.brukerNavn} navn={dokumentinfo.navn}/>
 
-                <div className="hoveddokumenttextwrapper"> <a
+                <div className="hoveddokumenttextwrapper"><a
                     className="hoveddokumenttext">{dokumentinfo.hoveddokument.tittel}</a></div>
                 {temaHvisAlleTemaer}
                 <div className="typo-info">
