ae67b910498b73b2ec002a4abe311ca181c5687d
Nicklas Utgaard <nicklas.utgaard@nav.no>
2016-03-02 10:02:14 +0100

    [FIX] Lenker har nå href og korrekt styling. Litt kodeforbedringer rundt omkring

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/saksoversikt/lerret/sakstema/SakstemaPage.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/saksoversikt/lerret/sakstema/SakstemaPage.js
@@ -5,6 +5,18 @@ import ViktigAViteLenke from './../viktigavite/ViktigAViteLenke'
 import TidligereDokumenter from './dokumentliste/tidligere-dokumenter';
 
 class SakstemaPage extends React.Component {
+    _visningDokumentliste(valgtTema, dokumentliste) {
+        if (valgtTema.temakode === 'BID') {
+            return <p> Modia viser ikke dokumenter på temaet Bidrag. </p>;
+        }
+        else if (valgtTema.dokumentMetadata.length > 0) {
+            return <div>{ dokumentliste }<TidligereDokumenter /></div>;
+        }
+
+        return (<p>Det finnes ingen dokumenter på dette temaet. Modia viser kun dokumenter etter dd.måned åååå
+            (prodsettingsdato). Du kan gå til Gosys for å se eldre dokumenter</p>);
+    }
+
     render() {
         const { sakstema, valgtTema, velgSak, brukerNavn, visSide } = this.props;
         const dokumenter = sakstema.reduce((acc, tema) => {
@@ -21,8 +33,7 @@ class SakstemaPage extends React.Component {
         return (
             <div className="sakstema-container">
                 <section className="saksoversikt-liste">
-                    <SakstemaListe sakstema={sakstema} velgSak={velgSak}
-                                   valgtTema={valgtTema}/>
+                    <SakstemaListe sakstema={sakstema} velgSak={velgSak} valgtTema={valgtTema}/>
                 </section>
                 <section className="saksoversikt-innhold side-innhold">
                     <ViktigAViteLenke valgtTema={valgtTema} visSide={visSide}/>
@@ -30,18 +41,6 @@ class SakstemaPage extends React.Component {
                 </section>
             </div>
         );
-    }
-
-    _visningDokumentliste(valgtTema, dokumentliste) {
-        if (valgtTema.temakode === 'BID') {
-            return <p> Modia viser ikke dokumenter på temaet Bidrag. </p>;
-        }
-        else if (valgtTema.dokumentMetadata.length > 0) {
-            return <div>{ dokumentliste }<TidligereDokumenter /></div>;
-        }
-
-        return (<p> Det finnes ingen dokumenter på dette temaet. Modia viser kun dokumenter etter dd.måned åååå
-            (prodsettingsdato). Du kan gå til Gosys for å se eldre dokumenter</p>);
     }
 }
 
