eb38da7d80efffa2734dbaa840090aa88b7c1000
Joanna Eriksson <joanna.eriksson@nav.no>
2016-03-02 13:43:40 +0100

    [PK-28262 PKULV-1347] Lenke til gosys fungerer. Litt refaktorering

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/saksoversikt/lerret/sakstema/SakstemaPage.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/saksoversikt/lerret/sakstema/SakstemaPage.js
@@ -1,23 +1,11 @@
 import React, { PropTypes as PT } from 'react';
 import SakstemaListe from './SakstemaListe';
-import DokumentListe from './dokumentliste/dokumentliste'
 import ViktigAViteLenke from './../viktigavite/ViktigAViteLenke'
-import TidligereDokumenter from './dokumentliste/tidligere-dokumenter';
-import { FormattedMessage } from 'react-intl';
+import VisningDokumentliste from './dokumentliste/visning-dokumentliste'
 
 class SakstemaPage extends React.Component {
     render() {
         const { sakstema, valgtTema, velgSak, brukerNavn, visSide } = this.props;
-        const dokumenter = sakstema.reduce((acc, tema) => {
-            return acc.concat(tema.dokumentMetadata);
-        }, []);
-
-        const dokumentliste = valgtTema.temakode !== 'alle' ?
-            <DokumentListe visTema="false"
-                           dokumentMetadata={valgtTema.dokumentMetadata}
-                           brukerNavn={brukerNavn}/> :
-            <DokumentListe visTema="true" dokumentMetadata={dokumenter}
-                           brukerNavn={brukerNavn}/>;
 
         return (
             <div className="sakstema-container">
@@ -27,31 +15,10 @@ class SakstemaPage extends React.Component {
                 </section>
                 <section className="saksoversikt-innhold side-innhold">
                     <ViktigAViteLenke valgtTema={valgtTema} visSide={visSide}/>
-                    {this._visningDokumentliste(valgtTema, dokumentliste)}
+                    <VisningDokumentliste sakstema={sakstema} valgtTema={valgtTema} brukerNavn={brukerNavn}/>
                 </section>
             </div>
         );
-    }
-
-    _visningDokumentliste(valgtTema, dokumentliste) {
-        if (valgtTema.dokumentMetadata.length > 0) {
-            return <div>{ dokumentliste }<TidligereDokumenter /></div>;
-        }
-        const ingendokumenter = <h1 className="robust-ikon-feil-strek ingendokumenterheader">
-            <FormattedMessage
-                id="dokumentinfo.sakstema.ingen.dokumenter.header"/></h1>;
-
-        if (valgtTema.temakode === 'BID') {
-            return (<div className="default-error ingendokumenter">{ingendokumenter}
-                <p className="ingendokumenterforklaring"><FormattedMessage
-                    id="dokumentinfo.sakstema.ingen.dokumenter.bidrag"/></p></div>);
-        }
-        return (
-            <div className="default-error ingendokumenter">{ingendokumenter}
-                <p className="ingendokumenterforklaring"><FormattedMessage
-                    id="dokumentinfo.sakstema.ingen.dokumenter.forklaring"/></p>
-                <a><FormattedMessage id="dokumentinfo.sakstema.lenke.gosys"/></a>
-            </div >);
     }
 }
 
