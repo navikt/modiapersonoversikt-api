19a0ededd8b69b7d108599e92584cfd7026313f5
Nicklas Utgaard <nicklas.utgaard@nav.no>
2016-03-07 14:56:22 +0100

    PK-22195 dokumentvisning lenker og styling

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/saksoversikt/lerret/dokumentvisning/DokumentVisning.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/saksoversikt/lerret/dokumentvisning/DokumentVisning.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { autobind } from './../../../utils/utils-module';
+import { debounce, autobind } from './../../../utils/utils-module';
 
 const a4Ratio = 2 / Math.sqrt(2);
-const iterimStyling = (antallSider, width = 750) => ({
+const stylingFn = (antallSider, width = 750) => ({
     width: '100%',
     maxWidth: '100%',
     marginBottom: '2rem',
-    height: a4Ratio * antallSider * width //magisk tall
+    height: a4Ratio * antallSider * width + 100 //magisk tall
 });
 
 class DokumentVisning extends Component {
@@ -14,7 +14,7 @@ class DokumentVisning extends Component {
         super(props);
 
         this.state = {
-            ...iterimStyling(props.dokument.antallSider)
+            ...stylingFn(props.dokument.antallSider)
         };
 
         autobind(this);
@@ -24,14 +24,32 @@ class DokumentVisning extends Component {
         const pdf = this.refs.pdf;
         const width = pdf.offsetWidth;
 
-        var height = a4Ratio * this.props.dokument.antallsider * width;
+        const height = stylingFn(this.props.dokument.antallsider, width).height;
 
         if (this.state.height !== height) {
             this.setState({ height: height });
         }
     }
 
+    _print(e) {
+        e.preventDefault();
+        try {
+            this.refs.pdf.print()
+        } catch (e) {
+            const printWindow = window.open(this.props.dokument.pdfUrl);
+
+            printWindow.onload = () => {
+                printWindow.print();
+            };
+
+            return false;
+        }
+
+    }
+
     componentDidMount() {
+        this.eventHandler = debounce(this._oppdaterPdfVisning, 150);
+        window.addEventListener('resize', this.eventHandler);
         this._oppdaterPdfVisning();
     }
 
@@ -39,15 +57,29 @@ class DokumentVisning extends Component {
         this._oppdaterPdfVisning();
     }
 
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.eventHandler);
+    }
+
     render() {
         const { dokument } = this.props;
-        const pdfData = `${dokument.pdfUrl}#view=FitH&amp;scrollbar=0&amp;toolbar=0&amp;statusbar=0&amp;messages=0&amp;navpanes=0`;
+        const pdfData = `${dokument.pdfUrl}#view=FitH&scrollbar=0&toolbar=0&statusbar=0&messages=0&navpanes=0`;
+        const style = { ...this.state };
 
         return (
             <section key={`${dokument.journalpostId}--${dokument.dokumentreferanse}`}>
-                <h2>{dokument.tittel}</h2>
-                <object ref="pdf" data={pdfData} type="application/pdf" scrolling="no"
-                        style={iterimStyling(dokument.antallsider)}>
+                <div className="dokumentheader blokk-xxxs">
+                    <h2 className="typo-element">{dokument.tittel}</h2>
+                    <div className="lokal-linker">
+                        <a target="_blank" href={dokument.pdfUrl}>
+                            <span>Åpne som PDF</span>
+                        </a>
+                        <a href="javscript:void(0)" onClick={this._print}>
+                            <span>Skriv ut</span>
+                        </a>
+                    </div>
+                </div>
+                <object ref="pdf" data={pdfData} type="application/pdf" scrolling="no" style={style} key={style.height}>
                     <param name="view" value="FitV"/>
 
                     <p>Kunne ikke vise pdf inline</p>
