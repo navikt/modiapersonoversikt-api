da723dc8d41fbcd33e51ef24ef14f92aab7bd66b
Knut Nygaard <knut.nygaard@nav.no>
2016-03-10 09:02:59 +0100

    Lager en kontruktør i resultatwrapperen hvor en ikke trenger sende inn et tomt sett med feil

--- src/main/java/no/nav/sbl/dialogarena/saksoversikt/service/providerdomain/resultatwrappere/ResultatWrapper.java
+++ src/main/java/no/nav/sbl/dialogarena/saksoversikt/service/providerdomain/resultatwrappere/ResultatWrapper.java
@@ -2,11 +2,17 @@ package no.nav.sbl.dialogarena.saksoversikt.service.providerdomain.resultatwrapp
 
 import no.nav.sbl.dialogarena.saksoversikt.service.providerdomain.Baksystem;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class ResultatWrapper<T> {
     public T resultat;
     public Set<Baksystem> feilendeSystemer;
+
+    public ResultatWrapper(T resultat) {
+        this.resultat = resultat;
+        feilendeSystemer = new HashSet<>();
+    }
 
     public ResultatWrapper(T resultat, Set<Baksystem> feilendeSystemer) {
         this.resultat = resultat;
