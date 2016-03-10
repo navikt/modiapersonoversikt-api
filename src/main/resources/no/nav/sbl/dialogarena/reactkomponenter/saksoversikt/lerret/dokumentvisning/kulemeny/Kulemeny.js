e74e70e63c59fd3f81c08069deedd26604756827
Nicklas Utgaard <nicklas.utgaard@nav.no>
2016-03-10 14:16:06 +0100

    PK-22195 fungerende kulemeny

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/saksoversikt/lerret/dokumentvisning/kulemeny/Kulemeny.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/saksoversikt/lerret/dokumentvisning/kulemeny/Kulemeny.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import { Button } from 'react-scroll';
 
+const isInside = (y, elemTopBound, elemBottomBound, containerCoords, elementCoords) => {
+    const midtpunktish = y + 650;
+    console.log(y, midtpunktish, elemTopBound, elemBottomBound, (midtpunktish >= elemTopBound && midtpunktish <= elemBottomBound));
+    return (midtpunktish >= elemTopBound && midtpunktish <= elemBottomBound);
+};
+
 const Kulemeny = ({ dokref, tittel }) => (
     <li>
-        <Button activeClass="active" to={dokref} spy smooth duration={500} offset={-80} title={tittel} type="button" containerId="js-kulemeny-scroll" />
+        <Button
+            activeClass="active"
+            to={dokref}
+            spy smooth
+            duration={500}
+            offset={-80}
+            title={tittel}
+            isInside={isInside}
+            type="button"
+            containerId="js-kulemeny-scroll"
+        />
     </li>
 );
 
