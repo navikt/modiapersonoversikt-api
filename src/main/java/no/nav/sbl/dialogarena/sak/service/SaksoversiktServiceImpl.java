49306b30b6c794f7ec83fd70e6e80d47763abe75
Joanna Eriksson <joanna.eriksson@nav.no>
2016-03-16 10:26:08 +0100

    [PKFEIL-16754] Filterer vekk dokument som kun finnes i Joark og har dato før prod-settning.
    Prodsettningsdato finnes i fasit

--- src/main/java/no/nav/sbl/dialogarena/sak/service/SaksoversiktServiceImpl.java
+++ src/main/java/no/nav/sbl/dialogarena/sak/service/SaksoversiktServiceImpl.java
@@ -7,6 +7,9 @@ import no.nav.sbl.dialogarena.sak.comparators.SistOppdaterteBehandlingComparator
 import no.nav.sbl.dialogarena.sak.service.interfaces.SaksoversiktService;
 import no.nav.sbl.dialogarena.sak.transformers.FilterImpl;
 import no.nav.sbl.dialogarena.sak.viewdomain.widget.Tema;
+import no.nav.sbl.dialogarena.saksoversikt.service.providerdomain.Baksystem;
+import no.nav.sbl.dialogarena.saksoversikt.service.providerdomain.DokumentMetadata;
+import no.nav.sbl.dialogarena.saksoversikt.service.providerdomain.Sakstema;
 import no.nav.sbl.dialogarena.saksoversikt.service.service.BulletproofKodeverkService;
 import no.nav.sbl.dialogarena.saksoversikt.service.service.SakOgBehandlingService;
 import no.nav.tjeneste.virksomhet.aktoer.v1.AktoerPortType;
@@ -16,8 +19,15 @@ import no.nav.tjeneste.virksomhet.sakogbehandling.v1.informasjon.finnsakogbehand
 import org.slf4j.Logger;
 
 import javax.inject.Inject;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+import static java.lang.System.getProperty;
 import static no.nav.modig.lang.collections.IterUtils.on;
 import static no.nav.sbl.dialogarena.sak.transformers.SakOgBehandlingTransformers.temaVMTransformer;
 import static org.slf4j.LoggerFactory.getLogger;
@@ -61,5 +71,58 @@ public class SaksoversiktServiceImpl implements SaksoversiktService {
         request.setIdent(fnr);
         return request;
     }
+
+    @Override
+    public void fjernGamleDokumenter(List<Sakstema> resultat) {
+
+        List<String> journalpostIdAaFjerne = getJournalpostIdAaFjerne(resultat);
+
+        resultat.stream()
+                .forEach(sakstema -> sakstema.withDokumentMetadata(sakstema.dokumentMetadata.stream()
+                        .filter(dokumentMetadata -> !journalpostIdAaFjerne
+                                .contains(dokumentMetadata.getJournalpostId())).collect(Collectors.toList())));
+    }
+
+
+    private List<String> getJournalpostIdAaFjerne(List<Sakstema> resultat) {
+        Map<String, DokumentMetadata> muligeJournalpostIdAaFjerne = new HashMap<>();
+
+        final LocalDateTime PROD_SETTNING_DATO = formatDate(getProperty("saksoversikt.prodsettningsdato"));
+
+
+        getMuligeJournalpostIdAaFjerne(resultat, muligeJournalpostIdAaFjerne);
+
+        return muligeJournalpostIdAaFjerne
+                .keySet()
+                .stream()
+                .filter(key -> muligeJournalpostIdAaFjerne
+                        .get(key).getBaksystem()
+                        .equals(Baksystem.JOARK))
+                .filter(joarkKey -> muligeJournalpostIdAaFjerne
+                        .get(joarkKey)
+                        .getDato()
+                        .isBefore(PROD_SETTNING_DATO))
+                .collect(Collectors.toList());
+    }
+
+    private void getMuligeJournalpostIdAaFjerne(List<Sakstema> resultat, Map<String, DokumentMetadata> muligeJournalpostIdAaFjerne) {
+        for (Sakstema sakstema : resultat) {
+            for (DokumentMetadata dokumentMetadata : sakstema.dokumentMetadata) {
+                String journalpostId = dokumentMetadata.getJournalpostId();
+                if (muligeJournalpostIdAaFjerne.containsKey(journalpostId)) {
+                    muligeJournalpostIdAaFjerne.remove(journalpostId);
+                } else {
+                    muligeJournalpostIdAaFjerne.put(journalpostId, dokumentMetadata);
+                }
+            }
+        }
+    }
+
+    private static LocalDateTime formatDate(String prodDate) {
+        LocalDate date = LocalDate.parse(prodDate);
+        LocalTime time = LocalTime.of(0, 0);
+        return LocalDateTime.of(date, time);
+    }
+
 
 }
---
+++ src/test/java/no/nav/sbl/dialogarena/sak/service/SaksoversiktServiceTest.java
@@ -0,0 +1,64 @@
+package no.nav.sbl.dialogarena.sak.service;
+
+import no.nav.sbl.dialogarena.sak.service.interfaces.SaksoversiktService;
+import no.nav.sbl.dialogarena.saksoversikt.service.providerdomain.DokumentMetadata;
+import no.nav.sbl.dialogarena.saksoversikt.service.providerdomain.Sakstema;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static no.nav.sbl.dialogarena.saksoversikt.service.providerdomain.Baksystem.HENVENDELSE;
+import static no.nav.sbl.dialogarena.saksoversikt.service.providerdomain.Baksystem.JOARK;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SaksoversiktServiceTest {
+
+    private static final String PROD_SETTNINGS_DATO = "2015-04-13";
+
+    @Before
+    public void setup() {
+        System.setProperty("saksoversikt.prodsettningsdato", PROD_SETTNINGS_DATO);
+    }
+
+    @InjectMocks
+    private SaksoversiktService saksoversiktService = new SaksoversiktServiceImpl();
+
+    @Test
+    public void fjernerGamleJournalpostSomKunFinnesIJoark() {
+        List<Sakstema> saksteman = getMockSaksteman();
+
+        saksoversiktService.fjernGamleDokumenter(saksteman);
+
+        assertThat(saksteman.get(0).dokumentMetadata.size(), is(2));
+        assertThat(saksteman.get(1).dokumentMetadata.size(), is(2));
+        assertThat(saksteman.get(0).dokumentMetadata.get(0).getJournalpostId(), is("123"));
+        assertThat(saksteman.get(0).dokumentMetadata.get(1).getJournalpostId(), is("456"));
+        assertThat(saksteman.get(1).dokumentMetadata.get(0).getJournalpostId(), is("123"));
+        assertThat(saksteman.get(1).dokumentMetadata.get(1).getJournalpostId(), is("012"));
+    }
+
+    private List<Sakstema> getMockSaksteman() {
+        List<Sakstema> saksteman = new ArrayList<>();
+        DokumentMetadata dokumentmetadata1 = new DokumentMetadata().withJournalpostId("123").withBaksystem(JOARK).withDato(LocalDateTime.of(2011, Month.APRIL, 8, 12, 30));
+        DokumentMetadata dokumentmetadata2 = new DokumentMetadata().withJournalpostId("123").withBaksystem(HENVENDELSE).withDato(LocalDateTime.of(2011, Month.APRIL, 8, 12, 30));
+        DokumentMetadata dokumentmetadata3 = new DokumentMetadata().withJournalpostId("456").withBaksystem(HENVENDELSE).withDato(LocalDateTime.of(2011, Month.APRIL, 8, 12, 30));
+        DokumentMetadata dokumentmetadata4 = new DokumentMetadata().withJournalpostId("789").withBaksystem(JOARK).withDato(LocalDateTime.of(2011, Month.APRIL, 8, 12, 30));
+        DokumentMetadata dokumentmetadata5 = new DokumentMetadata().withJournalpostId("012").withBaksystem(JOARK).withDato(LocalDateTime.of(2016, Month.APRIL, 8, 12, 30));
+
+        Sakstema sakstema1 = new Sakstema().withDokumentMetadata(asList(dokumentmetadata1, dokumentmetadata3, dokumentmetadata4));
+        Sakstema sakstema2 = new Sakstema().withDokumentMetadata(asList(dokumentmetadata2, dokumentmetadata5));
+
+        saksteman.addAll(asList(sakstema1, sakstema2));
+        return saksteman;
+    }
+}
