020dedf8aafc594dd3c91fb28bc0ab864f94765e
Nicklas Utgaard <nicklas.utgaard@nav.no>
2016-03-17 08:43:48 +0100

    PKFEIL-16996: la til hack for å fikse samspillet mellom pdf-er og modaler

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/modal/modal-portal.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/modal/modal-portal.js
@@ -135,10 +135,13 @@ const ModalPortal = React.createClass({
 
         const cls = this.props.isOpen ? '' : 'hidden';
 
+        // iframe må være her for å fikse en rendering bug i IE, uten iframe'n vil inline-pdf-visningen alltid legge
+        // seg over modal-vinduene.
         return (
             <div tabIndex="-1" className={cls} aria-hidden={!this.props.isOpen} onKeyDown={this.keyHandler}
                  role="dialog" aria-labelledby={title.id} aria-describedby={description.id}>
                 <div className="backdrop" onClick={this.props.modal.close}></div>
+                <iframe src="about:blank" className="cover" />
                 {title.hidden}
                 {description.hidden}
                 <div className="centering" style={this.props.width ? {'width': this.props.width + 'px'} : null}>
