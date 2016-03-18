5b9b23faffcb30f9c984bc17a17fec7085fe9bfe
Joanna Eriksson <joanna.eriksson@nav.no>
2016-03-18 09:09:23 +0100

    Fikser et feil ved merge

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/saksoversikt/lerret/dokumentvisning/dokument-visning-page.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/saksoversikt/lerret/dokumentvisning/dokument-visning-page.js
@@ -7,7 +7,7 @@ import VedleggFeilmeldingListe from './vedlegg-veilmelding-liste';
 import * as Const from './../../konstanter';
 import Snurrepipp from './../../../utils/snurrepipp';
 import { datoformat, javaLocalDateTimeToJSDate } from './../../utils/dato-utils';
-import DokumentVisningListe from './DokumentVisningListe';
+import DokumentVisningListe from './dokument-visning-liste';
 import { FormattedMessage, FormattedDate } from 'react-intl';
 import KulemenyListe from './kulemeny/kulemeny-liste';
 
