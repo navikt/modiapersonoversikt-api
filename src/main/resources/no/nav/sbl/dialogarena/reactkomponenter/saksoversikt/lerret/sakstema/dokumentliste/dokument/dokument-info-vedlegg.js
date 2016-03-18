52efc4fdbd8e089b1bfe6b224f7cab7cd519ede2
Joanna Eriksson <joanna.eriksson@nav.no>
2016-03-18 09:47:26 +0100

    skrevet om DokumentInfoVedlegg til stateless og fikset eslint

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/saksoversikt/lerret/sakstema/dokumentliste/dokument/dokument-info-vedlegg.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/saksoversikt/lerret/sakstema/dokumentliste/dokument/dokument-info-vedlegg.js
@@ -1,47 +1,34 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 
-const DokumentinfoVedlegg = ({visSide, velgJournalpost, dokumentinfo})=> {
+const DokumentinfoVedlegg = ({ visSide, velgJournalpost, dokumentinfo }) => {
     function _redirect(e) {
         e.preventDefault();
         visSide('dokumentvisning');
         velgJournalpost(dokumentinfo);
     }
-}
 
-// TODO stateless function
-class DokumentinfoVedlegg extends React.Component {
-
-    _redirect(e) {
-        e.preventDefault();
-        this.props.visSide('dokumentvisning');
-        this.props.velgJournalpost(this.props.dokumentinfo);
+    if (!dokumentinfo.vedlegg || dokumentinfo.vedlegg.length === 0) {
+        return <noscript />;
     }
 
-    render() {
-        const { dokumentinfo } = this.props;
+    const vedleggListe = dokumentinfo.vedlegg.map((dokumentVedlegg) => (
+        <li className="vedlegg-element">
+            <a href="javascript:void(0);" onClick={_redirect}
+              className="vedleggtext"
+            >
+                {dokumentVedlegg.tittel}
+            </a>
+        </li>));
+    return (
+        <div className="vedleggcontainer">
+            <div className="vedleggheader"><FormattedMessage id="dokumentinfo.vedlegg"/></div>
+            <ul className="vedleggliste">{vedleggListe}</ul>
+        </div>
 
-        if (!dokumentinfo.vedlegg || dokumentinfo.vedlegg.length === 0) {
-            return <noscript />;
-        }
+    );
+};
 
-        const vedleggListe = dokumentinfo.vedlegg.map((dokumentVedlegg) => (
-            <li className="vedlegg-element">
-                <a href="javascript:void(0);" onClick={this._redirect.bind(this)}
-                   className="vedleggtext"
-                >
-                    {dokumentVedlegg.tittel}
-                </a>
-            </li>));
-
-        return (
-            <div className="vedleggcontainer">
-                <div className="vedleggheader"><FormattedMessage id="dokumentinfo.vedlegg"/></div>
-                <ul className="vedleggliste">{vedleggListe}</ul>
-            </div>
-        );
-    }
-}
 
 DokumentinfoVedlegg.propTypes = {
     dokumentinfo: React.PropTypes.object.isRequired,
