971c32c740b62c0f7b5d04a3400f6b3e4ef6ebb1
Ingrid Guren <ingrid.guren@nav.no>
2016-03-29 15:32:31 +0200

    Delt opp behandlingsstatus i flere filer og laget tester for antall behandligskjeder og siste oppdateringsdato

---
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/saksoversikt/utils/siste-oppdatering/behandlingsstatus-test.js
@@ -0,0 +1,128 @@
+import './../../../test-config';
+import { expect } from 'chai';
+import sinon from 'sinon';
+import React from 'react';
+import assign from 'object-assign';
+import TestUtils from 'react-addons-test-utils';
+import { finnBehandlingsstatus, finnAntallBehandlingskjeder, underBehandlingEllerNyereEnnGrenseverdi } from './behandlingsstatus';
+
+const datoForGrense = {
+    year: new Date().getFullYear()-1,
+    monthValue: new Date().getMonth()+1,
+    dayOfMonth: new Date().getDate(),
+    hour: 10,
+    minute: 39,
+    second: 55
+};
+
+const datoEtterGrense = {
+    year: new Date().getFullYear(),
+    monthValue: new Date().getMonth()+1,
+    dayOfMonth: new Date().getDate(),
+    hour: 10,
+    minute: 39,
+    second: 55
+};
+
+const behandlingskjedeEtterGrenseFerdigbehandlet = {
+    sistOppdatert: datoEtterGrense,
+    status: "FERDIG_BEHANDLET"
+};
+
+const behandlingskjedeFoerGrenseFerdigbehandlet = {
+    sistOppdatert: datoForGrense,
+    status: "FERDIG_BEHANDLET"
+};
+
+const behandlingskjedeEtterGrenseUnderBehandling = {
+    sistOppdatert: datoEtterGrense,
+    status: "UNDER_BEHANDLING"
+};
+
+const behandlingskjedeForGrenseUnderBehandling = {
+    sistOppdatert: datoForGrense,
+    status: "UNDER_BEHANDLING"
+};
+
+describe('Behandlingsstatus', () => {
+
+    const antallDagerFerdigBehandletStatusErGyldig = "28";
+
+    it('finnes ikke hvis ingen behandlingskjeder er under behandling eller ferdig behandlet', () => {
+        const behandlingskjeder = [];
+
+        const behandlingsstatustekst = finnBehandlingsstatus(behandlingskjeder, antallDagerFerdigBehandletStatusErGyldig);
+
+        expect(behandlingsstatustekst).to.be.null;
+    });
+});
+
+describe('AntallBehandlingskjeder', () => {
+
+    it('Returnerer 4 behandlingskjeder', () => {
+        const behandlingskjeder = [
+            behandlingskjedeEtterGrenseFerdigbehandlet,
+            behandlingskjedeEtterGrenseUnderBehandling,
+            behandlingskjedeEtterGrenseUnderBehandling,
+            behandlingskjedeEtterGrenseFerdigbehandlet
+        ];
+
+        const antallBehandligsstatuser = finnAntallBehandlingskjeder(behandlingskjeder);
+
+        expect(antallBehandligsstatuser.antallFerdigBehandlet).to.be.eql(2);
+        expect(antallBehandligsstatuser.antallUnderBehandling).to.be.eql(2);
+    });
+
+
+    it('Skal returnerer ingen behandlingskjeder under behandling', () => {
+        const behandlingskjeder = [
+            behandlingskjedeEtterGrenseFerdigbehandlet,
+            behandlingskjedeEtterGrenseFerdigbehandlet
+        ];
+
+        const antallBehandligsstatuser = finnAntallBehandlingskjeder(behandlingskjeder);
+
+        expect(antallBehandligsstatuser.antallFerdigBehandlet).to.be.eql(2);
+        expect(antallBehandligsstatuser.antallUnderBehandling).to.be.eql(0);
+    });
+
+    it('Skal returnerer en behandlingskjede som er under behandling', () => {
+        const behandlingskjeder = [
+            behandlingskjedeEtterGrenseUnderBehandling
+        ];
+
+        const antallBehandligsstatuser = finnAntallBehandlingskjeder(behandlingskjeder);
+
+        expect(antallBehandligsstatuser.antallFerdigBehandlet).to.be.eql(0);
+        expect(antallBehandligsstatuser.antallUnderBehandling).to.be.eql(1);
+    });
+});
+
+describe('FiltrerBehandligskjeder', () => {
+
+    it('Returnerer tom liste om ferdigbehandlede behandlingskjeder foer grensen', () => {
+       const behandlingskjeder = [
+           behandlingskjedeFoerGrenseFerdigbehandlet
+       ];
+
+        const filtrertBehandlingskjedeListe = behandlingskjeder.filter(underBehandlingEllerNyereEnnGrenseverdi( 21 ));
+
+        expect(filtrertBehandlingskjedeListe.length).to.be.eql(0);
+
+    });
+
+
+    it('Returnerer behandlingskjeder under behandling eller etter grenseverdien', () => {
+        const behandlingskjeder = [
+            behandlingskjedeForGrenseUnderBehandling,
+            behandlingskjedeEtterGrenseUnderBehandling,
+            behandlingskjedeEtterGrenseFerdigbehandlet
+        ];
+
+        const filtrertBehandlingskjedeListe = behandlingskjeder.filter(underBehandlingEllerNyereEnnGrenseverdi( 21 ));
+
+        expect(filtrertBehandlingskjedeListe.length).to.be.eql(3);
+
+    });
+
+});
---
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/saksoversikt/utils/siste-oppdatering/siste-oppdateringsdato.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+import { javaLocalDateTimeToJSDate } from './../dato-utils';
+
+export const sisteOppdatering = (nyesteDokument, nyesteBehandlingskjede) => {
+    const harDokument = !!nyesteDokument;
+    const harBehandlingskjede = !!nyesteBehandlingskjede;
+
+    let sisteOppdateringISaken;
+
+    if (harBehandlingskjede && !harDokument) {
+        sisteOppdateringISaken = javaLocalDateTimeToJSDate(nyesteBehandlingskjede.sistOppdatert);
+    } else if (harDokument && !harBehandlingskjede) {
+        sisteOppdateringISaken = javaLocalDateTimeToJSDate(nyesteDokument.dato);
+    } else if (!nyesteBehandlingskjede && !nyesteDokument) {
+        sisteOppdateringISaken = null;
+    } else {
+        const sistOppdatertBehandlingskjedeDate = javaLocalDateTimeToJSDate(nyesteBehandlingskjede.sistOppdatert);
+        const sistOppdatertDokumentDate = javaLocalDateTimeToJSDate(nyesteDokument.dato);
+        sisteOppdateringISaken = sistOppdatertDokumentDate > sistOppdatertBehandlingskjedeDate ?
+            sistOppdatertDokumentDate : sistOppdatertBehandlingskjedeDate;
+    }
+
+    return sisteOppdateringISaken;
+};
