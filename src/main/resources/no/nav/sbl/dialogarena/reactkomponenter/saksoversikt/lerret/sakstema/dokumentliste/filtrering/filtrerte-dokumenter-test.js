63d4190a40558423b67c584ed38995396165a3a1
Ingrid Guren <ingrid.guren@nav.no>
2016-03-30 12:14:38 +0200

    Tester for filtrering etter avsender

---
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/saksoversikt/lerret/sakstema/dokumentliste/filtrering/filtrerte-dokumenter-test.js
@@ -0,0 +1,94 @@
+import './../../../../../test-config';
+import { expect } from 'chai';
+import React from 'react';
+import assign from 'object-assign';
+import { skalViseDokument } from './filtrerte-dokumenter';
+
+describe('FiltrerDokumenter', () => {
+
+    const utgaaendeDokument = {
+        'avsender': 'NAV',
+        'retning': 'UT'
+    };
+
+    const inngaaaendeDokumentSluttbruker = {
+        'avsender': 'SLUTTBRUKER',
+        'retning': 'INN'
+    };
+
+    const inngaaendeDokumentAndre = {
+        'avsender': 'ANDRE',
+        'retning': 'INN'
+    };
+
+    const internDokument = {
+        'avsender': 'NAV',
+        'retning': 'INTERN'
+    };
+
+    it('Skal vise alle dokumenter naar alle filtreringsvalg er valgt', () => {
+
+        const filtreringsvalg = {
+            'NAV': true,
+            'BRUKER': true,
+            'ANDRE': true
+        };
+
+        const skalViseUtgaaendeDokument = skalViseDokument(utgaaendeDokument, filtreringsvalg);
+        expect(skalViseUtgaaendeDokument).to.be.eql(true);
+
+        const skalViseInngaaendeDokumentSluttbruker = skalViseDokument(inngaaaendeDokumentSluttbruker, filtreringsvalg);
+        expect(skalViseInngaaendeDokumentSluttbruker).to.be.eql(true);
+
+        const skalViseInngaaendeDokumentAndre = skalViseDokument(inngaaendeDokumentAndre, filtreringsvalg);
+        expect(skalViseInngaaendeDokumentAndre).to.be.eql(true);
+
+        const skalViseinternDokument = skalViseDokument(internDokument, filtreringsvalg);
+        expect(skalViseinternDokument).to.be.eql(true);
+    });
+
+    it('Skal vise ingen dokumenter naar ingen filtreringsvalg er valgt', () => {
+
+        const filtreringsvalg = {
+            'NAV': false,
+            'BRUKER': false,
+            'ANDRE': false
+        };
+
+        const skalViseUtgaaendeDokument = skalViseDokument(utgaaendeDokument, filtreringsvalg);
+        expect(skalViseUtgaaendeDokument).to.be.eql(false);
+
+        const skalViseInngaaendeDokumentSluttbruker = skalViseDokument(inngaaaendeDokumentSluttbruker, filtreringsvalg);
+        expect(skalViseInngaaendeDokumentSluttbruker).to.be.eql(false);
+
+        const skalViseInngaaendeDokumentAndre = skalViseDokument(inngaaendeDokumentAndre, filtreringsvalg);
+        expect(skalViseInngaaendeDokumentAndre).to.be.eql(false);
+
+        const skalViseinternDokument = skalViseDokument(internDokument, filtreringsvalg);
+        expect(skalViseinternDokument).to.be.eql(false);
+
+    });
+
+    it('Skal vise alle dokumenter fra NAV naar filtreringsvalget NAV er valgt', () => {
+        const filtreringsvalg = {
+            'NAV': true,
+            'BRUKER': false,
+            'ANDRE': false
+        };
+
+        const skalViseUtgaaendeDokument = skalViseDokument(utgaaendeDokument, filtreringsvalg);
+        expect(skalViseUtgaaendeDokument).to.be.eql(true);
+
+        const skalViseInngaaendeDokumentSluttbruker = skalViseDokument(inngaaaendeDokumentSluttbruker, filtreringsvalg);
+        expect(skalViseInngaaendeDokumentSluttbruker).to.be.eql(false);
+
+        const skalViseInngaaendeDokumentAndre = skalViseDokument(inngaaendeDokumentAndre, filtreringsvalg);
+        expect(skalViseInngaaendeDokumentAndre).to.be.eql(false);
+
+        const skalViseinternDokument = skalViseDokument(internDokument, filtreringsvalg);
+        expect(skalViseinternDokument).to.be.eql(true);
+
+    })
+
+
+});
