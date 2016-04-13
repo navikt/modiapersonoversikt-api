8f6926d86e4530c5fcd9e96f3c23a43fa2fbdcaa
Andreas Lund <andreas.lund@nav.no>
2016-04-13 14:29:53 +0200

    Skal ha én statisk lenke til Se saker. Legger denne derfor på wicketsiden. Så har man ikke noe react for widgeten (  :(   )

--- src/main/java/no/nav/sbl/dialogarena/sak/widget/SaksoversiktWidget.java
+++ src/main/java/no/nav/sbl/dialogarena/sak/widget/SaksoversiktWidget.java
@@ -7,6 +7,7 @@ import org.apache.wicket.ajax.AjaxEventBehavior;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.behavior.AttributeAppender;
 import org.apache.wicket.event.Broadcast;
+import org.apache.wicket.markup.html.link.Link;
 
 import static no.nav.modig.modia.events.InternalEvents.WIDGET_HEADER_CLICKED;
 
@@ -22,11 +23,19 @@ public class SaksoversiktWidget extends Widget<Object> {
                 new AttributeAppender("role", "link").setSeparator(" ")
         );
 
+
+        add(
+                new Link("vissaker") {
+                    @Override
+                    public void onClick() {}
+                }.add(createHeaderClickBehavior())
+        );
     }
 
     public final void apneSaksoversiktLamell() {
         send(this, Broadcast.BUBBLE, new NamedEventPayload(WIDGET_HEADER_CLICKED, new WidgetHeaderPayload(this.getId())));
     }
+
     //Should be lambda. Denne kan ikke bli det.
     @SuppressWarnings("squid:S1604")
     private AjaxEventBehavior createHeaderClickBehavior() {
