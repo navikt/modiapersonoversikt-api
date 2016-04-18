c56951054d197ca593681962c003c1195085b369
Aleksander Hindenes <aleksander.hindenes@nav.no>
2016-04-18 08:29:27 +0200

    Noe kode gikk tapt i en merge

--- src/main/java/no/nav/sbl/dialogarena/saksoversikt/service/service/SakstemaService.java
+++ src/main/java/no/nav/sbl/dialogarena/saksoversikt/service/service/SakstemaService.java
@@ -85,30 +85,37 @@ public class SakstemaService {
         List<Sakstema> sakstema = temagruppe.getValue().stream()
                 .filter(ikkeGruppertOppfolingssak)
                 .map(temakode -> {
-                    List<Sak> tilhorendeSaker = alleSaker.stream()
-                            .filter(sak -> tilhorerSakTemagruppe(sak, temakode, temagruppe))
-                            .collect(toList());
-
-                    List<DokumentMetadata> tilhorendeDokumentMetadata = alleDokumentMetadata
-                            .stream()
-                            .filter(tilhorendeFraJoark(tilhorendeSaker).or(tilhorendeFraHenvendelse(temagruppe, temakode)))
-                            .collect(toList());
+                    List<Sak> tilhorendeSaker = sakerITemagruppe(temagruppe, alleSaker, temakode);
+                    List<DokumentMetadata> tilhorendeDokumentMetadata = tilhorendeDokumentMetadata(temagruppe, alleDokumentMetadata, temakode, tilhorendeSaker);
 
                     boolean erGruppert = !RESTERENDE_TEMA.equals(temagruppe.getKey());
 
-                    ResultatWrapper temanavn = temanavnFraKodeverk(temagruppe, temakode);
+                    ResultatWrapper<String> temanavn = temanavnFraKodeverk(temagruppe, temakode);
                     feilendeBaksystemer.addAll(temanavn.feilendeSystemer);
 
                     return new Sakstema()
                             .withTemakode(temakode)
                             .withBehandlingskjeder(optional(behandlingskjeder.get(temakode)).orElse(emptyList()))
                             .withTilhorendeSaker(tilhorendeSaker)
-                            .withTemanavn((String)temanavn.resultat)
+                            .withTemanavn(temanavn.resultat)
                             .withDokumentMetadata(tilhorendeDokumentMetadata)
                             .withErGruppert(erGruppert);
                 })
                 .collect(toList());
         return new ResultatWrapper<>(sakstema, feilendeBaksystemer);
+    }
+
+    private List<DokumentMetadata> tilhorendeDokumentMetadata(Map.Entry<String, Set<String>> temagruppe, List<DokumentMetadata> alleDokumentMetadata, String temakode, List<Sak> tilhorendeSaker) {
+        return alleDokumentMetadata
+                .stream()
+                .filter(tilhorendeFraJoark(tilhorendeSaker).or(tilhorendeFraHenvendelse(temagruppe, temakode)))
+                .collect(toList());
+    }
+
+    private List<Sak> sakerITemagruppe(Map.Entry<String, Set<String>> temagruppe, List<Sak> alleSaker, String temakode) {
+        return alleSaker.stream()
+                .filter(sak -> tilhorerSakTemagruppe(sak, temakode, temagruppe))
+                .collect(toList());
     }
 
     private ResultatWrapper<String> temanavnFraKodeverk(Map.Entry<String, Set<String>> temagruppe, String temakode) {
