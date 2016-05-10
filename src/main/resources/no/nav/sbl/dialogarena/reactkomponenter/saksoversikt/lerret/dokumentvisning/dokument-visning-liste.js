31a6c9edc1356ae6d25072702b13c44169a4a64a
Fredrik Stenbro <fredrik.stenbro@nav.no>
2016-05-10 08:56:13 +0200

    Setter eksplisitt at hoveddokument skal være forst i dokumentvisning

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/saksoversikt/lerret/dokumentvisning/dokument-visning-liste.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/saksoversikt/lerret/dokumentvisning/dokument-visning-liste.js
@@ -2,7 +2,12 @@ import React from 'react';
 import DokumentVisning from './dokument-visning';
 
 function DokumentVisningListe({ dokumenter }) {
-    const dokumentElementer = dokumenter.map((dokument) => (
+
+    const hoveddokument = dokumenter.find(dokument => dokument.erHoveddokument);
+    const andreDokumenter = dokumenter.filter(dokument => !dokument.erHoveddokument);
+    const sortertDokumentliste = [].concat(hoveddokument).concat(andreDokumenter);
+
+    const dokumentElementer = sortertDokumentliste.map((dokument) => (
         <DokumentVisning key={dokument.dokumentreferanse} dokument={dokument}/>
     ));
 
