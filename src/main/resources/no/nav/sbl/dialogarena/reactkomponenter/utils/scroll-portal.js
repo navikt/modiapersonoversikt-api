378adbdc8ff8e924a04a56afacfa058f4f5d89ad
Mats Krüger Svensson <mats.kruger.svensson@nav.no>
2016-08-04 13:09:37 +0200

    [sonar] fjernet utkommentert kode

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/utils/scroll-portal.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/utils/scroll-portal.js
@@ -2,29 +2,10 @@
 import React from 'react';
 import { omit } from './../utils/utils-module';
 
-// function oppdaterScrollbarEtterRenderingErFerdig(element) {
-//     setTimeout(function oppdaterScroll() {
-//         element.mCustomScrollbar('update');
-//     });
-// }
-
 const ScrollPortal = React.createClass({
     propTypes: {
         'innerClassName': React.PropTypes.string,
         'children': React.PropTypes.oneOfType([React.PropTypes.element, React.PropTypes.array])
-    },
-    componentDidMount: function componentDidMount() {
-        // this.renderList();
-    },
-    componentDidUpdate: function componentDidUpdate() {
-        // this.renderList();
-    },
-    renderList: function renderList() {
-        // const wrapper = $(this.refs.wrapper.getDOMNode());
-        // wrapper.mCustomScrollbar($.extend({}, Modig.scrollOptions, {scrollbarPosition: 'inside'}));
-        // const container = wrapper.find('.mCSB_container:first').get(0);
-        // React.render(<div className={this.props.innerClassName}>{this.props.children}</div>, container);
-        // oppdaterScrollbarEtterRenderingErFerdig(wrapper);
     },
     render: function render() {
         const props = omit(this.props, ['children', 'innerClassName']);
