1486ffc3d3bf846b3591e94ec164b6fc14ca6e93
Nicklas Utgaard <nicklas.utgaard@nav.no>
2016-09-16 14:54:49 +0200

    [PKULV-1721 PK-33222] Eslint

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/meldinger-sok/forhandsvisning.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/meldinger-sok/forhandsvisning.js
@@ -4,42 +4,40 @@ import DokumentMelding from './dokument-melding';
 import OppgaveMelding from './oppgave-melding';
 import ScrollPortal from './../utils/scroll-portal';
 
-class Forhandsvisning extends React.Component {
-    render() {
-        if (!this.props.traad.hasOwnProperty('meldinger')) {
-            return <noscript />;
+function Forhandsvisning() {
+    if (!this.props.traad.hasOwnProperty('meldinger')) {
+        return <noscript />;
+    }
+
+    const { traad, traad: { meldinger } } = this.props;
+
+    const meldingElementer = meldinger.map((melding) => {
+        if (melding.erDokumentMelding) {
+            return <DokumentMelding key={melding.id} melding={melding} />;
         }
+        if (melding.erOppgaveMelding) {
+            return <OppgaveMelding key={melding.id} melding={melding} />;
+        }
+        return <Melding key={melding.id} melding={melding} />;
+    });
 
-        const { traad, traad: { meldinger } } = this.props;
-
-        const meldingElementer = meldinger.map((melding) => {
-            if (melding.erDokumentMelding) {
-                return <DokumentMelding key={melding.id} melding={melding} />;
-            }
-            if (melding.erOppgaveMelding) {
-                return <OppgaveMelding key={melding.id} melding={melding} />
-            }
-            return <Melding key={melding.id} melding={melding} />;
-        });
-
-        const meldingBenevnelse = traad.antallMeldingerIOpprinneligTraad === 1 ? 'melding' : 'meldinger';
-        const antallInformasjon = `Viser <b>${meldinger.length}</b> av 
+    const meldingBenevnelse = traad.antallMeldingerIOpprinneligTraad === 1 ? 'melding' : 'meldinger';
+    const antallInformasjon = `Viser <b>${meldinger.length}</b> av 
                 <b>${traad.antallMeldingerIOpprinneligTraad}</b> ${meldingBenevnelse} i dialogen`;
 
-        return (
-            <div>
-                <ScrollPortal className="traadPanel" innerClassName="traad-panel-wrapper">
-                    <div className="traadinfo">
-                        <span dangerouslySetInnerHTML={{ __html: antallInformasjon }}></span>
-                    </div>
-                    <div>{meldingElementer}</div>
-                </ScrollPortal>
-                <div className="velgPanel">
-                    <input type="submit" value="Vis dialog" className="knapp-hoved-liten" />
+    return (
+        <div>
+            <ScrollPortal className="traadPanel" innerClassName="traad-panel-wrapper">
+                <div className="traadinfo">
+                    <span dangerouslySetInnerHTML={{ __html: antallInformasjon }}></span>
                 </div>
+                <div>{meldingElementer}</div>
+            </ScrollPortal>
+            <div className="velgPanel">
+                <input type="submit" value="Vis dialog" className="knapp-hoved-liten" />
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 Forhandsvisning.propTypes = {
