edcb556bc105d4206e59296aa8deac8d0b06c6da
Skjefstad, Vegard <vegard.skjefstad@nav.no>
2016-11-01 14:40:59 +0100

    En del tilsynelatende unødvendig bruk av with-metoder i Melding-klassen for å øke linjedekningen der...

--- src/test/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/api/domain/henvendelse/TraadTest.java
+++ src/test/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/api/domain/henvendelse/TraadTest.java
@@ -1,5 +1,6 @@
 package no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain.henvendelse;
 
+import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain.Temagruppe;
 import org.joda.time.DateTime;
 import org.junit.Test;
 
@@ -12,14 +13,40 @@ public class TraadTest {
 
     @Test
     public void traaderSkalSorteresMedNyesteMeldingForst() throws Exception {
-        final Traad traad = new Traad("traadId", 2,
+        final String traadId = "traadId";
+        final Traad traad = new Traad(traadId, 2,
                 Arrays.asList(
-                        new Melding().withId("melding_1").withOpprettetDato(new DateTime(1)),
-                        new Melding().withId("melding_2").withOpprettetDato(new DateTime(2)),
-                        new Melding().withId("melding_3").withOpprettetDato(new DateTime(3))
+                        lagMelding(traadId, "melding_1", 1),
+                        lagMelding(traadId, "melding_2", 2),
+                        lagMelding(traadId, "melding_3", 3)
                 ));
 
         assertThat(traad.meldinger.get(0).id, is("melding_3"));
+    }
+
+    private Melding lagMelding(final String traadId, final String id, final long dato) {
+        return new Melding()
+                .withId(id)
+                .withOpprettetDato(new DateTime(dato))
+                .withBrukersEnhet("brukersEnhet")
+                .withEksternAktor("eksternAktor")
+                .withErTilknyttetAnsatt(false)
+                .withFnr("10108000398")
+                .withFritekst("fritekst")
+                .withGjeldendeTemagruppe(Temagruppe.ANSOS)
+                .withJournalfortAvNavIdent("Z990335")
+                .withJournalfortDato(new DateTime(dato))
+                .withJournalfortTema("Dagpenger")
+                .withJournalfortTemaNavn("DAG")
+                .withTraadId(traadId)
+                .withOppgaveId("oppgaveId")
+                .withType(Meldingstype.OPPGAVE_VARSEL)
+                .withNavIdent("Z990335")
+                .withTemagruppe("temaGruppe")
+                .withKanal("kanal")
+                .withKontorsperretEnhet("2820")
+                .withJournalfortSaksId("journalfortSaksId")
+                .withTilknyttetEnhet("0216");
     }
 
 }
