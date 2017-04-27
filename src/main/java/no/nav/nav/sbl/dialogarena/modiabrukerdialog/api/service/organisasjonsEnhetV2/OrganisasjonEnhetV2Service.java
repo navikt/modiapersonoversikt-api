be39e86e62e26cac8f6e2481b4e952e803e5edb8
Skjefstad, Vegard <vegard.skjefstad@nav.no>
2017-04-27 14:24:53 +0200

    PK-39688, PKULV-2501: Bruker native Java 8 Optional.

--- src/main/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/api/service/organisasjonsEnhetV2/OrganisasjonEnhetV2Service.java
+++ src/main/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/api/service/organisasjonsEnhetV2/OrganisasjonEnhetV2Service.java
@@ -1,9 +1,9 @@
 package no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.service.organisasjonsEnhetV2;
 
-import no.nav.modig.lang.option.Optional;
 import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain.norg.AnsattEnhet;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OrganisasjonEnhetV2Service {
     List<AnsattEnhet> hentAlleEnheter();
