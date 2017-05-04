4604d73d16ca181589ac248df27d52aa1472fb37
Skjefstad, Vegard <vegard.skjefstad@nav.no>
2017-05-04 08:38:59 +0200

    PK-39688, PKULV-2501, PKULV-2533: Bruk av integrasjon mot OrganisasjonEnhetV2.finnNAVKontor.
    Må gjøres endringer når TPS-tjenesten som returnerer geografisk tilknytning er klar. Foreløpig benyttes bare TKNR fra den gamle TPS-tjenesten, så svaret fra finnNAVKontor blir ikke nødvendigvis korrekt.

--- consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/artifact/kjerneinfo/component/mockable/wrapper/PersonKjerneinfoWrapper.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/artifact/kjerneinfo/component/mockable/wrapper/PersonKjerneinfoWrapper.java
@@ -4,7 +4,7 @@ import no.nav.kjerneinfo.consumer.fim.mapping.KjerneinfoMapper;
 import no.nav.kjerneinfo.consumer.fim.person.PersonKjerneinfoServiceBi;
 import no.nav.kjerneinfo.consumer.fim.person.config.PersonKjerneinfoConsumerConfig;
 import no.nav.modig.security.tilgangskontroll.policy.pep.EnforcementPoint;
-import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.service.norg2.OrganisasjonEnhetService;
+import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.service.organisasjonsEnhetV2.OrganisasjonEnhetV2Service;
 import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.service.saksbehandler.SaksbehandlerInnstillingerService;
 import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.artifact.kjerneinfo.component.mockable.KjerneinfoMapperConfigResolver;
 import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.artifact.kjerneinfo.component.mockable.mockableimpl.PersonKjerneinfoConsumerConfigImpl;
@@ -33,17 +33,13 @@ public class PersonKjerneinfoWrapper {
     private PersonV2 personPortType;
 
     @Inject
-    @Named("hentSelftestPersonKjerneinfoJaxWsPortProxyFactoryBean")
-    private PersonV2 selfTestPersonPortType;
-
-    @Inject
     private KjerneinfoMapper kjerneinfoMapperBean;
 
     @Resource(name = "pep")
     private EnforcementPoint kjerneinfoPep;
 
     @Inject
-    private OrganisasjonEnhetService organisasjonEnhetService;
+    private OrganisasjonEnhetV2Service organisasjonEnhetV2Service;
 
     @Inject
     private SaksbehandlerInnstillingerService saksbehandlerInnstillingerService;
@@ -51,7 +47,7 @@ public class PersonKjerneinfoWrapper {
     @Bean
     @Qualifier("personKjerneinfoServiceDefault")
     public Wrapper<PersonKjerneinfoServiceBi> personKjerneinfoServiceDefault() {
-        return new Wrapper<>(new PersonKjerneinfoConsumerConfigImpl(personPortType, selfTestPersonPortType, kjerneinfoMapperBean, kjerneinfoPep, organisasjonEnhetService, saksbehandlerInnstillingerService).personKjerneinfoServiceBi());
+        return new Wrapper<>(new PersonKjerneinfoConsumerConfigImpl(personPortType, kjerneinfoMapperBean, kjerneinfoPep, organisasjonEnhetV2Service, saksbehandlerInnstillingerService).personKjerneinfoServiceBi());
     }
 
     @Bean
