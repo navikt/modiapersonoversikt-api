3890bb4d788d2b5563887b5935334205d563b858
Skjefstad, Vegard <vegard.skjefstad@nav.no>
2017-05-04 10:56:06 +0200

    PK-39688, PKULV-2501, PKULV-2533: Fjerner kode som ikke lenger er i bruk.
    Tilhørte OrganisasjonEnhetV1.

--- src/main/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/api/service/norg2/OrganisasjonEnhetService.java
+++ src/main/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/api/service/norg2/OrganisasjonEnhetService.java
@@ -1,13 +1,9 @@
 package no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.service.norg2;
 
-import no.nav.modig.lang.option.Optional;
-import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain.norg.AnsattEnhet;
 import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain.norg.Arbeidsfordeling;
 
 import java.util.List;
 
 public interface OrganisasjonEnhetService {
-    Optional<AnsattEnhet> hentEnhetGittGeografiskNedslagsfelt(final String geografiskNedslagsfelt);
-    
     List<Arbeidsfordeling> hentArbeidsfordeling(final String enhetId);
 }
