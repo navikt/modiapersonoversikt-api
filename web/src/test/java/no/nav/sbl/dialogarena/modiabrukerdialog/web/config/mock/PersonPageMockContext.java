77060d824d4e77876cfec4c031f66f59c1dd8603
Joakim Lindquister <joakim.lindquister@nav.no>
2017-05-09 12:09:46 +0200

    PK-39664 Brukerprofiltjenesten er erstattet av personV3

--- web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/mock/PersonPageMockContext.java
+++ web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/mock/PersonPageMockContext.java
@@ -1,19 +1,21 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.web.config.mock;
 
 import _0._0.nav_cons_sak_gosys_3.no.nav.inf.navorgenhet.GOSYSNAVOrgEnhet;
-import no.nav.brukerprofil.consumer.BrukerprofilServiceBi;
 import no.nav.kjerneinfo.consumer.fim.person.PersonKjerneinfoServiceBi;
+import no.nav.kjerneinfo.consumer.fim.person.to.HentKjerneinformasjonRequest;
+import no.nav.kjerneinfo.consumer.fim.person.to.HentKjerneinformasjonResponse;
+import no.nav.kjerneinfo.domain.person.Person;
+import no.nav.kjerneinfo.domain.person.Personfakta;
 import no.nav.modig.content.CmsContentRetriever;
 import no.nav.modig.security.tilgangskontroll.policy.pep.EnforcementPoint;
 import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain.norg.AnsattEnhet;
 import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.service.norg2.OrganisasjonEnhetService;
 import no.nav.personsok.consumer.fim.personsok.PersonsokServiceBi;
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.service.PlukkOppgaveService;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.*;
 
 import static no.nav.modig.lang.option.Optional.optional;
+import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -49,17 +51,23 @@ public class PersonPageMockContext {
 
     @Bean
     public PersonKjerneinfoServiceBi personKjerneinfoServiceBi() {
-        return mock(PersonKjerneinfoServiceBi.class);
+        PersonKjerneinfoServiceBi personKjerneinfoServiceBi = mock(PersonKjerneinfoServiceBi.class);
+        when(personKjerneinfoServiceBi.hentKjerneinformasjon(any(HentKjerneinformasjonRequest.class))).thenReturn(lagMockKjerneinfoResponse());
+        return personKjerneinfoServiceBi;
+    }
+
+    private HentKjerneinformasjonResponse lagMockKjerneinfoResponse() {
+        Personfakta personFakta = new Personfakta();
+        Person person = new Person();
+        person.setPersonfakta(personFakta);
+        HentKjerneinformasjonResponse hentKjerneinformasjonResponse = new HentKjerneinformasjonResponse();
+        hentKjerneinformasjonResponse.setPerson(person);
+        return hentKjerneinformasjonResponse;
     }
 
     @Bean
     public PersonsokServiceBi personsokServiceBi() {
         return mock(PersonsokServiceBi.class);
-    }
-
-    @Bean
-    public BrukerprofilServiceBi brukerprofilServiceBi() {
-        return mock(BrukerprofilServiceBi.class);
     }
 
     @Bean
