0ab9a8fb84c9ca17583a29bdc12a210294fcb5ad
Joakim Lindquister <joakim.lindquister@nav.no>
2017-08-30 13:56:50 +0200

    PK-39820 Mer innhold for utbetalinger

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/pleiepenger/pleiepenger-utbetalinger-panel-module.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/pleiepenger/pleiepenger-utbetalinger-panel-module.js
@@ -1,64 +1,65 @@
 import React from 'react';
 import moment from 'moment';
 
-const javaLocalDateTimeToJSDate = (dato) =>
-    new Date(dato.year, dato.monthValue - 1, dato.dayOfMonth);
+const formaterJavaDate = (dato) =>
+    moment(new Date(dato.year, dato.monthValue - 1, dato.dayOfMonth)).format('DD.MM.YYYY');
 
-class PleiepengerUtbetalingerPanel extends React.Component {
-    constructor(props) {
-        super(props);
-        console.log(props);
-        this.state = {
-            saker: []
-        };
+const Periode = ({periode, periodeNummer}) => {
+    const fraOgMed = formaterJavaDate(periode.fraOgMed);
+    const vedtak = periode.vedtakListe.map((vedtak, index) => (<Vedtak key={index} vedtak={vedtak}/>));
 
-    }
-
-    render() {
-
-        const lagVedtak = (vedtak, index) => {
-            const fraOgMed = moment(javaLocalDateTimeToJSDate(vedtak.periode.fraOgMed)).format('DD.MM.YYYY');
-            const tilOgMed = moment(javaLocalDateTimeToJSDate(vedtak.periode.tilOgMed)).format('DD.MM.YYYY');
-
-            return (
-                <li key={index}>
-                    <dl>
-                        <dt>Fra og med</dt>
-                        <dd>{fraOgMed}</dd>
-                        <dt>Til og Med</dt>
-                        <dd>{tilOgMed}</dd>
-                        <dt>Brutto beløp</dt>
-                        <dd>6 777 kr</dd>
-                        <dt>Anvist utbetaling</dt>
-                        <dd>17.08.2017</dd>
-                    </dl>
-                </li>)
-        };
-
-        const lagPerioder = (periode, index) => {
-            const fraOgMed = moment(javaLocalDateTimeToJSDate(periode.fraOgMed)).format('DD.MM.YYYY');
-            const vedtak = periode.vedtakListe.map(lagVedtak);
-            return (
-                <section key={index}>
-                    <h1>Periode {index + 1} {fraOgMed} </h1>
-                    <article className="kommende-utbetalinger liste">
-                        <h1 aria-label="Ekspanderingsliste">Kommende utbetalinger</h1>
-                        <ul>
-                            {vedtak}
-                        </ul>
-                    </article>
-                </section>
-            )
-        };
-
-        const perioder = this.props.perioder.map(lagPerioder);
-        return (
-            <div>
-                {perioder}
+    return (
+        <section className="periode">
+            <h2>Periode {periodeNummer} {fraOgMed} </h2>
+            <div className="periodeinfo">
+                <dl>
+                    <dt>Pleiepengergrad</dt>
+                    <dd>50 %</dd>
+                    <dt>Pleiepengerdager</dt>
+                    <dd>17</dd>
+                </dl>
             </div>
-        );
-    }
-}
+            <article className="utbetalinger">
+                <h3 className="utbetalinger-header" aria-label="Ekspanderingsliste">Kommende utbetalinger</h3>
+                <ul className="vedtaksliste">
+                    {vedtak}
+                </ul>
+            </article>
+        </section>
+    )
+};
+
+const Vedtak = ({vedtak}) => {
+    const fraOgMed = formaterJavaDate(vedtak.periode.fraOgMed);
+    const tilOgMed = formaterJavaDate(vedtak.periode.tilOgMed);
+    const anvistUtbetaling = formaterJavaDate(vedtak.anvistUtbetaling);
+
+    return (
+        <li>
+            <dl>
+                <dt>Fra og med</dt>
+                <dd>{fraOgMed}</dd>
+                <dt>Til og Med</dt>
+                <dd>{tilOgMed}</dd>
+                <dt>Brutto beløp</dt>
+                <dd>{vedtak.bruttoBelop}</dd>
+                <dt>Anvist utbetaling</dt>
+                <dd>{anvistUtbetaling}</dd>
+            </dl>
+        </li>
+    );
+};
+
+const PleiepengerUtbetalingerPanel = ({perioder}) => {
+    const perioderKomponenter = perioder.map((periode, index) =>
+        (<Periode key={index} periode={periode} periodeNummer={index+1}/>));
+
+    return (
+        <div>
+            {perioderKomponenter}
+        </div>
+    );
+};
 
 PleiepengerUtbetalingerPanel.propTypes = {
 };
