973a300041a00be742ad903ef641905efa63c71c
Joakim Lindquister <joakim.lindquister@nav.no>
2017-09-06 15:03:15 +0200

    PK-39820 Fiks feil ved rendring av react-component
    ReactKomponenter i Modia klarte ikke å dra inn en stateless komponent.
    Måtte derfor skrive om til React.Component

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/pleiepenger/pleiepenger-panel.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/pleiepenger/pleiepenger-panel.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import PleiepengerRettenPanel from './components/pleiepenger-retten';
 import ArbeidsituasjonPanel from './components/arbeidssituasjon';
 import UtbetalingerPanel from './components/utbetalinger-';
 
-const PleiepengerPanel = props => {
-    return (
-        <div className="pleiepenger-panel">
-            <PleiepengerRettenPanel tekst={props.tekst.pleiepengerRetten} graderingsgrad={props.graderingsgrad} pleiepengedager={props.pleiepengedager} forbrukteDagerTOMIDag={props.forbrukteDagerTOMIDag} forbrukteDagerEtterDennePerioden={props.forbrukteDagerEtterDennePerioden} barnet={props.barnet} fomDato={props.FOMDato} tomDato={props.TOMDato}/>
-            <ArbeidsituasjonPanel tekst={props.tekst.arbeidsforhold} arbeidsforhold={props.arbeidsforhold}/>
-            <UtbetalingerPanel tekst={props.tekst.utbetalinger} perioder={props.perioder}/>
-        </div>
-    );
-};
+class PleiepengerPanel extends Component {
+
+    render() {
+        const props = this.props;
+
+        return (
+            <div className="pleiepenger-panel">
+                <PleiepengerRettenPanel tekst={props.tekst.pleiepengerRetten} graderingsgrad={props.graderingsgrad} pleiepengedager={props.pleiepengedager} forbrukteDagerTOMIDag={props.forbrukteDagerTOMIDag} forbrukteDagerEtterDennePerioden={props.forbrukteDagerEtterDennePerioden} barnet={props.barnet} fomDato={props.FOMDato} tomDato={props.TOMDato}/>
+                <ArbeidsituasjonPanel tekst={props.tekst.arbeidsforhold} arbeidsforhold={props.arbeidsforhold}/>
+                <UtbetalingerPanel tekst={props.tekst.utbetalinger} perioder={props.perioder}/>
+            </div>
+        );
+    };
+}
 
 PleiepengerPanel.propTypes = {
     tekst: React.PropTypes.object.isRequired,
