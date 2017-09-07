3762264d5f0b38f2ebbe48ecc92252af5d42918d
Oscar Eckbo <oscar.eckbo@nav.no>
2017-09-07 13:34:55 +0200

    [OPP-25] legger til aria-attributter på alertstripe

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/alertstriper/alertstripe-module.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/alertstriper/alertstripe-module.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import Icon from 'nav-frontend-ikoner-assets';
 
 class AlertstripeSuksessSolid extends Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         const { tekst } = this.props;
         return (
-            <div className="alertstripe alertstripe--suksess alertstripe--solid blokk-s">
+            <div
+                className="alertstripe alertstripe--suksess alertstripe--solid blokk-s"
+                aria-live="assertive"
+                aria-atomic="true"
+                role="alert"
+            >
                 <Icon className="ikon" kind="ok-sirkel-fylt" />
                 <span className="alertstripe--tekst">
                     {tekst}
