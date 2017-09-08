b93207be5f75f9355dcd0057d9df4be25974a272
Oscar Eckbo <oscar.eckbo@nav.no>
2017-09-08 15:31:38 +0200

    [OPP-25] Henter tekst fra state for å støtte state-oppdatering fra wicket

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/alertstriper/alertstripe-module.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/alertstriper/alertstripe-module.js
@@ -1,9 +1,14 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Icon from 'nav-frontend-ikoner-assets';
 
 class AlertstripeSuksessSolid extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { tekst: props.tekst };
+    }
+
     render() {
-        const { tekst } = this.props;
+        const { tekst } = this.state;
         return (
             <div
                 className="alertstripe alertstripe--suksess alertstripe--solid blokk-s"
