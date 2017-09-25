4c105b59b242151cd2c03ed42132482e0efd7d32
Sneha Desai <sneha.desai@nav.no>
2017-09-25 10:47:13 +0200

    OPP-46 Modia: Automatisk navigering til siste skjermbilde på person ved teknisk feil

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/feilmeldingsmodaler/teknisk-feil.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/feilmeldingsmodaler/teknisk-feil.js
@@ -1,22 +1,38 @@
 import React, { PropTypes as pt } from 'react';
 import Modal, { AriaPropType, defaultHelper } from './../modal/modal-module';
+import { autobind } from './../utils/utils-module';
+import sendToWicket from './../react-wicket-mixin/wicket-sender';
 
 const styling = {
     text: {
         marginTop: 30 + 'px',
         textTransform: 'none !important'
+    },
+    okKnapp: {
+       marginLeft: 18 + 'rem',
     }
 };
 
 class TekniskFeil extends React.Component {
+
+    vis() {
+        this.refs.modal.open();
+    }
+
+    skjul() {
+        this.refs.modal.close();
+    }
+
     render() {
-        const { isOpen, title, closeButton, tekst } = this.props;
-        const modalProps = { isOpen, title, closeButton };
+        const { isOpen, title, closeButton, tekst, fnr} = this.props;
+        console.log('rn', fnr);
+        const modalProps = { isOpen, title, closeButton, fnr };
         return (
             <Modal {...modalProps} width={600} height={180} onClosing={() => (false)} ref="modal">
                 <section className="default-error">
                     <h1 className="robust-ikon-feil-strek" style={styling.text}>{tekst}</h1>
                 </section>
+                <a className="knapp-stor" style={styling.okKnapp} href={'/modiabrukerdialog/person/'+fnr}> Ok </a>
             </Modal>
         );
     }
@@ -34,7 +50,8 @@ TekniskFeil.propTypes = {
     title: AriaPropType,
     description: AriaPropType,
     closeButton: AriaPropType,
-    isOpen: pt.bool
+    isOpen: pt.bool,
+    fnr: pt.string.isRequired
 };
 
 export default TekniskFeil;
