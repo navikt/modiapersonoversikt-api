83f8988353ab1c8df772d2b11f3f02aeeff0cacb
Sneha Desai <sneha.desai@nav.no>
2017-09-25 10:55:30 +0200

    OPP-46 Modia: Automatisk navigering til siste skjermbilde på person ved teknisk feil

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/feilmeldingsmodaler/teknisk-feil.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/feilmeldingsmodaler/teknisk-feil.js
@@ -5,18 +5,31 @@ const styling = {
     text: {
         marginTop: 30 + 'px',
         textTransform: 'none !important'
+    },
+    okKnapp: {
+       marginLeft: 18 + 'rem',
     }
 };
 
 class TekniskFeil extends React.Component {
+
+    vis() {
+        this.refs.modal.open();
+    }
+
+    skjul() {
+        this.refs.modal.close();
+    }
+
     render() {
-        const { isOpen, title, closeButton, tekst } = this.props;
-        const modalProps = { isOpen, title, closeButton };
+        const { isOpen, title, closeButton, tekst, fnr} = this.props;
+        const modalProps = { isOpen, title, closeButton, fnr };
         return (
-            <Modal {...modalProps} width={600} height={180} onClosing={() => (true)} ref="modal">
+            <Modal {...modalProps} width={600} height={180} onClosing={() => (false)} ref="modal">
                 <section className="default-error">
                     <h1 className="robust-ikon-feil-strek" style={styling.text}>{tekst}</h1>
                 </section>
+                <a className="knapp-stor" style={styling.okKnapp} href={'/modiabrukerdialog/person/'+fnr}> Ok </a>
             </Modal>
         );
     }
@@ -26,7 +39,7 @@ TekniskFeil.defaultProps = {
     isOpen: false,
     title: defaultHelper('Det skjedde en teknisk feil', false, 'h1.vekk'),
     description: defaultHelper('', false, 'div.vekk'),
-    closeButton: defaultHelper('', true, 'span')
+    closeButton: defaultHelper('', false, 'span')
 };
 
 TekniskFeil.propTypes = {
@@ -34,7 +47,8 @@ TekniskFeil.propTypes = {
     title: AriaPropType,
     description: AriaPropType,
     closeButton: AriaPropType,
-    isOpen: pt.bool
+    isOpen: pt.bool,
+    fnr: pt.string.isRequired
 };
 
 export default TekniskFeil;
