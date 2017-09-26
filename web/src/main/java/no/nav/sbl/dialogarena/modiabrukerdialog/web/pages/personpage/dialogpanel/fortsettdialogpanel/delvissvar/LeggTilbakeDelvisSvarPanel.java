5b8f9e121ecb85156fe03a446f41a3602dfb01ba
Joakim Lindquister <joakim.lindquister@nav.no>
2017-09-26 15:23:49 +0200

    OPP-50 Flytt ut props-laging i egen klasse for bedre testbarhet

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/pages/personpage/dialogpanel/fortsettdialogpanel/delvissvar/LeggTilbakeDelvisSvarPanel.java
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/pages/personpage/dialogpanel/fortsettdialogpanel/delvissvar/LeggTilbakeDelvisSvarPanel.java
@@ -6,39 +6,29 @@ import no.nav.sbl.dialogarena.reactkomponenter.utils.wicket.ReactComponentPanel;
 import org.apache.wicket.Component;
 import org.apache.wicket.markup.html.panel.Panel;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class LeggTilbakeDelvisSvarPanel extends Panel {
 
     public static final String WICKET_REACT_PANEL_ID = "reactleggtilbakedelvissvarpanel";
     public static final String WICKET_REACT_WRAPPER_ID = "leggtilbakedelvissvarpanel";
     public static final String REACT_ID = "LeggTilbakeDelvisSvarPanel";
 
-    private final String behandlingsId;
+    private LeggTilbakeDelvisSvarProps leggTilbakeDelvisSvarProps;
 
-    public LeggTilbakeDelvisSvarPanel(String behandlingsId) {
+    public LeggTilbakeDelvisSvarPanel(Melding sporsmal, String behandlingsId) {
         super(WICKET_REACT_WRAPPER_ID);
         setOutputMarkupPlaceholderTag(true);
-
-        this.behandlingsId = behandlingsId;
+        leggTilbakeDelvisSvarProps = new LeggTilbakeDelvisSvarProps(sporsmal, behandlingsId);
 
         add(lagReactPanel());
     }
 
     private Component lagReactPanel() {
-        ReactComponentPanel reactComponentPanel = new ReactComponentPanel(WICKET_REACT_PANEL_ID, REACT_ID, lagProps());
+        ReactComponentPanel reactComponentPanel = new ReactComponentPanel(WICKET_REACT_PANEL_ID, REACT_ID, leggTilbakeDelvisSvarProps.lagProps());
         reactComponentPanel
                 .setOutputMarkupId(true)
                 .setVisibilityAllowed(true);
 
         return reactComponentPanel;
-    }
-
-    private Map<String, Object> lagProps() {
-        Map<String, Object> props = new HashMap<>();
-        props.put("henvendelseId", behandlingsId);
-        return props;
     }
 
 }
---
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/pages/personpage/dialogpanel/fortsettdialogpanel/delvissvar/LeggTilbakeDelvisSvarProps.java
@@ -0,0 +1,28 @@
+package no.nav.sbl.dialogarena.modiabrukerdialog.web.pages.personpage.dialogpanel.fortsettdialogpanel.delvissvar;
+
+import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain.henvendelse.Melding;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+class LeggTilbakeDelvisSvarProps implements Serializable {
+
+    private final String behandlingsId;
+    private final Melding sporsmal;
+
+    LeggTilbakeDelvisSvarProps(Melding sporsmal, String behandlingsId) {
+        this.behandlingsId = behandlingsId;
+        this.sporsmal = sporsmal;
+    }
+
+    Map<String, Object> lagProps() {
+        Map<String, Object> props = new HashMap<>();
+        props.put("henvendelseId", behandlingsId);
+        props.put("sporsmal", sporsmal.fritekst);
+        props.put("traadId", sporsmal.traadId);
+        props.put("fodselsnummer", sporsmal.fnrBruker);
+        return props;
+    }
+
+}
