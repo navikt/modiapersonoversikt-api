9d9229401d2e2104875c10fbc490bfe3c736147d
Joakim Lindquister <joakim.lindquister@nav.no>
2017-09-27 14:36:32 +0200

    OPP-50 Splitt ut i to filer og bruk wicket-callback

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/dialog-panel/leggtilbakedelvissvar/legg-tilbake-delvis-svar-module.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/dialog-panel/leggtilbakedelvissvar/legg-tilbake-delvis-svar-module.js
@@ -1,88 +1,44 @@
 import React, { Component } from 'react';
-import Ajax from '../../utils/ajax';
-
+import wicketSender from '../../react-wicket-mixin/wicket-sender';
+import DelvisSvar from './delvis-svar';
 
 class LeggTilbakeDelvisSvarPanel extends Component {
     constructor(props) {
         super(props);
-        this.svarDelvis = this.svarDelvis.bind(this);
-        this.handleSvarEndring = this.handleSvarEndring.bind(this);
         console.log(props);
+        this.svarCallback = this.svarCallback.bind(this);
+
         this.state = {
-            svarValue: ''
+            erUnderArbeid: true
         };
     }
-
-    svarDelvis() {
-        console.log(this.state.svarValue);
-        const url = `/modiabrukerdialog/rest/personer/${this.props.fodselsnummer}/traader/${this.props.traadId}/henvendelser/${this.props.henvendelseId}/ferdigstill`;
-        const data = JSON.stringify({ svar: this.state.svarValue });
-        const svarDelvisPromise = Ajax.put(url, data);
-        svarDelvisPromise.done(() => {
-            console.log('Well done!');
-        });
-
-        svarDelvisPromise.fail(err => {
-            console.err(err);
-        });
+    svarCallback() {
+        console.log("Svar Callback!");
+        this.setState({ erUnderArbeid: false });
+        wicketSender(this.props.wicketurl, this.props.wicketcomponent, this.props.svarDelvisCallbackId);
     }
-
-    handleSvarEndring(event) {
-        this.setState({ svarValue: event.target.value });
-    }
-
     render() {
-        const sporsmal = this.props.sporsmal.split('\n').map((paragraf, index) =>
-            <p key={`paragraf-${index}`}>{paragraf}</p>);
-
-
-        if
-
-        return (
-            <div>
-                <h3>Legg tilbake med delvis svar</h3>
-                <h2>Spørsmål</h2>
-                <h3>FAMILIE</h3>
-                <p>22.03.2017 kl 10:22</p>
-                <div className="sporsmaal">
-                    {sporsmal}
-                </div>
-                <div className="svar">
-                    <div className="svar-overskrift-boks">
-                        <h1 className="overskrift medium"><span>Delvis svar</span></h1>
-                    </div>
-                    <textarea
-                        value={this.state.svarValue}
-                        onChange={this.handleSvarEndring}
-                        className="svar-tekst"
-                        placeholder="Svaret blir ikke synlig for brukeren">
-                    </textarea>
-                </div>
-
-                <div className="temagruppe-velger">
-                    <h3>Velg temagruppe</h3>
-                    <select>
-                        <option>Arbeid</option>
-                        <option>Familie</option>
-                        <option>Hjelpemidler</option>
-                    </select>
-                </div>
-                <a
-                    className="knapp-hoved-stor submit"
-                    role="button"
-                    onClick={this.svarDelvis}
-                >
-                    Svar delvis og legg tilbake
-                </a>
-                <a>Avbryt</a>
-            </div>
-        );
+        if (this.state.erUnderArbeid) {
+            return (<DelvisSvar
+                henvendelseId={this.props.henvendelseId}
+                sporsmal={this.props.sporsmal}
+                fodselsnummer={this.props.fodselsnummer}
+                traadId={this.props.traadId}
+                svarCallback={this.svarCallback}
+            />);
+        }
+        return <h1>Delvis svar er registrert</h1>;
     }
 }
 
 LeggTilbakeDelvisSvarPanel.propTypes = {
+    wicketurl: React.PropTypes.string.isRequired,
+    wicketcomponent: React.PropTypes.string.isRequired,
+    svarDelvisCallbackId: React.PropTypes.string.isRequired,
     henvendelseId: React.PropTypes.string.isRequired,
-    sporsmal: React.PropTypes.string.isRequired
+    sporsmal: React.PropTypes.string.isRequired,
+    fodselsnummer: React.PropTypes.string.isRequired,
+    traadId: React.PropTypes.string.isRequired,
 };
 
 export default LeggTilbakeDelvisSvarPanel;
