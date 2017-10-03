780e00386b40756ef6cc913336405c19359d3761
Eigil Skjæveland <eigil.skjeveland@nav.no>
2017-10-03 15:05:11 +0200

    OPP-44 Fiks statustekst

--- src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/common/components/StatusIkon.java
+++ src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/common/components/StatusIkon.java
@@ -31,15 +31,17 @@ public class StatusIkon extends Panel {
 
         int traadlengde = meldingVM.traadlengde;
         Label antallMeldingerTekst = new Label("antallMeldingerIkonTekst", lagAntallMeldingerTekst(traadlengde));
-        Label statusIkonTekst = new Label("statusIkonTekst", lagStatustekst(meldingVM, underBehandling, traadlengde));
+        Label statusIkonTekst = new Label("statusIkonTekst", lagStatustekst(meldingVM, underBehandling,
+                traadlengde, erMonolog));
 
         addStyles(meldingVM, erMonolog);
 
         add(ikon, antallMeldingerTekst, statusIkonTekst);
     }
 
-    private String lagStatustekst(MeldingVM meldingVM, boolean underBehandling, int traadlengde) {
-        String besvartStatusKey = format("innboks.melding.%s", meldingVM.erBesvart().getObject() ? "besvart" : "ubesvart");
+    private String lagStatustekst(MeldingVM meldingVM, boolean underBehandling, int traadlengde, boolean erMonolog) {
+        boolean erBesvart = meldingVM.erBesvart().getObject() && !(meldingVM.erFraSaksbehandler() && erMonolog);
+        String besvartStatusKey = format("innboks.melding.%s", erBesvart ? "besvart" : "ubesvart");
         String besvartStatus = new StringResourceModel(besvartStatusKey, this, null).getString();
         String antallMeldinger = format("%d %s", traadlengde, traadlengde == 1 ? "melding" : "meldinger");
         return format("%s%s, %s, ", underBehandling ? "Under behandling, " : "", besvartStatus, antallMeldinger);
