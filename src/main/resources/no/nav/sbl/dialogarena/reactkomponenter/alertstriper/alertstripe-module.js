e5de4e89d33becdaf00229c90933ef5062cfcb3d
Joakim Lindquister <joakim.lindquister@nav.no>
2017-10-17 15:34:21 +0200

    Fiks lintefeil

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/alertstriper/alertstripe-module.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/alertstriper/alertstripe-module.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Icon from 'nav-frontend-ikoner-assets';
 
 class AlertstripeSuksessSolid extends Component {
