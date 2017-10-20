c34b3959f4e1d7de9b92d5e0d95d3b289aaabe7c
Daniel Winsvold <daniel.winsvold@nav.no>
2017-10-20 14:58:47 +0200

    OPP-153 Escaper apostrof i navn for bruk i js. Endrer ogsa css.
    Endrett taktikk etter forslag fra Jokke.
    
    Css-fiks: knapp "Del med [fornavn]" inherited unødvendig stor padding som skjulte lange navn.

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/pages/personpage/dialogpanel/GrunnInfo.java
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/pages/personpage/dialogpanel/GrunnInfo.java
@@ -1,7 +1,6 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.web.pages.personpage.dialogpanel;
 
 import no.nav.kjerneinfo.domain.person.Personnavn;
-import org.apache.commons.lang3.StringEscapeUtils;
 
 import java.io.Serializable;
 
