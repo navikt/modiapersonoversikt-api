d5833a9a0d6195c2e76546dc84afb1080618d999
Joakim Lindquister <joakim.lindquister@nav.no>
2017-11-01 14:12:59 +0100

    OPP-50 fjern property før test så vi er sikker på at den ikke finnes

--- consumer/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/FeatureToggleTest.java
+++ consumer/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/FeatureToggleTest.java
@@ -4,20 +4,22 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static java.lang.System.setProperty;
+import static no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.FeatureToggle.VIS_DELVISE_SVAR_PROPERTY_KEY;
 import static org.junit.Assert.assertEquals;
 
 class FeatureToggleTest {
-
+    
     @Test
     @DisplayName("Hvis property ikke er satt, skal delvise svar funksjonalitet ikke vises")
     void visDelviseSvarDefaulterTilFalse() {
+        System.clearProperty(VIS_DELVISE_SVAR_PROPERTY_KEY);
         assertEquals(false, FeatureToggle.visDelviseSvarFunksjonalitet());
     }
 
     @Test
     @DisplayName("Delvise svar skal kun vises hvis det er angitt i properties")
     void visDelviseSvarLeserProperty() {
-        setProperty(FeatureToggle.VIS_DELVISE_SVAR_PROPERTY_KEY, "true");
+        setProperty(VIS_DELVISE_SVAR_PROPERTY_KEY, "true");
         assertEquals(true, FeatureToggle.visDelviseSvarFunksjonalitet());
     }
 }
