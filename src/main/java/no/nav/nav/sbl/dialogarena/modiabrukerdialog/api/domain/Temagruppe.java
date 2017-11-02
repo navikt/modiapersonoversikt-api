014b918b7967aa4f92b336dc994327052585ce45
Eigil Skjæveland <eigil.skjeveland@nav.no>
2017-11-02 13:19:24 +0100

    OPP-23 Feature toggle nye køer

--- src/main/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/api/domain/Temagruppe.java
+++ src/main/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/api/domain/Temagruppe.java
@@ -2,6 +2,7 @@ package no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain;
 
 import java.util.List;
 
+import static java.lang.System.getProperty;
 import static java.util.Arrays.asList;
 
 public enum Temagruppe {
@@ -19,7 +20,11 @@ public enum Temagruppe {
     ANSOS;
 
     public static final List<Temagruppe> SAMTALEREFERAT = asList(ARBD, FMLI, HJLPM, PENS, OVRG, OKSOS, ANSOS);
-    public static final List<Temagruppe> LEGG_TILBAKE = asList(ARBD, FMLI, PLEIEPENGERSY, HJLPM, BIL, UTLAND, ORT_HJE, PENS, UFRT, OKSOS, ANSOS);
-    public static final List<Temagruppe> PLUKKBARE = asList(ARBD, FMLI, PLEIEPENGERSY, HJLPM, BIL, UTLAND, ORT_HJE, PENS, UFRT);
+    public static final List<Temagruppe> LEGG_TILBAKE = getProperty("visNyeKoer", "false").equals("true")
+            ? asList(ARBD, FMLI, PLEIEPENGERSY, HJLPM, BIL, UTLAND, ORT_HJE, PENS, UFRT, OKSOS, ANSOS)
+            : asList(ARBD, FMLI, HJLPM, BIL, ORT_HJE, PENS, UFRT, OKSOS, ANSOS);
+    public static final List<Temagruppe> PLUKKBARE = getProperty("visNyeKoer", "false").equals("true")
+            ? asList(ARBD, FMLI, PLEIEPENGERSY, HJLPM, BIL, UTLAND, ORT_HJE, PENS, UFRT)
+            : asList(ARBD, FMLI, HJLPM, BIL, ORT_HJE, PENS, UFRT);
     public static final List<Temagruppe> KOMMUNALE_TJENESTER = asList(OKSOS, ANSOS);
 }
