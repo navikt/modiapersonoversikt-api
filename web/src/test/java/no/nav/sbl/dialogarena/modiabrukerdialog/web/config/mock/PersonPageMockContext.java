5a8014050f7b18167181a1ca41b9fe110bd8e1ae
Joakim Lindquister <joakim.lindquister@nav.no>
2017-11-07 15:43:14 +0100

    Sett opp dependencies for tester

--- web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/mock/PersonPageMockContext.java
+++ web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/mock/PersonPageMockContext.java
@@ -8,10 +8,12 @@ import no.nav.kjerneinfo.consumer.fim.person.to.HentKjerneinformasjonResponse;
 import no.nav.kjerneinfo.domain.person.Person;
 import no.nav.kjerneinfo.domain.person.Personfakta;
 import no.nav.modig.content.CmsContentRetriever;
+import no.nav.modig.content.PropertyResolver;
 import no.nav.modig.security.tilgangskontroll.policy.pep.EnforcementPoint;
 import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain.norg.AnsattEnhet;
 import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.service.organisasjonsEnhetV2.OrganisasjonEnhetV2Service;
 import no.nav.personsok.consumer.fim.personsok.PersonsokServiceBi;
+import no.nav.sbl.dialogarena.modiabrukerdialog.web.config.utils.WicketInjectablePropertyResolver;
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.service.plukkoppgave.PlukkOppgaveService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -83,6 +85,18 @@ public class PersonPageMockContext {
         when(mock.getDefaultLocale()).thenReturn("nb");
         when(mock.hentTekst(anyString())).thenReturn("Tekst fra mock-cms");
         return mock;
+    }
+
+    @Bean
+    public WicketInjectablePropertyResolver wicketInjectablePropertyResolver() {
+        WicketInjectablePropertyResolver mock = mock(WicketInjectablePropertyResolver.class);
+        when(mock.getProperty(anyString())).thenReturn("Property");
+        return mock;
+    }
+
+    @Bean
+    public PropertyResolver propertyResolver() {
+        return mock(PropertyResolver.class);
     }
 
     private HentKjerneinformasjonResponse lagMockKjerneinfoResponse() {
