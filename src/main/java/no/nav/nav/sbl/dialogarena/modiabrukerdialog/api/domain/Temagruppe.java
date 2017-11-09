4ea868a7b53afdefbe3ff8ddcb5bf09412ebda8f
Eigil Skjæveland <eigil.skjeveland@nav.no>
2017-11-09 17:01:30 +0100

    OPP-178 Fjerne feature toggling for nye køer

--- src/main/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/api/domain/Temagruppe.java
+++ src/main/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/api/domain/Temagruppe.java
@@ -20,11 +20,7 @@ public enum Temagruppe {
     ANSOS;
 
     public static final List<Temagruppe> SAMTALEREFERAT = asList(ARBD, FMLI, HJLPM, PENS, OVRG, OKSOS, ANSOS);
-    public static final List<Temagruppe> LEGG_TILBAKE = getProperty("visNyeKoer", "false").equals("true")
-            ? asList(ARBD, FMLI, HJLPM, BIL, ORT_HJE, PENS, PLEIEPENGERSY, UFRT, UTLAND, OKSOS, ANSOS)
-            : asList(ARBD, FMLI, HJLPM, BIL, ORT_HJE, PENS, UFRT, OKSOS, ANSOS);
-    public static final List<Temagruppe> PLUKKBARE = getProperty("visNyeKoer", "false").equals("true")
-            ? asList(ARBD, FMLI, HJLPM, BIL, ORT_HJE, PENS, PLEIEPENGERSY, UFRT, UTLAND)
-            : asList(ARBD, FMLI, HJLPM, BIL, ORT_HJE, PENS, UFRT);
+    public static final List<Temagruppe> LEGG_TILBAKE = asList(ARBD, FMLI, HJLPM, BIL, ORT_HJE, PENS, PLEIEPENGERSY, UFRT, UTLAND, OKSOS, ANSOS);
+    public static final List<Temagruppe> PLUKKBARE = asList(ARBD, FMLI, HJLPM, BIL, ORT_HJE, PENS, PLEIEPENGERSY, UFRT, UTLAND);
     public static final List<Temagruppe> KOMMUNALE_TJENESTER = asList(OKSOS, ANSOS);
 }
