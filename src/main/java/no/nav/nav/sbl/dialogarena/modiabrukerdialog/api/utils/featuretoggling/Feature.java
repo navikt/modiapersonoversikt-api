bb4342287f305bfc2cd911efc4f8af879c68e78f
Joakim Lindquister <joakim.lindquister@nav.no>
2017-11-09 14:48:56 +0100

    Forsøkt å gjøre feature-toggling litt mer utvidbart med å innføre enums som skal inneholde alle features

---
+++ src/main/java/no/nav/nav/sbl/dialogarena/modiabrukerdialog/api/utils/featuretoggling/Feature.java
@@ -0,0 +1,17 @@
+package no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.utils.featuretoggling;
+
+public enum Feature {
+
+    DELVISE_SVAR("visDelviseSvarFunksjonalitet", false),
+    PLEIEPENGER("visPleiepenger", false),
+    ENDRE_NAVN("visEndreNavnFunksjonalitet", false);
+
+    public final String propertyKey;
+    public final boolean defaultValue;
+
+    Feature(String propertyKey, boolean defaultValue) {
+        this.propertyKey = propertyKey;
+        this.defaultValue = defaultValue;
+    }
+
+}
