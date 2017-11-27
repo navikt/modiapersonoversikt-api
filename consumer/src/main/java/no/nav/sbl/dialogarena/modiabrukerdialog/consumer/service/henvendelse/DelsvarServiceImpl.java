0605c6b245ab7f63c369f49610c75510a83c3dc7
Joakim Lindquister <joakim.lindquister@nav.no>
2017-11-27 15:02:08 +0100

    OPP-50 Bruk begrepet delsvar istedenfor svar delvis. Bedre å bruke navn som fag kjenner igjen!

--- consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/service/henvendelse/DelsvarServiceImpl.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/service/henvendelse/DelsvarServiceImpl.java
@@ -9,25 +9,25 @@ import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.service.HenvendelseUtse
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
-public class SvarDelvisServiceImpl implements SvarDelvisService {
+public class DelsvarServiceImpl implements DelsvarService {
 
     private final HenvendelseUtsendingService henvendelseUtsendingService;
 
-    public SvarDelvisServiceImpl(HenvendelseUtsendingService henvendelseUtsendingService) {
+    public DelsvarServiceImpl(HenvendelseUtsendingService henvendelseUtsendingService) {
         this.henvendelseUtsendingService = henvendelseUtsendingService;
     }
 
-    public void svarDelvis(SvarDelvisRequest request) {
-        Melding delvisSvar = lagDelvisSvar(hentBrukersSporsmal(request), request);
+    public void svarDelvis(DelsvarRequest request) {
+        Melding delsvar = lagDelsvar(hentBrukersSporsmal(request), request);
         try {
-            henvendelseUtsendingService.ferdigstillHenvendelse(delvisSvar, Optional.empty(),
+            henvendelseUtsendingService.ferdigstillHenvendelse(delsvar, Optional.empty(),
                     Optional.empty(), request.henvendelseId, request.saksbehandlersValgteEnhet);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
-    private Melding lagDelvisSvar(Melding brukersSporsmal, SvarDelvisRequest request) {
+    private Melding lagDelsvar(Melding brukersSporsmal, DelsvarRequest request) {
         return new Melding()
                 .withKanal(Kanal.TEKST.name())
                 .withFritekst(new Fritekst(request.svar))
@@ -42,7 +42,7 @@ public class SvarDelvisServiceImpl implements SvarDelvisService {
                 .withBrukersEnhet(brukersSporsmal.brukersEnhet);
     }
 
-    private Melding hentBrukersSporsmal(SvarDelvisRequest request) {
+    private Melding hentBrukersSporsmal(DelsvarRequest request) {
         return henvendelseUtsendingService.hentTraad(request.fodselsnummer, request.traadId, request.saksbehandlersValgteEnhet).stream()
                 .findFirst()
                 .orElseThrow(NoSuchElementException::new);
