9251f660484980f7377ce99838eb24b3efd153fd
Joakim Lindquister <joakim.lindquister@nav.no>
2017-11-29 17:30:58 +0100

    PK-44257 Kast feil som inkluderer feilmelding fra TPS

--- consumer/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/service/organisasjonenhet/kontaktinformasjon/OrganisasjonEnhetKontaktinformasjonServiceImplTest.java
+++ consumer/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/service/organisasjonenhet/kontaktinformasjon/OrganisasjonEnhetKontaktinformasjonServiceImplTest.java
@@ -7,12 +7,14 @@ import no.nav.tjeneste.virksomhet.organisasjonenhetkontaktinformasjon.v1.binding
 import no.nav.tjeneste.virksomhet.organisasjonenhetkontaktinformasjon.v1.informasjon.*;
 import no.nav.tjeneste.virksomhet.organisasjonenhetkontaktinformasjon.v1.meldinger.HentKontaktinformasjonForEnhetBolkRequest;
 import no.nav.tjeneste.virksomhet.organisasjonenhetkontaktinformasjon.v1.meldinger.HentKontaktinformasjonForEnhetBolkResponse;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.*;
 
@@ -20,19 +22,21 @@ class OrganisasjonEnhetKontaktinformasjonServiceImplTest {
 
     public static final String ENHET_ID = "2000";
     public static final String GATEADRESSE = "Islands gate";
+    public static final String FEILMELDING_FEILET_ENHET = "Feilmelding om feilet enhet";
 
     private final OrganisasjonEnhetKontaktinformasjonV1 serviceMock = mock(OrganisasjonEnhetKontaktinformasjonV1.class);
 
+    private OrganisasjonEnhetKontaktinformasjonServiceImpl service;
+
     @BeforeEach
     void beforeEach() throws HentKontaktinformasjonForEnhetBolkUgyldigInput {
+        service = new OrganisasjonEnhetKontaktinformasjonServiceImpl(serviceMock);
         when(serviceMock.hentKontaktinformasjonForEnhetBolk(any())).thenReturn(mockResponse());
     }
 
     @Test
-    @DisplayName("Setter opp request objekt riktig")
+    @DisplayName("Setter opp requestobjekt riktig")
     void setterOppRequestObjekt() throws HentKontaktinformasjonForEnhetBolkUgyldigInput {
-        OrganisasjonEnhetKontaktinformasjonServiceImpl service = new OrganisasjonEnhetKontaktinformasjonServiceImpl(serviceMock);
-
         service.hentKontaktinformasjon(ENHET_ID);
 
         ArgumentCaptor<HentKontaktinformasjonForEnhetBolkRequest> argumentCaptor = ArgumentCaptor.forClass(HentKontaktinformasjonForEnhetBolkRequest.class);
@@ -43,8 +47,6 @@ class OrganisasjonEnhetKontaktinformasjonServiceImplTest {
     @Test
     @DisplayName("Kontaktinformasjon returnerer data om forespurt enhet")
     void hentKontaktinformasjon() {
-        OrganisasjonEnhetKontaktinformasjonServiceImpl service = new OrganisasjonEnhetKontaktinformasjonServiceImpl(serviceMock);
-
         OrganisasjonEnhetKontaktinformasjon kontaktinformasjon = service.hentKontaktinformasjon(ENHET_ID);
 
         assertEquals(ENHET_ID, kontaktinformasjon.getEnhetId());
@@ -53,11 +55,18 @@ class OrganisasjonEnhetKontaktinformasjonServiceImplTest {
     @Test
     @DisplayName("Returnerer data om kontaktinformasjon")
     void returnererKontaktinformasjon() {
-        OrganisasjonEnhetKontaktinformasjonServiceImpl service = new OrganisasjonEnhetKontaktinformasjonServiceImpl(serviceMock);
-
         OrganisasjonEnhetKontaktinformasjon kontaktinfo = service.hentKontaktinformasjon(ENHET_ID);
 
         assertEquals(GATEADRESSE, kontaktinfo.getKontaktinformasjon().getPublikumsmottak().get(0).getBesoeksadresse().getGatenavn());
+    }
+
+    @Test
+    @DisplayName("Kaster exception med feilmelding fra TPS ved feil")
+    void enhetFeiler() throws HentKontaktinformasjonForEnhetBolkUgyldigInput {
+        when(serviceMock.hentKontaktinformasjonForEnhetBolk(any())).thenReturn(mockResponsMedFeiletEnhet());
+
+        RuntimeException runtimeException = assertThrows(RuntimeException.class, () -> service.hentKontaktinformasjon(ENHET_ID));
+        Assertions.assertTrue(runtimeException.getMessage().contains(FEILMELDING_FEILET_ENHET));
     }
 
     private HentKontaktinformasjonForEnhetBolkResponse mockResponse() {
@@ -83,4 +92,13 @@ class OrganisasjonEnhetKontaktinformasjonServiceImplTest {
         publikumsmottak.setAapningstider(new Aapningstider());
         return publikumsmottak;
     }
+
+    private HentKontaktinformasjonForEnhetBolkResponse mockResponsMedFeiletEnhet() {
+        HentKontaktinformasjonForEnhetBolkResponse response = new HentKontaktinformasjonForEnhetBolkResponse();
+        FeiletEnhet feiletEnhet = new FeiletEnhet();
+        feiletEnhet.setFeilmelding(FEILMELDING_FEILET_ENHET);
+        response.getFeiletEnhetListe().add(feiletEnhet);
+        return response;
+    }
+
 }
