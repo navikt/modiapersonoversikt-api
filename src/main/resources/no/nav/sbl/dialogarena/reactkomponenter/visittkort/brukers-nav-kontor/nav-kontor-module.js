36a046933a82788716c8f13cdf595ff9776df068
Joakim Lindquister <joakim.lindquister@nav.no>
2017-11-29 15:23:08 +0100

    PK-44257 Legg til lenke til mer informasjon om kontoret

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/visittkort/brukers-nav-kontor/nav-kontor-module.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/visittkort/brukers-nav-kontor/nav-kontor-module.js
@@ -6,9 +6,9 @@ import AsyncLoader from './async-loader';
 import Ajax from '../../utils/ajax';
 import { EkspanderbartpanelBase } from 'nav-frontend-ekspanderbartpanel';
 import NavKontorHeader from './components/header';
-import DetaljertEnhetsInformasjon from './components/detaljert-enhetsinformasjon';
+import DetaljertEnhetsinformasjon from './components/detaljert-enhetsinformasjon';
 
-function NavKontor({ organisasjon }) {
+function NavKontor({ organisasjon, baseUrlAppAdeo }) {
     return (
         <div className="nav-kontor-panel">
             <div className="nav-ikon" />
@@ -16,13 +16,14 @@ function NavKontor({ organisasjon }) {
                 ariaTittel={"Brukers NAV-kontor"}
                 heading={<NavKontorHeader organisasjon={organisasjon} />}
             >
-                <DetaljertEnhetsInformasjon organisasjon={organisasjon} />
+                <DetaljertEnhetsinformasjon organisasjon={organisasjon} baseUrlAppAdeo={baseUrlAppAdeo} />
             </EkspanderbartpanelBase>
         </div>
     );
 }
 NavKontor.propTypes = {
-    organisasjon: PT.object
+    organisasjon: PT.object,
+    baseUrlAppAdeo: PT.string.isRequired
 };
 
 class BrukersNavKontor extends React.Component {
@@ -42,13 +43,14 @@ class BrukersNavKontor extends React.Component {
         }
         return (
             <AsyncLoader promises={this.promise} toProp={"organisasjon"} >
-                <NavKontor />
+                <NavKontor baseUrlAppAdeo={this.props.baseUrlAppAdeo} />
             </AsyncLoader>
         );
     }
 }
 BrukersNavKontor.propTypes = {
-    organisasjonsenhetId: PT.string.isRequired
+    organisasjonsenhetId: PT.string.isRequired,
+    baseUrlAppAdeo: PT.string.isRequired
 };
 
 export default BrukersNavKontor;
