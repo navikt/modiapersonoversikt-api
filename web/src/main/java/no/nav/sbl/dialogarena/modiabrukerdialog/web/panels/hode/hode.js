55664e63f4ce77cb30c62c9791f00a3dc3749eae
Nicklas Utgaard <nicklas.utgaard@nav.no>
2017-12-07 16:11:08 +0100

    [OPP-51] enhet context

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/panels/hode/hode.js
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/panels/hode/hode.js
@@ -37,7 +37,11 @@ function fetchOkStatus(resp) {
 function toJson(resp) {
     return resp.json();
 }
-
+function getMe() {
+    return fetch('/modiabrukerdialog/rest/hode/me', { credentials: 'same-origin'})
+        .then(fetchOkStatus)
+        .then(toJson);
+}
 function nullstillContext() {
     return fetch('https://modapp' + finnMiljoStreng() + '.adeo.no/modiacontextholder/api/context/nullstill', {
         credentials: 'same-origin',
@@ -58,7 +62,7 @@ function hentContextEnhet() {
 }
 
 function oppdaterContextBruker(fnr) {
-    if (!fnr || fnr.lengt === 0) {
+    if (!fnr || fnr.length === 0) {
         return nullstillContext();
     } else {
         return fetch('https://modapp' + finnMiljoStreng() + '.adeo.no/modiacontextholder/api/context', {
@@ -92,12 +96,20 @@ function oppdatertValgtEnhet(valgtEnhet) {
             'Content-Type': 'application/json'
         }
     });
+    oppdaterContextEnhet(valgtEnhet);
 }
 
 function lastInnBruker(fnr) {
     window.location = 'https://modapp' + finnMiljoStreng() + '.adeo.no/modiabrukerdialog/hentPerson/' + fnr;
 }
 
+function getCookie(name) {
+    var value = ";" + document.cookie;
+    var parts = value.split("; " + name + "=");
+    if (parts.length === 2) {
+        return parts.pop().split(";").shift();
+    }
+}
 // Wicket integration
 function JSWicket(url, component) {
     this.url = url;
@@ -135,11 +147,8 @@ function resetBruker(sendToWicket) {
             });
     }
 }
-function byttEnhet(enhet) {
-    console.log('byttet enhet', enhet);
-}
 
-function lagDecoratorConfig(fnr, autoSubmit, feilmelding) {
+function lagDecoratorConfig(fnr, initiellEnhet, autoSubmit, feilmelding) {
     const config = {
         config: {
             dataSources: {
@@ -147,8 +156,9 @@ function lagDecoratorConfig(fnr, autoSubmit, feilmelding) {
                 enheter: '/modiabrukerdialog/rest/hode/enheter'
             },
             toggles: { visEnhet: false, visEnhetVelger: true, visSokefelt: true, visVeileder: true },
-            applicationName: 'Modiabrukerdialog',
-            handleChangeEnhet: byttEnhet,
+            applicationName: 'Modia personoversikt',
+            initiellEnhet: initiellEnhet,
+            handleChangeEnhet: oppdatertValgtEnhet,
             extraMarkup: {
                 etterSokefelt: '<button class="personsok-button" id="toggle-personsok" aria-label="Åpne avansert søk" title="Åpne avansert søk" data-apne="Åpne avansert søk" data-lukke="Lukk avansert søk"> <span> A <span class="personsok-pil"></span> </span> </button>'
             }
@@ -163,12 +173,10 @@ function lagDecoratorConfig(fnr, autoSubmit, feilmelding) {
         config.config.feilmelding = feilmelding;
     }
 
-    config.config.handleChangeEnhet = oppdatertValgtEnhet;
-
     return config;
 }
 const websocketCallbackMap = {
-    'NY_AKTIV_BRUKER': function(fnr, redirectModal) {
+    'NY_AKTIV_BRUKER': function(markupId, fnr, redirectModal) {
         hentContextBruker()
             .then(function(data) {
                 if (data.aktivBruker === fnr) {
@@ -185,15 +193,18 @@ const websocketCallbackMap = {
                 redirectModal.vis(data.aktivBruker, endreCallback, beholdCallback);
             });
     },
-    'NY_AKTIV_ENHET': function(fnr, redirectModal) {
+    'NY_AKTIV_ENHET': function(markupId, fnr) {
         hentContextEnhet()
-            .then(function(data) { console.log('data', data); });
+            .then(function(data) {
+                window.rerenderHode(markupId, lagDecoratorConfig(fnr, data.aktivEnhet, false, ''));
+            });
     },
 };
 
 // Hode init
 (function () {
     let decoratorConfig = null; // Må legges hit for å kunne deles mellom 'initHode' og 'update'
+    let redirectModal = null;
 
     window.initHode = function initHode(callbackUrl, markupId, fnr, autoSubmit, feilmelding) {
         // Setter opp wicket-callback-handler
@@ -201,23 +212,31 @@ const websocketCallbackMap = {
         document.addEventListener('dekorator-hode-personsok', sokOppFnr(sendToWicket));
         document.addEventListener('dekorator-hode-fjernperson', resetBruker(sendToWicket));
 
-        // Lager decorator og redirect modal
-        decoratorConfig = lagDecoratorConfig(fnr, autoSubmit, feilmelding);
-        const redirectModal = lagRedirectModal();
-        console.log('config', decoratorConfig);
-        window.renderDecoratorHead(decoratorConfig, markupId);
-
         // Laster inn/Oppdater bruker ved sidelast
-        hentContextBruker().then(function(data) {
-            console.log('brukerIContext', data);
-            if (data.aktivBruker !== fnr) {
-                if (fnr && fnr.length > 0) {
-                    oppdaterContextBruker(fnr);
-                } else {
-                    lastInnBruker(data.aktivBruker);
+        Promise.all([hentContextBruker(), hentContextEnhet(), getMe()])
+            .then(function(data) {
+                var brukerData = data[0];
+                var enhetData = data[1];
+                var me = data[2];
+
+                console.log('brukerIContext', brukerData, enhetData, me);
+                if (brukerData.aktivBruker !== fnr) {
+                    if (fnr && fnr.length > 0) {
+                        oppdaterContextBruker(fnr);
+                    } else if (brukerData.aktivBruker) {
+                        lastInnBruker(brukerData.aktivBruker);
+                    }
                 }
-            }
-        });
+
+                const valgtEnhet = enhetData.aktivEnhet || getCookie('saksbehandlerinnstillinger-'+me.ident);
+                oppdatertValgtEnhet(valgtEnhet);
+
+                // Lager decorator og redirect modal
+                decoratorConfig = lagDecoratorConfig(fnr, valgtEnhet, autoSubmit, feilmelding);
+                redirectModal = lagRedirectModal();
+                console.log('config', decoratorConfig);
+                window.renderDecoratorHead(decoratorConfig, markupId);
+            });
 
 
         // Setter opp context-lyttere
@@ -225,16 +244,18 @@ const websocketCallbackMap = {
             const type = event.data;
             const handler = websocketCallbackMap[type] || (function() { console.warn('Ukjent event fra contextholder', event); });
 
-            handler(fnr, redirectModal);
+            handler(markupId, fnr, redirectModal);
         });
     };
-
+    window.rerenderHode = function(markupId, config) {
+        console.log('update config', config, markupId);
+        window.renderDecoratorHead(config, markupId);
+    };
     window.updateHode = function (markupId, feilmelding) {
         let config = decoratorConfig;
         if (feilmelding && feilmelding.length > 0) {
             config.config.feilmelding = feilmelding;
         }
-        console.log('update config', config, markupId);
-        window.renderDecoratorHead(config, markupId);
+        window.rerenderHode(markupId, config);
     };
 })();
