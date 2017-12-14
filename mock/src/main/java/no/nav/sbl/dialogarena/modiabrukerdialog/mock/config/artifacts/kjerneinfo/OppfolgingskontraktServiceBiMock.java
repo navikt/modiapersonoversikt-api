fbc609daa18fc40370035917cf8ed3f539aa85e3
Daniel Winsvold <daniel.winsvold@nav.no>
2017-12-14 14:29:17 +0100

    Fjerner Aktivitet og Oppfolgingspunkt

--- mock/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/mock/config/artifacts/kjerneinfo/OppfolgingskontraktServiceBiMock.java
+++ mock/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/mock/config/artifacts/kjerneinfo/OppfolgingskontraktServiceBiMock.java
@@ -8,14 +8,14 @@ import no.nav.kontrakter.consumer.utils.OppfolgingskontraktMapper;
 import no.nav.kontrakter.domain.oppfolging.SYFOPunkt;
 import no.nav.tjeneste.virksomhet.oppfoelging.v1.informasjon.*;
 import no.nav.tjeneste.virksomhet.oppfoelging.v1.meldinger.HentOppfoelgingskontraktListeResponse;
-import org.joda.time.LocalDateTime;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
-import static no.nav.kjerneinfo.common.mockutils.DateUtils.*;
+import static no.nav.kjerneinfo.common.mockutils.DateUtils.convertDateToXmlGregorianCalendar;
+import static no.nav.kjerneinfo.common.mockutils.DateUtils.getRandomDatePair;
 import static no.nav.kontrakter.consumer.fim.ytelseskontrakt.mock.YtelseskontraktMockFactory.YTELSESSTATUS_AKTIV;
 import static org.joda.time.LocalDate.now;
 import static org.mockito.Matchers.any;
@@ -55,19 +55,6 @@ public class OppfolgingskontraktServiceBiMock {
         oppfoelgingskontrakt.setStatus("Aktiv");
         oppfoelgingskontrakt.setIhtGjeldendeVedtak(createVedtak(null, null, null, null));
         oppfoelgingskontrakt.getAvYtelse().add(createYtelseskontrakt(null, null, DateUtils.getRandomDate()));
-        oppfoelgingskontrakt.getHarOppfoelgingspunkt().addAll(Arrays.asList(
-                createOppfoelgingspunkt("Individuell samtale", "Avbrutt", DateUtils.getRandomLocalDateTime()),
-                createOppfoelgingspunkt("Individuell samtale", "Fullført", DateUtils.getRandomLocalDateTime()),
-                createOppfoelgingspunkt("Gruppeaktiviteter", "Utredning i spesialenhet/andrelinje", DateUtils.getRandomLocalDateTime()),
-                createOppfoelgingspunkt("Gruppeaktiviteter", "Informasjonsmøte om arbeidsavklaringspenger", DateUtils.getRandomLocalDateTime())));
-        oppfoelgingskontrakt.getMedAktivitet().addAll(Arrays.asList(
-                createAktivitet("Aktivitetsnavn 1", "Planlagt", null, null, true),
-                createAktivitet("Aktivitetsnavn 2", "Avbrutt", null, null, false),
-                createAktivitet("Aktivitetsnavn 3", "Fullført", null, null, false),
-                createAktivitet("Aktivitetsnavn 4", "Avbrutt gjennomføring", null, null, false),
-                createTiltaksaktivitet("Planlagt", "Aktuell", null, null),
-                createTiltaksaktivitet("Planlagt", "Fullført", null, null),
-                createTiltaksaktivitet("Avbrutt gjennomføring", "Gjennomføres", null, null)));
         oppfoelgingskontrakt.getMedPlan().addAll(Arrays.asList(
                 createPlan("Godkjent", "aktivitetsplan", "Beholde arbeid", null, null),
                 createPlan("Avbrutt", "individuellPlan", "Øke deltakelse eller mål om arbeid", null, null),
@@ -96,34 +83,6 @@ public class OppfolgingskontraktServiceBiMock {
     private static Periode createRandomPeriode() {
         Date[] datePair = getRandomDatePair();
         return createPeriode(datePair[0], datePair[1]);
-    }
-
-
-    private static Tiltaksaktivitet createTiltaksaktivitet(String status, String deltakelsestatus, Date fra, Date til) {
-        Tiltaksaktivitet tiltaksaktivitet = new Tiltaksaktivitet();
-        tiltaksaktivitet.setStatus(status);
-        tiltaksaktivitet.setAktivitetsnavn("Tiltaksaktivitetnavn");
-        tiltaksaktivitet.setTiltaksdeltakelsestatus(deltakelsestatus);
-        tiltaksaktivitet.setPeriode(fra == null ? createRandomPeriode() : createPeriode(fra, til));
-
-        return tiltaksaktivitet;
-    }
-
-    private static Aktivitet createAktivitet(String navn, String status, Date fra, Date til, boolean sensitiv) {
-        Aktivitet aktivitet = new Aktivitet();
-        aktivitet.setAktivitetsnavn(navn);
-        aktivitet.setStatus(status);
-        aktivitet.setSensitiv(sensitiv);
-        aktivitet.setPeriode(fra == null ? createRandomPeriode() : createPeriode(fra, til));
-        return aktivitet;
-    }
-
-    private static Oppfoelgingspunkt createOppfoelgingspunkt(String type, String status, LocalDateTime trefftidspunkt) {
-        Oppfoelgingspunkt oppfoelgingspunkt = new Oppfoelgingspunkt();
-        oppfoelgingspunkt.setDato(convertDateTimeToXmlGregorianCalendar(trefftidspunkt));
-        oppfoelgingspunkt.setStatus(status);
-        oppfoelgingspunkt.setType(type);
-        return oppfoelgingspunkt;
     }
 
     private static Ytelseskontrakt createYtelseskontrakt(String status, String type, Date datoKravMottat) {
