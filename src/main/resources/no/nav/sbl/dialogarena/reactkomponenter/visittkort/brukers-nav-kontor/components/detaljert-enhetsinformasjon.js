3ff549142d01a2f5b2ed134f238111b7b06e9d01
Joakim Lindquister <joakim.lindquister@nav.no>
2017-12-21 13:44:58 +0100

    PK-50027 Vis lenke til NORG selv om det ikke finnes publikumsmottak

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/visittkort/brukers-nav-kontor/components/detaljert-enhetsinformasjon.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/visittkort/brukers-nav-kontor/components/detaljert-enhetsinformasjon.js
@@ -6,9 +6,31 @@ import Apningstider from './apningstider';
 import Adresse from './adresse';
 import AntallPublikumsmottakInformasjon from './publikumsmottak-informasjon';
 
+function NORGLenke({ enhetId, norg2FrontendBaseUrl }) {
+    return (
+        <a
+            href={`${norg2FrontendBaseUrl}/#/startsok?enhetNr=${enhetId}`}
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            Mer informasjon om kontoret
+        </a>
+    );
+}
+
+NORGLenke.propTypes = {
+    enhetId: PT.string.isRequired,
+    norg2FrontendBaseUrl: PT.string.isRequired
+};
+
 function DetaljertEnhetsinformasjon({ organisasjon, norg2FrontendBaseUrl }) {
     if (organisasjon.kontaktinformasjon.publikumsmottak.length === 0) {
-        return <p>Ingen publikumsmottak</p>;
+        return (
+            <div>
+                <p className="infoblokk">Ingen publikumsmottak</p>
+                <NORGLenke enhetId={organisasjon.enhetId} norg2FrontendBaseUrl={norg2FrontendBaseUrl}/>
+            </div>
+        );
     }
     const publikumsmottak = organisasjon.kontaktinformasjon.publikumsmottak[0];
     return (
@@ -16,13 +38,7 @@ function DetaljertEnhetsinformasjon({ organisasjon, norg2FrontendBaseUrl }) {
             <Adresse adresse={publikumsmottak.besoeksadresse} />
             <AntallPublikumsmottakInformasjon publikumsmottak={organisasjon.kontaktinformasjon.publikumsmottak} />
             <Apningstider apningstider={publikumsmottak.apningstider} />
-            <a
-                href={`${norg2FrontendBaseUrl}/#/startsok?enhetNr=${organisasjon.enhetId}`}
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Mer informasjon om kontoret
-            </a>
+            <NORGLenke enhetId={organisasjon.enhetId} norg2FrontendBaseUrl={norg2FrontendBaseUrl} />
         </div>
     );
 }
