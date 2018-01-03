207917996d5d606871f007450ba77f2f41426024
Joakim Lindquister <joakim.lindquister@nav.no>
2018-01-03 10:07:33 +0100

    OPP-161 Fjern console.log statement

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/visittkort/adresse/components/matrikkeladresse.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/visittkort/adresse/components/matrikkeladresse.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PT from 'prop-types';
 
 function Matrikkeladresse({ adresse }) {
-    console.log(adresse);
     const { eiendomsnavn, postnummer, poststed } = adresse;
     const adresseLinje = `${eiendomsnavn ? `${eiendomsnavn}, ` : ''}${postnummer} ${poststed}`;
     return (<span>{adresseLinje}</span>);
