eac5bb86912005132862b43e9d7fa9487d021d51
Joakim Lindquister <joakim.lindquister@nav.no>
2018-02-13 14:38:49 +0100

    OPP-205 Slå sammen delsvar som er etterfulgt av avsluttende svar inn i det avsluttende svaret

--- src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/domain/Traader.java
+++ src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/domain/Traader.java
@@ -1,22 +1,69 @@
 package no.nav.sbl.dialogarena.sporsmalogsvar.domain;
 
+import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain.henvendelse.Fritekst;
 import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain.henvendelse.Melding;
 import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.utils.MeldingUtils;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Traader {
 
     private final Map<String, List<Melding>> traader;
 
     public Traader(List<Melding> meldinger) {
-        this.traader = MeldingUtils.skillUtTraader(meldinger);
-        sammenslaFullforteDelsvar();
+        Map<String, List<Melding>> traader = MeldingUtils.skillUtTraader(meldinger);
+        this.traader = sammenslaFullforteDelsvar(traader);
     }
 
-    private void sammenslaFullforteDelsvar() {
+    private Map<String, List<Melding>> sammenslaFullforteDelsvar(Map<String, List<Melding>> traader) {
+        return traader.values().stream()
+                .map(this::sammenslaFullforteDelsvar)
+                .collect(Collectors.toMap(traad -> traad.get(0).traadId, traad -> traad));
+    }
 
+    private List<Melding> sammenslaFullforteDelsvar(List<Melding> traad) {
+        if (traadHarAvsluttendeSvarEtterDelsvar(traad)) {
+            List<Fritekst> fritekster = getFriteksterFraDelsvar(traad);
+            Melding avsluttendeSvar = getAvsluttendeSvar(traad);
+            fritekster.addAll(avsluttendeSvar.getFriteksterMedEldsteForst());
+
+            avsluttendeSvar.withFritekst(fritekster.toArray(new Fritekst[fritekster.size()]));
+            return traad.stream()
+                    .filter(melding-> !melding.erDelvisSvar())
+                    .collect(Collectors.toList());
+        }
+        return traad;
+    }
+
+    private List<Fritekst> getFriteksterFraDelsvar(List<Melding> traad) {
+        return traad.stream()
+                        .filter(Melding::erDelvisSvar)
+                        .sorted(Comparator.comparing(a -> a.opprettetDato))
+                        .map(melding -> new Fritekst(melding.getFritekst(), melding.skrevetAv, melding.opprettetDato))
+                        .collect(Collectors.toList());
+    }
+
+    private Melding getAvsluttendeSvar(List<Melding> traad) {
+        return traad.stream()
+                .sorted(Comparator.comparing(melding -> melding.opprettetDato))
+                .filter(Melding::erSvarSkriftlig)
+                .findFirst()
+                .orElseThrow(IllegalStateException::new);
+    }
+
+    private boolean traadHarAvsluttendeSvarEtterDelsvar(List<Melding> traad) {
+        traad.sort(Comparator.comparing(melding -> melding.opprettetDato));
+        Collections.reverse(traad);
+
+        Iterator<Melding> iterator = traad.iterator();
+        Melding melding = iterator.next();
+        while (iterator.hasNext()) {
+            if (melding.erSvarSkriftlig() && iterator.hasNext() && iterator.next().erDelvisSvar()) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public Map<String, List<Melding>> getTraader() {
--- src/test/java/no/nav/sbl/dialogarena/sporsmalogsvar/domain/TraaderTest.java
+++ src/test/java/no/nav/sbl/dialogarena/sporsmalogsvar/domain/TraaderTest.java
@@ -1,22 +1,38 @@
 package no.nav.sbl.dialogarena.sporsmalogsvar.domain;
 
+import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain.Person;
+import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain.henvendelse.Fritekst;
 import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain.henvendelse.Melding;
+import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain.henvendelse.Meldingstype;
+import org.joda.time.DateTime;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TraaderTest {
 
+    public static final String TRAAD_ID_1 = "traad-id-1";
+
     @Test
     @DisplayName("Slår sammen meldinger med samme tråd-id til en tråd")
     void slarSammenMeldingerTilTrader() {
         Traader traader = new Traader(Arrays.asList(
-                new Melding().withId("traad-id-1").withTraadId("traad-id-1"),
-                new Melding().withId("id2").withTraadId("traad-id-1")
+                new Melding()
+                        .withId(TRAAD_ID_1)
+                        .withTraadId(TRAAD_ID_1)
+                        .withType(Meldingstype.SPORSMAL_SKRIFTLIG)
+                        .withOpprettetDato(DateTime.now()),
+                new Melding()
+                        .withId("id2")
+                        .withTraadId(TRAAD_ID_1)
+                        .withType(Meldingstype.SVAR_SKRIFTLIG)
+                        .withOpprettetDato(DateTime.now())
         ));
 
         assertEquals( 1, traader.getTraader().size());
@@ -26,8 +42,16 @@ class TraaderTest {
     @DisplayName("Skiller ut meldinger med forskjellige tråd-ider ut i flere tråder")
     void skillerUtMeldingerITraader() {
         Traader traader = new Traader(Arrays.asList(
-                new Melding().withId("traad-id-1").withTraadId("traad-id-1"),
-                new Melding().withId("traad-id-2").withTraadId("traad-id-2")
+                new Melding()
+                        .withId(TRAAD_ID_1)
+                        .withTraadId(TRAAD_ID_1)
+                        .withType(Meldingstype.SPORSMAL_SKRIFTLIG)
+                        .withOpprettetDato(DateTime.now()),
+                new Melding()
+                        .withId("traad-id-2")
+                        .withTraadId("traad-id-2")
+                        .withType(Meldingstype.SPORSMAL_SKRIFTLIG)
+                        .withOpprettetDato(DateTime.now())
         ));
 
         assertEquals( 2, traader.getTraader().size());
@@ -39,6 +63,93 @@ class TraaderTest {
         Traader traader = new Traader(Collections.emptyList());
 
         assertEquals( true, traader.erUtenMeldinger());
+    }
+
+    @Nested()
+    class DelsvarTest {
+
+        @Test
+        @DisplayName("Slår sammen 3 meldinger, hvorav en er et delsvar, til 2 meldinger")
+        void slarSammenIkkeAvsluttedeDelsvar() {
+            Traader traader = new Traader(mockMeldingskjedeMedDelsvar());
+
+            assertEquals(2, traader.getTraader().get(TRAAD_ID_1).size());
+        }
+
+        @Test
+        @DisplayName("Slår sammen tråd hvor det er et delsvar fulgt av to svar til 3 meldinger")
+        void slarSammenMedToSvar() {
+            Traader traader = new Traader(mockMeldingskjedeMedDelsvarOgToSvar());
+
+            assertEquals(3, traader.getTraader().get(TRAAD_ID_1).size());
+        }
+
+        @Test
+        @DisplayName("Slår friteksten fra delsvaret inn i det avsluttende svaret")
+        void slarSammenFritekst() {
+            Traader traader = new Traader(mockMeldingskjedeMedDelsvar());
+
+            Melding avsluttendeSvar = getAvsluttendeSvar(traader);
+            assertEquals(2, avsluttendeSvar.getFriteksterMedEldsteForst().size());
+        }
+
+        private List<Melding> mockMeldingskjedeMedDelsvar() {
+            return Arrays.asList(
+               new Melding()
+                       .withId(TRAAD_ID_1)
+                       .withTraadId(TRAAD_ID_1)
+                       .withType(Meldingstype.SPORSMAL_SKRIFTLIG)
+                       .withOpprettetDato(DateTime.now()),
+               new Melding()
+                       .withId("id2")
+                       .withTraadId(TRAAD_ID_1)
+                       .withType(Meldingstype.DELVIS_SVAR_SKRIFTLIG)
+                       .withOpprettetDato(DateTime.now())
+                       .withFritekst(new Fritekst("Jeg svarer med et delsvar!", new Person("John", "Johnsen"), DateTime.now())),
+               new Melding()
+                       .withId("id3")
+                       .withTraadId(TRAAD_ID_1)
+                       .withType(Meldingstype.SVAR_SKRIFTLIG)
+                       .withOpprettetDato(DateTime.now())
+                       .withFritekst(new Fritekst("Jeg svarer med et endelig svar!", new Person("Anne", "Saksbehandler"), DateTime.now()))
+            );
+        }
+
+        private List<Melding> mockMeldingskjedeMedDelsvarOgToSvar() {
+            return Arrays.asList(
+                    new Melding()
+                            .withId(TRAAD_ID_1)
+                            .withTraadId(TRAAD_ID_1)
+                            .withType(Meldingstype.SPORSMAL_SKRIFTLIG)
+                            .withOpprettetDato(DateTime.now()),
+                    new Melding()
+                            .withId("id2")
+                            .withTraadId(TRAAD_ID_1)
+                            .withType(Meldingstype.DELVIS_SVAR_SKRIFTLIG)
+                            .withOpprettetDato(DateTime.now())
+                            .withFritekst(new Fritekst("Jeg svarer med et delsvar!", new Person("John", "Johnsen"), DateTime.now())),
+                    new Melding()
+                            .withId("id3")
+                            .withTraadId(TRAAD_ID_1)
+                            .withType(Meldingstype.SVAR_SKRIFTLIG)
+                            .withOpprettetDato(DateTime.now())
+                            .withFritekst(new Fritekst("Jeg svarer med et endelig svar!", new Person("Anne", "Saksbehandler"), DateTime.now())),
+                    new Melding()
+                            .withId("id4")
+                            .withTraadId(TRAAD_ID_1)
+                            .withType(Meldingstype.SVAR_SKRIFTLIG)
+                            .withOpprettetDato(DateTime.now())
+                            .withFritekst(new Fritekst("Jeg svarer med enda et endelig svar!", new Person("Anne", "Saksbehandler"), DateTime.now()))
+            );
+        }
+
+        private Melding getAvsluttendeSvar(Traader traader) {
+            return traader.getTraader().get(TRAAD_ID_1).stream()
+                    .filter(Melding::erSvarSkriftlig)
+                    .findFirst()
+                    .get();
+        }
+
     }
 
 }
