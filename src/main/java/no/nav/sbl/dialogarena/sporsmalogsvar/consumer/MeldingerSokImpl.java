a511f1b799366909932b4330632757fc95b59e28
Joakim Lindquister <joakim.lindquister@nav.no>
2018-02-14 14:52:09 +0100

    OPP-205 Legg til støtte for delsvar i Meldingsøk-modulen

--- src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/consumer/MeldingerSokImpl.java
+++ src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/consumer/MeldingerSokImpl.java
@@ -199,22 +199,46 @@ public class MeldingerSokImpl implements MeldingerSok {
         Document document = new Document();
         document.add(new StoredField(ID, id));
         document.add(new StoredField(BEHANDLINGS_ID, melding.id));
-        document.add(new TextField(FRITEKST, ofNullable(melding.getFritekst()).orElse(""), YES));
+        document.add(new TextField(FRITEKST, getFritekst(melding), YES));
         document.add(new TextField(TEMAGRUPPE, ofNullable(melding.temagruppeNavn).orElse(""), YES));
         document.add(new TextField(ARKIVTEMA, ofNullable(melding.journalfortTemanavn).orElse(""), YES));
         document.add(new TextField(DATO, ofNullable(melding.visningsDatoTekst).orElse(""), YES));
-        document.add(new TextField(NAVIDENT, ofNullable(melding.navIdent).orElse(""), YES));
+        document.add(new TextField(NAVIDENT, getNavIdent(melding), YES));
         document.add(new TextField(STATUSTEKST, ofNullable(melding.statusTekst).orElse(""), YES));
         document.add(new TextField(IKONTEKST, ofNullable(melding.statusTekst).orElse(""), YES));
         document.add(new TextField(LEST_STATUS, ofNullable(melding.lestStatus).orElse(""), YES));
         document.add(new TextField(KANAL, ofNullable(melding.kanal).orElse(""), YES));
-        document.add(new TextField(SKREVET_AV_NAVN, ofNullable(melding.skrevetAv.navn).orElse(""), YES));
+        document.add(new TextField(SKREVET_AV_NAVN, getForfattere(melding), YES));
         document.add(new TextField(JOURNALFORT_AV_NAVN, ofNullable(melding.journalfortAv.navn).orElse(""), YES));
         document.add(new TextField(JOURNALFORT_AV_IDENT, ofNullable(melding.journalfortAvNavIdent).orElse(""), YES));
         document.add(new TextField(JOURNALFORT_DATO, ofNullable(melding.journalfortDatoTekst).orElse(""), YES));
         document.add(new TextField(JOURNALFORT_SAKSID, ofNullable(melding.journalfortSaksId).orElse(""), YES));
 
         return document;
+    }
+
+    private static String getNavIdent(Melding melding) {
+        List<String> saksbehandlerNavn = melding.getFriteksterMedEldsteForst().stream()
+                .map(Fritekst::getSaksbehandler)
+                .filter(Optional::isPresent)
+                .map(saksbehandler -> saksbehandler.get().getIdent())
+                .collect(toList());
+        return String.join(", ", saksbehandlerNavn);
+    }
+
+    private static String getForfattere(Melding melding) {
+        List<String> saksbehandlerNavn = melding.getFriteksterMedEldsteForst().stream()
+                .map(Fritekst::getSaksbehandler)
+                .filter(Optional::isPresent)
+                .map(saksbehandler -> saksbehandler.get().navn)
+                .collect(toList());
+        return String.join(", ", saksbehandlerNavn);
+    }
+
+    private static String getFritekst(Melding melding) {
+        return String.join("\n\n", melding.getFriteksterMedEldsteForst().stream()
+                .map(Fritekst::getFritekst)
+                .collect(toList()));
     }
 
     private static String query(String soketekst) {
