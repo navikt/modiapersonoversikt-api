1762858de20a2297b424beb28c7ba9e677504a41
Daniel Winsvold <daniel.winsvold@nav.no>
2018-02-15 09:46:00 +0100

    OPP-287 rydder i reacttester med warnings. Første pulje

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/slaa-sammen-traader/slaa-sammen-traader-module-test.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/slaa-sammen-traader/slaa-sammen-traader-module-test.js
@@ -1,13 +1,20 @@
 import '../test-config';
 import React from 'react';
-import { shallow, mount, render } from 'enzyme';
+import { shallow } from 'enzyme';
 import { expect } from 'chai';
 
 import SlaaSammenTraader from './slaa-sammen-traader-module';
 
+const slaaSammenTraaderPropsMock = {
+    traadIder: [],
+    fnr: '',
+    wicketurl: '',
+    wicketcomponent: ''
+};
+
 describe('<SlaaSammenTraader> komponent', () => {
     it('Rendrer et MeldingerSok', () => {
-        const node = shallow(<SlaaSammenTraader />);
+        const node = shallow(<SlaaSammenTraader {...slaaSammenTraaderPropsMock} />);
         expect(node.is('MeldingerSok')).to.be.true;
     });
 });
