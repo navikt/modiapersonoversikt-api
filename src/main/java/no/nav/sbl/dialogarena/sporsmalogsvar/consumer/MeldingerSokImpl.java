bf1d76fc6f18763f23139c54fed282678244c1a1
Joakim Lindquister <joakim.lindquister@nav.no>
2018-02-16 10:40:35 +0100

    OPP-205 Kodeforbedring: Bruk joining direkte på streamen

--- src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/consumer/MeldingerSokImpl.java
+++ src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/consumer/MeldingerSokImpl.java
@@ -1,14 +1,25 @@
 package no.nav.sbl.dialogarena.sporsmalogsvar.consumer;
 
 import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain.Person;
-import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain.henvendelse.*;
+import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain.henvendelse.Fritekst;
+import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain.henvendelse.Melding;
+import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain.henvendelse.Traad;
 import no.nav.sbl.dialogarena.sporsmalogsvar.common.utils.DateUtils;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.document.*;
-import org.apache.lucene.index.*;
-import org.apache.lucene.queryparser.classic.*;
-import org.apache.lucene.search.*;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.StoredField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopScoreDocCollector;
 import org.apache.lucene.search.highlight.*;
 import org.apache.lucene.store.RAMDirectory;
 import org.apache.lucene.util.Version;
@@ -218,21 +229,19 @@ public class MeldingerSokImpl implements MeldingerSok {
     }
 
     private static String getNavIdent(Melding melding) {
-        List<String> saksbehandlerNavn = melding.getFriteksterMedEldsteForst().stream()
+        return melding.getFriteksterMedEldsteForst().stream()
                 .map(Fritekst::getSaksbehandler)
                 .filter(Optional::isPresent)
                 .map(saksbehandler -> saksbehandler.get().getIdent())
-                .collect(toList());
-        return String.join(", ", saksbehandlerNavn);
+                .collect(joining(", "));
     }
 
     private static String getForfattere(Melding melding) {
-        List<String> saksbehandlerNavn = melding.getFriteksterMedEldsteForst().stream()
+        return melding.getFriteksterMedEldsteForst().stream()
                 .map(Fritekst::getSaksbehandler)
                 .filter(Optional::isPresent)
                 .map(saksbehandler -> saksbehandler.get().navn)
-                .collect(toList());
-        return String.join(", ", saksbehandlerNavn);
+                .collect(joining(", "));
     }
 
     private static String getFritekst(Melding melding) {
