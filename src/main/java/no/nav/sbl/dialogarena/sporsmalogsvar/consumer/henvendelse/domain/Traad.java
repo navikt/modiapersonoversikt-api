04422158bf50f9cc345bf4aa6bf38f70a08a9306
Joakim Lindquister <joakim.lindquister@nav.no>
2018-02-20 14:59:39 +0100

    OPP-205 Logikk for å slå sammen delsvar er flyttet inn i api-modulen

--- src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/consumer/henvendelse/domain/Traad.java
+++ src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/consumer/henvendelse/domain/Traad.java
@@ -1,6 +1,8 @@
 package no.nav.sbl.dialogarena.sporsmalogsvar.consumer.henvendelse.domain;
 
 import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.domain.henvendelse.Melding;
+import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.utils.henvendelse.delsvar.DelsvarSammenslaaer;
+import no.nav.nav.sbl.dialogarena.modiabrukerdialog.api.utils.henvendelse.delsvar.DelsvarUtils;
 
 import java.util.Collections;
 import java.util.Comparator;
@@ -12,38 +14,11 @@ public class Traad {
     private List<Melding> meldinger;
 
     public Traad(List<Melding> meldinger) {
-        if (harAvsluttendeSvarEtterDelsvar(meldinger)) {
-            this.meldinger = sammenslaFullforteDelsvar(meldinger);
+        if (DelsvarUtils.harAvsluttendeSvarEtterDelsvar(meldinger)) {
+            this.meldinger = DelsvarSammenslaaer.sammenslaFullforteDelsvar(meldinger);
         } else {
             this.meldinger = meldinger;
         }
-    }
-
-    private boolean harAvsluttendeSvarEtterDelsvar(List<Melding> traad) {
-        List<Melding> sorterMedNyesteForst = sorterMedNyesteForst(traad);
-
-        Iterator<Melding> iterator = sorterMedNyesteForst.iterator();
-        Melding current = iterator.next();
-        while (iterator.hasNext()) {
-            Melding next = iterator.next();
-            if (current.erSvarSkriftlig() && next.erDelvisSvar()) {
-                return true;
-            }
-            current = next;
-        }
-        return false;
-    }
-
-    private List<Melding> sorterMedNyesteForst(List<Melding> traad) {
-        List<Melding> listCopy = traad.subList(0, traad.size());
-        listCopy.sort(Comparator.comparing(melding -> melding.opprettetDato));
-        Collections.reverse(listCopy);
-        return listCopy;
-    }
-
-    private List<Melding> sammenslaFullforteDelsvar(List<Melding> meldinger) {
-        MeldingSammenslaaer meldingSammenslaaer = new MeldingSammenslaaer(meldinger);
-        return meldingSammenslaaer.sammenslaFullforteDelsvar();
     }
 
     public String getTraadId() {
