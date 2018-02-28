4bda70b43b1228ff71a9a8cd8677acd81f00f845
Daniel Winsvold <daniel.winsvold@nav.no>
2018-02-28 15:51:29 +0100

    OPP-24 flere-oppgaver alert er nå tabb-bar

--- src/main/java/no/nav/sbl/dialogarena/reactkomponenter/utils/wicket/ReactResources.java
+++ src/main/java/no/nav/sbl/dialogarena/reactkomponenter/utils/wicket/ReactResources.java
@@ -28,6 +28,7 @@ public class ReactResources {
                     new PackageResourceReference(ResourceReference.class, "build/delvis-svar.less"),
                     new PackageResourceReference(ResourceReference.class, "build/nav-kontor.less"),
                     new PackageResourceReference(ResourceReference.class, "build/traadvisning.less"),
-                    new PackageResourceReference(ResourceReference.class, "build/tildelt-flere-oppgaver-alert.less")
+                    new PackageResourceReference(ResourceReference.class, "build/tildelt-flere-oppgaver-alert.less"),
+                    new PackageResourceReference(ResourceReference.class, "build/alertstripe.less")
             ).done();
 }
