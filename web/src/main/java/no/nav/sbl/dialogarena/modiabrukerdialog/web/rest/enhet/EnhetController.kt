9bd974017f96289335e6c0ce59a70e148fcdb785
Richard Borge <Richard.Borge@nav.no>
2018-04-11 14:35:34 +0200

    OPP-395 Renamet variabler

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/rest/enhet/EnhetController.kt
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/rest/enhet/EnhetController.kt
@@ -1,15 +1,10 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.web.rest.enhet
 
-import no.nav.kjerneinfo.consumer.fim.person.PersonKjerneinfoServiceBi
-import no.nav.kjerneinfo.consumer.fim.person.to.HentKjerneinformasjonRequest
-import no.nav.sbl.dialogarena.modiabrukerdialog.api.domain.norg.AnsattEnhet
 import no.nav.sbl.dialogarena.modiabrukerdialog.api.service.organisasjonsEnhetV2.OrganisasjonEnhetV2Service
 import no.nav.sbl.dialogarena.modiabrukerdialog.api.utils.featuretoggling.Feature
 import no.nav.sbl.dialogarena.modiabrukerdialog.api.utils.featuretoggling.visFeature
 import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.service.organisasjonenhet.kontaktinformasjon.domain.OrganisasjonEnhetKontaktinformasjon
 import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.service.organisasjonenhet.kontaktinformasjon.service.OrganisasjonEnhetKontaktinformasjonService
-import no.nav.tjeneste.virksomhet.person.v3.HentPersonPersonIkkeFunnet
-import no.nav.tjeneste.virksomhet.person.v3.HentPersonSikkerhetsbegrensning
 
 import javax.inject.Inject
 import javax.ws.rs.*
@@ -18,14 +13,14 @@ import javax.ws.rs.core.MediaType.APPLICATION_JSON
 
 @Path("/enheter")
 class EnhetController @Inject
-constructor(private val oekservice: OrganisasjonEnhetKontaktinformasjonService,
-            private val oe2service: OrganisasjonEnhetV2Service) {
+constructor(private val organisasjonEnhetKontaktinformasjonService: OrganisasjonEnhetKontaktinformasjonService,
+            private val organisasjonEnhetV2Service: OrganisasjonEnhetV2Service) {
 
     @GET
     @Path("/{id}")
     @Produces(APPLICATION_JSON)
     fun hentMedId(@PathParam("id") organisasjonsid: String): OrganisasjonEnhetKontaktinformasjon {
-        return oekservice.hentKontaktinformasjon(organisasjonsid)
+        return organisasjonEnhetKontaktinformasjonService.hentKontaktinformasjon(organisasjonsid)
     }
 
     @GET
@@ -35,7 +30,7 @@ constructor(private val oekservice: OrganisasjonEnhetKontaktinformasjonService,
 
         check(visFeature(Feature.ENHETER_GEOGRAFISK_TILKNYTNING_API))
 
-        val enhetid = oe2service.finnNAVKontor(geografiskId, diskresjonskode)
+        val enhetid = organisasjonEnhetV2Service.finnNAVKontor(geografiskId, diskresjonskode)
                 .map { it.enhetId }
                 .orElseThrow{ NotFoundException() }
 
--- web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/rest/enhet/EnhetKontrollerTest.kt
+++ web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/rest/enhet/EnhetKontrollerTest.kt
@@ -1,10 +1,10 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.web.rest.enhet
 
+import no.nav.sbl.dialogarena.modiabrukerdialog.api.service.organisasjonsEnhetV2.OrganisasjonEnhetV2Service
 import no.nav.sbl.dialogarena.modiabrukerdialog.api.utils.featuretoggling.Feature
 import no.nav.sbl.dialogarena.modiabrukerdialog.api.utils.featuretoggling.disableFeature
 import no.nav.sbl.dialogarena.modiabrukerdialog.api.utils.featuretoggling.enableFeature
-import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.service.organisasjonenhet.OrganisasjonEnhetV2ServiceImpl
-import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.service.organisasjonenhet.kontaktinformasjon.service.OrganisasjonEnhetKontaktinformasjonServiceImpl
+import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.service.organisasjonenhet.kontaktinformasjon.service.OrganisasjonEnhetKontaktinformasjonService
 import org.junit.jupiter.api.*
 import org.mockito.Mock
 import org.mockito.Mockito
@@ -14,20 +14,20 @@ import java.util.*
 import javax.ws.rs.NotFoundException
 
 class EnhetKontrollerTest {
-    @Mock private lateinit var oe2service: OrganisasjonEnhetV2ServiceImpl
-    @Mock private lateinit var oekservice: OrganisasjonEnhetKontaktinformasjonServiceImpl
+    @Mock private lateinit var organisasjonEnhetV2Service: OrganisasjonEnhetV2Service
+    @Mock private lateinit var organisasjonEnhetKontaktinformasjonService: OrganisasjonEnhetKontaktinformasjonService
     private lateinit var controller: EnhetController
 
     @BeforeEach
     fun before() {
         MockitoAnnotations.initMocks(this)
-        controller = EnhetController(oekservice, oe2service)
+        controller = EnhetController(organisasjonEnhetKontaktinformasjonService, organisasjonEnhetV2Service)
     }
 
     @Test
     @DisplayName("Kaster 404 hvis enhet ikke ble funnet")
     fun kaster404HvisEnhetIkkeFunnet() {
-        `when`(oe2service.finnNAVKontor(Mockito.any(), Mockito.any())).thenReturn(Optional.empty())
+        `when`(organisasjonEnhetV2Service.finnNAVKontor(Mockito.any(), Mockito.any())).thenReturn(Optional.empty())
         Assertions.assertThrows(NotFoundException::class.java, { controller.hentMedGeoTilk("", "") })
     }
 
