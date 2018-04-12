1e93b99a06d22cec31de4765da8b2d0e9366a46c
Richard Borge <Richard.Borge@nav.no>
2018-04-12 14:16:34 +0200

    OPP-395 Remapper objekt til ønsket JSON format

---
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/rest/enhet/model/Klokkeslett.kt
@@ -0,0 +1,5 @@
+package no.nav.sbl.dialogarena.modiabrukerdialog.web.rest.enhet.model
+
+data class Klokkeslett(val time: String?,
+                       val minutt: String?,
+                       val sekund: String?)
