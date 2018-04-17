aec871e3f924485a56c7f2e9a64db3dd1040edcd
Eigil Skjæveland <eigil.skjeveland@nav.no>
2018-04-17 16:13:43 +0200

    Oppdater react-state når en tråd ferdigstilles uten svar
    OPP-383

--- sporsmalogsvar/src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/lamell/FerdigstiltUtenSvarPanel.java
+++ sporsmalogsvar/src/main/java/no/nav/sbl/dialogarena/sporsmalogsvar/lamell/FerdigstiltUtenSvarPanel.java
@@ -1,5 +1,6 @@
 package no.nav.sbl.dialogarena.sporsmalogsvar.lamell;
 
+import no.nav.modig.wicket.events.annotations.RunOnEvents;
 import no.nav.sbl.dialogarena.modiabrukerdialog.api.domain.Saksbehandler;
 import no.nav.sbl.dialogarena.reactkomponenter.utils.wicket.ReactComponentPanel;
 import no.nav.sbl.dialogarena.sporsmalogsvar.common.utils.DateUtils;
@@ -10,13 +11,31 @@ import org.apache.wicket.model.PropertyModel;
 import java.util.HashMap;
 
 import static no.nav.modig.wicket.conditional.ConditionalUtils.visibleIf;
+import static no.nav.sbl.dialogarena.modiabrukerdialog.api.constants.Events.SporsmalOgSvar.MELDING_VALGT;
+import static no.nav.sbl.dialogarena.sporsmalogsvar.lamell.haandtermelding.merke.MerkePanel.TRAAD_MERKET;
 
 public class FerdigstiltUtenSvarPanel extends Panel {
 
+    private final ReactComponentPanel reactComponent;
+    private final InnboksVM innboksVM;
+
     public FerdigstiltUtenSvarPanel(String id, InnboksVM innboksVM) {
         super(id, new PropertyModel<>(innboksVM, "valgtTraad"));
+        this.innboksVM = innboksVM;
         setOutputMarkupId(true);
 
+        reactComponent = new ReactComponentPanel("ferdigstiltUtenSvar", "AlertStripeSuksessSolid", getProps());
+
+        add(reactComponent);
+        add(visibleIf(new AbstractReadOnlyModel<Boolean>() {
+            @Override
+            public Boolean getObject() {
+                return innboksVM.getValgtTraad().erFerdigstiltUtenSvar();
+            }
+        }));
+    }
+
+    private HashMap<String, Object> getProps() {
         String veilederNavn = innboksVM.getValgtTraad().getFerdigstiltUtenSvarAv()
                 .map(saksbehandler -> saksbehandler.navn)
                 .orElse("<navn mangler>");
@@ -26,16 +45,17 @@ public class FerdigstiltUtenSvarPanel extends Panel {
         String ferdigstiltDato = innboksVM.getValgtTraad().getFerdigstiltUtenSvarDato()
                 .map(DateUtils::toString)
                 .orElse("<dato mangler>");
-
-        add(new ReactComponentPanel("ferdigstiltUtenSvar", "AlertStripeSuksessSolid", new HashMap<String, Object>(){{
+        return new HashMap<String, Object>() {{
             put("tekst", "Ferdigstilt uten svar av " + veilederNavn + " (" + veilederIdent + "), " + ferdigstiltDato);
-        }}));
-
-        add(visibleIf(new AbstractReadOnlyModel<Boolean>() {
-            @Override
-            public Boolean getObject() {
-                return innboksVM.getValgtTraad().erFerdigstiltUtenSvar();
-            }
-        }));
+        }};
     }
+
+    @RunOnEvents({TRAAD_MERKET, MELDING_VALGT})
+    private void oppdaterReactComponent() {
+        PropertyModel<Boolean> objectPropertyModel = new PropertyModel<>(getDefaultModel(), "erFerdigstiltUtenSvar");
+        if(objectPropertyModel.getObject()) {
+            reactComponent.updateState(getProps());
+        }
+    }
+
 }
