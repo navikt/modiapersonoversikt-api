528e550bde12c8ae66969858bbacaa009df4cc1c
Ketil Velle <ketil.s.velle@nav.no>
2018-04-24 07:55:30 +0100

    Lagt til nytt REST-endepunkt for informasjon om person er egenAnsatt.
    OPP-397

---
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/rest/egenansatt/EgenAnsattController.kt
@@ -0,0 +1,27 @@
+package no.nav.sbl.dialogarena.modiabrukerdialog.web.rest.egenansatt
+
+import no.nav.kjerneinfo.consumer.egenansatt.EgenAnsattService
+import no.nav.sbl.dialogarena.modiabrukerdialog.api.utils.featuretoggling.Feature
+import no.nav.sbl.dialogarena.modiabrukerdialog.api.utils.featuretoggling.visFeature
+import javax.inject.Inject
+import javax.ws.rs.GET
+import javax.ws.rs.Path
+import javax.ws.rs.PathParam
+import javax.ws.rs.Produces
+import javax.ws.rs.core.MediaType
+
+@Path("/egenansatt/{fnr}")
+class EgenAnsattController @Inject constructor(private val egenAnsattService: EgenAnsattService)  {
+
+    @GET
+    @Path("/")
+    @Produces(MediaType.APPLICATION_JSON)
+    fun erEgenAnsatt(@PathParam("fnr") fødselsnummer: String): Map<String, Boolean> {
+
+        check(visFeature(Feature.PERSON_REST_API))
+
+        return mapOf("erEgenAnsatt" to egenAnsattService.erEgenAnsatt(fødselsnummer))
+    }
+}
+
+
