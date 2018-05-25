02223692f6874c14e9057b55dd9a643004cc4c09
Joakim Lindquister <joakim.lindquister@nav.no>
2018-05-25 08:53:03 +0200

    OPP-443 Lagt til endepunkt for å endre navn
    Nytt rest-endepunkt for å endre-navn. Brukes fra
    administrer-brukerprofil-siden. Endre navn er en post-request, og det
    fungerte ikke med det CORS-oppsettet som var i RestConfig.java. Har
    derfor lagt til en filter i web.xml.

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/rest/RestConfig.java
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/rest/RestConfig.java
@@ -2,6 +2,7 @@ package no.nav.sbl.dialogarena.modiabrukerdialog.web.rest;
 
 import no.nav.brukerdialog.isso.RelyingPartyCallback;
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.config.JacksonConfig;
+import no.nav.sbl.dialogarena.modiabrukerdialog.web.rest.brukerprofil.BrukerprofilController;
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.rest.egenansatt.EgenAnsattController;
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.rest.enhet.EnhetController;
 import no.nav.sbl.dialogarena.modiabrukerdialog.web.rest.henvendelse.DelsvarController;
@@ -15,23 +16,7 @@ import no.nav.sbl.dialogarena.sak.rest.SaksoversiktController;
 import no.nav.sbl.dialogarena.varsel.rest.VarslerController;
 import org.glassfish.jersey.server.ResourceConfig;
 
-import javax.ws.rs.container.ContainerRequestContext;
-import javax.ws.rs.container.ContainerResponseContext;
-import javax.ws.rs.container.ContainerResponseFilter;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-
-import static no.nav.sbl.dialogarena.modiabrukerdialog.api.utils.featuretoggling.Feature.PERSON_REST_API;
-import static no.nav.sbl.dialogarena.modiabrukerdialog.api.utils.featuretoggling.FeatureToggleKt.visFeature;
-
 public class RestConfig extends ResourceConfig {
-
-    private static List<String> allowedOrigins = Arrays.asList(
-            ".adeo.no",
-            ".nais.preprod.local",
-            "http://localhost:3000"
-    );
 
     public RestConfig() {
         super(
@@ -51,21 +36,8 @@ public class RestConfig extends ResourceConfig {
                 PersonController.class,
                 VergemalController.class,
                 KontaktinformasjonController.class,
+                BrukerprofilController.class,
                 EgenAnsattController.class
         );
-        if (!visFeature(PERSON_REST_API)) {
-            return;
-        }
-        register(new ContainerResponseFilter() {
-            @Override
-            public void filter(ContainerRequestContext request, ContainerResponseContext response) throws IOException {
-                String origin = request.getHeaderString("Origin");
-                if (origin != null && allowedOrigins.stream().anyMatch(origin::endsWith)) {
-                    response.getHeaders().add("Access-Control-Allow-Origin", origin);
-                    response.getHeaders().add("Access-Control-Allow-Credentials", "true");
-                    response.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD");
-                }
-            }
-        });
     }
 }
---
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/rest/brukerprofil/domain/EndreNavnRequest.kt
@@ -0,0 +1,8 @@
+package no.nav.sbl.dialogarena.modiabrukerdialog.web.rest.brukerprofil.domain
+
+class EndreNavnRequest {
+    var fødselsnummer = ""
+    var fornavn = ""
+    var mellomnavn = ""
+    var etternavn = ""
+}
