853f89aa98c18257a3d827e8750dd6618c99055f
Joakim Lindquister <joakim.lindquister@nav.no>
2018-05-28 12:30:46 +0200

    OPP-443 Kun set CORS-headers når person-api feature-toggle er skrudd på

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/rest/CORSFilter.java
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/rest/CORSFilter.java
@@ -1,11 +1,15 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.web.rest;
 
+import no.nav.sbl.dialogarena.modiabrukerdialog.api.utils.featuretoggling.Feature;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+
+import static no.nav.sbl.dialogarena.modiabrukerdialog.api.utils.featuretoggling.FeatureToggleKt.visFeature;
 
 public class CORSFilter implements Filter {
 
@@ -26,14 +30,20 @@ public class CORSFilter implements Filter {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
 
         String origin = httpRequest.getHeader("Origin");
+        if (visFeature(Feature.PERSON_REST_API)) {
+            setCorsHeadere(httpResponse, origin);
+        }
+
+        filterChain.doFilter(httpRequest, httpResponse);
+    }
+
+    private void setCorsHeadere(HttpServletResponse httpResponse, String origin) {
         if (origin != null && allowedOrigins.stream().anyMatch(origin::endsWith)) {
             httpResponse.setHeader("Access-Control-Allow-Origin", origin);
             httpResponse.setHeader("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, NAV_CSRF_PROTECTION");
             httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
             httpResponse.setHeader("Access-Control-Allow-Credentials", "true");
         }
-
-        filterChain.doFilter(httpRequest, httpResponse);
     }
 
     @Override
