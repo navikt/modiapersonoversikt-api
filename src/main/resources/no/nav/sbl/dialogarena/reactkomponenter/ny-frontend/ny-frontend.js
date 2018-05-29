1392b3c9ae70c2fdf251eadfa2a1a1d75e26b7c4
Richard Borge <Richard.Borge@nav.no>
2018-05-29 08:59:31 +0200

    Fikset slik at visittkortet kan vises både i gammel og ny applikasjon

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/ny-frontend/ny-frontend.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/ny-frontend/ny-frontend.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Visittkort } from 'modiapersonoversikt';
+import Visittkort from 'modiapersonoversikt/lib/components/StandAloneVisittkort/VisittKortStandAlone';
 
 class NyFrontend extends React.Component {
     render() {
