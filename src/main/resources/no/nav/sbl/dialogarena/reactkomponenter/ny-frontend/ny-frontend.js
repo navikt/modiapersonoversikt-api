a0633a70ae33c969b32d6a6412549709db9c4097
Daniel Winsvold <daniel.winsvold@nav.no>
2018-05-31 09:55:25 +0200

    følger opp kommentarer fra PR

--- src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/ny-frontend/ny-frontend.js
+++ src/main/resources/no/nav/sbl/dialogarena/reactkomponenter/ny-frontend/ny-frontend.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import PT from 'prop-types';
 
 import Visittkort from 'modiapersonoversikt/build/dist/components/StandAloneVisittkort/VisittKortStandAlone';
 
-class NyFrontend extends React.Component {
-    render() {
-        return <Visittkort fødselsnummer={ this.props.fødselsnummer } />;
-    }
+function NyFrontend() {
+    return <Visittkort fødselsnummer={this.props.fødselsnummer} />;
 }
+
+NyFrontend.propTypes = {
+    fødselsnummer: PT.string.isRequired
+};
 
 export default NyFrontend;
