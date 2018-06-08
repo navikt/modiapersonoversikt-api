6c554f6f115193a34ef64ab637d3134aec1cdff2
Jørund Amsen <jorundamsen@nav.no>
2018-06-08 09:38:10 +0200

    Legg til sample toggle så vi alltid har noe å teste på og generell test

--- api/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/api/utils/featuretoggling/Feature.kt
+++ api/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/api/utils/featuretoggling/Feature.kt
@@ -2,6 +2,7 @@ package no.nav.sbl.dialogarena.modiabrukerdialog.api.utils.featuretoggling
 
 enum class Feature private constructor(val propertyKey: String, val defaultValue: Boolean) {
 
+    SAMPLE_FEATURE("feature.samplerfeature", false),
     PERSON_REST_API("feature.aktiverPersonRestApi", false),
     ENHETER_GEOGRAFISK_TILKNYTNING_API("feature.aktiverEnheterGeografiskTilknytningApi", false),
     NYTT_VISITTKORT("feature.nyttvisittkort", false)
