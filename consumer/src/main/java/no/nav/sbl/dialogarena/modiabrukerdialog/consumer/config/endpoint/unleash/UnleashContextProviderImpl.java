d5ac6cadb7adc73a901f57571eb73f40b372e6f4
Jørund Amsen <jorundamsen@nav.no>
2018-07-12 12:54:18 +0200

    Reimplementer enheter i unleash context etter feilen med cookies Velger å kaller norg2 for enheter da dette kallet alerede er cachet tidligere Velger også å sjekke på alle enheter en saksbehandler har tilgang på, heller enn bare valgte enhet

--- consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/endpoint/unleash/UnleashContextProviderImpl.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/endpoint/unleash/UnleashContextProviderImpl.java
@@ -3,21 +3,25 @@ package no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.endpoint.unleas
 import no.finn.unleash.UnleashContext;
 import no.finn.unleash.UnleashContextProvider;
 import no.nav.brukerdialog.security.context.SubjectHandler;
-import no.nav.sbl.dialogarena.modiabrukerdialog.api.service.saksbehandler.SaksbehandlerInnstillingerService;
-import no.nav.sbl.dialogarena.modiabrukerdialog.api.utils.RestUtils;
+import no.nav.sbl.dialogarena.modiabrukerdialog.api.service.norg.AnsattService;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.inject.Inject;
 import java.util.HashMap;
 
+import static java.util.stream.Collectors.joining;
+import static no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.endpoint.unleash.strategier.ByEnhetStrategy.ENHETER;
+
 public class UnleashContextProviderImpl implements UnleashContextProvider {
 
     private SubjectHandler subjectHandler;
+    private AnsattService annsattService;
 
     @Inject
-    public UnleashContextProviderImpl(SubjectHandler subjectHandler) {
+    public UnleashContextProviderImpl(SubjectHandler subjectHandler, AnsattService annsattService) {
         this.subjectHandler = subjectHandler;
+        this.annsattService = annsattService;
     }
 
     @Override
@@ -27,10 +31,19 @@ public class UnleashContextProviderImpl implements UnleashContextProvider {
         String remoteAddress = attributes.getRequest().getRemoteAddr();
 
         String ident = subjectHandler.getUid();
+        String ansattEnheter = getEnheter();
 
         HashMap<String, String> properties = new HashMap<String, String>() {{
+            put(ENHETER, ansattEnheter);
         }};
 
         return new UnleashContext(ident, sessionId, remoteAddress, properties);
+    }
+
+    private String getEnheter() {
+        return annsattService.hentEnhetsliste()
+                    .stream()
+                    .map(enhet -> enhet.enhetId)
+                    .collect(joining(","));
     }
 }
