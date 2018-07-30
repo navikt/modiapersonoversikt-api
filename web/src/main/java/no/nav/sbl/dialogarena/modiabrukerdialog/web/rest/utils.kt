8980f765bf973f09015b56ebb6a84011c01d611e
Daniel Winsvold <daniel.winsvold@nav.no>
2018-07-30 13:38:51 +0200

    sender ikke med felter som er null på bankobjekt. Utvider mapOfNotNull til å ikke sende med tomme maps

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/rest/utils.kt
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/rest/utils.kt
@@ -1,3 +1,5 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.web.rest
 
-fun <K, V> mapOfNotNull(vararg pairs: Pair<K, V>) = pairs.filterNot { it.second == null } .toMap()
+fun <K, V> mapOfNotNull(vararg pairs: Pair<K, V>) = pairs
+        .filterNot { it.second == null || it.second is Map<*, *> && (it.second as Map<*, *>).isEmpty() }
+        .toMap()
