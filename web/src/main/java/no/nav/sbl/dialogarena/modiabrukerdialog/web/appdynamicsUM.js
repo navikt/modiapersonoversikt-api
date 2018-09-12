1a5d11e9a7a8cfa26efd97b85295b2dfc0d91bc7
Oscar Eckbo <oscar.eckbo@nav.no>
2018-09-12 14:11:14 +0200

    Legger til flere miljøer for appdynamics

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/appdynamicsUM.js
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/appdynamicsUM.js
@@ -1,12 +1,20 @@
 (function (config) {
     var appKey = "";
-    if (window.location.href.indexOf("modapp.adeo.no") > -1 || window.location.href.indexOf("app.adeo.no") > -1) {
+    if (window.location.href.indexOf("modapp.adeo.no") > -1) {
         appKey = 'EUM-AAB-AUN';
-    } else if (window.location.href.indexOf("modapp-q1.adeo.no") > -1 || window.location.href.indexOf("app-q1.adeo.no") > -1) {
+    } else if (window.location.href.indexOf("app.adeo.no") > -1) {
+        appKey = 'EUM-AAB-AUN';
+    } else if (window.location.href.indexOf("app-q1.adeo.no") > -1) {
         appKey = 'EUM-AAB-AUP';
-    } else if (window.location.href.indexOf("modapp-q0.adeo.no") > -1 || window.location.href.indexOf("app-q0.adeo.no") > -1) {
+    } else if (window.location.href.indexOf("modapp-q1.adeo.no") > -1) {
         appKey = 'EUM-AAB-AUP';
-    } else if (window.location.href.indexOf("modapp-t6.adeo.no") > -1 || window.location.href.indexOf("app-t6.adeo.no") > -1) {
+    } else if (window.location.href.indexOf("app-q0.adeo.no") > -1) {
+        appKey = 'EUM-AAB-AUS';
+    } else if (window.location.href.indexOf("modapp-q0.adeo.no") > -1) {
+        appKey = 'EUM-AAB-AUS';
+    } else if (window.location.href.indexOf("app-t6.adeo.no") > -1) {
+        appKey = 'EUM-AAB-AUR';
+    } else if (window.location.href.indexOf("modapp-t6.adeo.no") > -1) {
         appKey = 'EUM-AAB-AUR';
     }
     config.appKey = appKey;
