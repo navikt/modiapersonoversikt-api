624a537e8ad1056156da4f912039ed945007b403
Jan Eirik B. Nævdal <jan.eirik.b.navdal@nav.no>
2018-11-27 15:14:16 +0100

    OPP-609: lokalt kjørende med auth ok.

--- consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/service/oppfolgingsinfo/OppfolgingsinfoApiServiceImpl.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/service/oppfolgingsinfo/OppfolgingsinfoApiServiceImpl.java
@@ -2,6 +2,8 @@ package no.nav.sbl.dialogarena.modiabrukerdialog.consumer.service.oppfolgingsinf
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import no.nav.brukerdialog.security.context.SubjectHandler;
+import no.nav.json.JsonUtils;
 import no.nav.sbl.dialogarena.modiabrukerdialog.api.domain.Saksbehandler;
 import no.nav.sbl.dialogarena.modiabrukerdialog.api.domain.norg.AnsattEnhet;
 import no.nav.sbl.dialogarena.modiabrukerdialog.api.domain.oppfolgingsinfo.Oppfolgingsinfo;
@@ -9,26 +11,22 @@ import no.nav.sbl.dialogarena.modiabrukerdialog.api.domain.oppfolgingsinfo.rest.
 import no.nav.sbl.dialogarena.modiabrukerdialog.api.domain.oppfolgingsinfo.rest.Oppfolgingsstatus;
 import no.nav.sbl.dialogarena.modiabrukerdialog.api.service.ldap.LDAPService;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.CookieStore;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.protocol.HttpClientContext;
-import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.cookie.BasicClientCookie;
-import org.apache.http.protocol.BasicHttpContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import static java.lang.String.format;
+import static javax.ws.rs.core.HttpHeaders.AUTHORIZATION;
 
 public class OppfolgingsinfoApiServiceImpl implements OppfolgingsinfoApiService {
     private static final Logger logger = LoggerFactory.getLogger(OppfolgingsinfoApiServiceImpl.class);
@@ -51,8 +49,8 @@ public class OppfolgingsinfoApiServiceImpl implements OppfolgingsinfoApiService 
     public Oppfolgingsinfo hentOppfolgingsinfo(String fodselsnummer, HttpServletRequest request) {
         Oppfolgingsinfo info = null;
         try {
-            Oppfolgingsstatus status = hentOppfolgingsstatus(fodselsnummer, request);
-            Oppfolging oppf = hentOppfolging(fodselsnummer, request);
+            Oppfolgingsstatus status = hentOppfolgingsstatus(fodselsnummer);
+            Oppfolging oppf = hentOppfolging(fodselsnummer);
             info = new Oppfolgingsinfo(oppf.isUnderOppfolging())
                     .withVeileder(hentSaksbehandler(status.getVeilederId()))
                     .withOppfolgingsenhet(new AnsattEnhet(status.getOppfolgingsenhet().getEnhetId(), status.getOppfolgingsenhet().getNavn()));
@@ -66,13 +64,13 @@ public class OppfolgingsinfoApiServiceImpl implements OppfolgingsinfoApiService 
         return ldapService.hentSaksbehandler(veilederIdent);
     }
 
-    private Oppfolgingsstatus hentOppfolgingsstatus(String fødselsnummer, HttpServletRequest request) throws IOException {
-        return new ObjectMapper().readValue(gjorSporring(hentOppfølgingsstatusURL(fødselsnummer), request), new TypeReference<Oppfolgingsstatus>() {
+    private Oppfolgingsstatus hentOppfolgingsstatus(String fødselsnummer) throws IOException {
+        return new ObjectMapper().readValue(gjorSporring(hentOppfølgingsstatusURL(fødselsnummer)), new TypeReference<Oppfolgingsstatus>() {
         });
     }
 
-    private Oppfolging hentOppfolging(String fødselsnummer, HttpServletRequest request) throws IOException{
-        return new ObjectMapper().readValue(gjorSporring(hentOppfølgingURL(fødselsnummer), request), new TypeReference<Oppfolging>() {
+    private Oppfolging hentOppfolging(String fødselsnummer) throws IOException{
+        return new ObjectMapper().readValue(gjorSporring(hentOppfølgingURL(fødselsnummer)), new TypeReference<Oppfolging>() {
         });
     }
 
@@ -84,30 +82,22 @@ public class OppfolgingsinfoApiServiceImpl implements OppfolgingsinfoApiService 
         return apiUrl+String.format("oppfolging?fnr=%s", fødselsnummer);
     }
 
-    private String gjorSporring(String url, HttpServletRequest httpServletRequest) throws IOException {
+    private String gjorSporring(String url) throws IOException {
         logger.info("Prøver å hente fra " + url);
         HttpGet request = new HttpGet(url);
-        BasicHttpContext localContext = new BasicHttpContext();
-        CookieStore cookieStore = new BasicCookieStore();
-        Cookie[] cookies = httpServletRequest.getCookies();
-        if(cookies != null) {
-            for(Cookie cookie : cookies) {
-                logger.info("Legger til cookie " + cookie.getName() + " - " + cookie.getDomain());
-                BasicClientCookie clientCookie = new BasicClientCookie(cookie.getName(), cookie.getValue());
-                clientCookie.setDomain(cookie.getDomain() == null ? "localhost" : cookie.getDomain());
-                clientCookie.setPath("/");
-                cookieStore.addCookie(clientCookie);
-            }
-        }
-        localContext.setAttribute(HttpClientContext.COOKIE_STORE, cookieStore);
-        HttpResponse response = client.execute(request, localContext);
+        request.addHeader(AUTHORIZATION, "Bearer " + SubjectHandler.getSubjectHandler().getInternSsoToken());
+        HttpResponse response = client.execute(request);
         if (response.getStatusLine().getStatusCode() != 200) {
             throw new IOException(format("Kall på URL=\"%s\" returnerte respons med status=\"%s\"", url, response.getStatusLine()));
         }
+
+        Oppfolgingsstatus map = JsonUtils.fromJson(response.getEntity().getContent(), Oppfolgingsstatus.class);
+
         String innhold = hentInnhold(response);
         logger.info("Innhold: " + innhold);
         return innhold;
     }
+
     private String hentInnhold(HttpResponse response) throws IOException {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()))) {
             return reader.lines().collect(Collectors.joining("\n"));
