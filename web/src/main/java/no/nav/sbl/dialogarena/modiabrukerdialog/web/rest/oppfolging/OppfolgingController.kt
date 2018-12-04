8047295c627e5bb68ac640448b302c325cfc245e
Jan Eirik B. Nævdal <jan.eirik.b.navdal@nav.no>
2018-12-04 22:26:43 +0100

    OPP-609: loggerfactory satt rett til kotlin-format

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/rest/oppfolging/OppfolgingController.kt
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/rest/oppfolging/OppfolgingController.kt
@@ -21,7 +21,7 @@ class OppfolgingController @Inject constructor(private val service: Oppfolgingsi
                                                private val ldapService: LDAPService,
                                                private val unleashService: UnleashService) {
 
-    private val logger = LoggerFactory.getLogger("OppfolgingController::java.class")
+    private val logger = LoggerFactory.getLogger(OppfolgingController::class.java)
 
     @GET
     @Path("/")
