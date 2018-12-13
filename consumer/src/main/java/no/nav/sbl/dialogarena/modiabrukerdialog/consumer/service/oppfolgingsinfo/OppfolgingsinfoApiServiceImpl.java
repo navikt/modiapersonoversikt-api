27f17dcdc943c573522975e3911a1abdd1ae73d9
Jan Berge Ommedal <jan.berge.ommedal@nav.no>
2018-12-13 14:42:16 +0100

    OPP-609: byttet ut apache http client for å unngå problemer med minnelekasje

--- consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/service/oppfolgingsinfo/OppfolgingsinfoApiServiceImpl.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/service/oppfolgingsinfo/OppfolgingsinfoApiServiceImpl.java
@@ -1,7 +1,6 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.consumer.service.oppfolgingsinfo;
 
 import no.nav.brukerdialog.security.context.SubjectHandler;
-import no.nav.json.JsonUtils;
 import no.nav.sbl.dialogarena.modiabrukerdialog.api.domain.Saksbehandler;
 import no.nav.sbl.dialogarena.modiabrukerdialog.api.domain.norg.AnsattEnhet;
 import no.nav.sbl.dialogarena.modiabrukerdialog.api.domain.oppfolgingsinfo.Oppfolgingsinfo;
@@ -9,52 +8,36 @@ import no.nav.sbl.dialogarena.modiabrukerdialog.api.domain.oppfolgingsinfo.rest.
 import no.nav.sbl.dialogarena.modiabrukerdialog.api.domain.oppfolgingsinfo.rest.OppfolgingsStatus;
 import no.nav.sbl.dialogarena.modiabrukerdialog.api.service.ldap.LDAPService;
 import no.nav.sbl.dialogarena.modiabrukerdialog.api.service.oppfolgingsinfo.OppfolgingsinfoApiService;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import no.nav.sbl.rest.RestUtils;
 
 import javax.inject.Inject;
-import java.io.IOException;
-import java.io.InputStream;
 
 import static javax.ws.rs.core.HttpHeaders.AUTHORIZATION;
 
 public class OppfolgingsinfoApiServiceImpl implements OppfolgingsinfoApiService {
-    private static final Logger logger = LoggerFactory.getLogger(OppfolgingsinfoApiServiceImpl.class);
+
     private String apiUrl;
-    private HttpClient client;
 
     @Inject
     public OppfolgingsinfoApiServiceImpl(String apiUrl) {
         this.apiUrl = sluttMedSlash(apiUrl);
-        this.client = lagHttpClient();
     }
+
     private String sluttMedSlash(String url) {
         return url.endsWith("/") ? url : url + "/";
     }
 
-    private HttpClient lagHttpClient() { return HttpClientBuilder.create().build(); }
-
     @Override
     public Oppfolgingsinfo hentOppfolgingsinfo(String fodselsnummer, LDAPService ldapService) {
-        Oppfolgingsinfo info = null;
-        try {
-            OppfolgingsStatus status = hentOppfolgingStatus(fodselsnummer);
-            OppfolgingsEnhetOgVeileder enhetOgVeileder = hentOppfolgingsEnhetOgVeileder(fodselsnummer);
-            info = new Oppfolgingsinfo(status.isUnderOppfolging())
-                    .withVeileder(hentSaksbehandler(enhetOgVeileder.getVeilederId(), ldapService))
-                    .withOppfolgingsenhet(new AnsattEnhet(enhetOgVeileder.getOppfolgingsenhet().getEnhetId(), enhetOgVeileder.getOppfolgingsenhet().getNavn()));
-        } catch (IOException ioe) {
-            logger.error("Feil i oppfølgingsinfo: ", ioe);
-        }
-        return info;
+        OppfolgingsStatus status = hentOppfolgingStatus(fodselsnummer);
+        OppfolgingsEnhetOgVeileder enhetOgVeileder = hentOppfolgingsEnhetOgVeileder(fodselsnummer);
+        return new Oppfolgingsinfo(status.isUnderOppfolging())
+                .withVeileder(hentSaksbehandler(enhetOgVeileder.getVeilederId(), ldapService))
+                .withOppfolgingsenhet(new AnsattEnhet(enhetOgVeileder.getOppfolgingsenhet().getEnhetId(), enhetOgVeileder.getOppfolgingsenhet().getNavn()));
     }
 
-    public void ping() throws IOException {
-        gjorSporring(hentPingURL());
+    public void ping() {
+        gjorSporring(hentPingURL(), String.class);
     }
 
     private Saksbehandler hentSaksbehandler(String veilederIdent, LDAPService ldapService) {
@@ -64,12 +47,12 @@ public class OppfolgingsinfoApiServiceImpl implements OppfolgingsinfoApiService 
         return ldapService.hentSaksbehandler(veilederIdent);
     }
 
-    private OppfolgingsEnhetOgVeileder hentOppfolgingsEnhetOgVeileder(String fodselsnummer) throws IOException {
-        return JsonUtils.fromJson(gjorSporring(hentOppfolgingsEnhetOgVeilederURL(fodselsnummer)), OppfolgingsEnhetOgVeileder.class);
+    private OppfolgingsEnhetOgVeileder hentOppfolgingsEnhetOgVeileder(String fodselsnummer) {
+        return gjorSporring(hentOppfolgingsEnhetOgVeilederURL(fodselsnummer), OppfolgingsEnhetOgVeileder.class);
     }
 
-    private OppfolgingsStatus hentOppfolgingStatus(String fodselsnummer) throws IOException {
-        return JsonUtils.fromJson(gjorSporring(hentOppfolgingsStatusURL(fodselsnummer)), OppfolgingsStatus.class);
+    private OppfolgingsStatus hentOppfolgingStatus(String fodselsnummer) {
+        return gjorSporring(hentOppfolgingsStatusURL(fodselsnummer), OppfolgingsStatus.class);
     }
 
     private String hentOppfolgingsEnhetOgVeilederURL(String fodselsnummer) {
@@ -84,15 +67,13 @@ public class OppfolgingsinfoApiServiceImpl implements OppfolgingsinfoApiService 
         return apiUrl + String.format("ping");
     }
 
-    private InputStream gjorSporring(String url) throws IOException {
-        HttpGet request = new HttpGet(url);
-        request.addHeader(AUTHORIZATION, "Bearer " + SubjectHandler.getSubjectHandler().getInternSsoToken());
-        HttpResponse response = client.execute(request);
-        int statusCode = response.getStatusLine().getStatusCode();
-        if (statusCode != 200) {
-            throw new IllegalStateException("Oppfølging svarte med statuskode: " + statusCode);
-        } else {
-            return response.getEntity().getContent();
-        }
+    private <T> T gjorSporring(String url, Class<T> targetClass) {
+        return RestUtils.withClient(client -> client
+                .target(url)
+                .request()
+                .header(AUTHORIZATION, "Bearer " + SubjectHandler.getSubjectHandler().getInternSsoToken())
+                .get(targetClass)
+        );
     }
+
 }
