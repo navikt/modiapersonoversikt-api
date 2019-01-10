83923ccbb23d5b8184d5681b8480b1131ea84e25
Jørund Amsen <jorundamsen@nav.no>
2019-01-10 12:45:32 +0100

    Legger til mulighet for mocking av foreldrepenger
    For å kunne skru av og på mocks runtime har vi løst det ved å ha to bønner som henholdsvis wrapper både mock og default tjeneste. Disse henter vi inn med qualifiers før vi unwrapper og tar i bruk typen vi ønsker.

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/rest/ytelse/YtelseController.kt
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/rest/ytelse/YtelseController.kt
@@ -1,22 +1,26 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.web.rest.ytelse
 
 import no.nav.kjerneinfo.consumer.organisasjon.OrganisasjonService
+import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.artifact.kjerneinfo.component.mockable.MockableContext.KJERNEINFO_KEY
 import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.service.unleash.Feature
 import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.service.unleash.UnleashService
+import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.util.MockUtil.mockErTillattOgSlaattPaaForKey
+import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.util.Wrapper
 import no.nav.sykmeldingsperioder.consumer.foreldrepenger.ForeldrepengerServiceBi
 import no.nav.sykmeldingsperioder.consumer.pleiepenger.PleiepengerService
 import no.nav.sykmeldingsperioder.consumer.sykepenger.SykepengerServiceBi
+import org.springframework.beans.factory.annotation.Qualifier
 import javax.inject.Inject
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
-
 @Path("/ytelse")
 @Produces(MediaType.APPLICATION_JSON)
 class YtelseController @Inject constructor(private val sykepengerService: SykepengerServiceBi,
-                                           private val forelderpengerService: ForeldrepengerServiceBi,
+                                           @Qualifier("foreldrepengerServiceDefault") private val foreldrepengerServiceDefault: Wrapper<ForeldrepengerServiceBi>,
+                                           @Qualifier("foreldrepengerServiceMock") private val foreldrepengerServiceMock: Wrapper<ForeldrepengerServiceBi>,
                                            private val pleiepengerService: PleiepengerService,
                                            private val unleashService: UnleashService,
                                            private val organisasjonService: OrganisasjonService) {
@@ -34,7 +38,7 @@ class YtelseController @Inject constructor(private val sykepengerService: Sykepe
     fun hentForeldrepenger(@PathParam("fnr") fødselsnummer: String): Map<String, Any?> {
         check(unleashService.isEnabled(Feature.NYTT_VISITTKORT))
 
-        return ForeldrepengerUttrekk(forelderpengerService).hent(fødselsnummer)
+        return ForeldrepengerUttrekk(getForeldrepengerService()).hent(fødselsnummer)
     }
 
     @GET
@@ -43,5 +47,13 @@ class YtelseController @Inject constructor(private val sykepengerService: Sykepe
         check(unleashService.isEnabled(Feature.NYTT_VISITTKORT))
 
         return PleiepengerUttrekk(pleiepengerService, organisasjonService).hent(fødselsnummer)
+    }
+
+    private fun getForeldrepengerService(): ForeldrepengerServiceBi {
+        return ForeldrepengerServiceBi { request ->
+            if (mockErTillattOgSlaattPaaForKey(KJERNEINFO_KEY)) {
+                foreldrepengerServiceMock.wrappedObject.hentForeldrepengerListe(request)
+            } else foreldrepengerServiceDefault.wrappedObject.hentForeldrepengerListe(request)
+        }
     }
 }
