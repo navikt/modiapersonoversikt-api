dd3e5887c3c8f2d5a01d949ee8307df2966385bd
Daniel Winsvold <daniel.winsvold@nav.no>
2019-01-17 12:19:39 +0100

    nytt visittkort og ny brukerprofil default på

--- consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/service/unleash/features.kt
+++ consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/service/unleash/features.kt
@@ -2,8 +2,8 @@ package no.nav.sbl.dialogarena.modiabrukerdialog.consumer.service.unleash
 
 enum class Feature private constructor(val propertyKey: String, val defaultValue: Boolean) {
     SAMPLE_FEATURE("feature.samplerfeature", false),
-    NYTT_VISITTKORT("modiabrukerdialog.nytt-visittkort", false),
-    NY_BRUKERPROFIL("modiabrukerdialog.ny-brukerprofil", false),
+    NYTT_VISITTKORT("modiabrukerdialog.nytt-visittkort", true),
+    NY_BRUKERPROFIL("modiabrukerdialog.ny-brukerprofil", true),
     SVAKSYNT_MODUS("modiabrukerdialog.svaksyntmodus", false),
     NY_UTBETALING("modiabrukerdialog.ny-utbetalinger", false),
     NY_SAKSOVERSIKT("modiabrukerdialog.ny-saksoversikt", false),
