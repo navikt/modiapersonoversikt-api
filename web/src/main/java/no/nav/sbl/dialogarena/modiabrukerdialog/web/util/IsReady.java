b69a564a5a522baaadd2f08e4a0860ce2b592a2c
Richard Borge <Richard.Borge@nav.no>
2019-02-19 13:21:25 +0100

    Tvinger bygg

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/util/IsReady.java
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/util/IsReady.java
@@ -7,6 +7,6 @@ import org.apache.wicket.markup.html.basic.Label;
 public class IsReady extends WebPage {
 
     public IsReady() {
-        this.add(new Component[]{new Label("status", "OK")});
+        this.add(new Component[]{new Label("ready", "OK")});
     }
 }
