c04d287506ce84a655e7ba61b7d6ec58d217fce6
Jørund Amsen <jorundamsen@nav.no>
2019-03-20 15:49:47 +0100

    Data-objekter som tilsvarer responsen fra SAF sin dokumentoversiktBruker
    Denne må stemme 1-1 både med navn og typer med SafQuery for at parsingen fra Json skal fungere

---
+++ saksoversikt/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/sak/service/saf/SafSakerResponse.kt
@@ -0,0 +1,69 @@
+package no.nav.sbl.dialogarena.modiabrukerdialog.sak.service.saf
+
+import java.time.LocalDateTime
+
+
+data class SAFSakerResponse(val data: Data?, val errors: List<SafError>?)
+
+data class Data(val dokumentoversiktBruker: DokumentoversiktBruker?)
+
+data class DokumentoversiktBruker(val journalposter: List<Journalpost>?)
+
+data class Journalpost(
+        val avsenderMottakerNavn: String?,
+        val avsenderMottakerId: String?,
+        val bruker: Bruker?,
+        val dokumenter: List<DokumentInfo>?,
+        val datoOpprettet: LocalDateTime,
+        val journalpostId: String,
+        val journalposttype: String?,
+        val journalstatus: String?,
+        val relevanteDatoer: List<RelevantDato>?,
+        val sak: Sak?,
+        val tema: String?,
+        val temanavn: String?,
+        val tittel: String?
+)
+
+data class Bruker(
+        val id: String?,
+        val type: String?
+)
+
+data class DokumentInfo(
+        val dokumentInfoId: String,
+        val dokumentvarianter: List<Dokumentvariant>,
+        val logiskeVedlegg: List<LogiskVedlegg>,
+        val tittel: String?
+)
+
+data class Dokumentvariant(
+        var saksbehandlerHarTilgang: Boolean?,
+        var variantformat: String?
+)
+
+data class LogiskVedlegg(
+        val tittel: String?
+)
+
+data class RelevantDato(
+        val dato: LocalDateTime,
+        val datotype: String
+)
+
+data class Sak(
+        val arkivsaksnummer: String?,
+        val arkivsaksystem: String?,
+        val fagsakId: String?,
+        val fagsaksystem: String?
+)
+
+data class SafError(
+        val message: String?,
+        val locations: List<Location>?
+)
+
+data class Location(
+        val line: Number?,
+        val column: Number?
+)
