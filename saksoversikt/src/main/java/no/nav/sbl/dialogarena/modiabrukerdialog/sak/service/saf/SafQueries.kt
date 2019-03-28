0c1a8b848606222b705bd8449ed661dcb0bb1690
Jørund Amsen <jorundamsen@nav.no>
2019-03-28 16:23:13 +0100

    Tar i bruk string literals for GraphQL-query for lesbarhet og lettere å endre
    Må fjerne \n, da dette er noe Kotlin legger til i string literals, men Saf klager.

--- saksoversikt/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/sak/service/saf/SafQueries.kt
+++ saksoversikt/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/sak/service/saf/SafQueries.kt
@@ -1,50 +1,55 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.sak.service.saf
 
-const val dokumentoversiktBrukerGraphQLQuery = "query {" +
-        "  dokumentoversiktBruker(brukerId: {id: \\\"[FNR]\\\", type: FNR}, journalstatuser: [JOURNALFOERT, FERDIGSTILT, EKSPEDERT], foerste: 9999) {" +
-        "    journalposter {" +
-        "      journalposttype" +
-        "      datoOpprettet" +
-        "      tittel" +
-        "      journalpostId" +
-        "      journalstatus" +
-        "      dokumenter {" +
-        "        tittel" +
-        "        dokumentInfoId" +
-        "        dokumentvarianter{" +
-        "          variantformat" +
-        "          saksbehandlerHarTilgang" +
-        "        }" +
-        "        logiskeVedlegg {" +
-        "          tittel" +
-        "        }" +
-        "      }" +
-        "      sak{" +
-        "        fagsakId" +
-        "        fagsaksystem" +
-        "        arkivsaksystem" +
-        "        arkivsaksnummer" +
-        "      }" +
-        "      tema" +
-        "      temanavn" +
-        "      journalstatus" +
-        "      avsenderMottaker {" +
-        "        erLikBruker" +
-        "        navn" +
-        "      }" +
-        "      bruker{" +
-        "        id" +
-        "        type" +
-        "      }" +
-        "      relevanteDatoer{" +
-        "        dato" +
-        "        datotype" +
-        "      }" +
-        "    }" +
-        "  }" +
-        "}"
+val dokumentoversiktBrukerGraphQLQuery = """
+    query {
+          dokumentoversiktBruker(brukerId: {id: \"[FNR]\", type: FNR}, journalstatuser: [JOURNALFOERT, FERDIGSTILT, EKSPEDERT], foerste: 9999) {
+            journalposter {
+              journalposttype
+              datoOpprettet
+              tittel
+              journalpostId
+              journalstatus
+              dokumenter {
+                tittel
+                dokumentInfoId
+                dokumentvarianter{
+                  variantformat
+                  saksbehandlerHarTilgang
+                }
+                logiskeVedlegg {
+                  tittel
+                }
+              }
+              sak{
+                fagsakId
+                fagsaksystem
+                arkivsaksystem
+                arkivsaksnummer
+              }
+              tema
+              temanavn
+              journalstatus
+              avsenderMottaker {
+                erLikBruker
+                navn
+              }
+              bruker{
+                id
+                type
+              }
+              relevanteDatoer{
+                dato
+                datotype
+              }
+            }
+          }
+        }
+""".trimIndent()
 
 fun dokumentoversiktBrukerJsonQuery(fnr: String): String {
-    val medFnr = dokumentoversiktBrukerGraphQLQuery.replace("[FNR]", fnr)
-    return "{\"query\":\"$medFnr\"}"
+    val medFnrOgFjernLinebreak = dokumentoversiktBrukerGraphQLQuery
+            .replace("[FNR]", fnr)
+            .replace("\n", "")
+    val medJsonQueryParameter = "{\"query\":\"$medFnrOgFjernLinebreak\"}"
+    return medJsonQueryParameter
 }
