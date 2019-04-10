bf03cd42ee48c6f9abaffba543505d6d2f2b1a49
Jan Eirik B. Nævdal <jan.eirik.b.navdal@nav.no>
2019-04-10 10:09:48 +0200

    Opp-670 Foreldrepengestying-rettelser

--- reactkomponenter/src/main/resources/no/nav/sbl/dialogarena/modiabrukerdialog/reactkomponenter/ny-foreldrepenger/ny-foreldrepenger.js
+++ reactkomponenter/src/main/resources/no/nav/sbl/dialogarena/modiabrukerdialog/reactkomponenter/ny-foreldrepenger/ny-foreldrepenger.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import PT from 'prop-types';
 
-import ForeldrepengerLamell from 'modiapersonoversikt/build/dist/components/standalone/Foreldrepenger/ForeldrepengerLamell';
+import ForeldrepengerLamell
+    from 'modiapersonoversikt/build/dist/components/standalone/Foreldrepenger/ForeldrepengerLamell';
 
 class NyForeldrepenger extends React.Component {
     render() {
         return (
-            <div className="ny-frontend">
-            <ForeldrepengerLamell fødselsnummer={this.props.fødselsnummer} />
-        </div>
-    );
+            <ForeldrepengerLamell fødselsnummer={this.props.fødselsnummer}/>
+        );
     }
 }
 
