8ad18dd623a5fbe2e55f78707bfd1d6996d1f91b
Richard Borge <Richard.Borge@nav.no>
2019-04-26 14:20:26 +0200

    Endrer gosys lenke med bruker i kontekst

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/tempnaisgosys/GosysNaisLenke.java
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/tempnaisgosys/GosysNaisLenke.java
@@ -4,9 +4,10 @@ import org.apache.wicket.markup.html.link.ExternalLink;
 import org.apache.wicket.markup.html.panel.Panel;
 
 public class GosysNaisLenke extends Panel {
-    public GosysNaisLenke(String id) {
+    public GosysNaisLenke(String id, String fnr) {
         super(id);
-        add(new ExternalLink("naisGosys", hentUrl()));
+        String url = hentUrl() + "/personoversikt/fnr=" + fnr;
+        add(new ExternalLink("naisGosys", url));
     }
 
     private String hentUrl() {
