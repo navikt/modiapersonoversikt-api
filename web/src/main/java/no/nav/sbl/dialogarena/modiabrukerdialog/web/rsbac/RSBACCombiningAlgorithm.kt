c20af7c1e6cccba461d42e88f39639684ee57968
Nicklas Utgaard <nicklas.utgaard@nav.no>
2019-09-03 16:25:58 +0200

    [OPP-866] Egen implementer tilgangskontroll

---
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/rsbac/RSBACCombiningAlgorithm.kt
@@ -0,0 +1,51 @@
+package no.nav.sbl.dialogarena.modiabrukerdialog.web.rsbac
+
+interface Combinable<CONTEXT> : Function<CONTEXT, DecisionEnums> {
+    fun getMessage(): String
+    override fun invoke(context: CONTEXT): DecisionEnums
+}
+
+abstract class CombiningAlgo {
+    abstract fun <CONTEXT> combine(policies: List<Combinable<CONTEXT>>, context: CONTEXT): Decision
+
+    companion object {
+        @JvmField val denyOverride: CombiningAlgo = DenyOverride()
+        @JvmField val firstApplicable: CombiningAlgo = FirstApplicable()
+    }
+}
+
+// Inspirert av https://www.axiomatics.com/blog/understanding-xacml-combining-algorithms/
+private class DenyOverride : CombiningAlgo() {
+    override fun <CONTEXT> combine(policies: List<Combinable<CONTEXT>>, context: CONTEXT): Decision {
+        var combinedDecision = Decision("No matching rule found", DecisionEnums.NOT_APPLICABLE)
+        for (policy: Combinable<CONTEXT> in policies) {
+            val ruleDecision = policy.invoke(context)
+
+            combinedDecision = when (ruleDecision) {
+                DecisionEnums.DENY -> return Decision(policy.getMessage(), ruleDecision)
+                DecisionEnums.NOT_APPLICABLE -> combinedDecision
+                else -> Decision(policy.getMessage(), ruleDecision)
+            }
+        }
+        return combinedDecision
+    }
+}
+
+private class FirstApplicable : CombiningAlgo() {
+    override fun <CONTEXT> combine(policies: List<Combinable<CONTEXT>>, context: CONTEXT): Decision {
+        var combinedDecision = Decision("No matching rule found", DecisionEnums.NOT_APPLICABLE)
+        for (policy: Combinable<CONTEXT> in policies) {
+            val ruleDecision = policy.invoke(context)
+
+            if (ruleDecision.isApplicable()) {
+                return Decision(policy.getMessage(), ruleDecision)
+            }
+
+            combinedDecision = when (combinedDecision.decision) {
+                DecisionEnums.DENY, DecisionEnums.PERMIT -> combinedDecision
+                DecisionEnums.NOT_APPLICABLE -> Decision(policy.getMessage(), ruleDecision)
+            }
+        }
+        return combinedDecision
+    }
+}
