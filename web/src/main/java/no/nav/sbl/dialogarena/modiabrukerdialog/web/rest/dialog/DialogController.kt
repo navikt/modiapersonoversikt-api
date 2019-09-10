f89205c4695698c6e2df66be604c53f0014b40d0
Nicklas Utgaard <nicklas.utgaard@nav.no>
2019-09-10 13:25:28 +0200

    [OPP-915] endepunkt for fortsett dialog

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/rest/dialog/DialogController.kt
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/rest/dialog/DialogController.kt
@@ -1,149 +1,154 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.web.rest.dialog
 
 import no.nav.brukerdialog.security.context.SubjectHandler
-import no.nav.sbl.dialogarena.modiabrukerdialog.api.domain.Person
-import no.nav.sbl.dialogarena.modiabrukerdialog.api.domain.Saksbehandler
 import no.nav.sbl.dialogarena.modiabrukerdialog.api.domain.henvendelse.Fritekst
 import no.nav.sbl.dialogarena.modiabrukerdialog.api.domain.henvendelse.Melding
 import no.nav.sbl.dialogarena.modiabrukerdialog.api.domain.henvendelse.Meldingstype
 import no.nav.sbl.dialogarena.modiabrukerdialog.api.service.HenvendelseUtsendingService
 import no.nav.sbl.dialogarena.modiabrukerdialog.api.service.gsak.SakerService
-import no.nav.sbl.dialogarena.modiabrukerdialog.api.service.norg.AnsattService
 import no.nav.sbl.dialogarena.modiabrukerdialog.api.utils.RestUtils
-import no.nav.sbl.dialogarena.modiabrukerdialog.api.utils.RestUtils.hentValgtEnhet
 import no.nav.sbl.dialogarena.modiabrukerdialog.api.utils.TemagruppeTemaMapping.hentTemagruppeForTema
-import no.nav.sbl.dialogarena.modiabrukerdialog.web.rest.DATO_TID_FORMAT
+import no.nav.sbl.dialogarena.modiabrukerdialog.web.rest.api.DTO
+import no.nav.sbl.dialogarena.modiabrukerdialog.web.rest.api.toDTO
 import no.nav.sbl.dialogarena.sporsmalogsvar.consumer.henvendelse.HenvendelseBehandlingService
 import no.nav.sbl.dialogarena.sporsmalogsvar.consumer.henvendelse.domain.Traad
-import org.joda.time.format.DateTimeFormat
 import java.util.*
 import javax.inject.Inject
 import javax.servlet.http.HttpServletRequest
-import javax.ws.rs.GET
-import javax.ws.rs.POST
-import javax.ws.rs.Path
-import javax.ws.rs.PathParam
+import javax.ws.rs.*
 import javax.ws.rs.core.Context
 import javax.ws.rs.core.Response
 
+data class TraadDTO(val traadId: String, val meldinger: List<MeldingDTO>) : DTO
+class MeldingDTO(val map: Map<String, Any?>) : HashMap<String, Any?>(map), DTO
+
 @Path("/dialog/{fnr}")
-class DialogController @Inject constructor(private val ansattService: AnsattService,
-                                           private val henvendelseService: HenvendelseBehandlingService,
-                                           private val henvendelseUtsendingService: HenvendelseUtsendingService,
-                                           private val sakerService: SakerService
+class DialogController @Inject constructor(
+        private val henvendelseService: HenvendelseBehandlingService,
+        private val henvendelseUtsendingService: HenvendelseUtsendingService,
+        private val sakerService: SakerService
 ) {
     @GET
     @Path("/meldinger")
     fun hentMeldinger(@Context request: HttpServletRequest,
-                      @PathParam("fnr") fødselsnummer: String): Response {
-        val valgtEnhet = hentValgtEnhet(request)
-        if (ansattService.hentEnhetsliste().map { it.enhetId }.contains(valgtEnhet)) {
-            return Response.ok(hentTråder(henvendelseService.hentMeldinger(fødselsnummer, valgtEnhet).traader))
-                    .build()
-        } else {
-            return Response.status(Response.Status.UNAUTHORIZED).build()
-        }
+                      @PathParam("fnr") fnr: String): List<TraadDTO> {
+        // TODO tilgangsstyring
+        val valgtEnhet = RestUtils.hentValgtEnhet(request)
+        return henvendelseService
+                .hentMeldinger(fnr, valgtEnhet)
+                .traader
+                .toDTO()
     }
-
-    private fun hentTråder(tråder: List<Traad>): List<Map<String, Any?>> {
-        return tråder.map {
-            mapOf(
-                    "traadId" to it.traadId,
-                    "meldinger" to hentMeldinger(it.meldinger)
-            )
-        }
-    }
-
-    private fun hentMeldinger(meldinger: List<Melding>): List<Map<String, Any?>> {
-        return meldinger.map {
-            mapOf(
-                    "id" to it.id,
-                    "oppgaveId" to it.oppgaveId,
-                    "meldingstype" to it.meldingstype?.name,
-                    "temagruppe" to it.gjeldendeTemagruppe?.name,
-                    "skrevetAv" to it.skrevetAv?.let(this::hentPerson),
-                    "journalfortAv" to it.journalfortAv?.let(this::hentPerson),
-                    "journalfortDato" to it.journalfortDato?.toString(DateTimeFormat.forPattern(DATO_TID_FORMAT)),
-                    "journalfortTema" to it.journalfortTema,
-                    "journalfortTemanavn" to it.journalfortTemanavn,
-                    "journalfortSaksid" to it.journalfortSaksId,
-                    "fritekst" to it.fritekst,
-                    "lestDato" to it.lestDato?.toString(DateTimeFormat.forPattern(DATO_TID_FORMAT)),
-                    "status" to it.status?.name,
-                    "opprettetDato" to it.opprettetDato?.toString(DateTimeFormat.forPattern(DATO_TID_FORMAT)),
-                    "ferdigstiltDato" to it.ferdigstiltDato?.toString(DateTimeFormat.forPattern(DATO_TID_FORMAT)),
-                    "erFerdigstiltUtenSvar" to it.erFerdigstiltUtenSvar,
-                    "ferdigstiltUtenSvarAv" to it.ferdigstiltUtenSvarAv?.let(this::hentSaksbehandler),
-                    "kontorsperretEnhet" to it.kontorsperretEnhet,
-                    "kontorsperretAv" to it.kontorsperretAv?.let(this::hentSaksbehandler),
-                    "markertSomFeilsendtAv" to it.markertSomFeilsendtAv?.let(this::hentSaksbehandler),
-                    "erDokumentMelding" to it.erDokumentMelding
-            )
-        }
-    }
-
-    private fun hentPerson(person: Person): Map<String, Any?> =
-            mapOf(
-                    "fornavn" to person.fornavn,
-                    "etternavn" to person.etternavn
-            )
-
-    private fun hentSaksbehandler(saksbehandler: Saksbehandler): Map<String, Any?> =
-            mapOf(
-                    "fornavn" to saksbehandler.fornavn,
-                    "etternavn" to saksbehandler.etternavn,
-                    "ident" to saksbehandler.ident
-            )
 
     @POST
     @Path("/sendreferat")
-    fun sendMelding(@Context request: HttpServletRequest,
-                    @PathParam("fnr") fødselsnummer: String,
-                    referatRequest: SendReferatRequest): Response {
-        val context = lagSendHenvendelseContext(fødselsnummer, request)
+    fun sendMelding(
+            @Context request: HttpServletRequest,
+            @PathParam("fnr") fnr: String, referatRequest: SendReferatRequest
+    ): Response {
+        // TODO tilgangsstyring
+        val context = lagSendHenvendelseContext(fnr, request)
         henvendelseUtsendingService.sendHenvendelse(lagReferat(referatRequest, context), Optional.empty(), Optional.empty(), context.enhet)
         return Response.ok().build()
     }
 
-    private fun lagReferat(referatRequest: SendReferatRequest, context: no.nav.sbl.dialogarena.modiabrukerdialog.web.rest.dialog.Context): Melding {
-        return Melding().withFnr(context.fnr)
-                .withNavIdent(context.ident)
-                .withEksternAktor(context.ident)
-                .withKanal(referatRequest.kanal.name)
-                .withType(Meldingstype.valueOf("SAMTALEREFERAT_" + referatRequest.kanal))
-                .withFritekst(Fritekst(referatRequest.fritekst))
-                .withTilknyttetEnhet(context.enhet)
-                .withErTilknyttetAnsatt(true)
-                .withTemagruppe(referatRequest.temagruppe)
-    }
-
     @POST
     @Path("/sendsporsmal")
-    fun sendSporsmal(@Context request: HttpServletRequest,
-                     @PathParam("fnr") fødselsnummer: String,
-                     sporsmalsRequest: SendSporsmalRequest): Response {
-        val context: no.nav.sbl.dialogarena.modiabrukerdialog.web.rest.dialog.Context = lagSendHenvendelseContext(fødselsnummer, request);
-        val gsakSaker = sakerService.hentSammensatteSaker(fødselsnummer)
-        val psakSaker = sakerService.hentPensjonSaker(fødselsnummer)
+    fun sendSporsmal(
+            @Context request: HttpServletRequest,
+            @PathParam("fnr") fnr: String,
+            sporsmalsRequest: SendSporsmalRequest
+    ): Response {
+        // TODO tilgangsstyring
+        val context = lagSendHenvendelseContext(fnr, request)
+        val gsakSaker = sakerService.hentSammensatteSaker(fnr)
+        val psakSaker = sakerService.hentPensjonSaker(fnr)
         val saker = gsakSaker.union(psakSaker)
 
-        val valgtSak = saker.find { it.saksId == sporsmalsRequest.saksID }
-        require(valgtSak != null)
+        val valgtSak = saker
+                .find { it.saksId == sporsmalsRequest.saksID }
+                ?: throw WebApplicationException("Fant ingen sak med id: ${sporsmalsRequest.saksID}", 400)
+
         henvendelseUtsendingService.sendHenvendelse(lagSporsmal(sporsmalsRequest, valgtSak.temaKode, context), Optional.empty(), Optional.of(valgtSak), context.enhet)
         return Response.ok().build()
     }
 
-    private fun lagSporsmal(sporsmalRequest: SendSporsmalRequest, sakstema: String, context: no.nav.sbl.dialogarena.modiabrukerdialog.web.rest.dialog.Context): Melding {
-        return Melding().withFnr(context.fnr)
-                .withNavIdent(context.ident)
-                .withEksternAktor(context.ident)
-                .withKanal("TEKST")
-                .withType(Meldingstype.SPORSMAL_MODIA_UTGAAENDE)
-                .withFritekst(Fritekst(sporsmalRequest.fritekst))
-                .withTilknyttetEnhet(context.enhet)
-                .withErTilknyttetAnsatt(sporsmalRequest.erOppgaveTilknyttetAnsatt)
-                .withTemagruppe(hentTemagruppeForTema(sakstema))
+    @POST
+    @Path("/fortsett")
+    fun fortsettDialog(
+            @Context request: HttpServletRequest,
+            @PathParam("fnr") fnr: String,
+            fortsettDialogRequest: FortsettDialogRequest
+    ) {
+        // TODO tilgangsstyring
+        val context = lagSendHenvendelseContext(fnr, request)
+        val traad = henvendelseService
+                .hentMeldinger(fnr, context.enhet)
+                .traader
+                .find { it.traadId == fortsettDialogRequest.traadId }
+                ?: throw WebApplicationException("Fant ingen tråd med id: ${fortsettDialogRequest.traadId}", 400)
+
+        val valgtSak = fortsettDialogRequest.saksId
+                ?.let {
+                    val gsakSaker = sakerService.hentSammensatteSaker(fnr)
+                    val psakSaker = sakerService.hentPensjonSaker(fnr)
+                    val saker = gsakSaker.union(psakSaker)
+
+                    saker
+                            .find { it.saksId == fortsettDialogRequest.saksId }
+                            ?: throw WebApplicationException("Fant ingen sak med id: ${fortsettDialogRequest.saksId}", 400)
+                }
+
+        henvendelseUtsendingService.ferdigstillHenvendelse(
+                lagFortsettDialog(fortsettDialogRequest, context, traad),
+                Optional.ofNullable(fortsettDialogRequest.oppgaveId),
+                Optional.ofNullable(valgtSak),
+                fortsettDialogRequest.behandlingsId,
+                context.enhet
+        )
     }
+}
+
+private fun lagReferat(referatRequest: SendReferatRequest, requestContext: RequestContext): Melding {
+    return Melding().withFnr(requestContext.fnr)
+            .withNavIdent(requestContext.ident)
+            .withEksternAktor(requestContext.ident)
+            .withKanal(referatRequest.kanal.name)
+            .withType(Meldingstype.valueOf("SAMTALEREFERAT_" + referatRequest.kanal))
+            .withFritekst(Fritekst(referatRequest.fritekst))
+            .withTilknyttetEnhet(requestContext.enhet)
+            .withErTilknyttetAnsatt(true)
+            .withTemagruppe(referatRequest.temagruppe)
+}
+
+private fun lagSporsmal(sporsmalRequest: SendSporsmalRequest, sakstema: String, requestContext: RequestContext): Melding {
+    return Melding().withFnr(requestContext.fnr)
+            .withNavIdent(requestContext.ident)
+            .withEksternAktor(requestContext.ident)
+            .withKanal(Kanal.TEKST.name)
+            .withType(Meldingstype.SPORSMAL_MODIA_UTGAAENDE)
+            .withFritekst(Fritekst(sporsmalRequest.fritekst))
+            .withTilknyttetEnhet(requestContext.enhet)
+            .withErTilknyttetAnsatt(sporsmalRequest.erOppgaveTilknyttetAnsatt)
+            .withTemagruppe(hentTemagruppeForTema(sakstema))
+}
+
+private fun lagFortsettDialog(request: FortsettDialogRequest, requestContext: RequestContext, traad: Traad): Melding {
+    val eldsteMelding = traad.meldinger[0]
+    return Melding()
+            .withFnr(requestContext.fnr)
+            .withNavIdent(requestContext.ident)
+            .withEksternAktor(requestContext.ident)
+            .withKanal(request.kanal.name)
+            .withType(meldingstype(request.kanal, request.kanBesvares))
+            .withFritekst(Fritekst(request.fritekst))
+            .withTilknyttetEnhet(requestContext.enhet)
+            .withErTilknyttetAnsatt(request.erOppgaveTilknyttetAnsatt)
+            .withTraadId(request.traadId)
+            .withKontorsperretEnhet(eldsteMelding.kontorsperretEnhet)
+            .withTemagruppe(eldsteMelding.temagruppe)
+            .withBrukersEnhet(eldsteMelding.brukersEnhet)
+
 }
 
 data class SendReferatRequest(
@@ -158,22 +163,44 @@ data class SendSporsmalRequest(
         val erOppgaveTilknyttetAnsatt: Boolean
 )
 
+data class FortsettDialogRequest(
+        val traadId: String,
+        val behandlingsId: String,
+        val fritekst: String,
+        val saksId: String?,
+        val erOppgaveTilknyttetAnsatt: Boolean,
+        val kanal: Kanal,
+        val kanBesvares: Boolean,
+        val oppgaveId: String?
+)
+
 enum class Kanal {
+    TEKST,
     OPPMOTE,
     TELEFON
 }
 
-fun lagSendHenvendelseContext(fnr: String, request: HttpServletRequest): no.nav.sbl.dialogarena.modiabrukerdialog.web.rest.dialog.Context {
+private fun meldingstype(kanal: Kanal, kanBesvares: Boolean): Meldingstype {
+    return if (kanBesvares && kanal == Kanal.TEKST)
+        Meldingstype.SPORSMAL_MODIA_UTGAAENDE
+    else
+        when (kanal) {
+            Kanal.TEKST -> Meldingstype.SVAR_SKRIFTLIG
+            Kanal.OPPMOTE -> Meldingstype.SVAR_OPPMOTE
+            Kanal.TELEFON -> Meldingstype.SVAR_TELEFON
+        }
+}
+
+fun lagSendHenvendelseContext(fnr: String, request: HttpServletRequest): RequestContext {
     val ident = SubjectHandler.getSubjectHandler().uid
     val enhet = RestUtils.hentValgtEnhet(request)
 
-    require(fnr != null)
     require(ident != null)
     require(enhet != null)
-    return Context(fnr, ident, enhet)
+    return RequestContext(fnr, ident, enhet)
 }
 
-data class Context(
+data class RequestContext(
         val fnr: String,
         val ident: String,
         val enhet: String
