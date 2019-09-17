ab803890387162c31ade3ac6a84f038ab7570231
Richard Borge <Richard.Borge@nav.no>
2019-09-17 13:53:38 +0200

    Fjernet unused import

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/rest/person/PersonsokController.kt
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/rest/person/PersonsokController.kt
@@ -16,7 +16,6 @@ import javax.ws.rs.POST
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
-import kotlin.reflect.jvm.internal.impl.protobuf.Internal
 
 @Path("/personsok")
 @Produces(MediaType.APPLICATION_JSON)
