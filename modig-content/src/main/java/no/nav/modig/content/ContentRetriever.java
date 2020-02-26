ed09b9a56a7cbccbd84e46e518f7aef6d56ef1da
Ankur Tade <Ankur.Tade@nav.no>
2020-02-26 14:13:44 +0100

    Introdusert default ResourceBundle for å lese norsk bokstaver.

--- modig-content/src/main/java/no/nav/modig/content/ContentRetriever.java
+++ modig-content/src/main/java/no/nav/modig/content/ContentRetriever.java
@@ -1,35 +1,25 @@
 package no.nav.modig.content;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Properties;
+import java.util.ResourceBundle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.*;
-import java.util.stream.Collectors;
-
 public class ContentRetriever {
-    private static Logger logger = LoggerFactory.getLogger(ContentRetriever.class);
+    private final static Logger logger = LoggerFactory.getLogger(ContentRetriever.class);
     private Map<String, String> tekster = new HashMap<>();
 
     public ContentRetriever load(String... resourceBundles) {
         for (String resourceBundleNavn : resourceBundles) {
-            Map<String, String> bundleTekster = getAll(ResourceBundle.getBundle(resourceBundleNavn, new Locale("nb"), new UTF8Control()));
+            Map<String, String> bundleTekster = getAll(ResourceBundle.getBundle(resourceBundleNavn, new Locale("nb")));
             tekster.putAll(bundleTekster);
         }
-
-        return this;
-    }
-
-    public ContentRetriever load(Map<String, String> tekster) {
-        this.tekster = tekster
-                .entrySet()
-                .stream()
-                .collect(Collectors.toMap(Map.Entry::getKey, entry -> ParagraphRemover.remove(entry.getValue())));
 
         return this;
     }
@@ -61,10 +51,6 @@ public class ContentRetriever {
         return tekster.get(key);
     }
 
-    public String getDefaultLocale() {
-        return "nb";
-    }
-
     private static Map<String, String> getAll(ResourceBundle bundle) {
         Map<String, String> map = new HashMap<>();
         for (String key : Collections.list(bundle.getKeys())) {
@@ -73,41 +59,4 @@ public class ContentRetriever {
         return map;
     }
 
-
-    /**
-     * A modification of the default implementation ResourceBundle.Control that uses an InputStream with UTF-8 charset. Ripped from
-     * http://stackoverflow.com/questions/4659929/how-to-use-utf-8-in-resource-properties-with-resourcebundle
-     */
-    private static class UTF8Control extends ResourceBundle.Control {
-        public ResourceBundle newBundle
-                (String baseName, Locale locale, String format, ClassLoader loader, boolean reload)
-                throws IllegalAccessException, InstantiationException, IOException {
-            // The below is a copy of the default implementation.
-            String bundleName = toBundleName(baseName, locale);
-            String resourceName = toResourceName(bundleName, "properties");
-            ResourceBundle bundle = null;
-            InputStream stream = null;
-            if (reload) {
-                URL url = loader.getResource(resourceName);
-                if (url != null) {
-                    URLConnection connection = url.openConnection();
-                    if (connection != null) {
-                        connection.setUseCaches(false);
-                        stream = connection.getInputStream();
-                    }
-                }
-            } else {
-                stream = loader.getResourceAsStream(resourceName);
-            }
-            if (stream != null) {
-                try {
-                    // Only this line is changed to make it to read properties files as UTF-8.
-                    bundle = new PropertyResourceBundle(new InputStreamReader(stream, "UTF-8"));
-                } finally {
-                    stream.close();
-                }
-            }
-            return bundle;
-        }
-    }
 }
