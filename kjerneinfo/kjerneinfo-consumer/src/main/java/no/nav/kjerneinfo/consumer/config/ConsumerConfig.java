8abcd2d36bf4bbc98e1fd46998b31e0ba3f7bc14
Ankur Tade <Ankur.Tade@nav.no>
2020-03-10 14:03:20 +0100

    Slettet brukeprofile code

--- kjerneinfo/kjerneinfo-consumer/src/main/java/no/nav/kjerneinfo/consumer/config/ConsumerConfig.java
+++ kjerneinfo/kjerneinfo-consumer/src/main/java/no/nav/kjerneinfo/consumer/config/ConsumerConfig.java
@@ -1,13 +1,11 @@
 package no.nav.kjerneinfo.consumer.config;
 
-import no.nav.kjerneinfo.consumer.fim.behandleperson.config.BehandlePersonEndpointConfig;
 import no.nav.kjerneinfo.consumer.fim.person.config.PersonKjerneinfoMapperConfig;
-import no.nav.kjerneinfo.consumer.fim.person.config.PersonV3EndpointConfig;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({PersonV3EndpointConfig.class, PersonKjerneinfoMapperConfig.class, BehandlePersonEndpointConfig.class})
+@Import({PersonKjerneinfoMapperConfig.class})
 public class ConsumerConfig {
 
 }
