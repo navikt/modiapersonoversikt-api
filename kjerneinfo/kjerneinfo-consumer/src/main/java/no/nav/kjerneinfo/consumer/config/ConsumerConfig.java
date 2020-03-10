8214a6aa89b171bfab04e68b4fadd52ffbc6b65e
Ankur Tade <Ankur.Tade@nav.no>
2020-03-10 16:27:51 +0100

    PR kommentarer

--- kjerneinfo/kjerneinfo-consumer/src/main/java/no/nav/kjerneinfo/consumer/config/ConsumerConfig.java
+++ kjerneinfo/kjerneinfo-consumer/src/main/java/no/nav/kjerneinfo/consumer/config/ConsumerConfig.java
@@ -1,11 +1,12 @@
 package no.nav.kjerneinfo.consumer.config;
 
 import no.nav.kjerneinfo.consumer.fim.person.config.PersonKjerneinfoMapperConfig;
+import no.nav.kjerneinfo.consumer.fim.person.config.PersonV3EndpointConfig;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({PersonKjerneinfoMapperConfig.class})
+@Import({PersonV3EndpointConfig.class, PersonKjerneinfoMapperConfig.class})
 public class ConsumerConfig {
 
 }
