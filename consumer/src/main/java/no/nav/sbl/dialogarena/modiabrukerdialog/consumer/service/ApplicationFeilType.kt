f73de24edd4ab18d41ea0d499c8652cb30237faa
ANKUR TADE <38241994+tadeankur@users.noreply.github.com>
2020-06-15 15:13:46 +0200

    Opp 1172 henvendelse flere ganger (#75)
    * prøver å gjenskape feil med flere henvendelser med samme innehold
    
    added finally
    
    Deploy bare på q0
    
    Deploy bare på q0
    
    Rullet tilbake nais filer
    
    Deploy direkt til q0
    
    Update main.yml
    
    remove check for dev and prod branch
    
    Håendtere feil ved knytte saken
    
    Slettet Thread.sleep
    
    Reverted endringer som deployer applikasjon bare på q0
    
    Introdusert Felles exception mapping class
    
    * Leverer feiltype istende for feilmelding fra backend
    
    * Introdusert throwable for lage feilmelding i kibana
    
    Co-authored-by: Nicklas Utgaard <nutgaard@gmail.com>
    Co-authored-by: Andreas Bergman <11627538+olofab@users.noreply.github.com>
    Co-authored-by: Jan-Eirik Nævdal <jan.eirik.b.navdal@nav.no>

---
+++ consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/service/ApplicationFeilType.kt
@@ -0,0 +1,19 @@
+package no.nav.sbl.dialogarena.modiabrukerdialog.consumer.service
+
+import no.nav.apiapp.feil.Feil
+import javax.ws.rs.core.Response
+
+enum class ApplikasjonsFeilType(private val status: Response.Status) : Feil.Type {
+    JOURNALFORING_FEILET(Response.Status.INTERNAL_SERVER_ERROR);
+
+    override fun getName(): String {
+        return name
+    }
+
+    override fun getStatus(): Response.Status {
+        return status
+    }
+
+}
+
+internal class JournalforingFeiletException(t: Throwable) : Feil(ApplikasjonsFeilType.JOURNALFORING_FEILET, t)
