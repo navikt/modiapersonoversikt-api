e8b57f90445e6d6d882d1c684a637cdde5832f61
Jan Eirik B. Nævdal <jan.eirik.b.navdal@nav.no>
2020-06-24 12:35:09 +0200

    [OPP-1162] PDL-query historisk trengs ikkje.

--- consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/service/pdl/PdlOppslagServiceImpl.kt
+++ consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/service/pdl/PdlOppslagServiceImpl.kt
@@ -43,7 +43,7 @@ class PdlOppslagServiceImpl : PdlOppslagService {
     override fun hentIdent(fnr: String, identtype: String): PdlIdentResponse? {
         val query = this::class.java.getResource("/pdl/hentIdent.graphql").readText().replace("[\n\r]", "")
         val pdlFnr = PdlSyntetiskFnrMapper.mapTilPdl(fnr)
-        return graphqlIdentRequest(PdlIdentRequest(query, IdentVariables(pdlFnr, identtype)))
+        return graphqlIdentRequest(PdlIdentRequest(query, IdentVariables(pdlFnr)))
     }
 
     private fun graphqlIdentRequest(identRequest: PdlIdentRequest): PdlIdentResponse? {
