d3cb4715da9e4de6876f15b9ab276d579744f3b9
Nicklas Utgaard <nutgaard@gmail.com>
2020-07-14 11:29:42 +0200

    [KAIZEN-0] slette ubrukt konstanter

--- api/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/api/constants/SessionParametere.java
+++
@@ -1,9 +0,0 @@
-package no.nav.sbl.dialogarena.modiabrukerdialog.api.constants;
-
-public class SessionParametere {
-    public static class SporsmalOgSvar {
-        private static final String PREFIX = "sporsmalogsvar.";
-
-        public static final String BESVARMODUS = PREFIX + "modus.besvarer";
-    }
-}
