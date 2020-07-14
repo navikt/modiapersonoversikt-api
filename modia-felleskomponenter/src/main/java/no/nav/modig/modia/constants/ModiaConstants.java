f594b6b9692b320fd1326e8130657def439bff36
Nicklas Utgaard <nutgaard@gmail.com>
2020-07-14 11:39:57 +0200

    [KAIZEN-0] slette ubrukte filer

--- modia-felleskomponenter/src/main/java/no/nav/modig/modia/constants/ModiaConstants.java
+++
@@ -1,8 +0,0 @@
-package no.nav.modig.modia.constants;
-
-/**
- * A collection of constants to be used among panels.
- */
-public class ModiaConstants {
-	public static final String HENT_PERSON_BEGRUNNET = "modia.global.hent.person.begrunnet";
-}
