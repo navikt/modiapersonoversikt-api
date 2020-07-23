7e9acd7e00c245137a2c5050966bb78a9c2b3b96
Nicklas Utgaard <nutgaard@gmail.com>
2020-07-23 14:15:56 +0200

    [KAIZEN-0] importere spring-context for kjerneinfo

--- consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/artifact/kjerneinfo/KjerneinfoContext.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/artifact/kjerneinfo/KjerneinfoContext.java
@@ -1,12 +1,24 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.artifact.kjerneinfo;
 
+import no.nav.dkif.config.spring.DkifConsumerConfig;
+import no.nav.kjerneinfo.consumer.fim.person.config.PersonV3EndpointConfig;
 import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.artifact.kjerneinfo.component.SykmeldingsperioderPanelContext;
+import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.artifact.kjerneinfo.component.mockable.wrapper.EgenAnsattWrapper;
+import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.artifact.kjerneinfo.component.mockable.wrapper.KjerneinfoMapperWrapper;
+import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.artifact.kjerneinfo.component.mockable.wrapper.KontrakterWrapper;
+import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.artifact.kjerneinfo.component.mockable.wrapper.SykmeldingsperioderWrapper;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import({
-        SykmeldingsperioderPanelContext.class
+        SykmeldingsperioderPanelContext.class,
+        KontrakterWrapper.class,
+        DkifConsumerConfig.class,
+        KjerneinfoMapperWrapper.class,
+        PersonV3EndpointConfig.class,
+        SykmeldingsperioderWrapper.class,
+        EgenAnsattWrapper.class
 })
 public class KjerneinfoContext {
 }
