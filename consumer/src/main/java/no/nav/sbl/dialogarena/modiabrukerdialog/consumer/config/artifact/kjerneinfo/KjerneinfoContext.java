735acbf21cd61ff7629631a81417c590841be22a
Nicklas Utgaard <nicklas.utgaard@nav.no>
2020-07-23 14:36:53 +0200

    [KAIZEN-0] fjerne flere unødvendig spring-config filer

--- consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/artifact/kjerneinfo/KjerneinfoContext.java
+++ consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/config/artifact/kjerneinfo/KjerneinfoContext.java
@@ -2,22 +2,20 @@ package no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.artifact.kjerne
 
 import no.nav.dkif.config.spring.DkifConsumerConfig;
 import no.nav.kjerneinfo.consumer.fim.person.config.PersonV3EndpointConfig;
-import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.artifact.kjerneinfo.component.SykmeldingsperioderPanelContext;
 import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.artifact.kjerneinfo.component.mockable.wrapper.EgenAnsattWrapper;
 import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.artifact.kjerneinfo.component.mockable.wrapper.KontrakterWrapper;
-import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.config.artifact.kjerneinfo.component.mockable.wrapper.SykmeldingsperioderWrapper;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import({
-        SykmeldingsperioderPanelContext.class,
         KontrakterWrapper.class,
         DkifConsumerConfig.class,
         PersonV3EndpointConfig.class,
-        SykmeldingsperioderWrapper.class,
         EgenAnsattWrapper.class,
-        no.nav.kodeverk.consumer.config.ConsumerConfig.class
+        no.nav.kjerneinfo.consumer.config.ConsumerConfig.class,
+        no.nav.kodeverk.consumer.config.ConsumerConfig.class,
+        no.nav.sykmeldingsperioder.config.spring.ConsumerConfig.class
 })
 public class KjerneinfoContext {
 }
