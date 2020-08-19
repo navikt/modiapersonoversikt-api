5e5a9cf7312022341dd45aa0d67a53c34def8674
Nicklas Utgaard <nutgaard@gmail.com>
2020-08-19 12:55:20 +0200

    [OPP-1212] bruke spring-web i VarselController.java

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/rest/VarslerController.java
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/rest/VarslerController.java
@@ -1,8 +1,6 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.web.rest;
 
 import kotlin.Pair;
-import no.nav.modig.content.ContentRetriever;
-import no.nav.sbl.dialogarena.modiabrukerdialog.api.domain.henvendelse.Melding;
 import no.nav.sbl.dialogarena.modiabrukerdialog.tilgangskontroll.Policies;
 import no.nav.sbl.dialogarena.modiabrukerdialog.tilgangskontroll.Tilgangskontroll;
 import no.nav.sbl.dialogarena.naudit.AuditIdentifier;
@@ -12,19 +10,18 @@ import no.nav.sbl.dialogarena.varsel.domain.Varsel;
 import no.nav.sbl.dialogarena.varsel.service.VarslerService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
 import static java.util.Collections.emptyList;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
-@Path("/varsler/{fnr}")
-@Produces(APPLICATION_JSON + ";charset=utf-8")
+@RestController
+@RequestMapping("/varsler/{fnr}")
 public class VarslerController {
 
     @Autowired
@@ -33,9 +30,8 @@ public class VarslerController {
     @Autowired
     Tilgangskontroll tilgangskontroll;
 
-    @GET
-    @Path("/")
-    public List<Varsel> hentAlleVarsler(@PathParam("fnr") String fnr) {
+    @GetMapping
+    public List<Varsel> hentAlleVarsler(@PathVariable("fnr") String fnr) {
         return tilgangskontroll
                 .check(Policies.tilgangTilBruker.with(fnr))
                 .get(Audit.describe(Audit.Action.READ, Person.Varsler, new Pair<>(AuditIdentifier.FNR, fnr)), () -> varslerService
