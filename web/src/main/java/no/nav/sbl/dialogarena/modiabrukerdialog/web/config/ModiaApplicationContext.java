b0c1ad6a43d018e5d8b5c8a0a30bd52dae773402
Nicklas Utgaard <nutgaard@gmail.com>
2020-08-20 08:46:24 +0200

    [OPP-1212] rydde i kode etter flytting av filter registrering

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/ModiaApplicationContext.java
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/config/ModiaApplicationContext.java
@@ -1,9 +1,5 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.web.config;
 
-import no.nav.common.auth.Constants;
-import no.nav.common.auth.oidc.filter.OidcAuthenticatorConfig;
-import no.nav.common.auth.subject.IdentType;
-import no.nav.common.utils.EnvironmentUtils;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
@@ -11,45 +7,15 @@ import javax.servlet.ServletContext;
 
 @Configuration
 @Import({
+        LoginContext.class,
         ApplicationContextBeans.class,
         ModulesApplicationContext.class,
-        RestApiBeans.class
+        SelftestContext.class
 })
 
 public class ModiaApplicationContext {
-    private static final String issoClientId = EnvironmentUtils.getRequiredProperty("ISSO_CLIENT_ID");
-    private static final String issoDiscoveryUrl = EnvironmentUtils.getRequiredProperty("ISSO_DISCOVERY_URL");
-    private static final String issoRefreshUrl = EnvironmentUtils.getRequiredProperty("ISSO_REFRESH_URL");
-    private static final String fpsakClientId = EnvironmentUtils.getRequiredProperty("FPSAK_CLIENT_ID");
-
     public void startup(ServletContext servletContext) {
         JmxExporterConfig.setup();
-    }
-
-    public void configure() {
-        OidcAuthenticatorConfig isso = new OidcAuthenticatorConfig()
-                .withClientId(issoClientId)
-                .withDiscoveryUrl(issoDiscoveryUrl)
-                .withIdTokenCookieName(Constants.OPEN_AM_ID_TOKEN_COOKIE_NAME)
-                .withIdentType(IdentType.InternBruker)
-                .withRefreshUrl(issoRefreshUrl)
-                .withRefreshTokenCookieName(Constants.REFRESH_TOKEN_COOKIE_NAME);
-
-        OidcAuthenticatorConfig fpsak = new OidcAuthenticatorConfig()
-                .withClientId(fpsakClientId)
-                .withDiscoveryUrl(issoDiscoveryUrl)
-                .withIdTokenCookieName(Constants.OPEN_AM_ID_TOKEN_COOKIE_NAME)
-                .withIdentType(IdentType.InternBruker)
-                .withRefreshUrl(issoRefreshUrl)
-                .withRefreshTokenCookieName(Constants.REFRESH_TOKEN_COOKIE_NAME);
-
-//        apiAppConfigurator
-//                .enableCXFSecureLogs()
-//                .sts()
-//                .objectMapper(JacksonConfig.mapper)
-//                .addOidcAuthenticator(isso)
-//                .addOidcAuthenticator(fpsak)
-//                .customizeJettyBuilder(jetty -> jetty.at("modiapersonoversikt-api"));
     }
 
     public String getApiBasePath() {
--- web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/MainTest.java
+++ web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/MainTest.java
@@ -1,6 +1,7 @@
+package no.nav.sbl.dialogarena.modiabrukerdialog.web;
+
 import no.nav.common.nais.NaisYamlUtils;
 import no.nav.common.test.SystemProperties;
-import no.nav.sbl.dialogarena.modiabrukerdialog.web.Main;
 import org.springframework.boot.SpringApplication;
 
 public class MainTest {
@@ -12,9 +13,5 @@ public class MainTest {
         SpringApplication application = new SpringApplication(Main.class);
         application.setAdditionalProfiles("local");
         application.run(args);
-
-//        setupTestContext(ApiAppTest.Config.builder().environment("q0").applicationName("modiabrukerdialog").build());
-//
-//        ApiApp.runApp(ModiaApplicationContext.class, new String[]{"8085", "8086"});
     }
 }
