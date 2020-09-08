35428a2c7b07c8a72ced9f077c56218eb1c51764
Nicklas Utgaard <nutgaard@gmail.com>
2020-09-08 11:52:07 +0200

    [OPP-1235] legge til endepunkt uthenting av jwt-expiration

--- web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/rest/TilgangController.kt
+++ web/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/rest/TilgangController.kt
@@ -1,10 +1,14 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.web.rest
 
+import no.nav.common.auth.SsoToken
+import no.nav.common.auth.SubjectHandler
 import no.nav.sbl.dialogarena.abac.AbacClient
 import no.nav.sbl.dialogarena.abac.AbacResponse
 import no.nav.sbl.dialogarena.abac.Decision
 import no.nav.sbl.dialogarena.abac.DenyCause
 import no.nav.sbl.dialogarena.modiabrukerdialog.tilgangskontroll.AbacPolicies
+import java.text.ParseException
+import java.util.*
 import javax.inject.Inject
 import javax.ws.rs.GET
 import javax.ws.rs.Path
@@ -31,9 +35,32 @@ class TilgangController @Inject constructor(private val abacClient: AbacClient) 
                 .makeResponse()
     }
 
+    @GET
+    @Path("/auth")
+    fun authIntropection(): AuthIntropectionDTO {
+        return SubjectHandler.getSsoToken()
+                .map(SsoToken::getExpirationDate)
+                .orElse(AuthIntropectionDTO.INVALID)
+    }
+
 }
 
 class TilgangDTO(val harTilgang: Boolean, val ikkeTilgangArsak: DenyCause?)
+class AuthIntropectionDTO(val expirationDate: Long) {
+    companion object {
+        val INVALID = AuthIntropectionDTO(-1)
+    }
+}
+
+internal fun SsoToken.getExpirationDate(): AuthIntropectionDTO {
+    val exp: Any? = this.attributes["exp"]
+    return when(exp) {
+        null -> AuthIntropectionDTO.INVALID
+        is Date -> AuthIntropectionDTO(exp.time)
+        is Number -> AuthIntropectionDTO(exp.toLong() * 1000) // Er epoch-seconds, men vil ha epoch-ms
+        else -> throw ParseException("The \"exp\" claim is not a Date", 0)
+    }
+}
 
 internal fun AbacResponse.makeResponse(): TilgangDTO {
     if (this.getBiasedDecision(Decision.Deny) == Decision.Permit) {
