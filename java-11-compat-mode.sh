deea8e3326c83e26ddfc73733d616afab2d56ca1
Nicklas Utgaard <nutgaard@gmail.com>
2020-09-22 23:04:26 +0200

    [KAIZEN-0] hotfix ehcache metrikker

--- java-11-compat-mode.sh
+++ java-11-compat-mode.sh
@@ -2,7 +2,7 @@
 
 # Set java11 in compat mode in production
 if [ "$NAIS_CLUSTER_NAME" = "dev-fss" ] || [ "$NAIS_CLUSTER_NAME" = "dev-sbs" ]; then
-  export JAVA_OPTS="${JAVA_OPTS} --illegal-access=deny"
+  export JAVA_OPTS="${JAVA_OPTS} --illegal-access=warn"
 else
   export JAVA_OPTS="${JAVA_OPTS} --illegal-access=warn"
 fi
