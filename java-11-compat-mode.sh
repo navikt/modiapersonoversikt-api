beed4330bc53d8c0e9f4c4effd51b6618f7d16d7
Nicklas Utgaard <nutgaard@gmail.com>
2020-10-05 11:21:53 +0200

    [OPP-1247] bumpe spring-boot
    fjerner modul-åpninger for å se hva slags feil som dukker opp i nyeste versjon

--- java-11-compat-mode.sh
+++ java-11-compat-mode.sh
@@ -4,9 +4,9 @@
 
 # Opens module-system for tomcat;
 # https://github.com/apache/tomcat/blob/master/bin/catalina.sh#L314
-export JAVA_OPTS="${JAVA_OPTS} --add-opens=java.base/java.lang=ALL-UNNAMED"
-export JAVA_OPTS="${JAVA_OPTS} --add-opens=java.base/java.io=ALL-UNNAMED"
-export JAVA_OPTS="${JAVA_OPTS} --add-opens=java.rmi/sun.rmi.transport=ALL-UNNAMED"
+#export JAVA_OPTS="${JAVA_OPTS} --add-opens=java.base/java.lang=ALL-UNNAMED"
+#export JAVA_OPTS="${JAVA_OPTS} --add-opens=java.base/java.io=ALL-UNNAMED"
+#export JAVA_OPTS="${JAVA_OPTS} --add-opens=java.rmi/sun.rmi.transport=ALL-UNNAMED"
 
 if [ "$NAIS_CLUSTER_NAME" = "dev-fss" ] || [ "$NAIS_CLUSTER_NAME" = "dev-sbs" ]; then
   export JAVA_OPTS="${JAVA_OPTS} --illegal-access=debug"
