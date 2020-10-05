b3f1397d4dfc3ddf275ddf3695f2d4c0c1c7e5bd
Nicklas Utgaard <nutgaard@gmail.com>
2020-10-05 10:29:03 +0200

    [OPP-1247] legge til i java-module system for tomcat

--- java-11-compat-mode.sh
+++ java-11-compat-mode.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
 # Set java11 in compat mode in production
+
+# Opens module-system for tomcat;
+# https://github.com/apache/tomcat/blob/master/bin/catalina.sh#L314
+export JAVA_OPTS="${JAVA_OPTS} --add-opens=java.base/java.lang=ALL-UNNAMED"
+export JAVA_OPTS="${JAVA_OPTS} --add-opens=java.base/java.io=ALL-UNNAMED"
+export JAVA_OPTS="${JAVA_OPTS} --add-opens=java.rmi/sun.rmi.transport=ALL-UNNAMED"
+
 if [ "$NAIS_CLUSTER_NAME" = "dev-fss" ] || [ "$NAIS_CLUSTER_NAME" = "dev-sbs" ]; then
   export JAVA_OPTS="${JAVA_OPTS} --illegal-access=debug"
 else
