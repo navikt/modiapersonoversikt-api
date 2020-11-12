53b03dc3f3729b8637869bb6545269c5f98d8cd9
vildestefferud <qwartz.ms@hotmail.com>
2020-11-12 09:29:16 +0100

    [OPP-891] flytter interfacet inn i egen fil

---
+++ api/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/api/service/OppgaveRestClient.kt
@@ -0,0 +1,8 @@
+package no.nav.sbl.dialogarena.modiabrukerdialog.api.service
+
+import no.nav.sbl.dialogarena.modiabrukerdialog.api.domain.Oppgave
+
+interface OppgaveRestClient {
+    fun opprettOppgave(request: OppgaveRequest): OppgaveResponse
+    fun hentOppgave(id: String): Oppgave
+}
