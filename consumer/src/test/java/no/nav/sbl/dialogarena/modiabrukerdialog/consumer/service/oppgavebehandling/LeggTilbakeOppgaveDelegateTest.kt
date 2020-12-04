6e75dcb020dea09d9d9191b2bf7b32a363b2a6b4
vildestefferud <qwartz.ms@hotmail.com>
2020-12-04 16:12:46 +0100

    [OPP-891] lagt til test på at får feilmelding hvis saksbehandler ikke har tilgang til oppgaven

--- consumer/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/service/oppgavebehandling/LeggTilbakeOppgaveDelegateTest.kt
+++ consumer/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/service/oppgavebehandling/LeggTilbakeOppgaveDelegateTest.kt
@@ -1,5 +1,6 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.consumer.service.oppgavebehandling;
 
+import io.ktor.http.cio.*
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
@@ -16,7 +17,11 @@ import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.service.kodeverksmapper
 import no.nav.sbl.dialogarena.modiabrukerdialog.tilgangskontroll.Tilgangskontroll
 import org.joda.time.DateTime
 import org.joda.time.format.DateTimeFormat
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.springframework.http.HttpStatus
+import org.springframework.web.server.ResponseStatusException
 import java.time.LocalDate
 
 
@@ -125,6 +130,56 @@ open class LeggTilbakeOppgaveDelegateTest {
                 versjon = 1,
                 tilordnetRessurs = ""
         )) }
+    }
+
+    @Test
+    @DisplayName("Sjekker om innlogget saksbehandler er samme som saksbehandler som er ansvarlig for oppgaven")
+    fun `skal kaste feil om saksbehandler ikke har tilgang til oppgaven`() {
+        assertThrows<ResponseStatusException> {
+
+            every { apiClient.endreOppgave(any(), any(), any()) } returns RestOppgaveMockFactory.mockOppgaveResponse.asPutResponse()
+            every { arbeidsfordelingService.finnBehandlendeEnhetListe(any(), any(), any(), any()) } returns RestOppgaveMockFactory.mockAnsattEnhetListe
+            every { ansattService.hentAnsattNavn(any()) } returns ""
+
+            SubjectHandlerUtil.withIdent("Z999997") {
+                leggTilbakeOppgaveDelegate.leggTilbake(
+                        RestOppgaveMockFactory.mockOppgaveResponse,
+                        LeggTilbakeOppgaveRequest(
+                                "4110",
+                                "1234",
+                                "ny beskrivelse",
+                                Temagruppe.ANSOS
+                        )
+                )
+            }
+
+            verify {
+                apiClient.endreOppgave(any(), any(), PutOppgaveRequestJsonDTO(
+                        id = 1234,
+                        tildeltEnhetsnr = "4100",
+                        aktoerId = "07063000250",
+                        behandlesAvApplikasjon = "FS22",
+                        beskrivelse = String.format("--- %s %s (%s, %s) ---\n",
+                                DateTimeFormat.forPattern("dd.MM.yyyy HH:mm").print(DateTime.now()),
+                                ansattService.hentAnsattNavn("Z999998"),
+                                "Z999998",
+                                "4110") + "ny beskrivelse" + "\n\n" + "beskrivelse",
+                        temagruppe = Temagruppe.ANSOS.name,
+                        tema = "KNA",
+                        behandlingstema = "",
+                        oppgavetype = "SPM_OG_SVR",
+                        behandlingstype = "",
+                        aktivDato = LocalDate.now(),
+                        fristFerdigstillelse = LocalDate.now(),
+                        prioritet = PutOppgaveRequestJsonDTO.Prioritet.NORM,
+                        endretAvEnhetsnr = "4110",
+                        status = PutOppgaveRequestJsonDTO.Status.AAPNET,
+                        versjon = 1,
+                        tilordnetRessurs = ""
+                ))
+            }
+
+        }
     }
 
 }
