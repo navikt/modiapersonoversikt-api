68c72ab5f403cd18269524b6c77b3c38f8a2a560
Nicklas Utgaard <nutgaard@gmail.com>
2021-02-03 13:26:55 +0100

    [KAIZEN-0] trekker invocationhandler i egen klasse slik at implisitt returns ikke skaper problemer

--- consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/service/oppgavebehandling/OppgaveBehandlingServiceSwitcher.kt
+++ consumer/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/consumer/service/oppgavebehandling/OppgaveBehandlingServiceSwitcher.kt
@@ -13,11 +13,35 @@ import no.nav.sbl.dialogarena.modiabrukerdialog.tilgangskontroll.Tilgangskontrol
 import no.nav.tjeneste.virksomhet.oppgave.v3.OppgaveV3
 import no.nav.tjeneste.virksomhet.oppgavebehandling.v3.OppgavebehandlingV3
 import no.nav.tjeneste.virksomhet.tildeloppgave.v1.TildelOppgaveV1
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.lang.reflect.InvocationHandler
 import java.lang.reflect.InvocationTargetException
+import java.lang.reflect.Method
 import java.lang.reflect.Proxy
 
+class UnleashHandler<T : Any>(
+    val feature: Feature,
+    val unleashService: UnleashService,
+    val ifEnabled: T,
+    val ifDisabled: T
+) : InvocationHandler {
+    val log: Logger = LoggerFactory.getLogger(UnleashHandler::class.java)
+
+    override fun invoke(proxy: Any, method: Method, args: Array<out Any>?): Any {
+        val nullsafeArgs = args ?: arrayOfNulls<Any>(0)
+        return try {
+            if (unleashService.isEnabled(feature)) {
+                log.warn("[OppgaveBehandlingService] bruker rest-implementasjonen av OppgaveBehandlingService")
+                method.invoke(ifEnabled, *nullsafeArgs)
+            } else {
+                method.invoke(ifDisabled, *nullsafeArgs)
+            }
+        } catch (e: InvocationTargetException) {
+            throw e.targetException
+        }
+    }
+}
 fun createOppgaveBehandlingSwitcher(
     oppgavebehandlingV3: OppgavebehandlingV3,
     tildelOppgaveV1: TildelOppgaveV1,
@@ -30,7 +54,6 @@ fun createOppgaveBehandlingSwitcher(
     stsService: SystemUserTokenProvider,
     unleashService: UnleashService
 ): OppgaveBehandlingService {
-    val log = LoggerFactory.getLogger(OppgaveBehandlingService::class.java)
     val restClient: OppgaveBehandlingService = RestOppgaveBehandlingServiceImpl(
         kodeverksmapperService,
         pdlOppslagService,
@@ -49,19 +72,12 @@ fun createOppgaveBehandlingSwitcher(
         restClient
     )
 
-    val invocationHandler = InvocationHandler { _, method, args ->
-        val nullsafeArgs = args ?: arrayOfNulls<Any>(0)
-        try {
-            if (unleashService.isEnabled(Feature.USE_REST_OPPGAVE_IMPL)) {
-                method.invoke(restClient, *nullsafeArgs)
-                log.warn("[OppgaveBehandlingService] bruker rest-implementasjonen av OppgaveBehandlingService")
-            } else {
-                method.invoke(soapClient, *nullsafeArgs)
-            }
-        } catch (e: InvocationTargetException) {
-            throw e.targetException
-        }
-    }
+    val invocationHandler = UnleashHandler(
+        unleashService = unleashService,
+        feature = Feature.USE_REST_OPPGAVE_IMPL,
+        ifDisabled = soapClient,
+        ifEnabled = restClient
+    )
     val proxy = Proxy.newProxyInstance(
         OppgaveBehandlingService::class.java.classLoader,
         arrayOf(OppgaveBehandlingService::class.java),
