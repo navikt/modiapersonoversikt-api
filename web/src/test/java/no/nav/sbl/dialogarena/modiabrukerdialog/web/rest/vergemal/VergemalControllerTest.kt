f3cf20ebb43eacee761090f8c690c298fdb75368
vildestefferud <qwartz.ms@hotmail.com>
2021-02-16 10:09:02 +0100

    [OPP-1153] Fikset test

--- web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/rest/vergemal/VergemalControllerTest.kt
+++ web/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/web/rest/vergemal/VergemalControllerTest.kt
@@ -3,13 +3,11 @@ package no.nav.sbl.dialogarena.modiabrukerdialog.web.rest.vergemal
 import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.whenever
-import no.nav.kjerneinfo.consumer.fim.person.vergemal.VergemalService
-import no.nav.kjerneinfo.consumer.fim.person.vergemal.domain.Periode
-import no.nav.kjerneinfo.consumer.fim.person.vergemal.domain.Verge
-import no.nav.sbl.dialogarena.modiabrukerdialog.api.domain.pdl.generated.HentNavnBolk
+import no.nav.kjerneinfo.consumer.fim.person.vergemal.PdlVergemalService
+import no.nav.kjerneinfo.consumer.fim.person.vergemal.domain.PdlVerge
+import no.nav.sbl.dialogarena.modiabrukerdialog.api.domain.pdl.generated.HentPersonVergemaalEllerFullmakt
 import no.nav.sbl.dialogarena.modiabrukerdialog.tilgangskontroll.TilgangskontrollMock
 import org.junit.jupiter.api.Test
-import java.util.*
 import kotlin.test.assertEquals
 
 private const val FNR = "10108000398"
@@ -17,7 +15,7 @@ private const val VERGES_IDENT = "123"
 
 class VergemalControllerTest {
 
-    private val vergemalService: VergemalService = mock()
+    private val vergemalService: PdlVergemalService = mock()
     private val controller: VergemalController = VergemalController(
             vergemalService,
             TilgangskontrollMock.get()
@@ -26,10 +24,19 @@ class VergemalControllerTest {
 
     @Test
     fun `Henter vergemål`() {
-        whenever(vergemalService.hentVergemal(any())).thenReturn(listOf(Verge()
-                .withIdent(VERGES_IDENT)
-                .withVirkningsperiode(Periode(null, null))
-                .withPersonnavn(HentNavnBolk.Navn("", null, ""))))
+        whenever(vergemalService.hentVergemal(any())).thenReturn(listOf(PdlVerge(
+                ident = VERGES_IDENT,
+                personnavn = HentPersonVergemaalEllerFullmakt.Personnavn2(
+                        fornavn = "",
+                        mellomnavn = null,
+                        etternavn = ""
+                ),
+                embete = null,
+                omfang = null,
+                vergesakstype = null,
+                gyldighetstidspunkt = null,
+                opphoerstidspunkt = null
+        )))
 
         val response = controller.hent(FNR)
 
