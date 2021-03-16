4ad6e01ded72d38d4b608b4c2a6e8dc5a4650c0d
ankur <H3rl3k1r123>
2021-03-16 14:48:35 +0100

    opprydding av sakV1 og featuretoggle.

--- saksoversikt/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/sak/service/SaksService.java
+++ saksoversikt/src/main/java/no/nav/sbl/dialogarena/modiabrukerdialog/sak/service/SaksService.java
@@ -1,7 +1,6 @@
 package no.nav.sbl.dialogarena.modiabrukerdialog.sak.service;
 
 import no.nav.sbl.dialogarena.modiabrukerdialog.api.service.saker.SakerService;
-import no.nav.sbl.dialogarena.modiabrukerdialog.consumer.service.unleash.UnleashService;
 import no.nav.sbl.dialogarena.modiabrukerdialog.sak.providerdomain.Baksystem;
 import no.nav.sbl.dialogarena.modiabrukerdialog.sak.providerdomain.FeilendeBaksystemException;
 import no.nav.sbl.dialogarena.modiabrukerdialog.sak.providerdomain.Sak;
@@ -14,13 +13,10 @@ import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
 import static no.nav.sbl.dialogarena.modiabrukerdialog.sak.providerdomain.Baksystem.GSAK;
-import static no.nav.sbl.dialogarena.modiabrukerdialog.sak.service.GsakSakerService.GSAK_FAGSYSTEM_ID;
+import static no.nav.sbl.dialogarena.modiabrukerdialog.sak.providerdomain.Baksystem.SAK;
 
 @SuppressWarnings("squid:S1166") // Either log or rethrow
 public class SaksService {
-
-    @Autowired
-    private GsakSakerService gsakSakerService;
 
     @Autowired
     private PesysService pesysService;
@@ -28,10 +24,8 @@ public class SaksService {
     @Autowired
     private SakerService sakerService;
 
-    @Autowired
-    private UnleashService unleashService;
+    public static final String GSAK_FAGSYSTEM_ID = "FS22";
 
-    private static final String SAK_FEATURE = "modiabrukerdialog.rest-sak-impl";
 
     public ResultatWrapper<List<Sak>> hentAlleSaker(String fnr) {
         Set<Baksystem> feilendeBaksystemer = new HashSet<>();
@@ -40,23 +34,19 @@ public class SaksService {
         Optional<List<Sak>> maybeFraPesys;
 
         try {
-            if (unleashService.isEnabled(SAK_FEATURE)) {
-                maybeFraGSak = Optional.of(sakerService.hentSakSaker(fnr).getSaker().stream().map(
-                        sak ->
-                                new Sak()
-                                        .withSaksId(sak.saksId)
-                                        .withFagsaksnummer(sak.fagsystemSaksId)
-                                        .withTemakode(sak.temaKode)
-                                        .withBaksystem(GSAK)
-                                        .withFagsystem(sak.fagsystemKode)
-                                        .withFagsystem(GSAK_FAGSYSTEM_ID)
+            maybeFraGSak = Optional.of(sakerService.hentSakSaker(fnr).getSaker().stream().map(
+                    sak ->
+                            new Sak()
+                                    .withSaksId(sak.saksId)
+                                    .withFagsaksnummer(sak.fagsystemSaksId)
+                                    .withTemakode(sak.temaKode)
+                                    .withBaksystem(SAK)
+                                    .withFagsystem(sak.fagsystemKode)
+                                    .withFagsystem(GSAK_FAGSYSTEM_ID)
 
-                ));
-            } else {
-                maybeFraGSak = gsakSakerService.hentSaker(fnr);
-            }
-        } catch (FeilendeBaksystemException e) {
-            feilendeBaksystemer.add(e.getBaksystem());
+            ));
+        } catch (Exception e) {
+            feilendeBaksystemer.add(SAK);
             maybeFraGSak = Optional.empty();
         }
 
---
+++ saksoversikt/src/test/java/no/nav/sbl/dialogarena/modiabrukerdialog/sak/service/SaksServiceTest.java
@@ -0,0 +1,69 @@
+package no.nav.sbl.dialogarena.modiabrukerdialog.sak.service;
+
+import no.nav.sbl.dialogarena.modiabrukerdialog.api.domain.saker.Sak;
+import no.nav.sbl.dialogarena.modiabrukerdialog.api.service.saker.SakerService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SaksServiceTest {
+
+    @Mock
+    private PesysService pesysService;
+
+    @Mock
+    private SakerService sakerService;
+
+    @InjectMocks
+    private SaksService saksService;
+
+    public static final String DAG = "DAG";
+    public static final String AAP = "AAP";
+    public static final String GSAK_SYSTEM = "FS22";
+
+
+    @Test
+    public void hentSakstema() {
+        Sak sakDAG = new Sak();
+        sakDAG.temaKode = DAG;
+        sakDAG.fagsystemKode = GSAK_SYSTEM;
+
+        Sak sakAAP = new Sak();
+        sakAAP.temaKode = AAP;
+        sakAAP.fagsystemKode = GSAK_SYSTEM;
+
+        List<Sak> lstSaker = new ArrayList();
+        lstSaker.add(sakDAG);
+        lstSaker.add(sakAAP);
+
+        when(sakerService.hentSakSaker(any()))
+                .thenReturn(new SakerService.Resultat(lstSaker, new ArrayList<>()));
+
+        List<no.nav.sbl.dialogarena.modiabrukerdialog.sak.providerdomain.Sak> saker = saksService.hentAlleSaker("11111111111").resultat;
+
+        assertThat(saker.get(0).getTemakode(), equalTo(DAG));
+        assertThat(saker.get(1).getTemakode(), equalTo(AAP));
+    }
+
+    @Test
+    public void hentSakstemaGirTomOptionalHvisTjenesteGirException() {
+
+        when(sakerService.hentSakSaker(any())).thenThrow(new RuntimeException());
+        List<no.nav.sbl.dialogarena.modiabrukerdialog.sak.providerdomain.Sak> saker = saksService.hentAlleSaker("11111111111").resultat;
+
+        assertTrue(saker.isEmpty());
+    }
+
+}
