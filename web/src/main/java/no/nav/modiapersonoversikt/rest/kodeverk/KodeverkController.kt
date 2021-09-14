8c5ecb918cf393cd13fb8f7cb15e8a7558a71804
Nicklas Utgaard <nutgaard@gmail.com>
2021-09-14 10:56:24 +0200

    [OPP-1384] eksponere enhetligkodeverk via v2-rest endepunkt

--- web/src/main/java/no/nav/modiapersonoversikt/rest/kodeverk/KodeverkController.kt
+++ web/src/main/java/no/nav/modiapersonoversikt/rest/kodeverk/KodeverkController.kt
@@ -5,6 +5,8 @@ import no.nav.modiapersonoversikt.infrastructure.naudit.Audit
 import no.nav.modiapersonoversikt.infrastructure.tilgangskontroll.Policies
 import no.nav.modiapersonoversikt.infrastructure.tilgangskontroll.Tilgangskontroll
 import no.nav.modiapersonoversikt.legacy.kjerneinfo.common.domain.Kodeverdi
+import no.nav.modiapersonoversikt.service.enhetligkodeverk.EnhetligKodeverk
+import no.nav.modiapersonoversikt.service.enhetligkodeverk.KodeverkConfig
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -12,12 +14,13 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/rest/kodeverk/{kodeverkRef}")
+@RequestMapping("/rest")
 class KodeverkController @Autowired constructor(
     private val tilgangskontroll: Tilgangskontroll,
-    private val kodeverkManager: KodeverkmanagerBi
+    private val kodeverkManager: KodeverkmanagerBi,
+    private val kodeverk: EnhetligKodeverk.Service
 ) {
-    @GetMapping
+    @GetMapping("/kodeverk/{kodeverkRef}")
     fun hentKodeverk(@PathVariable("kodeverkRef") kodeverkRef: String) =
         tilgangskontroll
             .check(Policies.tilgangTilModia)
@@ -28,6 +31,15 @@ class KodeverkController @Autowired constructor(
                         .map(::Kode)
                 )
             }
+
+    @GetMapping("/v2/kodeverk/{kodeverkRef}")
+    fun hentEnhetligKodeverk(@PathVariable("kodeverkRef") kodeverkRef: KodeverkConfig): EnhetligKodeverk.Kodeverk {
+        return tilgangskontroll
+            .check(Policies.tilgangTilModia)
+            .get(Audit.skipAuditLog()) {
+                kodeverk.hentKodeverk(kodeverkRef)
+            }
+    }
 }
 
 data class Kode(val kodeRef: String, val beskrivelse: String) {
