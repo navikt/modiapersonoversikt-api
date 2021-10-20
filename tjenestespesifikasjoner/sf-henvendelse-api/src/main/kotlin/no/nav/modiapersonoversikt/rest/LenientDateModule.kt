1b7f794af1da5df381723a4918b33ef10eb9fa67
Nicklas Utgaard <nutgaard@gmail.com>
2021-10-20 13:19:11 +0200

    [OPP-1378] legge til dato deserialiserer
    gjøres for å kunne takle SF sitt datoformat som ikke helt følger ISO8601 standarden. Ved en oppdaterting av SF-api så kan vi fjerne `LenientDateModule`

---
+++ tjenestespesifikasjoner/sf-henvendelse-api/src/main/kotlin/no/nav/modiapersonoversikt/rest/LenientDateModule.kt
@@ -0,0 +1,31 @@
+package no.nav.modiapersonoversikt.rest
+
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonDeserializer
+import com.fasterxml.jackson.databind.module.SimpleModule
+import java.time.OffsetDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeFormatterBuilder
+
+/**
+ * Brukes bare mens vi venter på at SF skal fikse dato-formatene sine
+ */
+class LenientDateModule : SimpleModule("lenient-date-module") {
+    init {
+        addDeserializer(OffsetDateTime::class.java, LenientDateDeserializer())
+    }
+}
+
+class LenientDateDeserializer : JsonDeserializer<OffsetDateTime>() {
+    val fmt = DateTimeFormatterBuilder()
+        .append(DateTimeFormatter.ISO_LOCAL_DATE_TIME)
+        .optionalStart().appendOffset("+HH:MM", "+00:00").optionalEnd()
+        .optionalStart().appendOffset("+HHMM", "+0000").optionalEnd()
+        .optionalStart().appendOffset("+HH", "+00").optionalEnd()
+        .toFormatter()
+
+    override fun deserialize(parser: JsonParser, context: DeserializationContext): OffsetDateTime {
+        return OffsetDateTime.parse(parser.text, fmt)
+    }
+}
