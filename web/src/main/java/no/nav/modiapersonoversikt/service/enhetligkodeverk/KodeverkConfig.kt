6b80ffdc1fbdae135b02c62fc02da04986f7de8d
vildestefferud <qwartz.ms@hotmail.com>
2022-01-07 08:52:42 +0100

    [OPP-1288] Legger til NAVSkjema og Vedleggskoder, fletter de sammen til et kodeverk

--- web/src/main/java/no/nav/modiapersonoversikt/service/enhetligkodeverk/KodeverkConfig.kt
+++ web/src/main/java/no/nav/modiapersonoversikt/service/enhetligkodeverk/KodeverkConfig.kt
@@ -13,9 +13,26 @@ enum class KodeverkConfig(private val kilde: EnhetligKodeverk.Kilde) {
     DISKRESJONSKODER(FellesKodeverkKilde("Diskresjonskoder")),
     VALUTA(FellesKodeverkKilde("Valutaer")),
     TEMA(FellesKodeverkKilde("Tema")),
-    SF_TEMAGRUPPER(SfHenvendelseKodeverkKilde());
+    SF_TEMAGRUPPER(SfHenvendelseKodeverkKilde()),
+    NAVSKJEMAOGVEDLEGGSKODER(FellesKodeverkFletter(
+        "NAVSkjemaOgVedleggskoder",
+        listOf(
+            FellesKodeverkKilde("NAVSkjema"),
+            FellesKodeverkKilde("Vedleggskoder")
+        )
+    ));
 
     fun hentKodeverk(providers: KodeverkProviders) = kilde.hentKodeverk(providers)
+}
+
+class FellesKodeverkFletter(val nyttKodeverkNavn: String, val kodeverkKilder: List<FellesKodeverkKilde>) : EnhetligKodeverk.Kilde {
+    override fun hentKodeverk(providers: KodeverkProviders): EnhetligKodeverk.Kodeverk {
+        val kodeverk = kodeverkKilder
+            .map { providers.fraFellesKodeverk(it.kodeverkNavn).kodeverk }
+            .reduce { a, b -> a + b }
+
+        return EnhetligKodeverk.Kodeverk(nyttKodeverkNavn, kodeverk)
+    }
 }
 
 class FellesKodeverkKilde(val kodeverkNavn: String) : EnhetligKodeverk.Kilde {
