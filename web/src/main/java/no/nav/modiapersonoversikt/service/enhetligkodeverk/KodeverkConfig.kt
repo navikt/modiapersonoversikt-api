6dd86d7d5eb56cac705c5f766299a268f9d71e78
vildestefferud <qwartz.ms@hotmail.com>
2022-01-20 15:02:08 +0100

    [OPP-1288] Fjerner ubrukt kode

--- web/src/main/java/no/nav/modiapersonoversikt/service/enhetligkodeverk/KodeverkConfig.kt
+++ web/src/main/java/no/nav/modiapersonoversikt/service/enhetligkodeverk/KodeverkConfig.kt
@@ -13,29 +13,9 @@ enum class KodeverkConfig(private val kilde: EnhetligKodeverk.Kilde) {
     DISKRESJONSKODER(FellesKodeverkKilde("Diskresjonskoder")),
     VALUTA(FellesKodeverkKilde("Valutaer")),
     TEMA(FellesKodeverkKilde("Tema")),
-    SF_TEMAGRUPPER(SfHenvendelseKodeverkKilde()),
-    NAVSKJEMAOGVEDLEGGSKODER(
-        FellesKodeverkFletter(
-            "NAVSkjemaOgVedleggskoder",
-            listOf(
-                FellesKodeverkKilde("NAVSkjema"),
-                FellesKodeverkKilde("Vedleggskoder")
-            )
-        )
-    );
+    SF_TEMAGRUPPER(SfHenvendelseKodeverkKilde());
 
     fun hentKodeverk(providers: KodeverkProviders) = kilde.hentKodeverk(providers)
-}
-
-class FellesKodeverkFletter(val nyttKodeverkNavn: String, val kodeverkKilder: List<FellesKodeverkKilde>) :
-    EnhetligKodeverk.Kilde {
-    override fun hentKodeverk(providers: KodeverkProviders): EnhetligKodeverk.Kodeverk {
-        val kodeverk = kodeverkKilder
-            .map { providers.fraFellesKodeverk(it.kodeverkNavn).kodeverk }
-            .reduce { a, b -> a + b }
-
-        return EnhetligKodeverk.Kodeverk(nyttKodeverkNavn, kodeverk)
-    }
 }
 
 class FellesKodeverkKilde(val kodeverkNavn: String) : EnhetligKodeverk.Kilde {
