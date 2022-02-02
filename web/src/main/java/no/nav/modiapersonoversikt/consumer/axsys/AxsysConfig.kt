386a4b4a481919b4137b2c84d77e106ed199330f
vildestefferud <qwartz.ms@hotmail.com>
2022-02-02 15:21:31 +0100

    [OPP-1342] Bytter til AxsysV2 og legger til tokenSupplier

--- web/src/main/java/no/nav/modiapersonoversikt/consumer/axsys/AxsysConfig.kt
+++ web/src/main/java/no/nav/modiapersonoversikt/consumer/axsys/AxsysConfig.kt
@@ -1,26 +1,39 @@
 package no.nav.modiapersonoversikt.consumer.axsys
 
 import com.github.benmanes.caffeine.cache.Cache
-import no.nav.common.client.axsys.AxsysClient
-import no.nav.common.client.axsys.AxsysClientImpl
-import no.nav.common.client.axsys.AxsysEnhet
-import no.nav.common.client.axsys.CachedAxsysClient
+import no.nav.common.client.axsys.*
 import no.nav.common.rest.client.RestClient
+import no.nav.common.sts.ServiceToServiceTokenProvider
 import no.nav.common.types.identer.EnhetId
 import no.nav.common.types.identer.NavIdent
+import no.nav.common.utils.EnvironmentUtils
 import okhttp3.OkHttpClient
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 open class AxsysConfig {
-    val url: String = ""
+    val url: String = EnvironmentUtils.getRequiredProperty("AXSYS_URL")
     val httpClient: OkHttpClient = RestClient.baseClient()
     private val enhetCache: Cache<EnhetId, MutableList<NavIdent>> = TODO("Lage cache")
     private val tilgangCache: Cache<NavIdent, MutableList<AxsysEnhet>> = TODO("Lage cache")
 
+    @Autowired
+    var tokenProvider: ServiceToServiceTokenProvider
+
     @Bean
     open fun axsys(): AxsysClient {
-        return CachedAxsysClient(AxsysClientImpl(url, httpClient), tilgangCache, enhetCache)
+        val tokenSupplier = {
+            tokenProvider
+                .getServiceToken(
+                    "axsys",
+                    "org",
+                    EnvironmentUtils
+                        .getRequiredProperty("AXSYS_CLUSTER")
+                )
+        }
+
+        return CachedAxsysClient(AxsysV2ClientImpl(url, tokenSupplier, httpClient), tilgangCache, enhetCache)
     }
 }
