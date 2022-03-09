906f580d51eb34233aa3446b4e7c3da7fb8b2fbf
vildestefferud <qwartz.ms@hotmail.com>
2022-03-09 14:37:06 +0100

    [KAIZEN-0] Fikser linting-feil

--- web/src/main/java/no/nav/modiapersonoversikt/config/artifact/kjerneinfo/EgenAnsattWrapper.kt
+++ web/src/main/java/no/nav/modiapersonoversikt/config/artifact/kjerneinfo/EgenAnsattWrapper.kt
@@ -2,13 +2,13 @@ package no.nav.modiapersonoversikt.config.artifact.kjerneinfo
 
 import no.nav.modiapersonoversikt.config.endpoint.v1.egenansatt.EgenAnsattV1EndpointConfig
 import no.nav.modiapersonoversikt.consumer.skjermedePersoner.SkjermedePersonerApi
-import org.springframework.beans.factory.annotation.Autowired
-import no.nav.tjeneste.pip.egen.ansatt.v1.EgenAnsattV1
 import no.nav.modiapersonoversikt.legacy.kjerneinfo.consumer.egenansatt.EgenAnsattService
 import no.nav.modiapersonoversikt.legacy.kjerneinfo.consumer.egenansatt.EgenAnsattServiceImpl
 import no.nav.modiapersonoversikt.service.unleash.Feature
 import no.nav.modiapersonoversikt.service.unleash.UnleashService
 import no.nav.modiapersonoversikt.utils.UnleashProxySwitcher
+import no.nav.tjeneste.pip.egen.ansatt.v1.EgenAnsattV1
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
