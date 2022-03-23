9434ea715505045b73aff1364487d2e3f5bd1616
Nicklas Utgaard <nutgaard@gmail.com>
2022-03-23 16:56:21 +0100

    [KAIZEN-0] samlokalisere dkif

--- web/src/main/java/no/nav/modiapersonoversikt/config/ServiceConfig.java
+++ web/src/main/java/no/nav/modiapersonoversikt/config/ServiceConfig.java
@@ -1,11 +1,5 @@
 package no.nav.modiapersonoversikt.config;
 
-import no.nav.common.utils.EnvironmentUtils;
-import no.nav.modiapersonoversikt.consumer.dkif.Dkif;
-import no.nav.modiapersonoversikt.consumer.dkif.DkifServiceImpl;
-import no.nav.modiapersonoversikt.consumer.dkif.DkifServiceRestImpl;
-import no.nav.tjeneste.virksomhet.digitalkontaktinformasjon.v1.DigitalKontaktinformasjonV1;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -16,15 +10,5 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @Configuration
 @EnableScheduling
 public class ServiceConfig {
-    @Bean(name = "DkifSoap")
-    public Dkif.Service defaultDkifService(DigitalKontaktinformasjonV1 dkifV1) {
-        return new DkifServiceImpl(dkifV1);
-    }
 
-    @Bean(name = "DkifRest")
-    public Dkif.Service restDkifService() {
-        return new DkifServiceRestImpl(
-                EnvironmentUtils.getRequiredProperty("DKIF_REST_URL")
-        );
-    }
 }
