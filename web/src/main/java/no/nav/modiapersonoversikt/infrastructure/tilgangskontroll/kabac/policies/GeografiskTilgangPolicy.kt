1175167e705422c15648bfd53d27ccbaa491cde4
Nicklas Utgaard <nutgaard@gmail.com>
2022-05-06 16:16:38 +0200

    [KAIZEN-0] fjerne unødvendig splitting av policies for geografisk kontroll

--- web/src/main/java/no/nav/modiapersonoversikt/infrastructure/tilgangskontroll/kabac/policies/GeografiskTilgangPolicy.kt
+++ web/src/main/java/no/nav/modiapersonoversikt/infrastructure/tilgangskontroll/kabac/policies/GeografiskTilgangPolicy.kt
@@ -2,79 +2,48 @@ package no.nav.modiapersonoversikt.infrastructure.tilgangskontroll.kabac.policie
 
 import no.nav.common.types.identer.EnhetId
 import no.nav.modiapersonoversikt.infrastructure.kabac.Kabac
-import no.nav.modiapersonoversikt.infrastructure.kabac.utils.CombiningAlgorithm
 import no.nav.modiapersonoversikt.infrastructure.kabac.utils.EvaluationContext
 import no.nav.modiapersonoversikt.infrastructure.tilgangskontroll.kabac.providers.*
 
-private object BrukerUtenEnhet : Kabac.Policy {
-    override fun evaluate(ctx: EvaluationContext): Kabac.Decision {
-        val brukersEnhet: EnhetId? = ctx.getValue(BrukersEnhetPip)
-        return if (brukersEnhet == null) {
-            Kabac.Decision.Permit()
-        } else {
-            Kabac.Decision.NotApplicable("Bruker hadde nav-enhet")
-        }
-    }
-}
-
-private object TilgangTilBrukersNavKontor : Kabac.Policy {
-    override fun evaluate(ctx: EvaluationContext): Kabac.Decision {
-        val brukersEnhet: EnhetId? = ctx.getValue(BrukersEnhetPip)
-        val veiledersEnheter: List<EnhetId> = ctx.getValue(VeiledersEnheterPip) ?: emptyList()
-        return if (veiledersEnheter.contains(brukersEnhet)) {
-            Kabac.Decision.Permit()
-        } else {
-            Kabac.Decision.NotApplicable("Veileder hadde ikke tilgang til bruker nav-enhet")
-        }
-    }
-}
-
-private object NasjonalTilgang : Kabac.Policy {
-    private val nasjonalTilgangRoller = setOf(
+@Suppress("FoldInitializerAndIfToElvis")
+object GeografiskTilgangPolicy : Kabac.Policy {
+    val nasjonalTilgangRoller = setOf(
         "0000-ga-gosys_nasjonal",
         "0000-ga-gosys_utvidbar_til_nasjonal",
         "0000-ga-pensjon_nasjonal_u_logg",
         "0000-ga-pensjon_nasjonal_m_logg"
     )
 
-    override fun evaluate(ctx: EvaluationContext): Kabac.Decision {
-        val veilederRoller = ctx.getValue(VeiledersRollerPip) ?: emptySet()
-        return if (nasjonalTilgangRoller.intersect(veilederRoller).isNotEmpty()) {
-            Kabac.Decision.Permit()
-        } else {
-            Kabac.Decision.NotApplicable("Veileder hadde ikke nasjonal tilgang")
-        }
-    }
-}
-
-private object RegionalTilgangTilBrukersRegion : Kabac.Policy {
-    private val regionalTilgangRoller = setOf(
+    val regionalTilgangRoller = setOf(
         "0000-ga-gosys_regional",
         "0000-ga-gosys_utvidbar_til_regional",
     )
 
     override fun evaluate(ctx: EvaluationContext): Kabac.Decision {
         val veilederRoller = ctx.getValue(VeiledersRollerPip) ?: emptySet()
-        if (regionalTilgangRoller.intersect(veilederRoller).isEmpty()) {
-            return Kabac.Decision.NotApplicable("Veileder har ikke regional tilgang")
+        if (nasjonalTilgangRoller.intersect(veilederRoller).isNotEmpty()) {
+            return Kabac.Decision.Permit()
         }
 
-        val brukersRegion: EnhetId? = ctx.getValue(BrukersRegionEnhetPip)
-        val veiledersRegioner: List<EnhetId> = ctx.getValue(VeiledersRegionEnheterPip) ?: emptyList()
-        return if (veiledersRegioner.contains(brukersRegion)) {
-            Kabac.Decision.Permit()
-        } else {
-            Kabac.Decision.NotApplicable("Veileder har ikke tilgang til brukers region")
+        val brukersEnhet: EnhetId? = ctx.getValue(BrukersEnhetPip)
+        if (brukersEnhet == null) {
+            return Kabac.Decision.Permit()
         }
+
+        val veiledersEnheter: List<EnhetId> = ctx.getValue(VeiledersEnheterPip) ?: emptyList()
+        if (veiledersEnheter.contains(brukersEnhet)) {
+            return Kabac.Decision.Permit()
+        }
+
+        if (regionalTilgangRoller.intersect(veilederRoller).isNotEmpty()) {
+            val brukersRegion: EnhetId? = ctx.getValue(BrukersRegionEnhetPip)
+            val veiledersRegioner: List<EnhetId> = ctx.getValue(VeiledersRegionEnheterPip) ?: emptyList()
+
+            if (veiledersRegioner.contains(brukersRegion)) {
+                return Kabac.Decision.Permit()
+            }
+        }
+
+        return Kabac.Decision.Deny("Veileder har ikke tilgang til bruker basert på geografisk tilgang")
     }
 }
-
-object GeografiskTilgangPolicy : Kabac.Policy by CombiningAlgorithm.firstApplicable.combine(
-    listOf(
-        NasjonalTilgang,
-        BrukerUtenEnhet,
-        TilgangTilBrukersNavKontor,
-        RegionalTilgangTilBrukersRegion,
-        Kabac.Policy { Kabac.Decision.Deny("Veileder har ikke tilgang til bruker basert på geografisk tilgang") }
-    )
-)
