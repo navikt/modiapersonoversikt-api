71f42eafde27081d1f15b4a963dbbf61888794b4
Nicklas Utgaard <nutgaard@gmail.com>
2022-05-09 14:38:01 +0200

    [KAIZEN-0] legge til feilhåndtering for uthenting av brukers enhet

--- web/src/main/java/no/nav/modiapersonoversikt/infrastructure/tilgangskontroll/kabac/providers/BrukersEnhetPip.kt
+++ web/src/main/java/no/nav/modiapersonoversikt/infrastructure/tilgangskontroll/kabac/providers/BrukersEnhetPip.kt
@@ -5,18 +5,25 @@ import no.nav.modiapersonoversikt.consumer.norg.NorgApi
 import no.nav.modiapersonoversikt.infrastructure.kabac.Kabac
 import no.nav.modiapersonoversikt.infrastructure.kabac.utils.EvaluationContext
 import no.nav.modiapersonoversikt.infrastructure.kabac.utils.Key
+import org.slf4j.LoggerFactory
 
 class BrukersEnhetPip(private val norgApi: NorgApi) : Kabac.AttributeProvider<EnhetId> {
+    private val log = LoggerFactory.getLogger(BrukersEnhetPip::class.java)
     override val key = Companion.key
     companion object : Kabac.AttributeKey<EnhetId> {
         override val key = Key<EnhetId>(BrukersEnhetPip::class.java.simpleName)
     }
 
     override fun provide(ctx: EvaluationContext): EnhetId? {
-        return ctx.getValue(BrukersGeografiskeTilknyttningPip)
-            ?.let {
-                norgApi.finnNavKontor(it, null)
+        val gt = ctx.getValue(BrukersGeografiskeTilknyttningPip)
+        return gt
+            ?.runCatching {
+                norgApi.finnNavKontor(this, null)
             }
+            ?.onFailure {
+                log.warn("Kunne ikke hente ut brukers enhet", it)
+            }
+            ?.getOrNull()
             ?.let { EnhetId(it.enhetId) }
     }
 }
