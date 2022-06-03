616e66a75f14a71d756a5aaa3d388d1000f4bc93
vildestefferud <qwartz.ms@hotmail.com>
2022-06-03 10:22:41 +0200

    [KAIZEN-0] Fjerner endringer i addressesøk
    Endringene gjorde ingen særlig forskjell i forbedring av søket

--- web/src/main/java/no/nav/modiapersonoversikt/rest/person/PersonsokController.kt
+++ web/src/main/java/no/nav/modiapersonoversikt/rest/person/PersonsokController.kt
@@ -122,11 +122,7 @@ private fun lagBostedsadresse(adr: List<SokPerson.Bostedsadresse>?): String? {
     if (adr.isNullOrEmpty()) {
         return null
     }
-    val adresse = adr
-        .filter { it.harGyldigPeriode() }
-        .sortedByDescending { it.gyldigTilOgMed?.value }
-        .firstOrNull()
-        ?: return null
+    val adresse = adr.first()
     when {
         adresse.ukjentBosted != null -> {
             return adresse.ukjentBosted!!.bostedskommune
@@ -173,11 +169,7 @@ fun lagPostadresse(adr: List<SokPerson.Kontaktadresse>?): String? {
     if (adr.isNullOrEmpty()) {
         return null
     }
-    val adresse = adr
-        .filter { it.harGyldigPeriode() }
-        .sortedByDescending { it.gyldigTilOgMed?.value }
-        .firstOrNull()
-        ?: return null
+    val adresse = adr.first()
     when {
         adresse.postadresseIFrittFormat != null -> {
             return listOfNotNull(
@@ -304,17 +296,6 @@ private fun lagBostedsadresse(adr: StrukturertAdresse): String? =
         is PostboksadresseNorsk -> arrayOf(adr.postboksanlegg, adr.poststed?.value).filterNotNull().joinToString(" ")
         else -> null
     }
-
-private fun SokPerson.Kontaktadresse.harGyldigPeriode(): Boolean = harGyldigPeriode(gyldigFraOgMed, gyldigTilOgMed)
-private fun SokPerson.Bostedsadresse.harGyldigPeriode(): Boolean = harGyldigPeriode(gyldigFraOgMed, gyldigTilOgMed)
-
-private fun harGyldigPeriode(start: SokPerson.DateTime?, end: SokPerson.DateTime?): Boolean {
-    val startDate = start?.value ?: LocalDateTime.MIN
-    val endDate = end?.value ?: LocalDateTime.MAX
-    val now = LocalDateTime.now()
-
-    return startDate.isBefore(now) && now.isBefore(endDate)
-}
 
 data class PersonnavnDTO(
     val fornavn: String,
