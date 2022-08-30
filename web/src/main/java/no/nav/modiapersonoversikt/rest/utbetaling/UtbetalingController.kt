161589d0ec404c2d6ee1a529b57e1c4a57e5fecd
Nicklas Utgaard <nutgaard@gmail.com>
2022-08-30 08:47:28 +0200

    [KAIZEN-0] behold samme api for utbetalinger controller

--- web/src/main/java/no/nav/modiapersonoversikt/rest/utbetaling/UtbetalingController.kt
+++ web/src/main/java/no/nav/modiapersonoversikt/rest/utbetaling/UtbetalingController.kt
@@ -6,6 +6,7 @@ import no.nav.modiapersonoversikt.infrastructure.naudit.AuditIdentifier
 import no.nav.modiapersonoversikt.infrastructure.naudit.AuditResources
 import no.nav.modiapersonoversikt.infrastructure.tilgangskontroll.Policies
 import no.nav.modiapersonoversikt.infrastructure.tilgangskontroll.Tilgangskontroll
+import no.nav.modiapersonoversikt.rest.DATOFORMAT
 import no.nav.modiapersonoversikt.rest.lagRiktigDato
 import no.nav.modiapersonoversikt.service.utbetaling.UtbetalingService
 import org.springframework.beans.factory.annotation.Autowired
@@ -19,12 +20,20 @@ class UtbetalingController @Autowired constructor(
     private val service: UtbetalingService,
     private val tilgangskontroll: Tilgangskontroll
 ) {
+    class UtbetalingerResponseDTO(
+        val utbetalinger: List<UtbetalingDTO>,
+        val periode: UtbetalingerPeriodeDTO
+    )
+    class UtbetalingerPeriodeDTO(
+        val startDato: String,
+        val sluttDato: String
+    )
     @GetMapping
     fun hent(
         @PathVariable("fnr") fnr: String,
         @RequestParam("startDato") start: String?,
         @RequestParam("sluttDato") slutt: String?
-    ): List<UtbetalingDTO> {
+    ): UtbetalingerResponseDTO {
         return tilgangskontroll
             .check(Policies.tilgangTilBruker(Fnr(fnr)))
             .get(Audit.describe(Audit.Action.READ, AuditResources.Person.Utbetalinger, AuditIdentifier.FNR to fnr)) {
@@ -37,11 +46,18 @@ class UtbetalingController @Autowired constructor(
                         "queryparam ?startDato=yyyy-MM-dd&sluttDato=yyyy-MM-dd må være satt"
                     )
                 } else {
-                    UtbetalingMapper.hentUtbetalinger(
+                    val utbetalinger = UtbetalingMapper.hentUtbetalinger(
                         service.hentWSUtbetalinger(
                             fnr,
                             startDato,
                             sluttDato
+                        )
+                    )
+                    UtbetalingerResponseDTO(
+                        utbetalinger = utbetalinger,
+                        periode = UtbetalingerPeriodeDTO(
+                            startDato = startDato.toString(DATOFORMAT),
+                            sluttDato = sluttDato.toString(DATOFORMAT),
                         )
                     )
                 }
