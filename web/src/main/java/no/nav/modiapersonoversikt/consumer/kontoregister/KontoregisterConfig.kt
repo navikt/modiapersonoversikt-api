dc51cb50fa537f392fe8b813bb38da181c965c1d
Nicklas Utgaard <nutgaard@gmail.com>
2022-08-31 13:37:35 +0200

    [OPP-1423] legge til ping for kontoregister

--- web/src/main/java/no/nav/modiapersonoversikt/consumer/kontoregister/KontoregisterConfig.kt
+++ web/src/main/java/no/nav/modiapersonoversikt/consumer/kontoregister/KontoregisterConfig.kt
@@ -1,9 +1,12 @@
 package no.nav.modiapersonoversikt.consumer.kontoregister
 
+import no.nav.common.health.HealthCheckResult
+import no.nav.common.health.selftest.SelfTestCheck
 import no.nav.common.rest.client.RestClient
 import no.nav.common.utils.EnvironmentUtils
 import no.nav.modiapersonoversikt.api.domain.kontoregister.generated.apis.KontoregisterV1Api
 import no.nav.modiapersonoversikt.infrastructure.http.LoggingInterceptor
+import no.nav.modiapersonoversikt.infrastructure.types.Pingable
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -22,4 +25,18 @@ open class KontoregisterConfig {
             )
             .build()
     )
+
+    @Bean
+    open fun kontoregisterPing(client: KontoregisterV1Api): Pingable {
+        return Pingable {
+            SelfTestCheck("Kontoregister - V1", false) {
+                try {
+                    client.hentValutakoder()
+                    HealthCheckResult.healthy()
+                } catch (e: Throwable) {
+                    HealthCheckResult.unhealthy(e)
+                }
+            }
+        }
+    }
 }
