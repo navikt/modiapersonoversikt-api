c67b77e282685818cff26396d34482fec575298c
Nicklas Utgaard <nutgaard@gmail.com>
2022-09-06 12:14:57 +0200

    [OPP-1423] skru av ping mot kontoregister i produksjon
    Løsningen er ikke i produksjon enda, og det gir derfor ikke noe verdi å ha ping'en konfigurert der.
    Når løsningen havner i produksjon kan dette skrues på, og feature-toggle'n `modiabrukerdialog.rest.kontoregister` lages for å gjøre en gradvis overgang til løsningen

---
+++ web/src/main/java/no/nav/modiapersonoversikt/config/ConfigConditions.kt
@@ -0,0 +1,18 @@
+package no.nav.modiapersonoversikt.config
+
+import no.nav.common.utils.EnvironmentUtils.*
+import org.springframework.context.annotation.Condition
+import org.springframework.context.annotation.ConditionContext
+import org.springframework.core.type.AnnotatedTypeMetadata
+
+class InProdCondition : Condition {
+    override fun matches(context: ConditionContext, metadata: AnnotatedTypeMetadata): Boolean {
+        return PROD_CLUSTERS.contains(requireClusterName())
+    }
+}
+
+class InDevCondition : Condition {
+    override fun matches(context: ConditionContext, metadata: AnnotatedTypeMetadata): Boolean {
+        return DEV_CLUSTERS.contains(requireClusterName())
+    }
+}
--- web/src/main/java/no/nav/modiapersonoversikt/consumer/kontoregister/KontoregisterConfig.kt
+++ web/src/main/java/no/nav/modiapersonoversikt/consumer/kontoregister/KontoregisterConfig.kt
@@ -6,16 +6,19 @@ import no.nav.common.rest.client.RestClient
 import no.nav.common.sts.ServiceToServiceTokenProvider
 import no.nav.common.utils.EnvironmentUtils
 import no.nav.modiapersonoversikt.api.domain.kontoregister.generated.apis.KontoregisterV1Api
+import no.nav.modiapersonoversikt.config.InDevCondition
 import no.nav.modiapersonoversikt.infrastructure.http.AuthorizationInterceptor
 import no.nav.modiapersonoversikt.infrastructure.http.LoggingInterceptor
 import no.nav.modiapersonoversikt.infrastructure.types.Pingable
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Conditional
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 open class KontoregisterConfig {
     val cluster = EnvironmentUtils.getRequiredProperty("GCP_CLUSTER")
     val url = EnvironmentUtils.getRequiredProperty("KONTOREGISTER_REST_URL")
+    val isProd = EnvironmentUtils.isProduction()
 
     @Bean
     open fun kontoregisterApi(tokenClient: ServiceToServiceTokenProvider) = KontoregisterV1Api(
@@ -37,6 +40,7 @@ open class KontoregisterConfig {
     )
 
     @Bean
+    @Conditional(InDevCondition::class)
     open fun kontoregisterPing(client: KontoregisterV1Api): Pingable {
         return Pingable {
             SelfTestCheck("Kontoregister - V1", false) {
