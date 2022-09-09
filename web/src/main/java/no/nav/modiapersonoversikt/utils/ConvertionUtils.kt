48d9c952c9b1269b7f27d6c3ed4eb08b419d0ce4
Nicklas Utgaard <nutgaard@gmail.com>
2022-09-09 12:23:22 +0200

    [OPP-1447] slette ubrukt utbetaldata mapping

--- web/src/main/java/no/nav/modiapersonoversikt/utils/ConvertionUtils.kt
+++ web/src/main/java/no/nav/modiapersonoversikt/utils/ConvertionUtils.kt
@@ -3,10 +3,17 @@ package no.nav.modiapersonoversikt.utils
 import java.time.LocalDate
 
 object ConvertionUtils {
+    @JvmStatic
     fun LocalDate.toJodaTime(): org.joda.time.LocalDate {
         return org.joda.time.LocalDate(this.year, this.monthValue, this.dayOfMonth)
     }
+
+    @JvmStatic
     fun org.joda.time.LocalDate.toJavaTime(): LocalDate {
         return LocalDate.of(this.year, this.monthOfYear, this.dayOfMonth)
+    }
+
+    fun org.joda.time.DateTime.toJavaTime(): LocalDate {
+        return this.toLocalDate().toJavaTime()
     }
 }
