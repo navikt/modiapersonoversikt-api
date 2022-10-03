d6ce8c7e7f0ae1594e2ce4f1d9b2543237da81e1
Nicklas Utgaard <nutgaard@gmail.com>
2022-10-03 12:35:43 +0200

    [KAIZEN-0] bruke mer moderene java-opts for java17

--- java-debug.sh
+++ java-debug.sh
@@ -2,6 +2,6 @@
 
 # Enables remote debugging on port 5005 if the application is running in one of the test clusters
 
-if [ "$NAIS_CLUSTER_NAME" = "dev-fss" ] || [ "$NAIS_CLUSTER_NAME" = "dev-sbs" ]; then
-  export JAVA_OPTS="${JAVA_OPTS} -Xdebug -Xrunjdwp:transport=dt_socket,address=5005,server=y,suspend=n"
+if [ "$NAIS_CLUSTER_NAME" = "dev-fss" ] || [ "$NAIS_CLUSTER_NAME" = "dev-sbs" ] || [ "$NAIS_CLUSTER_NAME" = "dev-gcp" ]; then
+  export JAVA_OPTS="${JAVA_OPTS} -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005"
 fi
