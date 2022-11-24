af4f9fd76e9767d8fb39955183c61a39e578eb43
Jesper Paulsen <jesper.gayosso.paulsen@nav.no>
2022-11-24 10:30:00 +0100

    [OPP-1508] Add metricslistener to OkHttpClient

---
+++ web/src/main/java/no/nav/modiapersonoversikt/config/MetricsConfig.kt
@@ -0,0 +1,49 @@
+package no.nav.modiapersonoversikt.config
+
+import io.micrometer.core.instrument.binder.okhttp3.OkHttpMetricsEventListener
+import io.micrometer.prometheus.PrometheusMeterRegistry
+import no.nav.common.rest.client.RestClient
+import no.nav.modiapersonoversikt.service.unleash.Feature
+import no.nav.modiapersonoversikt.service.unleash.UnleashService
+import no.nav.modiapersonoversikt.utils.MaskingUtils
+import no.nav.modiapersonoversikt.utils.UnleashProxySwitcher
+import okhttp3.Request
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Configuration
+import javax.annotation.PostConstruct
+
+@Configuration
+open class MetricsConfig {
+    @Autowired
+    lateinit var prometheusMeterRegistry: PrometheusMeterRegistry
+
+    @Autowired
+    lateinit var unleash: UnleashService
+
+    @PostConstruct
+    fun checkIfMetricsListenerShouldBeEnabled() {
+        return UnleashProxySwitcher.createSwitcher(
+            featureToggle = Feature.USE_REST_CLIENT_METRICS,
+            unleashService = unleash,
+            ifEnabled = initRestClientWithMetricsListener(),
+            ifDisabled = Unit
+        )
+    }
+
+    private fun initRestClientWithMetricsListener() {
+        RestClient.setBaseClient(
+            RestClient.baseClientBuilder().eventListener(
+                buildEventListener().uriMapper(::buildUriMap)
+                    .build()
+            ).build()
+        )
+    }
+
+    private fun buildEventListener(): OkHttpMetricsEventListener.Builder {
+        return OkHttpMetricsEventListener.builder(prometheusMeterRegistry, "okhttp.requests")
+    }
+
+    private fun buildUriMap(req: Request): String {
+        return MaskingUtils.maskSensitiveInfo(req.url().encodedPath())
+    }
+}
