a2df943d35be29ad1dec26436525e573025cf83e
Jesper Paulsen <jesper.gayosso.paulsen@nav.no>
2023-04-13 09:54:13 +0200

    [KAIZEN-0] Fjern referanser til revarsling toggle

--- web/src/main/java/no/nav/modiapersonoversikt/service/unleash/Feature.kt
+++ web/src/main/java/no/nav/modiapersonoversikt/service/unleash/Feature.kt
@@ -4,5 +4,4 @@ enum class Feature(val propertyKey: String) {
     SAMPLE_FEATURE("feature.samplerfeature"),
     USE_REST_KONTOREGISTER("modiabrukerdialog.rest.kontoregister.switcher"),
     USE_NEW_DIALOG_VISNING("modiapersonoversikt.vis-ny-meldingsvisning"),
-    VIS_REVARSLING("modiapersonoversikt.modiapersonoversikt-revarsling-visning")
 }
