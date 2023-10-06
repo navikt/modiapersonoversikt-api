072bd9fd54bcfb51a5eb1fe020292862a29117cd
abrha <abrha.kiros.abrha@nav.no>
2023-10-06 13:23:54 +0200

    [KAIZEN-0] enable feature toggle for KontonummerRegisterService

--- web/src/main/java/no/nav/modiapersonoversikt/service/kontonummer/KontonummerServiceConfig.kt
+++ web/src/main/java/no/nav/modiapersonoversikt/service/kontonummer/KontonummerServiceConfig.kt
@@ -1,5 +1,9 @@
 package no.nav.modiapersonoversikt.service.kontonummer
 
+import no.nav.modiapersonoversikt.consumer.kontoregister.generated.apis.KontoregisterV1Api
+import no.nav.modiapersonoversikt.service.unleash.Feature
+import no.nav.modiapersonoversikt.service.unleash.UnleashService
+import no.nav.modiapersonoversikt.utils.UnleashProxySwitcher
 import no.nav.tjeneste.virksomhet.person.v3.binding.PersonV3
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
@@ -10,6 +14,19 @@ open class KontonummerServiceConfig {
     @Autowired
     private lateinit var tps: PersonV3
 
+
+    @Autowired
+    private lateinit var kontoregister: KontoregisterV1Api
+
+    @Bean
+    open fun kontonummerService(unleash: UnleashService): KontonummerService {
+        return UnleashProxySwitcher.createSwitcher(
+            featureToggle = Feature.USE_REST_KONTOREGISTER,
+            unleashService = unleash,
+            ifEnabled = KontonummerRegisterService(kontoregister),
+            ifDisabled = TpsKontonummerService(tps)
+        )
+    }
     @Bean
     open fun kontonummerService() = TpsKontonummerService(tps)
 }
