51873d650157197566534c5ef9b82cb28fea3e7a
Jesper Paulsen <jesper.gayosso.paulsen@nav.no>
2024-01-03 15:08:22 +0100

    [KAIZEN-0] Legge til nye TPS ingresser

--- web/src/test/java/no/nav/modiapersonoversikt/config/endpoint/util/CacheTestConfig.java
+++ web/src/test/java/no/nav/modiapersonoversikt/config/endpoint/util/CacheTestConfig.java
@@ -9,6 +9,7 @@ import no.nav.modiapersonoversikt.consumer.veilarboppfolging.ArbeidsrettetOppfol
 import no.nav.modiapersonoversikt.consumer.veilarboppfolging.ArbeidsrettetOppfolgingServiceImpl;
 import no.nav.modiapersonoversikt.service.pdl.PdlOppslagService;
 import no.nav.modiapersonoversikt.service.pdl.PdlOppslagServiceImpl;
+import no.nav.modiapersonoversikt.service.unleash.UnleashService;
 import no.nav.tjeneste.virksomhet.sakogbehandling.v1.binding.SakOgBehandlingV1;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -49,4 +50,9 @@ public class CacheTestConfig {
     public PdlOppslagService pdlOppslagService() {
         System.setProperty("PDL_API_URL", "http://dummy.no");
         return mock(PdlOppslagServiceImpl.class); }
+
+    @Bean
+    public UnleashService unleashService() {
+        return mock(UnleashService.class);
+    }
 }
