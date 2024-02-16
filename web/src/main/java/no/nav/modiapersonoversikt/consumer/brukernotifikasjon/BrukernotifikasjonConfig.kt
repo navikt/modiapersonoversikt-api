939a63bef14d0dd6737c6cea7b24c0ab4f0de383
abrha <abrha.kiros.abrha@nav.no>
2024-02-16 10:29:10 +0100

    [OPP-1598] Oppdatere tms-event api i personoversikt-api

--- web/src/main/java/no/nav/modiapersonoversikt/consumer/brukernotifikasjon/BrukernotifikasjonConfig.kt
+++ web/src/main/java/no/nav/modiapersonoversikt/consumer/brukernotifikasjon/BrukernotifikasjonConfig.kt
@@ -4,10 +4,8 @@ import no.nav.common.token_client.client.OnBehalfOfTokenClient
 import no.nav.common.utils.EnvironmentUtils.getRequiredProperty
 import no.nav.modiapersonoversikt.infrastructure.AuthContextUtils
 import no.nav.modiapersonoversikt.infrastructure.http.HeadersInterceptor
-import no.nav.modiapersonoversikt.service.unleash.UnleashService
 import no.nav.modiapersonoversikt.utils.DownstreamApi
 import no.nav.modiapersonoversikt.utils.exchangeOnBehalfOfToken
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
