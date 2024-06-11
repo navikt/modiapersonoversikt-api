ba7cdcfd7baf6c9b079a1489e9ce371300e24018
abrha <abrha.kiros.abrha@nav.no>
2024-06-11 15:35:02 +0200

    [KAIZEN-0] fjern sts

--- web/src/main/java/no/nav/modiapersonoversikt/config/SelftestConfig.java
+++ web/src/main/java/no/nav/modiapersonoversikt/config/SelftestConfig.java
@@ -1,9 +1,7 @@
 package no.nav.modiapersonoversikt.config;
 
-import no.nav.common.cxf.StsConfig;
 import no.nav.common.health.selftest.SelfTestCheck;
 import no.nav.modiapersonoversikt.infrastructure.ping.DiskCheck;
-import no.nav.modiapersonoversikt.infrastructure.ping.StsSoapCheck;
 import no.nav.modiapersonoversikt.infrastructure.ping.TruststoreCheck;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,10 +16,5 @@ public class SelftestConfig {
     @Bean
     public SelfTestCheck truststoreCheck() {
         return TruststoreCheck.asSelftestCheck();
-    }
-
-    @Bean
-    public SelfTestCheck stsSelftestCheck(StsConfig stsConfig) {
-        return StsSoapCheck.asSelftestCheck(stsConfig);
     }
 }
