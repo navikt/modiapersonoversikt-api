39542ac2dbd0858fdf1cc95b0a9ff8f8edcb0c7c
Abrha K. Abrha <116550783+abrhanav@users.noreply.github.com>
2024-09-16 15:04:01 +0200

    [KAIZEN-0] optional fom og tom dato til ytelser filter (#1379)

--- proxy/src/main/java/no/nav/modiapersonoversiktproxy/rest/Utils.kt
+++ proxy/src/main/java/no/nav/modiapersonoversiktproxy/rest/Utils.kt
@@ -3,7 +3,6 @@ package no.nav.modiapersonoversiktproxy.rest
 import kotlinx.serialization.Serializable
 import org.joda.time.format.DateTimeFormat
 import java.time.format.DateTimeFormatter
-import java.time.format.DateTimeFormatterBuilder
 
 @Serializable
 data class RequestBodyContent(
@@ -14,10 +13,3 @@ data class RequestBodyContent(
 
 val JODA_DATOFORMAT: org.joda.time.format.DateTimeFormatter = DateTimeFormat.forPattern("yyyy-MM-dd")
 val DATOFORMAT: DateTimeFormatter = DateTimeFormatter.ISO_LOCAL_DATE
-val DATO_TID_FORMAT: DateTimeFormatter =
-    DateTimeFormatterBuilder()
-        .append(DateTimeFormatter.ISO_LOCAL_DATE)
-        .appendLiteral("T")
-        .appendPattern("HH:mm:ss")
-        .appendOffsetId()
-        .toFormatter()
--- web/src/main/java/no/nav/modiapersonoversikt/rest/Utils.kt
+++ web/src/main/java/no/nav/modiapersonoversikt/rest/Utils.kt
@@ -1,15 +1,5 @@
 package no.nav.modiapersonoversikt.rest
 
 import org.joda.time.format.DateTimeFormat
-import java.time.format.DateTimeFormatter
-import java.time.format.DateTimeFormatterBuilder
 
 val JODA_DATOFORMAT: org.joda.time.format.DateTimeFormatter = DateTimeFormat.forPattern("yyyy-MM-dd")
-val DATOFORMAT: DateTimeFormatter = DateTimeFormatter.ISO_LOCAL_DATE
-val DATO_TID_FORMAT: DateTimeFormatter =
-    DateTimeFormatterBuilder()
-        .append(DateTimeFormatter.ISO_LOCAL_DATE)
-        .appendLiteral("T")
-        .appendPattern("HH:mm:ss")
-        .appendOffsetId()
-        .toFormatter()
