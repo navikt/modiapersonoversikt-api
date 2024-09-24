022ae84782d701cbc84b29a9e7acfcb262fcfd59
Odd Gunnar <oddgunnarfatland@gmail.com>
2024-09-24 14:19:54 +0200

    Rename

--- web/src/main/java/no/nav/modiapersonoversikt/config/interceptor/InterceptorConfig.kt
+++ web/src/main/java/no/nav/modiapersonoversikt/config/interceptor/InterceptorConfig.kt
@@ -3,14 +3,13 @@ package no.nav.modiapersonoversikt.config.interceptor
 import no.nav.modiapersonoversikt.infrastructure.http.LoggingInterceptor
 import no.nav.modiapersonoversikt.service.unleash.UnleashService
 import no.nav.personoversikt.common.logging.TjenestekallLogger
-import okhttp3.Request
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 open class InterceptorConfig {
     @Bean
-    open fun loggingInterceptorFactory(
+    open fun tjenestekallLoggingInterceptorFactory(
         unleashService: UnleashService,
         tjenestekallLogger: TjenestekallLogger,
     ): TjenestekallLoggingInterceptorFactory =
@@ -18,5 +17,3 @@ open class InterceptorConfig {
             LoggingInterceptor(unleashService, tjenestekallLogger, name, interceptor)
         }
 }
-
-typealias TjenestekallLoggingInterceptorFactory = (name: String, interceptor: (Request) -> String) -> LoggingInterceptor
