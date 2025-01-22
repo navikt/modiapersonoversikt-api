1327511724fa6e53f0e4532fb60609cff7c71cdc
abrha <abrha.kiros.abrha@nav.no>
2025-01-22 13:02:55 +0100

    [KAIZEN-0] lint

--- web/src/main/java/no/nav/modiapersonoversikt/consumer/veilarboppfolging/VeilarbvedtaksstotteService.kt
+++ web/src/main/java/no/nav/modiapersonoversikt/consumer/veilarboppfolging/VeilarbvedtaksstotteService.kt
@@ -5,11 +5,11 @@ import no.nav.common.health.HealthCheckUtils
 import no.nav.common.health.selftest.SelfTestCheck
 import no.nav.common.types.identer.Fnr
 import no.nav.common.utils.UrlUtils
-import no.nav.modiapersonoversikt.infrastructure.cache.CacheUtils
-import no.nav.modiapersonoversikt.infrastructure.ping.Pingable
 import no.nav.modiapersonoversikt.domain.veilarbvedtaksstotte.api.generated.apis.Siste14AVedtakV2Api
 import no.nav.modiapersonoversikt.domain.veilarbvedtaksstotte.api.generated.models.Siste14aVedtakDTO
 import no.nav.modiapersonoversikt.domain.veilarbvedtaksstotte.api.generated.models.Siste14aVedtakRequest
+import no.nav.modiapersonoversikt.infrastructure.cache.CacheUtils
+import no.nav.modiapersonoversikt.infrastructure.ping.Pingable
 import java.time.LocalDateTime
 
 interface VeilarbvedtaksstotteService : Pingable {
@@ -27,18 +27,20 @@ class VeilarbvedtaksstotteServiceImpl(
     private val siste14AVedtakV2Api: Siste14AVedtakV2Api,
     private val cache: Cache<Fnr, Siste14aVedtak> = CacheUtils.createCache(),
 ) : VeilarbvedtaksstotteService {
-
     override fun hentSiste14aVedtak(fnr: Fnr): Siste14aVedtak? =
-            cache.get(fnr) {
-                siste14AVedtakV2Api.hentSiste14aVedtak(Siste14aVedtakRequest(fnr.get()))?.let { mapToSiste14aVedtak(it) }
-            }
+        cache.get(fnr) {
+            siste14AVedtakV2Api.hentSiste14aVedtak(Siste14aVedtakRequest(fnr.get()))?.let { mapToSiste14aVedtak(it) }
+        }
 
     override fun ping() =
         SelfTestCheck(
             "VeilarbvedtaksstotteApi via ${siste14AVedtakV2Api.client.baseUrl}",
             false,
         ) {
-            HealthCheckUtils.pingUrl(UrlUtils.joinPaths(siste14AVedtakV2Api.client.baseUrl, "/internal/health/liveness"), siste14AVedtakV2Api.client.client)
+            HealthCheckUtils.pingUrl(
+                UrlUtils.joinPaths(siste14AVedtakV2Api.client.baseUrl, "/internal/health/liveness"),
+                siste14AVedtakV2Api.client.client,
+            )
         }
 
     private fun mapToSiste14aVedtak(dto: Siste14aVedtakDTO) =
@@ -46,7 +48,6 @@ class VeilarbvedtaksstotteServiceImpl(
             innsatsgruppe = dto.innsatsgruppe.value,
             hovedmal = dto.hovedmal?.value,
             fattetDato = dto.fattetDato.toLocalDateTime(),
-            fraArena = dto.fraArena
+            fraArena = dto.fraArena,
         )
-
 }
