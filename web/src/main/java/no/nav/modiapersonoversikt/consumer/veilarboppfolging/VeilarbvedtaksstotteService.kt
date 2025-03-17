767725a8228ff5af751429aa3c1dc43b57c97ecd
abrha <abrha.kiros.abrha@nav.no>
2025-03-17 14:15:21 +0100

    [KAIZEN-0] gjeldende 14a vedtak api

--- web/src/main/java/no/nav/modiapersonoversikt/consumer/veilarboppfolging/VeilarbvedtaksstotteService.kt
+++ web/src/main/java/no/nav/modiapersonoversikt/consumer/veilarboppfolging/VeilarbvedtaksstotteService.kt
@@ -5,8 +5,8 @@ import no.nav.common.health.HealthCheckUtils
 import no.nav.common.health.selftest.SelfTestCheck
 import no.nav.common.types.identer.Fnr
 import no.nav.common.utils.UrlUtils
+import no.nav.modiapersonoversikt.domain.veilarbvedtaksstotte.api.generated.apis.Gjeldende14AVedtakApi
 import no.nav.modiapersonoversikt.domain.veilarbvedtaksstotte.api.generated.apis.KodeverkFor14AVedtakApi
-import no.nav.modiapersonoversikt.domain.veilarbvedtaksstotte.api.generated.apis.Siste14AVedtakV2Api
 import no.nav.modiapersonoversikt.domain.veilarbvedtaksstotte.api.generated.models.*
 import no.nav.modiapersonoversikt.infrastructure.cache.CacheUtils
 import no.nav.modiapersonoversikt.infrastructure.ping.Pingable
@@ -14,34 +14,43 @@ import java.time.Duration
 import java.time.LocalDateTime
 
 interface VeilarbvedtaksstotteService : Pingable {
-    fun hentSiste14aVedtak(fodselsnummer: Fnr): Siste14aVedtak?
+    fun hentGjeldende14aVedtak(fodselsnummer: Fnr): Gjeldende14aVedtak?
 }
 
-data class Siste14aVedtakResponse(
-    val siste14aVedtak: Siste14aVedtak?,
+data class Gjeldende14aVedtakResponse(
+    val gjeldende14aVedtak: Gjeldende14aVedtak?,
 )
 
-data class Siste14aVedtak(
-    val innsatsgruppe: InnsatsgruppeDetaljert,
-    val hovedmal: HovedmalDetaljert?,
+data class Gjeldende14aVedtak(
+    val innsatsgruppe: Innsatsgruppe,
+    val hovedmal: Hovedmal?,
     val fattetDato: LocalDateTime,
-    val fraArena: Boolean?,
+)
+
+data class Innsatsgruppe(
+    val kode: String,
+    val beskrivelse: String,
+)
+
+data class Hovedmal(
+    val kode: String,
+    val beskrivelse: String,
 )
 
 class VeilarbvedtaksstotteServiceImpl(
-    private val siste14AVedtakV2Api: Siste14AVedtakV2Api,
+    private val gjeldende14AVedtakApi: Gjeldende14AVedtakApi,
     private val kodeverkFor14AVedtakApi: KodeverkFor14AVedtakApi,
-    private val cache: Cache<Fnr, Siste14aVedtak> = CacheUtils.createCache(),
-    private val innsatsgruppeCache: Cache<String, InnsatsgruppeDetaljert> = CacheUtils.createCache(expireAfterWrite = Duration.ofHours(24)),
-    private val hovedmalCache: Cache<String, HovedmalDetaljert> = CacheUtils.createCache(expireAfterWrite = Duration.ofHours(24)),
+    private val cache: Cache<Fnr, Gjeldende14aVedtak> = CacheUtils.createCache(),
+    private val innsatsgruppeCache: Cache<String, Innsatsgruppe> = CacheUtils.createCache(expireAfterWrite = Duration.ofHours(24)),
+    private val hovedmalCache: Cache<String, Hovedmal> = CacheUtils.createCache(expireAfterWrite = Duration.ofHours(24)),
 ) : VeilarbvedtaksstotteService {
     init {
         prepopulerCache()
     }
 
-    override fun hentSiste14aVedtak(fnr: Fnr): Siste14aVedtak? =
+    override fun hentGjeldende14aVedtak(fnr: Fnr): Gjeldende14aVedtak? =
         cache.get(fnr) {
-            siste14AVedtakV2Api.hentSiste14aVedtak(Siste14aVedtakRequest(fnr.get()))?.let { mapToSiste14aVedtak(it) }
+            gjeldende14AVedtakApi.hentGjeldende14aVedtakEksternt(Gjeldende14aVedtakRequest(fnr.get()))?.let { mapToGjeldende14aVedtak(it) }
         }
 
     override fun ping() =
@@ -55,28 +64,32 @@ class VeilarbvedtaksstotteServiceImpl(
             )
         }
 
-    private fun mapToSiste14aVedtak(dto: Siste14aVedtakDTO) =
-        Siste14aVedtak(
+    private fun mapToGjeldende14aVedtak(dto: Gjeldende14aVedtakDto) =
+        Gjeldende14aVedtak(
             innsatsgruppe = innsatsgruppeCache.get(dto.innsatsgruppe.value) { getInnsatsgruppe(dto.innsatsgruppe.value) },
             hovedmal = dto.hovedmal?.let { hovedmal -> hovedmalCache.get(hovedmal.value) { getHovedmal(hovedmal.value) } },
             fattetDato = dto.fattetDato.toLocalDateTime(),
-            fraArena = dto.fraArena,
         )
 
-    private fun getInnsatsgruppe(key: String): InnsatsgruppeDetaljert? {
-        kodeverkFor14AVedtakApi.getInnsatsgrupper()?.map { innsatsgruppeCache.put(it.kode, it) }
+    private fun getInnsatsgruppe(key: String): Innsatsgruppe? {
+        kodeverkFor14AVedtakApi.getInnsatsgrupper()?.map {
+            innsatsgruppeCache.put(
+                it.kode.value,
+                Innsatsgruppe(it.kode.value, it.beskrivelse),
+            )
+        }
         return innsatsgruppeCache.getIfPresent(key)
     }
 
-    private fun getHovedmal(key: String): HovedmalDetaljert? {
-        kodeverkFor14AVedtakApi.getHovedmal()?.map { hovedmalCache.put(it.kode, it) }
+    private fun getHovedmal(key: String): Hovedmal? {
+        kodeverkFor14AVedtakApi.getHovedmal()?.map { hovedmalCache.put(it.kode.value, Hovedmal(it.kode.value, it.beskrivelse)) }
         return hovedmalCache.getIfPresent(key)
     }
 
     private fun prepopulerCache() {
-        kodeverkFor14AVedtakApi.getKodeverk()?.let { kodeverk ->
-            kodeverk.innsatsgrupper?.let { innsatsgruppe -> innsatsgruppeCache.putAll(innsatsgruppe.associateBy { it.kode }) }
-            kodeverk.hovedmal?.let { hovedmal -> hovedmalCache.putAll(hovedmal.associateBy { it.kode }) }
+        kodeverkFor14AVedtakApi.getInnsatsgruppeOgHovedmalKodeverk()?.let { kodeverk ->
+            kodeverk.innsatsgrupper.map { innsatsgruppeCache.put(it.kode.value, Innsatsgruppe(it.kode.value, it.beskrivelse)) }
+            kodeverk.hovedmal.map { hovedmalCache.put(it.kode.value, Hovedmal(it.kode.value, it.beskrivelse)) }
         }
     }
 }
--- web/src/test/java/no/nav/modiapersonoversikt/service/oppfolgingsinfo/VeilarbvedtaksstotteServiceTest.kt
+++ web/src/test/java/no/nav/modiapersonoversikt/service/oppfolgingsinfo/VeilarbvedtaksstotteServiceTest.kt
@@ -5,55 +5,62 @@ import io.mockk.mockk
 import io.mockk.verifySequence
 import no.nav.common.types.identer.Fnr
 import no.nav.modiapersonoversikt.consumer.veilarboppfolging.VeilarbvedtaksstotteServiceImpl
+import no.nav.modiapersonoversikt.domain.veilarbvedtaksstotte.api.generated.apis.Gjeldende14AVedtakApi
 import no.nav.modiapersonoversikt.domain.veilarbvedtaksstotte.api.generated.apis.KodeverkFor14AVedtakApi
-import no.nav.modiapersonoversikt.domain.veilarbvedtaksstotte.api.generated.apis.Siste14AVedtakV2Api
-import no.nav.modiapersonoversikt.domain.veilarbvedtaksstotte.api.generated.models.HovedmalDetaljert
-import no.nav.modiapersonoversikt.domain.veilarbvedtaksstotte.api.generated.models.InnsatsgruppeDetaljert
-import no.nav.modiapersonoversikt.domain.veilarbvedtaksstotte.api.generated.models.KodeverkDTO
-import no.nav.modiapersonoversikt.domain.veilarbvedtaksstotte.api.generated.models.Siste14aVedtakDTO
+import no.nav.modiapersonoversikt.domain.veilarbvedtaksstotte.api.generated.models.*
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.time.OffsetDateTime
 
 class VeilarbvedtaksstotteServiceTest {
-    private val siste14AVedtakV2Api: Siste14AVedtakV2Api = mockk()
+    private val gjeldende14AVedtakApi: Gjeldende14AVedtakApi = mockk()
     private val kodeverkFor14AVedtakApi: KodeverkFor14AVedtakApi = mockk()
     private val fnr = Fnr("12345678910")
 
     @BeforeEach
     fun setupStandardMocker() {
-        every { siste14AVedtakV2Api.hentSiste14aVedtak(any()) } answers {
-            Siste14aVedtakDTO(
-                innsatsgruppe = Siste14aVedtakDTO.Innsatsgruppe.STANDARD_INNSATS,
-                hovedmal = Siste14aVedtakDTO.Hovedmal.SKAFFE_ARBEID,
+        every { gjeldende14AVedtakApi.hentGjeldende14aVedtakEksternt(any()) } answers {
+            Gjeldende14aVedtakDto(
+                innsatsgruppe = Gjeldende14aVedtakDto.Innsatsgruppe.GODE_MULIGHETER,
+                hovedmal = Gjeldende14aVedtakDto.Hovedmal.SKAFFE_ARBEID,
                 fattetDato = OffsetDateTime.now(),
-                fraArena = false,
             )
         }
-        every { kodeverkFor14AVedtakApi.getKodeverk() } answers {
-            KodeverkDTO(
+        every { kodeverkFor14AVedtakApi.getInnsatsgruppeOgHovedmalKodeverk() } answers {
+            KodeverkV2DTO(
                 innsatsgrupper =
                     listOf(
-                        InnsatsgruppeDetaljert(kode = "STANDARD_INNSATS", beskrivelse = "Standard innsats"),
+                        InnsatsgruppeKodeverkV2DTO(
+                            kode = InnsatsgruppeKodeverkV2DTO.Kode.GODE_MULIGHETER,
+                            beskrivelse = "GODE MULIGHETER",
+                            gammelKode = InnsatsgruppeKodeverkV2DTO.GammelKode.STANDARD_INNSATS,
+                            arenaKode = InnsatsgruppeKodeverkV2DTO.ArenaKode.BATT,
+                        ),
                     ),
-                hovedmal = listOf(HovedmalDetaljert(kode = "SKAFFE_ARBEID", beskrivelse = "Skaffe arbeide")),
+                hovedmal =
+                    listOf(
+                        HovedmalKodeverkV2DTO(
+                            kode = HovedmalKodeverkV2DTO.Kode.SKAFFE_ARBEID,
+                            beskrivelse = "Skaffe arbeide",
+                        ),
+                    ),
             )
         }
     }
 
     @Test
-    fun `hent Siste 14a Vedtak`() {
+    fun `hent gjeldende 14a Vedtak`() {
         val veilarbvedtaksstotteService =
-            VeilarbvedtaksstotteServiceImpl(siste14AVedtakV2Api, kodeverkFor14AVedtakApi)
-        val siste14aVedtak = veilarbvedtaksstotteService.hentSiste14aVedtak(fnr)
+            VeilarbvedtaksstotteServiceImpl(gjeldende14AVedtakApi, kodeverkFor14AVedtakApi)
+        val gjeldende14aVedtak = veilarbvedtaksstotteService.hentGjeldende14aVedtak(fnr)
 
         verifySequence {
-            kodeverkFor14AVedtakApi.getKodeverk()
-            siste14AVedtakV2Api.hentSiste14aVedtak(any())
+            kodeverkFor14AVedtakApi.getInnsatsgruppeOgHovedmalKodeverk()
+            gjeldende14AVedtakApi.hentGjeldende14aVedtakEksternt(any())
         }
 
-        assertThat(siste14aVedtak?.innsatsgruppe?.kode).isEqualTo("STANDARD_INNSATS")
-        assertThat(siste14aVedtak?.hovedmal?.kode).isEqualTo("SKAFFE_ARBEID")
+        assertThat(gjeldende14aVedtak?.innsatsgruppe?.kode).isEqualTo("GODE_MULIGHETER")
+        assertThat(gjeldende14aVedtak?.hovedmal?.kode).isEqualTo("SKAFFE_ARBEID")
     }
 }
