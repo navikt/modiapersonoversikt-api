3777edcd40c295322a67e3ee8204c839af1d92b9
Ludvig Hozman <ludvig123@gmail.com>
2025-04-23 12:07:09 +0200

    Fikse nullable DTOer for ansatte og enheter
    Og gjør om til kotlin

--- web/src/main/java/no/nav/modiapersonoversikt/service/ansattservice/domain/Ansatt.java
+++
@@ -1,6 +0,0 @@
-package no.nav.modiapersonoversikt.service.ansattservice.domain;
-
-import java.io.Serializable;
-
-public record Ansatt(String fornavn, String etternavn, String ident) implements Serializable {
-}
---
+++ web/src/main/java/no/nav/modiapersonoversikt/service/ansattservice/domain/Ansatt.kt
@@ -0,0 +1,7 @@
+package no.nav.modiapersonoversikt.service.ansattservice.domain
+
+data class Ansatt(
+    val fornavn: String,
+    val etternavn: String,
+    val ident: String,
+)
