ceda316ab69af6c9dac79f57f87a76f98241aa0a
abrha <abrha.kiros.abrha@nav.no>
2025-08-01 15:28:47 +0200

    [KAIZEN-0] error handling for proxy-api

--- arena-infotrygd-models/src/main/java/no/nav/modiapersonoversikt/commondomain/Periode.kt
+++ arena-infotrygd-models/src/main/java/no/nav/modiapersonoversikt/commondomain/Periode.kt
@@ -1,55 +1,26 @@
-package no.nav.modiapersonoversikt.commondomain;
+package no.nav.modiapersonoversikt.commondomain
 
-import org.joda.time.LocalDate;
-import org.joda.time.LocalDateTime;
+import org.joda.time.LocalDate
+import org.joda.time.LocalDateTime
+import java.io.Serializable
 
-import java.io.Serializable;
-
-public class Periode implements Serializable {
-
-    private LocalDate from;
-    private LocalDate to;
-
-    public Periode() {
+data class Periode(
+    var from: LocalDate? = null,
+    var to: LocalDate? = null
+) : Serializable {
+    constructor(from: LocalDateTime, toan: LocalDateTime) : this() {
+        this.from = LocalDate(from)
+        this.to = LocalDate(toan)
     }
 
-    public Periode(LocalDate from, LocalDate to) {
-        this.from = from;
-        this.to = to;
-    }
+    fun erGyldig(): Boolean {
+        val today = LocalDate()
 
-    public Periode(LocalDateTime from, LocalDateTime toan) {//Ikke rename toan til to, da brekker plutselig orika mapping
-        this.from = new LocalDate(from);
-        this.to = new LocalDate(toan);
-    }
-
-    public LocalDate getFrom() {
-        return from;
-    }
-
-    public void setFrom(LocalDate from) {
-        this.from = from;
-    }
-
-    public LocalDate getTo() {
-        return to;
-    }
-
-    public void setTo(LocalDate to) {
-        this.to = to;
-    }
-
-    public boolean erGyldig() {
-        LocalDate today = new LocalDate();
-
-        if (from == null) {
-            return false;
+        return when {
+            from == null -> false
+            to == null -> today.isEqual(from) || today.isAfter(from)
+            else -> (today.isEqual(from) || today.isAfter(from)) &&
+                    (today.isEqual(to) || today.isBefore(to))
         }
-
-        if (to == null) {
-            return (today.isEqual(from) || today.isAfter(from));
-        }
-
-        return (today.isEqual(from) || today.isAfter(from)) && (today.isEqual(to) || today.isBefore(to));
     }
 }
