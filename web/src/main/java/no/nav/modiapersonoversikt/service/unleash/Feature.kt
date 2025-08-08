55f9eff23c2c25bb9a00188db0a37cbe4bb32c80
Nummedal <sigrun.asheim.nummedal@nav.no>
2025-08-08 13:21:14 +0200

    Fjern feature toggle for utvidet betaling spørring

--- web/src/main/java/no/nav/modiapersonoversikt/service/unleash/Feature.kt
+++ web/src/main/java/no/nav/modiapersonoversikt/service/unleash/Feature.kt
@@ -4,9 +4,7 @@ enum class Feature(
     val propertyKey: String,
 ) {
     SAMPLE_FEATURE("feature.samplerfeature"),
-    UTVIDET_UTBETALINGS_SPORRING("modiapersonoversikt.utvidet-utbetalings-sporring"),
     LOG_REQUEST_BODY("modiapersonoversikt.log-request-body"),
     LOG_RESPONSE_BODY("modiapersonoversikt.log-response-body"),
     TJENESTEKALL_LOGGING("modiapersonoversikt.tjenestekall-logging"),
-    VIS_14A_VEDTAK("modiapersonoversikt.vis-siste-14a-vedtak"),
 }
--- web/src/main/java/no/nav/modiapersonoversikt/service/utbetaling/UtbetalingServiceConfig.kt
+++ web/src/main/java/no/nav/modiapersonoversikt/service/utbetaling/UtbetalingServiceConfig.kt
@@ -1,7 +1,6 @@
 package no.nav.modiapersonoversikt.service.utbetaling
 
 import no.nav.modiapersonoversikt.api.domain.utbetaling.generated.apis.UtbetaldataV2Api
-import no.nav.modiapersonoversikt.service.unleash.UnleashService
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -10,8 +9,5 @@ import org.springframework.context.annotation.Configuration
 @EnableCaching
 open class UtbetalingServiceConfig {
     @Bean
-    open fun utbetalingerService(
-        restClient: UtbetaldataV2Api,
-        unleash: UnleashService,
-    ): UtbetalingService = UtbetalingServiceImpl(restClient, unleash)
+    open fun utbetalingerService(restClient: UtbetaldataV2Api): UtbetalingService = UtbetalingServiceImpl(restClient)
 }
