c0a4968bea7011e73e94fd269a28fb2a60b8e871
Nummedal <sigrun.asheim.nummedal@nav.no>
2025-08-20 13:03:58 +0200

    Revert team logs

--- web/src/main/java/no/nav/modiapersonoversikt/service/oppgavebehandling/RestOppgaveBehandlingServiceImpl.kt
+++ web/src/main/java/no/nav/modiapersonoversikt/service/oppgavebehandling/RestOppgaveBehandlingServiceImpl.kt
@@ -53,7 +53,7 @@ class RestOppgaveBehandlingServiceImpl(
         }, tjenestekallLoggingInterceptorFactory),
     private val clock: Clock = Clock.systemDefaultZone(),
 ) : OppgaveBehandlingService {
-    private val tjenestekallLogg = Logging.teamLog
+    private val tjenestekallLogg = Logging.secureLog
 
     override fun opprettOppgave(request: OpprettOppgaveRequest?): OpprettOppgaveResponse {
         requireNotNull(request)
@@ -164,7 +164,6 @@ class RestOppgaveBehandlingServiceImpl(
             return
         } else if (tvungenTilordning) {
             tjenestekallLogg.warn(
-                Logging.TEAM_LOGS_MARKER,
                 "[OPPGAVE] $ident gjorde en tvungen tilordning av $oppgaveId, som allerede var tildelt ${oppgave.tilordnetRessurs}",
             )
         } else if (oppgave.tilordnetRessurs != null) {
@@ -371,7 +370,6 @@ class RestOppgaveBehandlingServiceImpl(
             ).getWithFailureHandling { failures ->
                 val oppgaveIds = failures.joinToString(", ") { it.id?.toString() ?: "Mangler oppgave id" }
                 tjenestekallLogg.warn(
-                    Logging.TEAM_LOGS_MARKER,
                     "[OPPGAVE] hentOppgaverPaginertOgTilgangskontroll la tilbake oppgaver pga manglende tilgang: $oppgaveIds",
                 )
                 systemLeggTilbakeOppgaver(failures)
