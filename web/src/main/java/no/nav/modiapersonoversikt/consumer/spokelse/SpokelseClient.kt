eb6a2d02b17efc056c61a421bc7a467de2e53d8a
Abrha.Kiros.Abrha <abrha.kiros.abrha@nav.no>
2025-12-18 15:36:01 +0100

    [KAIZEN-0] lint

--- web/src/main/java/no/nav/modiapersonoversikt/consumer/spokelse/SpokelseClient.kt
+++ web/src/main/java/no/nav/modiapersonoversikt/consumer/spokelse/SpokelseClient.kt
@@ -62,7 +62,7 @@ open class SpokelseClientImpl(
 
         val body = response.body?.string()
 
-        val perioder =  objectMapper.readValue(body, Utbetalingsperioder::class.java)
+        val perioder = objectMapper.readValue(body, Utbetalingsperioder::class.java)
 
         return Utbetalingsperioder(
             utbetaltePerioder = perioder.utbetaltePerioder.sortedByDescending { it.fom },
