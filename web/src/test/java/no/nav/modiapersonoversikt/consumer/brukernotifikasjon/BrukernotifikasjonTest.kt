427e93f690498b180ae1edd3f77590fa61c7ed63
martinsomm-nav <martin.dagfinn.boler.sommerseth@nav.no>
2026-02-19 14:28:14 +0100

    Lint fix

--- web/src/test/java/no/nav/modiapersonoversikt/consumer/brukernotifikasjon/BrukernotifikasjonTest.kt
+++ web/src/test/java/no/nav/modiapersonoversikt/consumer/brukernotifikasjon/BrukernotifikasjonTest.kt
@@ -33,10 +33,11 @@ class BrukernotifikasjonTest {
     fun `skal sette renotifikasjonSendt til true naar opprettet foer cutover og sist oppdatert ikke er samme dato som opprettet`() {
         val opprettet = ZonedDateTime.parse("2026-01-20T09:00:00.000Z")
         val sistOppdatert = ZonedDateTime.parse("2026-01-25T10:00:00.000Z")
-        val event = getEvent().copy(
-            opprettet = opprettet,
-            eksternVarsling = getEvent().eksternVarsling?.copy(sistOppdatert = sistOppdatert)
-        )
+        val event =
+            getEvent().copy(
+                opprettet = opprettet,
+                eksternVarsling = getEvent().eksternVarsling?.copy(sistOppdatert = sistOppdatert),
+            )
         val mappedEvent = Brukernotifikasjon.Mapper.lagVarselFraEvent(event)
 
         assertEquals(true, mappedEvent.eksternVarsling.renotifikasjonSendt)
@@ -52,47 +53,46 @@ class BrukernotifikasjonTest {
         assertEquals(mappedEvent.eksternVarsling.renotifikasjonTidspunkt, null)
     }
 
-    private fun getEvent() : Brukernotifikasjon.Event {
-        return Brukernotifikasjon.Event(
-                type = Brukernotifikasjon.Type.OPPGAVE,
-                varselId = "12345",
-                aktiv = true,
-                produsent =
-                    Brukernotifikasjon.Produsent(
-                        namespace = "test-space",
-                        appnavn = "et app-navn",
-                    ),
-                sensitivitet = "high",
-                innhold =
-                    Brukernotifikasjon.Innhold(
-                        tekst = "Et nytt samtalereferat er tilgjengelig i din innboks",
-                        link = "https://nav.test.no",
-                    ),
-                eksternVarsling =
-                    Brukernotifikasjon.EksternVarslingInfo(
-                        sendt = true,
-                        sendtTidspunkt = ZonedDateTime.parse("2026-01-29T09:03:00.000Z"),
-                        sendtSomBatch = false,
-                        renotifikasjonSendt = true,
-                        renotifikasjonTidspunkt = ZonedDateTime.parse("2026-02-08T10:00:00.000Z"),
-                        kanaler = listOf("SMS", "EPOST"),
-                        feilHistorikk =
-                            listOf(
-                                Brukernotifikasjon.Feilhistorikk(
-                                    feilmelding = "En feil",
-                                    tidspunkt = ZonedDateTime.parse("2026-01-31T09:02:00.000Z"),
-                                ),
-                                Brukernotifikasjon.Feilhistorikk(
-                                    feilmelding = "Enda en feil",
-                                    tidspunkt = ZonedDateTime.parse("2026-02-06T09:01:11.000Z"),
-                                ),
+    private fun getEvent(): Brukernotifikasjon.Event =
+        Brukernotifikasjon.Event(
+            type = Brukernotifikasjon.Type.OPPGAVE,
+            varselId = "12345",
+            aktiv = true,
+            produsent =
+                Brukernotifikasjon.Produsent(
+                    namespace = "test-space",
+                    appnavn = "et app-navn",
+                ),
+            sensitivitet = "high",
+            innhold =
+                Brukernotifikasjon.Innhold(
+                    tekst = "Et nytt samtalereferat er tilgjengelig i din innboks",
+                    link = "https://nav.test.no",
+                ),
+            eksternVarsling =
+                Brukernotifikasjon.EksternVarslingInfo(
+                    sendt = true,
+                    sendtTidspunkt = ZonedDateTime.parse("2026-01-29T09:03:00.000Z"),
+                    sendtSomBatch = false,
+                    renotifikasjonSendt = true,
+                    renotifikasjonTidspunkt = ZonedDateTime.parse("2026-02-08T10:00:00.000Z"),
+                    kanaler = listOf("SMS", "EPOST"),
+                    feilHistorikk =
+                        listOf(
+                            Brukernotifikasjon.Feilhistorikk(
+                                feilmelding = "En feil",
+                                tidspunkt = ZonedDateTime.parse("2026-01-31T09:02:00.000Z"),
                             ),
-                        sistOppdatert = ZonedDateTime.parse("2026-02-08T10:00:00.000Z"),
-                    ),
-                opprettet = ZonedDateTime.parse("2026-01-29T09:03:00.000Z"),
-                aktivFremTil = ZonedDateTime.parse("2026-02-18T10:00:00.000Z"),
-                inaktivert = null,
-                inaktivertAv = null,
-            )
-    }
+                            Brukernotifikasjon.Feilhistorikk(
+                                feilmelding = "Enda en feil",
+                                tidspunkt = ZonedDateTime.parse("2026-02-06T09:01:11.000Z"),
+                            ),
+                        ),
+                    sistOppdatert = ZonedDateTime.parse("2026-02-08T10:00:00.000Z"),
+                ),
+            opprettet = ZonedDateTime.parse("2026-01-29T09:03:00.000Z"),
+            aktivFremTil = ZonedDateTime.parse("2026-02-18T10:00:00.000Z"),
+            inaktivert = null,
+            inaktivertAv = null,
+        )
 }
