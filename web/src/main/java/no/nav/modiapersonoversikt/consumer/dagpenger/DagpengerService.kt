60c949c6631e731d1ed667bebf29b559848eb444
nocturnalnematode <106680091+nocturnalnematode@users.noreply.github.com>
2026-03-17 13:39:23 +0100

    use openapi provided client, save test snapshot

--- web/src/main/java/no/nav/modiapersonoversikt/consumer/dagpenger/DagpengerService.kt
+++ web/src/main/java/no/nav/modiapersonoversikt/consumer/dagpenger/DagpengerService.kt
@@ -1,17 +1,8 @@
 package no.nav.modiapersonoversikt.consumer.dagpenger
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.readValue
+import no.nav.modiapersonoversikt.consumer.dagpenger.generated.apis.DefaultApi
 import no.nav.modiapersonoversikt.consumer.dagpenger.generated.models.DatadelingRequestDagpengerDto
-import no.nav.modiapersonoversikt.consumer.dagpenger.generated.models.DatadelingResponseDagpengerDto
 import no.nav.modiapersonoversikt.consumer.dagpenger.generated.models.PeriodeDagpengerDto
-import no.nav.modiapersonoversikt.infrastructure.http.OkHttpUtils
-import no.nav.modiapersonoversikt.infrastructure.http.OkHttpUtils.objectMapper
-import okhttp3.MediaType.Companion.toMediaTypeOrNull
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.RequestBody.Companion.toRequestBody
 import java.time.LocalDate
 
 /**
@@ -35,34 +26,12 @@ interface DagpengerService {
 }
 
 open class DagpengerServiceImpl(
-    val baseUrl: String,
-    val client: OkHttpClient,
+    val client: DefaultApi,
 ) : DagpengerService {
     override fun hentVedtak(datodelingRequest: DatadelingRequestDagpengerDto): PseudoDagpengerVedtak {
-        val url = "$baseUrl/dagpenger/datadeling/v1/perioder"
-        val mapper = ObjectMapper()
-        mapper.registerModule(JavaTimeModule())
-        val requestContent =
-            mapper.writeValueAsString(
-                datodelingRequest,
-            )
-        val requestBody =
-            requestContent
-                .toRequestBody("application/json".toMediaTypeOrNull())
-        val response =
-            client
-                .newCall(
-                    Request
-                        .Builder()
-                        .url(url)
-                        .post(requestBody)
-                        .build(),
-                ).execute()
-
-        // TODO throw exception here maybe
-        if (response.body?.contentLength() == 0L) return PseudoDagpengerVedtak(listOf())
-        return response.body?.let {
-            PseudoDagpengerVedtak((OkHttpUtils.objectMapper.readValue(it.string()) as DatadelingResponseDagpengerDto).perioder)
-        } ?: PseudoDagpengerVedtak(listOf())
+        val response = client.dagpengerDatadelingV1PerioderPost(datodelingRequest)
+        // the above should throw an exception upon failure, so we can safely
+        // assume we have a DatadelingResponseDagpengerDto.
+        return PseudoDagpengerVedtak(response!!.perioder)
     }
 }
