d646822364f3f187ed1d9a17e1c53a5c37511d9c
Nummedal <sigrun.asheim.nummedal@nav.no>
2026-04-20 15:32:17 +0200

    Legg til en dag på chung start sykepengerperiode

--- proxy/src/main/java/no/nav/modiapersonoversikt/rest/ytelse/SykepengerUttrekk.kt
+++ proxy/src/main/java/no/nav/modiapersonoversikt/rest/ytelse/SykepengerUttrekk.kt
@@ -49,8 +49,8 @@ class SykepengerUttrekk constructor(
         do {
             val chunkEnd = if (chunkStart.plusYears(2).isBefore(to)) chunkStart.plusYears(2) else to
             perioder.add(Periode(chunkStart, chunkEnd))
-            chunkStart = chunkEnd
-        } while (chunkStart.isBefore(to))
+            chunkStart = chunkEnd.plusDays(1)
+        } while (chunkStart.isBefore(to) || chunkStart.isEqual(to))
         return perioder
     }
 
