20ab117d31e95eea94adb5151a8542a229bd38b7
Nummedal <sigrun.asheim.nummedal@nav.no>
2026-06-12 09:19:42 +0200

    Saksmodell for dokumenter bruker fortsatt saksid

--- web/src/main/java/no/nav/modiapersonoversikt/rest/saker/SakerController.kt
+++ web/src/main/java/no/nav/modiapersonoversikt/rest/saker/SakerController.kt
@@ -177,6 +177,7 @@ class SakerController
             val saker =
                 this.saker.map {
                     Sak()
+                        .withSaksId(it.fagsystemSaksId)
                         .withFagsaksnummer(it.fagsystemSaksId)
                         .withTemakode(it.temaKode)
                         .withBaksystem(Baksystem.SAF)
