8e52fcc5f598befa39b08800fb7582c1994c2a4e
Nummedal <sigrun.asheim.nummedal@nav.no>
2026-08-24 08:59:26 +0200

    Oppdater @WebMvcTest-import og importer ObjectMapperConfig i tester

--- web/src/test/java/no/nav/modiapersonoversikt/rest/enhet/EnhetControllerSnapshotTest.kt
+++ web/src/test/java/no/nav/modiapersonoversikt/rest/enhet/EnhetControllerSnapshotTest.kt
@@ -15,7 +15,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.test.web.servlet.MockMvc
--- web/src/test/java/no/nav/modiapersonoversikt/rest/featuretoggle/FeatureToggleControllerTest.kt
+++ web/src/test/java/no/nav/modiapersonoversikt/rest/featuretoggle/FeatureToggleControllerTest.kt
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.test.web.servlet.MockMvc
